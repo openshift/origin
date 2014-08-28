b19e6d7235742f9b53243c80704c2e1c146d71e5
Vojtech Vitek (V-Teq) <vvitek@redhat.com>
2014-08-28 13:25:04 +0200

    Add Godoc files to all OpenShift 3 packages

---
+++ pkg/cmd/client/doc.go
@@ -0,0 +1,4 @@
+// Package client contains the implementation of the client side communication with the
+// Kubernetes master. OpenShift currently embeds the Kubernetes's kubecfg command line
+// for prototyping and debugging.
+package client
---
+++ pkg/service/doc.go
@@ -0,0 +1,3 @@
+// Package service contains a generic ServiceRegistry interface and
+// implements the storage for the Service api server objects.
+package service
