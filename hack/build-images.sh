d920810078c2c0c67dc392141c6fdcbb930dd4f8
Clayton Coleman <ccoleman@redhat.com>
2014-09-08 23:32:29 -0400

    Script building the docker images

---
+++ hack/build-images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script sets up a go workspace locally and builds all go components.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+hackdir=$(CDPATH="" cd $(dirname $0); pwd)
+
+# Set the environment variables required by the build.
+. "${hackdir}/config-go.sh"
+
+# Go to the top of the tree.
+cd "${OS_REPO_ROOT}"
+
+# Fetch the version.
+version=$(gitcommit)
+kube_version=$(go run ${hackdir}/version.go ${hackdir}/../Godeps/Godeps.json github.com/GoogleCloudPlatform/kubernetes/pkg/api)
+
+docker build -t openshift/kubernetes-fedora-dind images/builder/docker/kubernetes-fedora-dind
+docker build -t openshift/docker-builder images/builder/docker/docker-builder
+docker build -t openshift/sti-builder images/builder/docker/sti-builder
+docker build -t openshift/hello-openshift examples/hello-openshift
