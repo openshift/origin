0358242484f34271d19c5b15df7e1f71366019a2
Clayton Coleman <ccoleman@redhat.com>
2014-09-25 23:35:34 -0400

    UPSTREAM: Support PUT returning 201 on Create

--- Godeps/_workspace/src/github.com/GoogleCloudPlatform/kubernetes/pkg/apiserver/handlers.go
+++ Godeps/_workspace/src/github.com/GoogleCloudPlatform/kubernetes/pkg/apiserver/handlers.go
@@ -40,6 +40,7 @@ func RecoverPanics(handler http.Handler) http.Handler {
 		defer httplog.NewLogged(req, &w).StacktraceWhen(
 			httplog.StatusIsNot(
 				http.StatusOK,
+				http.StatusCreated,
 				http.StatusAccepted,
 				http.StatusMovedPermanently,
 				http.StatusTemporaryRedirect,
