601ffe03cbc07e6b6988e5dd4b0b7cadd6a8308a
Clayton Coleman <ccoleman@redhat.com>
2014-10-02 21:12:09 -0400

    Ensure a reference to assets exists in the source tree
    Godeps will drop it otherwise.

---
+++ assets/assets.go
@@ -0,0 +1,5 @@
+package assets
+
+import (
+	_ "github.com/jteeuwen/go-bindata" // to take a reference for godep
+)
---
+++ assets/doc.go
@@ -0,0 +1,5 @@
+// Package assets includes all of the static assets used by the OpenShift console and login flows.
+// Assets are generated into a .go file and then compiled into the OpenShift all-in-one binary.
+//
+// See README.md in this directory for more information about working with these assets.
+package assets
