16aecd472931ee04f77b456c732602067e753b93
Mrunal Patel <mrunalp@gmail.com>
2014-10-09 17:05:09 -0700

    Vagrant multi-node networking for origin.

---
+++ vagrant/provision-minion.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -ex
+source $(dirname $0)/provision-config.sh
+
+MINION_IP=$4
+
+# Setup hosts file to support ping by hostname to master
+if [ ! "$(cat /etc/hosts | grep $MASTER_NAME)" ]; then
+  echo "Adding $MASTER_NAME to hosts file"
+  echo "$MASTER_IP $MASTER_NAME" >> /etc/hosts
+fi
+
+# Setup hosts file to support ping by hostname to each minion in the cluster
+minion_ip_array=(${MINION_IPS//,/ })
+for (( i=0; i<${#MINION_NAMES[@]}; i++)); do
+  minion=${MINION_NAMES[$i]}
+  ip=${minion_ip_array[$i]}  
+  if [ ! "$(cat /etc/hosts | grep $minion)" ]; then
+    echo "Adding $minion to hosts file"
+    echo "$ip $minion" >> /etc/hosts
+  fi  
+done
+
+# Install the required packages
+yum install -y docker-io git golang e2fsprogs hg openvswitch net-tools bridge-utils
+
+# Build openshift
+echo "Building openshift"
+pushd /vagrant
+  ./hack/build-go.sh
+  cp _output/go/bin/openshift /usr/bin
+popd
+
+# Setup networking between the nodes
+$(dirname $0)/provision-network.sh $@
+
+usermod -a -G docker vagrant
+
+# Create systemd service
+cat <<EOF > /usr/lib/systemd/system/openshift-node.service
+[Unit]
+Description=openshift node
+After=network.service
+
+[Service]
+ExecStart=/usr/bin/openshift start node --master=http://${MASTER_IP}:8080
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start the service
+systemctl daemon-reload
+systemctl enable openshift-node.service
+systemctl start openshift-node.service
+
+# Register with the master
+#curl -X POST -H 'Accept: application/json' -d "{\"kind\":\"Minion\", \"id\":"${MINION_IP}", \"apiVersion\":\"v1beta1\", \"hostIP\":"${MINION_IP}" }" http://${MASTER_IP}:8080/api/v1beta1/minions
