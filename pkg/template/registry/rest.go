eae3e9f96348d1038da6cf2d8fcbf6d0f9659702
Vojtech Vitek (V-Teq) <vvitek@redhat.com>
2014-11-06 08:43:49 +0100

    Reorganize the template pkg, make it match upstream patterns
    * Storage -> REST

---
+++ pkg/template/registry/rest.go
@@ -0,0 +1,73 @@
+package template
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	kapi "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/apiserver"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/runtime"
+
+	"github.com/openshift/origin/pkg/config"
+	"github.com/openshift/origin/pkg/template"
+	"github.com/openshift/origin/pkg/template/api"
+	"github.com/openshift/origin/pkg/template/api/validation"
+	"github.com/openshift/origin/pkg/template/generator"
+)
+
+// REST implements RESTStorage interface for Template objects.
+type REST struct{}
+
+// NewREST creates new RESTStorage interface for Template objects.
+func NewREST() *REST {
+	return &REST{}
+}
+
+func (s *REST) New() runtime.Object {
+	return &api.Template{}
+}
+
+func (s *REST) List(ctx kapi.Context, selector, fields labels.Selector) (runtime.Object, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (s *REST) Get(ctx kapi.Context, id string) (runtime.Object, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (s *REST) Create(ctx kapi.Context, obj runtime.Object) (<-chan runtime.Object, error) {
+	tpl, ok := obj.(*api.Template)
+	if !ok {
+		return nil, errors.New("Not a template config.")
+	}
+	if errs := validation.ValidateTemplate(tpl); len(errs) > 0 {
+		return nil, errors.New(fmt.Sprintf("Invalid template config: %#v", errs))
+	}
+	return apiserver.MakeAsync(func() (runtime.Object, error) {
+		generators := map[string]generator.Generator{
+			"expression": generator.NewExpressionValueGenerator(rand.New(rand.NewSource(time.Now().UnixNano()))),
+		}
+		processor := template.NewTemplateProcessor(generators)
+		cfg, err := processor.Process(tpl)
+		if err != nil {
+			return nil, err
+		}
+		if err := config.AddConfigLabels(cfg, labels.Set{"template": tpl.ID}); err != nil {
+			return nil, err
+		}
+		return cfg, nil
+	}), nil
+}
+
+func (s *REST) Update(ctx kapi.Context, tpl runtime.Object) (<-chan runtime.Object, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (s *REST) Delete(ctx kapi.Context, id string) (<-chan runtime.Object, error) {
+	return apiserver.MakeAsync(func() (runtime.Object, error) {
+		return nil, errors.New("not implemented")
+	}), nil
+}
