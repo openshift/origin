330255c07651177f5ef40802e40a3ca4ff05d204
Clayton Coleman <ccoleman@redhat.com>
2014-11-10 23:08:47 -0500

    Add utility functions for commands that connect to a master

---
+++ pkg/cmd/util/env.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"os"
+)
+
+// Env returns an environment variable or a default value if not specified.
+func Env(key string, defaultValue string) string {
+	val := os.Getenv(key)
+	if len(val) == 0 {
+		return defaultValue
+	}
+	return val
+}
+
+// GetEnv returns an environment value if specified
+func GetEnv(key string) (string, bool) {
+	val := os.Getenv(key)
+	if len(val) == 0 {
+		return "", false
+	}
+	return val, true
+}
