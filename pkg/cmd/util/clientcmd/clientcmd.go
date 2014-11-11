07ae34b5ccd3f5ea476549f12d3bbf727885d301
deads2k <deads@redhat.com>
2014-11-11 13:39:51 -0500

    add token option to clientcmd

--- pkg/cmd/util/clientcmd/clientcmd.go
+++ pkg/cmd/util/clientcmd/clientcmd.go
@@ -16,18 +16,21 @@ const ConfigSyntax = " --master=<addr>"
 type Config struct {
 	MasterAddr     flagtypes.Addr
 	KubernetesAddr flagtypes.Addr
+	BearerToken    string
 }
 
 func NewConfig() *Config {
 	return &Config{
 		MasterAddr:     flagtypes.Addr{Value: "localhost:8080", DefaultScheme: "http", DefaultPort: 8080, AllowPrefix: true}.Default(),
 		KubernetesAddr: flagtypes.Addr{Value: "localhost:8080", DefaultScheme: "http", DefaultPort: 8080}.Default(),
+		BearerToken:    "",
 	}
 }
 
 func (cfg *Config) Bind(flag *pflag.FlagSet) {
 	flag.Var(&cfg.MasterAddr, "master", "The address the master can be reached on (host, host:port, or URL).")
 	flag.Var(&cfg.KubernetesAddr, "kubernetes", "The address of the Kubernetes server (host, host:port, or URL). If omitted defaults to the master.")
+	flag.StringVar(&cfg.BearerToken, "token", "", "If present, the bearer token for this request.")
 }
 
 func (cfg *Config) bindEnv() {
@@ -36,6 +39,9 @@ func (cfg *Config) bindEnv() {
 	}
 	if value, ok := util.GetEnv("OPENSHIFT_MASTER"); ok && !cfg.MasterAddr.Provided {
 		cfg.MasterAddr.Set(value)
+	}
+	if value, ok := util.GetEnv("BEARER_TOKEN"); ok && len(cfg.BearerToken) == 0 {
+		cfg.BearerToken = value
 	}
 }
 
@@ -47,8 +53,8 @@ func (cfg *Config) Clients() (*kclient.Client, *osclient.Client, error) {
 		kaddr = cfg.MasterAddr
 	}
 
-	config := &kclient.Config{Host: cfg.MasterAddr.String()}
-	kubeClient, err := kclient.New(&kclient.Config{Host: kaddr.URL.String()})
+	config := &kclient.Config{Host: cfg.MasterAddr.String(), BearerToken: cfg.BearerToken}
+	kubeClient, err := kclient.New(&kclient.Config{Host: kaddr.URL.String(), BearerToken: cfg.BearerToken})
 	if err != nil {
 		return nil, nil, fmt.Errorf("Unable to configure Kubernetes client: %v", err)
 	}
