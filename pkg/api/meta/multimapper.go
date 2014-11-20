d66625d1d96d689c9cfc1c2c642b487435773499
Michal Fojtik <mfojtik@redhat.com>
2014-11-20 13:08:12 +0100

    Removed MultiObjectTyper as it is not needed for now

--- pkg/api/meta/multimapper.go
+++ pkg/api/meta/multimapper.go
@@ -1,41 +1,9 @@
 package meta
 
-import (
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/api/meta"
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/runtime"
-)
+import "github.com/GoogleCloudPlatform/kubernetes/pkg/api/meta"
 
 // MultiRESTMapper is a wrapper for multiple RESTMappers.
 type MultiRESTMapper []meta.RESTMapper
-
-// MultiObjectTyper is a wrapper for multiple ObjectTypers.
-type MultiObjectTyper []runtime.ObjectTyper
-
-// DataVersionAndKind provides the Version and Kind from a raw JSON/YAML string
-// based on the current Schema. This implementation searches multiple schemas
-// and return the first match.
-func (m MultiObjectTyper) DataVersionAndKind(data []byte) (version, kind string, err error) {
-	for _, t := range m {
-		version, kind, err = t.DataVersionAndKind(data)
-		if err == nil {
-			return
-		}
-	}
-	return
-}
-
-// ObjectVersionAndKind provides the Version and Kind from a runtime Object
-// based on the current Schema. This implementation searches multiple schemas
-// and return the first match.
-func (m MultiObjectTyper) ObjectVersionAndKind(obj runtime.Object) (version, kind string, err error) {
-	for _, t := range m {
-		version, kind, err = t.ObjectVersionAndKind(obj)
-		if err == nil {
-			return
-		}
-	}
-	return
-}
 
 // VersionAndKindForResource provides the Version and Kind  mappings for the
 // REST resources. This implementation supports multiple REST schemas and return
