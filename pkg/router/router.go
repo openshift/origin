86bd76565f56c4fae45151e7b571662a96e263e7
Akram Ben Aissi <akram@free.fr>
2014-11-24 11:53:01 +0100

    Refactoring router.go to handle errors. Improving code coverage. Adding a constructor to Routes structure to pass filename to persist in. Turns the class testable

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strconv"
 	"time"
 )
@@ -21,7 +22,7 @@ const (
 )
 
 const (
-	RouteFile = "/var/lib/containers/router/routes.json"
+	DefaultRouteFile = "/var/lib/containers/router/routes.json"
 )
 
 type Frontend struct {
@@ -56,20 +57,29 @@ type Endpoint struct {
 
 type Routes struct {
 	GlobalRoutes map[string]Frontend
+	RouteFile    string
 }
 
 type Router interface {
-	ReadRoutes()
-	WriteRoutes()
+	ReadRoutes() (*Routes, error)
+	WriteRoutes() (*Routes, error)
 	FindFrontend(name string) (v Frontend, ok bool)
 	DeleteBackends(name string)
-	CreateFrontend(name string, url string)
-	DeleteFrontend(frontendname string)
-	AddAlias(alias string, frontendname string)
-	RemoveAlias(alias string, frontendname string)
-	AddRoute(frontendname string, fePath string, bePath string, protocols []string, endpoints []Endpoint)
+	CreateFrontend(name string, url string) (*Routes, error)
+	DeleteFrontend(frontendname string) (*Routes, error)
+	AddAlias(alias string, frontendname string) (*Routes, error)
+	RemoveAlias(alias string, frontendname string) (*Routes, error)
+	AddRoute(frontendname string, fePath string, bePath string, protocols []string, endpoints []Endpoint) (*Routes, error)
 	WriteConfig()
 	ReloadRouter() bool
+}
+
+func NewRoutes(filename ...string) *Routes {
+	file := DefaultRouteFile
+	if filename != nil && len(filename) > 0 {
+		file = filename[0]
+	}
+	return &Routes{make(map[string]Frontend), file}
 }
 
 func makeID() string {
@@ -78,30 +88,39 @@ func makeID() string {
 	return s
 }
 
-func (routes *Routes) ReadRoutes() {
-	//fmt.Printf("Reading routes file (%s)\n", RouteFile)
-	dat, err := ioutil.ReadFile(RouteFile)
+func (routes *Routes) ReadRoutes() (*Routes, error) {
+	file := routes.RouteFile
+	fmt.Printf("Reading routes file (%s)\n", file)
+	dat, err := ioutil.ReadFile(file)
 	if err != nil {
+		fmt.Printf("Error while reading file (%s)\n", file)
 		routes.GlobalRoutes = make(map[string]Frontend)
-		return
+		return nil, err
 	}
 	json.Unmarshal(dat, &routes.GlobalRoutes)
+	fmt.Printf("Marshall result %+v\n", routes.GlobalRoutes)
+	return routes, nil
 }
 
-func (routes *Routes) WriteRoutes() {
+func (routes *Routes) WriteRoutes() (*Routes, error) {
 	dat, err := json.MarshalIndent(routes.GlobalRoutes, "", "  ")
 	if err != nil {
 		fmt.Println("Failed to marshal routes - %s", err.Error())
+		return nil, err
 	}
-	err = ioutil.WriteFile(RouteFile, dat, 0644)
+	file := routes.RouteFile
+	fmt.Printf("Writing routes tofile (%s)\n", file)
+	err = ioutil.WriteFile(file, dat, 0644)
 	if err != nil {
 		fmt.Println("Failed to write to routes file - %s", err.Error())
+		return nil, err
 	}
+	return routes, nil
 }
 
 func (routes *Routes) FindFrontend(name string) (v Frontend, ok bool) {
 	v, ok = routes.GlobalRoutes[name]
-	return
+	return v, ok
 }
 
 func (routes *Routes) DeleteBackends(name string) {
@@ -114,7 +133,7 @@ func (routes *Routes) DeleteBackends(name string) {
 	routes.GlobalRoutes[name] = a
 }
 
-func (routes *Routes) CreateFrontend(name string, url string) {
+func (routes *Routes) CreateFrontend(name string, url string) (*Routes, error) {
 	a := Frontend{}
 	a.Backends = make(map[string]Backend)
 	a.EndpointTable = make(map[string]Endpoint)
@@ -124,29 +143,41 @@ func (routes *Routes) CreateFrontend(name string, url string) {
 		a.HostAliases = append(a.HostAliases, url)
 	}
 	routes.GlobalRoutes[a.Name] = a
-	routes.WriteRoutes()
+	return routes.WriteRoutes()
 }
 
-func (routes *Routes) DeleteFrontend(frontendname string) {
+func (routes *Routes) DeleteFrontend(frontendname string) (*Routes, error) {
 	delete(routes.GlobalRoutes, frontendname)
 	routes.WriteRoutes()
+	return routes, nil
 }
 
-func (routes *Routes) AddAlias(alias string, frontendname string) {
-	a := routes.GlobalRoutes[frontendname]
+func (routes *Routes) AddAlias(alias string, frontendname string) (*Routes, error) {
+	a, ok := routes.GlobalRoutes[frontendname]
+	if !ok {
+		err := fmt.Errorf("Error getting frontend with name: %v, ensure that the frontend has been previously created using the CreateFronted method", frontendname)
+		fmt.Printf("%v\n", err.Error())
+		return nil, err
+	}
 	for _, v := range a.HostAliases {
 		if v == alias {
-			return
+			return routes, nil
 		}
 	}
 
 	a.HostAliases = append(a.HostAliases, alias)
 	routes.GlobalRoutes[frontendname] = a
 	routes.WriteRoutes()
+	return routes, nil
 }
 
-func (routes *Routes) RemoveAlias(alias string, frontendname string) {
-	a := routes.GlobalRoutes[frontendname]
+func (routes *Routes) RemoveAlias(alias string, frontendname string) (*Routes, error) {
+	a, ok := routes.GlobalRoutes[frontendname]
+	if !ok {
+		err := fmt.Errorf("Error getting frontend with name: %v, ensure that the frontend has been previously created using the CreateFronted method", frontendname)
+		fmt.Printf("%v\n", err.Error())
+		return nil, err
+	}
 	newAliases := make([]string, 0)
 	for _, v := range a.HostAliases {
 		if v == alias || v == "" {
@@ -157,11 +188,18 @@ func (routes *Routes) RemoveAlias(alias string, frontendname string) {
 	a.HostAliases = newAliases
 	routes.GlobalRoutes[frontendname] = a
 	routes.WriteRoutes()
+	return routes, nil
 }
 
-func (routes *Routes) AddRoute(frontendname string, fePath string, bePath string, protocols []string, endpoints []Endpoint) {
+func (routes *Routes) AddRoute(frontendname string, fePath string, bePath string, protocols []string, endpoints []Endpoint) (*Routes, error) {
 	var id string
-	a := routes.GlobalRoutes[frontendname]
+	a, ok := routes.GlobalRoutes[frontendname]
+	if !ok {
+		err := fmt.Errorf("Error getting frontend with name: %v, ensure that the frontend has been previously created using the CreateFronted method", frontendname)
+		fmt.Printf("%v\n", err.Error())
+		return nil, err
+	}
+	a.Name = frontendname
 
 	epIDs := make([]string, 1)
 	for newEpId := range endpoints {
@@ -180,14 +218,24 @@ func (routes *Routes) AddRoute(frontendname string, fePath string, bePath string
 		if !found {
 			id = makeID()
 			ep := Endpoint{id, newEndpoint.IP, newEndpoint.Port}
+			fmt.Printf("Frontend  %+v\n", a)
+			fmt.Printf("Endpoint %+v\n", ep)
+			fmt.Printf("Routes %+v\n", a.EndpointTable[id])
 			a.EndpointTable[id] = ep
+			fmt.Printf("Routes after %+v\n", a.EndpointTable[id])
 			epIDs = append(epIDs, ep.ID)
 		}
 	}
+
 	// locate a backend that may already exist with this protocol and fe/be path
 	found := false
+	fmt.Printf("Backends  %+v\n", a.Backends)
 	for _, be := range a.Backends {
-		if be.FePath == fePath && be.BePath == bePath && cmpStrSlices(protocols, be.Protocols) {
+		sort.Strings(protocols)
+		sort.Strings(be.Protocols)
+		strProtocols := fmt.Sprintf("%v", protocols)
+		strBeProtocols := fmt.Sprintf("%v", be.Protocols)
+		if be.FePath == fePath && be.BePath == bePath && strProtocols == strBeProtocols {
 			for _, epId := range epIDs {
 				be.EndpointIDs = append(be.EndpointIDs, epId)
 			}
@@ -200,25 +248,7 @@ func (routes *Routes) AddRoute(frontendname string, fePath string, bePath string
 		id = makeID()
 		a.Backends[id] = Backend{id, fePath, bePath, protocols, epIDs, TERM_EDGE, nil}
 	}
-	routes.GlobalRoutes[a.Name] = a
+	fmt.Printf("Frontend %+v\n", a)
 	routes.WriteRoutes()
-}
-
-func cmpStrSlices(first []string, second []string) bool {
-	if len(first) != len(second) {
-		return false
-	}
-	for _, fi := range first {
-		found := false
-		for _, si := range second {
-			if fi == si {
-				found = true
-				break
-			}
-		}
-		if !found {
-			return false
-		}
-	}
-	return true
+	return routes, nil
 }
