1a418ed8b1accbb8b021a4a452ae55b4878f845f
Jordan Liggitt <jliggitt@redhat.com>
2014-11-25 16:50:45 -0500

    Make install-assets.sh work outside TRAVIS

--- hack/install-assets.sh
+++ hack/install-assets.sh
@@ -7,18 +7,18 @@ source "${OS_ROOT}/hack/common.sh"
 
 # If we are running inside of Travis then do not run the rest of this
 # script unless we want to TEST_ASSETS
-if [[ "${TRAVIS}" == "true" && "${TEST_ASSETS}" == "false" ]]; then
+if [[ "${TRAVIS-}" == "true" && "${TEST_ASSETS-}" == "false" ]]; then
   exit
 fi
 
 # Lock version of npm to work around https://github.com/npm/npm/issues/6309
-if [[ "${TRAVIS}" == "true" ]]; then
+if [[ "${TRAVIS-}" == "true" ]]; then
   npm install npm@2.1.1 -g
 fi
 
 # Install bower if needed
 if ! which bower > /dev/null 2>&1 ; then
-  if [[ "${TRAVIS}" == "true" ]]; then
+  if [[ "${TRAVIS-}" == "true" ]]; then
     npm install -g bower
   else
     sudo npm install -g bower
@@ -27,7 +27,7 @@ fi
  
 # Install grunt if needed
 if ! which grunt > /dev/null 2>&1 ; then
-  if [[ "${TRAVIS}" == "true" ]]; then
+  if [[ "${TRAVIS-}" == "true" ]]; then
     npm install -g grunt-cli
   else
     sudo npm install -g grunt-cli
