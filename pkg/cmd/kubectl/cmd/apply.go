4f3818423c266f9d256756f6d57370b9ad7cd8d1
Michal Fojtik <mfojtik@redhat.com>
2014-11-25 17:40:50 +0100

    Use ResourceFromFile to get namespace and data

--- pkg/cmd/kubectl/cmd/apply.go
+++ pkg/cmd/kubectl/cmd/apply.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"io"
 
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
 	kubecmd "github.com/GoogleCloudPlatform/kubernetes/pkg/kubectl/cmd"
 	"github.com/golang/glog"
 	"github.com/openshift/origin/pkg/config"
@@ -32,15 +31,7 @@ Examples:
 				usageError(cmd, "Must pass a filename to update")
 			}
 
-			data, err := kubecmd.ReadConfigData(filename)
-			checkErr(err)
-
-			// TODO: Wouldn't be necessary, in upstream it is builtin.
-			namespace := api.NamespaceDefault
-			if ns := kubecmd.GetFlagString(cmd, "namespace"); len(ns) > 0 {
-				namespace = ns
-			}
-
+			_, namespace, _, data := kubecmd.ResourceFromFile(filename, f.Typer, f.Mapper)
 			result, err := config.Apply(namespace, data, f.GetRESTHelperFunc(cmd))
 			checkErr(err)
 
--- pkg/cmd/kubectl/cmd/buildlogs.go
+++ pkg/cmd/kubectl/cmd/buildlogs.go
@@ -25,7 +25,6 @@ $ kubectl build-logs 566bed879d2d
 				usageError(cmd, "<build> are required for log")
 			}
 
-			// TODO: Wouldn't be necessary, in upstream it is builtin.
 			namespace := api.NamespaceDefault
 			if ns := kubecmd.GetFlagString(cmd, "namespace"); len(ns) > 0 {
 				namespace = ns
