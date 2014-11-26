e05eebb2df0f0c29435b457ede9317cab6d9a8e8
Paul Weil <pweil@redhat.com>
2014-11-26 09:02:22 -0500

    clean up ifs/switch

--- pkg/util/fields.go
+++ pkg/util/fields.go
@@ -2,9 +2,9 @@ package util
 
 import (
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
+	"github.com/golang/glog"
 	"reflect"
 	"strconv"
-	"github.com/golang/glog"
 )
 
 func FieldSet(object interface{}) labels.Set {
@@ -21,34 +21,30 @@ func FieldSet(object interface{}) labels.Set {
 
 	for i := 0; i < objectVal.NumField(); i++ {
 		fieldVal := objectVal.Field(i)
+		fieldType := objectType.Field(i)
 
 		//if we're dealing with a struct the recurse and add everything.
-		if fieldVal.Kind() == reflect.Struct {
+		switch fieldVal.Kind() {
+		case reflect.Struct: //TODO if there is an embedded pointer type we need to go through the recursion again rather than list as an unsupported type
 			embeddedData := FieldSet(fieldVal.Interface())
 
 			for k, v := range embeddedData {
 				fieldSet[k] = v
 			}
-		}else {
-			fieldType := objectType.Field(i)
-
-			switch fieldVal.Kind() {
-			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-				fieldSet[fieldType.Name] = strconv.FormatInt(fieldVal.Int(), 10)
-			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-				fieldSet[fieldType.Name] = strconv.FormatUint(fieldVal.Uint(), 10)
-			case reflect.Bool:
-				fieldSet[fieldType.Name] = strconv.FormatBool(fieldVal.Bool())
-			case reflect.Float32, reflect.Float64:
-				fieldSet[fieldType.Name] = strconv.FormatFloat(fieldVal.Float(), 'f', -1, 64)
-			case reflect.String:
-				fieldSet[fieldType.Name] = fieldVal.String()
-			default:
-				glog.Warningf("Skipping unsupported field %s of type %s.", fieldType.Name, fieldVal.Kind())
-			}
-
-			//TODO if there is an embedded pointer type we need to go through the recursion again rather than list as an unsupported type
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			fieldSet[fieldType.Name] = strconv.FormatInt(fieldVal.Int(), 10)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			fieldSet[fieldType.Name] = strconv.FormatUint(fieldVal.Uint(), 10)
+		case reflect.Bool:
+			fieldSet[fieldType.Name] = strconv.FormatBool(fieldVal.Bool())
+		case reflect.Float32, reflect.Float64:
+			fieldSet[fieldType.Name] = strconv.FormatFloat(fieldVal.Float(), 'f', -1, 64)
+		case reflect.String:
+			fieldSet[fieldType.Name] = fieldVal.String()
+		default:
+			glog.Warningf("Skipping unsupported field %s of type %s.", fieldType.Name, fieldVal.Kind())
 		}
+
 	}
 
 	return fieldSet
@@ -57,6 +53,6 @@ func FieldSet(object interface{}) labels.Set {
 //matchers are used in List and Watch commands.  However, if the list/watch is returning n objects it means
 //we must construct n maps to run the selector.  This utility method avoids the map overhead if the field selector
 //is labels.Everything() which is a common use case
-func FieldMatches(obj interface{}, selector labels.Selector) bool{
+func FieldMatches(obj interface{}, selector labels.Selector) bool {
 	return selector.Empty() || selector.Matches(FieldSet(obj))
 }
