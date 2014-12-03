bee63623271f87c705aff39f5d8a8467cbab98fd
Michal Fojtik <mfojtik@redhat.com>
2014-12-03 14:08:54 +0100

    Replace errors.New(fmt.Sprintf(...)) with fmt.Errorf(...)

--- pkg/template/registry/rest.go
+++ pkg/template/registry/rest.go
@@ -44,7 +44,7 @@ func (s *REST) Create(ctx kapi.Context, obj runtime.Object) (<-chan apiserver.RE
 		return nil, errors.New("Not a template config.")
 	}
 	if errs := validation.ValidateTemplate(tpl); len(errs) > 0 {
-		return nil, errors.New(fmt.Sprintf("Invalid template config: %#v", errs))
+		return nil, fmt.Errorf("Invalid template config: %#v", errs)
 	}
 	return apiserver.MakeAsync(func() (runtime.Object, error) {
 		generators := map[string]generator.Generator{
