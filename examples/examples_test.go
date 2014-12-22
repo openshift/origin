5ba49752aa30399f48a63aac8700413bb00350eb
Clayton Coleman <ccoleman@redhat.com>
2014-12-22 13:49:08 -0500

    Improve example validation and add template fixtures for example data

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -1,6 +1,7 @@
 package examples
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -8,88 +9,191 @@ import (
 	"testing"
 
 	kapi "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api/meta"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api/validation"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/runtime"
+	//"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
+	"github.com/golang/glog"
 
 	"github.com/openshift/origin/pkg/api/latest"
+	buildapi "github.com/openshift/origin/pkg/build/api"
+	buildv "github.com/openshift/origin/pkg/build/api/validation"
 	configapi "github.com/openshift/origin/pkg/config/api"
 	deployapi "github.com/openshift/origin/pkg/deploy/api"
+	deployv "github.com/openshift/origin/pkg/deploy/api/validation"
 	imageapi "github.com/openshift/origin/pkg/image/api"
+	imagev "github.com/openshift/origin/pkg/image/api/validation"
 	projectapi "github.com/openshift/origin/pkg/project/api"
+	projectv "github.com/openshift/origin/pkg/project/api/validation"
 	routeapi "github.com/openshift/origin/pkg/route/api"
+	routev "github.com/openshift/origin/pkg/route/api/validation"
 	templateapi "github.com/openshift/origin/pkg/template/api"
+	templatev "github.com/openshift/origin/pkg/template/api/validation"
 )
 
-func TestExamples(t *testing.T) {
-	expected := map[string]runtime.Object{
+type mockService struct{}
 
-		"guestbook/template.json": &templateapi.Template{},
+func (mockService) ListServices(kapi.Context) (*kapi.ServiceList, error) {
+	return &kapi.ServiceList{}, nil
+}
 
-		"hello-openshift/hello-pod.json":     &kapi.Pod{},
-		"hello-openshift/hello-project.json": &projectapi.Project{},
+func validateObject(obj runtime.Object) (errors []error) {
+	ctx := kapi.NewDefaultContext()
 
-		"sample-app/github-webhook-example.json":           nil, // Skip.
-		"sample-app/docker-registry-config.json":           &configapi.Config{},
-		"sample-app/application-template-stibuild.json":    &templateapi.Template{},
-		"sample-app/application-template-dockerbuild.json": &templateapi.Template{},
-
-		"jenkins/jenkins-config.json":         &configapi.Config{},
-		"jenkins/docker-registry-config.json": &configapi.Config{},
-		"jenkins/application-template.json":   &templateapi.Template{},
-
-		"../test/integration/fixtures/test-deployment-config.json": &deployapi.DeploymentConfig{},
-		"../test/integration/fixtures/test-image-repository.json":  &imageapi.ImageRepository{},
-		"../test/integration/fixtures/test-image.json":             &imageapi.Image{},
-		"../test/integration/fixtures/test-mapping.json":           &imageapi.ImageRepositoryMapping{},
-		"../test/integration/fixtures/test-route.json":             &routeapi.Route{},
-		"../test/integration/fixtures/test-service.json":           &kapi.Service{},
+	if m, err := meta.Accessor(obj); err == nil {
+		if len(m.Namespace()) == 0 {
+			m.SetNamespace(kapi.NamespaceDefault)
+		}
 	}
 
-	// Add the root directory to search for files you want to test, if is not in the list below.
-	rootDirs := []string{".", "../test/integration/fixtures"}
-	files := []string{}
+	switch t := obj.(type) {
 
-	for _, rootDir := range rootDirs {
-		err := filepath.Walk(rootDir, func(path string, f os.FileInfo, err error) error {
-			if filepath.Ext(path) == ".json" {
-				files = append(files, path)
+	case *kapi.ReplicationController:
+		errors = validation.ValidateReplicationController(t)
+	case *kapi.Service:
+		errors = validation.ValidateService(t, mockService{}, ctx)
+	case *kapi.Pod:
+		errors = validation.ValidatePod(t)
+
+	case *imageapi.Image:
+		errors = imagev.ValidateImage(t)
+	case *imageapi.ImageRepository:
+		// TODO: validate image repository
+		// 	errors = imagev.ValidateImageRepository(t)
+	case *imageapi.ImageRepositoryMapping:
+		errors = imagev.ValidateImageRepositoryMapping(t)
+	case *deployapi.DeploymentConfig:
+		errors = deployv.ValidateDeploymentConfig(t)
+	case *deployapi.Deployment:
+		errors = deployv.ValidateDeployment(t)
+	case *projectapi.Project:
+		errors = projectv.ValidateProject(t)
+	case *routeapi.Route:
+		errors = routev.ValidateRoute(t)
+
+	case *buildapi.BuildConfig:
+		errors = buildv.ValidateBuildConfig(t)
+	case *buildapi.Build:
+		errors = buildv.ValidateBuild(t)
+
+	case *templateapi.Template:
+		errors = templatev.ValidateTemplate(t)
+		for i := range t.Items {
+			obj, err := latest.Codec.Decode(t.Items[i].RawJSON)
+			if err != nil {
+				errors = append(errors, err)
+				continue
 			}
-			return err
-		})
+			errors = append(errors, validateObject(obj)...)
+		}
+	case *configapi.Config:
+		for i := range t.Items {
+			obj, err := latest.Codec.Decode(t.Items[i].RawJSON)
+			if err != nil {
+				errors = append(errors, err)
+				continue
+			}
+			errors = append(errors, validateObject(obj)...)
+		}
+	default:
+		if list, err := runtime.ExtractList(obj); err == nil {
+			for i := range list {
+				errs := validateObject(list[i])
+				errors = append(errors, errs...)
+			}
+			return
+		}
+		return []error{fmt.Errorf("no validation defined for %#v", obj)}
+	}
+	return errors
+}
 
+func walkJSONFiles(inDir string, fn func(name, path string, data []byte)) error {
+	err := filepath.Walk(inDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			t.Errorf("%v", err)
+			return err
 		}
+		if info.IsDir() && path != inDir {
+			return filepath.SkipDir
+		}
+		name := filepath.Base(path)
+		ext := filepath.Ext(name)
+		if ext != "" {
+			name = name[:len(name)-len(ext)]
+		}
+		if !(ext == ".json" || ext == ".yaml") {
+			return nil
+		}
+		glog.Infof("testing %s", path)
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		fn(name, path, data)
+		return nil
+	})
+	return err
+}
+
+func TestExampleObjectSchemas(t *testing.T) {
+	cases := map[string]map[string]runtime.Object{
+		"../examples/guestbook": {
+			"template": &templateapi.Template{},
+		},
+		"../examples/hello-openshift": {
+			"hello-pod":     &kapi.Pod{},
+			"hello-project": &projectapi.Project{},
+		},
+		"../examples/sample-app": {
+			"github-webhook-example":           nil, // Skip.
+			"docker-registry-config":           &configapi.Config{},
+			"application-template-stibuild":    &templateapi.Template{},
+			"application-template-dockerbuild": &templateapi.Template{},
+		},
+		"../examples/jenkins": {
+			"jenkins-config":         &configapi.Config{},
+			"docker-registry-config": &configapi.Config{},
+			"application-template":   &templateapi.Template{},
+		},
+		"../test/integration/fixtures": {
+			"test-deployment-config": &deployapi.DeploymentConfig{},
+			"test-image-repository":  &imageapi.ImageRepository{},
+			"test-image":             &imageapi.Image{},
+			"test-mapping":           &imageapi.ImageRepositoryMapping{},
+			"test-route":             &routeapi.Route{},
+			"test-service":           &kapi.Service{},
+		},
+		"../test/templates/fixtures": {
+			"crunchydata-pod": nil, // Explicitly fails validation, but should pass transformation
+		},
 	}
 
-	// Check all files which are expected to be validated, each file should have
-	// an existent corresponding JSON file on disk.
-	for fileName := range expected {
-		if exists := func() bool {
-			for _, file := range files {
-				if fileName == file {
-					return true
-				}
+	for path, expected := range cases {
+		tested := 0
+		err := walkJSONFiles(path, func(name, path string, data []byte) {
+			expectedType, found := expected[name]
+			if !found {
+				t.Errorf("%s does not have a test case defined", path)
+				return
 			}
-			return false
-		}(); exists == false {
-			t.Errorf("No JSON file was found for the expected file: '%v'", fileName)
-			continue
+			tested += 1
+			if expectedType == nil {
+				t.Logf("%q is skipped", path)
+				return
+			}
+			if err := latest.Codec.DecodeInto(data, expectedType); err != nil {
+				t.Errorf("%s did not decode correctly: %v\n%s", path, err, string(data))
+				return
+			}
+			if errors := validateObject(expectedType); len(errors) > 0 {
+				t.Errorf("%s did not validate correctly: %v", path, errors)
+			}
+		})
+		if err != nil {
+			t.Errorf("Expected no error, Got %v", err)
 		}
-	}
-
-	for _, file := range files {
-		expectedObject, ok := expected[file]
-		if !ok {
-			t.Errorf("No test case defined for example JSON file '%v'", file)
-			continue
-		}
-		if expectedObject == nil {
-			continue
-		}
-
-		jsonData, _ := ioutil.ReadFile(file)
-		if err := latest.Codec.DecodeInto(jsonData, expectedObject); err != nil {
-			t.Errorf("Unexpected error while decoding example JSON file '%v': %v", file, err)
+		if tested != len(expected) {
+			t.Errorf("Expected %d examples, Got %d", len(expected), tested)
 		}
 	}
 }
---
+++ test/templates/templates_test.go
@@ -0,0 +1,76 @@
+package templates
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/apiserver"
+	kclient "github.com/GoogleCloudPlatform/kubernetes/pkg/client"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/master"
+
+	"github.com/openshift/origin/pkg/api/latest"
+	osclient "github.com/openshift/origin/pkg/client"
+	config "github.com/openshift/origin/pkg/config/api"
+	templateregistry "github.com/openshift/origin/pkg/template/registry"
+)
+
+func walkJSONFiles(inDir string, fn func(name, path string, data []byte)) error {
+	err := filepath.Walk(inDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() && path != inDir {
+			return filepath.SkipDir
+		}
+		name := filepath.Base(path)
+		ext := filepath.Ext(name)
+		if ext != "" {
+			name = name[:len(name)-len(ext)]
+		}
+		if !(ext == ".json" || ext == ".yaml") {
+			return nil
+		}
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		fn(name, path, data)
+		return nil
+	})
+	return err
+}
+
+func TestTemplateTransformationFromConfig(t *testing.T) {
+	osMux := http.NewServeMux()
+	server := httptest.NewServer(osMux)
+	defer server.Close()
+
+	osClient := osclient.NewOrDie(&kclient.Config{Host: server.URL})
+
+	storage := map[string]apiserver.RESTStorage{
+		"templateConfigs": templateregistry.NewREST(),
+	}
+
+	interfaces, _ := latest.InterfacesFor(latest.Version)
+	osPrefix := "/osapi/v1beta1"
+	handlerContainer := master.NewHandlerContainer(osMux)
+	apiserver.NewAPIGroupVersion(storage, latest.Codec, osPrefix, interfaces.MetadataAccessor).InstallREST(handlerContainer, "/osapi", "v1beta1")
+
+	walkJSONFiles("fixtures", func(name, path string, _ []byte) {
+		config := &config.Config{}
+		err := osClient.RESTClient.Post().Path("/templateConfigs").Body(path).Do().Into(config)
+		if err != nil {
+			t.Errorf("%q: unexpected error: %v", path, err)
+			return
+		}
+		if len(config.Items) == 0 {
+			t.Errorf("%q: no items in config object", path)
+			return
+		}
+		t.Logf("tested %q", path)
+	})
+}
