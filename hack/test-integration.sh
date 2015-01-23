4b207900809a1f38b28998ef688b9bced5d0ea6e
Clayton Coleman <ccoleman@redhat.com>
2015-01-23 11:30:19 -0500

    Disable race detection on test-integration because of #731

--- hack/test-integration.sh
+++ hack/test-integration.sh
@@ -22,4 +22,5 @@ trap cleanup EXIT SIGINT
 echo
 echo Integration test cases ...
 echo
-KUBE_RACE="${KUBE_RACE:--race}" KUBE_COVER=" " "${OS_ROOT}/hack/test-go.sh" test/integration -tags 'integration no-docker' "${@:1}"
+# TODO: race is disabled because of origin #731
+KUBE_RACE="${KUBE_RACE:-}" KUBE_COVER=" " "${OS_ROOT}/hack/test-go.sh" test/integration -tags 'integration no-docker' "${@:1}"
