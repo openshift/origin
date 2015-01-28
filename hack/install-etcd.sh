6320d5d5905414cf3a26e87fea76d670dc7994ca
Akram Ben Aissi <akram@free.fr>
2015-01-28 16:47:49 +0100

    Do not clone etc in _output directory if it is already cloned

--- hack/install-etcd.sh
+++ hack/install-etcd.sh
@@ -6,7 +6,10 @@ source "${OS_ROOT}/hack/common.sh"
 mkdir -p "${OS_ROOT}/_output"
 cd "${OS_ROOT}/_output"
 
-git clone https://github.com/coreos/etcd.git
+if [ ! -d "etcd" ]; then
+  git clone https://github.com/coreos/etcd.git
+fi
+
 cd etcd
 git checkout $(go run ${OS_ROOT}/hack/version.go ${OS_ROOT}/Godeps/Godeps.json github.com/coreos/etcd/server)
 ./build
