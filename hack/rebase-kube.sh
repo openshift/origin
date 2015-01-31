81611339f0232b1ee48c80652a6a249c09cea487
Clayton Coleman <ccoleman@redhat.com>
2015-01-30 20:33:40 -0500

    Properly version Kubernetes and OpenShift binaries

--- hack/rebase-kube.sh
+++ hack/rebase-kube.sh
@@ -25,7 +25,9 @@ if [[ $(git remote -v | grep -c 'openshift/kubernetes.git') -eq 0 ]]; then
 fi
 echo "Fetching latest ..."
 git fetch
+git fetch --tags
 popd > /dev/null
+git fetch --tags
 
 echo "Restoring Origin dependencies ..."
 make clean
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,37 +1,46 @@
 package version
 
-import (
-	"fmt"
-)
+import ()
 
-// commitFromGit is a constant representing the source version that
-// generated this build. It should be set during build via -ldflags.
-var commitFromGit string
+var (
+	// commitFromGit is a constant representing the source version that
+	// generated this build. It should be set during build via -ldflags.
+	commitFromGit string
+	// versionFromGit is a constant representing the version tag that
+	// generated this build. It should be set during build via -ldflags.
+	versionFromGit string
+	// major version
+	majorFromGit string
+	// minor version
+	minorFromGit string
+)
 
 // Info contains versioning information.
 // TODO: Add []string of api versions supported? It's still unclear
 // how we'll want to distribute that information.
 type Info struct {
-	Major     string `json:"major"`
-	Minor     string `json:"minor"`
-	GitCommit string `json:"gitCommit"`
+	Major      string `json:"major"`
+	Minor      string `json:"minor"`
+	GitCommit  string `json:"gitCommit"`
+	GitVersion string `json:"gitVersion"`
 }
 
 // Get returns the overall codebase version. It's for detecting
 // what code a binary was built from.
 func Get() Info {
 	return Info{
-		Major:     "0",
-		Minor:     "1",
-		GitCommit: commitFromGit,
+		Major:      majorFromGit,
+		Minor:      minorFromGit,
+		GitCommit:  commitFromGit,
+		GitVersion: versionFromGit,
 	}
 }
 
 // String returns info as a human-friendly version string.
 func (info Info) String() string {
-	commit := info.GitCommit
-	if commit == "" {
-		commit = "(unknown)"
+	version := info.GitVersion
+	if version == "" {
+		version = "unknown"
 	}
-	return fmt.Sprintf("version %s.%s, build %s", info.Major, info.Minor, commit)
+	return version
 }
