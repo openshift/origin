17219582025d9560b593cc06436cf62e3f1dfdd7
fabianofranz <contact@fabianofranz.com>
2015-02-02 17:45:31 -0200

    Removed "Additional Help Topics" section from help template

--- pkg/cmd/openshift/templates.go
+++ pkg/cmd/openshift/templates.go
@@ -17,9 +17,5 @@ Available Commands: {{range .Commands}}{{if .Runnable}}
 {{ if .HasLocalFlags}}Options:
 {{.LocalFlags.FlagUsages}}{{end}}
 {{ if .HasAnyPersistentFlags}}Global Options:
-{{.AllPersistentFlags.FlagUsages}}{{end}}{{if .HasParent}}{{if and (gt .Commands 0) (gt .Parent.Commands 1) }}
-Additional help topics: {{if gt .Commands 0 }}{{range .Commands}}{{if not .Runnable}} {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if gt .Parent.Commands 1 }}{{range .Parent.Commands}}{{if .Runnable}}{{if not (eq .Name $cmd.Name) }}{{end}}
-  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{end}}
-{{end}}{{ if .HasSubCommands }}
-Use "{{.Root.Name}} help [command]" for more information about that command.
-{{end}}`
+{{.AllPersistentFlags.FlagUsages}}{{end}}{{ if .HasSubCommands }}
+Use "{{.Root.Name}} help [command]" for more information about that command.{{end}}`
