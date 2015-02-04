b999dba868c69b30c81f268b250dc1582764058e
Clayton Coleman <ccoleman@redhat.com>
2015-02-04 16:55:23 -0500

    OpenShift should set a client UserAgent on all calls

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,9 +1,16 @@
 package client
 
 import (
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+
 	kclient "github.com/GoogleCloudPlatform/kubernetes/pkg/client"
 
 	"github.com/openshift/origin/pkg/api/latest"
+	"github.com/openshift/origin/pkg/version"
 )
 
 // Interface exposes methods on OpenShift resources.
@@ -114,21 +121,38 @@ type Client struct {
 // objects. An error is returned if the provided configuration is not valid.
 func New(c *kclient.Config) (*Client, error) {
 	config := *c
+	if err := SetOpenShiftDefaults(&config); err != nil {
+		return nil, err
+	}
+	client, err := kclient.RESTClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{client}, nil
+}
+
+func SetOpenShiftDefaults(config *kclient.Config) error {
 	if config.Prefix == "" {
 		config.Prefix = "/osapi"
+	}
+	if len(config.UserAgent) == 0 {
+		config.UserAgent = DefaultOpenShiftUserAgent()
 	}
 	if config.Version == "" {
 		// Clients default to the preferred code API version
 		// TODO: implement version negotiation (highest version supported by server)
 		config.Version = latest.Version
 	}
-	config.Codec = latest.Codec
-	config.LegacyBehavior = (config.Version == "v1beta1")
-	client, err := kclient.RESTClientFor(&config)
+	version := config.Version
+	versionInterfaces, err := latest.InterfacesFor(version)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("API version '%s' is not recognized (valid values: %s)", version, strings.Join(latest.Versions, ", "))
 	}
-	return &Client{client}, nil
+	if config.Codec == nil {
+		config.Codec = versionInterfaces.Codec
+	}
+	config.LegacyBehavior = (config.Version == "v1beta1")
+	return nil
 }
 
 // NewOrDie creates an OpenShift client and panics if the provided API version is not recognized.
@@ -138,4 +162,19 @@ func NewOrDie(c *kclient.Config) *Client {
 		panic(err)
 	}
 	return client
+}
+
+// DefaultOpenShiftUserAgent returns the default user agent that clients can use.
+func DefaultOpenShiftUserAgent() string {
+	commit := version.Get().GitCommit
+	if len(commit) > 7 {
+		commit = commit[:7]
+	}
+	if len(commit) == 0 {
+		commit = "unknown"
+	}
+	version := version.Get().GitVersion
+	seg := strings.SplitN(version, "-", 2)
+	version = seg[0]
+	return fmt.Sprintf("%s/%s (%s/%s) openshift/%s", path.Base(os.Args[0]), version, runtime.GOOS, runtime.GOARCH, commit)
 }
