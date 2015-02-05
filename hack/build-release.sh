794be1e2733a99b85f7d622ea9cd150fbffdb72c
Dan Mace <ironcladlou@gmail.com>
2015-02-04 19:03:39 -0500

    Platform independent image builds
    Make the build/release process more generic to allow multiple release targets
    such as core and image binaries. This makes the build/release process once
    again platform independent (provided docker is available on the host), and
    re-enables support for build-release/build-images on OSX.

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -15,35 +15,31 @@ cd "${OS_ROOT}"
 
 context="${OS_ROOT}/_output/buildenv-context"
 
-# clean existing output
+# Clean existing output.
 rm -rf "${OS_ROOT}/_output/local/releases"
 rm -rf "${OS_ROOT}/_output/local/go/bin"
 rm -rf "${context}"
 mkdir -p "${context}"
 mkdir -p "${OS_ROOT}/_output/local"
 
-# generate version definitions
+# Generate version definitions.
 os::build::get_version_vars
 os::build::save_version_vars "${context}/os-version-defs"
 
-# create the input archive
+# Create the input archive.
 git archive --format=tar -o "${context}/archive.tar" HEAD
 tar -rf "${context}/archive.tar" -C "${context}" os-version-defs
 gzip -f "${context}/archive.tar"
 
-# build in the clean environment
+# Perform the build and release in Docker.
 cat "${context}/archive.tar.gz" | docker run -i --cidfile="${context}/cid" openshift/origin-release
 docker cp $(cat ${context}/cid):/go/src/github.com/openshift/origin/_output/local/releases "${OS_ROOT}/_output/local"
 
-# copy the linux release back to the _output/local/go/bin dir
-releases=$(find _output/local/releases/ -print | grep 'openshift-origin-.*-linux-' --color=never)
-if [[ $(echo $releases | wc -l) -ne 1 ]]; then
-  echo "There should be exactly one Linux release tar in _output/local/releases"
-  exit 1
-fi
+# Copy the linux release archives release back to the local _output/local/go/bin directory.
+os::build::detect_local_release_tars "linux"
 
-bindir="_output/local/go/bin"
-mkdir -p "${bindir}"
-tar mxzf "${releases}" -C "${bindir}"
+mkdir -p "${OS_LOCAL_BINPATH}"
+tar mxzf "${OS_CORE_RELEASE_TAR}" -C "${OS_LOCAL_BINPATH}"
+tar mxzf "${OS_IMAGE_RELEASE_TAR}" -C "${OS_LOCAL_BINPATH}"
 
 os::build::make_openshift_binary_symlinks
