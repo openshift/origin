019a00aeb85a1c6c832fba4f569eb7a0292f1a44
deads2k <deads@redhat.com>
2015-02-05 08:47:23 -0500

    modify .kubeconfig

--- pkg/cmd/experimental/login/login.go
+++ pkg/cmd/experimental/login/login.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"os"
 
+	kclient "github.com/GoogleCloudPlatform/kubernetes/pkg/client"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/client/clientcmd"
+	clientcmdapi "github.com/GoogleCloudPlatform/kubernetes/pkg/client/clientcmd/api"
 	kubecmd "github.com/GoogleCloudPlatform/kubernetes/pkg/kubectl/cmd"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
+
 	"github.com/golang/glog"
 	"github.com/openshift/origin/pkg/cmd/cli/cmd"
 	"github.com/openshift/origin/pkg/cmd/util/tokencmd"
@@ -40,6 +44,11 @@ prompt for user input if not provided.
 				glog.Fatalf("%v\n", err)
 			}
 
+			err = updateKubeconfigFile(usernameFlag, accessToken, clientCfg)
+			if err != nil {
+				glog.Fatalf("%v\n", err)
+			}
+
 			fmt.Printf("Auth token: %v\n", string(accessToken))
 		},
 	}
@@ -63,4 +72,99 @@ func defaultClientConfig(flags *pflag.FlagSet) clientcmd.ClientConfig {
 	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, overrides)
 
 	return clientConfig
+}
+
+func updateKubeconfigFile(username, token string, clientConfig *kclient.Config) error {
+	config, err := getConfigFromFile(".kubeconfig")
+	if err != nil {
+		return err
+	}
+
+	credentialsName := username
+	if len(credentialsName) == 0 {
+		credentialsName = "osc-login"
+	}
+	credentialsName = getUniqueName(credentialsName, getAuthInfoNames(config))
+	credentials := clientcmdapi.NewAuthInfo()
+	credentials.Token = token
+	config.AuthInfos[credentialsName] = *credentials
+
+	clusterName := getUniqueName("osc-login-cluster", getClusterNames(config))
+	cluster := clientcmdapi.NewCluster()
+	cluster.Server = clientConfig.Host
+	cluster.InsecureSkipTLSVerify = clientConfig.Insecure
+	cluster.CertificateAuthority = clientConfig.CAFile
+	config.Clusters[clusterName] = *cluster
+
+	contextName := getUniqueName(clusterName+"-"+credentialsName, getContextNames(config))
+	context := clientcmdapi.NewContext()
+	context.Cluster = clusterName
+	context.AuthInfo = credentialsName
+	config.Contexts[contextName] = *context
+
+	config.CurrentContext = contextName
+
+	err = clientcmd.WriteToFile(*config, ".kubeconfig")
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func getAuthInfoNames(config *clientcmdapi.Config) *util.StringSet {
+	ret := &util.StringSet{}
+	for key := range config.AuthInfos {
+		ret.Insert(key)
+	}
+
+	return ret
+}
+
+func getContextNames(config *clientcmdapi.Config) *util.StringSet {
+	ret := &util.StringSet{}
+	for key := range config.Contexts {
+		ret.Insert(key)
+	}
+
+	return ret
+}
+
+func getClusterNames(config *clientcmdapi.Config) *util.StringSet {
+	ret := &util.StringSet{}
+	for key := range config.Clusters {
+		ret.Insert(key)
+	}
+
+	return ret
+}
+
+func getConfigFromFile(filename string) (*clientcmdapi.Config, error) {
+	var err error
+	config, err := clientcmd.LoadFromFile(filename)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	if config == nil {
+		config = clientcmdapi.NewConfig()
+	}
+
+	return config, nil
+}
+
+func getUniqueName(basename string, existingNames *util.StringSet) string {
+	if !existingNames.Has(basename) {
+		return basename
+	}
+
+	for i := 0; i < 100; i++ {
+		trialName := fmt.Sprintf("%v-%d", basename, i)
+		if !existingNames.Has(trialName) {
+			return trialName
+		}
+	}
+
+	return string(util.NewUUID())
 }
