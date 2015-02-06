e9d5726cc2a554d4eda14224e19b93d13202485c
Scott Dodson <sdodson@redhat.com>
2015-02-06 12:02:50 -0500

    Update the custom tagger and builder to provide OpenShift ldflags
    	openshift.spec

--- rel-eng/lib/embeddedcommittagger/__init__.py
+++ rel-eng/lib/embeddedcommittagger/__init__.py
@@ -1,5 +1,5 @@
 """
-Code for tagging Spacewalk/Satellite packages.
+Code for tagging Openshift v3 packages
 """
 
 from tito.common import get_latest_commit, run_command
@@ -7,20 +7,25 @@ from tito.tagger import VersionTagger
 
 class EmbeddedCommitTagger(VersionTagger):
   """
-  Tagger which defines a specfile global with the git hash at which the tag was
-  created.
+  Tagger which defines a specfile global 'commit' with the git hash at
+  which the tag was created. This also defines ldflags by importing
+  hack/common.sh and executing os::build::ldflags Setting %commit isn't
+  currently used, but seems to be the norm for RPM packaging of golang apps.
 
   Requires that your commit is written on one single line as:
   %global commit 460abe2a3abe0fa22ac96c551fe71c0fc36f7475
 
+  And that your ldflags are similarly on a single line, ie:
+  %global ldflags -X foo -X bar
+
   Used For:
-    - Packages that are to be built via dist-git and need to have the git hash
-      available to them.
+    - Openshift v3, probably not much else
   """
 
   def _tag_release(self):
     """
-    Tag a new release of the package, add specfile global named commit. (ie: x.y.z-r+1)
+    Tag a new release of the package, add specfile global named commit. (ie:
+    x.y.z-r+1) and ldflags from hack/common.sh os::build::ldflags
     """
     self._make_changelog()
     new_version = self._bump_version(release=True)
@@ -29,6 +34,11 @@ class EmbeddedCommitTagger(VersionTagger):
       (git_hash, self.spec_file)
     output = run_command(update_commit)
 
+    cmd = '. ./hack/common.sh ; echo $(os::build::ldflags)'
+    ldflags = run_command('bash -c \'%s\''  % (cmd) )
+    update_ldflags = "sed -i 's|^%%global ldflags .*$|%%global ldflags %s|' %s" % \
+           (ldflags, self.spec_file)
+    output = run_command(update_ldflags)
 
     self._check_tag_does_not_exist(self._get_new_tag(new_version))
     self._update_changelog(new_version)
