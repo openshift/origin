88375ed39232e1eb964936bf33637e03054368c7
Jordan Liggitt <jliggitt@redhat.com>
2015-02-20 15:44:36 -0500

    Strip access_token param from requests in auth layer

--- pkg/auth/authenticator/request/paramtoken/paramtoken.go
+++ pkg/auth/authenticator/request/paramtoken/paramtoken.go
@@ -13,18 +13,27 @@ import (
 // For this authenticator to work, tokens will be part of the request URL, and are more likely to be logged or otherwise exposed.
 // Every effort should be made to filter tokens from being logged when using this authenticator.
 type Authenticator struct {
+	// Param is the query param to use as a token
 	param string
-	auth  authenticator.Token
+	// Auth is the token authenticator to use to validate the token
+	auth authenticator.Token
+	// Remove indicates whether the parameter should be stripped from the incoming request
+	remove bool
 }
 
-func New(param string, auth authenticator.Token) *Authenticator {
-	return &Authenticator{param, auth}
+func New(param string, auth authenticator.Token, remove bool) *Authenticator {
+	return &Authenticator{param, auth, remove}
 }
 
 func (a *Authenticator) AuthenticateRequest(req *http.Request) (user.Info, bool, error) {
-	token := strings.TrimSpace(req.FormValue(a.param))
+	q := req.URL.Query()
+	token := strings.TrimSpace(q.Get(a.param))
 	if token == "" {
 		return nil, false, nil
+	}
+	if a.remove {
+		q.Del(a.param)
+		req.URL.RawQuery = q.Encode()
 	}
 	return a.auth.AuthenticateToken(token)
 }
