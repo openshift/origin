81e3e81b519e89b37f0b470a1cf8e23d527ca11c
Scott Dodson <sdodson@redhat.com>
2015-03-12 11:58:25 -0400

    Add save-images.sh that saves docker images to your home directory

---
+++ images/save-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+OS_ROOT=/home/cloud-user/origin
+source "${OS_ROOT}/hack/common.sh"
+
+os::build::get_version_vars
+# OS_GIT_VERSION is like 'v0.3.1-4-g2c853ed-dirty'
+IMAGE_VERSION=`echo ${OS_GIT_VERSION} | cut -d '-' -f 1`
+
+for i in ose-deployer ose-docker-builder ose-docker-registry ose-haproxy-router ose-pod ose-sti-builder ose-sti-image-builder;
+do
+  docker save localhost:5000/openshift3_beta/${i}:${IMAGE_VERSION} | gzip > ~/openshift3_beta--${i}-${IMAGE_VERSION}.tar.gz
+done
+
+echo "Images for ${IMAGE_VERSION} saved to ~/openshift3_beta--IMAGE-NAME-${IMAGE_VERSION}.tar.gz"
