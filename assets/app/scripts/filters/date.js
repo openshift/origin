d5bdc46bdb969ecd842165c54d507e0038b848ab
Jessica Forrester <jforrest@redhat.com>
2015-03-19 14:40:20 -0400

    Add build duration to web console

--- assets/app/scripts/filters/date.js
+++ assets/app/scripts/filters/date.js
@@ -7,6 +7,54 @@ angular.module('openshiftConsole')
       return moment(timestamp).fromNow();
     };
   })
+  .filter('duration', function() {
+    return function(timestampLhs, timestampRhs) {
+      if (!timestampLhs) {
+        return timestampLhs;
+      }
+      timestampRhs = timestampRhs || Date();
+
+      var ms = moment(timestampRhs).diff(timestampLhs);
+      var duration = moment.duration(ms);
+      // the out of the box humanize in moment.js rounds to the nearest time unit
+      // but we need more details
+      var humanizedDuration = [];
+      var years = duration.years();
+      var months = duration.months();
+      var days = duration.days();
+      var hours = duration.hours();
+      var minutes = duration.minutes();
+      var seconds = duration.seconds();
+      if (years > 0) {
+        humanizedDuration.push(years + (years == 1 ? " year" : " years"));
+      }
+      if (months > 0) {
+        humanizedDuration.push(months + (months == 1 ? " month" : " months"));
+      }
+      if (days > 0) {
+        humanizedDuration.push(days + (days == 1 ? " day" : " days"));
+      }
+      if (hours > 0) {
+        humanizedDuration.push(hours + (hours == 1 ? " hour" : " hours"));
+      }
+      if (minutes > 0) {
+        humanizedDuration.push(minutes + (minutes == 1 ? " minute" : " minutes"));
+      }
+      if (seconds > 0) {
+        humanizedDuration.push(seconds + (seconds == 1 ? " second" : " seconds"));
+      }      
+
+      if (humanizedDuration.length == 0) {
+        humanizedDuration.push("0 seconds");
+      }
+
+      if (humanizedDuration.length > 2) {
+        humanizedDuration.length = 2;
+      }
+
+      return humanizedDuration.join(", ");
+    };
+  })  
   .filter('ageLessThan', function() {
     // ex:  amt = 5  and unit = 'minutes'
     return function(timestamp, amt, unit) {
