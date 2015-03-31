0e8936da3a604044ee399291e7777311783ff4af
Clayton Coleman <ccoleman@redhat.com>
2015-03-30 23:51:44 -0400

    Add extra tests for DNS and prepare for Headless services

--- test/integration/dns_test.go
+++ test/integration/dns_test.go
@@ -3,9 +3,11 @@
 package integration
 
 import (
+	"net"
 	"testing"
 	"time"
 
+	//kapi "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
 
 	"github.com/miekg/dns"
@@ -17,6 +19,8 @@ func TestDNS(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+
+	var masterIP net.IP
 
 	// verify service DNS entry is visible
 	stop := make(chan struct{})
@@ -36,24 +40,68 @@ func TestDNS(t *testing.T) {
 		}
 		if a, ok := in.Answer[0].(*dns.A); ok {
 			if a.A == nil {
-				t.Errorf("expected an A record with an IP: %#v", a)
+				t.Fatalf("expected an A record with an IP: %#v", a)
 			}
+			masterIP = a.A
 		} else {
-			t.Errorf("expected an A record: %#v", in)
+			t.Fatalf("expected an A record: %#v", in)
 		}
 		t.Log(in)
 		close(stop)
 	}, 50*time.Millisecond, stop)
 
+	// TODO: uncomment when headless services are supported
+	/*client, err := testutil.GetClusterAdminKubeClient(clientFile)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if _, err := client.Services(kapi.NamespaceDefault).Create(&kapi.Service{
+		ObjectMeta: kapi.ObjectMeta{
+			Name: "headless",
+		},
+		Spec: kapi.ServiceSpec{
+			PortalIP: "None",
+			Port:     443,
+		},
+	}); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if _, err := client.Endpoints(kapi.NamespaceDefault).Create(&kapi.Endpoints{
+		ObjectMeta: kapi.ObjectMeta{
+			Name: "headless",
+		},
+		Endpoints: []kapi.Endpoint{
+			{
+				IP:   "172.0.0.1",
+				Port: 2345,
+			},
+		},
+	}); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	headlessIP := net.ParseIP("172.0.0.1")*/
+
 	// verify recursive DNS lookup is visible when expected
 	tests := []struct {
 		dnsQuestionName   string
 		recursionExpected bool
+		expect            *net.IP
 	}{
 		{
 			dnsQuestionName:   "foo.kubernetes.default.local.",
 			recursionExpected: false,
+			expect:            &masterIP,
 		},
+		{
+			dnsQuestionName:   "openshift.default.local.",
+			recursionExpected: false,
+			expect:            &masterIP,
+		},
+		/*{
+			dnsQuestionName:   "headless.default.local.",
+			recursionExpected: false,
+			expect:            &headlessIP,
+		},*/
 		{
 			dnsQuestionName:   "www.google.com.",
 			recursionExpected: true,
@@ -76,6 +124,10 @@ func TestDNS(t *testing.T) {
 		if a, ok := in.Answer[0].(*dns.A); ok {
 			if a.A == nil {
 				t.Errorf("expected an A record with an IP: %#v", a)
+			} else {
+				if tc.expect != nil && tc.expect.String() != a.A.String() {
+					t.Errorf("A record has a different IP than the test case: %v / %v", a.A, *tc.expect)
+				}
 			}
 		} else {
 			t.Errorf("expected an A record: %#v", in)
