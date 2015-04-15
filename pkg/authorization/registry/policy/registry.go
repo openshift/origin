557f14365312d6243da34a88f99f746c7176916a
deads2k <deads@redhat.com>
2015-04-15 13:43:57 -0400

    migrate policy to new rest storage

--- pkg/authorization/registry/policy/registry.go
+++ pkg/authorization/registry/policy/registry.go
@@ -2,6 +2,7 @@ package policy
 
 import (
 	kapi "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api/rest"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/fields"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/watch"
@@ -21,6 +22,58 @@ type Registry interface {
 	UpdatePolicy(ctx kapi.Context, policy *authorizationapi.Policy) error
 	// DeletePolicy deletes a policy.
 	DeletePolicy(ctx kapi.Context, id string) error
-	// WatchPolicyBindings watches policyBindings.
+	// WatchPolicyBindings watches policies.
 	WatchPolicies(ctx kapi.Context, label labels.Selector, field fields.Selector, resourceVersion string) (watch.Interface, error)
+}
+
+// Storage is an interface for a standard REST Storage backend
+type Storage interface {
+	rest.StandardStorage
+}
+
+// storage puts strong typing around storage calls
+type storage struct {
+	Storage
+}
+
+// NewRegistry returns a new Registry interface for the given Storage. Any mismatched
+// types will panic.
+func NewRegistry(s Storage) Registry {
+	return &storage{s}
+}
+
+func (s *storage) ListPolicies(ctx kapi.Context, label labels.Selector, field fields.Selector) (*authorizationapi.PolicyList, error) {
+	obj, err := s.List(ctx, label, field)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj.(*authorizationapi.PolicyList), nil
+}
+
+func (s *storage) CreatePolicy(ctx kapi.Context, node *authorizationapi.Policy) error {
+	_, err := s.Create(ctx, node)
+	return err
+}
+
+func (s *storage) UpdatePolicy(ctx kapi.Context, node *authorizationapi.Policy) error {
+	_, _, err := s.Update(ctx, node)
+	return err
+}
+
+func (s *storage) WatchPolicies(ctx kapi.Context, label labels.Selector, field fields.Selector, resourceVersion string) (watch.Interface, error) {
+	return s.Watch(ctx, label, field, resourceVersion)
+}
+
+func (s *storage) GetPolicy(ctx kapi.Context, name string) (*authorizationapi.Policy, error) {
+	obj, err := s.Get(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+	return obj.(*authorizationapi.Policy), nil
+}
+
+func (s *storage) DeletePolicy(ctx kapi.Context, name string) error {
+	_, err := s.Delete(ctx, name, nil)
+	return err
 }
--- pkg/authorization/registry/policy/rest.go
+++ pkg/authorization/registry/policy/rest.go
@@ -1,58 +1,81 @@
 package policy
 
 import (
+	"fmt"
+
 	kapi "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/fields"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/registry/generic"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/runtime"
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/watch"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/util/fielderrors"
 
 	authorizationapi "github.com/openshift/origin/pkg/authorization/api"
+	"github.com/openshift/origin/pkg/authorization/api/validation"
 )
 
-// REST implements the RESTStorage interface in terms of an Registry.
-type REST struct {
-	registry Registry
+// strategy implements behavior for nodes
+type strategy struct {
+	runtime.ObjectTyper
 }
 
-// NewREST creates a new REST for policies.
-func NewREST(registry Registry) *REST {
-	return &REST{registry}
+// Strategy is the default logic that applies when creating and updating Policy objects.
+var Strategy = strategy{kapi.Scheme}
+
+// NamespaceScoped is true for policies.
+func (strategy) NamespaceScoped() bool {
+	return true
 }
 
-// New creates a new Policy object
-func (r *REST) New() runtime.Object {
-	return &authorizationapi.Policy{}
-}
-func (r *REST) NewList() runtime.Object {
-	return &authorizationapi.PolicyList{}
+// AllowCreateOnUpdate is false for policies.
+func (strategy) AllowCreateOnUpdate() bool {
+	return false
 }
 
-// List obtains a list of Policies that match selector.
-func (r *REST) List(ctx kapi.Context, label labels.Selector, field fields.Selector) (runtime.Object, error) {
-	policies, err := r.registry.ListPolicies(ctx, label, field)
-	if err != nil {
-		return nil, err
+func (strategy) GenerateName(base string) string {
+	return base
+}
+
+// PrepareForCreate clears fields that are not allowed to be set by end users on creation.
+func (strategy) PrepareForCreate(obj runtime.Object) {
+	policy := obj.(*authorizationapi.Policy)
+
+	policy.Name = authorizationapi.PolicyName
+}
+
+// PrepareForUpdate clears fields that are not allowed to be set by end users on update.
+func (strategy) PrepareForUpdate(obj, old runtime.Object) {
+	_ = obj.(*authorizationapi.Policy)
+}
+
+// Validate validates a new policy.
+func (strategy) Validate(ctx kapi.Context, obj runtime.Object) fielderrors.ValidationErrorList {
+	return validation.ValidatePolicy(obj.(*authorizationapi.Policy))
+}
+
+// ValidateUpdate is the default update validation for an end user.
+func (strategy) ValidateUpdate(ctx kapi.Context, obj, old runtime.Object) fielderrors.ValidationErrorList {
+	return validation.ValidatePolicyUpdate(old.(*authorizationapi.Policy), obj.(*authorizationapi.Policy))
+}
+
+// Matcher returns a generic matcher for a given label and field selector.
+func Matcher(label labels.Selector, field fields.Selector) generic.Matcher {
+	return &generic.SelectionPredicate{
+		Label: label,
+		Field: field,
+		GetAttrs: func(obj runtime.Object) (labels.Set, fields.Set, error) {
+			policy, ok := obj.(*authorizationapi.Policy)
+			if !ok {
+				return nil, nil, fmt.Errorf("not a policy")
+			}
+			return labels.Set(policy.ObjectMeta.Labels), SelectableFields(policy), nil
+		},
 	}
-	return policies, err
-
 }
 
-// Get obtains the policy specified by its id.
-func (r *REST) Get(ctx kapi.Context, id string) (runtime.Object, error) {
-	policy, err := r.registry.GetPolicy(ctx, id)
-	if err != nil {
-		return nil, err
+// SelectableFields returns a label set that represents the object
+func SelectableFields(policy *authorizationapi.Policy) fields.Set {
+	return fields.Set{
+		"name": policy.Name,
 	}
-	return policy, err
-}
-
-// Delete asynchronously deletes the Policy specified by its id.
-func (r *REST) Delete(ctx kapi.Context, id string) (runtime.Object, error) {
-	return &kapi.Status{Status: kapi.StatusSuccess}, r.registry.DeletePolicy(ctx, id)
-}
-
-// Watch begins watching for new, changed, or deleted PolicyBindings.
-func (r *REST) Watch(ctx kapi.Context, label labels.Selector, field fields.Selector, resourceVersion string) (watch.Interface, error) {
-	return r.registry.WatchPolicies(ctx, label, field, resourceVersion)
 }
