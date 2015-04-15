deea1aaab3332e174a2c4143ae721b73030f6183
Brenton Leanhardt <bleanhar@redhat.com>
2015-04-15 14:06:33 -0400

    Reverting the v2 registry switch
    Origin isnt' using it yet.  There are a lot of implications for builds that we
    aren't ready for.
    
    This reverts commit 5bcd528f7141afdc63186ff0789214516074e5d7.

--- images/build-ose-images.sh
+++ images/build-ose-images.sh
@@ -20,7 +20,6 @@ docker build --no-cache=true -t openshift3_beta/ose-docker-builder ./builder/doc
 docker build --no-cache=true -t openshift3_beta/ose-sti-builder ./builder/docker/sti-builder/ && \
 docker build --no-cache=true -t openshift3_beta/ose-sti-image-builder ./builder/docker/sti-image-builder/ && \
 docker build --no-cache=true -t openshift3_beta/ose-pod ./pod/
-docker build --no-cache=true -t openshift3_beta/ose-docker-registry ./dockerregistry
 
 docker tag -f openshift3_beta/ose-docker-builder localhost:5000/openshift3_beta/ose-docker-builder
 docker tag -f openshift3_beta/ose-docker-builder localhost:5000/openshift3_beta/ose-docker-builder:${IMAGE_VERSION}
@@ -40,8 +39,7 @@ docker tag -f openshift3_beta/ose-haproxy-router localhost:5000/openshift3_beta/
 docker tag -f openshift3_beta/ose-pod localhost:5000/openshift3_beta/ose-pod
 docker tag -f openshift3_beta/ose-pod localhost:5000/openshift3_beta/ose-pod:${IMAGE_VERSION}
 
-docker tag -f openshift3_beta/ose-docker-registry localhost:5000/openshift3_beta/ose-docker-registry
-docker tag -f openshift3_beta/ose-docker-registry localhost:5000/openshift3_beta/ose-docker-registry:${IMAGE_VERSION}
+
 
 docker push localhost:5000/openshift3_beta/ose-docker-builder:latest &&
 docker push localhost:5000/openshift3_beta/ose-docker-builder:${IMAGE_VERSION} &&
@@ -55,7 +53,16 @@ docker push localhost:5000/openshift3_beta/ose-haproxy-router:latest &&
 docker push localhost:5000/openshift3_beta/ose-haproxy-router:${IMAGE_VERSION} &&
 docker push localhost:5000/openshift3_beta/ose-pod:latest &&
 docker push localhost:5000/openshift3_beta/ose-pod:${IMAGE_VERSION}
-docker push localhost:5000/openshift3_beta/ose-docker-registry:latest &&
-docker push localhost:5000/openshift3_beta/ose-docker-registry:${IMAGE_VERSION}
+
+if [[ -d ${OS_ROOT}/../docker-registry-extensions ]]; then
+  docker build --no-cache=true -t openshift3_beta/ose-docker-registry ${OS_ROOT}/../docker-registry-extensions/
+  docker tag -f openshift3_beta/ose-docker-registry localhost:5000/openshift3_beta/ose-docker-registry
+  docker tag -f openshift3_beta/ose-docker-registry localhost:5000/openshift3_beta/ose-docker-registry:${IMAGE_VERSION}
+  docker push localhost:5000/openshift3_beta/ose-docker-registry:latest &&
+  docker push localhost:5000/openshift3_beta/ose-docker-registry:${IMAGE_VERSION}
+else
+  echo "WARNING -- Could not find ../docker-registry-extensions I have not built the registry, this build is incomplete"
+  exit 1
+fi
 
 docker rmi $(docker images -q --filter "dangling=true")
