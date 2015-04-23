244b137a09bb3097af775098c8cf9e629dad63ad
Jessica Forrester <jforrest@redhat.com>
2015-04-23 14:09:21 -0400

    Deployments page throws JS error, still referencing /images when should be getting imageStreams

--- assets/app/scripts/controllers/deployments.js
+++ assets/app/scripts/controllers/deployments.js
@@ -8,34 +8,44 @@
  * Controller of the openshiftConsole
  */
 angular.module('openshiftConsole')
-  .controller('DeploymentsController', function ($scope, DataService, $filter, LabelFilter, Logger) {
+  .controller('DeploymentsController', function ($scope, DataService, $filter, LabelFilter, Logger, ImageStreamResolver) {
     $scope.deployments = {};
+    $scope.podTemplates = {};
     $scope.unfilteredDeployments = {};
-    $scope.images = {};
+    $scope.imageStreams = {};
     $scope.imagesByDockerReference = {};
+    $scope.imageStreamImageRefByDockerReference = {}; // lets us determine if a particular container's docker image reference belongs to an imageStream
     $scope.builds = {};
     $scope.labelSuggestions = {};
     $scope.alerts = $scope.alerts || {};    
     $scope.emptyMessage = "Loading...";
     var watches = [];
 
+    var extractPodTemplates = function() {
+      angular.forEach($scope.deployments, function(deployment, deploymentId){
+        $scope.podTemplates[deploymentId] = deployment.spec.template;
+      });
+    };
+
     watches.push(DataService.watch("replicationcontrollers", $scope, function(deployments) {
       $scope.unfilteredDeployments = deployments.by("metadata.name");
       LabelFilter.addLabelSuggestionsFromResources($scope.unfilteredDeployments, $scope.labelSuggestions);
       LabelFilter.setLabelSuggestions($scope.labelSuggestions);
       $scope.deployments = LabelFilter.getLabelSelector().select($scope.unfilteredDeployments);
+      extractPodTemplates();
+      ImageStreamResolver.fetchReferencedImageStreamImages($scope.podTemplates, $scope.imagesByDockerReference, $scope.imageStreamImageRefByDockerReference, $scope);      
       $scope.emptyMessage = "No deployments to show";
       updateFilterWarning();
       Logger.log("deployments (subscribe)", $scope.deployments);
     }));    
 
-    // Also load images and builds to fill out details in the pod template
-    watches.push(DataService.watch("images", $scope, function(images) {
-      $scope.images = images.by("metadata.name");
-      $scope.imagesByDockerReference = images.by("dockerImageReference");
-      Logger.log("images (subscribe)", $scope.images);
-      Logger.log("imagesByDockerReference (subscribe)", $scope.imagesByDockerReference);
-    }));    
+    // Sets up subscription for imageStreams
+    watches.push(DataService.watch("imageStreams", $scope, function(imageStreams) {
+      $scope.imageStreams = imageStreams.by("metadata.name");
+      ImageStreamResolver.buildDockerRefMapForImageStreams($scope.imageStreams, $scope.imageStreamImageRefByDockerReference);
+      ImageStreamResolver.fetchReferencedImageStreamImages($scope.podTemplates, $scope.imagesByDockerReference, $scope.imageStreamImageRefByDockerReference, $scope);
+      Logger.log("imageStreams (subscribe)", $scope.imageStreams);
+    }));  
 
     watches.push(DataService.watch("builds", $scope, function(builds) {
       $scope.builds = builds.by("metadata.name");
