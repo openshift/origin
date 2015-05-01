3a8035faa136f4afdb65d0e8476a45e63302d762
deads2k <deads@redhat.com>
2015-05-01 15:49:51 -0400

    grant admin and edit access to secrets

--- pkg/cmd/server/bootstrappolicy/policy.go
+++ pkg/cmd/server/bootstrappolicy/policy.go
@@ -59,7 +59,7 @@ func GetBootstrapMasterRoles(masterNamespace string) []authorizationapi.Role {
 			Rules: []authorizationapi.PolicyRule{
 				{
 					Verbs:     util.NewStringSet("get", "list", "watch", "create", "update", "delete"),
-					Resources: util.NewStringSet(authorizationapi.OpenshiftExposedGroupName, authorizationapi.PermissionGrantingGroupName, authorizationapi.KubeExposedGroupName, "projects"),
+					Resources: util.NewStringSet(authorizationapi.OpenshiftExposedGroupName, authorizationapi.PermissionGrantingGroupName, authorizationapi.KubeExposedGroupName, "projects", "secrets"),
 				},
 				{
 					Verbs:     util.NewStringSet("get", "list", "watch"),
@@ -75,7 +75,7 @@ func GetBootstrapMasterRoles(masterNamespace string) []authorizationapi.Role {
 			Rules: []authorizationapi.PolicyRule{
 				{
 					Verbs:     util.NewStringSet("get", "list", "watch", "create", "update", "delete"),
-					Resources: util.NewStringSet(authorizationapi.OpenshiftExposedGroupName, authorizationapi.KubeExposedGroupName),
+					Resources: util.NewStringSet(authorizationapi.OpenshiftExposedGroupName, authorizationapi.KubeExposedGroupName, "secrets"),
 				},
 				{
 					Verbs:     util.NewStringSet("get", "list", "watch"),
