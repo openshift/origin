33b7dabd8b09f8421798d628343af1c1a4ef0423
deads2k <deads@redhat.com>
2015-05-13 14:17:19 -0400

    osc login stanza names

--- pkg/cmd/cli/config/smart_merge.go
+++ pkg/cmd/cli/config/smart_merge.go
@@ -1,25 +1,87 @@
 package config
 
 import (
-	"fmt"
-	"net"
 	"net/url"
 	"reflect"
-	"regexp"
-
-	"github.com/golang/glog"
+	"strings"
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/client"
 	clientcmdapi "github.com/GoogleCloudPlatform/kubernetes/pkg/client/clientcmd/api"
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
+	"github.com/GoogleCloudPlatform/kubernetes/third_party/golang/netutil"
 
-	"github.com/openshift/origin/pkg/cmd/flagtypes"
+	osclient "github.com/openshift/origin/pkg/client"
 )
 
-var invalidSafeStringSep = regexp.MustCompile(`[.:/]`)
+// getClusterNickname returns host:port of the clientConfig.Host, with .'s replaced by -'s
+func getClusterNickname(clientCfg *client.Config) (string, error) {
+	u, err := url.Parse(clientCfg.Host)
+	if err != nil {
+		return "", err
+	}
+	hostPort := netutil.CanonicalAddr(u)
+
+	// we need a character other than "." to avoid conflicts with.  replace with '-'
+	return strings.Replace(hostPort, ".", "-", -1), nil
+}
+
+// getUserNickname returns "username(as known by the server)/getClusterNickname".  This allows tab completion for switching users to
+// work easily and obviously.
+func getUserNickname(clientCfg *client.Config) (string, error) {
+	client, err := osclient.New(clientCfg)
+	if err != nil {
+		return "", err
+	}
+	userInfo, err := client.Users().Get("~")
+	if err != nil {
+		return "", err
+	}
+
+	clusterNick, err := getClusterNickname(clientCfg)
+	if err != nil {
+		return "", err
+	}
+
+	return userInfo.Name + "/" + clusterNick, nil
+}
+
+// getContextNickname returns "namespace/getClusterNickname/username(as known by the server)".  This allows tab completion for switching projects/context
+// to work easily.  First tab is the most selective on project.  Second stanza in the next most selective on cluster name.  The chances of a user trying having
+// one projects on a single server that they want to operate against with two identities is low, so username is last.
+func getContextNickname(namespace string, clientCfg *client.Config) (string, error) {
+	client, err := osclient.New(clientCfg)
+	if err != nil {
+		return "", err
+	}
+	userInfo, err := client.Users().Get("~")
+	if err != nil {
+		return "", err
+	}
+
+	clusterNick, err := getClusterNickname(clientCfg)
+	if err != nil {
+		return "", err
+	}
+
+	return namespace + "/" + clusterNick + "/" + userInfo.Name, nil
+}
 
 // CreatePartialConfig takes a clientCfg and builds a config (kubeconfig style) from it.
-func CreateConfig(credentialsName string, namespace string, clientCfg *client.Config) clientcmdapi.Config {
+func CreateConfig(namespace string, clientCfg *client.Config) (*clientcmdapi.Config, error) {
+	clusterNick, err := getClusterNickname(clientCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	userNick, err := getUserNickname(clientCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	contextNick, err := getContextNickname(namespace, clientCfg)
+	if err != nil {
+		return nil, err
+	}
+
 	config := clientcmdapi.NewConfig()
 
 	credentials := clientcmdapi.NewAuthInfo()
@@ -32,13 +94,8 @@ func CreateConfig(credentialsName string, namespace string, clientCfg *client.Co
 	if len(credentials.ClientKey) == 0 {
 		credentials.ClientKeyData = clientCfg.TLSClientConfig.KeyData
 	}
-	if len(credentialsName) == 0 {
-		credentialsName = "osc-login"
-	}
-	config.AuthInfos[credentialsName] = *credentials
+	config.AuthInfos[userNick] = *credentials
 
-	serverAddr := flagtypes.Addr{Value: clientCfg.Host}.Default()
-	clusterName := fmt.Sprintf("%v:%v", serverAddr.Host, serverAddr.Port)
 	cluster := clientcmdapi.NewCluster()
 	cluster.Server = clientCfg.Host
 	cluster.CertificateAuthority = clientCfg.CAFile
@@ -46,87 +103,47 @@ func CreateConfig(credentialsName string, namespace string, clientCfg *client.Co
 		cluster.CertificateAuthorityData = clientCfg.CAData
 	}
 	cluster.InsecureSkipTLSVerify = clientCfg.Insecure
-	config.Clusters[clusterName] = *cluster
+	config.Clusters[clusterNick] = *cluster
 
-	contextName := clusterName + "-" + credentialsName
 	context := clientcmdapi.NewContext()
-	context.Cluster = clusterName
-	context.AuthInfo = credentialsName
+	context.Cluster = clusterNick
+	context.AuthInfo = userNick
 	context.Namespace = namespace
-	config.Contexts[contextName] = *context
-	config.CurrentContext = contextName
+	config.Contexts[contextNick] = *context
+	config.CurrentContext = contextNick
 
-	return *config
+	return config, nil
 }
 
-// MergeConfig takes a haystack to look for existing stanzas in (probably the merged config) and the new additions to merge in.
-// It tries to find equivalents for the addition inside of the
-// haystack and uses the mapping to avoid creating additional stanzas with duplicate information.  It either locates or original
-// stanzas or creates new ones for clusters and users.  Then it uses the mapped names to build the correct contexts
-func MergeConfig(haystack, addition clientcmdapi.Config) (*clientcmdapi.Config, error) {
-	ret := haystack
+// MergeConfig adds the additional Config stanzas to the startingConfig.  It blindly stomps clusters and users, but
+// it searches for a matching context before writing a new one.
+func MergeConfig(startingConfig, addition clientcmdapi.Config) (*clientcmdapi.Config, error) {
+	ret := startingConfig
 
-	requestedClusterNamesToActualClusterNames := map[string]string{}
-	existingClusterNames, err := getMapKeys(reflect.ValueOf(haystack.Clusters))
-	if err != nil {
-		return nil, err
-	}
-	for requestedKey, needle := range addition.Clusters {
-		if existingName := FindExistingClusterName(haystack, needle); len(existingName) > 0 {
-			requestedClusterNamesToActualClusterNames[requestedKey] = existingName
-			continue
-		}
-
-		uniqueName := getUniqueName(requestedKey, existingClusterNames)
-		requestedClusterNamesToActualClusterNames[requestedKey] = uniqueName
-		ret.Clusters[uniqueName] = needle
+	for requestedKey, value := range addition.Clusters {
+		ret.Clusters[requestedKey] = value
 	}
 
-	requestedAuthInfoNamesToActualAuthInfoNames := map[string]string{}
-	existingAuthInfoNames, err := getMapKeys(reflect.ValueOf(haystack.AuthInfos))
-	if err != nil {
-		return nil, err
-	}
-	for requestedKey, needle := range addition.AuthInfos {
-		if existingName := FindExistingAuthInfoName(haystack, needle); len(existingName) > 0 {
-			requestedAuthInfoNamesToActualAuthInfoNames[requestedKey] = existingName
-			continue
-		}
-
-		uniqueName := getUniqueName(requestedKey, existingAuthInfoNames)
-		requestedAuthInfoNamesToActualAuthInfoNames[requestedKey] = uniqueName
-		ret.AuthInfos[uniqueName] = needle
+	for requestedKey, value := range addition.AuthInfos {
+		ret.AuthInfos[requestedKey] = value
 	}
 
 	requestedContextNamesToActualContextNames := map[string]string{}
-	existingContextNames, err := getMapKeys(reflect.ValueOf(haystack.Contexts))
-	if err != nil {
-		return nil, err
-	}
-	for requestedKey, needle := range addition.Contexts {
-		exists := false
-
+	for requestedKey, newContext := range addition.Contexts {
 		actualContext := clientcmdapi.NewContext()
-		actualContext.AuthInfo, exists = requestedAuthInfoNamesToActualAuthInfoNames[needle.AuthInfo]
-		if !exists {
-			actualContext.AuthInfo = needle.AuthInfo
-		}
-		actualContext.Cluster, exists = requestedClusterNamesToActualClusterNames[needle.Cluster]
-		if !exists {
-			actualContext.Cluster = needle.Cluster
-		}
-		actualContext.Namespace = needle.Namespace
-		actualContext.Extensions = needle.Extensions
+		actualContext.AuthInfo = newContext.AuthInfo
+		actualContext.Cluster = newContext.Cluster
+		actualContext.Namespace = newContext.Namespace
+		actualContext.Extensions = newContext.Extensions
 
-		if existingName := FindExistingContextName(haystack, *actualContext); len(existingName) > 0 {
+		if existingName := FindExistingContextName(startingConfig, *actualContext); len(existingName) > 0 {
 			// if this already exists, just move to the next, our job is done
 			requestedContextNamesToActualContextNames[requestedKey] = existingName
 			continue
 		}
 
-		uniqueName := GenerateContextIdentifier(actualContext.Namespace, actualContext.Cluster, actualContext.AuthInfo, existingContextNames)
-		requestedContextNamesToActualContextNames[requestedKey] = uniqueName
-		ret.Contexts[uniqueName] = *actualContext
+		requestedContextNamesToActualContextNames[requestedKey] = requestedKey
+		ret.Contexts[requestedKey] = *actualContext
 	}
 
 	if len(addition.CurrentContext) > 0 {
@@ -140,30 +157,6 @@ func MergeConfig(haystack, addition clientcmdapi.Config) (*clientcmdapi.Config, 
 	return &ret, nil
 }
 
-// FindExistingClusterName finds the nickname for the passed cluster config
-func FindExistingClusterName(haystack clientcmdapi.Config, needle clientcmdapi.Cluster) string {
-	for key, cluster := range haystack.Clusters {
-		cluster.LocationOfOrigin = ""
-		if reflect.DeepEqual(cluster, needle) {
-			return key
-		}
-	}
-
-	return ""
-}
-
-// FindExistingAuthInfoName finds the nickname for the passed auth info
-func FindExistingAuthInfoName(haystack clientcmdapi.Config, needle clientcmdapi.AuthInfo) string {
-	for key, authInfo := range haystack.AuthInfos {
-		authInfo.LocationOfOrigin = ""
-		if reflect.DeepEqual(authInfo, needle) {
-			return key
-		}
-	}
-
-	return ""
-}
-
 // FindExistingContextName finds the nickname for the passed context
 func FindExistingContextName(haystack clientcmdapi.Config, needle clientcmdapi.Context) string {
 	for key, context := range haystack.Contexts {
@@ -173,117 +166,5 @@ func FindExistingContextName(haystack clientcmdapi.Config, needle clientcmdapi.C
 		}
 	}
 
-	return ""
-}
-
-func getMapKeys(theMap reflect.Value) (*util.StringSet, error) {
-	if theMap.Kind() != reflect.Map {
-		return nil, fmt.Errorf("theMap must be of type %v, not %v", reflect.Map, theMap.Kind())
-	}
-
-	ret := &util.StringSet{}
-
-	switch theMap.Kind() {
-	case reflect.Map:
-		for _, keyValue := range theMap.MapKeys() {
-			ret.Insert(keyValue.String())
-		}
-
-	}
-
-	return ret, nil
-
-}
-
-func getUniqueName(basename string, existingNames *util.StringSet) string {
-	if parsedUrl, err := url.Parse(basename); err != nil {
-		if host, port, err := net.SplitHostPort(parsedUrl.Host); err != nil {
-			if !existingNames.Has(host) {
-				return host
-			}
-			if id := host + "-" + port; !existingNames.Has(id) {
-				return id
-			}
-		}
-	}
-
-	basename = invalidSafeStringSep.ReplaceAllString(basename, "-")
-
-	if !existingNames.Has(basename) {
-		return basename
-	}
-
-	for i := 0; i < 100; i++ {
-		trialName := fmt.Sprintf("%v-%d", basename, i)
-		if !existingNames.Has(trialName) {
-			return trialName
-		}
-	}
-
-	return string(util.NewUUID())
-}
-
-// GenerateContextIdentifier generates the best context identifier possible based on the information it gets.
-func GenerateContextIdentifier(namespace string, cluster string, authInfo string, existingContextIdentifiers *util.StringSet) string {
-	ctx := ""
-
-	// try to use plain namespace
-	if len(namespace) > 0 {
-		ctx += namespace
-
-		if !existingContextIdentifiers.Has(ctx) {
-			return ctx
-		}
-	}
-
-	// tries appending "-host" or "-host-port"
-	if len(cluster) > 0 {
-		if parsedUrl, err := url.Parse(cluster); err != nil {
-			if host, port, err := net.SplitHostPort(parsedUrl.Host); err != nil {
-				if len(ctx) > 0 {
-					ctx += "-"
-				}
-				ctx += host
-				if !existingContextIdentifiers.Has(ctx) {
-					return ctx
-				}
-
-				ctx += "-" + port
-				if !existingContextIdentifiers.Has(ctx) {
-					return ctx
-				}
-
-			} else {
-				if len(ctx) > 0 {
-					ctx += "-"
-				}
-				ctx += "-" + parsedUrl.Host
-				if !existingContextIdentifiers.Has(ctx) {
-					return ctx
-				}
-			}
-		}
-	}
-
-	// tries appending "-username"
-	if len(authInfo) > 0 {
-		if len(ctx) > 0 {
-			ctx += "-"
-		}
-		ctx += authInfo
-
-		if !existingContextIdentifiers.Has(ctx) {
-			return ctx
-		}
-	}
-
-	// append an integer
-	for i := 0; i < 100; i++ {
-		if trialName := fmt.Sprintf("%v-%d", ctx, i); !existingContextIdentifiers.Has(trialName) {
-			return trialName
-		}
-	}
-
-	glog.Fatalf("Unable to generate a context identifier. Please provide a context using the '--context=<context>' flag.")
 	return ""
 }
