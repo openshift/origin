e9bd3df7f2c171fb76182dd7e5f07bdab14d5998
Paul Weil <pweil@redhat.com>
2015-05-20 23:38:14 -0400

    remove unnecessary double escape replacement and add validation to ensure no one is relying on the removed behavior

--- pkg/route/api/validation/validation_test.go
+++ pkg/route/api/validation/validation_test.go
@@ -247,6 +247,19 @@ func TestValidateTLS(t *testing.T) {
 			},
 			expectedErrors: 1,
 		},
+		{
+			name: "Double escaped newlines",
+			route: &api.Route{
+				TLS: &api.TLSConfig{
+					Termination:              api.TLSTerminationReencrypt,
+					Certificate:              "d\\nef",
+					Key:                      "g\\nhi",
+					CACertificate:            "j\\nkl",
+					DestinationCACertificate: "j\\nkl",
+				},
+			},
+			expectedErrors: 4,
+		},
 	}
 
 	for _, tc := range tests {
