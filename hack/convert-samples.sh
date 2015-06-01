e8440d95c5557a133c45115a5001e889a17f8407
Michal Fojtik <mfojtik@redhat.com>
2015-06-01 16:09:56 +0200

    Fix hack/convert-samples to deal with YAML files

--- hack/convert-samples.sh
+++ hack/convert-samples.sh
@@ -9,7 +9,7 @@ fi
 
 IGNORE_FILES={$IGNORE_FILES:-"examples/sample-app/github-webhook-example.json"}
 
-sample_files=$(find {api,examples,docs,images,plugins,test} -name "*.json")
+sample_files=$(find {api,examples,docs,images,plugins,test} -name "*.json" -o -name "*.yaml")
 ignore_arr=(${IGNORE_FILES//,/ })
 
 for f in $sample_files; do
