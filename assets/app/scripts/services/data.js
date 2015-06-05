73540718168267a0315c24fd35861cf0f1504a45
Stef Walter <stefw@redhat.com>
2015-06-05 12:57:36 +0200

    app: Normalize kind property on retrieved items
    One of the warts in the kubernetes REST API is that depending on how
    you get an item, it may have a kind property or not. Clients are
    expected to infer the kind from type they requested.
    
    The topology view processes items generically, and this the kind
    property plays a large role in that.

--- assets/app/scripts/services/data.js
+++ assets/app/scripts/services/data.js
@@ -663,6 +663,18 @@ angular.module('openshiftConsole')
   };
 
   DataService.prototype._listOpComplete = function(resource, context, data) {
+    // Here we normalize all items to have a kind property.
+    // One of the warts in the kubernetes REST API is that items retrieved
+    // via GET on a list resource won't have a kind property set.
+    // See: https://github.com/kubernetes/kubernetes/issues/3030
+    if (data.kind && data.kind.indexOf("List") === data.kind.length - 4) {
+      angular.forEach(data.items, function(item) {
+        if (!item.kind) {
+          item.kind = data.kind.slice(0, -4);
+        }
+      });
+    }
+
     this._resourceVersion(resource, context, data.resourceVersion || data.metadata.resourceVersion);
     this._data(resource, context, data.items);
     this._listCallbacks(resource, context).fire(this._data(resource, context));
