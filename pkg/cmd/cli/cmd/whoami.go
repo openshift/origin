f735312251ace0b9bee7d22ddbd42999a9efee20
Clayton Coleman <ccoleman@redhat.com>
2015-06-10 12:14:05 -0400

    whoami should show the current token and context
    Add as --token and --context

--- pkg/cmd/cli/cmd/whoami.go
+++ pkg/cmd/cli/cmd/whoami.go
@@ -15,6 +15,13 @@ import (
 
 const WhoAmIRecommendedCommandName = "whoami"
 
+const whoamiLong = `Show information about the current session
+
+The default options for this command will return the currently authenticated user name
+or an empty string.  Other flags support returning the currently used token or the
+user context.
+`
+
 type WhoAmIOptions struct {
 	UserInterface osclient.UserInterface
 
@@ -26,19 +33,16 @@ func NewCmdWhoAmI(name, fullName string, f *clientcmd.Factory, out io.Writer) *c
 
 	cmd := &cobra.Command{
 		Use:   name,
-		Short: "displays the username of the currently authenticated user",
-		Long:  `displays the username of the currently authenticated user`,
+		Short: "Return information about the current session",
+		Long:  whoamiLong,
 		Run: func(cmd *cobra.Command, args []string) {
-			client, _, err := f.Clients()
-			kcmdutil.CheckErr(err)
-
-			o.UserInterface = client.Users()
-			o.Out = out
-
-			_, err = o.WhoAmI()
+			err := RunWhoAmI(f, out, cmd, args, o)
 			kcmdutil.CheckErr(err)
 		},
 	}
+	cmd.Flags().BoolP("token", "t", false, "Print the token the current session is using. This will return an error if you are using a different form of authentication.")
+	cmd.Flags().BoolP("context", "c", false, "Print the current user context name")
+
 	return cmd
 }
 
@@ -49,4 +53,40 @@ func (o WhoAmIOptions) WhoAmI() (*userapi.User, error) {
 	}
 
 	return me, err
+}
+
+func RunWhoAmI(f *clientcmd.Factory, out io.Writer, cmd *cobra.Command, args []string, o *WhoAmIOptions) error {
+	if kcmdutil.GetFlagBool(cmd, "token") {
+		cfg, err := f.OpenShiftClientConfig.ClientConfig()
+		if err != nil {
+			return err
+		}
+		if len(cfg.BearerToken) == 0 {
+			return fmt.Errorf("no token is currently in use for this session")
+		}
+		fmt.Fprintf(out, "%s\n", cfg.BearerToken)
+		return nil
+	}
+	if kcmdutil.GetFlagBool(cmd, "context") {
+		cfg, err := f.OpenShiftClientConfig.RawConfig()
+		if err != nil {
+			return err
+		}
+		if len(cfg.CurrentContext) == 0 {
+			return fmt.Errorf("no context has been set")
+		}
+		fmt.Fprintf(out, "%s\n", cfg.CurrentContext)
+		return nil
+	}
+
+	client, _, err := f.Clients()
+	if err != nil {
+		return err
+	}
+
+	o.UserInterface = client.Users()
+	o.Out = out
+
+	_, err = o.WhoAmI()
+	return err
 }
