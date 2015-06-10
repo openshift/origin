75ae6f1a51c0b9c6c9793686f458aeda6b3edee8
Paul Morie <pmorie@gmail.com>
2015-06-10 02:40:27 -0400

    UPSTREAM: MISSING PULL: Handle SecurityContext correctly for emptyDir volumes

--- Godeps/_workspace/src/github.com/GoogleCloudPlatform/kubernetes/pkg/securitycontext/util.go
+++ Godeps/_workspace/src/github.com/GoogleCloudPlatform/kubernetes/pkg/securitycontext/util.go
@@ -16,7 +16,13 @@ limitations under the License.
 
 package securitycontext
 
-import "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+	"github.com/golang/glog"
+)
 
 // HasPrivilegedRequest returns the value of SecurityContext.Privileged, taking into account
 // the possibility of nils
@@ -40,4 +46,59 @@ func HasCapabilitiesRequest(container *api.Container) bool {
 		return false
 	}
 	return len(container.SecurityContext.Capabilities.Add) > 0 || len(container.SecurityContext.Capabilities.Drop) > 0
+}
+
+// SELinuxOptionsString returns the string representation of a
+// set of SELinuxOptions.
+func SELinuxOptionsString(sel *api.SELinuxOptions) string {
+	if sel == nil {
+		return ""
+	}
+
+	return fmt.Sprintf("%s:%s:%s:%s", sel.User, sel.Role, sel.Type, sel.Level)
+}
+
+const expectedSELinuxContextFields = 4
+
+// ParseSELinuxOptions parses a string containing a full SELinux context
+// (user, role, type, and level) into an SELinuxOptions object.  If the
+// context is malformed, an error is returned.
+func ParseSELinuxOptions(context string) (*api.SELinuxOptions, error) {
+	fields := strings.SplitN(context, ":", expectedSELinuxContextFields)
+
+	if len(fields) != expectedSELinuxContextFields {
+		return nil, fmt.Errorf("expected %v fields in selinuxcontext; got %v (context: %v)", expectedSELinuxContextFields, len(fields), context)
+	}
+
+	return &api.SELinuxOptions{
+		User:  fields[0],
+		Role:  fields[1],
+		Type:  fields[2],
+		Level: fields[3],
+	}, nil
+}
+
+// ProjectSELinuxOptions projects a source SELinuxOptions onto a target
+// SELinuxOptions and returns a _new_ SELinuxOptions containing the result.
+func ProjectSELinuxOptions(source, target *api.SELinuxOptions) *api.SELinuxOptions {
+	glog.V(4).Infof("Projecting security context %v onto %v", SELinuxOptionsString(source), SELinuxOptionsString(target))
+
+	// Copy the receiving options to project onto
+	result := &api.SELinuxOptions{}
+	*result = *target
+
+	if source.User != "" {
+		result.User = source.User
+	}
+	if source.Role != "" {
+		result.Role = source.Role
+	}
+	if source.Type != "" {
+		result.Type = source.Type
+	}
+	if source.Level != "" {
+		result.Level = source.Level
+	}
+
+	return result
 }
