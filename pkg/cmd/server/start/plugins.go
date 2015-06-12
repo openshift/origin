d9fdc80f0781eb72ec7d7f92029c3c0ef14b4782
Cesar Wong <cewong@redhat.com>
2015-06-12 15:11:10 -0400

    Add admission controller for build strategy policy check

--- pkg/cmd/server/start/plugins.go
+++ pkg/cmd/server/start/plugins.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/GoogleCloudPlatform/kubernetes/plugin/pkg/admission/namespace/lifecycle"
 	_ "github.com/GoogleCloudPlatform/kubernetes/plugin/pkg/admission/resourcequota"
 	_ "github.com/GoogleCloudPlatform/kubernetes/plugin/pkg/admission/serviceaccount"
+	_ "github.com/openshift/origin/pkg/build/admission"
 	_ "github.com/openshift/origin/pkg/project/admission/lifecycle"
 	_ "github.com/openshift/origin/pkg/project/admission/nodeenv"
 	_ "github.com/openshift/origin/pkg/security/admission"
