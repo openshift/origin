cf4d4d33ee793e6a1d8d8d62007c68afcbc72694
deads2k <deads@redhat.com>
2015-07-01 13:52:14 -0400

    swagger API changes

--- pkg/cmd/server/origin/swagger.go
+++ pkg/cmd/server/origin/swagger.go
@@ -18,29 +18,35 @@ var apiInfo = map[string]swagger.Info{
 
 // customizeSwaggerDefinition applies selective patches to the swagger API docs
 // TODO: move most of these upstream or to go-restful
-func customizeSwaggerDefinition(api map[string]swagger.ApiDeclaration) {
+func customizeSwaggerDefinition(api *swagger.ApiDeclarationList) {
 	for path, info := range apiInfo {
-		if dec, ok := api[path]; ok {
+		if dec, ok := api.At(path); ok {
 			if len(info.Title) > 0 {
 				dec.Info.Title = info.Title
 			}
 			if len(info.Description) > 0 {
 				dec.Info.Description = info.Description
 			}
-			api[path] = dec
+			api.Put(path, dec)
 		} else {
 			glog.Warningf("No API exists for predefined swagger description %s", path)
 		}
 	}
 	for _, version := range []string{OpenShiftAPIPrefixV1} {
-		model := api[version].Models["runtime.RawExtension"]
-		model.Required = []string{}
-		model.Properties = map[string]swagger.ModelProperty{}
-		model.Description = "this may be any JSON object with a 'kind' and 'apiVersion' field; and is preserved unmodified by processing"
-		api[version].Models["runtime.RawExtension"] = model
+		apiDeclaration, _ := api.At(version)
+		models := &apiDeclaration.Models
 
-		model = api[version].Models["patch.Object"]
+		model, _ := models.At("runtime.RawExtension")
+		model.Required = []string{}
+		model.Properties = swagger.ModelPropertyList{}
+		model.Description = "this may be any JSON object with a 'kind' and 'apiVersion' field; and is preserved unmodified by processing"
+		models.Put("runtime.RawExtension", model)
+
+		model, _ = models.At("patch.Object")
 		model.Description = "represents an object patch, which may be any of: JSON patch (RFC 6902), JSON merge patch (RFC 7396), or the Kubernetes strategic merge patch"
-		api[version].Models["patch.Object"] = model
+		models.Put("patch.Object", model)
+
+		apiDeclaration.Models = *models
+		api.Put(version, apiDeclaration)
 	}
 }
