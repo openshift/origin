280cd68d004c39ebb4be9f548812d831f6f2a922
Ben Parees <bparees@redhat.com>
2015-07-01 11:30:02 -0400

    sample that uses direct docker pullspec for builder image

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -65,10 +65,11 @@ func TestExampleObjectSchemas(t *testing.T) {
 			"hello-project": &projectapi.Project{},
 		},
 		"../examples/sample-app": {
-			"github-webhook-example":           nil, // Skip.
-			"application-template-stibuild":    &templateapi.Template{},
-			"application-template-dockerbuild": &templateapi.Template{},
-			"application-template-custombuild": &templateapi.Template{},
+			"github-webhook-example":             nil, // Skip.
+			"application-template-stibuild":      &templateapi.Template{},
+			"application-template-dockerbuild":   &templateapi.Template{},
+			"application-template-custombuild":   &templateapi.Template{},
+			"application-template-pullspecbuild": &templateapi.Template{},
 		},
 		"../examples/jenkins": {
 			"jenkins-config":       &kapi.List{},
