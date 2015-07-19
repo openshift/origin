fac88cf9e3c3a41057fe742bdff0fe21c9a92c6a
Clayton Coleman <ccoleman@redhat.com>
2015-07-19 01:15:52 -0400

    Route should default to name, not serviceName

--- plugins/route/allocation/simple/plugin.go
+++ plugins/route/allocation/simple/plugin.go
@@ -11,8 +11,7 @@ import (
 )
 
 // Default DNS suffix to use if no configuration is passed to this plugin.
-// Would be better if we could use "v3.openshift.app", someone bought that!
-const defaultDNSSuffix = "v3.openshift.com"
+const defaultDNSSuffix = "router.default.svc.cluster.local"
 
 // SimpleAllocationPlugin implements the route.AllocationPlugin interface
 // to provide a simple unsharded (or single sharded) allocation plugin.
@@ -40,8 +39,7 @@ func NewSimpleAllocationPlugin(suffix string) (*SimpleAllocationPlugin, error) {
 // the "global" router shard.
 func (p *SimpleAllocationPlugin) Allocate(route *routeapi.Route) (*routeapi.RouterShard, error) {
 
-	glog.V(4).Infof("Allocating global shard *.%s to Route: %s",
-		p.DNSSuffix, route.ServiceName)
+	glog.V(4).Infof("Allocating global shard *.%s to Route: %s", p.DNSSuffix, route.ServiceName)
 
 	return &routeapi.RouterShard{ShardName: "global", DNSSuffix: p.DNSSuffix}, nil
 }
@@ -50,20 +48,23 @@ func (p *SimpleAllocationPlugin) Allocate(route *routeapi.Route) (*routeapi.Rout
 // namespace (if provided) and the router shard dns suffix.
 func (p *SimpleAllocationPlugin) GenerateHostname(route *routeapi.Route, shard *routeapi.RouterShard) string {
 
-	name := route.ServiceName
+	name := route.Name
+	if len(name) == 0 {
+		name = route.ServiceName
+	}
 	if len(name) == 0 {
 		name = uuid.NewUUID().String()
 		glog.V(4).Infof("No service name passed, using generated name: %s", name)
 	}
 
 	s := ""
-	if len(route.Namespace) <= 0 {
+	if len(route.Namespace) == 0 {
 		s = fmt.Sprintf("%s.%s", name, shard.DNSSuffix)
 	} else {
 		s = fmt.Sprintf("%s.%s.%s", name, route.Namespace, shard.DNSSuffix)
 	}
 
-	glog.V(4).Infof("Generated hostname=%s for Route: %s", s, route.ServiceName)
+	glog.V(4).Infof("Generated hostname=%s for Route: %s", s, route.Name)
 
 	return s
 }
