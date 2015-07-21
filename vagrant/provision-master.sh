3ed6ccc48e3ee0e654acde7226e7a9f9cd7ee0ac
Paul Weil <pweil@redhat.com>
2015-07-21 09:23:49 -0400

    do not use vagrant shared dir for volumes

--- vagrant/provision-master.sh
+++ vagrant/provision-master.sh
@@ -42,7 +42,7 @@ popd
 echo "Generating certs"
 pushd /vagrant
   SERVER_CONFIG_DIR="`pwd`/openshift.local.config"
-  VOLUMES_DIR="`pwd`/openshift.local.volumes"
+  VOLUMES_DIR="/openshift.local.volumes"
   MASTER_CONFIG_DIR="${SERVER_CONFIG_DIR}/master"
   CERT_DIR="${MASTER_CONFIG_DIR}"
 
@@ -69,7 +69,7 @@ pushd /vagrant
       --signer-cert="${CERT_DIR}/ca.crt" \
       --signer-key="${CERT_DIR}/ca.key" \
       --signer-serial="${CERT_DIR}/ca.serial.txt" \
-      --volume-dir="${VOLUMES_DIR}_${minion}"
+      --volume-dir="${VOLUMES_DIR}"
   done
 
 popd
