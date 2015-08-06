471f6885d698c5beda1389866cb14cd49eb224a9
Cesar Wong <cewong@redhat.com>
2015-08-06 09:53:48 -0400

    Disable starting builds of a particular type when you don't have access

--- pkg/build/admission/admission_test.go
+++ pkg/build/admission/admission_test.go
@@ -22,7 +22,9 @@ func TestBuildAdmission(t *testing.T) {
 		name             string
 		kind             string
 		resource         string
+		subResource      string
 		object           runtime.Object
+		responseObject   runtime.Object
 		reviewResponse   *authorizationapi.SubjectAccessReviewResponse
 		expectedResource string
 		expectAccept     bool
@@ -37,10 +39,32 @@ func TestBuildAdmission(t *testing.T) {
 			expectAccept:     true,
 		},
 		{
+			name:             "allowed source build clone",
+			object:           testBuildRequest("buildname"),
+			responseObject:   testBuild(buildapi.SourceBuildStrategyType),
+			kind:             "Build",
+			resource:         buildsResource,
+			subResource:      "clone",
+			reviewResponse:   reviewResponse(true, ""),
+			expectedResource: authorizationapi.SourceBuildResource,
+			expectAccept:     true,
+		},
+		{
 			name:             "denied docker build",
 			object:           testBuild(buildapi.DockerBuildStrategyType),
 			kind:             "Build",
 			resource:         buildsResource,
+			reviewResponse:   reviewResponse(false, "cannot create build of type docker build"),
+			expectAccept:     false,
+			expectedResource: authorizationapi.DockerBuildResource,
+		},
+		{
+			name:             "denied docker build clone",
+			object:           testBuildRequest("buildname"),
+			responseObject:   testBuild(buildapi.DockerBuildStrategyType),
+			kind:             "Build",
+			resource:         buildsResource,
+			subResource:      "clone",
 			reviewResponse:   reviewResponse(false, "cannot create build of type docker build"),
 			expectAccept:     false,
 			expectedResource: authorizationapi.DockerBuildResource,
@@ -64,6 +88,17 @@ func TestBuildAdmission(t *testing.T) {
 			expectedResource: authorizationapi.DockerBuildResource,
 		},
 		{
+			name:             "allowed build config instantiate",
+			responseObject:   testBuildConfig(buildapi.DockerBuildStrategyType),
+			object:           testBuildRequest("buildname"),
+			kind:             "BuildConfig",
+			resource:         buildConfigsResource,
+			subResource:      "instantiate",
+			reviewResponse:   reviewResponse(true, ""),
+			expectAccept:     true,
+			expectedResource: authorizationapi.DockerBuildResource,
+		},
+		{
 			name:             "forbidden build config",
 			object:           testBuildConfig(buildapi.CustomBuildStrategyType),
 			kind:             "BuildConfig",
@@ -72,11 +107,30 @@ func TestBuildAdmission(t *testing.T) {
 			expectAccept:     false,
 			expectedResource: authorizationapi.CustomBuildResource,
 		},
+		{
+			name:             "forbidden build config instantiate",
+			responseObject:   testBuildConfig(buildapi.CustomBuildStrategyType),
+			object:           testBuildRequest("buildname"),
+			kind:             "BuildConfig",
+			resource:         buildConfigsResource,
+			subResource:      "instantiate",
+			reviewResponse:   reviewResponse(false, ""),
+			expectAccept:     false,
+			expectedResource: authorizationapi.CustomBuildResource,
+		},
+		{
+			name:           "unrecognized request object",
+			object:         &fakeObject{},
+			kind:           "BuildConfig",
+			resource:       buildConfigsResource,
+			reviewResponse: reviewResponse(true, ""),
+			expectAccept:   false,
+		},
 	}
 
 	for _, test := range tests {
-		c := NewBuildByStrategy(fakeClient(test.expectedResource, test.reviewResponse))
-		attrs := admission.NewAttributesRecord(test.object, test.kind, "default", "name", test.resource, "" /*subresource*/, admission.Create, fakeUser())
+		c := NewBuildByStrategy(fakeClient(test.expectedResource, test.reviewResponse, test.responseObject))
+		attrs := admission.NewAttributesRecord(test.object, test.kind, "default", "name", test.resource, test.subResource, admission.Create, fakeUser())
 		err := c.Admit(attrs)
 		if err != nil && test.expectAccept {
 			t.Errorf("%s: unexpected error: %v", test.name, err)
@@ -87,17 +141,22 @@ func TestBuildAdmission(t *testing.T) {
 	}
 }
 
+type fakeObject struct{}
+
+func (*fakeObject) IsAnAPIObject() {}
+
 func fakeUser() user.Info {
 	return &user.DefaultInfo{
 		Name: "testuser",
 	}
 }
 
-func fakeClient(expectedResource string, reviewResponse *authorizationapi.SubjectAccessReviewResponse) client.Interface {
+func fakeClient(expectedResource string, reviewResponse *authorizationapi.SubjectAccessReviewResponse, obj runtime.Object) client.Interface {
 	emptyResponse := &authorizationapi.SubjectAccessReviewResponse{}
 	return &testclient.Fake{
 		ReactFn: func(action ktestclient.Action) (runtime.Object, error) {
-			if action.Matches("create", "subjectaccessreviews") {
+			switch {
+			case action.Matches("create", "subjectaccessreviews"):
 				review, ok := action.(ktestclient.CreateAction).GetObject().(*authorizationapi.SubjectAccessReview)
 				if !ok {
 					return emptyResponse, fmt.Errorf("unexpected object received: %#v", review)
@@ -107,6 +166,10 @@ func fakeClient(expectedResource string, reviewResponse *authorizationapi.Subjec
 						review.Resource, expectedResource)
 				}
 				return reviewResponse, nil
+			case action.Matches("get", "buildconfigs"):
+				return obj, nil
+			case action.Matches("get", "builds"):
+				return obj, nil
 			}
 			return nil, nil
 		},
@@ -145,5 +208,13 @@ func reviewResponse(allowed bool, msg string) *authorizationapi.SubjectAccessRev
 	return &authorizationapi.SubjectAccessReviewResponse{
 		Allowed: allowed,
 		Reason:  msg,
+	}
+}
+
+func testBuildRequest(name string) runtime.Object {
+	return &buildapi.BuildRequest{
+		ObjectMeta: kapi.ObjectMeta{
+			Name: name,
+		},
 	}
 }
