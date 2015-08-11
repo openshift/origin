904886f946b31dad5d621d890e629da4b32ed09f
deads2k <deads@redhat.com>
2015-08-11 13:56:46 -0400

    UPSTREAM: 8890: Allowing ActiveDeadlineSeconds to be updated for a pod
    Conflicts:
    	Godeps/_workspace/src/k8s.io/kubernetes/pkg/api/validation/validation_test.go

--- Godeps/_workspace/src/k8s.io/kubernetes/pkg/api/validation/validation_test.go
+++ Godeps/_workspace/src/k8s.io/kubernetes/pkg/api/validation/validation_test.go
@@ -1438,6 +1438,9 @@ func TestValidatePodUpdate(t *testing.T) {
 	now := unversioned.Now()
 	grace := int64(30)
 	grace2 := int64(31)
+	activeDeadlineSecondsZero := int64(0)
+	activeDeadlineSecondsNegative := int64(-30)
+	activeDeadlineSecondsPositive := int64(30)
 	tests := []struct {
 		a       api.Pod
 		b       api.Pod
@@ -1523,6 +1526,46 @@ func TestValidatePodUpdate(t *testing.T) {
 			},
 			false,
 			"more containers",
+		},
+		{
+			api.Pod{
+				Spec: api.PodSpec{
+					ActiveDeadlineSeconds: &activeDeadlineSecondsZero,
+				},
+			},
+			api.Pod{},
+			false,
+			"activedeadlineseconds change to 0",
+		},
+		{
+			api.Pod{
+				Spec: api.PodSpec{
+					ActiveDeadlineSeconds: &activeDeadlineSecondsPositive,
+				},
+			},
+			api.Pod{},
+			true,
+			"activedeadlineseconds change to positive",
+		},
+		{
+			api.Pod{
+				Spec: api.PodSpec{
+					ActiveDeadlineSeconds: &activeDeadlineSecondsNegative,
+				},
+			},
+			api.Pod{},
+			false,
+			"activedeadlineseconds change to negative",
+		},
+		{
+			api.Pod{
+				Spec: api.PodSpec{
+					ActiveDeadlineSeconds: &activeDeadlineSecondsPositive,
+				},
+			},
+			api.Pod{},
+			true,
+			"activedeadlineseconds change back to nil",
 		},
 		{
 			api.Pod{
