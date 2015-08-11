539b1b117ce521f06aebb84200a6f6569e523f63
deads2k <deads@redhat.com>
2015-08-11 13:10:40 -0400

    UPSTREAM: <carry>: Disable UIs for Kubernetes and etcd

--- Godeps/_workspace/src/k8s.io/kubernetes/pkg/master/master.go
+++ Godeps/_workspace/src/k8s.io/kubernetes/pkg/master/master.go
@@ -81,7 +81,7 @@ import (
 	"k8s.io/kubernetes/pkg/storage"
 	etcdstorage "k8s.io/kubernetes/pkg/storage/etcd"
 	"k8s.io/kubernetes/pkg/tools"
-	"k8s.io/kubernetes/pkg/ui"
+	//"k8s.io/kubernetes/pkg/ui"
 	"k8s.io/kubernetes/pkg/util"
 	"k8s.io/kubernetes/pkg/util/sets"
 
@@ -711,9 +711,9 @@ func (m *Master) init(c *Config) {
 	if c.EnableLogsSupport {
 		apiserver.InstallLogsSupport(m.muxHelper)
 	}
-	if c.EnableUISupport {
-		ui.InstallSupport(m.muxHelper, m.enableSwaggerSupport)
-	}
+	/*if c.EnableUISupport {
+		ui.InstallSupport(m.mux)
+	}*/
 
 	if c.EnableProfiling {
 		m.mux.HandleFunc("/debug/pprof/", pprof.Index)
