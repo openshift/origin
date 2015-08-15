01c37adc0f21b3878dfce8f2f002618b0aa2ca93
Steve Kuznetsov <skuznets@redhat.com>
2015-08-15 12:52:21 -0400

    changed check logic for new go versions

--- hack/verify-golint.sh
+++ hack/verify-golint.sh
@@ -11,8 +11,8 @@ fi
 
 GO_VERSION=($(go version))
 
-if [[ -z $(echo "${GO_VERSION[2]}" | grep -E 'go1.4') ]]; then
-  echo "Unknown go version '${GO_VERSION}', skipping golint."
+if [[ -z $(echo "${GO_VERSION[2]}" | grep -E 'go1.4?(\.[0-9]+)') ]]; then
+  echo "Unknown go version '${GO_VERSION[2]}', skipping golint."
   exit 0
 fi
 
