9bea8db25e85fbee648022b8a734c6b4e96de9b6
Dan Mace <ironcladlou@gmail.com>
2015-08-19 10:31:31 -0400

    Support prefixed deploymentConfig name
    Support prefixed deploymentConfig name references to allow command
    chaining. Refactor the code to use client interfaces. Update all tests
    to use fake clients.

--- pkg/cmd/cli/cmd/deploy.go
+++ pkg/cmd/cli/cmd/deploy.go
@@ -14,7 +14,7 @@ import (
 	kclient "k8s.io/kubernetes/pkg/client"
 	"k8s.io/kubernetes/pkg/fields"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
-	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/kubectl/resource"
 
 	"github.com/openshift/origin/pkg/client"
 	"github.com/openshift/origin/pkg/cmd/cli/describe"
@@ -25,8 +25,9 @@ import (
 
 type DeployOptions struct {
 	out             io.Writer
-	osClient        *client.Client
-	kubeClient      *kclient.Client
+	osClient        client.Interface
+	kubeClient      kclient.Interface
+	builder         *resource.Builder
 	namespace       string
 	baseCommandName string
 
@@ -123,16 +124,23 @@ func (o *DeployOptions) Complete(f *clientcmd.Factory, args []string, out io.Wri
 		return err
 	}
 
+	mapper, typer := f.Object()
+	o.builder = resource.NewBuilder(mapper, typer, f.ClientMapperForCommand())
+
 	o.out = out
 
 	if len(args) > 0 {
-		o.deploymentConfigName = args[0]
+		name := args[0]
+		if strings.Index(name, "/") == -1 {
+			name = fmt.Sprintf("dc/%s", name)
+		}
+		o.deploymentConfigName = name
 	}
 
 	return nil
 }
 
-func (o DeployOptions) Validate(args []string) error {
+func (o *DeployOptions) Validate(args []string) error {
 	if len(args) == 0 || len(args[0]) == 0 {
 		return errors.New("a DeploymentConfig name is required.")
 	}
@@ -158,60 +166,33 @@ func (o DeployOptions) Validate(args []string) error {
 	return nil
 }
 
-func (o DeployOptions) RunDeploy() error {
-	config, err := o.osClient.DeploymentConfigs(o.namespace).Get(o.deploymentConfigName)
+func (o *DeployOptions) RunDeploy() error {
+	r := o.builder.
+		NamespaceParam(o.namespace).
+		ResourceTypeOrNameArgs(false, o.deploymentConfigName).
+		SingleResourceType().
+		Do()
+	if r.Err() != nil {
+		return r.Err()
+	}
+	resultObj, err := r.Object()
 	if err != nil {
 		return err
 	}
-
-	commandClient := &deployCommandClientImpl{
-		GetDeploymentFn: func(namespace, name string) (*kapi.ReplicationController, error) {
-			return o.kubeClient.ReplicationControllers(namespace).Get(name)
-		},
-		ListDeploymentsForConfigFn: func(namespace, configName string) (*kapi.ReplicationControllerList, error) {
-			list, err := o.kubeClient.ReplicationControllers(namespace).List(deployutil.ConfigSelector(configName))
-			if err != nil {
-				return nil, err
-			}
-			return list, nil
-		},
-
-		UpdateDeploymentConfigFn: func(config *deployapi.DeploymentConfig) (*deployapi.DeploymentConfig, error) {
-			return o.osClient.DeploymentConfigs(config.Namespace).Update(config)
-		},
-		UpdateDeploymentFn: func(deployment *kapi.ReplicationController) (*kapi.ReplicationController, error) {
-			return o.kubeClient.ReplicationControllers(deployment.Namespace).Update(deployment)
-		},
-
-		ListDeployerPodsForFn: func(namespace, deploymentName string) (*kapi.PodList, error) {
-			selector, err := labels.Parse(fmt.Sprintf("%s=%s", deployapi.DeployerPodForDeploymentLabel, deploymentName))
-			if err != nil {
-				return nil, err
-			}
-			return o.kubeClient.Pods(namespace).List(selector, fields.Everything())
-		},
-		DeletePodFn: func(pod *kapi.Pod) error {
-			return o.kubeClient.Pods(pod.Namespace).Delete(pod.Name, nil)
-		},
+	config, ok := resultObj.(*deployapi.DeploymentConfig)
+	if !ok {
+		return fmt.Errorf("%s is not a valid deploymentconfig", o.deploymentConfigName)
 	}
 
 	switch {
 	case o.deployLatest:
-		c := &deployLatestCommand{client: commandClient}
-		err = c.deploy(config, o.out)
+		err = o.deploy(config, o.out)
 	case o.retryDeploy:
-		c := &retryDeploymentCommand{client: commandClient}
-		err = c.retry(config, o.out)
+		err = o.retry(config, o.out)
 	case o.cancelDeploy:
-		c := &cancelDeploymentCommand{client: commandClient}
-		err = c.cancel(config, o.out)
+		err = o.cancel(config, o.out)
 	case o.enableTriggers:
-		t := &triggerEnabler{
-			updateConfig: func(namespace string, config *deployapi.DeploymentConfig) (*deployapi.DeploymentConfig, error) {
-				return o.osClient.DeploymentConfigs(namespace).Update(config)
-			},
-		}
-		err = t.enableTriggers(config, o.out)
+		err = o.reenableTriggers(config, o.out)
 	default:
 		describer := describe.NewLatestDeploymentsDescriber(o.osClient, o.kubeClient, -1)
 		desc, err := describer.Describe(config.Namespace, config.Name)
@@ -224,31 +205,16 @@ func (o DeployOptions) RunDeploy() error {
 	return err
 }
 
-// deployCommandClient abstracts access to the API server.
-type deployCommandClient interface {
-	GetDeployment(namespace, name string) (*kapi.ReplicationController, error)
-	ListDeploymentsForConfig(namespace, configName string) (*kapi.ReplicationControllerList, error)
-	UpdateDeploymentConfig(*deployapi.DeploymentConfig) (*deployapi.DeploymentConfig, error)
-	UpdateDeployment(*kapi.ReplicationController) (*kapi.ReplicationController, error)
-
-	ListDeployerPodsFor(namespace, deploymentName string) (*kapi.PodList, error)
-	DeletePod(pod *kapi.Pod) error
-}
-
-// deployLatestCommand can launch new deployments.
-type deployLatestCommand struct {
-	client deployCommandClient
-}
-
 // deploy launches a new deployment unless there's already a deployment
 // process in progress for config.
-func (c *deployLatestCommand) deploy(config *deployapi.DeploymentConfig, out io.Writer) error {
+func (o *DeployOptions) deploy(config *deployapi.DeploymentConfig, out io.Writer) error {
 	deploymentName := deployutil.LatestDeploymentNameForConfig(config)
-	deployment, err := c.client.GetDeployment(config.Namespace, deploymentName)
+	deployment, err := o.kubeClient.ReplicationControllers(config.Namespace).Get(deploymentName)
 	if err != nil {
-		if !kerrors.IsNotFound(err) {
-			return err
+		if kerrors.IsNotFound(err) {
+			return fmt.Errorf("couldn't find deployment %q", deploymentName)
 		}
+		return err
 	} else {
 		// Reject attempts to start a concurrent deployment.
 		status := deployutil.DeploymentStatusFor(deployment)
@@ -258,27 +224,22 @@ func (c *deployLatestCommand) deploy(config *deployapi.DeploymentConfig, out io.
 	}
 
 	config.LatestVersion++
-	_, err = c.client.UpdateDeploymentConfig(config)
+	_, err = o.osClient.DeploymentConfigs(config.Namespace).Update(config)
 	if err == nil {
 		fmt.Fprintf(out, "Started deployment #%d\n", config.LatestVersion)
 	}
 	return err
 }
 
-// retryDeploymentCommand can retry failed deployments.
-type retryDeploymentCommand struct {
-	client deployCommandClient
-}
-
 // retry resets the status of the latest deployment to New, which will cause
 // the deployment to be retried. An error is returned if the deployment is not
 // currently in a failed state.
-func (c *retryDeploymentCommand) retry(config *deployapi.DeploymentConfig, out io.Writer) error {
+func (o *DeployOptions) retry(config *deployapi.DeploymentConfig, out io.Writer) error {
 	if config.LatestVersion == 0 {
 		return fmt.Errorf("no deployments found for %s/%s", config.Namespace, config.Name)
 	}
 	deploymentName := deployutil.LatestDeploymentNameForConfig(config)
-	deployment, err := c.client.GetDeployment(config.Namespace, deploymentName)
+	deployment, err := o.kubeClient.ReplicationControllers(config.Namespace).Get(deploymentName)
 	if err != nil {
 		if kerrors.IsNotFound(err) {
 			return fmt.Errorf("Unable to find the latest deployment (#%d).\nYou can start a new deployment using the --latest option.", config.LatestVersion)
@@ -298,12 +259,12 @@ func (c *retryDeploymentCommand) retry(config *deployapi.DeploymentConfig, out i
 	}
 
 	// Delete the deployer pod as well as the deployment hooks pods, if any
-	pods, err := c.client.ListDeployerPodsFor(config.Namespace, deploymentName)
+	pods, err := o.kubeClient.Pods(config.Namespace).List(deployutil.DeployerPodSelector(deploymentName), fields.Everything())
 	if err != nil {
 		return fmt.Errorf("Failed to list deployer/hook pods for deployment #%d: %v", config.LatestVersion, err)
 	}
 	for _, pod := range pods.Items {
-		err := c.client.DeletePod(&pod)
+		err := o.kubeClient.Pods(pod.Namespace).Delete(pod.Name, kapi.NewDeleteOptions(0))
 		if err != nil {
 			return fmt.Errorf("Failed to delete deployer/hook pod %s for deployment #%d: %v", pod.Name, config.LatestVersion, err)
 		}
@@ -313,21 +274,16 @@ func (c *retryDeploymentCommand) retry(config *deployapi.DeploymentConfig, out i
 	// clear out the cancellation flag as well as any previous status-reason annotation
 	delete(deployment.Annotations, deployapi.DeploymentStatusReasonAnnotation)
 	delete(deployment.Annotations, deployapi.DeploymentCancelledAnnotation)
-	_, err = c.client.UpdateDeployment(deployment)
+	_, err = o.kubeClient.ReplicationControllers(deployment.Namespace).Update(deployment)
 	if err == nil {
 		fmt.Fprintf(out, "retried #%d\n", config.LatestVersion)
 	}
 	return err
 }
 
-// cancelDeploymentCommand cancels the in-progress deployments.
-type cancelDeploymentCommand struct {
-	client deployCommandClient
-}
-
 // cancel cancels any deployment process in progress for config.
-func (c *cancelDeploymentCommand) cancel(config *deployapi.DeploymentConfig, out io.Writer) error {
-	deployments, err := c.client.ListDeploymentsForConfig(config.Namespace, config.Name)
+func (o *DeployOptions) cancel(config *deployapi.DeploymentConfig, out io.Writer) error {
+	deployments, err := o.kubeClient.ReplicationControllers(config.Namespace).List(deployutil.ConfigSelector(config.Name))
 	if err != nil {
 		return err
 	}
@@ -351,7 +307,7 @@ func (c *cancelDeploymentCommand) cancel(config *deployapi.DeploymentConfig, out
 
 			deployment.Annotations[deployapi.DeploymentCancelledAnnotation] = deployapi.DeploymentCancelledAnnotationValue
 			deployment.Annotations[deployapi.DeploymentStatusReasonAnnotation] = deployapi.DeploymentCancelledByUser
-			_, err := c.client.UpdateDeployment(&deployment)
+			_, err := o.kubeClient.ReplicationControllers(deployment.Namespace).Update(&deployment)
 			if err == nil {
 				fmt.Fprintf(out, "cancelled deployment #%d\n", config.LatestVersion)
 				anyCancelled = true
@@ -370,14 +326,8 @@ func (c *cancelDeploymentCommand) cancel(config *deployapi.DeploymentConfig, out
 	return nil
 }
 
-// triggerEnabler can enable image triggers for a config.
-type triggerEnabler struct {
-	// updateConfig persists config.
-	updateConfig func(namespace string, config *deployapi.DeploymentConfig) (*deployapi.DeploymentConfig, error)
-}
-
-// enableTriggers enables all image triggers and then persists config.
-func (t *triggerEnabler) enableTriggers(config *deployapi.DeploymentConfig, out io.Writer) error {
+// reenableTriggers enables all image triggers and then persists config.
+func (o *DeployOptions) reenableTriggers(config *deployapi.DeploymentConfig, out io.Writer) error {
 	enabled := []string{}
 	for _, trigger := range config.Triggers {
 		if trigger.Type == deployapi.DeploymentTriggerOnImageChange {
@@ -389,45 +339,10 @@ func (t *triggerEnabler) enableTriggers(config *deployapi.DeploymentConfig, out 
 		fmt.Fprintln(out, "no image triggers found to enable")
 		return nil
 	}
-	_, err := t.updateConfig(config.Namespace, config)
+	_, err := o.osClient.DeploymentConfigs(config.Namespace).Update(config)
 	if err != nil {
 		return err
 	}
 	fmt.Fprintf(out, "enabled image triggers: %s\n", strings.Join(enabled, ","))
 	return nil
-}
-
-// deployCommandClientImpl is a pluggable deployCommandClient.
-type deployCommandClientImpl struct {
-	GetDeploymentFn            func(namespace, name string) (*kapi.ReplicationController, error)
-	ListDeploymentsForConfigFn func(namespace, configName string) (*kapi.ReplicationControllerList, error)
-	UpdateDeploymentConfigFn   func(*deployapi.DeploymentConfig) (*deployapi.DeploymentConfig, error)
-	UpdateDeploymentFn         func(*kapi.ReplicationController) (*kapi.ReplicationController, error)
-
-	ListDeployerPodsForFn func(namespace, deploymentName string) (*kapi.PodList, error)
-	DeletePodFn           func(pod *kapi.Pod) error
-}
-
-func (c *deployCommandClientImpl) GetDeployment(namespace, name string) (*kapi.ReplicationController, error) {
-	return c.GetDeploymentFn(namespace, name)
-}
-
-func (c *deployCommandClientImpl) ListDeploymentsForConfig(namespace, configName string) (*kapi.ReplicationControllerList, error) {
-	return c.ListDeploymentsForConfigFn(namespace, configName)
-}
-
-func (c *deployCommandClientImpl) UpdateDeploymentConfig(config *deployapi.DeploymentConfig) (*deployapi.DeploymentConfig, error) {
-	return c.UpdateDeploymentConfigFn(config)
-}
-
-func (c *deployCommandClientImpl) UpdateDeployment(deployment *kapi.ReplicationController) (*kapi.ReplicationController, error) {
-	return c.UpdateDeploymentFn(deployment)
-}
-
-func (c *deployCommandClientImpl) ListDeployerPodsFor(namespace, deploymentName string) (*kapi.PodList, error) {
-	return c.ListDeployerPodsForFn(namespace, deploymentName)
-}
-
-func (c *deployCommandClientImpl) DeletePod(pod *kapi.Pod) error {
-	return c.DeletePodFn(pod)
 }
