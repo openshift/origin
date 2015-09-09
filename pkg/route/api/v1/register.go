f0a625e03647f92455e94dc55c22cc345d23d69a
Clayton Coleman <ccoleman@redhat.com>
2015-09-09 13:24:03 -0400

    Allow field labels to work for routes

--- pkg/route/api/v1/register.go
+++ pkg/route/api/v1/register.go
@@ -1,6 +1,8 @@
 package v1
 
 import (
+	"fmt"
+
 	"k8s.io/kubernetes/pkg/api"
 )
 
@@ -9,6 +11,25 @@ func init() {
 		&Route{},
 		&RouteList{},
 	)
+
+	// Add field conversion funcs.
+	err := api.Scheme.AddFieldLabelConversionFunc("v1", "Route",
+		func(label, value string) (string, string, error) {
+			switch label {
+			case "metadata.name",
+				"spec.host",
+				"spec.path",
+				"spec.to.name":
+				return label, value, nil
+				// This is for backwards compatibility with old v1 clients which send spec.host
+			default:
+				return "", "", fmt.Errorf("field label not supported: %s", label)
+			}
+		})
+	if err != nil {
+		// If one of the conversion functions is malformed, detect it immediately.
+		panic(err)
+	}
 }
 
 func (*Route) IsAnAPIObject()     {}
