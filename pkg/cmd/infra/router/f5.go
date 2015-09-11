openshift/origin

commit f88e8dc5a2635f0158a7ed0435960b5d5812987f

Router can filter on namespace, label, field
Allows router sharding (routers that answer different subsets of
resources). Also, split the f5 router so that it is in a different
command.

pkg/cmd/infra/router/f5.go
@@ -2,15 +2,39 @@ package router
 
 import (
 	"errors"
+	"fmt"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 
+	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+
 	"github.com/openshift/origin/pkg/cmd/util"
+	"github.com/openshift/origin/pkg/cmd/util/clientcmd"
+	"github.com/openshift/origin/pkg/version"
 	f5plugin "github.com/openshift/origin/plugins/router/f5"
 )
 
-// f5RouterConfig is the config necessary to start an F5 router plugin.
-type f5RouterConfig struct {
+const (
+	f5Long = `
+Start an F5 route synchronizer
+
+This command launches a process that will synchronize an F5 to the route configuration of your master.
+You may restrict the set of routes the F5 will expose by using the --labels, --fields, or --namespace
+arguments.`
+)
+
+// F5RouterOptions represent the complete structure needed to start an F5 router
+// sync process.
+type F5RouterOptions struct {
+	Config *clientcmd.Config
+
+	F5Router
+	RouterSelection
+}
+
+// F5Router is the config necessary to start an F5 router plugin.
+type F5Router struct {
 	// Host specifies the hostname or IP address of the F5 BIG-IP host.
 	Host string
 
@@ -40,45 +64,97 @@ type f5RouterConfig struct {
 	Insecure bool
 }
 
-// bindFlagsForTemplateRouterConfig binds flags for template router
-// configuration.
-func bindFlagsForF5RouterConfig(flag *pflag.FlagSet, cfg *routerConfig) {
-	flag.StringVar(&cfg.F5RouterConfig.Host, "f5-host", util.Env("ROUTER_EXTERNAL_HOST_HOSTNAME", ""), "The host of F5 BIG-IP's management interface")
-	flag.StringVar(&cfg.F5RouterConfig.Username, "f5-username", util.Env("ROUTER_EXTERNAL_HOST_USERNAME", ""), "The username for F5 BIG-IP's management utility")
-	flag.StringVar(&cfg.F5RouterConfig.Password, "f5-password", util.Env("ROUTER_EXTERNAL_HOST_PASSWORD", ""), "The password for F5 BIG-IP's management utility")
-	flag.StringVar(&cfg.F5RouterConfig.HttpVserver, "f5-http-vserver", util.Env("ROUTER_EXTERNAL_HOST_HTTP_VSERVER", "ose-vserver"), "The F5 BIG-IP virtual server for HTTP connections")
-	flag.StringVar(&cfg.F5RouterConfig.HttpsVserver, "f5-https-vserver", util.Env("ROUTER_EXTERNAL_HOST_HTTPS_VSERVER", "https-ose-vserver"), "The F5 BIG-IP virtual server for HTTPS connections")
-	flag.StringVar(&cfg.F5RouterConfig.PrivateKey, "f5-private-key", util.Env("ROUTER_EXTERNAL_HOST_PRIVKEY", ""), "The path to the F5 BIG-IP SSH private key file")
-	flag.BoolVar(&cfg.F5RouterConfig.Insecure, "f5-insecure", util.Env("ROUTER_EXTERNAL_HOST_INSECURE", "") == "true", "Skip strict certificate verification")
+// Bind binds F5Router arguments to flags
+func (o *F5Router) Bind(flag *pflag.FlagSet) {
+	flag.StringVar(&o.Host, "f5-host", util.Env("ROUTER_EXTERNAL_HOST_HOSTNAME", ""), "The host of F5 BIG-IP's management interface")
+	flag.StringVar(&o.Username, "f5-username", util.Env("ROUTER_EXTERNAL_HOST_USERNAME", ""), "The username for F5 BIG-IP's management utility")
+	flag.StringVar(&o.Password, "f5-password", util.Env("ROUTER_EXTERNAL_HOST_PASSWORD", ""), "The password for F5 BIG-IP's management utility")
+	flag.StringVar(&o.HttpVserver, "f5-http-vserver", util.Env("ROUTER_EXTERNAL_HOST_HTTP_VSERVER", "ose-vserver"), "The F5 BIG-IP virtual server for HTTP connections")
+	flag.StringVar(&o.HttpsVserver, "f5-https-vserver", util.Env("ROUTER_EXTERNAL_HOST_HTTPS_VSERVER", "https-ose-vserver"), "The F5 BIG-IP virtual server for HTTPS connections")
+	flag.StringVar(&o.PrivateKey, "f5-private-key", util.Env("ROUTER_EXTERNAL_HOST_PRIVKEY", ""), "The path to the F5 BIG-IP SSH private key file")
+	flag.BoolVar(&o.Insecure, "f5-insecure", util.Env("ROUTER_EXTERNAL_HOST_INSECURE", "") == "true", "Skip strict certificate verification")
 }
 
-// makeF5Plugin makes an F5 router plugin.
-func makeF5Plugin(cfg *routerConfig) (*f5plugin.F5Plugin, error) {
-	if cfg.F5RouterConfig.Host == "" {
-		return nil, errors.New("F5 host must be specified")
+// Validate verifies the required F5 flags are present
+func (o *F5Router) Validate() error {
+	if o.Host == "" {
+		return errors.New("F5 host must be specified")
 	}
 
-	if cfg.F5RouterConfig.Username == "" {
-		return nil, errors.New("F5 username must be specified")
+	if o.Username == "" {
+		return errors.New("F5 username must be specified")
 	}
 
-	if cfg.F5RouterConfig.Password == "" {
-		return nil, errors.New("F5 password must be specified")
+	if o.Password == "" {
+		return errors.New("F5 password must be specified")
 	}
 
-	if cfg.F5RouterConfig.HttpVserver == "" &&
-		cfg.F5RouterConfig.HttpsVserver == "" {
-		return nil, errors.New("F5 HTTP and HTTPS vservers cannot both be blank")
+	if len(o.HttpVserver) == 0 && len(o.HttpsVserver) == 0 {
+		return errors.New("F5 HTTP and HTTPS vservers cannot both be blank")
+	}
+	return nil
+}
+
+// NewCommndF5Router provides CLI handler for the F5 router sync plugin.
+func NewCommandF5Router(name string) *cobra.Command {
+	options := &F5RouterOptions{
+		Config: clientcmd.NewConfig(),
+	}
+	options.Config.FromFile = true
+
+	cmd := &cobra.Command{
+		Use:   fmt.Sprintf("%s%s", name, clientcmd.ConfigSyntax),
+		Short: "Start an F5 route synchronizer",
+		Long:  f5Long,
+		Run: func(c *cobra.Command, args []string) {
+			options.RouterSelection.Namespace = cmdutil.GetFlagString(c, "namespace")
+			cmdutil.CheckErr(options.Complete())
+			cmdutil.CheckErr(options.Validate())
+			cmdutil.CheckErr(options.Run())
+		},
 	}
 
-	f5PluginCfg := f5plugin.F5PluginConfig{
-		Host:         cfg.F5RouterConfig.Host,
-		Username:     cfg.F5RouterConfig.Username,
-		Password:     cfg.F5RouterConfig.Password,
-		HttpVserver:  cfg.F5RouterConfig.HttpVserver,
-		HttpsVserver: cfg.F5RouterConfig.HttpsVserver,
-		PrivateKey:   cfg.F5RouterConfig.PrivateKey,
-		Insecure:     cfg.F5RouterConfig.Insecure,
+	cmd.AddCommand(version.NewVersionCommand(name))
+
+	flag := cmd.Flags()
+	options.Config.Bind(flag)
+	options.RouterSelection.Bind(flag)
+
+	return cmd
+}
+
+func (o *F5RouterOptions) Complete() error {
+	return o.RouterSelection.Complete()
+}
+
+func (o *F5RouterOptions) Validate() error {
+	return o.F5Router.Validate()
+}
+
+// Run launches an F5 route sync process using the provided options. It never exits.
+func (o *F5RouterOptions) Run() error {
+	cfg := f5plugin.F5PluginConfig{
+		Host:         o.Host,
+		Username:     o.Username,
+		Password:     o.Password,
+		HttpVserver:  o.HttpVserver,
+		HttpsVserver: o.HttpsVserver,
+		PrivateKey:   o.PrivateKey,
+		Insecure:     o.Insecure,
 	}
-	return f5plugin.NewF5Plugin(f5PluginCfg)
+	plugin, err := f5plugin.NewF5Plugin(cfg)
+	if err != nil {
+		return err
+	}
+
+	oc, kc, err := o.Config.Clients()
+	if err != nil {
+		return err
+	}
+
+	factory := o.RouterSelection.NewFactory(oc, kc)
+	controller := factory.Create(plugin)
+	controller.Run()
+
+	select {}
 }

rel-eng/completions/bash/openshift
@@ -4513,28 +4513,83 @@ _openshift_infra_router()
     flags_with_completion=()
     flags_completion=()
 
+    flags+=("--api-version=")
     flags+=("--certificate-authority=")
     flags+=("--client-certificate=")
     flags+=("--client-key=")
-    flags+=("--f5-host=")
-    flags+=("--f5-http-vserver=")
-    flags+=("--f5-https-vserver=")
-    flags+=("--f5-insecure")
-    flags+=("--f5-password=")
-    flags+=("--f5-private-key=")
-    flags+=("--f5-username=")
+    flags+=("--cluster=")
+    flags+=("--config=")
+    flags+=("--context=")
+    flags+=("--default-certificate=")
+    flags+=("--fields=")
     flags+=("--help")
     flags+=("-h")
     flags+=("--insecure-skip-tls-verify")
     flags+=("--kubernetes=")
+    flags+=("--labels=")
     flags+=("--master=")
+    flags+=("--namespace=")
+    two_word_flags+=("-n")
     flags+=("--reload=")
-    flags+=("--stats-password=")
-    flags+=("--stats-port=")
-    flags+=("--stats-user=")
+    flags+=("--resync-interval=")
+    flags+=("--server=")
     flags+=("--template=")
     flags+=("--token=")
-    flags+=("--type=")
+    flags+=("--user=")
+    flags+=("--working-dir=")
+
+    must_have_one_flag=()
+    must_have_one_noun=()
+}
+
+_openshift_infra_f5-router_version()
+{
+    last_command="openshift_infra_f5-router_version"
+    commands=()
+
+    flags=()
+    two_word_flags=()
+    flags_with_completion=()
+    flags_completion=()
+
+    flags+=("--help")
+    flags+=("-h")
+
+    must_have_one_flag=()
+    must_have_one_noun=()
+}
+
+_openshift_infra_f5-router()
+{
+    last_command="openshift_infra_f5-router"
+    commands=()
+    commands+=("version")
+
+    flags=()
+    two_word_flags=()
+    flags_with_completion=()
+    flags_completion=()
+
+    flags+=("--api-version=")
+    flags+=("--certificate-authority=")
+    flags+=("--client-certificate=")
+    flags+=("--client-key=")
+    flags+=("--cluster=")
+    flags+=("--config=")
+    flags+=("--context=")
+    flags+=("--fields=")
+    flags+=("--help")
+    flags+=("-h")
+    flags+=("--insecure-skip-tls-verify")
+    flags+=("--kubernetes=")
+    flags+=("--labels=")
+    flags+=("--master=")
+    flags+=("--namespace=")
+    two_word_flags+=("-n")
+    flags+=("--resync-interval=")
+    flags+=("--server=")
+    flags+=("--token=")
+    flags+=("--user=")
 
     must_have_one_flag=()
     must_have_one_noun=()
@@ -4659,6 +4714,7 @@ _openshift_infra()
     last_command="openshift_infra"
     commands=()
     commands+=("router")
+    commands+=("f5-router")
     commands+=("deploy")
     commands+=("sti-build")
     commands+=("docker-build")

hack/build-images.sh
@@ -40,7 +40,6 @@ tar xzpf "${OS_IMAGE_RELEASE_TAR}" -C "${imagedir}"
 cp -pf "${imagedir}/openshift" images/origin/bin
 cp -pf "${imagedir}/openshift" images/router/haproxy/bin
 cp -pf "${imagedir}/openshift" images/ipfailover/keepalived/bin
-cp -pf "${imagedir}/openshift" images/router/f5/bin
 
 # Copy image binaries to the appropriate locations.
 cp -pf "${imagedir}/pod" images/pod/bin
@@ -61,7 +60,6 @@ image openshift/origin-pod                   images/pod
 # images that depend on openshift/origin-base
 image openshift/origin                       images/origin
 image openshift/origin-haproxy-router        images/router/haproxy
-image openshift/origin-f5-router             images/router/f5
 image openshift/origin-keepalived-ipfailover images/ipfailover/keepalived
 image openshift/origin-docker-registry       images/dockerregistry
 # images that depend on openshift/origin
@@ -69,6 +67,7 @@ image openshift/origin-deployer              images/deployer
 image openshift/origin-docker-builder        images/builder/docker/docker-builder
 image openshift/origin-gitserver             examples/gitserver
 image openshift/origin-sti-builder           images/builder/docker/sti-builder
+image openshift/origin-f5-router             images/router/f5
 # unpublished images
 image openshift/origin-custom-docker-builder images/builder/docker/custom-docker-builder
 

images/origin/Dockerfile
@@ -17,7 +17,7 @@ RUN ln -s /usr/bin/openshift /usr/bin/oc && \
     ln -s /usr/bin/openshift /usr/bin/openshift-deploy && \
     ln -s /usr/bin/openshift /usr/bin/openshift-docker-build && \
     ln -s /usr/bin/openshift /usr/bin/openshift-sti-build && \
-    ln -s /usr/bin/openshift /usr/bin/openshift-router && \
+    ln -s /usr/bin/openshift /usr/bin/openshift-f5-router && \
     setcap 'cap_net_bind_service=ep' /usr/bin/openshift
 
 ADD scripts/recycler.sh /usr/share/openshift/scripts/volumes/recycler.sh

images/router/f5/Dockerfile
@@ -1,11 +1,8 @@
 #
-# This is the HAProxy router for OpenShift Origin.
+# This is the F5 router for OpenShift Origin.
 #
 # The standard name for this image is openshift/origin-f5-router
 #
-FROM openshift/origin-base
+FROM openshift/origin
 
-ADD bin/openshift /usr/bin/openshift
-RUN ln -s /usr/bin/openshift /usr/bin/openshift-router
-
-ENTRYPOINT ["/usr/bin/openshift-router", "--type=f5"]
+ENTRYPOINT ["/usr/bin/openshift-f5-router"]

images/router/haproxy/Dockerfile
@@ -21,4 +21,6 @@ RUN ln -s /usr/bin/openshift /usr/bin/openshift-router && \
 WORKDIR /var/lib/haproxy/conf
 
 EXPOSE 80
-ENTRYPOINT ["/usr/bin/openshift-router", "--type=template", "--template=/var/lib/haproxy/conf/haproxy-config.template", "--reload=/var/lib/haproxy/reload-haproxy"]
+ENV TEMPLATE_FILE=/var/lib/haproxy/conf/haproxy-config.template \
+    RELOAD_SCRIPT=/var/lib/haproxy/reload-haproxy
+ENTRYPOINT ["/usr/bin/openshift-router"]

images/router/haproxy/conf/haproxy-config.template
@@ -4,6 +4,7 @@
     					backend for a route and contains all the endpoints for the service
 */}}
 {{ define "/var/lib/haproxy/conf/haproxy.config" }}
+{{ $workingDir := .WorkingDir }}
 global
   # maxconn 4096
   daemon
@@ -81,7 +82,7 @@ backend be_sni
 
 frontend fe_sni
   # terminate ssl on edge
-  bind 127.0.0.1:10444 ssl {{ if (len .DefaultCertificate) gt 0 }}crt {{.DefaultCertificate}}{{ else }}crt /var/lib/haproxy/conf/default_pub_keys.pem{{ end }} crt /var/lib/containers/router/certs accept-proxy
+  bind 127.0.0.1:10444 ssl {{ if (len .DefaultCertificate) gt 0 }}crt {{.DefaultCertificate}}{{ else }}crt /var/lib/haproxy/conf/default_pub_keys.pem{{ end }} crt {{ $workingDir }}/certs accept-proxy
   mode http
 
   # check re-encrypt backends first - from most specific to general path.
@@ -194,7 +195,7 @@ backend be_secure_{{$cfgIdx}}
   timeout check 5000ms
   cookie OPENSHIFT_REENCRYPT_{{$cfgIdx}}_SERVERID insert indirect nocache httponly secure
                 {{ range $idx, $endpoint := $serviceUnit.EndpointTable }}
-  server {{$endpoint.ID}} {{$endpoint.IP}}:{{$endpoint.Port}} ssl check inter 5000ms verify required ca-file /var/lib/containers/router/cacerts/{{$cfgIdx}}.pem cookie {{$endpoint.ID}}
+  server {{$endpoint.ID}} {{$endpoint.IP}}:{{$endpoint.Port}} ssl check inter 5000ms verify required ca-file {{ $workingDir }}/cacerts/{{$cfgIdx}}.pem cookie {{$endpoint.ID}}
                 {{ end }}
             {{ end  }}
         {{ end  }}{{/* $serviceUnit.ServiceAliasConfigs*/}}

pkg/cmd/infra/router/router.go
@@ -2,135 +2,74 @@ package router
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/golang/glog"
-	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
+	kclient "k8s.io/kubernetes/pkg/client"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
+
+	oclient "github.com/openshift/origin/pkg/client"
 	"github.com/openshift/origin/pkg/cmd/util"
-	"github.com/openshift/origin/pkg/cmd/util/clientcmd"
-	"github.com/openshift/origin/pkg/router"
 	controllerfactory "github.com/openshift/origin/pkg/router/controller/factory"
-	"github.com/openshift/origin/pkg/util/proc"
-	"github.com/openshift/origin/pkg/version"
 )
 
-const (
-	routerLong = `
-Start a router
+// RouterSelection controls what routes and resources on the server are considered
+// part of this router.
+type RouterSelection struct {
+	LabelSelector string
+	Labels        labels.Selector
+	FieldSelector string
+	Fields        fields.Selector
 
-This command launches a router connected to your cluster master. The router listens for routes and endpoints
-created by users and keeps a local router configuration up to date with those changes.`
-)
+	Namespace string
 
-// routerConfig represents a container for all other router config types.
-// Any global config belongs on this level.
-type routerConfig struct {
-	// Config holds global flags that are passed to all client commands that need
-	// to connect to master.
-	Config *clientcmd.Config
-
-	// DefaultCertificate holds the certificate that will be used if no more
-	// specific certificate is found.  This is typically a wildcard certificate.
-	DefaultCertificate string
-
-	// TemplateRouterConfig holds config items for template router plugins.
-	TemplateRouterConfig *templateRouterConfig
-
-	// F5RouterConfig holds the configuration information for the F5
-	// router plugin.
-	F5RouterConfig *f5RouterConfig
+	ResyncInterval time.Duration
 }
 
-const (
-	// routerTypeF5 is the type value for an f5 router plugin.
-	routerTypeF5 = "f5"
-	// routerTypeTemplate is the type value for a template router plugin.
-	routerTypeTemplate = "template"
-)
-
-// NewCommandRouter provides a CLI handler for the router.
-func NewCommandRouter(name string) *cobra.Command {
-	cfg := &routerConfig{
-		Config:               clientcmd.NewConfig(),
-		TemplateRouterConfig: &templateRouterConfig{},
-		F5RouterConfig:       &f5RouterConfig{},
-	}
-
-	cfgType := util.Env("ROUTER_TYPE", "")
-
-	cmd := &cobra.Command{
-		Use:   fmt.Sprintf("%s%s", name, clientcmd.ConfigSyntax),
-		Short: "Start a router",
-		Long:  routerLong,
-		Run: func(c *cobra.Command, args []string) {
-			if !isValidType(cfgType) {
-				glog.Fatalf("Invalid type specified: %s.  Valid values are %s, %s",
-					cfgType, routerTypeF5, routerTypeTemplate)
-			}
-
-			if cfgType == routerTypeF5 {
-				cfg.TemplateRouterConfig = nil
-			} else if cfgType == routerTypeTemplate {
-				cfg.F5RouterConfig = nil
-			}
-
-			defaultCert := util.Env("DEFAULT_CERTIFICATE", "")
-			if len(defaultCert) > 0 {
-				cfg.DefaultCertificate = defaultCert
-			}
-
-			var plugin router.Plugin
-			var err error
-
-			if cfg.F5RouterConfig != nil {
-				plugin, err = makeF5Plugin(cfg)
-			} else if cfg.TemplateRouterConfig != nil {
-				plugin, err = makeTemplatePlugin(cfg)
-			} else {
-				err = fmt.Errorf("Plugin type not recognized")
-			}
-			if err != nil {
-				glog.Fatal(err)
-			}
-
-			if err := start(cfg.Config, plugin); err != nil {
-				glog.Fatal(err)
-			}
-		},
-	}
-
-	cmd.AddCommand(version.NewVersionCommand(name))
-
-	flag := cmd.Flags()
-
-	// Binds for generic config
-	cfg.Config.Bind(flag)
-
-	flag.StringVar(&cfgType, "type", util.Env("ROUTER_TYPE", ""), "The type of router to create.  Valid values: template, f5")
-
-	bindFlagsForTemplateRouterConfig(flag, cfg)
-	bindFlagsForF5RouterConfig(flag, cfg)
-
-	return cmd
+// Bind sets the appropriate labels
+func (o *RouterSelection) Bind(flag *pflag.FlagSet) {
+	flag.StringVar(&o.LabelSelector, "labels", util.Env("ROUTE_LABELS", ""), "A label selector to apply to the routes to watch")
+	flag.StringVar(&o.FieldSelector, "fields", util.Env("ROUTE_FIELDS", ""), "A field selector to apply to routes to watch")
+	flag.DurationVar(&o.ResyncInterval, "resync-interval", 10*time.Minute, "The interval at which the route list should be fully refreshed")
 }
 
-// isValidType checks that the router type specified is a known type.
-func isValidType(t string) bool {
-	return t == routerTypeF5 || t == routerTypeTemplate
-}
-
-// start launches the router.
-func start(cfg *clientcmd.Config, plugin router.Plugin) error {
-	osClient, kubeClient, err := cfg.Clients()
-	if err != nil {
-		return err
+// Complete converts string representations of field and label selectors to their parsed equivalent, or
+// returns an error.
+func (o *RouterSelection) Complete() error {
+	if len(o.LabelSelector) > 0 {
+		s, err := labels.Parse(o.LabelSelector)
+		if err != nil {
+			return fmt.Errorf("label selector is not valid: %v", err)
+		}
+		o.Labels = s
+	} else {
+		o.Labels = labels.Everything()
 	}
 
-	proc.StartReaper()
+	if len(o.FieldSelector) > 0 {
+		s, err := fields.ParseSelector(o.FieldSelector)
+		if err != nil {
+			return fmt.Errorf("field selector is not valid: %v", err)
+		}
+		o.Fields = s
+	} else {
+		o.Fields = fields.Everything()
+	}
+	return nil
+}
 
-	factory := controllerfactory.RouterControllerFactory{KClient: kubeClient, OSClient: osClient}
-	controller := factory.Create(plugin)
-	controller.Run()
-
-	select {}
+// NewFactory initializes a factory that will watch the requested routes
+func (o *RouterSelection) NewFactory(oc oclient.Interface, kc kclient.Interface) *controllerfactory.RouterControllerFactory {
+	factory := controllerfactory.NewDefaultRouterControllerFactory(oc, kc)
+	factory.Labels = o.Labels
+	factory.Fields = o.Fields
+	factory.Namespace = o.Namespace
+	factory.ResyncInterval = o.ResyncInterval
+	if len(factory.Namespace) > 0 {
+		glog.Infof("Router is only using resources in namespace %s", factory.Namespace)
+	}
+	return factory
 }

commit a064a461c6a9062b236de1eb2f4fbaa217ef65fc

Fix oadm router F5 flags
NewCommandF5Router: Call F5Router.Bind so that the openshift-f5-router
command's -f5-* flags work.

Fix a typo in the comment for NewCommandF5Router.

This commit fixes a defect introduced in commit
f88e8dc5a2635f0158a7ed0435960b5d5812987f.

pkg/cmd/infra/router/f5.go
@@ -99,7 +99,7 @@ func (o *F5Router) Validate() error {
 	return nil
 }
 
-// NewCommndF5Router provides CLI handler for the F5 router sync plugin.
+// NewCommandF5Router provides CLI handler for the F5 router sync plugin.
 func NewCommandF5Router(name string) *cobra.Command {
 	options := &F5RouterOptions{
 		Config: clientcmd.NewConfig(),
@@ -122,6 +122,7 @@ func NewCommandF5Router(name string) *cobra.Command {
 
 	flag := cmd.Flags()
 	options.Config.Bind(flag)
+	options.F5Router.Bind(flag)
 	options.RouterSelection.Bind(flag)
 
 	return cmd

rel-eng/completions/bash/openshift
@@ -4630,6 +4630,13 @@ _openshift_infra_f5-router()
     flags+=("--cluster=")
     flags+=("--config=")
     flags+=("--context=")
+    flags+=("--f5-host=")
+    flags+=("--f5-http-vserver=")
+    flags+=("--f5-https-vserver=")
+    flags+=("--f5-insecure")
+    flags+=("--f5-password=")
+    flags+=("--f5-private-key=")
+    flags+=("--f5-username=")
     flags+=("--fields=")
     flags+=("--help")
     flags+=("-h")