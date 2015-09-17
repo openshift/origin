7fa7f3b50227861f224f57e0453ed771e1baeea7
Clayton Coleman <ccoleman@redhat.com>
2015-09-17 09:04:04 -0400

    Build from a Dockerfile directly
    Support a new --dockerfile option on oc new-build that performs a
    docker build based on the contents of a dockerfile, and support
    Dockerfile as a source type on builds. Fix lots of NPEs in the build
    code where .Git != nil is assumed. If you specify Git and Dockerfile
    during a Docker build, the Dockerfile will overwrite whatever is in
    the git repo in the context dir.
    
    Update oc get builds to display slightly more info

--- pkg/build/api/v1/types.go
+++ pkg/build/api/v1/types.go
@@ -108,12 +108,20 @@ type BuildSourceType string
 const (
 	//BuildSourceGit is a Git SCM
 	BuildSourceGit BuildSourceType = "Git"
+	// bulidSourceDockerfile is an embedded dockerfile
+	BuildSourceDockerfile BuildSourceType = "Dockerfile"
 )
 
 // BuildSource is the SCM used for the build
 type BuildSource struct {
 	// Type of source control management system
 	Type BuildSourceType `json:"type" description:"type of source control management system"`
+
+	// Dockerfile is the raw contents of a Dockerfile which should be built. When this option is
+	// specified, the From and Env on the Docker build strategy are applied on top of this file.
+	// The Dockerfile source type may be used with other options like .git - in those cases the
+	// Git repo will have any innate Dockerfile replaced in the context dir.
+	Dockerfile *string `json:"dockerfile,omitempty" description:"the contents of a Dockerfile to build; FROM and ENV may be overridden if you have specified 'from' and 'env' on the build strategy"`
 
 	// Git contains optional information about git build source
 	Git *GitBuildSource `json:"git,omitempty" description:"optional information about git build source"`
