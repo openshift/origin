5ef7b053a19da57f91a85cf0bc191971020524d8
Clayton Coleman <ccoleman@redhat.com>
2015-09-18 19:52:40 -0400

    Only run k8s upstream tests that are passing
    Tests are grouped by triage status. Added TEST_ONLY flag to simplify
    repeated execution. Support default -skip and override with SKIP=' '

--- test/extended/util/test.go
+++ test/extended/util/test.go
@@ -33,7 +33,11 @@ func InitTest() {
 	extendedOutputDir := filepath.Join(os.TempDir(), "openshift-extended-tests")
 	os.MkdirAll(extendedOutputDir, 0777)
 
-	flag.StringVar(&TestContext.KubeConfig, clientcmd.RecommendedConfigPathFlag, KubeConfigPath(), "Path to kubeconfig containing embedded authinfo.")
+	TestContext.RepoRoot = os.Getenv("KUBE_REPO_ROOT")
+	TestContext.KubectlPath = "oc"
+	os.Setenv("KUBECONFIG", KubeConfigPath())
+
+	//flag.StringVar(&TestContext.KubeConfig, clientcmd.RecommendedConfigPathFlag, KubeConfigPath(), "Path to kubeconfig containing embedded authinfo.")
 	flag.StringVar(&TestContext.OutputDir, "extended-tests-output-dir", extendedOutputDir, "Output directory for interesting/useful test data, like performance data, benchmarks, and other metrics.")
 
 	// Override the default Kubernetes E2E configuration
