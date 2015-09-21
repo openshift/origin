openshift/origin

commit f025ff9c06bb7a7a6157cea78a80512be12831cd

Add ServiceNetwork field to ClusterNetwork struct

pkg/cmd/server/start/network_args.go
@@ -13,6 +13,9 @@ type NetworkArgs struct {
 	ClusterNetworkCIDR string
 	// HostSubnetLength is the length of subnet each host is given from the network-cidr.
 	HostSubnetLength uint
+	// ServiceNetworkCIDR is the CIDR string representing the network that service IP
+	// addresses will be allocated from
+	ServiceNetworkCIDR string
 }
 
 // BindNetworkArgs binds values to the given arguments by using flags
@@ -20,6 +23,7 @@ func BindNetworkArgs(args *NetworkArgs, flags *pflag.FlagSet, prefix string) {
 	flags.StringVar(&args.NetworkPluginName, prefix+"network-plugin", args.NetworkPluginName, "The name of the networking plugin to be used for networking.")
 	flags.StringVar(&args.ClusterNetworkCIDR, prefix+"network-cidr", args.ClusterNetworkCIDR, "The CIDR string representing the network that all containers should belong to.")
 	flags.UintVar(&args.HostSubnetLength, prefix+"host-subnet-length", args.HostSubnetLength, "The length of subnet each host is given from the network-cidr.")
+	flags.StringVar(&args.ServiceNetworkCIDR, prefix+"portal-net", args.ServiceNetworkCIDR, "The CIDR string representing the network that portal IPs will be assigned from. This must not overlap with any IP ranges assigned to nodes for pods.")
 }
 
 // NewDefaultNetworkArgs returns a new set of network arguments
@@ -28,6 +32,7 @@ func NewDefaultNetworkArgs() *NetworkArgs {
 		NetworkPluginName:  "",
 		ClusterNetworkCIDR: "10.1.0.0/16",
 		HostSubnetLength:   8,
+		ServiceNetworkCIDR: "172.30.0.0/16",
 	}
 
 	return config

rel-eng/completions/bash/openshift
@@ -576,7 +576,6 @@ _openshift_start()
     flags_completion+=("__handle_filename_extension_flag yaml|yml")
     flags+=("--nodes=")
     flags+=("--pause")
-    flags+=("--portal-net=")
     flags+=("--print-ip")
     flags+=("--public-master=")
     flags+=("--volume-dir=")

pkg/api/deep_copy_generated.go
@@ -2199,6 +2199,7 @@ func deepCopy_api_ClusterNetwork(in sdnapi.ClusterNetwork, out *sdnapi.ClusterNe
 	}
 	out.Network = in.Network
 	out.HostSubnetLength = in.HostSubnetLength
+	out.ServiceNetwork = in.ServiceNetwork
 	return nil
 }
 

pkg/api/v1/conversion_generated.go
@@ -2175,6 +2175,7 @@ func convert_api_ClusterNetwork_To_v1_ClusterNetwork(in *sdnapi.ClusterNetwork, 
 	}
 	out.Network = in.Network
 	out.HostSubnetLength = in.HostSubnetLength
+	out.ServiceNetwork = in.ServiceNetwork
 	return nil
 }
 
@@ -2290,6 +2291,7 @@ func convert_v1_ClusterNetwork_To_api_ClusterNetwork(in *sdnapiv1.ClusterNetwork
 	}
 	out.Network = in.Network
 	out.HostSubnetLength = in.HostSubnetLength
+	out.ServiceNetwork = in.ServiceNetwork
 	return nil
 }
 

pkg/api/v1/deep_copy_generated.go
@@ -2120,6 +2120,7 @@ func deepCopy_v1_ClusterNetwork(in sdnapiv1.ClusterNetwork, out *sdnapiv1.Cluste
 	}
 	out.Network = in.Network
 	out.HostSubnetLength = in.HostSubnetLength
+	out.ServiceNetwork = in.ServiceNetwork
 	return nil
 }
 

pkg/api/v1beta3/conversion_generated.go
@@ -2111,6 +2111,7 @@ func convert_api_ClusterNetwork_To_v1beta3_ClusterNetwork(in *sdnapi.ClusterNetw
 	}
 	out.Network = in.Network
 	out.HostSubnetLength = in.HostSubnetLength
+	out.ServiceNetwork = in.ServiceNetwork
 	return nil
 }
 
@@ -2226,6 +2227,7 @@ func convert_v1beta3_ClusterNetwork_To_api_ClusterNetwork(in *sdnapiv1beta3.Clus
 	}
 	out.Network = in.Network
 	out.HostSubnetLength = in.HostSubnetLength
+	out.ServiceNetwork = in.ServiceNetwork
 	return nil
 }
 

pkg/api/v1beta3/deep_copy_generated.go
@@ -2110,6 +2110,7 @@ func deepCopy_v1beta3_ClusterNetwork(in sdnapiv1beta3.ClusterNetwork, out *sdnap
 	}
 	out.Network = in.Network
 	out.HostSubnetLength = in.HostSubnetLength
+	out.ServiceNetwork = in.ServiceNetwork
 	return nil
 }
 

pkg/cmd/cli/describe/printer.go
@@ -57,7 +57,7 @@ var (
 
 	hostSubnetColumns     = []string{"NAME", "HOST", "HOST IP", "SUBNET"}
 	netNamespaceColumns   = []string{"NAME", "NETID"}
-	clusterNetworkColumns = []string{"NAME", "NETWORK", "HOST SUBNET LENGTH"}
+	clusterNetworkColumns = []string{"NAME", "NETWORK", "HOST SUBNET LENGTH", "SERVICE NETWORK"}
 )
 
 // NewHumanReadablePrinter returns a new HumanReadablePrinter
@@ -660,7 +660,7 @@ func printNetNamespaceList(list *sdnapi.NetNamespaceList, w io.Writer, withNames
 }
 
 func printClusterNetwork(n *sdnapi.ClusterNetwork, w io.Writer, withNamespace, wide bool, columnLabels []string) error {
-	_, err := fmt.Fprintf(w, "%s\t%s\t%d\n", n.Name, n.Network, n.HostSubnetLength)
+	_, err := fmt.Fprintf(w, "%s\t%s\t%d\t%s\n", n.Name, n.Network, n.HostSubnetLength, n.ServiceNetwork)
 	return err
 }
 

commit e5f325a75fffa8fc85cb13bdaf2b32b547743912

Add --portal-net back to all-in-one args
f025ff9 accidentally dropped --portal-net from the all-in-one args.
Fix that.

pkg/cmd/server/start/network_args.go
@@ -23,7 +23,7 @@ func BindNetworkArgs(args *NetworkArgs, flags *pflag.FlagSet, prefix string) {
 	flags.StringVar(&args.NetworkPluginName, prefix+"network-plugin", args.NetworkPluginName, "The name of the networking plugin to be used for networking.")
 	flags.StringVar(&args.ClusterNetworkCIDR, prefix+"network-cidr", args.ClusterNetworkCIDR, "The CIDR string representing the network that all containers should belong to.")
 	flags.UintVar(&args.HostSubnetLength, prefix+"host-subnet-length", args.HostSubnetLength, "The length of subnet each host is given from the network-cidr.")
-	flags.StringVar(&args.ServiceNetworkCIDR, prefix+"portal-net", args.ServiceNetworkCIDR, "The CIDR string representing the network that portal IPs will be assigned from. This must not overlap with any IP ranges assigned to nodes for pods.")
+	flags.StringVar(&args.ServiceNetworkCIDR, prefix+"portal-net", args.ServiceNetworkCIDR, "The CIDR string representing the network that portal/service IPs will be assigned from. This must not overlap with any IP ranges assigned to nodes for pods.")
 }
 
 // NewDefaultNetworkArgs returns a new set of network arguments

pkg/cmd/server/start/start_allinone.go
@@ -29,9 +29,10 @@ type AllInOneOptions struct {
 
 	NodeArgs *NodeArgs
 
-	ConfigDir      util.StringFlag
-	NodeConfigFile string
-	PrintIP        bool
+	ConfigDir          util.StringFlag
+	NodeConfigFile     string
+	PrintIP            bool
+	ServiceNetworkCIDR string
 }
 
 const allInOneLong = `
@@ -99,6 +100,7 @@ func NewCommandStartAllInOne(basename string, out io.Writer) (*cobra.Command, *A
 	flags.StringVar(&options.NodeConfigFile, "node-config", "", "Location of the node configuration file to run from. When running from configuration files, all other command-line arguments are ignored.")
 	flags.BoolVar(&options.MasterOptions.CreateCertificates, "create-certs", true, "Indicates whether missing certs should be created.")
 	flags.BoolVar(&options.PrintIP, "print-ip", false, "Print the IP that would be used if no master IP is specified and exit.")
+	flags.StringVar(&options.ServiceNetworkCIDR, "portal-net", NewDefaultNetworkArgs().ServiceNetworkCIDR, "The CIDR string representing the network that portal/service IPs will be assigned from. This must not overlap with any IP ranges assigned to nodes for pods.")
 
 	masterArgs, nodeArgs, listenArg, imageFormatArgs, _ := GetAllInOneArgs()
 	options.MasterOptions.MasterArgs, options.NodeArgs = masterArgs, nodeArgs
@@ -202,6 +204,7 @@ func (o *AllInOneOptions) Complete() error {
 	o.MasterOptions.MasterArgs.NodeList = nodeList.List()
 
 	o.MasterOptions.MasterArgs.NetworkArgs.NetworkPluginName = o.NodeArgs.NetworkPluginName
+	o.MasterOptions.MasterArgs.NetworkArgs.ServiceNetworkCIDR = o.ServiceNetworkCIDR
 
 	masterAddr, err := o.MasterOptions.MasterArgs.GetMasterAddress()
 	if err != nil {

rel-eng/completions/bash/openshift
@@ -567,6 +567,7 @@ _openshift_start()
     flags_completion+=("__handle_filename_extension_flag yaml|yml")
     flags+=("--nodes=")
     flags+=("--pause")
+    flags+=("--portal-net=")
     flags+=("--print-ip")
     flags+=("--public-master=")
     flags+=("--volume-dir=")

test/util/server.go
@@ -213,6 +213,7 @@ func DefaultAllInOneOptions() (*configapi.MasterConfig, *configapi.NodeConfig, e
 	startOptions.MasterOptions.MasterArgs, startOptions.NodeArgs, _, _, _ = setupStartOptions()
 	startOptions.MasterOptions.MasterArgs.NodeList = nil
 	startOptions.NodeArgs.AllowDisabledDocker = true
+	startOptions.ServiceNetworkCIDR = start.NewDefaultNetworkArgs().ServiceNetworkCIDR
 	startOptions.Complete()
 	startOptions.MasterOptions.MasterArgs.ConfigDir.Default(path.Join(GetBaseDir(), "openshift.local.config", "master"))
 	startOptions.NodeArgs.ConfigDir.Default(path.Join(GetBaseDir(), "openshift.local.config", admin.DefaultNodeDir(startOptions.NodeArgs.NodeName)))