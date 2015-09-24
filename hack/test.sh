5390c56c9e22ad025a66fd82b7fccc5df49d71b9
Ravi Sankar Penta <rpenta@redhat.com>
2015-09-24 16:33:50 -0700

    Fix hack/test.sh

--- hack/test.sh
+++ hack/test.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+OSDN_ROOT=$(readlink -f -- $(dirname "${BASH_SOURCE}")/..)
+
+TEST_BASE_DIR=_output/src/github.com/openshift
+# symlink $TEST_BASE_DIR/openshift-sdn to source dir
+if [ ! -L "$TEST_BASE_DIR/openshift-sdn" ]; then
+  mkdir -p $TEST_BASE_DIR || true
+  pushd $TEST_BASE_DIR
+  ln -s ../../../../ openshift-sdn || true
+  popd
+fi
+
+GOPATH=${OSDN_ROOT}/_output:${OSDN_ROOT}/Godeps/_workspace
+export GOPATH
+
 go test -v github.com/openshift/openshift-sdn/pkg/netutils
 go test -v github.com/openshift/openshift-sdn/pkg/netutils/server
