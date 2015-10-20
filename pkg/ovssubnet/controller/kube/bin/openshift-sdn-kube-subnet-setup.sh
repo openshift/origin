a9d3674ab2f1529ee53991221ed53ecce2017956
Scott Dodson <sdodson@redhat.com>
2015-10-20 17:05:03 -0400

    Isolate restarting docker
    This isolates reconfiguring and restarting docker from the test of the setup
    tasks so that we restart docker only when necessary. Without this change we
    restart docker on un-related changes to ovs which do not require docker to be
    restarted.

--- pkg/ovssubnet/controller/kube/bin/openshift-sdn-kube-subnet-setup.sh
+++ pkg/ovssubnet/controller/kube/bin/openshift-sdn-kube-subnet-setup.sh
@@ -41,10 +41,12 @@ function docker_network_config() {
 
 DOCKER_NETWORK_OPTIONS='${DOCKER_NETWORK_OPTIONS}'
 EOF
-
-		systemctl daemon-reload
-		systemctl restart docker.service
-
+		## linux bridge
+		ip link set lbr0 down || true
+		brctl delbr lbr0 || true
+		brctl addbr lbr0
+		ip addr add ${local_subnet_gateway}/${local_subnet_mask_len} dev lbr0
+		ip link set lbr0 up
 
 	    if [ ! -f /.dockerinit ]; then
 		# disable iptables for lbr0
@@ -56,6 +58,11 @@ EOF
 		modprobe br_netfilter || true
 		sysctl -w net.bridge.bridge-nf-call-iptables=0
 	    fi
+		# when using --pid=host to run docker container, systemctl inside it refuses
+		# to work because it detects that it's running in chroot. using dbus instead
+		# of systemctl is just a workaround
+		dbus-send --system --print-reply --reply-timeout=2000 --type=method_call --dest=org.freedesktop.systemd1 /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.Reload
+		dbus-send --system --print-reply --reply-timeout=2000 --type=method_call --dest=org.freedesktop.systemd1 /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.RestartUnit string:'docker.service' string:'replace'
 	    ;;
     esac
 }
@@ -63,9 +70,6 @@ EOF
 function setup_required() {
     ip=$(echo `ip a s lbr0 2>/dev/null|awk '/inet / {print $2}'`)
     if [ "$ip" != "${local_subnet_gateway}/${local_subnet_mask_len}" ]; then
-        return 0
-    fi
-    if ! docker_network_config check; then
         return 0
     fi
     if ! ovs-ofctl -O OpenFlow13 dump-flows br0 | grep -q 'table=0.*arp'; then
@@ -115,25 +119,15 @@ function setup() {
     ip link set vovsbr up
     ip link set vlinuxbr txqueuelen 0
     ip link set vovsbr txqueuelen 0
+    brctl addif lbr0 vlinuxbr
 
     ovs-vsctl del-port br0 vovsbr || true
     ovs-vsctl add-port br0 vovsbr -- set Interface vovsbr ofport_request=9
-
-    ## linux bridge
-    ip link set lbr0 down || true
-    brctl delbr lbr0 || true
-    brctl addbr lbr0
-    ip addr add ${local_subnet_gateway}/${local_subnet_mask_len} dev lbr0
-    ip link set lbr0 up
-    brctl addif lbr0 vlinuxbr
 
     # setup tun address
     ip addr add ${local_subnet_gateway}/${local_subnet_mask_len} dev ${TUN}
     ip link set ${TUN} up
     ip route add ${cluster_network_cidr} dev ${TUN} proto kernel scope link
-
-    ## docker
-    docker_network_config update
 
     # Cleanup docker0 since docker won't do it
     ip link set docker0 down || true
@@ -152,6 +146,10 @@ function setup() {
 }
 
 set +e
+if ! docker_network_config check; then
+  lockwrap docker_network_config update
+fi
+
 if ! setup_required; then
     echo "SDN setup not required."
     exit 140
