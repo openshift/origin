8534338b04d7dbb0a3c2c68a42e4e9e392ba58f1
Samuel Padgett <spadgett@redhat.com>
2015-10-21 11:10:22 -0400

    Update pod status chart styles
    - Move pods donut chart to left of pod template.
    - Increase the size of the donut chart.
    - Remove pod template component label.
    - Set min-width on the pod flex column to avoid overlapping controls on Safari.
    - Avoid flicker and animate the chart when data is updated by calling
      chart.load().

--- assets/app/scripts/directives/podStatusChart.js
+++ assets/app/scripts/directives/podStatusChart.js
@@ -14,56 +14,15 @@ angular.module('openshiftConsole')
       },
       templateUrl: 'views/_pod-status-chart.html',
       link: function($scope, element) {
+        var chart, config;
+
         // The phases to show (in order).
         var phases = ["Running", "Warning", "Failed", "Pending", "Succeeded", "Unknown"];
 
         lastId++;
         $scope.chartId = 'pods-donut-chart-' + lastId;
 
-        // c3.js config for the pods donut chart
-        $scope.chartConfig = {
-          type: "donut",
-          donut: {
-            width: 8,
-            label: {
-              show: false
-            }
-          },
-          size: {
-            height: 130,
-            width: 130
-          },
-          legend: {
-            show: false
-          },
-          tooltip: {
-            format: {
-              value: function(value) {
-                return value;
-              }
-            },
-            position: function() {
-              // Position in the top-left to avoid problems with tooltip text wrapping.
-              return { top: 0, left: 0 };
-            }
-          },
-          data: {
-            type: "donut",
-            groups: [ phases ],
-            // Keep groups in our order.
-            order: null,
-            colors: {
-             Running: "#6eb664",
-             Warning: "#f9d67a",
-             Failed: "#d9534f",
-             Pending: "#e8e8e8",
-             Succeeded: "#0088ce",
-             Unknown: "#f9d67a"
-            }
-          }
-        };
-
-        var updateCenterText = function() {
+        function updateCenterText() {
           var donutChartTitle = d3.select(element[0]).select('text.c3-chart-arcs-title'),
             total = hashSizeFilter($scope.pods),
             smallText;
@@ -82,42 +41,86 @@ angular.module('openshiftConsole')
           donutChartTitle.html('');
           donutChartTitle.insert('tspan').text(total).classed('pod-count donut-title-big-pf', true).attr('dy', 0).attr('x', 0);
           donutChartTitle.insert('tspan').text(smallText).classed('donut-title-small-pf', true).attr('dy', 20).attr('x', 0);
+        }
+
+
+        // c3.js config for the pods donut chart
+        config = {
+          type: "donut",
+          bindto: '#' + $scope.chartId,
+          donut: {
+            width: 10,
+            label: {
+              show: false
+            }
+          },
+          size: {
+            height: 150,
+            width: 150
+          },
+          legend: {
+            show: false
+          },
+          onrendered: updateCenterText,
+          tooltip: {
+            format: {
+              value: function(value) {
+                return value;
+              }
+            },
+            position: function() {
+              // Position in the top-left to avoid problems with tooltip text wrapping.
+              return { top: 0, left: 0 };
+            }
+          },
+          data: {
+            type: "donut",
+            groups: [ phases ],
+            // Start with an empty chart and call chart.load() when we have data.
+            columns: ['Running', 0],
+            // Keep groups in our order.
+            order: null,
+            colors: {
+             Running: "#00b9e4",
+             Warning: "#f9d67a",
+             Failed: "#d9534f",
+             Pending: "#e8e8e8",
+             Succeeded: "#3f9c35",
+             Unknown: "#f9d67a"
+            }
+          }
         };
 
-        var updateChart = function() {
-          // Make sure we're inside a digest loop.
-          $scope.$evalAsync(function() {
-            var countByPhase = {};
-            var config = $scope.chartConfig;
-            var incrementCount = function(phase) {
-              countByPhase[phase] = (countByPhase[phase] || 0) + 1;
-            };
+        function updateChart() {
+          var columns = [];
+          var countByPhase = {};
+          var incrementCount = function(phase) {
+            countByPhase[phase] = (countByPhase[phase] || 0) + 1;
+          };
 
-            angular.forEach($scope.pods, function(pod) {
-              // Count 'Warning' as its own phase, even if not strictly accurate,
-              // so it appears in the donut chart. Warnings are too important not
-              // to call out.
-              if (isTroubledPodFilter(pod)) {
-                incrementCount('Warning');
-              } else {
-                incrementCount(pod.status.phase);
-              }
-            });
-
-            config.data.columns = [];
-            angular.forEach(phases, function(phase) {
-              var count = countByPhase[phase];
-              if (count) {
-                config.data.columns.push([phase, count]);
-              }
-            });
-
-            config.oninit = updateCenterText;
+          angular.forEach($scope.pods, function(pod) {
+            // Count 'Warning' as its own phase, even if not strictly accurate,
+            // so it appears in the donut chart. Warnings are too important not
+            // to call out.
+            if (isTroubledPodFilter(pod)) {
+              incrementCount('Warning');
+            } else {
+              incrementCount(pod.status.phase);
+            }
           });
-        };
 
-        $scope.$watch('pods', updateChart, true);
-        $scope.$watch('desired', updateCenterText);
+          angular.forEach(phases, function(phase) {
+            columns.push([phase, countByPhase[phase] || 0]);
+          });
+
+          chart.load({ columns: columns });
+        }
+
+        $timeout(function() {
+          chart = c3.generate(config);
+          $scope.$watch('pods', updateChart, true);
+          $scope.$watch('desired', updateCenterText);
+        }, 0, false);
       }
     };
   });
