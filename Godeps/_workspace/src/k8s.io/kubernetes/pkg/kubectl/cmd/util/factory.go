aae43dc781671d199411e323b4bd375a36821ca6
deads2k <deads@redhat.com>
2015-10-22 13:10:24 -0400

    UPSTREAM: 16042: fix missing error handling

--- Godeps/_workspace/src/k8s.io/kubernetes/pkg/kubectl/cmd/util/factory.go
+++ Godeps/_workspace/src/k8s.io/kubernetes/pkg/kubectl/cmd/util/factory.go
@@ -135,6 +135,9 @@ func NewFactory(optionalClientConfig clientcmd.ClientConfig) *Factory {
 		},
 		RESTClient: func(mapping *meta.RESTMapping) (resource.RESTClient, error) {
 			group, err := api.RESTMapper.GroupForResource(mapping.Resource)
+			if err != nil {
+				return nil, err
+			}
 			client, err := clients.ClientForVersion(mapping.APIVersion)
 			if err != nil {
 				return nil, err
