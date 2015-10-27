8cb159d41123300329bad384d8e9120f820c6808
Ben Parees <bparees@redhat.com>
2015-10-27 14:40:29 -0400

    upper case first letter of status message

--- pkg/util/errors/errors.go
+++ pkg/util/errors/errors.go
@@ -1,5 +1,7 @@
 package errors
 
+import "strings"
+
 import kapierrors "k8s.io/kubernetes/pkg/api/errors"
 
 // TolerateNotFoundError tolerates 'not found' errors
@@ -8,4 +10,18 @@ func TolerateNotFoundError(err error) error {
 		return nil
 	}
 	return err
+}
+
+// ErrorToSentence will capitalize the first letter of the error
+// message and add a period to the end if one is not present.
+func ErrorToSentence(err error) string {
+	msg := err.Error()
+	if len(msg) == 0 {
+		return msg
+	}
+	msg = strings.ToUpper(msg)[:1] + msg[1:]
+	if !strings.HasSuffix(msg, ".") {
+		msg = msg + "."
+	}
+	return msg
 }
