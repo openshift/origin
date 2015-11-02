7ce44798ec9ff7938620b5118d0a4d387ec84404
benjaminapetersen <admin@benjaminapetersen.me>
2015-11-02 13:54:07 -0500

    Add error clause to service/project.js

--- assets/app/scripts/services/project.js
+++ assets/app/scripts/services/project.js
@@ -2,11 +2,12 @@
 
 angular.module('openshiftConsole')
   .factory('project', [
+    '$location',
     '$q',
     '$routeParams',
     'AuthService',
     'DataService',
-    function($q, $routeParams, AuthService, DataService) {
+    function($location, $q, $routeParams, AuthService, DataService) {
       return {
         get: function(projectName) {
           return  AuthService
@@ -16,7 +17,7 @@ angular.module('openshiftConsole')
                         // TODO: swap $.Deferred() for $q.defer()
                         projectName: projectName,
                         projectPromise: $.Deferred()
-                      };                      
+                      };
                       return DataService
                               .get('projects', context.projectName, context, {errorNotification: false})
                               .then(function(project) {
@@ -26,6 +27,23 @@ angular.module('openshiftConsole')
                                 return [project, context];
                               }, function(e) {
                                 context.projectPromise.reject(e);
+                                var description = 'The project could not be loaded.';
+                                var type = 'error';
+                                if(e.status === 403) {
+                                  description = 'The project ' + context.projectName + ' does not exist or you are not authorized to view it.';
+                                  type = 'access_denied';
+                                } else if (e.status === 404) {
+                                  description = 'The project " + context.projectName + " does not exist.';
+                                  type = 'not_found';
+                                }
+                                $location
+                                  .url(
+                                    URI('error')
+                                      .query({
+                                        "error" : type,
+                                        "error_description": description
+                                      })
+                                      .toString());
                               });
                     });
           }
