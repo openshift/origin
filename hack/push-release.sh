bf5af09edbee9744b1c4d4316712ce17ec66de31
Jordan Liggitt <jliggitt@redhat.com>
2015-11-13 09:25:59 -0500

    Push recycler image

--- hack/push-release.sh
+++ hack/push-release.sh
@@ -49,6 +49,7 @@ images=(
   openshift/origin-sti-builder
   openshift/origin-haproxy-router
   openshift/origin-f5-router
+  openshift/origin-recycler
   openshift/hello-openshift
   openshift/openvswitch
   openshift/node
