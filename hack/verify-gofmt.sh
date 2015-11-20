60fb7c020ed681360b3805c7f4082abae66bddfc
Maciej Szulik <maszulik@redhat.com>
2015-11-20 18:01:06 +0100

    Use go1.5 when invoking gofmt and go vet as well

--- hack/verify-gofmt.sh
+++ hack/verify-gofmt.sh
@@ -8,7 +8,7 @@ set -o pipefail
 
 GO_VERSION=($(go version))
 
-if [[ -z $(echo "${GO_VERSION[2]}" | grep -E 'go1.4') ]]; then
+if [ ${GO_VERSION[2]} \< "go1.4" ]; then
   echo "Unknown go version '${GO_VERSION}', skipping gofmt."
   exit 0
 fi
