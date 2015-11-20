openshift/origin

commit df2419231f5ad7efea1028cea1690e729204c378

Don't return an error if the plugin is unknown

plugins/osdn/factory/factory.go
@@ -24,5 +24,5 @@ func NewPlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Cl
 		return multitenant.CreatePlugin(osdn.NewRegistry(osClient, kClient), hostname, selfIP, ready)
 	}
 
-	return nil, nil, fmt.Errorf("unknown network plugin type: %v", pluginType)
+	return nil, nil, nil
 }

commit dafbf8f8a67b376d5cba2077f30d6a52ec85c4ce

Trivial build fix
Fixes: df2419231f5ad7efea1028cea1690e729204c378

plugins/osdn/factory/factory.go
@@ -1,7 +1,6 @@
 package factory
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/openshift/openshift-sdn/plugins/osdn"