c1f5205598bd9d98618b335c4dad5b267684fca3
Ben Parees <bparees@redhat.com>
2015-11-21 12:50:24 -0500

    Revert "bump(github.com/openshift/openshift-sdn) 919e0142fe594ab5115ecf7fa3f7ad4f5810f009"
    This reverts commit abafebae6b6b4c6e62c60325d98f3c2f4bf80c1a.

--- Godeps/_workspace/src/github.com/openshift/openshift-sdn/plugins/osdn/multitenant/multitenant.go
+++ Godeps/_workspace/src/github.com/openshift/openshift-sdn/plugins/osdn/multitenant/multitenant.go
@@ -1,100 +1,51 @@
 package multitenant
 
 import (
-	"fmt"
-	"strconv"
-
 	"github.com/golang/glog"
-
-	"github.com/openshift/openshift-sdn/plugins/osdn"
-	"github.com/openshift/openshift-sdn/plugins/osdn/api"
-	oskserver "github.com/openshift/origin/pkg/cmd/server/kubernetes"
+	"strings"
 
 	knetwork "k8s.io/kubernetes/pkg/kubelet/network"
-	kubeletTypes "k8s.io/kubernetes/pkg/kubelet/types"
-	utilexec "k8s.io/kubernetes/pkg/util/exec"
-)
+	"k8s.io/kubernetes/pkg/util/exec"
 
-type multitenantPlugin struct {
-	osdn.OvsController
-}
+	"github.com/openshift/openshift-sdn/pkg/ovssubnet"
+	"github.com/openshift/openshift-sdn/plugins/osdn"
+)
 
 func NetworkPluginName() string {
 	return "redhat/openshift-ovs-multitenant"
 }
 
-func CreatePlugin(registry *osdn.Registry, hostname string, selfIP string, ready chan struct{}) (api.OsdnPlugin, oskserver.FilteringEndpointsConfigHandler, error) {
-	mtp := &multitenantPlugin{}
-
-	err := mtp.BaseInit(registry, NewFlowController(), mtp, hostname, selfIP, ready)
+func Master(registry *osdn.OsdnRegistryInterface, clusterNetworkCIDR string, clusterBitsPerSubnet uint, serviceNetworkCIDR string) {
+	// get hostname from the gateway
+	output, err := exec.New().Command("uname", "-n").CombinedOutput()
 	if err != nil {
-		return nil, nil, err
+		glog.Fatalf("SDN initialization failed: %v", err)
 	}
+	host := strings.TrimSpace(string(output))
 
-	return mtp, registry, err
-}
-
-func (plugin *multitenantPlugin) PluginStartMaster(clusterNetworkCIDR string, clusterBitsPerSubnet uint, serviceNetworkCIDR string) error {
-	if err := plugin.SubnetStartMaster(clusterNetworkCIDR, clusterBitsPerSubnet, serviceNetworkCIDR); err != nil {
-		return err
+	kc, err := ovssubnet.NewMultitenantController(registry, host, "", nil)
+	if err != nil {
+		glog.Fatalf("SDN initialization failed: %v", err)
 	}
-
-	if err := plugin.VnidStartMaster(); err != nil {
-		return err
+	kc.AdminNamespaces = append(kc.AdminNamespaces, "default")
+	err = kc.StartMaster(clusterNetworkCIDR, clusterBitsPerSubnet, serviceNetworkCIDR)
+	if err != nil {
+		glog.Fatalf("SDN initialization failed: %v", err)
 	}
-
-	return nil
 }
 
-func (plugin *multitenantPlugin) PluginStartNode(mtu uint) error {
-	if err := plugin.SubnetStartNode(mtu); err != nil {
-		return err
+func Node(registry *osdn.OsdnRegistryInterface, hostname string, publicIP string, ready chan struct{}, plugin knetwork.NetworkPlugin, mtu uint) {
+	mp, ok := plugin.(*MultitenantPlugin)
+	if !ok {
+		glog.Fatalf("Failed to type cast provided plugin to a multitenant type plugin")
 	}
-
-	if err := plugin.VnidStartNode(); err != nil {
-		return err
+	kc, err := ovssubnet.NewMultitenantController(registry, hostname, publicIP, ready)
+	if err != nil {
+		glog.Fatalf("SDN initialization failed: %v", err)
 	}
-
-	return nil
-}
-
-//-----------------------------------------------
-
-const (
-	setUpCmd    = "setup"
-	tearDownCmd = "teardown"
-	statusCmd   = "status"
-)
-
-func (plugin *multitenantPlugin) getExecutable() string {
-	return "openshift-ovs-multitenant"
-}
-
-func (plugin *multitenantPlugin) Init(host knetwork.Host) error {
-	return nil
-}
-
-func (plugin *multitenantPlugin) Name() string {
-	return NetworkPluginName()
-}
-
-func (plugin *multitenantPlugin) SetUpPod(namespace string, name string, id kubeletTypes.DockerID) error {
-	vnid, found := plugin.VNIDMap[namespace]
-	if !found {
-		return fmt.Errorf("Error fetching VNID for namespace: %s", namespace)
+	mp.OvsController = kc
+	err = kc.StartNode(mtu)
+	if err != nil {
+		glog.Fatalf("SDN Node failed: %v", err)
 	}
-	out, err := utilexec.New().Command(plugin.getExecutable(), setUpCmd, namespace, name, string(id), strconv.FormatUint(uint64(vnid), 10)).CombinedOutput()
-	glog.V(5).Infof("SetUpPod 'multitenant' network plugin output: %s, %v", string(out), err)
-	return err
-}
-
-func (plugin *multitenantPlugin) TearDownPod(namespace string, name string, id kubeletTypes.DockerID) error {
-	// The script's teardown functionality doesn't need the VNID
-	out, err := utilexec.New().Command(plugin.getExecutable(), tearDownCmd, namespace, name, string(id), "-1").CombinedOutput()
-	glog.V(5).Infof("TearDownPod 'multitenant' network plugin output: %s, %v", string(out), err)
-	return err
-}
-
-func (plugin *multitenantPlugin) Status(namespace string, name string, id kubeletTypes.DockerID) (*knetwork.PodNetworkStatus, error) {
-	return nil, nil
 }
