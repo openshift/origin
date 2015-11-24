ed6adf3316e2a5ca92c8f8c32159a1b2695602ba
Steve Kuznetsov <skuznets@redhat.com>
2015-11-24 09:53:11 -0500

    refactored test/cmd/templates to use wrapper methods

--- test/cmd/templates.sh
+++ test/cmd/templates.sh
@@ -6,58 +6,58 @@ set -o pipefail
 
 OS_ROOT=$(dirname "${BASH_SOURCE}")/../..
 source "${OS_ROOT}/hack/util.sh"
+source "${OS_ROOT}/hack/cmd_util.sh"
 os::log::install_errexit
 
 # This test validates template commands
 
-oc get templates
-oc create -f examples/sample-app/application-template-dockerbuild.json
-oc get templates
-oc get templates ruby-helloworld-sample
-oc get template ruby-helloworld-sample -o json | oc process -f - >/dev/null
-oc process ruby-helloworld-sample >/dev/null
-oc describe templates ruby-helloworld-sample
-[ "$(oc describe templates ruby-helloworld-sample | grep -E "BuildConfig.*ruby-sample-build")" ]
-oc delete templates ruby-helloworld-sample
-oc get templates
+os::cmd::expect_success 'oc get templates'
+os::cmd::expect_success 'oc create -f examples/sample-app/application-template-dockerbuild.json'
+os::cmd::expect_success 'oc get templates'
+os::cmd::expect_success 'oc get templates ruby-helloworld-sample'
+os::cmd::expect_success 'oc get template ruby-helloworld-sample -o json | oc process -f -'
+os::cmd::expect_success 'oc process ruby-helloworld-sample'
+os::cmd::expect_success_and_text 'oc describe templates ruby-helloworld-sample' "BuildConfig.*ruby-sample-build"
+os::cmd::expect_success 'oc delete templates ruby-helloworld-sample'
+os::cmd::expect_success 'oc get templates'
 # TODO: create directly from template
 echo "templates: ok"
 
-oc process -f test/templates/fixtures/guestbook.json -l app=guestbook | oc create -f -
-oc status
-[ "$(oc status | grep frontend-service)" ]
+os::cmd::expect_success 'oc process -f test/templates/fixtures/guestbook.json -l app=guestbook | oc create -f -'
+os::cmd::expect_success_and_text 'oc status' 'frontend-service'
 echo "template+config: ok"
 
 
 # Run as cluster-admin to allow choosing any supplemental groups we want
 # Ensure large integers survive unstructured JSON creation
-oc create -f test/fixtures/template-type-precision.json
+os::cmd::expect_success 'oc create -f test/fixtures/template-type-precision.json'
 # ... and processing
-[ "$(oc process template-type-precision | grep 1000030003)" ]
-[ "$(oc process template-type-precision | grep 2147483647)" ]
-[ "$(oc process template-type-precision | grep 9223372036854775807)" ]
+os::cmd::expect_success_and_text 'oc process template-type-precision' '1000030003'
+os::cmd::expect_success_and_text 'oc process template-type-precision' '2147483647'
+os::cmd::expect_success_and_text 'oc process template-type-precision' '9223372036854775807'
 # ... and re-encoding as structured resources
-[ "$(oc process template-type-precision | oc create -f -)" ]
+os::cmd::expect_success 'oc process template-type-precision | oc create -f -'
 # ... and persisting
-[ "$(oc get pod/template-type-precision -o json | grep 1000030003)" ]
-[ "$(oc get pod/template-type-precision -o json | grep 2147483647)" ]
-[ "$(oc get pod/template-type-precision -o json | grep 9223372036854775807)" ]
+os::cmd::expect_success_and_text 'oc get pod/template-type-precision -o json' '1000030003'
+os::cmd::expect_success_and_text 'oc get pod/template-type-precision -o json' '2147483647'
+os::cmd::expect_success_and_text 'oc get pod/template-type-precision -o json' '9223372036854775807'
 # Ensure patch computation preserves data
-oc patch pod template-type-precision -p '{"metadata":{"annotations":{"comment":"patch comment"}}}'
-[ "$(oc get pod/template-type-precision -o json | grep 9223372036854775807)" ]
-[ "$(oc get pod/template-type-precision -o json | grep "patch comment")" ]
-oc delete template/template-type-precision
-oc delete pod/template-type-precision
+patch='{"metadata":{"annotations":{"comment":"patch comment"}}}'
+os::cmd::expect_success "oc patch pod template-type-precision -p '${patch}'"
+os::cmd::expect_success_and_text 'oc get pod/template-type-precision -o json' '9223372036854775807'
+os::cmd::expect_success_and_text 'oc get pod/template-type-precision -o json' 'patch comment'
+os::cmd::expect_success 'oc delete template/template-type-precision'
+os::cmd::expect_success 'oc delete pod/template-type-precision'
 echo "template data precision: ok"
 
 
-oc create -f examples/sample-app/application-template-dockerbuild.json -n openshift
-oc policy add-role-to-user admin test-user
-oc login -u test-user -p password
-oc new-project test-template-project
-oc create -f examples/sample-app/application-template-dockerbuild.json
-oc process template/ruby-helloworld-sample >/dev/null
-oc process templates/ruby-helloworld-sample > /dev/null
-oc process openshift//ruby-helloworld-sample > /dev/null
-oc process openshift/template/ruby-helloworld-sample >/dev/null
+os::cmd::expect_success 'oc create -f examples/sample-app/application-template-dockerbuild.json -n openshift'
+os::cmd::expect_success 'oc policy add-role-to-user admin test-user'
+os::cmd::expect_success 'oc login -u test-user -p password'
+os::cmd::expect_success 'oc new-project test-template-project'
+os::cmd::expect_success 'oc create -f examples/sample-app/application-template-dockerbuild.json'
+os::cmd::expect_success 'oc process template/ruby-helloworld-sample'
+os::cmd::expect_success 'oc process templates/ruby-helloworld-sample'
+os::cmd::expect_success 'oc process openshift//ruby-helloworld-sample'
+os::cmd::expect_success 'oc process openshift/template/ruby-helloworld-sample'
 echo "processing templates in different namespace: ok"
