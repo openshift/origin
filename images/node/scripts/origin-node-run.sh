1640f9ae8266dfd42840619cfeace4763b5ffe30
Scott Dodson <sdodson@redhat.com>
2015-12-01 11:26:51 -0500

    Sync Dockerfile.product from dist-git

--- images/node/scripts/origin-node-run.sh
+++ images/node/scripts/origin-node-run.sh
@@ -2,10 +2,11 @@
 
 set -eu
 
+conf=${CONFIG_FILE:-/etc/origin/node/node-config.yaml}
+opts=${OPTIONS:---loglevel=2}
+
 function quit {
-    pkill openshift
-    /usr/share/openvswitch/scripts/ovs-ctl stop
-    rm -f ${HOST_ETC}/systemd/system/docker.service.d/docker-sdn-ovs.conf
+    pkill -g 0 openshift
     exit 0
 }
 
@@ -16,9 +17,6 @@ if [ ! -f ${HOST_ETC}/systemd/system/docker.service.d/docker-sdn-ovs.conf ]; the
     cp /usr/lib/systemd/system/docker.service.d/docker-sdn-ovs.conf ${HOST_ETC}/systemd/system/docker.service.d
 fi
 
-/usr/share/openvswitch/scripts/ovs-ctl start --system-id=random
-
-/usr/bin/openshift start node --config=/etc/origin/node/node-config.yaml --loglevel=2 &
+/usr/bin/openshift start node "--config=${conf}" "${opts}" &
 
 while true; do sleep 5; done
-
