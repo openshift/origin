aaaf5e6dbdc5f7602c50791531d9ed081ca61187
Ravi Sankar Penta <rpenta@redhat.com>
2015-12-03 15:00:40 -0800

    Test cases for VNID range interface

---
+++ pkg/sdn/registry/netnamespace/vnid/vnid_test.go
@@ -0,0 +1,79 @@
+package vnid
+
+import (
+	"testing"
+)
+
+func TestVNIDRange(t *testing.T) {
+	testCases := []struct {
+		base     uint
+		size     uint
+		success  bool
+		expected string
+		included int
+		excluded int
+		usecase  string
+	}{
+		{101, 100, true, "101-200", 150, 201, "valid input"},
+		{201, 100, true, "201-300", 201, 301, "valid input, check min"},
+		{201, 100, true, "201-300", 300, 301, "valid input, check max"},
+		{10, 1, true, "10-10", 10, 11, "input with size=1"},
+		{100, 0, false, "", -1, -1, "invalid size"},
+		{1, 100, false, "", -1, -1, "invalid min"},
+		{1, (1 << 24), false, "", -1, -1, "invalid max"},
+	}
+
+	for i := range testCases {
+		tc := &testCases[i]
+		r, err := NewVNIDRange(tc.base, tc.size)
+		if err != nil && tc.success == true {
+			t.Errorf("expected success for %s, got %q", tc.usecase, err)
+			continue
+		} else if err == nil && tc.success == false {
+			t.Errorf("expected failure for %s", tc.usecase)
+			continue
+		} else if tc.success {
+			if r.String() != tc.expected {
+				t.Errorf("expected %q, got %q", tc.expected, r.String())
+			}
+			if tc.included >= 0 && !r.Contains(uint(tc.included)) {
+				t.Errorf("expected %q to include %d", r.String(), tc.included)
+			}
+			if tc.excluded >= 0 && r.Contains(uint(tc.excluded)) {
+				t.Errorf("expected %q to exclude %d", r.String(), tc.excluded)
+			}
+		}
+	}
+}
+
+func TestParseVNIDRange(t *testing.T) {
+	testCases := []struct {
+		input    string
+		success  bool
+		expected string
+		usecase  string
+	}{
+		{"101-200", true, "101-200", "valid input"},
+		{"", false, "", "invalid input"},
+		{"100", false, "", "invalid input, no hyphen in the range"},
+		{"100:", false, "", "invalid input, missing max value"},
+		{"100/200", false, "", "invalid input, unsupported format"},
+		{"0-99", false, "", "violates min vnid value"},
+	}
+
+	for i := range testCases {
+		tc := &testCases[i]
+		r, err := ParseVNIDRange(tc.input)
+		if err != nil && tc.success == true {
+			t.Errorf("expected success for %s, got %q", tc.usecase, err)
+			continue
+		} else if err == nil && tc.success == false {
+			t.Errorf("expected failure for %s", tc.usecase)
+			continue
+		} else if tc.success {
+			if r.String() != tc.expected {
+				t.Errorf("expected %q, got %q", tc.expected, r.String())
+			}
+		}
+	}
+}
