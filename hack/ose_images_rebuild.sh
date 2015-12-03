397a0e1c475a4683fef44c52d1f808fd0926a30b
Troy Dawson <tdawson@redhat.com>
2015-12-03 16:19:50 -0600

    add new options

--- hack/ose_images_rebuild.sh
+++ hack/ose_images_rebuild.sh
@@ -9,13 +9,13 @@
 #    - run build in OSBS
 #    (BZs need to be created before running rebuild)
 #
-# Before working with bugzilla, you need to log in:
-#   bugzilla login
+# Before working , you need your kerberos log in:
+#   kinit
 #
 # Required packages:
-#   python-bugzilla
 #   rhpkg
 #   krb5-workstation
+#   git
 
 
 BRANCH_RELEASE=2.0
@@ -23,16 +23,16 @@ BRANCH_RELEASE=2.0
 # format:
 # dist-git_name	image_dependency dist-git_branch git_repo git_path
 base_images_list="
-openshift-enterprise-base-docker None rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/base
-openshift-enterprise-pod-docker None rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/pod
-openshift-enterprise-keepalived-ipfailover-docker openshift-enterprise-base-docker rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/ipfailover/keepalived
-openshift-enterprise-dockerregistry-docker None openshift-enterprise-base-docker-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/dockerregistry
-openshift-enterprise-docker openshift-enterprise-base-docker rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/ose
-openshift-enterprise-haproxy-router-base-docker openshift-enterprise-base-docker rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/router/haproxy-base
-openshift-enterprise-deployer-docker openshift-enterprise-docker rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/deployer
-openshift-enterprise-sti-builder-docker openshift-enterprise-docker rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/builder/docker/sti-builder
-openshift-enterprise-docker-builder-docker openshift-enterprise-docker rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/builder/docker/docker-builder
-openshift-enterprise-haproxy-router-docker openshift-enterprise-haproxy-router-base-docker rhaos-3.1-rhel-7 https://github.com/openshift/ose.git ose/images/router/haproxy
+openshift-enterprise-base-docker None rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/base
+openshift-enterprise-pod-docker None rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/pod
+openshift-enterprise-keepalived-ipfailover-docker openshift-enterprise-base-docker rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/ipfailover/keepalived
+openshift-enterprise-dockerregistry-docker None openshift-enterprise-base-docker-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/dockerregistry
+openshift-enterprise-docker openshift-enterprise-base-docker rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/ose
+openshift-enterprise-haproxy-router-base-docker openshift-enterprise-base-docker rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/router/haproxy-base
+openshift-enterprise-deployer-docker openshift-enterprise-docker rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/deployer
+openshift-enterprise-sti-builder-docker openshift-enterprise-docker rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/builder/docker/sti-builder
+openshift-enterprise-docker-builder-docker openshift-enterprise-docker rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/builder/docker/docker-builder
+openshift-enterprise-haproxy-router-docker openshift-enterprise-haproxy-router-base-docker rhaos-3.1-rhel-7 git@github.com:openshift/ose.git ose/images/router/haproxy
 "
 
 # format:
@@ -53,12 +53,12 @@ usage() {
   echo "Usage `basename $0` <action> <version>" >&2
   echo >&2
   echo "Actions:" >&2
-  echo "  everything_base  - git_update, build_image (non s2i images)" >&2
-  echo "  everything_s2i   - git_update, build_image (s2i images)" >&2
-  echo "  git_update_base  - Clone dist-git, bump release, commit (non-s2i images)" >&2
-  echo "  git_update_s2i   - Clone dist-git, bump release, commit (s2i images)" >&2
-  echo "  build_image_base - Clone dist-git, bump release, commit (non-s2i images)" >&2
-  echo "  build_image_s2i  - Clone dist-git, bump release, commit (s2i images)" >&2
+  echo "  everything_base  - git_update,  build_container (non s2i images)" >&2
+  echo "  everything_s2i   - git_update,  build_container (s2i images)" >&2
+  echo "  git_update_base  - Clone git and dist-git, bump release, compare (non-s2i images)" >&2
+  echo "  git_update_s2i   - Clone git and dist-git, bump release, compare (s2i images)" >&2
+  echo "  build_container_base - Clone dist-git, build containers (non-s2i images)" >&2
+  echo "  build_container_s2i  - Clone dist-git, build containers (s2i images)" >&2
   echo >&2
   echo "Version:" >&2
   echo "  specific image version, e.g. 3.1.1.2 or 1.1 (What should be in LABEL Version)" >&2
@@ -73,32 +73,22 @@ bump_release() {
   sed -i -e "s/Release=\"[0-9]*\"/Release=\"$cur\"/" Dockerfile
 }
 
-testrebase() {
-  if ! klist &>/dev/null ; then
-    echo "Error: Kerberos token not found." ; popd &>/dev/null ; exit 1
-  fi
-  echo "=== $bz_component ==="
-  rhpkg clone "$bz_component" &>/dev/null
-  cd $bz_component
-  rhpkg switch-branch "$branch" &>/dev/null
-  bump_release
-  tracker_bz=$(bugzilla query -i -c distribution -p 'Red Hat Software Collections' -s "$base_image" | head -n 1)
-  [ -z "$tracker_bz" ] && echo "Error: Tracker bug couldn't be guessed" && exit 1
-  bug_id=$(bugzilla query -i -c rh-mariadb100-docker --blocked=$tracker_bz | head -n 1)
-  [ -z "$bug_id" ] && echo "Error: Component bug couldn't be guessed" && exit 1
-  git commit -am "Rebuild on base image update (${base_image})
-Resolves: #${bug_id}"
-  git --no-pager show
-}
-
 setup_dist_git() {
   if ! klist &>/dev/null ; then
     echo "Error: Kerberos token not found." ; popd &>/dev/null ; exit 1
   fi
-  echo "=== $collection ==="
-  rhpkg clone "$collection" &>/dev/null
-  cd $collection
+  echo "=== ${container} ==="
+  rhpkg clone "${container}" &>/dev/null
+  pushd ${container} >/dev/null
   rhpkg switch-branch "$branch" &>/dev/null
+  popd >/dev/null
+}
+
+setup_git_repo() {
+  pushd "${workingdir}" >/dev/null
+  git clone -q ${repo} 2>/dev/null
+  popd >/dev/null
+
 }
 
 check_builds() {
@@ -154,30 +144,56 @@ check_build_dependencies() {
 
 build_image() {
   check_build_dependencies
-  rhpkg container-build --scratch --repo http://file.rdu.redhat.com/sdodson/aos-unsigned.repo >> ${workingdir}/logs/${collection}.buildlog 2>&1 &
+  rhpkg container-build --scratch --repo http://file.rdu.redhat.com/sdodson/aos-unsigned.repo >> ${workingdir}/logs/${container}.buildlog 2>&1 &
   echo -n "  Waiting for createContainer taskid ."
-  taskid=`grep createContainer ${workingdir}/logs/${collection}.buildlog | awk '{print $1}' | sort -u`
+  taskid=`grep createContainer ${workingdir}/logs/${container}.buildlog | awk '{print $1}' | sort -u`
   while [ "${taskid}" == "" ]
   do
     echo -n "."
     sleep 5
-    if grep -q -e "buildContainer (noarch) failed" -e "server startup error" ${workingdir}/logs/${collection}.buildlog ; then
+    if grep -q -e "buildContainer (noarch) failed" -e "server startup error" ${workingdir}/logs/${container}.buildlog ; then
       echo " error"
-      echo "=== ${collection} IMAGE BUILD FAILED ==="
+      echo "=== ${container} IMAGE BUILD FAILED ==="
       echo "  exiting"
       exit 23
     fi
-    taskid=`grep createContainer ${workingdir}/logs/${collection}.buildlog | awk '{print $1}' | sort -u`
+    taskid=`grep createContainer ${workingdir}/logs/${container}.buildlog | awk '{print $1}' | sort -u`
   done
   echo " "
-  brew watch-logs ${taskid} >> ${workingdir}/logs/${collection}.watchlog 2>&1 &
+  brew watch-logs ${taskid} >> ${workingdir}/logs/${container}.watchlog 2>&1 &
 
 }
 
-rebase() {
+show_git_diffs() {
+  pushd "${workingdir}/${container}" >/dev/null
+  find . -name ".git*" -prune -o -name ".osbs*" -prune -o -type f -print | while read line
+  do
+    updatetime=`git log --date=iso -n 1 --pretty="%ad" ${line}`
+    pushd "${workingdir}/${path}" >/dev/null
+    newupdates=`git log --oneline --after="${updatetime}" ${line} | wc -l`
+    if [ ${newupdates} -gt 0 ] ; then
+      echo "=== Change in ${container}/${line} ==="
+      lastgit=`git log -1 --pretty="%H" --before="${updatetime}" ${line}`
+      git diff ${lastgit} ${line}
+    fi
+    popd >/dev/null
+  done
+  popd >/dev/null
+
+}
+
+build_container() {
   pushd "${workingdir}" >/dev/null
   setup_dist_git
   build_image
+  popd >/dev/null
+}
+
+git_update() {
+  pushd "${workingdir}" >/dev/null
+  setup_dist_git
+  setup_git_repo
+  show_git_diffs
   popd >/dev/null
 }
 
@@ -196,20 +212,31 @@ export action="$1"
 export base_image="$2"
 
 case "$action" in
-  rebase_base|testrebase_base) list="${base_images_list}" ;;
-  rebase_s2i|testrebase_s2i) list="${s2i_images_list}" ;;
-  createbz|testcreatebz) create_tracker ; list="${base_images_list}
+  everything_base|git_update_base|build_container_base) list="${base_images_list}" ;;
+  everything_s2i|git_update_s2i|build_container_s2i) list="${s2i_images_list}" ;;
+  everything_all|git_update_all|build_container_all) create_tracker ; list="${base_images_list}
 ${s2i_images_list}" ;;
   *) usage ;;
 esac
 
 echo "$list" | while read spec ; do
   [ -z "$spec" ] && continue
-  export collection=$(echo "$spec" | awk '{print $1}')
+  export container=$(echo "$spec" | awk '{print $1}')
   export dependency=$(echo "$spec" | awk '{print $2}')
   export branch=$(echo "$spec" | awk '{print $3}')
+  export repo=$(echo "$spec" | awk '{print $4}')
+  export path=$(echo "$spec" | awk '{print $5}')
   case "$action" in
-    rebase_base | rebase_s2i ) rebase ;;
+    build_container_base | build_container_s2i )
+      build_container
+      ;;
+    git_update_base | git_update_s2i )
+       git_update
+       ;;
+    everything_base | everything_s2i )
+      git_update
+      build_container
+      ;;
     * ) usage ;;
   esac
 done
