35230256efef1db952084a50b9471e98ef431398
Clayton Coleman <ccoleman@redhat.com>
2015-12-10 00:27:43 -0500

    Unit test for hostname override

--- test/extended/router/scoped.go
+++ test/extended/router/scoped.go
@@ -61,6 +61,60 @@ var _ = g.Describe("Router", func() {
 				}
 			}
 		})
+		g.It("should override the route host with a custom value", func() {
+			oc.SetOutputDir(exutil.TestContext.OutputDir)
+			ns := oc.KubeFramework().Namespace.Name
+
+			g.By(fmt.Sprintf("creating a scoped router from a config file %q", configPath))
+			err := oc.Run("create").Args("-f", configPath).Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			var routerIP string
+			err = wait.Poll(time.Second, 2*time.Minute, func() (bool, error) {
+				pod, err := oc.KubeFramework().Client.Pods(ns).Get("router-override")
+				if err != nil {
+					return false, err
+				}
+				if len(pod.Status.PodIP) == 0 {
+					return false, nil
+				}
+				routerIP = pod.Status.PodIP
+				return true, nil
+			})
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			// router expected to listen on port 80
+			routerURL := fmt.Sprintf("http://%s", routerIP)
+			pattern := "%s-%s.myapps.mycompany.com"
+
+			g.By("waiting for the valid route to respond")
+			err = waitForRouterOKResponse(routerURL, fmt.Sprintf(pattern, "route-1", ns), 2*time.Minute)
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			g.By("checking that the stored domain name does not match a route")
+			host := "first.example.com"
+			req, err := requestViaReverseProxy("GET", routerURL, host)
+			o.Expect(err).NotTo(o.HaveOccurred())
+			resp, err := http.DefaultClient.Do(req)
+			o.Expect(err).NotTo(o.HaveOccurred())
+			resp.Body.Close()
+			if resp.StatusCode != http.StatusServiceUnavailable {
+				e2e.Failf("should have had a 503 status code for %s", host)
+			}
+
+			for _, host := range []string{"route-2", "route-2"} {
+				host = fmt.Sprintf(pattern, host, ns)
+				g.By(fmt.Sprintf("checking that %s does not match a route", host))
+				req, err := requestViaReverseProxy("GET", routerURL, host)
+				o.Expect(err).NotTo(o.HaveOccurred())
+				resp, err := http.DefaultClient.Do(req)
+				o.Expect(err).NotTo(o.HaveOccurred())
+				resp.Body.Close()
+				if resp.StatusCode != http.StatusServiceUnavailable {
+					e2e.Failf("should have had a 503 status code for %s", host)
+				}
+			}
+		})
 	})
 })
 
