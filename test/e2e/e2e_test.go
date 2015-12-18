d4486238f79e7aa2c0e518ee53486cd3bc3388d0
Jay Vyas <jay@apache.org>
2015-12-18 14:57:06 -0500

    Add example of a simple test extension to wrapper

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -2,12 +2,18 @@ package e2e
 
 import (
 	"testing"
-	//. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo"
 	"k8s.io/kubernetes/test/e2e"
 )
 
-//var _ = Describe("CustomTest", func() {
-//})
+var _ = Describe("Custom Extension", func() {
+	It("Should be extensible", func() {
+		By("Adding a new test on top of the other E2Es")
+		if(1 == 0){
+			e2e.Failf("example test, will never fail.")
+		}
+	})
+})
 
 func TestE2EWrapper(t *testing.T) {
 	e2e.TestE2E(t);
