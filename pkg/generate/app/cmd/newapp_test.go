openshift/origin

commit 891fc216e97b9b74cc7f2ba4a568022b1e92a4e4

leverage new source-to-image API around git clone spec validation/correction

pkg/generate/app/sourcelookup.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"net/url"
 	"path/filepath"
-	"regexp"
 	"strings"
 
 	"github.com/docker/docker/builder/parser"
@@ -13,11 +12,7 @@ import (
 	"github.com/openshift/origin/pkg/generate/dockerfile"
 	"github.com/openshift/origin/pkg/generate/git"
 	"github.com/openshift/origin/pkg/generate/source"
-)
-
-var (
-	argumentGit         = regexp.MustCompile("^(http://|https://|git@|git://).*(?:#([a-zA-Z0-9]*))?$")
-	argumentGitProtocol = regexp.MustCompile("^(git@|git://)")
+	s2igit "github.com/openshift/source-to-image/pkg/scm/git"
 )
 
 type Dockerfile interface {
@@ -67,7 +62,14 @@ func IsPossibleSourceRepository(s string) bool {
 
 // IsRemoteRepository checks whether the provided string is a remote repository or not
 func IsRemoteRepository(s string) bool {
-	return argumentGit.MatchString(s) || argumentGitProtocol.MatchString(s)
+	if !s2igit.New().ValidCloneSpecRemoteOnly(s) {
+		return false
+	}
+	gitRepo := git.NewRepository()
+	if err := gitRepo.ListRemote(s); err != nil {
+		return false
+	}
+	return true
 }
 
 // SourceRepository represents a code repository that may be the target of a build.

pkg/generate/app/cmd/newapp_test.go
@@ -66,8 +66,8 @@ func TestAddArguments(t *testing.T) {
 			unknown:    []string{},
 		},
 		"source": {
-			args:    []string{".", testDir, "git://server/repo.git"},
-			repos:   util.StringList([]string{".", testDir, "git://server/repo.git"}),
+			args:    []string{".", testDir, "git://github.com/openshift/origin.git"},
+			repos:   util.StringList([]string{".", testDir, "git://github.com/openshift/origin.git"}),
 			unknown: []string{},
 		},
 		"env": {
@@ -76,9 +76,9 @@ func TestAddArguments(t *testing.T) {
 			unknown: []string{},
 		},
 		"mix 1": {
-			args:       []string{"git://server/repo.git", "mysql+ruby~git@test.server/repo.git", "env1=test", "ruby-helloworld-sample"},
-			repos:      util.StringList([]string{"git://server/repo.git"}),
-			components: util.StringList([]string{"mysql+ruby~git@test.server/repo.git", "ruby-helloworld-sample"}),
+			args:       []string{"git://github.com/openshift/origin.git", "mysql+ruby~git@github.com/openshift/origin.git", "env1=test", "ruby-helloworld-sample"},
+			repos:      util.StringList([]string{"git://github.com/openshift/origin.git"}),
+			components: util.StringList([]string{"mysql+ruby~git@github.com/openshift/origin.git", "ruby-helloworld-sample"}),
 			env:        util.StringList([]string{"env1=test"}),
 			unknown:    []string{},
 		},
@@ -256,6 +256,9 @@ func TestBuildTemplates(t *testing.T) {
 }
 
 func TestEnsureHasSource(t *testing.T) {
+	gitLocalDir := createLocalGitDirectory(t)
+	defer os.RemoveAll(gitLocalDir)
+
 	tests := []struct {
 		name              string
 		cfg               AppConfig
@@ -271,7 +274,7 @@ func TestEnsureHasSource(t *testing.T) {
 					ExpectToBuild: true,
 				}),
 			},
-			repositories: MockSourceRepositories(t),
+			repositories: MockSourceRepositories(t, gitLocalDir),
 			expectedErr:  "there are multiple code locations provided - use one of the following suggestions",
 		},
 		{
@@ -284,7 +287,7 @@ func TestEnsureHasSource(t *testing.T) {
 					ExpectToBuild: true,
 				}),
 			},
-			repositories: MockSourceRepositories(t),
+			repositories: MockSourceRepositories(t, gitLocalDir),
 			expectedErr:  "Use '[image]~[repo]' to declare which code goes with which image",
 		},
 		{
@@ -319,7 +322,7 @@ func TestEnsureHasSource(t *testing.T) {
 					ExpectToBuild: false,
 				}),
 			},
-			repositories: MockSourceRepositories(t)[:1],
+			repositories: MockSourceRepositories(t, gitLocalDir)[:1],
 			expectedErr:  "",
 		},
 		{
@@ -329,7 +332,7 @@ func TestEnsureHasSource(t *testing.T) {
 					ExpectToBuild: false,
 				}),
 			},
-			repositories: MockSourceRepositories(t),
+			repositories: MockSourceRepositories(t, gitLocalDir),
 			expectedErr:  "",
 		},
 	}
@@ -421,10 +424,13 @@ func TestResolve(t *testing.T) {
 
 func TestDetectSource(t *testing.T) {
 	skipExternalGit(t)
+	gitLocalDir := createLocalGitDirectory(t)
+	defer os.RemoveAll(gitLocalDir)
+
 	dockerSearcher := app.DockerRegistrySearcher{
 		Client: dockerregistry.NewClient(),
 	}
-	mocks := MockSourceRepositories(t)
+	mocks := MockSourceRepositories(t, gitLocalDir)
 	tests := []struct {
 		name         string
 		cfg          *AppConfig
@@ -441,7 +447,7 @@ func TestDetectSource(t *testing.T) {
 				},
 				dockerSearcher: dockerSearcher,
 			},
-			repositories: []*app.SourceRepository{mocks[1]},
+			repositories: []*app.SourceRepository{mocks[0]},
 			expectedLang: "ruby",
 			expectedErr:  "",
 		},
@@ -1503,14 +1509,22 @@ func fakeSimpleDockerSearcher() app.Searcher {
 	}
 }
 
+func createLocalGitDirectory(t *testing.T) string {
+	dir, err := ioutil.TempDir(os.TempDir(), "s2i-test")
+	if err != nil {
+		t.Error(err)
+	}
+	os.Mkdir(filepath.Join(dir, ".git"), 0600)
+	return dir
+}
+
 // MockSourceRepositories is a set of mocked source repositories used for
 // testing
-func MockSourceRepositories(t *testing.T) []*app.SourceRepository {
+func MockSourceRepositories(t *testing.T, file string) []*app.SourceRepository {
 	var b []*app.SourceRepository
 	for _, location := range []string{
-		"some/location.git",
 		"https://github.com/openshift/ruby-hello-world.git",
-		"another/location.git",
+		file,
 	} {
 		s, err := app.NewSourceRepository(location)
 		if err != nil {

test/cmd/newapp.sh
@@ -88,7 +88,7 @@ oc delete all -l app=helloworld
 [ "$(oc new-app ruby-helloworld-sample -l app=helloworld -o name 2>&1 | grep 'Service/frontend')" ]
 oc delete all -l app=helloworld
 # create from template with code explicitly set is not supported
-[ ! "$(oc new-app ruby-helloworld-sample~git@github.com/mfojtik/sinatra-app-example)" ]
+[ ! "$(oc new-app ruby-helloworld-sample~git@github.com:mfojtik/sinatra-app-example)" ]
 oc delete template ruby-helloworld-sample
 # override component names
 [ "$(oc new-app mysql --name=db | grep db)" ]

pkg/build/builder/sti.go
@@ -149,7 +149,6 @@ func (s *STIBuilder) Build() error {
 
 		BuilderImage: s.build.Spec.Strategy.SourceStrategy.From.Name,
 		Incremental:  s.build.Spec.Strategy.SourceStrategy.Incremental,
-		ForcePull:    s.build.Spec.Strategy.SourceStrategy.ForcePull,
 
 		Environment:       buildEnvVars(s.build),
 		DockerNetworkMode: getDockerNetworkMode(),
@@ -157,6 +156,16 @@ func (s *STIBuilder) Build() error {
 		Source:     sourceURI.String(),
 		Tag:        tag,
 		ContextDir: s.build.Spec.Source.ContextDir,
+	}
+
+	if s.build.Spec.Strategy.SourceStrategy.ForcePull {
+		glog.V(4).Infof("With force pull true, setting policies to %s", s2iapi.PullAlways)
+		config.PreviousImagePullPolicy = s2iapi.PullAlways
+		config.BuilderPullPolicy = s2iapi.PullAlways
+	} else {
+		glog.V(4).Infof("With force pull false, setting policies to %s", s2iapi.PullIfNotPresent)
+		config.PreviousImagePullPolicy = s2iapi.PullIfNotPresent
+		config.BuilderPullPolicy = s2iapi.PullIfNotPresent
 	}
 
 	allowedUIDs := os.Getenv("ALLOWED_UIDS")

pkg/generate/app/test/fakegit.go
@@ -66,3 +66,7 @@ func (f *FakeGit) AddRemote(source, remote, url string) error {
 func (f *FakeGit) ShowFormat(source, ref, format string) (string, error) {
 	return "", nil
 }
+
+func (f *FakeGit) ListRemote(url string) error {
+	return nil
+}

pkg/generate/git/git.go
@@ -2,10 +2,10 @@ package git
 
 import (
 	"bufio"
+	s2igit "github.com/openshift/source-to-image/pkg/scm/git"
 	"io"
 	"net/url"
 	"path"
-	"path/filepath"
 	"strings"
 )
 
@@ -16,29 +16,25 @@ import (
 // - http, https
 // - file
 // - git
+// - ssh
 func ParseRepository(s string) (*url.URL, error) {
 	uri, err := url.Parse(s)
 	if err != nil {
 		return nil, err
 	}
 
-	if uri.Scheme == "" && !strings.HasPrefix(uri.Path, "git@") {
-		path := s
-		ref := ""
-		segments := strings.SplitN(path, "#", 2)
-		if len(segments) == 2 {
-			path, ref = segments[0], segments[1]
-		}
-		path, err := filepath.Abs(path)
-		if err != nil {
-			return nil, err
-		}
-		uri = &url.URL{
-			Scheme:   "file",
-			Path:     path,
-			Fragment: ref,
-		}
+	// There are some shortcomings with url.Parse when it comes to GIT, namely wrt
+	// the GIT local/file and ssh protocols - it does not handle implied schema (i.e. no <proto>:// prefix)well;
+	// We handle those caveats here
+	err = s2igit.New().MungeNoProtocolURL(s, uri)
+	if err != nil {
+		return nil, err
 	}
+	//TODO temporary work around for s2i's MungeNoProtocolURL (which is not used in s2i at the moment)
+	// there is some overloaded usage between the uri Path and the Path needed for DownloaderForSource
+	// this temporary work around  can sit here even after the s2i change (including duplicating tests in this dir's git_test.go  to source-to-image/pkg/scm/git/git_test.go)
+	// gets Godeps into origin, and then we can remove this
+	uri.Path = strings.TrimPrefix(uri.Path, "file://")
 
 	return uri, nil
 }

pkg/generate/git/git_test.go
@@ -1,0 +1,132 @@
+package git
+
+import (
+	"io/ioutil"
+	"net/url"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func createLocalGitDirectory(t *testing.T) string {
+	dir, err := ioutil.TempDir(os.TempDir(), "s2i-test")
+	if err != nil {
+		t.Error(err)
+	}
+	os.Mkdir(filepath.Join(dir, ".git"), 0600)
+	return dir
+}
+
+func TestParseRepository(t *testing.T) {
+	gitLocalDir := createLocalGitDirectory(t)
+	defer os.RemoveAll(gitLocalDir)
+
+	tests := map[string]url.URL{
+		"git@github.com:user/repo.git": {
+			Scheme: "ssh",
+			Host:   "github.com",
+			User:   url.User("git"),
+			Path:   "user/repo.git",
+		},
+		"git://github.com/user/repo.git": {
+			Scheme: "git",
+			Host:   "github.com",
+			Path:   "/user/repo.git",
+		},
+		"git://github.com/user/repo": {
+			Scheme: "git",
+			Host:   "github.com",
+			Path:   "/user/repo",
+		},
+		"http://github.com/user/repo.git": {
+			Scheme: "http",
+			Host:   "github.com",
+			Path:   "/user/repo.git",
+		},
+		"http://github.com/user/repo": {
+			Scheme: "http",
+			Host:   "github.com",
+			Path:   "/user/repo",
+		},
+		"https://github.com/user/repo.git": {
+			Scheme: "https",
+			Host:   "github.com",
+			Path:   "/user/repo.git",
+		},
+		"https://github.com/user/repo": {
+			Scheme: "https",
+			Host:   "github.com",
+			Path:   "/user/repo",
+		},
+		"file://" + gitLocalDir: {
+			Scheme: "file",
+			Path:   gitLocalDir,
+		},
+		gitLocalDir: {
+			Scheme: "file",
+			Path:   gitLocalDir,
+		},
+		"git@192.168.122.1:repositories/authooks": {
+			Scheme: "ssh",
+			Host:   "192.168.122.1",
+			User:   url.User("git"),
+			Path:   "repositories/authooks",
+		},
+		"mbalazs@build.ulx.hu:/var/git/eap-ulx.git": {
+			Scheme: "ssh",
+			Host:   "build.ulx.hu",
+			User:   url.User("mbalazs"),
+			Path:   "/var/git/eap-ulx.git",
+		},
+		"ssh://git@[2001:db8::1]/repository.git": {
+			Scheme: "ssh",
+			Host:   "[2001:db8::1]",
+			User:   url.User("git"),
+			Path:   "/repository.git",
+		},
+		"ssh://git@mydomain.com:8080/foo/bar": {
+			Scheme: "ssh",
+			Host:   "mydomain.com:8080",
+			User:   url.User("git"),
+			Path:   "/foo/bar",
+		},
+		"git@[2001:db8::1]:repository.git": {
+			Scheme: "ssh",
+			Host:   "[2001:db8::1]",
+			User:   url.User("git"),
+			Path:   "repository.git",
+		},
+		"git@[2001:db8::1]:/repository.git": {
+			Scheme: "ssh",
+			Host:   "[2001:db8::1]",
+			User:   url.User("git"),
+			Path:   "/repository.git",
+		},
+	}
+
+	for scenario, test := range tests {
+		out, err := ParseRepository(scenario)
+		if err != nil {
+			t.Errorf("ParseRepository returned err: %v", err)
+		}
+
+		// reflect.DeepEqual was not dealing with url.URL correctly, have to check each field individually
+		// First, the easy string compares
+		equal := out.Scheme == test.Scheme && out.Opaque == test.Opaque && out.Host == test.Host && out.Path == test.Path && out.RawQuery == test.RawQuery && out.Fragment == test.Fragment
+		if equal {
+			// now deal with User, a Userinfo struct ptr
+			if out.User == nil && test.User != nil {
+				equal = false
+			} else if out.User != nil && test.User == nil {
+				equal = false
+			} else if out.User != nil && test.User != nil {
+				equal = out.User.String() == test.User.String()
+			}
+		}
+		if !equal {
+			t.Errorf("For URL string %s, field by field check for scheme %v opaque %v host %v path %v rawq %v frag %v out user nil %v test user nil %v out scheme  %s out opaque %s out host %s out path %s  out raw query %s out frag %s", scenario,
+				out.Scheme == test.Scheme, out.Opaque == test.Opaque, out.Host == test.Host, out.Path == test.Path, out.RawQuery == test.RawQuery,
+				out.Fragment == test.Fragment, out.User == nil, test.User == nil, out.Scheme, out.Opaque, out.Host, out.Path, out.RawQuery, out.Fragment)
+		}
+	}
+}

pkg/generate/git/repository.go
@@ -28,6 +28,7 @@ type Repository interface {
 	AddRemote(dir string, name, url string) error
 	AddLocalConfig(dir, name, value string) error
 	ShowFormat(dir, commit, format string) (string, error)
+	ListRemote(url string) error
 }
 
 // execGitFunc is a function that executes a Git command
@@ -144,6 +145,12 @@ func (r *repository) AddLocalConfig(location, name, value string) error {
 // Clone clones a remote git repository to a local directory
 func (r *repository) Clone(location string, url string) error {
 	_, _, err := r.git(nil, "", "clone", "--recursive", url, location)
+	return err
+}
+
+// LsRemote lists references in a remote repository
+func (r *repository) ListRemote(url string) error {
+	_, _, err := r.git(nil, "", "ls-remote", url)
 	return err
 }
 

commit f808579f7e7bbf5bd9013cd08244556b36fda9d9

Fix regression: new-app with custom Git ref
Regression introduced in 891fc216e97b9b74cc7f2ba4a568022b1e92a4e4.

pkg/generate/app/sourcelookup.go
@@ -65,8 +65,13 @@ func IsRemoteRepository(s string) bool {
 	if !s2igit.New().ValidCloneSpecRemoteOnly(s) {
 		return false
 	}
+	url, err := url.Parse(s)
+	if err != nil {
+		return false
+	}
+	url.Fragment = ""
 	gitRepo := git.NewRepository()
-	if _, _, err := gitRepo.ListRemote(s); err != nil {
+	if _, _, err := gitRepo.ListRemote(url.String()); err != nil {
 		return false
 	}
 	return true

pkg/generate/app/cmd/newapp_test.go
@@ -68,6 +68,11 @@ func TestAddArguments(t *testing.T) {
 			repos:   []string{".", testDir, "git://github.com/openshift/origin.git"},
 			unknown: []string{},
 		},
+		"source custom ref": {
+			args:    []string{"https://github.com/openshift/ruby-hello-world#beta4"},
+			repos:   []string{"https://github.com/openshift/ruby-hello-world#beta4"},
+			unknown: []string{},
+		},
 		"env": {
 			args:    []string{"first=one", "second=two", "third=three"},
 			env:     []string{"first=one", "second=two", "third=three"},

test/cmd/newapp.sh
@@ -159,4 +159,7 @@ os::cmd::expect_success_and_text 'oc new-app installable:serviceaccount --grant-
 # Ensure output is valid JSON
 os::cmd::expect_success 'oc new-app mongo -o json | python -m json.tool'
 
+# Ensure custom branch/ref works
+os::cmd::expect_success 'oc new-app https://github.com/openshift/ruby-hello-world#beta4'
+
 echo "new-app: ok"