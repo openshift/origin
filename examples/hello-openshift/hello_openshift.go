3e5ef2eee0ee0397f18b0e5a41c3e9c344347ec5
Vincent Behar <v.behar@free.fr>
2016-01-19 14:01:52 +0100

    hello-openshift example: make the ports configurable
    with env vars PORT and SECOND_PORT
    Default to 8080 and 8888

--- examples/hello-openshift/hello_openshift.go
+++ examples/hello-openshift/hello_openshift.go
@@ -3,29 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello OpenShift!")
 }
 
+func listenAndServe(port string) {
+	fmt.Printf("serving on %s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
 
-	go func() {
-		fmt.Println("serving on 8080")
-		err := http.ListenAndServe(":8080", nil)
-		if err != nil {
-			panic("ListenAndServe: " + err.Error())
-		}
-	}()
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	go listenAndServe(port)
 
-	go func() {
-		fmt.Println("serving on 8888")
-		err := http.ListenAndServe(":8888", nil)
-		if err != nil {
-			panic("ListenAndServe: " + err.Error())
-		}
-	}()
+	port = os.Getenv("SECOND_PORT")
+	if len(port) == 0 {
+		port = "8888"
+	}
+	go listenAndServe(port)
+
 	select {}
 }
