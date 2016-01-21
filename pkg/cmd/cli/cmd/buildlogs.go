35ea38f41712cc94f4ff26968263a58d5426a1c8
Slava Semushin <vsemushi@redhat.com>
2016-01-21 15:41:31 +0100

    Improve message for deprecated build-logs option.

--- pkg/cmd/cli/cmd/buildlogs.go
+++ pkg/cmd/cli/cmd/buildlogs.go
@@ -37,7 +37,7 @@ func NewCmdBuildLogs(fullName string, f *clientcmd.Factory, out io.Writer) *cobr
 		Short:      "Show logs from a build",
 		Long:       buildLogsLong,
 		Example:    fmt.Sprintf(buildLogsExample, fullName),
-		Deprecated: fmt.Sprintf("use %q instead.", LogsRecommendedName),
+		Deprecated: fmt.Sprintf("use \"oc %v build/<build-name>\" instead.", LogsRecommendedName),
 		Run: func(cmd *cobra.Command, args []string) {
 			err := RunBuildLogs(fullName, f, out, cmd, opts, args)
 
