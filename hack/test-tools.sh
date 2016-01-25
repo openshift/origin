9b5aaaadf6e2dd6fd536250ded91f0c82b2fc502
Steve Kuznetsov <skuznets@redhat.com>
2016-01-25 11:02:06 -0500

    migrated test utilities

--- hack/test-tools.sh
+++ hack/test-tools.sh
@@ -10,12 +10,13 @@ STARTTIME=$(date +%s)
 OS_ROOT=$(dirname "${BASH_SOURCE}")/..
 cd "${OS_ROOT}"
 source "${OS_ROOT}/hack/util.sh"
-source "${OS_ROOT}/hack/cmd_util.sh"
+source "${OS_ROOT}/hack/lib/cmd.sh"
 source "${OS_ROOT}/hack/lib/util/trap.sh"
 source "${OS_ROOT}/hack/lib/log/stacktrace.sh"
 
 os::util::trap::init_err
 os::log::stacktrace::install
+
 os::cmd::expect_success 'tools/junitreport/test/integration.sh'
 
 echo "test-tools: ok"
