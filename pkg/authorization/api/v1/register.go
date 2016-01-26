f78cb5d25cd7bc7cfde5e679d3c248299cad3be3
Maciej Szulik <maszulik@redhat.com>
2016-01-26 04:33:25 -0500

    API registration

--- pkg/authorization/api/v1/register.go
+++ pkg/authorization/api/v1/register.go
@@ -2,10 +2,18 @@ package v1
 
 import (
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/runtime"
 )
 
+// SchemeGroupVersion is group version used to register these objects
+var SchemeGroupVersion = unversioned.GroupVersion{Group: "", Version: "v1"}
+
+// Codec encodes internal objects to the v1 scheme
+var Codec = runtime.CodecFor(api.Scheme, SchemeGroupVersion.String())
+
 func init() {
-	api.Scheme.AddKnownTypes("v1",
+	api.Scheme.AddKnownTypes(SchemeGroupVersion,
 		&Role{},
 		&RoleBinding{},
 		&Policy{},
--- pkg/image/api/v1/register.go
+++ pkg/image/api/v1/register.go
@@ -2,13 +2,21 @@ package v1
 
 import (
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/runtime"
 
 	_ "github.com/openshift/origin/pkg/image/api/docker10"
 	_ "github.com/openshift/origin/pkg/image/api/dockerpre012"
 )
 
+// SchemeGroupVersion is group version used to register these objects
+var SchemeGroupVersion = unversioned.GroupVersion{Group: "", Version: "v1"}
+
+// Codec encodes internal objects to the v1 scheme
+var Codec = runtime.CodecFor(api.Scheme, SchemeGroupVersion.String())
+
 func init() {
-	api.Scheme.AddKnownTypes("v1",
+	api.Scheme.AddKnownTypes(SchemeGroupVersion,
 		&Image{},
 		&ImageList{},
 		&ImageStream{},
