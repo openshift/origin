6748f0613e402257d2293e76e2d1bc8985105e24
Clayton Coleman <ccoleman@redhat.com>
2016-01-27 01:07:25 -0500

    Make image stream import level driven
    If an identical import request is made, and no update is necessary,
    the stream is not updated.

--- pkg/image/registry/imagestreamimport/rest.go
+++ pkg/image/registry/imagestreamimport/rest.go
@@ -143,80 +143,45 @@ func (r *REST) Create(ctx kapi.Context, obj runtime.Object) (runtime.Object, err
 		stream.Annotations = make(map[string]string)
 	}
 	now := unversioned.Now()
-	stream.Annotations[api.DockerImageRepositoryCheckAnnotation] = now.UTC().Format(time.RFC3339)
-	gen := stream.Generation + 1
-	zero := int64(0)
+	_, hasAnnotation := stream.Annotations[api.DockerImageRepositoryCheckAnnotation]
+	nextGeneration := stream.Generation + 1
 
+	original, err := kapi.Scheme.DeepCopy(stream)
+	if err != nil {
+		return nil, err
+	}
+
+	// walk the retrieved images, ensuring each one exists in etcd
 	importedImages := make(map[string]error)
 	updatedImages := make(map[string]*api.Image)
-	if spec := isi.Spec.Repository; spec != nil {
-		for i, imageStatus := range isi.Status.Repository.Images {
-			image := imageStatus.Image
-			if image == nil {
-				continue
-			}
 
-			// update the spec tag
+	if spec := isi.Spec.Repository; spec != nil {
+		for i, status := range isi.Status.Repository.Images {
+			image := status.Image
 			ref, err := api.ParseDockerImageReference(image.DockerImageReference)
 			if err != nil {
-				// ???
+				util.HandleError(fmt.Errorf("unable to parse image reference during import: %v", err))
 				continue
 			}
+			from, err := api.ParseDockerImageReference(spec.From.Name)
+			if err != nil {
+				util.HandleError(fmt.Errorf("unable to parse from reference during import: %v", err))
+				continue
+			}
+
 			tag := ref.Tag
-			if len(imageStatus.Tag) > 0 {
-				tag = imageStatus.Tag
+			if len(status.Tag) > 0 {
+				tag = status.Tag
 			}
-			if _, ok := stream.Spec.Tags[tag]; !ok {
-				if stream.Spec.Tags == nil {
-					stream.Spec.Tags = make(map[string]api.TagReference)
-				}
-				stream.Spec.Tags[tag] = api.TagReference{
-					From: &kapi.ObjectReference{
-						Kind: "DockerImage",
-						Name: image.DockerImageReference,
-					},
-					Generation:   &gen,
-					ImportPolicy: api.TagImportPolicy{Insecure: spec.ImportPolicy.Insecure},
-				}
-			}
+			// we've imported a set of tags, ensure spec tag will point to this for later imports
+			from.ID, from.Tag = "", tag
 
-			// import or reuse the image
-			importErr, imported := importedImages[image.Name]
-			if importErr != nil {
-				api.SetTagConditions(stream, tag, newImportFailedCondition(err, gen, now))
-			}
-
-			pullSpec, _ := api.MostAccuratePullSpec(image.DockerImageReference, image.Name, "")
-			api.AddTagEventToImageStream(stream, tag, api.TagEvent{
-				Created:              now,
-				DockerImageReference: pullSpec,
-				Image:                image.Name,
-				Generation:           gen,
-			})
-
-			if imported {
-				if updatedImage, ok := updatedImages[image.Name]; ok {
-					isi.Status.Repository.Images[i].Image = updatedImage
-				}
+			if checkImportFailure(status, stream, tag, nextGeneration, now) {
 				continue
 			}
 
-			// establish the image into the store
-			updated, err := r.images.Create(ctx, image)
-			switch {
-			case kapierrors.IsAlreadyExists(err):
-				if err := api.ImageWithMetadata(image); err != nil {
-					glog.V(4).Infof("Unable to update image metadata during image import when image already exists %q: err", image.Name, err)
-				}
-				updated = image
-				fallthrough
-			case err == nil:
-				updatedImage := updated.(*api.Image)
-				updatedImages[image.Name] = updatedImage
-				isi.Status.Repository.Images[i].Image = updatedImage
-				importedImages[image.Name] = nil
-			default:
-				importedImages[image.Name] = err
+			if updated, ok := r.importSuccessful(ctx, image, stream, tag, from.Exact(), nextGeneration, now, spec.ImportPolicy, importedImages, updatedImages); ok {
+				isi.Status.Repository.Images[i].Image = updated
 			}
 		}
 	}
@@ -227,56 +192,17 @@ func (r *REST) Create(ctx kapi.Context, obj runtime.Object) (runtime.Object, err
 		}
 		tag := spec.To.Name
 
-		if stream.Spec.Tags == nil {
-			stream.Spec.Tags = make(map[string]api.TagReference)
-		}
-		specTag := stream.Spec.Tags[tag]
-		from := spec.From
-		specTag.From = &from
-		specTag.Generation = &zero
-		specTag.ImportPolicy.Insecure = spec.ImportPolicy.Insecure
-		stream.Spec.Tags[tag] = specTag
-
+		// record a failure condition
 		status := isi.Status.Images[i]
-		if status.Image == nil || status.Status.Status == unversioned.StatusFailure {
-			message := status.Status.Message
-			if len(message) == 0 {
-				message = "unknown error prevented import"
-			}
-			api.SetTagConditions(stream, tag, api.TagEventCondition{
-				Type:       api.ImportSuccess,
-				Status:     kapi.ConditionFalse,
-				Message:    message,
-				Reason:     string(status.Status.Reason),
-				Generation: gen,
-
-				LastTransitionTime: now,
-			})
+		if checkImportFailure(status, stream, tag, nextGeneration, now) {
 			continue
 		}
+
+		// record success
 		image := status.Image
-
-		importErr, imported := importedImages[image.Name]
-		if importErr != nil {
-			api.SetTagConditions(stream, tag, newImportFailedCondition(err, gen, now))
+		if updated, ok := r.importSuccessful(ctx, image, stream, tag, spec.From.Name, nextGeneration, now, spec.ImportPolicy, importedImages, updatedImages); ok {
+			isi.Status.Images[i].Image = updated
 		}
-
-		pullSpec, _ := api.MostAccuratePullSpec(image.DockerImageReference, image.Name, "")
-		api.AddTagEventToImageStream(stream, tag, api.TagEvent{
-			Created:              now,
-			DockerImageReference: pullSpec,
-			Image:                image.Name,
-			Generation:           gen,
-		})
-
-		if imported {
-			continue
-		}
-		_, err = r.images.Create(ctx, image)
-		if kapierrors.IsAlreadyExists(err) {
-			err = nil
-		}
-		importedImages[image.Name] = err
 	}
 
 	// TODO: should we allow partial failure?
@@ -288,16 +214,123 @@ func (r *REST) Create(ctx kapi.Context, obj runtime.Object) (runtime.Object, err
 
 	clearManifests(isi)
 
+	hasChanges := !kapi.Semantic.DeepEqual(original, stream)
 	if create {
+		stream.Annotations[api.DockerImageRepositoryCheckAnnotation] = now.UTC().Format(time.RFC3339)
+		glog.V(4).Infof("create new stream: %#v", stream)
 		obj, err = r.internalStreams.Create(ctx, stream)
 	} else {
-		obj, _, err = r.internalStreams.Update(ctx, stream)
+		if hasAnnotation && !hasChanges {
+			glog.V(4).Infof("stream did not change: %#v", stream)
+			obj, err = original.(*api.ImageStream), nil
+		} else {
+			glog.V(4).Infof("updated stream %s", util.ObjectDiff(original, stream))
+			stream.Annotations[api.DockerImageRepositoryCheckAnnotation] = now.UTC().Format(time.RFC3339)
+			obj, _, err = r.internalStreams.Update(ctx, stream)
+		}
 	}
+
 	if err != nil {
 		return nil, err
 	}
 	isi.Status.Import = obj.(*api.ImageStream)
 	return isi, nil
+}
+
+func checkImportFailure(status api.ImageImportStatus, stream *api.ImageStream, tag string, nextGeneration int64, now unversioned.Time) bool {
+	if status.Image != nil && status.Status.Status == unversioned.StatusSuccess {
+		return false
+	}
+	message := status.Status.Message
+	if len(message) == 0 {
+		message = "unknown error prevented import"
+	}
+	condition := api.TagEventCondition{
+		Type:       api.ImportSuccess,
+		Status:     kapi.ConditionFalse,
+		Message:    message,
+		Reason:     string(status.Status.Reason),
+		Generation: nextGeneration,
+
+		LastTransitionTime: now,
+	}
+	if !api.HasTagCondition(stream, tag, condition) {
+		api.SetTagConditions(stream, tag, condition)
+		if tagRef, ok := stream.Spec.Tags[tag]; ok {
+			zero := int64(0)
+			tagRef.Generation = &zero
+			stream.Spec.Tags[tag] = tagRef
+		}
+	}
+	return true
+}
+
+func (r *REST) importSuccessful(
+	ctx kapi.Context,
+	image *api.Image, stream *api.ImageStream, tag string, from string, nextGeneration int64, now unversioned.Time, importPolicy api.TagImportPolicy,
+	importedImages map[string]error, updatedImages map[string]*api.Image,
+) (*api.Image, bool) {
+
+	pullSpec, _ := api.MostAccuratePullSpec(image.DockerImageReference, image.Name, "")
+	tagEvent := api.TagEvent{
+		Created:              now,
+		DockerImageReference: pullSpec,
+		Image:                image.Name,
+		Generation:           nextGeneration,
+	}
+
+	if stream.Spec.Tags == nil {
+		stream.Spec.Tags = make(map[string]api.TagReference)
+	}
+
+	// ensure the spec and status tag match the imported image
+	changed := api.DifferentTagEvent(stream, tag, tagEvent)
+	specTag, ok := stream.Spec.Tags[tag]
+	if changed || !ok {
+		specTag.From = &kapi.ObjectReference{
+			Kind: "DockerImage",
+			Name: from,
+		}
+		zero := int64(0)
+		specTag.Generation = &zero
+		api.AddTagEventToImageStream(stream, tag, tagEvent)
+	}
+	// always reset the import policy
+	specTag.ImportPolicy = importPolicy
+	stream.Spec.Tags[tag] = specTag
+
+	// import or reuse the image
+	importErr, alreadyImported := importedImages[image.Name]
+	if importErr != nil {
+		api.SetTagConditions(stream, tag, newImportFailedCondition(importErr, nextGeneration, now))
+	}
+
+	// create the image if it does not exist, otherwise cache the updated status from the store for use by other tags
+	if alreadyImported {
+		if updatedImage, ok := updatedImages[image.Name]; ok {
+			return updatedImage, true
+		}
+		return nil, false
+	}
+
+	updated, err := r.images.Create(ctx, image)
+	switch {
+	case kapierrors.IsAlreadyExists(err):
+		if err := api.ImageWithMetadata(image); err != nil {
+			glog.V(4).Infof("Unable to update image metadata during image import when image already exists %q: err", image.Name, err)
+		}
+		updated = image
+		fallthrough
+	case err == nil:
+		updatedImage := updated.(*api.Image)
+		updatedImages[image.Name] = updatedImage
+		//isi.Status.Repository.Images[i].Image = updatedImage
+		importedImages[image.Name] = nil
+		return updatedImage, true
+	default:
+		importedImages[image.Name] = err
+	}
+	return nil, false
 }
 
 // clearManifests unsets the manifest for each object that does not request it
