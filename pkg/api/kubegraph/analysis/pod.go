57aab46daaf03ac5bf31ae0f0fb3e907add33a16
Clayton Coleman <ccoleman@redhat.com>
2016-01-27 22:47:47 -0500

    Add a crashlooping error to oc status and a suggestion
    Make the suggestion vary based on whether the container is root or
    non-root.

--- pkg/api/kubegraph/analysis/pod.go
+++ pkg/api/kubegraph/analysis/pod.go
@@ -2,18 +2,27 @@ package analysis
 
 import (
 	"fmt"
+	"time"
+
+	. "github.com/MakeNowJust/heredoc/dot"
 
 	kapi "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
 
 	osgraph "github.com/openshift/origin/pkg/api/graph"
 	kubegraph "github.com/openshift/origin/pkg/api/kubegraph/nodes"
 )
 
 const (
+	CrashLoopingPodError = "CrashLoopingPod"
 	RestartingPodWarning = "RestartingPod"
 
-	RestartThreshold = 3
+	RestartThreshold      = 5
+	RestartRecentDuration = 10 * time.Minute
 )
+
+// exposed for testing
+var nowFn = unversioned.Now
 
 // FindRestartingPods inspects all Pods to see if they've restarted more than the threshold
 func FindRestartingPods(g osgraph.Graph, f osgraph.Namer) []osgraph.Marker {
@@ -27,7 +36,47 @@ func FindRestartingPods(g osgraph.Graph, f osgraph.Namer) []osgraph.Marker {
 		}
 
 		for _, containerStatus := range pod.Status.ContainerStatuses {
-			if containerStatus.RestartCount >= RestartThreshold {
+			switch {
+			case containerCrashLoopBackOff(containerStatus):
+				var suggestion string
+				switch {
+				case containerIsNonRoot(pod, containerStatus.Name):
+					suggestion = D(`
+						The container is starting and exiting repeatedly, which usually means the container cannot
+						start, is misconfigured, or is unable to perform an action due to security restrictions on
+						the container. The container logs may contain messages indicating the reason the pod cannot
+						start.
+
+						This container is being run as as a non-root user due to administrative policy, and
+						some images may fail expecting to be able to change ownership or set permissions on
+						directories. Your administrator may need to grant permission for you to run root
+						containers.`)
+				default:
+					suggestion = D(`
+						The container is starting and exiting repeatedly, which usually means the container cannot
+						start, is misconfigured, or is unable to perform an action due to security restrictions on
+						the container. The container logs may contain messages indicating the reason the pod cannot
+						start.`)
+				}
+				markers = append(markers, osgraph.Marker{
+					Node: podNode,
+
+					Severity: osgraph.ErrorSeverity,
+					Key:      CrashLoopingPodError,
+					Message: fmt.Sprintf("container %q in %s is crash-looping", containerStatus.Name,
+						f.ResourceName(podNode)),
+					Suggestion: osgraph.Suggestion(suggestion),
+				})
+			case containerRestartedRecently(containerStatus, nowFn()):
+				markers = append(markers, osgraph.Marker{
+					Node: podNode,
+
+					Severity: osgraph.WarningSeverity,
+					Key:      RestartingPodWarning,
+					Message: fmt.Sprintf("container %q in %s has restarted within the last 10 minutes", containerStatus.Name,
+						f.ResourceName(podNode)),
+				})
+			case containerRestartedFrequently(containerStatus):
 				markers = append(markers, osgraph.Marker{
 					Node: podNode,
 
@@ -41,4 +90,36 @@ func FindRestartingPods(g osgraph.Graph, f osgraph.Namer) []osgraph.Marker {
 	}
 
 	return markers
+}
+
+func containerIsNonRoot(pod *kapi.Pod, container string) bool {
+	for _, c := range pod.Spec.Containers {
+		if c.Name != container || c.SecurityContext == nil {
+			continue
+		}
+		switch {
+		case c.SecurityContext.RunAsUser != nil && *c.SecurityContext.RunAsUser != 0:
+			//c.SecurityContext.RunAsNonRoot != nil && *c.SecurityContext.RunAsNonRoot,
+			return true
+		}
+	}
+	return false
+}
+
+func containerCrashLoopBackOff(status kapi.ContainerStatus) bool {
+	return status.State.Waiting != nil && status.State.Waiting.Reason == "CrashLoopBackOff"
+}
+
+func containerRestartedRecently(status kapi.ContainerStatus, now unversioned.Time) bool {
+	if status.RestartCount == 0 {
+		return false
+	}
+	if status.LastTerminationState.Terminated != nil && now.Sub(status.LastTerminationState.Terminated.FinishedAt.Time) < RestartRecentDuration {
+		return true
+	}
+	return false
+}
+
+func containerRestartedFrequently(status kapi.ContainerStatus) bool {
+	return status.RestartCount > RestartThreshold
 }
--- pkg/api/kubegraph/analysis/pod_test.go
+++ pkg/api/kubegraph/analysis/pod_test.go
@@ -2,6 +2,9 @@ package analysis
 
 import (
 	"testing"
+	"time"
+
+	"k8s.io/kubernetes/pkg/api/unversioned"
 
 	osgraph "github.com/openshift/origin/pkg/api/graph"
 	osgraphtest "github.com/openshift/origin/pkg/api/graph/test"
@@ -12,12 +15,34 @@ func TestRestartingPodWarning(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+	defer func() { nowFn = unversioned.Now }()
 
+	recent, _ := time.Parse(time.RFC3339, "2015-07-13T19:36:06Z")
+	nowFn = func() unversioned.Time { return unversioned.NewTime(recent.UTC()) }
 	markers := FindRestartingPods(g, osgraph.DefaultNamer)
-	if e, a := 1, len(markers); e != a {
+	if e, a := 3, len(markers); e != a {
 		t.Fatalf("expected %v, got %v", e, a)
 	}
 	if e, a := RestartingPodWarning, markers[0].Key; e != a {
+		t.Fatalf("expected %v, got %v", e, a)
+	}
+	if e, a := CrashLoopingPodError, markers[1].Key; e != a {
+		t.Fatalf("expected %v, got %v", e, a)
+	}
+	if e, a := RestartingPodWarning, markers[2].Key; e != a {
+		t.Fatalf("expected %v, got %v", e, a)
+	}
+
+	future, _ := time.Parse(time.RFC3339, "2015-07-13T19:46:06Z")
+	nowFn = func() unversioned.Time { return unversioned.NewTime(future.UTC()) }
+	markers = FindRestartingPods(g, osgraph.DefaultNamer)
+	if e, a := 2, len(markers); e != a {
+		t.Fatalf("expected %v, got %v", e, a)
+	}
+	if e, a := CrashLoopingPodError, markers[0].Key; e != a {
+		t.Fatalf("expected %v, got %v", e, a)
+	}
+	if e, a := RestartingPodWarning, markers[1].Key; e != a {
 		t.Fatalf("expected %v, got %v", e, a)
 	}
 }
