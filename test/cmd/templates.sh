571061864ef608d70e994e10dc4d2645f2544586
Clayton Coleman <ccoleman@redhat.com>
2016-02-05 17:40:03 -0500

    Template test is not reentrant

--- test/cmd/templates.sh
+++ test/cmd/templates.sh
@@ -13,6 +13,9 @@ os::log::install_errexit
 (
   set +e
   oc delete all,templates --all
+  oc delete template/ruby-helloworld-sample -n openshift
+  oc delete project test-template-project
+  os::cmd::try_until_failure 'oc get project test-template-project'
   exit 0
 ) &>/dev/null
 
@@ -76,6 +79,9 @@ echo "template data precision: ok"
 
 os::cmd::expect_success 'oc create -f examples/sample-app/application-template-dockerbuild.json -n openshift'
 os::cmd::expect_success 'oc policy add-role-to-user admin test-user'
+new="$(mktemp -d)/tempconfig"
+os::cmd::expect_success "oc config view --raw > $new"
+export KUBECONFIG=$new
 os::cmd::expect_success 'oc login -u test-user -p password'
 os::cmd::expect_success 'oc new-project test-template-project'
 # make sure the permissions on the new project are set up
