dc274c4dd5b6766f92e312c51c1ab306d2991fce
Jordan Liggitt <jliggitt@redhat.com>
2016-02-08 10:56:29 -0500

    GitLab IDP tweaks

--- pkg/auth/oauth/external/gitlab/gitlab.go
+++ pkg/auth/oauth/external/gitlab/gitlab.go
@@ -23,16 +23,16 @@ const (
 	// Requires GitLab 7.7.0 or higher
 	gitlabAuthorizePath = "/oauth/authorize"
 	gitlabTokenPath     = "/oauth/token"
-	gitlabUserApiPath   = "/api/v3/user"
+	gitlabUserAPIPath   = "/api/v3/user"
 	gitlabOAuthScope    = "api"
 )
 
 type provider struct {
 	providerName string
 	transport    http.RoundTripper
-	authorizeUrl string
-	tokenUrl     string
-	userApiUrl   string
+	authorizeURL string
+	tokenURL     string
+	userAPIURL   string
 	clientID     string
 	clientSecret string
 }
@@ -50,44 +50,48 @@ func NewProvider(providerName string, transport http.RoundTripper, URL, clientID
 	if err != nil {
 		return nil, errors.New("Host URL is invalid")
 	}
-	authorizePath := path.Join(u.Path, gitlabAuthorizePath)
-	tokenPath := path.Join(u.Path, gitlabTokenPath)
-	userApiPath := path.Join(u.Path, gitlabUserApiPath)
-	u.Path = authorizePath
-	authorizeUrl := u.String()
-	u.Path = tokenPath
-	tokenUrl := u.String()
-	u.Path = userApiPath
-	userApiUrl := u.String()
 
-	return provider{providerName, transport, authorizeUrl, tokenUrl, userApiUrl, clientID, clientSecret}, nil
+	return &provider{
+		providerName: providerName,
+		transport:    transport,
+		authorizeURL: appendPath(*u, gitlabAuthorizePath),
+		tokenURL:     appendPath(*u, gitlabTokenPath),
+		userAPIURL:   appendPath(*u, gitlabUserAPIPath),
+		clientID:     clientID,
+		clientSecret: clientSecret,
+	}, nil
 }
 
-func (p provider) GetTransport() (http.RoundTripper, error) {
+func appendPath(u url.URL, subpath string) string {
+	u.Path = path.Join(u.Path, subpath)
+	return u.String()
+}
+
+func (p *provider) GetTransport() (http.RoundTripper, error) {
 	return p.transport, nil
 }
 
 // NewConfig implements external/interfaces/Provider.NewConfig
-func (p provider) NewConfig() (*osincli.ClientConfig, error) {
+func (p *provider) NewConfig() (*osincli.ClientConfig, error) {
 	config := &osincli.ClientConfig{
 		ClientId:                 p.clientID,
 		ClientSecret:             p.clientSecret,
 		ErrorsInStatusCode:       true,
 		SendClientSecretInParams: true,
-		AuthorizeUrl:             p.authorizeUrl,
-		TokenUrl:                 p.tokenUrl,
+		AuthorizeUrl:             p.authorizeURL,
+		TokenUrl:                 p.tokenURL,
 		Scope:                    gitlabOAuthScope,
 	}
 	return config, nil
 }
 
 // AddCustomParameters implements external/interfaces/Provider.AddCustomParameters
-func (p provider) AddCustomParameters(req *osincli.AuthorizeRequest) {
+func (p *provider) AddCustomParameters(req *osincli.AuthorizeRequest) {
 }
 
 // GetUserIdentity implements external/interfaces/Provider.GetUserIdentity
-func (p provider) GetUserIdentity(data *osincli.AccessData) (authapi.UserIdentityInfo, bool, error) {
-	req, _ := http.NewRequest("GET", p.userApiUrl, nil)
+func (p *provider) GetUserIdentity(data *osincli.AccessData) (authapi.UserIdentityInfo, bool, error) {
+	req, _ := http.NewRequest("GET", p.userAPIURL, nil)
 	req.Header.Set("Authorization", fmt.Sprintf("bearer %s", data.AccessToken))
 
 	res, err := http.DefaultClient.Do(req)
--- pkg/auth/oauth/external/gitlab/gitlab_test.go
+++ pkg/auth/oauth/external/gitlab/gitlab_test.go
@@ -1,6 +1,7 @@
 package gitlab
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/openshift/origin/pkg/auth/oauth/external"
@@ -12,4 +13,16 @@ func TestGitLab(t *testing.T) {
 		t.Fatalf("Unexpected error: %v", err)
 	}
 	_ = external.Provider(p)
+
+	expectedProvider := &provider{
+		providerName: "gitlab",
+		authorizeURL: "https://gitlab.com/oauth/authorize",
+		tokenURL:     "https://gitlab.com/oauth/token",
+		userAPIURL:   "https://gitlab.com/api/v3/user",
+		clientID:     "clientid",
+		clientSecret: "clientsecret",
+	}
+	if !reflect.DeepEqual(p, expectedProvider) {
+		t.Fatalf("Expected\n%#v\ngot\n%#v", expectedProvider, p)
+	}
 }
