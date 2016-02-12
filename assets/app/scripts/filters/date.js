21d498d1642a98250ca3ac745dc190ac6af0c80c
Samuel Padgett <spadgett@redhat.com>
2016-02-12 13:49:35 -0500

    Improve performance of overview page with many deployments

--- assets/app/scripts/filters/date.js
+++ assets/app/scripts/filters/date.js
@@ -66,13 +66,31 @@ angular.module('openshiftConsole')
   .filter('orderObjectsByDate', function(toArrayFilter) {
     return function(items, reverse) {
       items = toArrayFilter(items);
+
+      /*
+       * Note: This is a hotspot in our code. We sort frequently by date on
+       *       the overview and browse pages.
+       */
+
       items.sort(function (a, b) {
         if (!a.metadata || !a.metadata.creationTimestamp || !b.metadata || !b.metadata.creationTimestamp) {
           throw "orderObjectsByDate expects all objects to have the field metadata.creationTimestamp";
         }
-        var diff = moment(a.metadata.creationTimestamp).diff(moment(b.metadata.creationTimestamp));
-        return reverse ? diff * -1 : diff;
+
+        // The date format can be sorted using straight string comparison.
+        // Compare as strings for performance.
+        // Example Date: 2016-02-02T21:53:07Z
+        if (a.metadata.creationTimestamp < b.metadata.creationTimestamp) {
+          return reverse ? 1 : -1;
+        }
+
+        if (a.metadata.creationTimestamp > b.metadata.creationTimestamp) {
+          return reverse ? -1 : 1;
+        }
+
+        return 0;
       });
+
       return items;
     };
   });
