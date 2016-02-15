bd56c709ebe1cc858dacc8ab6a45635d617e4911
Gladkov Alexey <agladkov@redhat.com>
2016-02-15 13:57:18 +0100

    Fix admission attribute comparison

--- pkg/security/admission/admission.go
+++ pkg/security/admission/admission.go
@@ -95,7 +95,7 @@ func (a *constraint) Stop() {
 //     with the validated SCC.  If we don't find any reject the pod and give all errors from the
 //     failed attempts.
 func (c *constraint) Admit(a kadmission.Attributes) error {
-	if a.GetResource().Resource != string(kapi.ResourcePods) {
+	if a.GetResource() != kapi.Resource("pods") {
 		return nil
 	}
 
