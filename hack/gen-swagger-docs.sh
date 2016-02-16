8378004eb39f053905c45e31604b6ab14b4f9024
Steve Kuznetsov <skuznets@redhat.com>
2016-02-16 08:29:48 -0500

    added automatic swagger doc generator

--- hack/gen-swagger-docs.sh
+++ hack/gen-swagger-docs.sh
@@ -7,10 +7,13 @@ set -o nounset
 set -o pipefail
 
 OS_ROOT=$(dirname "${BASH_SOURCE}")/..
+cd "${OS_ROOT}"
 source "${OS_ROOT}/hack/util.sh"
+source "${OS_ROOT}/hack/common.sh"
+os::log::install_errexit
 
 pushd "${OS_ROOT}/hack/swagger-doc" > /dev/null
 gradle gendocs --info
 popd > /dev/null
 
-echo "SUCCESS"
+echo "[INFO] Swagger doc generation successful"
