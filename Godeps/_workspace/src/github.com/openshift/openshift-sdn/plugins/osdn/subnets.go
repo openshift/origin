1e80d9be65a4dbe663b145e7066e9ba63ca8cfe7
Dan Winship <danw@redhat.com>
2016-02-23 11:09:04 -0500

    bump(github.com/openshift/openshift-sdn): 08a79d5adc8af21b14adcc0b9650df2d5fccf2f0

--- Godeps/_workspace/src/github.com/openshift/openshift-sdn/plugins/osdn/subnets.go
+++ Godeps/_workspace/src/github.com/openshift/openshift-sdn/plugins/osdn/subnets.go
@@ -9,8 +9,6 @@ import (
 
 	"github.com/openshift/openshift-sdn/pkg/netutils"
 	"github.com/openshift/openshift-sdn/plugins/osdn/api"
-	kapi "k8s.io/kubernetes/pkg/api"
-	kubetypes "k8s.io/kubernetes/pkg/kubelet/container"
 )
 
 func (oc *OvsController) SubnetStartMaster(clusterNetworkCIDR string, clusterBitsPerSubnet uint, serviceNetworkCIDR string) error {
@@ -98,26 +96,26 @@ func (oc *OvsController) deleteNode(nodeName string) error {
 	return oc.Registry.DeleteSubnet(nodeName)
 }
 
-func (oc *OvsController) SubnetStartNode(mtu uint) error {
+func (oc *OvsController) SubnetStartNode(mtu uint) (bool, error) {
 	err := oc.initSelfSubnet()
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	// Assume we are working with IPv4
 	clusterNetworkCIDR, err := oc.Registry.GetClusterNetworkCIDR()
 	if err != nil {
 		log.Errorf("Failed to obtain ClusterNetwork: %v", err)
-		return err
+		return false, err
 	}
 	servicesNetworkCIDR, err := oc.Registry.GetServicesNetworkCIDR()
 	if err != nil {
 		log.Errorf("Failed to obtain ServicesNetwork: %v", err)
-		return err
+		return false, err
 	}
 	networkChanged, err := oc.flowController.Setup(oc.localSubnet.SubnetCIDR, clusterNetworkCIDR, servicesNetworkCIDR, mtu)
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	getSubnets := func(registry *Registry) (interface{}, string, error) {
@@ -125,27 +123,14 @@ func (oc *OvsController) SubnetStartNode(mtu uint) error {
 	}
 	result, err := oc.watchAndGetResource("HostSubnet", watchSubnets, getSubnets)
 	if err != nil {
-		return err
+		return false, err
 	}
 	subnets := result.([]api.Subnet)
 	for _, s := range subnets {
 		oc.flowController.AddOFRules(s.NodeIP, s.SubnetCIDR, oc.localIP)
 	}
 
-	if networkChanged {
-		pods, err := oc.Registry.GetRunningPods(oc.hostName, kapi.NamespaceAll)
-		if err != nil {
-			return err
-		}
-		for _, p := range pods {
-			err = oc.pluginHooks.UpdatePod(p.Namespace, p.Name, kubetypes.DockerID(p.ContainerID))
-			if err != nil {
-				log.Warningf("Could not update pod %q (%s): %s", p.Name, p.ContainerID, err)
-			}
-		}
-	}
-
-	return nil
+	return networkChanged, nil
 }
 
 func (oc *OvsController) initSelfSubnet() error {
