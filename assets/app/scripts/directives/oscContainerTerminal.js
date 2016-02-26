9c73ec8f73c132d77e5d6090f18f69ec88182baa
benjaminapetersen <admin@benjaminapetersen.me>
2016-02-26 12:06:58 -0500

    squash

--- assets/app/scripts/directives/oscContainerTerminal.js
+++ assets/app/scripts/directives/oscContainerTerminal.js
@@ -25,25 +25,23 @@ angular
       link: function($scope, $elem) {
         // to test for # of cols
         var proxyDOM = $elem[0].find('.terminal-wrap');
+        // CSS kubernetes-container-terminal .terminal sets font-size to 10px.
+        // At this size, each character takes up roughly 6.5px space.
+        // Therefore, once we get the width of the .terminal-wrap node, we can
+        // calculate the # of cols we can generate to roughly fill the parent
+        // node at render time.
+        var COL_PIX = 6.5;
 
-        $scope.canRender = false;
-
-        if(!$scope.rows) {
-          $scope.rows = 24;
-        }
-        if(!$scope.screenKeys) {
-          $scope.screenKeys = true;
-        }
+        $scope.rows = ('rows' in $scope) ? $scope.rows : 24;
+        $scope.screenKeys = ('screenKeys' in $scope) ? $scope.screenKeys : true;
 
         $scope.$watch('prevent', function(prevent) {
           var maxWidth;
-          if(!prevent) {
+          if(!prevent && !$scope.cols) {
             $timeout(function() {
-              if(!$scope.cols) {
-                maxWidth = Math.floor(proxyDOM.clientWidth/6);
-                $scope.cols = (maxWidth <= 80) ? maxWidth : 80;
-                  $scope.canRender = true;
-                }
+              maxWidth = Math.floor(proxyDOM.clientWidth/COL_PIX);
+              // once cols is set the kubernetes-container-terminal will render.
+              $scope.cols = (maxWidth <= 80) ? maxWidth : 80;
             },1);
           }
         });
