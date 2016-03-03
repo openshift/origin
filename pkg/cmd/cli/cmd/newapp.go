0fd621f4081c1c4a0d2c1fe23d0a2c5d40406b56
Cesar Wong <cewong@redhat.com>
2016-03-03 14:53:15 -0500

    oc new-app/new-build: handle the case when an imagestream matches but has no tags

--- pkg/cmd/cli/cmd/newapp.go
+++ pkg/cmd/cli/cmd/newapp.go
@@ -162,6 +162,7 @@ func NewCmdNewApplication(fullName string, f *clientcmd.Factory, out io.Writer) 
 	cmd.Flags().BoolVarP(&config.AsList, "list", "L", false, "List all local templates and image streams that can be used to create.")
 	cmd.Flags().BoolVarP(&config.AsSearch, "search", "S", false, "Search all templates, image streams, and Docker images that match the arguments provided.")
 	cmd.Flags().BoolVar(&config.AllowMissingImages, "allow-missing-images", false, "If true, indicates that referenced Docker images that cannot be found locally or in a registry should still be used.")
+	cmd.Flags().BoolVar(&config.AllowMissingImageStreamTags, "allow-missing-imagestream-tags", false, "If true, indicates that image stream tags that don't exist should still be used.")
 	cmd.Flags().BoolVar(&config.AllowSecretUse, "grant-install-rights", false, "If true, a component that requires access to your account may use your token to install software into your project. Only grant images you trust the right to run with your token.")
 	cmd.Flags().BoolVar(&config.SkipGeneration, "no-install", false, "Do not attempt to run images that describe themselves as being installable")
 	cmd.Flags().BoolVar(&config.DryRun, "dry-run", false, "If true, do not actually create resources.")
@@ -680,6 +681,20 @@ func transformError(err error, c *cobra.Command, fullName string, groups errorGr
 					The argument %[1]q only partially matched the following Docker image or OpenShift image stream:
 
 					%[2]s`, t.Value, buf.String(),
+			),
+			t,
+			t.Errs...,
+		)
+		return
+	case newapp.ErrNoTagsFound:
+		buf := &bytes.Buffer{}
+		fmt.Fprintf(buf, "  Use --allow-missing-imagestream-tags to use this image stream\n\n")
+		groups.Add(
+			"no-tags",
+			Df(`
+					The image stream %[1]q exists, but it has no tags.
+
+					%[2]s`, t.Match.Name, buf.String(),
 			),
 			t,
 			t.Errs...,
