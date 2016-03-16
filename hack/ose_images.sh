7b72d65e8fd328e3c9f14edd8204388a3acaab11
Troy Dawson <tdawson@redhat.com>
2016-03-16 08:05:43 -0500

    Update ose build scripts

--- hack/ose_images.sh
+++ hack/ose_images.sh
@@ -16,8 +16,10 @@
 #   rhpkg
 #   krb5-workstation
 #   git
+## COMMON VARIABLES ##
+source ose.conf
 
-BASE_GIT_REPO="git@github.com:openshift/ose.git"
+## LOCAL VARIABLES ##
 DIST_GIT_BRANCH="rhaos-3.2-rhel-7"
 #DIST_GIT_BRANCH="rhaos-3.1-rhel-7"
 #DIST_GIT_BRANCH="rhaos-3.2-rhel-7-candidate"
@@ -29,52 +31,12 @@ OSBS_REGISTRY=brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888
 #OSBS_REGISTRY=rcm-img-docker01.build.eng.bos.redhat.com:5001
 PUSH_REGISTRY=registry.qe.openshift.com
 
-packagelist=""
-declare -A packagekey
-# format:
-# dist-git_name	image_dependency git_repo git_path
-packagekey['aos-f5-router-docker']="aos-f5-router-docker openshift-enterprise-docker ${BASE_GIT_REPO} ose/images/router/f5 openshift3/ose-f5-router aep3_beta/aep-f5-router"
-packagekey['image-inspector-docker']="image-inspector-docker None None None openshift3/image-inspector aep3_beta/image-inspector"
-packagekey['logging-auth-proxy-docker']="logging-auth-proxy-docker None None None openshift3/logging-auth-proxy aep3_beta/logging-auth-proxy"
-packagekey['logging-deployment-docker']="logging-deployment-docker openshift-enterprise-docker None None openshift3/logging-deployment aep3_beta/logging-deployment"
-packagekey['logging-elasticsearch-docker']="logging-elasticsearch-docker None None None openshift3/logging-elasticsearch aep3_beta/logging-elasticsearch"
-packagekey['logging-fluentd-docker']="logging-fluentd-docker None None None openshift3/logging-fluentd aep3_beta/logging-fluentd"
-packagekey['logging-kibana-docker']="logging-kibana-docker None None None openshift3/logging-kibana aep3_beta/logging-kibana"
-packagekey['metrics-cassandra-docker']="metrics-cassandra-docker None https://github.com/openshift/origin-metrics.git origin-metrics/cassandra openshift3/metrics-cassandra aep3_beta/metrics-cassandra"
-packagekey['metrics-deployer-docker']="metrics-deployer-docker openshift-enterprise-docker https://github.com/openshift/origin-metrics.git origin-metrics/deployer openshift3/metrics-deployer aep3_beta/metrics-deployer"
-packagekey['metrics-hawkular-metrics-docker']="metrics-hawkular-metrics-docker None https://github.com/openshift/origin-metrics.git origin-metrics/hawkular-metrics openshift3/metrics-hawkular-metrics aep3_beta/metrics-hawkular-metrics"
-packagekey['metrics-heapster-docker']="metrics-heapster-docker None https://github.com/openshift/origin-metrics.git origin-metrics/heapster openshift3/metrics-heapster aep3_beta/metrics-heapster"
-packagekey['openshift-enterprise-base-docker']="openshift-enterprise-base-docker None ${BASE_GIT_REPO} ose/images/base none none"
-packagekey['openshift-enterprise-deployer-docker']="openshift-enterprise-deployer-docker openshift-enterprise-docker ${BASE_GIT_REPO} ose/images/deployer openshift3/ose-deployer aep3_beta/aep-deployer"
-packagekey['openshift-enterprise-docker']="openshift-enterprise-docker openshift-enterprise-base-docker ${BASE_GIT_REPO} ose/images/ose openshift3/ose aep3_beta/aep"
-packagekey['openshift-enterprise-docker-builder-docker']="openshift-enterprise-docker-builder-docker openshift-enterprise-docker ${BASE_GIT_REPO} ose/images/builder/docker/docker-builder openshift3/ose-docker-builder none"
-packagekey['openshift-enterprise-dockerregistry-docker']="openshift-enterprise-dockerregistry-docker openshift-enterprise-base-docker ${BASE_GIT_REPO} ose/images/dockerregistry openshift3/ose-docker-registry aep3_beta/aep-docker-registry"
-packagekey['openshift-enterprise-haproxy-router-base-docker']="openshift-enterprise-haproxy-router-base-docker openshift-enterprise-base-docker ${BASE_GIT_REPO} ose/images/router/haproxy-base none none"
-packagekey['openshift-enterprise-haproxy-router-docker']="openshift-enterprise-haproxy-router-docker openshift-enterprise-haproxy-router-base-docker ${BASE_GIT_REPO} ose/images/router/haproxy openshift3/ose-haproxy-router aep3_beta/aep-haproxy-router"
-packagekey['openshift-enterprise-keepalived-ipfailover-docker']="openshift-enterprise-keepalived-ipfailover-docker openshift-enterprise-base-docker ${BASE_GIT_REPO} ose/images/ipfailover/keepalived openshift3/ose-keepalived-ipfailover aep3_beta/aep-keepalived-ipfailover"
-packagekey['openshift-enterprise-node-docker']="openshift-enterprise-node-docker openshift-enterprise-docker ${BASE_GIT_REPO} ose/images/node openshift3/node aep3_beta/node"
-packagekey['openshift-enterprise-openvswitch-docker']="openshift-enterprise-openvswitch-docker None ${BASE_GIT_REPO} ose/images/openvswitch openshift3/openvswitch aep3_beta/openvswitch"
-packagekey['openshift-enterprise-pod-docker']="openshift-enterprise-pod-docker None ${BASE_GIT_REPO} ose/images/pod openshift3/ose-pod aep3_beta/aep-pod"
-packagekey['openshift-enterprise-recycler-docker']="openshift-enterprise-recycler-docker openshift-enterprise-base-docker ${BASE_GIT_REPO} ose/images/recycler openshift3/ose-recycler aep3_beta/aep-recycler"
-packagekey['openshift-enterprise-sti-builder-docker']="openshift-enterprise-sti-builder-docker openshift-enterprise-docker ${BASE_GIT_REPO} ose/images/builder/docker/sti-builder openshift3/ose-sti-builder none"
-packagekey['openshift-jenkins-docker']="openshift-jenkins-docker None https://github.com/openshift/mongodb mongodb/2.4 openshift3/jenkins-1-rhel7 none"
-packagekey['openshift-mongodb-docker']="openshift-mongodb-docker None https://github.com/openshift/mongodb mongodb/2.4 openshift3/mongodb-24-rhel7 none"
-packagekey['openshift-mysql-docker']="openshift-mysql-docker None https://github.com/openshift/mysql mysql/5.5 openshift3/mysql-55-rhel7 none"
-packagekey['openshift-postgresql-docker']="openshift-postgresql-docker None https://github.com/openshift/postgresql postgresql/9.2 openshift3/postgresql-92-rhel7 none"
-packagekey['openshift-sti-base-docker']="openshift-sti-base-docker None https://github.com/openshift/sti-base sti-base none none"
-packagekey['openshift-sti-nodejs-docker']="openshift-sti-nodejs-docker openshift-sti-base-docker https://github.com/openshift/sti-nodejs sti-nodejs/0.10 openshift3/nodejs-010-rhel7 none"
-packagekey['openshift-sti-perl-docker']="openshift-sti-perl-docker openshift-sti-base-docker https://github.com/openshift/sti-perl sti-perl/5.16 openshift3/perl-516-rhel7 none"
-packagekey['openshift-sti-php-docker']="openshift-sti-php-docker openshift-sti-base-docker https://github.com/openshift/sti-php sti-php/5.5 openshift3/php-55-rhel7 none"
-packagekey['openshift-sti-python-docker']="openshift-sti-python-docker openshift-sti-base-docker https://github.com/openshift/sti-python sti-python/3.3 openshift3/python-33-rhel7 none"
-packagekey['openshift-sti-ruby-docker']="openshift-sti-ruby-docker openshift-sti-base-docker https://github.com/openshift/sti-ruby sti-ruby/2.0 openshift3/ruby-20-rhel7 none"
-
 usage() {
   echo "Usage `basename $0` [action] <options>" >&2
   echo >&2
   echo "Actions:" >&2
   echo "  build_container :: Clone dist-git, build containers" >&2
   echo "  docker_update   :: Clone dist-git, update version, release, or rhel" >&2
-  echo "  bump_and_build  :: docker_update, build containers" >&2
   echo "  docker_backfill :: Copy dist-git Dockerfile to git Dockerfile.product" >&2
   echo "  git_compare     :: Clone dist-git and git, compare files and Dockerfile" >&2
   echo "  push_images     :: Push images to qe-registry" >&2
@@ -106,13 +68,10 @@ usage() {
 
 add_to_list() {
   if ! [[ ${packagelist} =~ "::${1}::" ]] ; then
-    NEWLINE=$'\n'
-    export list+="${packagekey[${1}]}${NEWLINE}"
     export packagelist+=" ::${1}::"
     if [ "${VERBOSE}" == "TRUE" ] ; then
       echo "----------"
       echo ${packagelist}
-      echo ${list}
     fi
   fi
 }
@@ -132,8 +91,8 @@ add_group_to_list() {
       add_to_list openshift-enterprise-deployer-docker
       add_to_list openshift-enterprise-node-docker
       add_to_list openshift-enterprise-sti-builder-docker
-      add_to_list metrics-deployer-docker
       add_to_list logging-deployment-docker
+      add_to_list metrics-deployer-docker
       add_to_list openshift-enterprise-docker-builder-docker
       add_to_list openshift-enterprise-haproxy-router-docker
       ;;
@@ -178,7 +137,7 @@ setup_dist_git() {
 
 setup_git_repo() {
   pushd "${workingdir}" >/dev/null
-  git clone -q ${repo} 2>/dev/null
+  git clone -q ${git_repo} 2>/dev/null
   popd >/dev/null
 
 }
@@ -223,14 +182,14 @@ wait_for_all_builds() {
 }
 
 check_build_dependencies() {
-  depcheck=`grep ::${dependency}:: ${workingdir}/logs/finished`
+  depcheck=`grep ::${build_dependency}:: ${workingdir}/logs/finished`
   while [ "${depcheck}" == "" ]
   do
     now=`date`
-    echo "Waiting for ${dependency} to be built - ${now}"
+    echo "Waiting for ${build_dependency} to be built - ${now}"
     sleep 120
     check_builds
-    depcheck=`grep ::${dependency}:: ${workingdir}/logs/finished`
+    depcheck=`grep ::${build_dependency}:: ${workingdir}/logs/finished`
   done
 }
 
@@ -241,11 +200,12 @@ build_image() {
     #rhpkg container-build --repo http://file.rdu.redhat.com/tdawson/repo/aos-unsigned.repo >> ${workingdir}/logs/${container}.buildlog 2>&1 &
     #rhpkg container-build --repo http://file.rdu.redhat.com/sdodson/aos-unsigned.repo >> ${workingdir}/logs/${container}.buildlog 2>&1 &
     echo -n "  Waiting for createContainer taskid ."
+    sleep 10
     taskid=`grep createContainer ${workingdir}/logs/${container}.buildlog | awk '{print $1}' | sort -u`
     while [ "${taskid}" == "" ]
     do
       echo -n "."
-      sleep 5
+      sleep 10
       taskid=`grep createContainer ${workingdir}/logs/${container}.buildlog | awk '{print $1}' | sort -u`
       if grep -q -e "Unknown build target:" -e "buildContainer (noarch) failed" -e "server startup error" ${workingdir}/logs/${container}.buildlog ; then
         echo " error"
@@ -268,11 +228,11 @@ start_build_image() {
     build_image
   else
     check_build_dependencies
-    failedcheck=`grep ::${dependency}:: ${workingdir}/logs/buildfailed`
+    failedcheck=`grep ::${build_dependency}:: ${workingdir}/logs/buildfailed`
     if [ "${failedcheck}" == "" ] ; then
       build_image
     else
-      echo "  dependency error: ${dependency} failed to build"
+      echo "  dependency error: ${build_dependency} failed to build"
       echo "=== ${container} IMAGE BUILD FAILED ==="
       echo "::${container}::" >> ${workingdir}/logs/finished
       echo "::${container}::" >> ${workingdir}/logs/buildfailed
@@ -307,15 +267,15 @@ update_dockerfile() {
 show_git_diffs() {
   pushd "${workingdir}/${container}" >/dev/null
   echo "  ---- Checking files changed, added or removed ----"
-  extra_check=$(diff --brief -r ${workingdir}/${container} ${workingdir}/${path} | grep -v -e Dockerfile -e git -e osbs )
+  extra_check=$(diff --brief -r ${workingdir}/${container} ${workingdir}/${git_path} | grep -v -e Dockerfile -e git -e osbs )
   if ! [ "${extra_check}" == "" ] ; then
     echo "${extra_check}"
   fi
   differ_check=$(echo "${extra_check}" | grep " differ")
-  new_file=$(echo "${extra_check}" | grep "Only in ${workingdir}/${path}")
+  new_file=$(echo "${extra_check}" | grep "Only in ${workingdir}/${git_path}")
   old_file=$(echo "${extra_check}" | grep "Only in ${workingdir}/${container}")
   if ! [ "${differ_check}" == "" ] ; then
-    echo "  ---- Non-Dockerfile changes ----"
+    echo "  ---- Non-Docker file changes ----"
     echo "${differ_check}" | while read differ_line
     do
       myold_file=$(echo "${differ_line}" | awk '{print $2}')
@@ -326,28 +286,32 @@ show_git_diffs() {
     done
   fi
   if ! [ "${old_file}" == "" ] ; then
-    echo "  ---- Removed Non-Dockerfiles ----"
+    echo "  ---- Removed Non-Docker files ----"
     echo "${old_file}" | while read old_file_line
     do
-      myold_file=$(echo "${old_file_line}" | awk '{print $3}')
+      myold_file=$(echo "${old_file_line}" | awk '{print $4}')
+      # echo "Removing: ${myold_file}"
       git rm ${myold_file}
     done
   fi
   if ! [ "${new_file}" == "" ] ; then
-    echo "  ---- New Non-Dockerfiles ----"
-    echo " New files must be added by hand - sorry about that"
+    echo "  ---- New Non-Docker files ----"
+    #echo " New files must be added by hand - sorry about that"
     echo "${new_file}"
-    working_path="${workingdir}/${path}"
+    working_path="${workingdir}/${git_path}"
     echo "${new_file}" | while read new_file_line
     do
-      mynew_file=$(echo "${new_file_line}" | awk '{print $3}')
+      mynew_file=$(echo "${new_file_line}" | awk '{print $4}')
       mynew_file_trim="${mynew_file#$working_path}"
-      cp -v ${mynew_file} ${workingdir}/${container}/${mynew_file_trim}
-      git add ${workingdir}/${container}/${mynew_file_trim}
+      cp -rv ${workingdir}/${git_path}/${mynew_file} ${workingdir}/${container}/${mynew_file}
+      git add ${workingdir}/${container}/${mynew_file}
     done
   fi
   echo "  ---- Checking Dockerfile changes ----"
-  diff --label Dockerfile --label ${path}/Dockerfile -u Dockerfile ${workingdir}/${path}/Dockerfile >> .osbs-logs/Dockerfile.diff.new
+  diff --label Dockerfile --label ${git_path}/Dockerfile -u Dockerfile ${workingdir}/${git_path}/${git_dockerfile} >> .osbs-logs/Dockerfile.diff.new
+  if ! [ -f .osbs-logs/Dockerfile.diff ] ; then
+    touch .osbs-logs/Dockerfile.diff
+  fi
   newdiff=`diff .osbs-logs/Dockerfile.diff .osbs-logs/Dockerfile.diff.new`
   if [ "${newdiff}" == "" ] ; then
     rm -f .osbs-logs/Dockerfile.diff.new
@@ -377,11 +341,11 @@ show_git_diffs() {
 backfill_dockerfile() {
   pushd "${workingdir}/${container}" >/dev/null
   echo "  ---- Dockerfile.product changes ----"
-  diff --label Dockerfile --label ${path}/Dockerfile.product -u Dockerfile ${workingdir}/${path}/Dockerfile.product
+  diff --label Dockerfile --label ${git_path}/Dockerfile.product -u Dockerfile ${workingdir}/${git_path}/Dockerfile.product
   if [ "${FORCE}" == "TRUE" ] ; then
     echo "  Force Option Selected - Assuming Continue"
-    cp -f Dockerfile ${workingdir}/${path}/Dockerfile.product
-    pushd ${workingdir}/${path} >/dev/null
+    cp -f Dockerfile ${workingdir}/${git_path}/Dockerfile.product
+    pushd ${workingdir}/${git_path} >/dev/null
     git add Dockerfile.product
     popd >/dev/null
   else
@@ -389,8 +353,8 @@ backfill_dockerfile() {
     read choice < /dev/tty
     case ${choice} in
       c | C | continue )
-        cp -f Dockerfile ${workingdir}/${path}/Dockerfile.product
-        pushd ${workingdir}/${path} >/dev/null
+        cp -f Dockerfile ${workingdir}/${git_path}/Dockerfile.product
+        pushd ${workingdir}/${git_path} >/dev/null
         git add Dockerfile.product
         popd >/dev/null
         ;;
@@ -451,25 +415,18 @@ show_dockerfile_diffs() {
 }
 
 show_yaml() {
-  if ! [ "${brew_name}" == "none" ] ; then
-    pushd "${workingdir}/${container}" >/dev/null
-    package_name=`grep Name= Dockerfile | cut -d'"' -f2`
-    package_version=`grep Version= Dockerfile | cut -d'"' -f2`
-    package_release=`grep Release= Dockerfile | cut -d'"' -f2`
+  pushd "${workingdir}/${container}" >/dev/null
+  package_version=`grep Version= Dockerfile | cut -d'"' -f2`
+  package_release=`grep Release= Dockerfile | cut -d'"' -f2`
+  for image_name in ${docker_name_list}
+  do
     echo "---"
-    echo "repository: ${brew_name}"
+    echo "repository: ${image_name}"
     echo "tags: ${package_version},${package_version}-${package_release},latest"
     echo "build: ${container}-${package_version}-${package_release}"
-    echo "repository_tag: ${brew_name}:${package_version}-${package_release}"
-    if ! [ "${alt_name}" == "none" ] ; then
-      echo "---"
-      echo "repository: ${alt_name}"
-      echo "tags: ${package_version},${package_version}-${package_release},latest"
-      echo "build: ${container}-${package_version}-${package_release}"
-      echo "repository_tag: ${alt_name}:${package_version}-${package_release}"
-    fi
-    popd >/dev/null
-  fi
+    echo "repository_tag: ${image_name}:${package_version}-${package_release}"
+  done
+  popd >/dev/null
 }
 
 function push_image {
@@ -541,13 +498,13 @@ check_dependents() {
         echo "Checking dependents for: ${line}"
       fi
 
-      for index in ${!packagekey[@]}; do
+      for index in ${!dict_image_from[@]}; do
         if [[ ${dependent_list} =~ "::${index}::" ]] ; then
           if [ "${VERBOSE}" == "TRUE" ] ; then
             echo "  Already have on list: ${index}"
           fi
         else
-          checkdep=$(echo "${packagekey[${index}]}" | awk '{print $2}')
+          checkdep=$(echo "${dict_image_from[${index}]}" | awk '{print $2}')
           if [ "${VERBOSE}" == "TRUE" ] ; then
             echo "  Not on list - checking: ${index}"
             echo "    Dependency is: ${checkdep}"
@@ -613,7 +570,8 @@ push_images() {
 }
 
 test_function() {
-  echo "container: ${container} dependency: ${dependency} branch: ${branch}"
+  echo "container: ${container} dependency: ${build_dependency} branch: ${branch}"
+  echo "docker names: ${dict_image_name[${container}]}"
 }
 
 if [ "$#" -lt 1 ] ; then
@@ -735,7 +693,7 @@ if [ "${DEPENDENTS}" == "TRUE" ] ; then
   fi
   for item_name in ${packagelist}
   do
-    container_name=$(echo "$item_name" | cut -d':' -f3)
+    container_name=$(echo "${item_name}" | cut -d':' -f3)
     if [ "${VERBOSE}" == "TRUE" ] ; then
       echo "Item Name: ${item_name}"
       echo "Container: ${container_name}"
@@ -750,24 +708,28 @@ else
   export parent=""
 fi
 
-echo "${list}" | while read spec
+for unique_package in ${packagelist}
 do
-  [ -z "$spec" ] && continue
+  [ -z "${unique_package}" ] && continue
   export branch="${DIST_GIT_BRANCH}"
-  export container=$(echo "$spec" | awk '{print $1}')
-  export dependency=$(echo "$spec" | awk '{print $2}')
-  export repo=$(echo "$spec" | awk '{print $3}')
-  export path=$(echo "$spec" | awk '{print $4}')
-  export brew_name=$(echo "$spec" | awk '{print $5}')
-  export alt_name=$(echo "$spec" | awk '{print $6}')
+  export container=$(echo "${unique_package}" | cut -d':' -f3)
+  export build_dependency=$(echo "${dict_image_from[${container}]}" | awk '{print $2}')
   case "$action" in
     build_container )
       echo "=== ${container} ==="
       build_container
       ;;
     git_compare )
+      export git_repo=$(echo "${dict_git_compare[${container}]}" | awk '{print $1}')
+      export git_path=$(echo "${dict_git_compare[${container}]}" | awk '{print $2}')
+      export git_dockerfile=$(echo "${dict_git_compare[${container}]}" | awk '{print $3}')
       echo "=== ${container} ==="
-      git_compare
+      if ! [ "${git_repo}" == "None" ] ; then
+        git_compare
+      else
+        echo " No git repo to compare to."
+        echo " Skipping"
+      fi
       ;;
     docker_update )
       echo "=== ${container} ==="
@@ -783,10 +745,15 @@ do
       build_container
       ;;
     make_yaml )
-      build_yaml
+      docker_name_list="${dict_image_name[${container}]}"
+      if ! [ "${docker_name_list}" == "" ] ; then
+        build_yaml
+      fi
       ;;
     push_images )
       echo "=== ${container} ==="
+      export brew_name=$(echo "${dict_image_name[${container}]}" | awk '{print $1}')
+      export alt_name=$(echo "${dict_image_name[${container}]}" | awk '{print $2}')
       push_images
       ;;
     test | list )
