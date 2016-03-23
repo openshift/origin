251eb33f5cf48ce4e7497881ff9662147ab594f4
benjaminapetersen <admin@benjaminapetersen.me>
2016-03-23 11:07:45 -0400

    Update extension points from hawtio manager to angular-extension-registry
        - Add angular-extension-registry
            - TODO: register openshift/angular-extension-registry w/bower & update
        - Update hawtio javalink extension to new manager
        - Update online-extensions
        - Swap java console link icon to fa-share
        - Update extensions nav-system-status, nav-system-status-mobile, nav-user-dropdown
        - eliminate documtentation & about link html, register as extension in run block
        - Move logout link into extension
        - Style adjustments to needed to position utility nav links

--- assets/app/scripts/extensions/javaLink.js
+++ assets/app/scripts/extensions/javaLink.js
@@ -1,86 +1,79 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name openshiftConsoleExtensions.extension:JavaLinkController
- * @description
- * # ProjectController
- * Controller of the openshiftConsole
- */
-angular.module('openshiftConsoleExtensions', ['openshiftConsole'])
-  .factory("ProxyPod", function(DataService) {
-    return function(namespace, podName, port) {
-      return new URI(DataService.url({
-        resource: 'pods/proxy',
-        // always use https when connecting to jolokia in a pod
-        name: ['https', podName, port || ''].join(':'),
-        namespace: namespace
-      }));
-    };
-  })
-  .run(function(ProxyPod, BaseHref, HawtioExtension, $templateCache, $compile, AuthService) {
+angular
+  .module('openshiftConsoleExtensions')
+  .run([
+    'AuthService',
+    'BaseHref',
+    'DataService',
+    'extensionRegistry',
+    function(AuthService, BaseHref, DataService, extensionRegistry) {
 
-    var template = [
-      '<div row ',
+      var template = [
+        '<div row ',
+        'ng-show="item.url" ',
         'class="icon-row" ',
-        'ng-show="jolokiaUrl" ',
         'title="Connect to container">',
-        '<div>',
-          '<i class="fa fa-share" aria-hidden="true"></i>',
-        '</div>',
-        '<div flex>',
-          '<a ng-click="gotoContainerView($event, container, jolokiaUrl)" ng-href="jolokiaUrl">',
-            'Open Java Console',
-          '</a>',
-        '</div>',
-      '</div>'
-    ].join('');
+          '<div>',
+            '<i class="fa fa-share" aria-hidden="true"></i>',
+          '</div>',
+          '<div flex>',
+            '<a ng-click="item.onClick($event)" ',
+              'ng-href="item.url">',
+              'Open Java Console',
+            '</a>',
+          '</div>',
+        '</div>'
+      ].join('');
 
-    HawtioExtension.add('container-links', function ($scope) {
-      var container = $scope.container;
-      if (!container) {
-        return;
-      }
-      var jolokiaPort = _.find((container.ports || []), function(port) {
-        return port.name && port.name.toLowerCase() === 'jolokia';
-      });
-      if (!jolokiaPort) {
-        return;
-      }
-      var pod = $scope.$eval('podTemplate');
-      // TODO distinguish between pod and pod templates for now...
-      if (!pod || !pod.status || pod.status.phase !== 'Running') {
-        return;
-      }
-      var containerStatuses = pod.status.containerStatuses;
-      var containerStatus = _.find(containerStatuses, function(status) {
-        return status.name === container.name;
-      });
-      if (!containerStatus || !containerStatus.ready) {
-        return;
-      }
-      var podName = pod.metadata.name;
-      var namespace = pod.metadata.namespace;
-      $scope.jolokiaUrl = ProxyPod(namespace, podName, jolokiaPort.containerPort).segment('jolokia/').toString();
-      $scope.gotoContainerView = function($event, container, jolokiaUrl) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        var returnTo = window.location.href;
-        var title = container.name || 'Untitled Container';
-        var token = AuthService.UserStore().getToken() || '';
-        var targetURI = new URI().path(BaseHref)
-                                 .segment('java/') // Must have a trailing slash to avoid runtime errors in Safari
-                                 .hash(token)
-                                 .query({
-                                   jolokiaUrl: jolokiaUrl,
-                                   title: title,
-                                   returnTo: returnTo
-                                 });
-        window.location.href = targetURI.toString();
+      var makeJolokiaUrl = function(namespace, podName, port) {
+        return new URI(DataService.url({
+                    resource: 'pods/proxy',
+                    // always use https when connecting to jolokia in a pod
+                    name: ['https', podName, port || ''].join(':'),
+                    namespace: namespace
+                  }))
+                  .segment('jolokia/');
       };
-      var answer = $compile(template)($scope);
-      return answer;
-    });
-  });
 
-hawtioPluginLoader.addModule('openshiftConsoleExtensions');
+      extensionRegistry.add('container-links', _.spread(function(container, pod) {
+        var jolokiaPort = _.find((container.ports || []), function(port) {
+          return port.name && port.name.toLowerCase() === 'jolokia';
+        });
+        if (!jolokiaPort) {
+          return;
+        }
+        if(_.get(pod, 'status.phase') !== 'Running') {
+          return;
+        }
+        var podName = pod.metadata.name;
+        var namespace = pod.metadata.namespace;
+        var jolokiaUrl = makeJolokiaUrl(namespace, podName, jolokiaPort.containerPort).toString();
+        var gotoContainerView = function($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+          var returnTo = window.location.href;
+          var title = container.name || 'Untitled Container';
+          var token = AuthService.UserStore().getToken() || '';
+          var targetURI = new URI().path(BaseHref)
+                                   .segment('java/') // Must have a trailing slash to avoid runtime errors in Safari
+                                   .hash(token)
+                                   .query({
+                                     jolokiaUrl: jolokiaUrl,
+                                     title: title,
+                                     returnTo: returnTo
+                                   });
+
+          window.location.href = targetURI.toString();
+        };
+
+        return {
+          type: 'dom',
+          node: template,
+          onClick: gotoContainerView,
+          url: jolokiaUrl
+        };
+
+      }));
+    }
+  ]);
