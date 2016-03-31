3481ac6bd8eba4c330a73547e7cd651c0d1a59a4
benjaminapetersen <admin@benjaminapetersen.me>
2016-03-31 11:26:27 -0400

    Update online extensions, avoid rendering emtpy DOM nodes

--- assets/extensions/examples/online-extensions.js
+++ assets/extensions/examples/online-extensions.js
@@ -35,18 +35,17 @@ window.OPENSHIFT_CONSTANTS.HELP = {
 
 
 angular
-  .module('openshiftConsoleExtensions')
+  .module('openshiftOnlineExtensions', ['openshiftConsole'])
   .run([
     'extensionRegistry',
     function(extensionRegistry) {
 
-      var system_status_elem = $('<a href="http://status.openshift.com/" target="_blank" class="nav-item-iconic system-status project-action-btn" style="display: none;">');
-      var system_status_elem_mobile = $('<div row flex class="navbar-flex-btn system-status-mobile" style="display: none;">');
+      var system_status_elem = $('<a href="http://status.openshift.com/" target="_blank" class="nav-item-iconic system-status project-action-btn">');
+      var system_status_elem_mobile = $('<div row flex class="navbar-flex-btn system-status-mobile">');
 
 
       $.getJSON("https://m0sg3q4t415n.statuspage.io/api/v2/summary.json", function (data) {
         var n = (data.incidents || [ ]).length;
-        n = 32;
         if (n > 0) {
           var issueStr = n + ' open issue';
           if (n !== 1) {
@@ -54,31 +53,29 @@ angular
           }
           $('<span title="System Status" class="fa status-icon pficon-warning-triangle-o"></span>').appendTo(system_status_elem);
           $('<span class="status-issue">' + issueStr + '</span>').appendTo(system_status_elem);
-          system_status_elem.css('display', '');
 
           system_status_elem_mobile.append(system_status_elem.clone());
-          system_status_elem_mobile.css('display', '');
+
+          // only add the extension if there is something to show so we
+          // do not generate empty nodes if no issues
+          extensionRegistry
+            .add('nav-system-status', function() {
+              return [{
+                type: 'dom',
+                node: system_status_elem
+              }];
+            });
+
+          extensionRegistry
+            .add('nav-system-status-mobile', function() {
+              return [{
+                type: 'dom',
+                node: system_status_elem_mobile
+              }];
+            });
+
         }
       });
-
-
-      extensionRegistry
-        .add('nav-system-status', function() {
-          return [{
-            type: 'dom',
-            node: system_status_elem
-          }];
-        });
-
-
-      extensionRegistry
-        .add('nav-system-status-mobile', function() {
-          return [{
-            type: 'dom',
-            node: system_status_elem_mobile
-          }];
-        });
-
 
       extensionRegistry
         .add('nav-help-dropdown', function() {
@@ -101,3 +98,5 @@ angular
 
     }
   ]);
+
+hawtioPluginLoader.addModule('openshiftOnlineExtensions');
