8439971d6186820daab14b829f6c84ce51728a45
Clayton Coleman <ccoleman@redhat.com>
2016-04-21 18:53:03 -0400

    Support extracting release binaries on other platforms

--- hack/extract-release.sh
+++ hack/extract-release.sh
@@ -13,13 +13,13 @@ source "${OS_ROOT}/hack/common.sh"
 # Go to the top of the tree.
 cd "${OS_ROOT}"
 
-# Copy the linux release archives release back to the local _output/local/bin/linux/amd64 directory.
-# TODO: support different OS's?
+# Copy the release archives release back to the local _output/local/bin/... directories.
+# NOTE: On Mac and Windows you must pass WARN=1 in order to extract the output.
 os::build::detect_local_release_tars $(os::build::host_platform_friendly)
 
-mkdir -p "${OS_OUTPUT_BINPATH}/linux/amd64"
-os::build::extract_tar "${OS_PRIMARY_RELEASE_TAR}" "${OS_OUTPUT_BINPATH}/linux/amd64"
-os::build::extract_tar "${OS_CLIENT_RELEASE_TAR}" "${OS_OUTPUT_BINPATH}/linux/amd64"
-os::build::extract_tar "${OS_IMAGE_RELEASE_TAR}" "${OS_OUTPUT_BINPATH}/linux/amd64"
+mkdir -p "${OS_OUTPUT_BINPATH}/$(os::build::host_platform)"
+os::build::extract_tar "${OS_PRIMARY_RELEASE_TAR}" "${OS_OUTPUT_BINPATH}/$(os::build::host_platform)"
+os::build::extract_tar "${OS_CLIENT_RELEASE_TAR}" "${OS_OUTPUT_BINPATH}/$(os::build::host_platform)"
+os::build::extract_tar "${OS_IMAGE_RELEASE_TAR}" "${OS_OUTPUT_BINPATH}/$(os::build::host_platform)"
 
 os::build::make_openshift_binary_symlinks
