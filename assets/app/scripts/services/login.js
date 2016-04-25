1adc162ea9f8bc46601c45c63b9bf84aa5cff5d6
Jordan Liggitt <jliggitt@redhat.com>
2016-04-25 11:11:46 -0400

    Add login csrf prompts

--- assets/app/scripts/services/login.js
+++ assets/app/scripts/services/login.js
@@ -29,6 +29,70 @@ angular.module('openshiftConsole')
   this.$get = function($location, $q, Logger) {
     var authLogger = Logger.get("auth");
 
+    var getRandomInts = function(length) {
+      var randomValues;
+
+      if (window.crypto && window.Uint32Array) {
+        try {
+          var r = new Uint32Array(length);
+          window.crypto.getRandomValues(r);
+          randomValues = [];
+          for (var j=0; j < length; j++) {
+            randomValues.push(r[j]);
+          }
+        } catch(e) {
+          authLogger.debug("RedirectLoginService.getRandomInts: ", e);
+          randomValues = null;
+        }
+      }
+      
+      if (!randomValues) {
+        randomValues = [];
+        for (var i=0; i < length; i++) {
+          randomValues.push(Math.floor(Math.random() * 4294967296));
+        }
+      }
+      
+      return randomValues;
+    };
+    
+    var nonceKey = "RedirectLoginService.nonce";
+    var makeState = function(then) {
+      var nonce = String(new Date().getTime()) + "-" + getRandomInts(8).join("");
+      try {
+        window.localStorage[nonceKey] = nonce;
+      } catch(e) {
+        authLogger.log("RedirectLoginService.makeState, localStorage error: ", e);
+      }
+      return JSON.stringify({then: then, nonce:nonce});
+    };
+    var parseState = function(state) {
+      var retval = {
+        then: null,
+        verified: false
+      };
+
+      var nonce = "";
+      try {
+        nonce = window.localStorage[nonceKey];
+        window.localStorage.removeItem(nonceKey);
+      } catch(e) {
+        authLogger.log("RedirectLoginService.parseState, localStorage error: ", e);
+      }
+      
+      try {
+        var data = state ? JSON.parse(state) : {};
+        if (data && data.nonce && nonce && data.nonce === nonce) {
+          retval.verified = true;
+          retval.then = data.then;
+        }
+      } catch(e) {
+        authLogger.error("RedirectLoginService.parseState, state error: ", e);
+      }
+      authLogger.error("RedirectLoginService.parseState", retval);
+      return retval;
+    };
+
     return {
       // Returns a promise that resolves with {user:{...}, token:'...', ttl:X}, or rejects with {error:'...'[,error_description:'...',error_uri:'...']}
       login: function() {
@@ -49,7 +113,7 @@ angular.module('openshiftConsole')
         uri.query({
           client_id: _oauth_client_id,
           response_type: 'token',
-          state: returnUri.toString(),
+          state: makeState(returnUri.toString()),
           redirect_uri: _oauth_redirect_uri
         });
         authLogger.log("RedirectLoginService.login(), redirecting", uri.toString());
@@ -59,7 +123,7 @@ angular.module('openshiftConsole')
       },
 
       // Parses oauth callback parameters from window.location
-      // Returns a promise that resolves with {token:'...',then:'...'}, or rejects with {error:'...'[,error_description:'...',error_uri:'...']}
+      // Returns a promise that resolves with {token:'...',then:'...',verified:true|false}, or rejects with {error:'...'[,error_description:'...',error_uri:'...']}
       // If no token and no error is present, resolves with {}
       // Example error codes: https://tools.ietf.org/html/rfc6749#section-5.2
       finish: function() {
@@ -71,12 +135,12 @@ angular.module('openshiftConsole')
         var fragmentParams = new URI("?" + u.fragment()).query(true);
         authLogger.log("RedirectLoginService.finish()", queryParams, fragmentParams);
 
-       // Error codes can come in query params or fragment params
-       // Handle an error response from the OAuth server
+        // Error codes can come in query params or fragment params
+        // Handle an error response from the OAuth server
         var error = queryParams.error || fragmentParams.error;
-       if (error) {
-         var error_description = queryParams.error_description || fragmentParams.error_description;
-         var error_uri = queryParams.error_uri || fragmentParams.error_uri;
+        if (error) {
+          var error_description = queryParams.error_description || fragmentParams.error_description;
+          var error_uri = queryParams.error_uri || fragmentParams.error_uri;
           authLogger.log("RedirectLoginService.finish(), error", error, error_description, error_uri);
           return $q.reject({
             error: error,
@@ -85,13 +149,16 @@ angular.module('openshiftConsole')
           });
         }
 
+        var stateData = parseState(fragmentParams.state);
+        
         // Handle an access_token response
         if (fragmentParams.access_token && (fragmentParams.token_type || "").toLowerCase() === "bearer") {
           var deferred = $q.defer();
           deferred.resolve({
             token: fragmentParams.access_token,
             ttl: fragmentParams.expires_in,
-            then: fragmentParams.state
+            then: stateData.state,
+            verified: stateData.verified
           });
           return deferred.promise;
         }
