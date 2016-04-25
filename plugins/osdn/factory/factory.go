efa9c45e8f8bb4fc1b57477e16b3064acea9fe77
Dan Williams <dcbw@redhat.com>
2016-04-25 14:53:06 -0500

    Split proxy endpoint filtering out into separate object

--- plugins/osdn/factory/factory.go
+++ plugins/osdn/factory/factory.go
@@ -13,7 +13,7 @@ import (
 )
 
 // Call by higher layers to create the plugin instance
-func NewPlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Client, hostname string, selfIP string) (api.OsdnPlugin, api.FilteringEndpointsConfigHandler, error) {
+func NewPlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Client, hostname string, selfIP string) (api.OsdnPlugin, error) {
 	switch strings.ToLower(pluginType) {
 	case ovs.SingleTenantPluginName:
 		return ovs.CreatePlugin(osdn.NewRegistry(osClient, kClient), false, hostname, selfIP)
@@ -21,5 +21,15 @@ func NewPlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Cl
 		return ovs.CreatePlugin(osdn.NewRegistry(osClient, kClient), true, hostname, selfIP)
 	}
 
-	return nil, nil, nil
+	return nil, nil
+}
+
+// Call by higher layers to create the proxy plugin instance; only used by nodes
+func NewProxyPlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Client) (api.FilteringEndpointsConfigHandler, error) {
+	switch strings.ToLower(pluginType) {
+	case ovs.MultiTenantPluginName:
+		return ovs.CreateProxyPlugin(osdn.NewRegistry(osClient, kClient))
+	}
+
+	return nil, nil
 }
