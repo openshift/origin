06b1616282ae361ef9f99b27ee34104760ebaf04
Jordan Liggitt <jliggitt@redhat.com>
2016-04-25 11:11:46 -0400

    Add login csrf prompts

--- assets/app/scripts/controllers/util/oauth.js
+++ assets/app/scripts/controllers/util/oauth.js
@@ -8,13 +8,22 @@
  * Controller of the openshiftConsole
  */
 angular.module('openshiftConsole')
-  .controller('OAuthController', function ($location, $q, RedirectLoginService, DataService, AuthService, Logger) {
+  .controller('OAuthController', function ($scope, $location, $q, RedirectLoginService, DataService, AuthService, Logger) {
     var authLogger = Logger.get("auth");
+
+    // Initialize to a no-op function.
+    // Needed to let the view confirm a login when the state is unverified. 
+    $scope.completeLogin = function(){};
+    $scope.cancelLogin = function() {
+      $location.replace();
+      $location.url("./");
+    };
 
     RedirectLoginService.finish()
     .then(function(data) {
       var token = data.token;
       var then = data.then;
+      var verified = data.verified;
       var ttl = data.ttl;
 
       // Try to fetch the user
@@ -25,21 +34,41 @@ angular.module('openshiftConsole')
       .then(function(user) {
         // Set the new user and token in the auth service
         authLogger.log("OAuthController, got user", user);
-        AuthService.setUser(user, token, ttl);
 
-        // Redirect to original destination (or default to '/')
-        var destination = then || './';
-        if (URI(destination).is('absolute')) {
-          authLogger.log("OAuthController, invalid absolute redirect", destination);
-          destination = './';
+        $scope.completeLogin = function() {
+          // Persist the user
+          AuthService.setUser(user, token, ttl);
+          
+          // Redirect to original destination (or default to './')
+          var destination = then || './';
+          if (URI(destination).is('absolute')) {
+            authLogger.log("OAuthController, invalid absolute redirect", destination);
+            destination = './';
+          }
+          authLogger.log("OAuthController, redirecting", destination);
+          $location.replace();
+          $location.url(destination);
+        };
+        
+        if (verified) {
+          // Automatically complete
+          $scope.completeLogin();
+        } else {
+          // Require the UI to prompt
+          $scope.confirmUser = user;
+          
+          // Additionally, give the UI info about the user being overridden
+          var currentUser = AuthService.UserStore().getUser();
+          if (currentUser && currentUser.metadata.name !== user.metadata.name) {
+            $scope.overriddenUser = currentUser;
+          }
         }
-        authLogger.log("OAuthController, redirecting", destination);
-        $location.url(destination);
       })
       .catch(function(rejection) {
         // Handle an API error response fetching the user
         var redirect = URI('error').query({error: 'user_fetch_failed'}).toString();
         authLogger.error("OAuthController, error fetching user", rejection, "redirecting", redirect);
+        $location.replace();
         $location.url(redirect);
       });
 
@@ -51,6 +80,7 @@ angular.module('openshiftConsole')
         error_uri: rejection.error_uri || ""
       }).toString();
       authLogger.error("OAuthController, error", rejection, "redirecting", redirect);
+      $location.replace();
       $location.url(redirect);
     });
 
