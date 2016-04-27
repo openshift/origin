f24b863aa0788b279ad202e381af4fed1d9e0f73
Dan Williams <dcbw@redhat.com>
2016-04-27 11:29:28 -0500

    Split plugin creation into NewMasterPlugin and NewNodePlugin

--- plugins/osdn/factory/factory.go
+++ plugins/osdn/factory/factory.go
@@ -12,8 +12,17 @@ import (
 	"github.com/openshift/openshift-sdn/plugins/osdn/ovs"
 )
 
-// Call by higher layers to create the plugin instance
-func NewPlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Client, hostname string, selfIP string) (api.OsdnPlugin, error) {
+// Call by higher layers to create the plugin SDN master instance
+func NewMasterPlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Client) (api.OsdnPlugin, error) {
+	return newPlugin(pluginType, osClient, kClient, "", "")
+}
+
+// Call by higher layers to create the plugin SDN node instance
+func NewNodePlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Client, hostname string, selfIP string) (api.OsdnPlugin, error) {
+	return newPlugin(pluginType, osClient, kClient, hostname, selfIP)
+}
+
+func newPlugin(pluginType string, osClient *osclient.Client, kClient *kclient.Client, hostname string, selfIP string) (api.OsdnPlugin, error) {
 	switch strings.ToLower(pluginType) {
 	case ovs.SingleTenantPluginName:
 		return ovs.CreatePlugin(osdn.NewRegistry(osClient, kClient), false, hostname, selfIP)
