7f19d19f6b5cd812c4d0c5a5a8a0c02987913869
Clayton Coleman <ccoleman@redhat.com>
2016-04-29 16:12:41 -0400

    Enable the gcs, oss, and inmemory storage drivers in registry
    Also enable the proxy code.

--- pkg/cmd/dockerregistry/dockerregistry.go
+++ pkg/cmd/dockerregistry/dockerregistry.go
@@ -25,9 +25,14 @@ import (
 
 	_ "github.com/docker/distribution/registry/auth/htpasswd"
 	_ "github.com/docker/distribution/registry/auth/token"
+
+	_ "github.com/docker/distribution/registry/proxy"
 	_ "github.com/docker/distribution/registry/storage/driver/azure"
 	_ "github.com/docker/distribution/registry/storage/driver/filesystem"
+	_ "github.com/docker/distribution/registry/storage/driver/gcs"
+	_ "github.com/docker/distribution/registry/storage/driver/inmemory"
 	_ "github.com/docker/distribution/registry/storage/driver/middleware/cloudfront"
+	_ "github.com/docker/distribution/registry/storage/driver/oss"
 	_ "github.com/docker/distribution/registry/storage/driver/s3"
 	_ "github.com/docker/distribution/registry/storage/driver/swift"
 
