e9b50210227f4157c7ffc8030ffb3190c434d33d
Jessica Forrester <jforrest@redhat.com>
2016-05-13 12:44:56 -0400

    Add script for vendoring the console repo into bindata.go files

---
+++ hack/vendor-console.sh
@@ -0,0 +1,58 @@
+#!/bin/bash 
+# 
+#  This script vendors the Web Console source files into bindata.go files that can be built into the openshift binary.
+#
+#  Accepted environment variables are:
+#   - GIT_REF:           specifies which branch / tag of the web console to vendor. If set, then any untracked/uncommitted changes
+#                        will cause the script to exit with an error. If not set then the current working state of the web console
+#                        directory will be used.
+#   - CONSOLE_REPO_PATH: specifies a directory path to look for the web console repo.  If not set it is assumed to be
+#                        a sibling to this repository.
+
+set -o errexit 
+set -o nounset 
+set -o pipefail
+
+OS_ROOT=$(dirname "${BASH_SOURCE}")/..
+source "${OS_ROOT}/hack/common.sh"
+
+CONSOLE_REPO_PATH=${CONSOLE_REPO_PATH:-$OS_ROOT/../origin-web-console}
+if [[ ! -d "$CONSOLE_REPO_PATH" ]]; then
+  echo "The console repo at path $CONSOLE_REPO_PATH does not exist."
+  echo "Make sure you have cloned the web console repo locally:  git@github.com:openshift/origin-web-console.git"
+  echo "Or, you can specify a different path with the CONSOLE_REPO_PATH environment variable."
+  exit 1
+fi
+
+echo "Making sure go-bindata binary has been built..."
+pushd "${OS_ROOT}/Godeps/_workspace" > /dev/null
+  godep_path=$(pwd)
+  pushd src/github.com/jteeuwen/go-bindata > /dev/null
+    GOPATH=$godep_path go install ./...
+  popd > /dev/null
+popd > /dev/null
+
+if [[ -z "${GIT_REF:+x}" ]]; then 
+  echo "No git ref specified, using current state of the repo including any unstaged changes...";
+else 
+  echo "Using git ref ${GIT_REF} ..."
+  pushd "${CONSOLE_REPO_PATH}" > /dev/null
+    if [[ -n "$(git status --porcelain -uall)" ]]; then
+      echo "You have untracked or uncommitted changes in your console repository."
+      echo "Since a GIT_REF was specified you must stash or commit your changes and then run this again."
+      exit 1
+    fi
+    git checkout "${GIT_REF}"
+  popd > /dev/null
+fi
+
+echo "Building bindata.go files..."
+pushd "${OS_ROOT}" > /dev/null
+  # Put each component in its own go package for compilation performance
+  # Strip off the dist folder from each package to flatten the resulting directory structure
+  # Force timestamps to unify, and mode to 493 (0755)
+  Godeps/_workspace/bin/go-bindata -nocompress -nometadata -prefix "${CONSOLE_REPO_PATH}/dist"      -pkg "assets" -o "pkg/assets/bindata.go"      "${CONSOLE_REPO_PATH}/dist/..."
+  Godeps/_workspace/bin/go-bindata -nocompress -nometadata -prefix "${CONSOLE_REPO_PATH}/dist.java" -pkg "java"   -o "pkg/assets/java/bindata.go" "${CONSOLE_REPO_PATH}/dist.java/..."
+popd > /dev/null
+
+echo "Done vendoring.  To run the console, run 'make clean build' and restart your origin server."
