1f19ef3098174394fbb164bd08777c413d83c729
Michail Kargakis <mkargaki@redhat.com>
2016-05-18 17:18:45 +0200

    deployapi: fix automatic description

--- pkg/deploy/api/v1/types.go
+++ pkg/deploy/api/v1/types.go
@@ -310,7 +310,10 @@ const (
 
 // DeploymentTriggerImageChangeParams represents the parameters to the ImageChange trigger.
 type DeploymentTriggerImageChangeParams struct {
-	// Automatic means that the detection of a new tag value should result in a new deployment.
+	// Automatic means that the detection of a new tag value should result in an image update
+	// inside the pod template. Deployment configs that haven't been deployed yet will always
+	// have their images updated. Deployment configs that have been deployed at least once, will
+	// have their images updated only if this is set to true.
 	Automatic bool `json:"automatic,omitempty"`
 	// ContainerNames is used to restrict tag updates to the specified set of container names in a pod.
 	ContainerNames []string `json:"containerNames,omitempty"`
