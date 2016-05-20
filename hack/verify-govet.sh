33264bbfa8f4daa312bd3a4aa7f0bc0a61479544
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-05-20 17:45:23 +0200

    Do not check for shadowed variables
    According to Rob Pike:
    
      (...) the vet -shadow feature is disabled for a reason. It does
      not give the level of confidence the other analyses do.
    
    (https://groups.google.com/d/msg/golang-nuts/fbxCYRPKTuM/6pKJ8qPdw6AJ)
    
    We're getting a lot of false positives for legit code like:
    
    	if err := ...; err != nil {
    		...
    	}

--- hack/verify-govet.sh
+++ hack/verify-govet.sh
@@ -7,70 +7,13 @@ set -o pipefail
 echo $(go version)
 
 OS_ROOT=$(dirname "${BASH_SOURCE}")/..
-source "${OS_ROOT}/hack/common.sh"
-source "${OS_ROOT}/hack/util.sh"
 
 cd "${OS_ROOT}"
 
-os::build::setup_env
-
-FAILURE=false
-test_dirs=$(find_files | cut -d '/' -f 1-2 | sort -u)
-for test_dir in $test_dirs
-do
-  if ! go tool vet -shadow=false $test_dir
-  then
-    FAILURE=true
-  fi
-done
-
-# For the sake of slowly white-listing `shadow` checks, we need to keep track of which
-# directories we're searching through. The following are all of the directories we care about:
-# all top-level directories except for 'pkg', and all second-level subdirectories of 'pkg'.
-ALL_DIRS=$(find_files | grep -Eo "\./([^/]+|pkg/[^/]+)" | sort -u)
-
-DIR_BLACKLIST='./hack
-./pkg/api
-./pkg/authorization
-./pkg/build
-./pkg/client
-./pkg/cmd
-./pkg/deploy
-./pkg/diagnostics
-./pkg/dockerregistry
-./pkg/generate
-./pkg/gitserver
-./pkg/image
-./pkg/oauth
-./pkg/project
-./pkg/router
-./pkg/security
-./pkg/serviceaccounts
-./pkg/template
-./pkg/user
-./pkg/util
-./test
-./third_party
-./tools'
-
-for test_dir in $ALL_DIRS
-do
-  # use `grep` failure to determine that a directory is not in the blacklist
-  if ! echo "${DIR_BLACKLIST}" | grep -q "${test_dir}"; then
-    if ! go tool vet -shadow -shadowstrict $test_dir
-    then
-      FAILURE=true
-    fi
-  fi
-done
-
-# We don't want to exit on the first failure of go vet, so just keep track of
-# whether a failure occurred or not.
-if $FAILURE
-then
-  echo "FAILURE: go vet failed!"
-  exit 1
-else
+if go vet ./...; then
   echo "SUCCESS: go vet succeded!"
   exit 0
+else
+  echo "FAILURE: go vet failed!"
+  exit 1
 fi
