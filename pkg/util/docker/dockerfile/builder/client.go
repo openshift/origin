291e78dcbf9d047b96a90277b7453ab5fe581df8
Clayton Coleman <ccoleman@redhat.com>
2016-05-21 22:19:16 -0400

    Docker image cannot have - or _ in succession

--- pkg/util/docker/dockerfile/builder/client.go
+++ pkg/util/docker/dockerfile/builder/client.go
@@ -240,7 +240,13 @@ func (e *ClientExecutor) CreateScratchImage() (string, error) {
 	if _, err := io.ReadFull(rand.Reader, random); err != nil {
 		return "", err
 	}
-	name := strings.ToLower(fmt.Sprintf("scratch-%s", strings.TrimRight(base64.URLEncoding.EncodeToString(random), "=")))
+	s := strings.Map(func(r rune) rune {
+		if r == '-' || r == '_' {
+			return 'a'
+		}
+		return r
+	}, strings.TrimRight(base64.URLEncoding.EncodeToString(random), "="))
+	name := strings.ToLower(fmt.Sprintf("scratch-%s", s))
 
 	buf := &bytes.Buffer{}
 	w := tar.NewWriter(buf)
