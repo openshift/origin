176cdd92d9e52a29b2b60e731cf4f5d218e5ea9a
benjaminapetersen <admin@benjaminapetersen.me>
2016-05-27 15:38:34 -0400

    Backport bug #1333118: adding labels and envs is tricky

--- assets/app/scripts/directives/oscKeyValues.js
+++ assets/app/scripts/directives/oscKeyValues.js
@@ -19,51 +19,6 @@ angular.module("openshiftConsole")
       }
     };
   })
-  .controller("KeyValuesController", function($scope){
-    var added = {};
-    $scope.allowDelete = function(value){
-      if ($scope.preventEmpty && (Object.keys($scope.entries).length === 1)) {
-        return false;
-      }
-      if($scope.deletePolicy === "never") {
-        return false;
-      }
-      if($scope.deletePolicy === "added"){
-        return added[value] !== undefined;
-      }
-      return true;
-    };
-    $scope.addEntry = function() {
-      if($scope.key && $scope.value){
-        var readonly = $scope.readonlyKeys.split(",");
-        if(readonly.indexOf($scope.key) !== -1){
-          return;
-        }
-        added[$scope.key] = "";
-        $scope.entries[$scope.key] = $scope.value;
-        $scope.key = null;
-        $scope.value = null;
-        $scope.form.$setPristine();
-        $scope.form.$setUntouched();
-      }
-    };
-    $scope.deleteEntry = function(key) {
-      if ($scope.entries[key]) {
-        delete $scope.entries[key];
-        delete added[key];
-        $scope.form.$setDirty();
-      }
-    };
-    $scope.setErrorText = function(keyTitle) {
-      if (keyTitle === 'path') {
-        return "absolute path";
-      } else if (keyTitle === 'label') {
-        return "label";
-      } else {
-        return "key";
-      }
-    };
-  })
   .directive("oscInputValidator", function(){
 
     var validators = {
@@ -162,10 +117,128 @@ angular.module("openshiftConsole")
         readonlyKeys: "@",
         keyValidationTooltip: "@",
         valueValidationTooltip: "@",
-        preventEmpty: "=?",
+        preventEmpty: "=?"
       },
       controller: function($scope){
+        var focusElem;
+        var added = {};
+        var isUncommitted = function() {
+          return (!!$scope.key) || (!!$scope.value);
+        };
+        var checkCommitted = function() {
+          if(isUncommitted()) {
+            $scope.showCommmitWarning = true;
+          } else {
+            $scope.showCommmitWarning = false;
+          }
+        };
+        // checks if the key,value inputs have any text value.
+        // if so, sets the form name="clean" to an 'invalid' state, which will
+        // invalidate any parent form up the chain.  This should result in an
+        // inability to submit that form until the user commits the new key-value pair.
+        var isClean = _.debounce(function() {
+          $scope.$applyAsync(function() {
+            if(!!$scope.key) {
+              $scope.clean.isClean.$setValidity('isClean', false);
+            } else if(!!$scope.value) {
+              $scope.clean.isClean.$setValidity('isClean', false);
+            } else {
+              $scope.clean.isClean.$setValidity('isClean', true);
+            }
+          });
+        }, 200);
+
+        // returns a new function bound to the set of DOM nodes provided as an array.
+        // allows us to treat the osc-key-values directive as a single node on blur
+        // events, though it is actually made up of a number of nodes.  If any of the
+        // provided nodes is the document.activeElement, we know the osc-key-values
+        // directive as a whole still has focus. When the document.activeElement no
+        // longer matches a child node, we can test to see if there are uncommitted
+        // values left in the osc-key-values directive & notify the user if so.
+        var onBlur = function(nodes) {
+          return function(evt) {
+            $scope.$applyAsync(function() {
+              if(!_.includes(nodes, document.activeElement)) {
+                checkCommitted();
+                isClean();
+              }
+            });
+          };
+        };
+        $scope.isClean = isClean;
+
+        $scope.clear = function() {
+          $scope.key = '';
+          $scope.value = '';
+          checkCommitted();
+          isClean();
+        };
+
+        $scope.allowDelete = function(value){
+          if ($scope.preventEmpty && (Object.keys($scope.entries).length === 1)) {
+            return false;
+          }
+          if($scope.deletePolicy === "never") {
+            return false;
+          }
+          if($scope.deletePolicy === "added"){
+            return added[value] !== undefined;
+          }
+          return true;
+        };
+
+        $scope.addEntry = function() {
+          if($scope.key && $scope.value){
+            var readonly = $scope.readonlyKeys.split(",");
+            if(readonly.indexOf($scope.key) !== -1){
+              return;
+            }
+            added[$scope.key] = "";
+            $scope.entries[$scope.key] = $scope.value;
+            $scope.key = null;
+            $scope.value = null;
+            $scope.form.$setPristine();
+            $scope.form.$setUntouched();
+            checkCommitted();
+            isClean();
+            focusElem.focus();
+          }
+        };
+
+        $scope.deleteEntry = function(key) {
+          if ($scope.entries[key]) {
+            delete $scope.entries[key];
+            delete added[key];
+            $scope.form.$setDirty();
+          }
+        };
+
+        $scope.setErrorText = function(keyTitle) {
+          if (keyTitle === 'path') {
+            return "absolute path";
+          } else if (keyTitle === 'label') {
+            return "label";
+          } else {
+            return "key";
+          }
+        };
+
         this.scope = $scope;
+
+        this.init = function(keyInput, valInput, submitBtn) {
+          var nodes = [keyInput[0], valInput[0], submitBtn[0]];
+          var boundBlur = onBlur(nodes);
+          focusElem = keyInput;
+          keyInput.on('blur', boundBlur);
+          valInput.on('blur', boundBlur);
+          submitBtn.on('blur', boundBlur);
+
+          $scope.$on('$destroy', function() {
+            keyInput.off('blur', boundBlur);
+            valInput.off('blur', boundBlur);
+            submitBtn.off('blur', boundBlur);
+          });
+        };
       },
       templateUrl: "views/directives/osc-key-values.html",
       compile: function(element, attrs){
@@ -185,6 +258,15 @@ angular.module("openshiftConsole")
         if(!attrs.readonlyKeys){
           attrs.readonlyKeys = "";
         }
+        return {
+          post: function($scope, $elem, $attrs, ctrl) {
+            ctrl.init(
+                $elem.find('input[name="key"]'),
+                $elem.find('input[name="value"]'),
+                $elem.find('a.add-key-value')
+              );
+          }
+        };
       }
     };
   });
