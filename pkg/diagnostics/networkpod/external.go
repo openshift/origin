e31e4f05a4401faaaf7c8c96e26a46f4df867f38
Ravi Sankar Penta <rpenta@redhat.com>
2016-07-17 12:58:50 -0700

    Diagnostics: Added external connectivity checks

---
+++ pkg/diagnostics/networkpod/external.go
@@ -0,0 +1,44 @@
+package network
+
+import (
+	"fmt"
+
+	kexec "k8s.io/kubernetes/pkg/util/exec"
+
+	"github.com/openshift/origin/pkg/diagnostics/types"
+)
+
+const (
+	CheckExternalNetworkName = "CheckExternalNetwork"
+)
+
+// CheckExternalNetwork is a Diagnostic to check accessibility of external network within a pod
+type CheckExternalNetwork struct {
+}
+
+// Name is part of the Diagnostic interface and just returns name.
+func (d CheckExternalNetwork) Name() string {
+	return CheckExternalNetworkName
+}
+
+// Description is part of the Diagnostic interface and just returns the diagnostic description.
+func (d CheckExternalNetwork) Description() string {
+	return "Check that external network is accessible within a pod"
+}
+
+// CanRun is part of the Diagnostic interface; it determines if the conditions are right to run this diagnostic.
+func (d CheckExternalNetwork) CanRun() (bool, error) {
+	return true, nil
+}
+
+// Check is part of the Diagnostic interface; it runs the actual diagnostic logic
+func (d CheckExternalNetwork) Check() types.DiagnosticResult {
+	r := types.NewDiagnosticResult(CheckExternalNetworkName)
+
+	externalAddress := "www.redhat.com"
+	kexecer := kexec.New()
+	if _, err := kexecer.Command("ping", "-c1", "-W1", externalAddress).CombinedOutput(); err != nil {
+		r.Error("DExtNet1001", err, fmt.Sprintf("Pinging external address %q failed. Error: %s", externalAddress, err))
+	}
+	return r
+}
