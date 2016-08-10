e09d0d9001751e61eb10926c4bcf77923b8e4975
juanvallejo <jvallejo@redhat.com>
2016-08-10 14:57:56 -0400

    show namespace for custom strategy bc

--- test/cmd/basicresources.sh
+++ test/cmd/basicresources.sh
@@ -307,6 +307,11 @@ os::cmd::expect_success 'oc new-app node'
 os::cmd::expect_success_and_text 'oc get all' 'is/test1'
 os::cmd::expect_success_and_not_text 'oc get is' 'is/test1'
 echo "resource printer: ok"
+# Test that resource printer includes namespaces for buildconfigs with custom strategies
+os::cmd::expect_success 'oc create -f examples/sample-app/application-template-custombuild.json'
+os::cmd::expect_failure_and_text 'oc new-app ruby-helloworld-sample' 'deploymentconfig "frontend" created'
+os::cmd::expect_success_and_text 'oc get all --all-namespaces' 'cmd-basicresources[\ ]+bc\/ruby\-sample\-build'
+echo "resource printer namespace handling: ok"
 os::test::junit::declare_suite_end
 
 # Test OAuth access token describer
