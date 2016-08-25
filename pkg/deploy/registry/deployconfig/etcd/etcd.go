156aef00629f34fb5c178af7b4da7459485266d9
yuexiao-wang <wang.yuexiao@zte.com.cn>
2016-08-25 19:32:22 +0800

    Fix a func name and return value in the comment of NewREST
    Signed-off-by: yuexiao-wang <wang.yuexiao@zte.com.cn>

--- pkg/deploy/registry/deployconfig/etcd/etcd.go
+++ pkg/deploy/registry/deployconfig/etcd/etcd.go
@@ -25,8 +25,9 @@ type REST struct {
 	*registry.Store
 }
 
-// NewStorage returns a DeploymentConfigStorage containing the REST storage for
-// DeploymentConfig objects and their Scale subresources.
+// NewREST returns a deploymentConfigREST containing the REST storage for DeploymentConfig objects,
+// a statusREST containing the REST storage for changing the status of a DeploymentConfig,
+// and a scaleREST containing the REST storage for the Scale subresources of DeploymentConfigs.
 func NewREST(optsGetter restoptions.Getter, rcNamespacer kclient.ReplicationControllersNamespacer) (*REST, *StatusREST, *ScaleREST, error) {
 	prefix := "/deploymentconfigs"
 
