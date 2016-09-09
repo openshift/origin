eba6986924b488ce95be302d00f29b3b5b4a0593
Clayton Coleman <ccoleman@redhat.com>
2016-09-08 22:16:15 -0400

    Changes to util package reflected in tests

--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -195,8 +195,12 @@ func (l *mockLimiter) Accept() {
 
 func (l *mockLimiter) Stop() {}
 
-func (t *mockLimiter) Saturation() float64 {
-	return float64(0.0)
+func (l *mockLimiter) Saturation() float64 {
+	return 0.0
+}
+
+func (l *mockLimiter) QPS() float32 {
+	return 0.0
 }
 
 type testObj struct {
