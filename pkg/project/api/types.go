d308245ae34264bb230ce714b488e4ca650cf24b
Michal Fojtik <mfojtik@redhat.com>
2016-09-14 14:04:03 +0200

    make projects non-namespaced

--- pkg/project/api/types.go
+++ pkg/project/api/types.go
@@ -28,7 +28,7 @@ type ProjectStatus struct {
 	Phase kapi.NamespacePhase
 }
 
-// +genclient=true
+// +genclient=true,nonNamespaced=true
 
 // Project is a logical top-level container for a set of origin resources
 type Project struct {
