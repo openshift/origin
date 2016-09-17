783070e267385b3bd798b2121799cfc37733f11b
Maru Newby <marun@redhat.com>
2016-09-17 03:17:52 +0000

    Re-enable the networking tests.

--- test/extended/networking.sh
+++ test/extended/networking.sh
@@ -4,11 +4,6 @@
 # documentation.
 source "$(dirname "${BASH_SOURCE}")/../../hack/lib/init.sh"
 
-echo "+"
-echo "+ FIXME: Networking tests are disabled due to a broken configuration"
-echo "+"
-exit 0
-
 if [[ -n "${OPENSHIFT_VERBOSE_OUTPUT:-}" ]]; then
   set -o xtrace
   export PS4='+ \D{%b %d %H:%M:%S} $(basename ${BASH_SOURCE}):${LINENO} ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
@@ -41,6 +36,10 @@ DEFAULT_SKIP_LIST=(
   "should work after restarting kube-proxy"
   "should work after restarting apiserver"
   "should be able to change the type and ports of a service"
+
+  # Skip tests broken by #10777 for now
+  "should provide unchanging, static URL paths for kubernetes api services"
+  "should preserve source pod IP for traffic thru service cluster IP"
 )
 
 CLUSTER_CMD="${OS_ROOT}/hack/dind-cluster.sh"
