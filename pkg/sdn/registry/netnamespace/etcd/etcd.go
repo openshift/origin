openshift/origin

commit 58e8f93d86a5a877adc18fab8a96a9346c8c0c9a

Refactor NewConfigGetter again to get closer to correct behavior

pkg/sdn/registry/netnamespace/etcd/etcd.go
@@ -19,7 +19,7 @@ type REST struct {
 	registry.Store
 }
 
-const etcdPrefix = "/registry/sdnnetnamespaces"
+const etcdPrefix = "/r"
 
 // NewREST returns a RESTStorage object that will work against netnamespaces
 func NewREST(optsGetter restoptions.Getter) (*REST, error) {
@@ -44,7 +44,7 @@ func NewREST(optsGetter restoptions.Getter) (*REST, error) {
 		UpdateStrategy: netnamespace.Strategy,
 	}
 
-	if err := restoptions.ApplyOptions(optsGetter, store, etcdPrefix, storage.NoTriggerPublisher); err != nil {
+	if err := restoptions.ApplyOptions(optsGetter, store, false, storage.NoTriggerPublisher); err != nil {
 		return nil, err
 	}
 

pkg/authorization/registry/clusterpolicy/etcd/etcd.go
@@ -1,7 +1,6 @@
 package etcd
 
 import (
-	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/registry/generic"
@@ -11,11 +10,8 @@ import (
 
 	authorizationapi "github.com/openshift/origin/pkg/authorization/api"
 	"github.com/openshift/origin/pkg/authorization/registry/clusterpolicy"
-	"github.com/openshift/origin/pkg/util"
 	"github.com/openshift/origin/pkg/util/restoptions"
 )
-
-const ClusterPolicyPath = "/authorization/cluster/policies"
 
 type REST struct {
 	*registry.Store
@@ -28,12 +24,6 @@ func NewStorage(optsGetter restoptions.Getter) (*REST, error) {
 		NewFunc:           func() runtime.Object { return &authorizationapi.ClusterPolicy{} },
 		NewListFunc:       func() runtime.Object { return &authorizationapi.ClusterPolicyList{} },
 		QualifiedResource: authorizationapi.Resource("clusterpolicies"),
-		KeyRootFunc: func(ctx kapi.Context) string {
-			return ClusterPolicyPath
-		},
-		KeyFunc: func(ctx kapi.Context, id string) (string, error) {
-			return util.NoNamespaceKeyFunc(ctx, ClusterPolicyPath, id)
-		},
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*authorizationapi.ClusterPolicy).Name, nil
 		},
@@ -45,7 +35,7 @@ func NewStorage(optsGetter restoptions.Getter) (*REST, error) {
 		UpdateStrategy: clusterpolicy.Strategy,
 	}
 
-	if err := restoptions.ApplyOptions(optsGetter, store, ClusterPolicyPath, storage.NoTriggerPublisher); err != nil {
+	if err := restoptions.ApplyOptions(optsGetter, store, false, storage.NoTriggerPublisher); err != nil {
 		return nil, err
 	}
 

pkg/authorization/registry/clusterpolicybinding/etcd/etcd.go
@@ -1,7 +1,6 @@
 package etcd
 
 import (
-	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/registry/generic"
@@ -11,11 +10,8 @@ import (
 
 	authorizationapi "github.com/openshift/origin/pkg/authorization/api"
 	"github.com/openshift/origin/pkg/authorization/registry/clusterpolicybinding"
-	"github.com/openshift/origin/pkg/util"
 	"github.com/openshift/origin/pkg/util/restoptions"
 )
-
-const ClusterPolicyBindingPath = "/authorization/cluster/policybindings"
 
 type REST struct {
 	*registry.Store
@@ -28,12 +24,6 @@ func NewStorage(optsGetter restoptions.Getter) (*REST, error) {
 		NewFunc:           func() runtime.Object { return &authorizationapi.ClusterPolicyBinding{} },
 		NewListFunc:       func() runtime.Object { return &authorizationapi.ClusterPolicyBindingList{} },
 		QualifiedResource: authorizationapi.Resource("clusterpolicybindings"),
-		KeyRootFunc: func(ctx kapi.Context) string {
-			return ClusterPolicyBindingPath
-		},
-		KeyFunc: func(ctx kapi.Context, id string) (string, error) {
-			return util.NoNamespaceKeyFunc(ctx, ClusterPolicyBindingPath, id)
-		},
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*authorizationapi.ClusterPolicyBinding).Name, nil
 		},
@@ -45,7 +35,7 @@ func NewStorage(optsGetter restoptions.Getter) (*REST, error) {
 		UpdateStrategy: clusterpolicybinding.Strategy,
 	}
 
-	if err := restoptions.ApplyOptions(optsGetter, store, ClusterPolicyBindingPath, storage.NoTriggerPublisher); err != nil {
+	if err := restoptions.ApplyOptions(optsGetter, store, false, storage.NoTriggerPublisher); err != nil {
 		return nil, err
 	}
 

pkg/authorization/registry/policy/etcd/etcd.go
@@ -1,7 +1,6 @@
 package etcd
 
 import (
-	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/registry/generic"
@@ -14,8 +13,6 @@ import (
 	"github.com/openshift/origin/pkg/util/restoptions"
 )
 
-const PolicyPath = "/authorization/local/policies"
-
 type REST struct {
 	*registry.Store
 }
@@ -26,12 +23,6 @@ func NewStorage(optsGetter restoptions.Getter) (*REST, error) {
 		NewFunc:           func() runtime.Object { return &authorizationapi.Policy{} },
 		NewListFunc:       func() runtime.Object { return &authorizationapi.PolicyList{} },
 		QualifiedResource: authorizationapi.Resource("policies"),
-		KeyRootFunc: func(ctx kapi.Context) string {
-			return registry.NamespaceKeyRootFunc(ctx, PolicyPath)
-		},
-		KeyFunc: func(ctx kapi.Context, id string) (string, error) {
-			return registry.NamespaceKeyFunc(ctx, PolicyPath, id)
-		},
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*authorizationapi.Policy).Name, nil
 		},
@@ -43,7 +34,7 @@ func NewStorage(optsGetter restoptions.Getter) (*REST, error) {
 		UpdateStrategy: policy.Strategy,
 	}
 
-	if err := restoptions.ApplyOptions(optsGetter, store, PolicyPath, storage.NoTriggerPublisher); err != nil {
+	if err := restoptions.ApplyOptions(optsGetter, store, true, storage.NoTriggerPublisher); err != nil {
 		return nil, err
 	}
 

pkg/authorization/registry/policybinding/etcd/etcd.go
@@ -1,7 +1,6 @@
 package etcd
 
 import (
-	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/registry/generic"
@@ -14,8 +13,6 @@ import (
 	"github.com/openshift/origin/pkg/util/restoptions"
 )
 
-const PolicyBindingPath = "/authorization/local/policybindings"
-
 type REST struct {
 	*registry.Store
 }
@@ -26,12 +23,6 @@ func NewStorage(optsGetter restoptions.Getter) (*REST, error) {
 		NewFunc:           func() runtime.Object { return &authorizationapi.PolicyBinding{} },
 		NewListFunc:       func() runtime.Object { return &authorizationapi.PolicyBindingList{} },
 		QualifiedResource: authorizationapi.Resource("policybindings"),
-		KeyRootFunc: func(ctx kapi.Context) string {
-			return registry.NamespaceKeyRootFunc(ctx, PolicyBindingPath)
-		},
-		KeyFunc: func(ctx kapi.Context, id string) (string, error) {
-			return registry.NamespaceKeyFunc(ctx, PolicyBindingPath, id)
-		},
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*authorizationapi.PolicyBinding).Name, nil
 		},
@@ -43,7 +34,7 @@ func NewStorage(optsGetter restoptions.Getter) (*REST, error) {
 		UpdateStrategy: policybinding.Strategy,
 	}
 
-	if err := restoptions.ApplyOptions(optsGetter, store, PolicyBindingPath, storage.NoTriggerPublisher); err != nil {
+	if err := restoptions.ApplyOptions(optsGetter, store, true, storage.NoTriggerPublisher); err != nil {
 		return nil, err
 	}
 

pkg/build/registry/build/etcd/etcd.go
@@ -21,18 +21,11 @@ type REST struct {
 
 // NewStorage returns a RESTStorage object that will work against Build objects.
 func NewREST(optsGetter restoptions.Getter) (*REST, *DetailsREST, error) {
-	prefix := "/builds"
 
 	store := &registry.Store{
 		NewFunc:           func() runtime.Object { return &api.Build{} },
 		NewListFunc:       func() runtime.Object { return &api.BuildList{} },
 		QualifiedResource: api.Resource("builds"),
-		KeyRootFunc: func(ctx kapi.Context) string {
-			return registry.NamespaceKeyRootFunc(ctx, prefix)
-		},
-		KeyFunc: func(ctx kapi.Context, id string) (string, error) {
-			return registry.NamespaceKeyFunc(ctx, prefix, id)
-		},
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*api.Build).Name, nil
 		},
@@ -46,7 +39,7 @@ func NewREST(optsGetter restoptions.Getter) (*REST, *DetailsREST, error) {
 		ReturnDeletedObject: false,
 	}
 
-	if err := restoptions.ApplyOptions(optsGetter, store, prefix, storage.NoTriggerPublisher); err != nil {
+	if err := restoptions.ApplyOptions(optsGetter, store, true, storage.NoTriggerPublisher); err != nil {
 		return nil, nil, err
 	}
 

pkg/build/registry/buildconfig/etcd/etcd.go
@@ -1,7 +1,6 @@
 package etcd
 
 import (
-	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/registry/generic"
@@ -20,18 +19,10 @@ type REST struct {
 
 // NewStorage returns a RESTStorage object that will work against nodes.
 func NewREST(optsGetter restoptions.Getter) (*REST, error) {
-	prefix := "/buildconfigs"
-
 	store := &registry.Store{
 		NewFunc:           func() runtime.Object { return &api.BuildConfig{} },
 		NewListFunc:       func() runtime.Object { return &api.BuildConfigList{} },
 		QualifiedResource: api.Resource("buildconfigs"),
-		KeyRootFunc: func(ctx kapi.Context) string {
-			return registry.NamespaceKeyRootFunc(ctx, prefix)
-		},
-		KeyFunc: func(ctx kapi.Context, id string) (string, error) {
-			return registry.NamespaceKeyFunc(ctx, prefix, id)
-		},
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*api.BuildConfig).Name, nil
 		},
@@ -45,7 +36,7 @@ func NewREST(optsGetter restoptions.Getter) (*REST, error) {
 		ReturnDeletedObject: false,
 	}
 
-	if err := restoptions.ApplyOptions(optsGetter, store, prefix, storage.NoTriggerPublisher); err != nil {
+	if err := restoptions.ApplyOptions(optsGetter, store, true, storage.NoTriggerPublisher); err != nil {
 		return nil, err
 	}
 

pkg/deploy/registry/deployconfig/etcd/etcd.go
@@ -29,18 +29,10 @@ type REST struct {
 // NewStorage returns a DeploymentConfigStorage containing the REST storage for
 // DeploymentConfig objects and their Scale subresources.
 func NewREST(optsGetter restoptions.Getter, rcNamespacer kclient.ReplicationControllersNamespacer) (*REST, *StatusREST, *ScaleREST, error) {
-	prefix := "/deploymentconfigs"
-
 	store := &registry.Store{
 		NewFunc:           func() runtime.Object { return &api.DeploymentConfig{} },
 		NewListFunc:       func() runtime.Object { return &api.DeploymentConfigList{} },
 		QualifiedResource: api.Resource("deploymentconfigs"),
-		KeyRootFunc: func(ctx kapi.Context) string {
-			return registry.NamespaceKeyRootFunc(ctx, prefix)
-		},
-		KeyFunc: func(ctx kapi.Context, id string) (string, error) {
-			return registry.NamespaceKeyFunc(ctx, prefix, id)
-		},
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*api.DeploymentConfig).Name, nil
 		},
@@ -53,7 +45,7 @@ func NewREST(optsGetter restoptions.Getter, rcNamespacer kclient.ReplicationCont
 		ReturnDeletedObject: false,
 	}
 
-	if err := restoptions.ApplyOptions(optsGetter, store, prefix, storage.NoTriggerPublisher); err != nil {
+	if err := restoptions.ApplyOptions(optsGetter, store, true, storage.NoTriggerPublisher); err != nil {
 		return nil, nil, nil, err
 	}
 

commit 798e00829fe9add36eaab3330148d2810c82a840

Fix NetNamespaces, which got accidentally broken in 58e8f93

pkg/sdn/registry/netnamespace/etcd/etcd.go
@@ -1,7 +1,6 @@
 package etcd
 
 import (
-	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/registry/generic"
@@ -19,19 +18,11 @@ type REST struct {
 	registry.Store
 }
 
-const etcdPrefix = "/r"
-
 // NewREST returns a RESTStorage object that will work against netnamespaces
 func NewREST(optsGetter restoptions.Getter) (*REST, error) {
 	store := &registry.Store{
 		NewFunc:     func() runtime.Object { return &api.NetNamespace{} },
 		NewListFunc: func() runtime.Object { return &api.NetNamespaceList{} },
-		KeyRootFunc: func(ctx kapi.Context) string {
-			return etcdPrefix
-		},
-		KeyFunc: func(ctx kapi.Context, name string) (string, error) {
-			return registry.NoNamespaceKeyFunc(ctx, etcdPrefix, name)
-		},
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*api.NetNamespace).NetName, nil
 		},