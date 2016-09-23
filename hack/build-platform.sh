d94e26cbd138ae862e50f6bf1b0f2aaee576d48a
Adam Miller <maxamillion@fedoraproject.org>
2016-09-23 12:31:53 -0500

    use case statement for host/arch check, fix quoting on array expansion, update to :- functionality
    Signed-off-by: Adam Miller <maxamillion@fedoraproject.org>

--- hack/build-platform.sh
+++ hack/build-platform.sh
@@ -7,35 +7,41 @@ source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
 source <(go env)
 platforms=( "${GOHOSTOS}/${GOARCH}" )
-if [[ -n "${OS_ONLY_BUILD_PLATFORMS-}" ]]; then
+if [[ -n "${OS_ONLY_BUILD_PLATFORMS:-}" ]]; then
   filtered=()
-  for platform in ${platforms[@]}; do
+  for platform in "${platforms[@]}"; do
     if [[ "${platform}" =~ "${OS_ONLY_BUILD_PLATFORMS}" ]]; then
       filtered+=("${platform}")
     fi
   done
   platforms=("${filtered[@]}")
-  echo "DEBUGGING: ${filtered[@]}"
 fi
 
 # Build the primary client/server for the host platform
 OS_BUILD_PLATFORMS=("${platforms[@]}")
-if [[ "${GOHOSTOS}/${GOARCH}" == "linux/amd64" ]]; then
+case "${GOHOSTOS}/${GOARCH}" in
+  "linux/amd64")
     OS_GOFLAGS_LINUX_AMD64="-tags=gssapi" os::build::build_binaries "${OS_CROSS_COMPILE_TARGETS[@]}"
-elif [[ "${GOHOSTOS}/${GOARCH}" == "linux/arm64" ]]; then
+    ;;
+  "linux/arm64")
     OS_GOFLAGS_LINUX_ARM64="-tags=gssapi" os::build::build_binaries "${OS_CROSS_COMPILE_TARGETS[@]}"
-elif [[ "${GOHOSTOS}/${GOARCH}" == "linux/386" ]]; then
+    ;;
+  "linux/386")
     OS_GOFLAGS_LINUX_386="-tags=gssapi" os::build::build_binaries "${OS_CROSS_COMPILE_TARGETS[@]}"
-elif [[ "${GOHOSTOS}/${GOARCH}" == "linux/arm" ]]; then
+    ;;
+  "linux/arm")
     OS_GOFLAGS_LINUX_ARM="-tags=gssapi" os::build::build_binaries "${OS_CROSS_COMPILE_TARGETS[@]}"
-elif [[ "${GOHOSTOS}/${GOARCH}" == "linux/ppc64" ]]; then
+    ;;
+  "linux/ppc64")
     OS_GOFLAGS_LINUX_PPC64="-tags=gssapi" os::build::build_binaries "${OS_CROSS_COMPILE_TARGETS[@]}"
-elif [[ "${GOHOSTOS}/${GOARCH}" == "linux/ppc64le" ]]; then
+    ;;
+  "linux/ppc64le")
     OS_GOFLAGS_LINUX_PPC64LE="-tags=gssapi" os::build::build_binaries "${OS_CROSS_COMPILE_TARGETS[@]}"
-fi
+    ;;
+esac
 
 # Pass the necessary tags
-OS_GOFLAGS="${OS_GOFLAGS:-} ${OS_IMAGE_COMPILE_GOFLAGS}" os::build::build_static_binaries "${OS_IMAGE_COMPILE_TARGETS[@]-}" "${OS_SCRATCH_IMAGE_COMPILE_TARGETS[@]-}"
+OS_GOFLAGS="${OS_GOFLAGS:-} ${OS_IMAGE_COMPILE_GOFLAGS}" os::build::build_static_binaries "${OS_IMAGE_COMPILE_TARGETS[@]:-}" "${OS_SCRATCH_IMAGE_COMPILE_TARGETS[@]:-}"
 
 # Make the primary client/server release.
 OS_RELEASE_ARCHIVE="openshift-origin"
