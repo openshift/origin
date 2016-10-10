442adafdc04cd98b1c970400d8db03a2740bc2f3
Michail Kargakis <mkargaki@redhat.com>
2016-10-10 16:49:32 +0200

    oc: update valid arguments for upstream commands

--- pkg/cmd/cli/cmd/rollout/rollout.go
+++ pkg/cmd/cli/cmd/rollout/rollout.go
@@ -72,6 +72,7 @@ func NewCmdRolloutHistory(fullName string, f *clientcmd.Factory, out io.Writer) 
 	cmd := rollout.NewCmdRolloutHistory(f.Factory, out)
 	cmd.Long = rolloutHistoryLong
 	cmd.Example = fmt.Sprintf(rolloutHistoryExample, fullName)
+	cmd.ValidArgs = append(cmd.ValidArgs, "deploymentconfig")
 	return cmd
 }
 
@@ -93,6 +94,7 @@ func NewCmdRolloutPause(fullName string, f *clientcmd.Factory, out io.Writer) *c
 	cmd := rollout.NewCmdRolloutPause(f.Factory, out)
 	cmd.Long = rolloutPauseLong
 	cmd.Example = fmt.Sprintf(rolloutPauseExample, fullName)
+	cmd.ValidArgs = append(cmd.ValidArgs, "deploymentconfig")
 	return cmd
 }
 
@@ -112,6 +114,7 @@ func NewCmdRolloutResume(fullName string, f *clientcmd.Factory, out io.Writer) *
 	cmd := rollout.NewCmdRolloutResume(f.Factory, out)
 	cmd.Long = rolloutResumeLong
 	cmd.Example = fmt.Sprintf(rolloutResumeExample, fullName)
+	cmd.ValidArgs = append(cmd.ValidArgs, "deploymentconfig")
 	return cmd
 }
 
@@ -148,5 +151,6 @@ func NewCmdRolloutUndo(fullName string, f *clientcmd.Factory, out io.Writer) *co
 	cmd := rollout.NewCmdRolloutUndo(f.Factory, out)
 	cmd.Long = rolloutUndoLong
 	cmd.Example = fmt.Sprintf(rolloutUndoExample, fullName)
+	cmd.ValidArgs = append(cmd.ValidArgs, "deploymentconfig")
 	return cmd
 }
