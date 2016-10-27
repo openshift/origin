openshift/origin

commit cf69a41bfed068adcd54b00bcf856120c0273c48

sdn: convert pod network setup to a CNI plugin
Use the same kubelet network plugin interfaces as everyone else.  This converts the
openshift-sdn plugin from a plugin compiled into kubelet to one that uses the
kubelet CNI driver to call a standard CNI plugin.  This plugin sends requests
from kubelet over a root-only unix domain socket back to the openshift-sdn
node process which handles the actual pod setup/teardown operations.

We want to consolidate these operations inside the node process instead of
leaving them to the CNI plugin itself because we need to ensure serialized
access to OVS, and we need a long-running process to handle HostPort reservation.
While we could serialize operations for each pod rather than serializing
all pod operations, it turns out to be difficult and error-prone to ensure
previous operations complete and those operations can still be GCed in a
race-safe manner.

General flow:

1) kubelet wants to set up pod networking
2) kubelet calls internal CNI driver
3) CNI driver looks for CNI network config files, finds
/etc/cni/net.d/80-openshift-sdn.conf, and calls the
/opt/cni/bin/openshift-sdn CNI plugin executable with CNI_COMMAND=ADD
4) openshift-sdn CNI plugin sends environment and stdin to the
openshift-node process via HTTP over a root-only unix domain socket
5) openshift-node process sets up pod networking with OVS, veth
creation
6) openshift-node process calls the CNI 'host-local' IPAM plugin to
allocate an IP address for the pod from the local node subnet
7) openshift-node process returns the IPAM details via HTTP over the
unix domain socket to the waiting openshift-sdn CNI plugin
8) openshift-sdn CNI plugin prints IPAM details to stdout
9) kubelet reads IPAM details (or error) and completes pod setup

pkg/sdn/plugin/node.go
@@ -9,6 +9,8 @@ import (
 
 	log "github.com/golang/glog"
 
+	"github.com/openshift/origin/pkg/sdn/plugin/cniserver"
+
 	osclient "github.com/openshift/origin/pkg/client"
 	osapi "github.com/openshift/origin/pkg/sdn/api"
 	"github.com/openshift/origin/pkg/util/ipcmd"
@@ -34,6 +36,7 @@ type OsdnNode struct {
 	osClient           *osclient.Client
 	ovs                *ovs.Interface
 	networkInfo        *NetworkInfo
+	podManager         *podManager
 	localSubnetCIDR    string
 	localIP            string
 	hostName           string
@@ -42,8 +45,9 @@ type OsdnNode struct {
 	iptablesSyncPeriod time.Duration
 	mtu                uint32
 	egressPolicies     map[uint32][]*osapi.EgressNetworkPolicy
-	host               knetwork.Host
-	cniConfig          []byte
+
+	host             knetwork.Host
+	kubeletCniPlugin knetwork.NetworkPlugin
 
 	clearLbr0IptablesRule bool
 }
@@ -199,6 +203,11 @@ func (node *OsdnNode) Start() error {
 		}
 	}
 
+	node.podManager, err = newPodManager(node.host, node.multitenant, node.localSubnetCIDR, node.networkInfo, node.kClient, node.vnids, node.mtu)
+	if err != nil {
+		return err
+	}
+
 	if networkChanged {
 		var pods []kapi.Pod
 		pods, err = node.GetLocalPods(kapi.NamespaceAll)
@@ -214,9 +223,30 @@ func (node *OsdnNode) Start() error {
 		}
 	}
 
+	if err := node.podManager.Start(cniserver.CNIServerSocketPath); err != nil {
+		return err
+	}
+
 	node.markPodNetworkReady()
 
 	return nil
+}
+
+// FIXME: this should eventually go into kubelet via a CNI UPDATE/CHANGE action
+// See https://github.com/containernetworking/cni/issues/89
+func (node *OsdnNode) UpdatePod(namespace string, name string, id kubeletTypes.ContainerID) error {
+	req := &cniserver.PodRequest{
+		Command:      cniserver.CNI_UPDATE,
+		PodNamespace: namespace,
+		PodName:      name,
+		ContainerId:  id.String(),
+		// netns is read from docker if needed, since we don't get it from kubelet
+		Result: make(chan *cniserver.PodResult),
+	}
+
+	// Send request and wait for the result
+	_, err := node.podManager.handleCNIRequest(req)
+	return err
 }
 
 func (node *OsdnNode) GetLocalPods(namespace string) ([]kapi.Pod, error) {
@@ -244,18 +274,11 @@ func (node *OsdnNode) markPodNetworkReady() {
 	close(node.podNetworkReady)
 }
 
-func (node *OsdnNode) WaitForPodNetworkReady() error {
-	logInterval := 10 * time.Second
-	numIntervals := 12 // timeout: 2 mins
-
-	for i := 0; i < numIntervals; i++ {
-		select {
-		// Wait for StartNode() to finish SDN setup
-		case <-node.podNetworkReady:
-			return nil
-		case <-time.After(logInterval):
-			log.Infof("Waiting for SDN pod network to be ready...")
-		}
+func (node *OsdnNode) IsPodNetworkReady() error {
+	select {
+	case <-node.podNetworkReady:
+		return nil
+	default:
+		return fmt.Errorf("SDN pod network is not ready")
 	}
-	return fmt.Errorf("SDN pod network is not ready(timeout: 2 mins)")
 }

contrib/node/install-sdn.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 os::provision::install-sdn() {
-  local default_target="/usr"
-
   local deployed_root=$1
-  local target=${2:-${default_target}}
+  local binaries_path=$2
+  local target=${3:-}
+  local target_usrdir="${target}/usr"
+  local target_bindir="${target_usrdir}/bin"
+  local target_confdir="${target}/etc/cni/net.d"
+  local target_cnidir="${target}/opt/cni/bin"
 
-  if [ ! -d ${target} ]; then
-    mkdir -p ${target}
-  fi
+  mkdir -p -m u+rwx,g+rwx,o+rx "${target_usrdir}"
+  mkdir -p -m u+rwx,g+rwx,o+rx "${target_bindir}"
+  mkdir -p -m u+rwx,g+rwx,o+rx "${target_confdir}"
+  mkdir -p -m u+rwx,g+rwx,o+rx "${target_cnidir}"
 
   local osdn_plugin_path="${deployed_root}/pkg/sdn/plugin"
-  mkdir -p "${target}/bin/"
-  pushd "${osdn_plugin_path}" > /dev/null
-    install bin/openshift-sdn-ovs "${target}/bin/"
-  popd > /dev/null
+  install -m u+rwx,g+rwx,o+rx "${osdn_plugin_path}/bin/openshift-sdn-ovs" "${target_bindir}"
+  install -m u+rw,g+rw,o+r "${osdn_plugin_path}/sdn-cni-plugin/80-openshift-sdn.conf" "${target_confdir}"
 
-  # Assume a non-default target is an indication of deploying in an
-  # environment where openvswitch is managed in a separate container
-  # (e.g. atomic host).
-  if [[ "${target}" = "${default_target}" ]]; then
+  install -m u+rwx,g+rwx,o+rx "${binaries_path}/sdn-cni-plugin" "${target_cnidir}/openshift-sdn"
+  install -m u+rwx,g+rwx,o+rx "${binaries_path}/host-local" "${target_cnidir}"
+  install -m u+rwx,g+rwx,o+rx "${binaries_path}/loopback" "${target_cnidir}"
+
+  # Assume an empty/default target is an indication of deploying in an
+  # environment where openvswitch should be started by us
+  if [[ -z "${target}" ]]; then
     systemctl enable openvswitch
     systemctl start openvswitch
   fi

contrib/vagrant/provision-util.sh
@@ -49,7 +49,7 @@ os::provision::base-install() {
 
   echo "Installing openshift"
   os::provision::install-cmds "${origin_root}"
-  os::provision::install-sdn "${origin_root}"
+  os::provision::install-sdn "${origin_root}" "$(os::build::get-bin-output-path "${OS_ROOT}")"
   os::provision::set-os-env "${origin_root}" "${config_root}"
 }
 

hack/build-go.sh
@@ -4,11 +4,13 @@
 STARTTIME=$(date +%s)
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
+build_targets=("$@")
+platform="$(os::build::host_platform)"
+
 # only works on Linux for now, all other platforms must build binaries themselves
 if [[ -z "$@" ]]; then
   if [[ "${OS_RELEASE:-}" != "n" ]] && \
     os::build::detect_local_release_tars $(os::build::host_platform_friendly) >/dev/null; then
-    platform=$(os::build::host_platform)
     echo "++ Using release artifacts from ${OS_RELEASE_COMMIT} for ${platform} instead of building"
     mkdir -p "${OS_OUTPUT_BINPATH}/${platform}"
     os::build::extract_tar "${OS_PRIMARY_RELEASE_TAR}" "${OS_OUTPUT_BINPATH}/${platform}"
@@ -19,10 +21,17 @@ if [[ -z "$@" ]]; then
 
     ret=$?; ENDTIME=$(date +%s); echo "$0 took $(($ENDTIME - $STARTTIME)) seconds"; exit "$ret"
   fi
+
+  build_targets=("${OS_ALL_TARGETS[@]}")
+  # Also build SDN components on Linux by default
+  if [[ "${platform}" =~ linux/.* ]]; then
+    build_targets=("${build_targets[@]}" "${OS_SDN_COMPILE_TARGETS_LINUX[@]}")
+  fi
 fi
 
-os::build::build_binaries "$@"
-os::build::place_bins "$@"
+OS_BUILD_PLATFORMS=("${OS_BUILD_PLATFORMS[@]:-${platform}}")
+os::build::build_binaries "${build_targets[@]}"
+os::build::place_bins "${build_targets[@]}"
 os::build::make_openshift_binary_symlinks
 
 ret=$?; ENDTIME=$(date +%s); echo "$0 took $(($ENDTIME - $STARTTIME)) seconds"; exit "$ret"

hack/build-images.sh
@@ -77,7 +77,7 @@ ln_or_cp "${imagedir}/oc"              examples/gitserver/bin
 ln_or_cp "${imagedir}/dockerregistry"  images/dockerregistry/bin
 
 # Copy SDN scripts into images/node
-os::provision::install-sdn "${OS_ROOT}" "${OS_ROOT}/images/node"
+os::provision::install-sdn "${OS_ROOT}" "${imagedir}" "${OS_ROOT}/images/node"
 mkdir -p images/node/conf/
 cp -pf "${OS_ROOT}/contrib/systemd/openshift-sdn-ovs.conf" images/node/conf/
 

hack/common.sh
@@ -23,10 +23,16 @@ readonly -f os::build::host_platform
 
 readonly OS_IMAGE_COMPILE_PLATFORMS=("$(os::build::host_platform)")
 
+readonly OS_SDN_COMPILE_TARGETS_LINUX=(
+  pkg/sdn/plugin/sdn-cni-plugin
+  vendor/github.com/containernetworking/cni/plugins/ipam/host-local
+  vendor/github.com/containernetworking/cni/plugins/main/loopback
+)
 readonly OS_IMAGE_COMPILE_TARGETS=(
   images/pod
   cmd/dockerregistry
   cmd/gitserver
+  "${OS_SDN_COMPILE_TARGETS_LINUX[@]}"
 )
 readonly OS_IMAGE_COMPILE_GOFLAGS="-tags include_gcs"
 readonly OS_SCRATCH_IMAGE_COMPILE_TARGETS=(
@@ -356,14 +362,16 @@ function os::build::export_targets() {
   done
 
   if [[ ${#targets[@]} -eq 0 ]]; then
-    targets=("${OS_ALL_TARGETS[@]}")
+    echo "No targets to export!"
+    exit 1
   fi
 
   binaries=($(os::build::binaries_from_targets "${targets[@]}"))
 
   platforms=("${OS_BUILD_PLATFORMS[@]:+${OS_BUILD_PLATFORMS[@]}}")
   if [[ ${#platforms[@]} -eq 0 ]]; then
-    platforms=("$(os::build::host_platform)")
+    echo "No platforms to build for!"
+    exit 1
   fi
 }
 readonly -f os::build::export_targets

hack/dind-cluster.sh
@@ -201,8 +201,11 @@ function copy-runtime() {
 
   cp "$(os::build::find-binary openshift)" "${target}"
   cp "$(os::build::find-binary host-local)" "${target}"
-  local osdn_plugin_path="${origin_root}/pkg/sdn/plugin/bin"
-  cp "${osdn_plugin_path}/openshift-sdn-ovs" "${target}"
+  cp "$(os::build::find-binary loopback)" "${target}"
+  cp "$(os::build::find-binary sdn-cni-plugin)" "${target}/openshift-sdn"
+  local osdn_plugin_path="${origin_root}/pkg/sdn/plugin"
+  cp "${osdn_plugin_path}/bin/openshift-sdn-ovs" "${target}"
+  cp "${osdn_plugin_path}/sdn-cni-plugin/80-openshift-sdn.conf" "${target}"
 }
 
 function wait-for-cluster() {

images/dind/node/Dockerfile
@@ -47,6 +47,7 @@ COPY openshift-enable-ssh-access.service /etc/systemd/system/
 RUN systemctl enable openshift-enable-ssh-access.service
 
 # SDN plugin setup
+RUN mkdir -p /etc/cni/net.d
 RUN mkdir -p /opt/cni/bin
 
 # Symlink from the data path intended to be mounted as a volume to
@@ -63,6 +64,9 @@ RUN ln -sf /data/openshift-sdn-ovs /usr/local/bin/ && \
     ln -sf /data/openshift /usr/local/bin/openshift-docker-build && \
     ln -sf /data/openshift /usr/local/bin/openshift-sti-build && \
     ln -sf /data/openshift /usr/local/bin/openshift-f5-router && \
-    ln -sf /data/host-local /opt/cni/bin/
+    ln -sf /data/openshift-sdn /opt/cni/bin/ && \
+    ln -sf /data/host-local    /opt/cni/bin/ && \
+    ln -sf /data/loopback      /opt/cni/bin/ && \
+    ln -sf /data/80-openshift-sdn.conf /etc/cni/net.d/
 
 ENV KUBECONFIG /data/openshift.local.config/master/admin.kubeconfig

commit d861f0630f5888756516277e6e5800a83089208c

sdn: start pod manager before trying to update if VNIDs have changed
Fixes: cf69a41bfed068adcd54b00bcf856120c0273c48

pkg/sdn/plugin/node.go
@@ -207,6 +207,9 @@ func (node *OsdnNode) Start() error {
 	if err != nil {
 		return err
 	}
+	if err := node.podManager.Start(cniserver.CNIServerSocketPath); err != nil {
+		return err
+	}
 
 	if networkChanged {
 		var pods []kapi.Pod
@@ -221,10 +224,6 @@ func (node *OsdnNode) Start() error {
 				log.Warningf("Could not update pod %q (%s): %s", p.Name, containerID, err)
 			}
 		}
-	}
-
-	if err := node.podManager.Start(cniserver.CNIServerSocketPath); err != nil {
-		return err
 	}
 
 	node.markPodNetworkReady()