a005e811484f262fbf133b38503aa5dcf89e4773
xiangpengzhao <zhao.xiangpeng@zte.com.cn>
2016-11-10 02:15:30 -0500

    Move test case TestGenerateGateway to common_test.go

--- pkg/util/netutils/common_test.go
+++ pkg/util/netutils/common_test.go
@@ -20,3 +20,23 @@ func TestConversion(t *testing.T) {
 		t.Fatal("Conversion back and forth failed")
 	}
 }
+
+func TestGenerateGateway(t *testing.T) {
+	sna, err := NewSubnetAllocator("10.1.0.0/16", 8, nil)
+	if err != nil {
+		t.Fatal("Failed to initialize IP allocator: ", err)
+	}
+
+	sn, err := sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.0.0/24" {
+		t.Fatalf("Did not get expected subnet (sn=%s)", sn.String())
+	}
+
+	gatewayIP := GenerateDefaultGateway(sn)
+	if gatewayIP.String() != "10.1.0.1" {
+		t.Fatalf("Did not get expected gateway IP Address (gatewayIP=%s)", gatewayIP.String())
+	}
+}
