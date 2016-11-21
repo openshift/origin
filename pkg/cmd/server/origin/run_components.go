be8b03d6f40aed4e76040a2a36d65188d88ffe3c
Ben Parees <bparees@redhat.com>
2016-11-21 13:46:47 -0500

    manage dockerfile

--- pkg/cmd/server/origin/run_components.go
+++ pkg/cmd/server/origin/run_components.go
@@ -236,6 +236,7 @@ func (c *MasterConfig) RunBuildController(informers shared.InformerFactory) erro
 	dockerImage := c.ImageFor("docker-builder")
 	stiImage := c.ImageFor("sti-builder")
 	gitCloneImage := c.ImageFor("git-clone")
+	manageDockerfileImage := c.ImageFor("manage-dockerfile")
 
 	storageVersion := c.Options.EtcdStorageConfig.OpenShiftStorageVersion
 	groupVersion := unversioned.GroupVersion{Group: "", Version: storageVersion}
@@ -262,8 +263,9 @@ func (c *MasterConfig) RunBuildController(informers shared.InformerFactory) erro
 		BuildUpdater: buildclient.NewOSClientBuildClient(osclient),
 		BuildLister:  buildclient.NewOSClientBuildClient(osclient),
 		DockerBuildStrategy: &buildstrategy.DockerBuildStrategy{
-			GitCloneImage: gitCloneImage,
-			Image:         dockerImage,
+			GitCloneImage:         gitCloneImage,
+			ManageDockerfileImage: manageDockerfileImage,
+			Image: dockerImage,
 			// TODO: this will be set to --storage-version (the internal schema we use)
 			Codec: codec,
 		},
