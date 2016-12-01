openshift/origin

commit 4b54d01c576da183068aefa86039b880542a356b

add failure reasons

pkg/build/controller/controller.go
@@ -11,6 +11,7 @@ import (
 	"k8s.io/kubernetes/pkg/client/cache"
 	kcoreclient "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/core/unversioned"
 	"k8s.io/kubernetes/pkg/client/record"
+	utilruntime "k8s.io/kubernetes/pkg/util/runtime"
 
 	builddefaults "github.com/openshift/origin/pkg/build/admission/defaults"
 	buildoverrides "github.com/openshift/origin/pkg/build/admission/overrides"
@@ -72,10 +73,12 @@ func (bc *BuildController) CancelBuild(build *buildapi.Build) error {
 	}
 
 	build.Status.Phase = buildapi.BuildPhaseCancelled
-	build.Status.Reason = ""
-	build.Status.Message = ""
 	now := unversioned.Now()
 	build.Status.CompletionTimestamp = &now
+	// set the status details for the cancelled build before updating the build
+	// object.
+	build.Status.Reason = buildapi.StatusReasonCancelledBuild
+	build.Status.Message = buildapi.StatusMessageCancelledBuild
 	if err := bc.BuildUpdater.Update(build.Namespace, build); err != nil {
 		return fmt.Errorf("Failed to update build %s/%s: %v", build.Namespace, build.Name, err)
 	}
@@ -111,6 +114,10 @@ func (bc *BuildController) HandleBuild(build *buildapi.Build) error {
 		glog.V(5).Infof("Marking build %s/%s as cancelled", build.Namespace, build.Name)
 		if err := bc.CancelBuild(build); err != nil {
 			build.Status.Reason = buildapi.StatusReasonCancelBuildFailed
+			build.Status.Message = buildapi.StatusMessageCancelBuildFailed
+			if err = bc.BuildUpdater.Update(build.Namespace, build); err != nil {
+				utilruntime.HandleError(fmt.Errorf("Failed to update build %s/%s: %v", build.Namespace, build.Name, err))
+			}
 			return fmt.Errorf("Failed to cancel build %s/%s: %v, will retry", build.Namespace, build.Name, err)
 		}
 	}
@@ -147,8 +154,6 @@ func (bc *BuildController) nextBuildPhase(build *buildapi.Build) error {
 	if build.Status.Cancelled {
 		glog.V(4).Infof("Cancelling build %s/%s.", build.Namespace, build.Name)
 		build.Status.Phase = buildapi.BuildPhaseCancelled
-		build.Status.Reason = ""
-		build.Status.Message = ""
 		return nil
 	}
 
@@ -156,6 +161,7 @@ func (bc *BuildController) nextBuildPhase(build *buildapi.Build) error {
 	ref, err := bc.resolveOutputDockerImageReference(build)
 	if err != nil {
 		build.Status.Reason = buildapi.StatusReasonInvalidOutputReference
+		build.Status.Message = buildapi.StatusMessageInvalidOutputRef
 		return err
 	}
 	build.Status.OutputDockerImageReference = ref
@@ -185,6 +191,7 @@ func (bc *BuildController) nextBuildPhase(build *buildapi.Build) error {
 	podSpec, err := bc.BuildStrategy.CreateBuildPod(buildCopy)
 	if err != nil {
 		build.Status.Reason = buildapi.StatusReasonCannotCreateBuildPodSpec
+		build.Status.Reason = buildapi.StatusMessageCannotCreateBuildPodSpec
 		if strategy.IsFatal(err) {
 			return strategy.FatalError(fmt.Sprintf("failed to create a build pod spec for build %s/%s: %v", build.Namespace, build.Name, err))
 		}
@@ -201,13 +208,14 @@ func (bc *BuildController) nextBuildPhase(build *buildapi.Build) error {
 
 	if _, err := bc.PodManager.CreatePod(build.Namespace, podSpec); err != nil {
 		if errors.IsAlreadyExists(err) {
-			bc.Recorder.Eventf(build, kapi.EventTypeWarning, "failedCreate", "Pod already exists: %s/%s", podSpec.Namespace, podSpec.Name)
+			bc.Recorder.Eventf(build, kapi.EventTypeWarning, "FailedCreate", "Pod already exists: %s/%s", podSpec.Namespace, podSpec.Name)
 			glog.V(4).Infof("Build pod already existed: %#v", podSpec)
 			return nil
 		}
 		// Log an event if the pod is not created (most likely due to quota denial).
 		bc.Recorder.Eventf(build, kapi.EventTypeWarning, "FailedCreate", "Error creating: %v", err)
 		build.Status.Reason = buildapi.StatusReasonCannotCreateBuildPod
+		build.Status.Message = buildapi.StatusMessageCannotCreateBuildPod
 		return fmt.Errorf("failed to create build pod: %v", err)
 	}
 	setBuildPodNameAnnotation(build, podSpec.Name)
@@ -294,20 +302,24 @@ func (bc *BuildPodController) HandlePod(pod *kapi.Pod) error {
 	case kapi.PodRunning:
 		// The pod's still running
 		build.Status.Reason = ""
+		build.Status.Message = ""
 		nextStatus = buildapi.BuildPhaseRunning
 	case kapi.PodPending:
 		build.Status.Reason = ""
+		build.Status.Message = ""
 		nextStatus = buildapi.BuildPhasePending
 		if secret := build.Spec.Output.PushSecret; secret != nil && currentReason != buildapi.StatusReasonMissingPushSecret {
 			if _, err := bc.SecretClient.Secrets(build.Namespace).Get(secret.Name); err != nil && errors.IsNotFound(err) {
 				build.Status.Reason = buildapi.StatusReasonMissingPushSecret
+				build.Status.Message = buildapi.StatusMessageMissingPushSecret
 				glog.V(4).Infof("Setting reason for pending build to %q due to missing secret %s/%s", build.Status.Reason, build.Namespace, secret.Name)
 			}
 		}
 	case kapi.PodSucceeded:
+		build.Status.Reason = ""
+		build.Status.Message = ""
 		// Check the exit codes of all the containers in the pod
 		nextStatus = buildapi.BuildPhaseComplete
-		build.Status.Reason = ""
 		if len(pod.Status.ContainerStatuses) == 0 {
 			// no containers in the pod means something went badly wrong, so the build
 			// should be failed.
@@ -322,8 +334,10 @@ func (bc *BuildPodController) HandlePod(pod *kapi.Pod) error {
 			}
 		}
 	case kapi.PodFailed:
-		build.Status.Reason = ""
 		nextStatus = buildapi.BuildPhaseFailed
+	default:
+		build.Status.Reason = ""
+		build.Status.Message = ""
 	}
 
 	// Update the build object when it progress to a next state or the reason for
@@ -336,7 +350,7 @@ func (bc *BuildPodController) HandlePod(pod *kapi.Pod) error {
 		}
 		glog.V(4).Infof("Updating build %s/%s status %s -> %s%s", build.Namespace, build.Name, build.Status.Phase, nextStatus, reason)
 		build.Status.Phase = nextStatus
-		build.Status.Message = ""
+
 		if buildutil.IsBuildComplete(build) {
 			now := unversioned.Now()
 			build.Status.CompletionTimestamp = &now
@@ -398,7 +412,7 @@ func (bc *BuildPodDeleteController) HandleBuildPodDeletion(pod *kapi.Pod) error 
 		glog.V(4).Infof("Updating build %s/%s status %s -> %s", build.Namespace, build.Name, build.Status.Phase, nextStatus)
 		build.Status.Phase = nextStatus
 		build.Status.Reason = buildapi.StatusReasonBuildPodDeleted
-		build.Status.Message = "The pod for this build was deleted before the build completed."
+		build.Status.Message = buildapi.StatusMessageBuildPodDeleted
 		now := unversioned.Now()
 		build.Status.CompletionTimestamp = &now
 		if err := bc.BuildUpdater.Update(build.Namespace, build); err != nil {

pkg/build/api/types.go
@@ -286,6 +286,46 @@ const (
 	// secret for pushing the output image.
 	// The build will stay in the pending state until the secret is created, or the build times out.
 	StatusReasonMissingPushSecret StatusReason = "MissingPushSecret"
+
+	// StatusReasonPostCommitHookFailed indicates the post-commit hook failed.
+	StatusReasonPostCommitHookFailed StatusReason = "PostCommitHookFailed"
+
+	// StatusReasonPushImageToRegistryFailed indicates that an image failed to be
+	// pushed to the registry.
+	StatusReasonPushImageToRegistryFailed StatusReason = "PushImageToRegistryFailed"
+
+	// StatusReasonPullBuilderImageFailed indicates that we failed to pull the
+	// builder image.
+	StatusReasonPullBuilderImageFailed StatusReason = "PullBuilderImageFailed"
+
+	// StatusReasonFetchSourceFailed indicates that fetching the source of the
+	// build has failed.
+	StatusReasonFetchSourceFailed StatusReason = "FetchSourceFailed"
+
+	// StatusReasonCancelledBuild indicates that the build was cancelled by the
+	// user.
+	StatusReasonCancelledBuild StatusReason = "CancelledBuild"
+
+	// StatusReasonDockerBuildFailed indicates that the docker build strategy has
+	// failed.
+	StatusReasonDockerBuildFailed StatusReason = "DockerBuildFailed"
+)
+
+// NOTE: These messages might change.
+const (
+	StatusMessageCannotCreateBuildPodSpec  = "Failed to create pod spec"
+	StatusMessageCannotCreateBuildPod      = "Failed creating build pod"
+	StatusMessageInvalidOutputRef          = "Output image could not be resolved"
+	StatusMessageCancelBuildFailed         = "Failed to cancel build"
+	StatusMessageBuildPodDeleted           = "The pod for this build was deleted before the build completed"
+	StatusMessageExceededRetryTimeout      = "Build did not complete and retrying timed out"
+	StatusMessageMissingPushSecret         = "Missing push secret"
+	StatusMessagePostCommitHookFailed      = "Build failed because of post commit hook"
+	StatusMessagePushImageToRegistryFailed = "Failed to push the image to the registry"
+	StatusMessagePullBuilderImageFailed    = "Failed pulling builder image"
+	StatusMessageFetchSourceFailed         = "Failed to fetch the input source"
+	StatusMessageCancelledBuild            = "The build was cancelled by the user"
+	StatusMessageDockerBuildFailed         = "Docker build strategy has failed"
 )
 
 // BuildSource is the input used for the build.

pkg/build/builder/common.go
@@ -10,6 +10,8 @@ import (
 	"github.com/docker/distribution/reference"
 	"github.com/fsouza/go-dockerclient"
 
+	kclient "k8s.io/kubernetes/pkg/client/unversioned"
+
 	"github.com/openshift/origin/pkg/build/api"
 	"github.com/openshift/origin/pkg/client"
 	"github.com/openshift/origin/pkg/generate/git"
@@ -20,7 +22,14 @@ import (
 // client facing libraries should not be using glog
 var glog = utilglog.ToFile(os.Stderr, 2)
 
-const OriginalSourceURLAnnotationKey = "openshift.io/original-source-url"
+const (
+	OriginalSourceURLAnnotationKey = "openshift.io/original-source-url"
+
+	// containerNamePrefix prefixes the name of containers launched by a build.
+	// We cannot reuse the prefix "k8s" because we don't want the containers to
+	// be managed by a kubelet.
+	containerNamePrefix = "openshift"
+)
 
 // KeyValue can be used to build ordered lists of key-value pairs.
 type KeyValue struct {
@@ -66,35 +75,6 @@ func buildInfo(build *api.Build) []KeyValue {
 	return kv
 }
 
-func updateBuildRevision(c client.BuildInterface, build *api.Build, sourceInfo *git.SourceInfo) {
-	if build.Spec.Revision != nil {
-		return
-	}
-	build.Spec.Revision = &api.SourceRevision{
-		Git: &api.GitSourceRevision{
-			Commit:  sourceInfo.CommitID,
-			Message: sourceInfo.Message,
-			Author: api.SourceControlUser{
-				Name:  sourceInfo.AuthorName,
-				Email: sourceInfo.AuthorEmail,
-			},
-			Committer: api.SourceControlUser{
-				Name:  sourceInfo.CommitterName,
-				Email: sourceInfo.CommitterEmail,
-			},
-		},
-	}
-
-	// Reset ResourceVersion to avoid a conflict with other updates to the build
-	build.ResourceVersion = ""
-
-	glog.V(4).Infof("Setting build revision to %#v", build.Spec.Revision.Git)
-	_, err := c.UpdateDetails(build)
-	if err != nil {
-		glog.V(0).Infof("error: An error occurred saving build revision: %v", err)
-	}
-}
-
 // randomBuildTag generates a random tag used for building images in such a way
 // that the built image can be referred to unambiguously even in the face of
 // concurrent builds with the same name in the same namespace.
@@ -107,11 +87,6 @@ func randomBuildTag(namespace, name string) string {
 	}
 	return fmt.Sprintf("%s:%s", repo, randomTag)
 }
-
-// containerNamePrefix prefixes the name of containers launched by a build. We
-// cannot reuse the prefix "k8s" because we don't want the containers to be
-// managed by a kubelet.
-const containerNamePrefix = "openshift"
 
 // containerName creates names for Docker containers launched by a build. It is
 // meant to resemble Kubernetes' pkg/kubelet/dockertools.BuildDockerName.
@@ -178,5 +153,49 @@ func execPostCommitHook(client DockerClient, postCommitSpec api.BuildPostCommitS
 		Follow:       true,
 		Stdout:       true,
 		Stderr:       true,
+	})
+}
+
+func updateBuildRevision(build *api.Build, sourceInfo *git.SourceInfo) *api.SourceRevision {
+	if build.Spec.Revision != nil {
+		return build.Spec.Revision
+	}
+	return &api.SourceRevision{
+		Git: &api.GitSourceRevision{
+			Commit:  sourceInfo.CommitID,
+			Message: sourceInfo.Message,
+			Author: api.SourceControlUser{
+				Name:  sourceInfo.AuthorName,
+				Email: sourceInfo.AuthorEmail,
+			},
+			Committer: api.SourceControlUser{
+				Name:  sourceInfo.CommitterName,
+				Email: sourceInfo.CommitterEmail,
+			},
+		},
+	}
+}
+
+func retryBuildStatusUpdate(build *api.Build, client client.BuildInterface, sourceRev *api.SourceRevision) error {
+	return kclient.RetryOnConflict(kclient.DefaultBackoff, func() error {
+		// before updating, make sure we are using the latest version of the build
+		latestBuild, err := client.Get(build.Name)
+		if err != nil {
+			// usually this means we failed to get resources due to the missing
+			// privilleges
+			return err
+		}
+		if sourceRev != nil {
+			latestBuild.Spec.Revision = sourceRev
+			latestBuild.ResourceVersion = ""
+		}
+
+		latestBuild.Status.Reason = build.Status.Reason
+		latestBuild.Status.Message = build.Status.Message
+
+		if _, err := client.UpdateDetails(latestBuild); err != nil {
+			return err
+		}
+		return nil
 	})
 }

pkg/build/builder/docker.go
@@ -12,7 +12,9 @@ import (
 	dockercmd "github.com/docker/docker/builder/dockerfile/command"
 	"github.com/docker/docker/builder/dockerfile/parser"
 	docker "github.com/fsouza/go-dockerclient"
+
 	kapi "k8s.io/kubernetes/pkg/api"
+	utilruntime "k8s.io/kubernetes/pkg/util/runtime"
 
 	s2iapi "github.com/openshift/source-to-image/pkg/api"
 	"github.com/openshift/source-to-image/pkg/tar"
@@ -69,12 +71,22 @@ func (d *DockerBuilder) Build() error {
 	}
 	sourceInfo, err := fetchSource(d.dockerClient, buildDir, d.build, d.urlTimeout, os.Stdin, d.gitClient)
 	if err != nil {
+		d.build.Status.Reason = api.StatusReasonFetchSourceFailed
+		d.build.Status.Message = api.StatusMessageFetchSourceFailed
+		if updateErr := retryBuildStatusUpdate(d.build, d.client, nil); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 		return err
 	}
+
 	if sourceInfo != nil {
-		updateBuildRevision(d.client, d.build, sourceInfo)
+		glog.V(4).Infof("Setting build revision with details %#v", sourceInfo)
+		revision := updateBuildRevision(d.build, sourceInfo)
+		if updateErr := retryBuildStatusUpdate(d.build, d.client, revision); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 	}
-	if err := d.addBuildParameters(buildDir); err != nil {
+	if err = d.addBuildParameters(buildDir); err != nil {
 		return err
 	}
 
@@ -91,14 +103,14 @@ func (d *DockerBuilder) Build() error {
 	dockerfilePath := d.getDockerfilePath(buildDir)
 	imageNames := getDockerfileFrom(dockerfilePath)
 	if len(imageNames) == 0 {
-		return fmt.Errorf("no from image in dockerfile.")
+		return fmt.Errorf("no FROM image in Dockerfile")
 	}
 	for _, imageName := range imageNames {
 		if imageName == "scratch" {
 			glog.V(4).Infof("\nSkipping image \"scratch\"")
 			continue
 		}
-		var imageExists bool = true
+		imageExists := true
 		_, err = d.dockerClient.InspectImage(imageName)
 		if err != nil {
 			if err != docker.ErrNoSuchImage {
@@ -113,18 +125,33 @@ func (d *DockerBuilder) Build() error {
 				dockercfg.PullAuthType,
 			)
 			glog.V(0).Infof("\nPulling image %s ...", imageName)
-			if err := pullImage(d.dockerClient, imageName, pullAuthConfig); err != nil {
+			if err = pullImage(d.dockerClient, imageName, pullAuthConfig); err != nil {
+				d.build.Status.Reason = api.StatusReasonPullBuilderImageFailed
+				d.build.Status.Message = api.StatusMessagePullBuilderImageFailed
+				if updateErr := retryBuildStatusUpdate(d.build, d.client, nil); updateErr != nil {
+					utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+				}
 				return fmt.Errorf("failed to pull image: %v", err)
 			}
 		}
 	}
 
-	if err := d.dockerBuild(buildDir, buildTag, d.build.Spec.Source.Secrets); err != nil {
+	if err = d.dockerBuild(buildDir, buildTag, d.build.Spec.Source.Secrets); err != nil {
+		d.build.Status.Reason = api.StatusReasonDockerBuildFailed
+		d.build.Status.Message = api.StatusMessageDockerBuildFailed
+		if updateErr := retryBuildStatusUpdate(d.build, d.client, nil); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 		return err
 	}
 
 	cname := containerName("docker", d.build.Name, d.build.Namespace, "post-commit")
 	if err := execPostCommitHook(d.dockerClient, d.build.Spec.PostCommit, buildTag, cname); err != nil {
+		d.build.Status.Reason = api.StatusReasonPostCommitHookFailed
+		d.build.Status.Message = api.StatusMessagePostCommitHookFailed
+		if updateErr := retryBuildStatusUpdate(d.build, d.client, nil); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 		return err
 	}
 
@@ -149,6 +176,11 @@ func (d *DockerBuilder) Build() error {
 		}
 		glog.V(0).Infof("\nPushing image %s ...", pushTag)
 		if err := pushImage(d.dockerClient, pushTag, pushAuthConfig); err != nil {
+			d.build.Status.Reason = api.StatusReasonPushImageToRegistryFailed
+			d.build.Status.Message = api.StatusMessagePushImageToRegistryFailed
+			if updateErr := retryBuildStatusUpdate(d.build, d.client, nil); updateErr != nil {
+				utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+			}
 			return reportPushFailure(err, authPresent, pushAuthConfig)
 		}
 		glog.V(0).Infof("Push successful")

pkg/build/builder/sti.go
@@ -12,6 +12,8 @@ import (
 	"strings"
 	"time"
 
+	utilruntime "k8s.io/kubernetes/pkg/util/runtime"
+
 	s2iapi "github.com/openshift/source-to-image/pkg/api"
 	"github.com/openshift/source-to-image/pkg/api/describe"
 	"github.com/openshift/source-to-image/pkg/api/validation"
@@ -27,7 +29,7 @@ import (
 // builderFactory is the internal interface to decouple S2I-specific code from Origin builder code
 type builderFactory interface {
 	// Create S2I Builder based on S2I configuration
-	Builder(config *s2iapi.Config, overrides s2ibuild.Overrides) (s2ibuild.Builder, error)
+	Builder(config *s2iapi.Config, overrides s2ibuild.Overrides) (s2ibuild.Builder, s2iapi.BuildInfo, error)
 }
 
 // validator is the interval interface to decouple S2I-specific code from Origin builder code
@@ -40,9 +42,9 @@ type validator interface {
 type runtimeBuilderFactory struct{}
 
 // Builder delegates execution to S2I-specific code
-func (_ runtimeBuilderFactory) Builder(config *s2iapi.Config, overrides s2ibuild.Overrides) (s2ibuild.Builder, error) {
-	builder, _, err := s2i.Strategy(config, overrides)
-	return builder, err
+func (_ runtimeBuilderFactory) Builder(config *s2iapi.Config, overrides s2ibuild.Overrides) (s2ibuild.Builder, s2iapi.BuildInfo, error) {
+	builder, buildInfo, err := s2i.Strategy(config, overrides)
+	return builder, buildInfo, err
 }
 
 // runtimeConfigValidator is the default implementation of stiConfigValidator
@@ -67,7 +69,8 @@ type S2IBuilder struct {
 }
 
 // NewS2IBuilder creates a new STIBuilder instance
-func NewS2IBuilder(dockerClient DockerClient, dockerSocket string, buildsClient client.BuildInterface, build *api.Build, gitClient GitClient, cgLimits *s2iapi.CGroupLimits) *S2IBuilder {
+func NewS2IBuilder(dockerClient DockerClient, dockerSocket string, buildsClient client.BuildInterface, build *api.Build,
+	gitClient GitClient, cgLimits *s2iapi.CGroupLimits) *S2IBuilder {
 	// delegate to internal implementation passing default implementation of builderFactory and validator
 	return newS2IBuilder(dockerClient, dockerSocket, buildsClient, build, gitClient, runtimeBuilderFactory{}, runtimeConfigValidator{}, cgLimits)
 }
@@ -88,7 +91,8 @@ func newS2IBuilder(dockerClient DockerClient, dockerSocket string, buildsClient 
 	}
 }
 
-// Build executes STI build based on configured builder, S2I builder factory and S2I config validator
+// Build executes STI build based on configured builder, S2I builder factory
+// and S2I config validator
 func (s *S2IBuilder) Build() error {
 	if s.build.Spec.Strategy.SourceStrategy == nil {
 		return errors.New("the source to image builder must be used with the source strategy")
@@ -103,11 +107,11 @@ func (s *S2IBuilder) Build() error {
 		return err
 	}
 	srcDir := filepath.Join(buildDir, s2iapi.Source)
-	if err := os.MkdirAll(srcDir, os.ModePerm); err != nil {
+	if err = os.MkdirAll(srcDir, os.ModePerm); err != nil {
 		return err
 	}
 	tmpDir := filepath.Join(buildDir, "tmp")
-	if err := os.MkdirAll(tmpDir, os.ModePerm); err != nil {
+	if err = os.MkdirAll(tmpDir, os.ModePerm); err != nil {
 		return err
 	}
 
@@ -210,7 +214,7 @@ func (s *S2IBuilder) Build() error {
 	allowedUIDs := os.Getenv(api.AllowedUIDs)
 	glog.V(4).Infof("The value of %s is [%s]", api.AllowedUIDs, allowedUIDs)
 	if len(allowedUIDs) > 0 {
-		err := config.AllowedUIDs.Set(allowedUIDs)
+		err = config.AllowedUIDs.Set(allowedUIDs)
 		if err != nil {
 			return err
 		}
@@ -245,29 +249,43 @@ func (s *S2IBuilder) Build() error {
 	}
 
 	glog.V(4).Infof("Creating a new S2I builder with build config: %#v\n", describe.Config(config))
-	builder, err := s.builder.Builder(config, s2ibuild.Overrides{Downloader: download})
+	builder, buildInfo, err := s.builder.Builder(config, s2ibuild.Overrides{Downloader: download})
 	if err != nil {
+		s.build.Status.Reason, s.build.Status.Message = convertS2IFailureType(buildInfo.FailureReason.Reason, buildInfo.FailureReason.Message)
+		if updateErr := retryBuildStatusUpdate(s.build, s.client, nil); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 		return err
 	}
 
 	glog.V(4).Infof("Starting S2I build from %s/%s BuildConfig ...", s.build.Namespace, s.build.Name)
+	result, err := builder.Build(config)
+	if err != nil {
+		s.build.Status.Reason, s.build.Status.Message = convertS2IFailureType(result.BuildInfo.FailureReason.Reason, result.BuildInfo.FailureReason.Message)
 
-	if _, err = builder.Build(config); err != nil {
+		if updateErr := retryBuildStatusUpdate(s.build, s.client, nil); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 		return err
 	}
 
-	cname := containerName("s2i", s.build.Name, s.build.Namespace, "post-commit")
-	if err := execPostCommitHook(s.dockerClient, s.build.Spec.PostCommit, buildTag, cname); err != nil {
+	cName := containerName("s2i", s.build.Name, s.build.Namespace, "post-commit")
+	if err = execPostCommitHook(s.dockerClient, s.build.Spec.PostCommit, buildTag, cName); err != nil {
+		s.build.Status.Reason = api.StatusReasonPostCommitHookFailed
+		s.build.Status.Message = api.StatusMessagePostCommitHookFailed
+		if updateErr := retryBuildStatusUpdate(s.build, s.client, nil); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 		return err
 	}
 
 	if push {
-		if err := tagImage(s.dockerClient, buildTag, pushTag); err != nil {
+		if err = tagImage(s.dockerClient, buildTag, pushTag); err != nil {
 			return err
 		}
 	}
 
-	if err := removeImage(s.dockerClient, buildTag); err != nil {
+	if err = removeImage(s.dockerClient, buildTag); err != nil {
 		glog.V(0).Infof("warning: Failed to remove temporary build tag %v: %v", buildTag, err)
 	}
 
@@ -283,7 +301,12 @@ func (s *S2IBuilder) Build() error {
 			glog.V(3).Infof("No push secret provided")
 		}
 		glog.V(0).Infof("\nPushing image %s ...", pushTag)
-		if err := pushImage(s.dockerClient, pushTag, pushAuthConfig); err != nil {
+		if err = pushImage(s.dockerClient, pushTag, pushAuthConfig); err != nil {
+			s.build.Status.Reason = api.StatusReasonPushImageToRegistryFailed
+			s.build.Status.Message = api.StatusMessagePushImageToRegistryFailed
+			if updateErr := retryBuildStatusUpdate(s.build, s.client, nil); updateErr != nil {
+				utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+			}
 			return reportPushFailure(err, authPresent, pushAuthConfig)
 		}
 		glog.V(0).Infof("Push successful")
@@ -312,10 +335,18 @@ func (d *downloader) Download(config *s2iapi.Config) (*s2iapi.SourceInfo, error)
 	// fetch source
 	sourceInfo, err := fetchSource(d.s.dockerClient, targetDir, d.s.build, d.timeout, d.in, d.s.gitClient)
 	if err != nil {
+		d.s.build.Status.Reason = api.StatusReasonFetchSourceFailed
+		d.s.build.Status.Message = api.StatusMessageFetchSourceFailed
+		if updateErr := retryBuildStatusUpdate(d.s.build, d.s.client, nil); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 		return nil, err
 	}
 	if sourceInfo != nil {
-		updateBuildRevision(d.s.client, d.s.build, sourceInfo)
+		revision := updateBuildRevision(d.s.build, sourceInfo)
+		if updateErr := retryBuildStatusUpdate(d.s.build, d.s.client, revision); updateErr != nil {
+			utilruntime.HandleError(fmt.Errorf("error: An error occured while updating the build status: %v", updateErr))
+		}
 	}
 	if sourceInfo != nil {
 		sourceInfo.ContextDir = config.ContextDir
@@ -408,4 +439,8 @@ func copyToVolumeList(artifactsMapping []api.ImageSourcePath) (volumeList s2iapi
 		})
 	}
 	return
+}
+
+func convertS2IFailureType(reason s2iapi.StepFailureReason, message s2iapi.StepFailureMessage) (api.StatusReason, string) {
+	return api.StatusReason(reason), fmt.Sprintf("%s", message)
 }

pkg/build/builder/sti_test.go
@@ -23,12 +23,12 @@ type testStiBuilderFactory struct {
 
 // Builder implements builderFactory. It returns a mock S2IBuilder instance that
 // returns specific errors.
-func (factory testStiBuilderFactory) Builder(config *s2iapi.Config, overrides s2ibuild.Overrides) (s2ibuild.Builder, error) {
+func (factory testStiBuilderFactory) Builder(config *s2iapi.Config, overrides s2ibuild.Overrides) (s2ibuild.Builder, s2iapi.BuildInfo, error) {
 	// Return a strategy error if non-nil.
 	if factory.getStrategyErr != nil {
-		return nil, factory.getStrategyErr
+		return nil, s2iapi.BuildInfo{}, factory.getStrategyErr
 	}
-	return testBuilder{buildError: factory.buildError}, nil
+	return testBuilder{buildError: factory.buildError}, s2iapi.BuildInfo{}, nil
 }
 
 // testBuilder is a mock implementation of s2iapi.Builder.
@@ -38,7 +38,9 @@ type testBuilder struct {
 
 // Build implements s2iapi.Builder. It always returns a mocked build error.
 func (builder testBuilder) Build(config *s2iapi.Config) (*s2iapi.Result, error) {
-	return nil, builder.buildError
+	return &s2iapi.Result{
+		BuildInfo: s2iapi.BuildInfo{},
+	}, builder.buildError
 }
 
 type testS2IBuilderConfig struct {

pkg/build/controller/factory/factory.go
@@ -53,6 +53,7 @@ func limitedLogAndRetry(buildupdater buildclient.BuildUpdater, maxTimeout time.D
 		build.Status.Phase = buildapi.BuildPhaseFailed
 		if !isFatal {
 			build.Status.Reason = buildapi.StatusReasonExceededRetryTimeout
+			build.Status.Message = buildapi.StatusMessageExceededRetryTimeout
 		}
 		build.Status.Message = errors.ErrorToSentence(err)
 		now := unversioned.Now()

commit c0e570a3f68e6b49c0d994c3ea158df7e0d64738

Fix typo introduced in 4b54d01c

pkg/build/controller/controller.go
@@ -191,7 +191,7 @@ func (bc *BuildController) nextBuildPhase(build *buildapi.Build) error {
 	podSpec, err := bc.BuildStrategy.CreateBuildPod(buildCopy)
 	if err != nil {
 		build.Status.Reason = buildapi.StatusReasonCannotCreateBuildPodSpec
-		build.Status.Reason = buildapi.StatusMessageCannotCreateBuildPodSpec
+		build.Status.Message = buildapi.StatusMessageCannotCreateBuildPodSpec
 		if strategy.IsFatal(err) {
 			return strategy.FatalError(fmt.Sprintf("failed to create a build pod spec for build %s/%s: %v", build.Namespace, build.Name, err))
 		}