4995ba02a754fa15a4141d37d539eecd26209da4
Jim Minter <jminter@redhat.com>
2016-12-01 11:30:04 +0000

    bump(github.com/golang/glog) 65d674618f712aa808a7d0104131b9206fc3d5ad

--- vendor/github.com/golang/glog/glog.go
+++ vendor/github.com/golang/glog/glog.go
@@ -676,7 +676,10 @@ func (l *loggingT) output(s severity, buf *buffer, file string, line int, alsoTo
 		}
 	}
 	data := buf.Bytes()
-	if l.toStderr {
+	if !flag.Parsed() {
+		os.Stderr.Write([]byte("ERROR: logging before flag.Parse: "))
+		os.Stderr.Write(data)
+	} else if l.toStderr {
 		os.Stderr.Write(data)
 	} else {
 		if alsoToStderr || l.alsoToStderr || s >= l.stderrThreshold.get() {
@@ -1043,14 +1046,6 @@ func (v Verbose) Infoln(args ...interface{}) {
 func (v Verbose) Infof(format string, args ...interface{}) {
 	if v {
 		logging.printf(infoLog, format, args...)
-	}
-}
-
-// InfoDepth is equivalent to the global InfoDepth function, guarded by the value of v.
-// See the documentation of V for usage.
-func (v Verbose) InfoDepth(depth int, args ...interface{}) {
-	if v {
-		logging.printDepth(infoLog, depth, args...)
 	}
 }
 
