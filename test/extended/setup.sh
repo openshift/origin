4f9c4944061c830873aca08f178f0f7c00ab4f13
Steve Kuznetsov <skuznets@redhat.com>
2016-12-09 10:22:43 -0500

    Provide the directory for Ginkgo jUnit output explicitly
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- test/extended/setup.sh
+++ test/extended/setup.sh
@@ -91,6 +91,8 @@ function os::test::extended::setup () {
 		# Allow setting $JUNIT_REPORT to toggle output behavior
 		if [[ -n "${JUNIT_REPORT:-}" ]]; then
 			export JUNIT_REPORT_OUTPUT="${LOG_DIR}/raw_test_output.log"
+			# the Ginkgo tests also generate jUnit but expect different envars
+			export TEST_REPORT_DIR="${ARTIFACT_DIR}"
 		fi
 
 		os::log::system::start
