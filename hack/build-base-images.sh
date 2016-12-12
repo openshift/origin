b34ae0789689583779be82e6ff387575e8a836ae
Clayton Coleman <ccoleman@redhat.com>
2016-12-12 16:22:17 -0500

    Revert "Update build logic to support more flexibility in startup"

--- hack/build-base-images.sh
+++ hack/build-base-images.sh
@@ -5,7 +5,15 @@
 STARTTIME=$(date +%s)
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
-# Build the base image without the default image args
-OS_BUILD_IMAGE_ARGS="${OS_BUILD_IMAGE_BASE_ARGS-}" os::build::image "${OS_ROOT}/images/base" openshift/origin-base
+os::util::ensure::built_binary_exists 'oc'
+
+function build() {
+  eval "oc ex dockerbuild $2 $1 ${OS_BUILD_IMAGE_ARGS:-}"
+}
+
+# Build the images
+build openshift/origin-base                   "${OS_ROOT}/images/base"
+build openshift/origin-haproxy-router-base    "${OS_ROOT}/images/router/haproxy-base"
+build openshift/origin-release                "${OS_ROOT}/images/release"
 
 ret=$?; ENDTIME=$(date +%s); echo "$0 took $(($ENDTIME - $STARTTIME)) seconds"; exit "$ret"
