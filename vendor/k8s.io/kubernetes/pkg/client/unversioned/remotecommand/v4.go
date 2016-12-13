bbb21a4db3db82bddb6b23f30ca8dd969e1038b4
juanvallejo <jvallejo@redhat.com>
2016-12-13 15:58:39 -0500

    UPSTREAM: 38642: update opt to only timeout if idle
    Instead of closing the remote shell after the amount of time defined in
    --stream-timeout, this patch takes into account any bytes being copied
    to remoteStdout. If there are any bytes being copied from the remote
    shell's standard output, the timeout counter will be reset back to the
    original value provided by the user. The shell will only actually
    timeout once the timeout counter reaches zero from the original value
    specified by the user.

--- vendor/k8s.io/kubernetes/pkg/client/unversioned/remotecommand/v4.go
+++ vendor/k8s.io/kubernetes/pkg/client/unversioned/remotecommand/v4.go
@@ -20,12 +20,17 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"strconv"
 	"sync"
+	"time"
+
+	"bufio"
 
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/kubelet/server/remotecommand"
 	"k8s.io/kubernetes/pkg/util/exec"
+	"k8s.io/kubernetes/pkg/util/runtime"
 )
 
 // streamProtocolV4 implements version 4 of the streaming protocol for attach
@@ -33,6 +38,8 @@ import (
 // the use of unversioned.Status instead of plain text messages.
 type streamProtocolV4 struct {
 	*streamProtocolV3
+
+	keepAliveChan chan int
 }
 
 var _ streamProtocolHandler = &streamProtocolV4{}
@@ -40,6 +47,7 @@ var _ streamProtocolHandler = &streamProtocolV4{}
 func newStreamProtocolV4(options StreamOptions) streamProtocolHandler {
 	return &streamProtocolV4{
 		streamProtocolV3: newStreamProtocolV3(options).(*streamProtocolV3),
+		keepAliveChan:    make(chan int),
 	}
 }
 
@@ -51,28 +59,103 @@ func (p *streamProtocolV4) handleResizes() {
 	p.streamProtocolV3.handleResizes()
 }
 
+// copy bytes from p.remoteStdout to p.Stdout without using io.Copy, in order to
+// reuse the stream to notify p.keepAliveChan of ongoing activity in the remote shell
+func (p *streamProtocolV4) copyStdout(wg *sync.WaitGroup) {
+	if p.Stdout == nil {
+		return
+	}
+
+	wg.Add(1)
+	go func() {
+		defer runtime.HandleCrash()
+		defer wg.Done()
+		defer close(p.keepAliveChan)
+
+		chunkReader := bufio.NewReader(p.remoteStdout)
+		buffer := make([]byte, 32*1024)
+		for {
+			n, err := chunkReader.Read(buffer)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				runtime.HandleError(err)
+				break
+			}
+
+			p.Stdout.Write(buffer[0:n])
+
+			// send amount of bytes written to keepAliveChan
+			// in order to notify it of ongoing activity in
+			// the remote shell.
+			p.keepAliveChan <- n
+		}
+	}()
+}
+
 func (p *streamProtocolV4) stream(conn streamCreator) error {
 	if err := p.createStreams(conn); err != nil {
 		return err
 	}
 
-	// now that all the streams have been created, proceed with reading & copying
-
-	errorChan := watchErrorStream(p.errorStream, &errorDecoderV4{})
-
-	p.handleResizes()
-
-	p.copyStdin()
-
-	var wg sync.WaitGroup
-	p.copyStdout(&wg)
-	p.copyStderr(&wg)
-
-	// we're waiting for stdout/stderr to finish copying
-	wg.Wait()
-
 	// waits for errorStream to finish reading with an error or nil
-	return <-errorChan
+	errorChan := watchErrorStream(p.errorStream, &errorDecoderV4{})
+	waitChan := make(chan bool)
+
+	go func() {
+		defer runtime.HandleCrash()
+
+		// now that all the streams have been created, proceed with reading & copying
+
+		p.handleResizes()
+		p.copyStdin()
+
+		var wg sync.WaitGroup
+		p.copyStdout(&wg)
+		p.copyStderr(&wg)
+
+		// we're waiting for stdout/stderr to finish copying
+		wg.Wait()
+
+		close(waitChan)
+	}()
+
+	if p.StreamTimeout == 0 {
+		for {
+			select {
+			case <-waitChan:
+				return <-errorChan
+			case <-p.keepAliveChan:
+				// noop - keep reading from this channel to prevent
+				// blocking the stdout stream copy.
+			}
+		}
+	} else {
+		totalIdleTime := p.StreamTimeout.Seconds()
+		tickDuration := 1 * time.Second
+
+		// if tickDuration is >= user-specified idle timeout,
+		// make the ticker count equal to the user-defined timeout
+		if p.StreamTimeout < tickDuration {
+			tickDuration = p.StreamTimeout
+		}
+
+		for {
+			select {
+			case <-waitChan:
+				return <-errorChan
+			case <-p.keepAliveChan:
+				totalIdleTime = p.StreamTimeout.Seconds()
+			case <-time.After(tickDuration):
+				totalIdleTime--
+				if totalIdleTime <= 0 {
+					return fmt.Errorf("Timeout exceeded for this operation.")
+				}
+			}
+		}
+	}
+	return nil
 }
 
 // errorDecoderV4 interprets the json-marshaled unversioned.Status on the error channel
