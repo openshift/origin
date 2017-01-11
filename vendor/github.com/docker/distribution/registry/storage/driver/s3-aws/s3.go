a216478e6bb8a706b5eb8ab24863ed7488583127
Michal Fojtik <mfojtik@redhat.com>
2017-01-11 22:27:07 +0100

    UPSTREAM: docker/distribution: 2140: Add 'ca-central-1' region for registry S3 storage driver
    Signed-off-by: Michal Fojtik <mfojtik@redhat.com>

--- vendor/github.com/docker/distribution/registry/storage/driver/s3-aws/s3.go
+++ vendor/github.com/docker/distribution/registry/storage/driver/s3-aws/s3.go
@@ -72,10 +72,13 @@ type DriverParameters struct {
 func init() {
 	for _, region := range []string{
 		"us-east-1",
+		"us-east-2",
 		"us-west-1",
 		"us-west-2",
 		"eu-west-1",
+		"eu-west-2",
 		"eu-central-1",
+		"ap-south-1",
 		"ap-southeast-1",
 		"ap-southeast-2",
 		"ap-northeast-1",
@@ -83,6 +86,7 @@ func init() {
 		"sa-east-1",
 		"cn-north-1",
 		"us-gov-west-1",
+		"ca-central-1",
 	} {
 		validRegions[region] = struct{}{}
 	}
