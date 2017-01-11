e2dbd1232e7bfd98f19c94605f462173603c66b4
Maciej Szulik <maszulik@redhat.com>
2017-01-11 17:26:06 +0100

    Switch image quota to user shared informers

--- pkg/quota/registry.go
+++ pkg/quota/registry.go
@@ -4,24 +4,24 @@ import (
 	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	"k8s.io/kubernetes/pkg/controller/informers"
 	kquota "k8s.io/kubernetes/pkg/quota"
 	"k8s.io/kubernetes/pkg/quota/install"
 
 	osclient "github.com/openshift/origin/pkg/client"
+	"github.com/openshift/origin/pkg/controller/shared"
 	imageapi "github.com/openshift/origin/pkg/image/api"
 	"github.com/openshift/origin/pkg/quota/image"
 )
 
 // NewOriginQuotaRegistry returns a registry object that knows how to evaluate quota usage of OpenShift
 // resources.
-func NewOriginQuotaRegistry(osClient osclient.Interface) kquota.Registry {
-	return image.NewImageQuotaRegistry(osClient)
+func NewOriginQuotaRegistry(isInformer shared.ImageStreamInformer, osClient osclient.Interface) kquota.Registry {
+	return image.NewImageQuotaRegistry(isInformer, osClient)
 }
 
 // NewAllResourceQuotaRegistry returns a registry object that knows how to evaluate all resources
-func NewAllResourceQuotaRegistry(osClient osclient.Interface, kubeClientSet clientset.Interface, sharedInformerFactory informers.SharedInformerFactory) kquota.Registry {
-	return kquota.UnionRegistry{install.NewRegistry(kubeClientSet, sharedInformerFactory), NewOriginQuotaRegistry(osClient)}
+func NewAllResourceQuotaRegistry(informerFactory shared.InformerFactory, osClient osclient.Interface, kubeClientSet clientset.Interface) kquota.Registry {
+	return kquota.UnionRegistry{install.NewRegistry(kubeClientSet, informerFactory.KubernetesInformers()), NewOriginQuotaRegistry(informerFactory.ImageStreams(), osClient)}
 }
 
 // AllEvaluatedGroupKinds is the list of all group kinds that we evaluate for quotas in openshift and kube
