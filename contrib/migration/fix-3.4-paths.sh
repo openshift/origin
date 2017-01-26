cfd7a9ba4b4d714a0fdc25244e80c49a1f00b61a
Seth Jennings <sjenning@redhat.com>
2017-01-26 10:58:44 -0600

    backup and remove keys during migration

--- contrib/migration/fix-3.4-paths.sh
+++ contrib/migration/fix-3.4-paths.sh
@@ -11,17 +11,19 @@
 # identities -> useridentities
 # egressnetworkpolicies -> registry/egressnetworkpolicy
 
-USAGE="${0} [-a] [-c os-master-config-dir] [-p os-etcd-prefix] etcd-endpoints"
+USAGE="${0} [-a] [-c os-master-config-dir] [-p os-etcd-prefix] [-b backup-dir] etcd-endpoints"
 usage() {
   echo "${USAGE}"
   exit 1
 }
 
+# default values
 APPLY=false
 OS_MASTER_CONFIG_DIR="/etc/origin/master"
 OS_ETCD_PREFIX="/openshift.io"
+BACKUP_DIR="$HOME/openshift-3.4-migration-backup"
 
-while getopts ":ac:p:" opt; do
+while getopts ":ac:p:b:" opt; do
   case $opt in
     a)
       APPLY=true
@@ -31,6 +33,9 @@ while getopts ":ac:p:" opt; do
       ;;
     p)
       OS_ETCD_PREFIX="${OPTARG}"
+      ;;
+    b)
+      BACKUP_DIR="${OPTARG}"
       ;;
     \?)
       usage
@@ -73,6 +78,11 @@ fi
 
 if [[ "$APPLY" != "true" ]]; then
   echo "Running in dry-run mode. Use -a option to apply changes."
+else
+  if ! mkdir -p "${BACKUP_DIR}"; then
+    echo "Unable to create backup directory ${BACKUP_DIR}"
+    exit 1
+  fi
 fi
 
 if ! command -v etcdctl &>/dev/null; then
@@ -88,36 +98,66 @@ echo_mode() {
   fi
 }
 
+backup_key() {
+  key="${1}"
+  value="${2}"
+
+  backupfile="${BACKUP_DIR}/${key}"
+  mkdir -p "$(dirname "${backupfile}")"
+  echo "$value" > "${backupfile}"
+}
+
 copy_key() {
   echo_mode "copying ${1} to ${2}"
   if ! value="$(etcdctl get "${1}")"; then
-    echo "failed to get key ${1}"
-    return 1
+    echo_mode "failed to get key ${1}"
+    exit 1
   fi
-  if etcdctl get "${2}" &>/dev/null; then
+  if existing=$(etcdctl get "${2}" 2>/dev/null); then
     echo_mode "overwriting existing key ${2}"
   fi
   if [[ "$APPLY" = "true" ]]; then
+    backup_key "${1}" "${value}"
+    if [[ -n "${existing}" ]]; then
+      backup_key "${2}" "${existing}"
+    fi
     if ! etcdctl set "${2}" "$value" >/dev/null; then
-        echo "failed to set key ${2}"
-        return 1
+      echo "failed to set key ${2}"
+      exit 1
+    fi
+    if ! etcdctl rm "${1}" >/dev/null; then
+      echo "failed to remove old key ${1}"
+      exit 1
     fi
   fi
   return 0
 }
 
 copy_keys() {
-  for key in $(etcdctl ls "${1}"); do
+  output="$(etcdctl ls "${1}")"
+  if [[ $? -ne 0 || -z "$output" ]]; then
+    echo_mode "No keys found to migrate"
+    return
+  fi
+  for key in $output; do
     newkey="${2}/$(basename "${key}")"
     copy_key "${key}" "${newkey}"
   done
 }
 
-echo "Migrating Users"
+IFS=$'\n'
+
+echo_mode "Migrating Users"
 copy_keys "${OS_ETCD_PREFIX}/identities" "${OS_ETCD_PREFIX}/useridentities"
 
-echo "Migrating Egress Policies"
-for project in $(etcdctl ls "${OS_ETCD_PREFIX}/egressnetworkpolicies"); do
-  projectname="$(basename "${project}")"
-  copy_keys "${OS_ETCD_PREFIX}/egressnetworkpolicies/${projectname}" "${OS_ETCD_PREFIX}/registry/egressnetworkpolicy/${projectname}"
-done 
+echo_mode "Migrating Egress Policies"
+output="$(etcdctl ls "${OS_ETCD_PREFIX}/egressnetworkpolicies")"
+if [[ $? -ne 0 || -z "$output" ]]; then
+  echo_mode "No keys found to migrate"
+else
+  for project in $output; do
+    projectname="$(basename "${project}")"
+    echo_mode "Project $projectname"
+    copy_keys "${OS_ETCD_PREFIX}/egressnetworkpolicies/${projectname}" "${OS_ETCD_PREFIX}/registry/egressnetworkpolicy/${projectname}"
+  done
+fi
