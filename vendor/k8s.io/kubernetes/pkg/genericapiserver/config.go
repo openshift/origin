4d636714807259e72b16eef25f0fe14d74858ac3
Maciej Szulik <maszulik@redhat.com>
2017-02-28 17:48:59 +0100

    UPSTREAM: revert: 15aaac7f8391a1e0514f80e6450f4bf12c0db191: <drop>: add ExtraClientCACerts to SecureServingInfo"
    This reverts commit 15aaac7f8391a1e0514f80e6450f4bf12c0db191.

--- vendor/k8s.io/kubernetes/pkg/genericapiserver/config.go
+++ vendor/k8s.io/kubernetes/pkg/genericapiserver/config.go
@@ -17,7 +17,6 @@ limitations under the License.
 package genericapiserver
 
 import (
-	"crypto/x509"
 	"fmt"
 	"io"
 	"net"
@@ -168,9 +167,6 @@ type SecureServingInfo struct {
 	SNICerts []NamedCertKey
 	// ClientCA is the certificate bundle for all the signers that you'll recognize for incoming client certificates
 	ClientCA string
-	// ExtraClientCACerts are additional ClientCA certs added to the pool.
-	// TODO(sttts): remove again with Kube 1.6
-	ExtraClientCACerts []*x509.Certificate
 }
 
 type CertKey struct {
