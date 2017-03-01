93874a5a7ffb028fcef479dd0562d73a26283adc
deads2k <deads@redhat.com>
2017-03-01 13:05:12 -0500

    tests added for quota scopes while debugging

--- pkg/quota/api/validation/validation_test.go
+++ pkg/quota/api/validation/validation_test.go
@@ -4,14 +4,15 @@ import (
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/resource"
 	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/api/validation"
 	"k8s.io/kubernetes/pkg/util/validation/field"
 	"testing"
 
 	quotaapi "github.com/openshift/origin/pkg/quota/api"
 )
 
-func TestValidationClusterQuota(t *testing.T) {
-	spec := api.ResourceQuotaSpec{
+func spec(scopes ...api.ResourceQuotaScope) api.ResourceQuotaSpec {
+	return api.ResourceQuotaSpec{
 		Hard: api.ResourceList{
 			api.ResourceCPU:                    resource.MustParse("100"),
 			api.ResourceMemory:                 resource.MustParse("10000"),
@@ -26,8 +27,25 @@ func TestValidationClusterQuota(t *testing.T) {
 			api.ResourceConfigMaps:             resource.MustParse("10"),
 			api.ResourceSecrets:                resource.MustParse("10"),
 		},
+		Scopes: scopes,
 	}
+}
 
+func scopeableSpec(scopes ...api.ResourceQuotaScope) api.ResourceQuotaSpec {
+	return api.ResourceQuotaSpec{
+		Hard: api.ResourceList{
+			api.ResourceCPU:            resource.MustParse("100"),
+			api.ResourceMemory:         resource.MustParse("10000"),
+			api.ResourceRequestsCPU:    resource.MustParse("100"),
+			api.ResourceRequestsMemory: resource.MustParse("10000"),
+			api.ResourceLimitsCPU:      resource.MustParse("100"),
+			api.ResourceLimitsMemory:   resource.MustParse("10000"),
+		},
+		Scopes: scopes,
+	}
+}
+
+func TestValidationClusterQuota(t *testing.T) {
 	// storage is not yet supported as a quota tracked resource
 	invalidQuotaResourceSpec := api.ResourceQuotaSpec{
 		Hard: api.ResourceList{
@@ -41,7 +59,7 @@ func TestValidationClusterQuota(t *testing.T) {
 			ObjectMeta: api.ObjectMeta{Name: "good"},
 			Spec: quotaapi.ClusterResourceQuotaSpec{
 				Selector: quotaapi.ClusterResourceQuotaSelector{LabelSelector: &unversioned.LabelSelector{MatchLabels: validLabels}},
-				Quota:    spec,
+				Quota:    spec(),
 			},
 		},
 	)
@@ -59,7 +77,7 @@ func TestValidationClusterQuota(t *testing.T) {
 				ObjectMeta: api.ObjectMeta{Namespace: "bad", Name: "good"},
 				Spec: quotaapi.ClusterResourceQuotaSpec{
 					Selector: quotaapi.ClusterResourceQuotaSelector{LabelSelector: &unversioned.LabelSelector{MatchLabels: validLabels}},
-					Quota:    spec,
+					Quota:    spec(),
 				},
 			},
 			T: field.ErrorTypeForbidden,
@@ -69,11 +87,32 @@ func TestValidationClusterQuota(t *testing.T) {
 			A: quotaapi.ClusterResourceQuota{
 				ObjectMeta: api.ObjectMeta{Name: "good"},
 				Spec: quotaapi.ClusterResourceQuotaSpec{
-					Quota: spec,
+					Quota: spec(),
 				},
 			},
 			T: field.ErrorTypeRequired,
 			F: "spec.selector",
+		},
+		"ok scope": {
+			A: quotaapi.ClusterResourceQuota{
+				ObjectMeta: api.ObjectMeta{Name: "good"},
+				Spec: quotaapi.ClusterResourceQuotaSpec{
+					Quota: scopeableSpec(api.ResourceQuotaScopeNotTerminating),
+				},
+			},
+			T: field.ErrorTypeRequired,
+			F: "spec.selector",
+		},
+		"bad scope": {
+			A: quotaapi.ClusterResourceQuota{
+				ObjectMeta: api.ObjectMeta{Name: "good"},
+				Spec: quotaapi.ClusterResourceQuotaSpec{
+					Selector: quotaapi.ClusterResourceQuotaSelector{LabelSelector: &unversioned.LabelSelector{MatchLabels: validLabels}},
+					Quota:    spec(api.ResourceQuotaScopeNotTerminating),
+				},
+			},
+			T: field.ErrorTypeInvalid,
+			F: "spec.quota.scopes",
 		},
 		"bad quota spec": {
 			A: quotaapi.ClusterResourceQuota{
@@ -100,6 +139,28 @@ func TestValidationClusterQuota(t *testing.T) {
 			if errs[i].Field != v.F {
 				t.Errorf("%s: expected errors to have field %s: %v", k, v.F, errs[i])
 			}
+		}
+	}
+}
+
+func TestValidationQuota(t *testing.T) {
+	tests := map[string]struct {
+		A api.ResourceQuota
+		T field.ErrorType
+		F string
+	}{
+		"scope": {
+			A: api.ResourceQuota{
+				ObjectMeta: api.ObjectMeta{Namespace: "ns", Name: "good"},
+				Spec:       scopeableSpec(api.ResourceQuotaScopeNotTerminating),
+			},
+		},
+	}
+	for k, v := range tests {
+		errs := validation.ValidateResourceQuota(&v.A)
+		if len(errs) != 0 {
+			t.Errorf("%s: %v", k, errs)
+			continue
 		}
 	}
 }
