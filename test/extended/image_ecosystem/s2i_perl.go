8d7f76ed8b107307e0a4d370abbe4a8f1b58b9f3
Corey Daley <cdaley@redhat.com>
2017-03-06 08:45:25 -0500

    Updating image_ecosystem extended tests
    Updating the image_ecosystem extended tests to not scale using the
    replication controllers.
    
    Fixes #13230

--- test/extended/image_ecosystem/s2i_perl.go
+++ test/extended/image_ecosystem/s2i_perl.go
@@ -8,6 +8,7 @@ import (
 	o "github.com/onsi/gomega"
 
 	kapi "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/labels"
 
 	exutil "github.com/openshift/origin/test/extended/util"
 )
@@ -19,8 +20,11 @@ var _ = g.Describe("[image_ecosystem][perl][Slow] hot deploy for openshift perl 
 		oc             = exutil.NewCLI("s2i-perl", exutil.KubeConfigPath())
 		modifyCommand  = []string{"sed", "-ie", `s/data => \$data\[0\]/data => "1337"/`, "lib/default.pm"}
 		pageCountFn    = func(count int) string { return fmt.Sprintf(`<span class="code" id="count-value">%d</span>`, count) }
-		dcName         = "dancer-mysql-example-1"
-		dcLabel        = exutil.ParseLabelsOrDie(fmt.Sprintf("deployment=%s", dcName))
+		dcName         = "dancer-mysql-example"
+		rcNameOne      = fmt.Sprintf("%s-1", dcName)
+		rcNameTwo      = fmt.Sprintf("%s-2", dcName)
+		dcLabelOne     = exutil.ParseLabelsOrDie(fmt.Sprintf("deployment=%s", rcNameOne))
+		dcLabelTwo     = exutil.ParseLabelsOrDie(fmt.Sprintf("deployment=%s", rcNameTwo))
 	)
 
 	g.Describe("Dancer example", func() {
@@ -33,55 +37,56 @@ var _ = g.Describe("[image_ecosystem][perl][Slow] hot deploy for openshift perl 
 			o.Expect(err).NotTo(o.HaveOccurred())
 
 			g.By("waiting for build to finish")
-			err = exutil.WaitForABuild(oc.Client().Builds(oc.Namespace()), "dancer-mysql-example-1", nil, nil, nil)
+			err = exutil.WaitForABuild(oc.Client().Builds(oc.Namespace()), rcNameOne, nil, nil, nil)
 			if err != nil {
-				exutil.DumpBuildLogs("dancer-mysql-example", oc)
+				exutil.DumpBuildLogs(dcName, oc)
 			}
 			o.Expect(err).NotTo(o.HaveOccurred())
 
 			// oc.KubeFramework().WaitForAnEndpoint currently will wait forever;  for now, prefacing with our WaitForADeploymentToComplete,
 			// which does have a timeout, since in most cases a failure in the service coming up stems from a failed deployment
-			err = exutil.WaitForADeploymentToComplete(oc.KubeClient().Core().ReplicationControllers(oc.Namespace()), "dancer-mysql-example", oc)
+			err = exutil.WaitForADeploymentToComplete(oc.KubeClient().Core().ReplicationControllers(oc.Namespace()), dcName, oc)
 			o.Expect(err).NotTo(o.HaveOccurred())
 
 			g.By("waiting for endpoint")
-			err = oc.KubeFramework().WaitForAnEndpoint("dancer-mysql-example")
+			err = oc.KubeFramework().WaitForAnEndpoint(dcName)
 			o.Expect(err).NotTo(o.HaveOccurred())
 
-			assertPageCountIs := func(i int) {
+			assertPageCountIs := func(i int, dcLabel labels.Selector) {
 				_, err := exutil.WaitForPods(oc.KubeClient().Core().Pods(oc.Namespace()), dcLabel, exutil.CheckPodIsRunningFn, 1, 2*time.Minute)
 				o.Expect(err).NotTo(o.HaveOccurred())
 
-				result, err := CheckPageContains(oc, "dancer-mysql-example", "", pageCountFn(i))
+				result, err := CheckPageContains(oc, dcName, "", pageCountFn(i))
 				o.Expect(err).NotTo(o.HaveOccurred())
 				o.Expect(result).To(o.BeTrue())
 			}
 
 			g.By("checking page count")
-			assertPageCountIs(1)
-			assertPageCountIs(2)
+			assertPageCountIs(1, dcLabelOne)
+			assertPageCountIs(2, dcLabelOne)
 
 			g.By("modifying the source code with disabled hot deploy")
-			RunInPodContainer(oc, dcLabel, modifyCommand)
-			assertPageCountIs(3)
+			RunInPodContainer(oc, dcLabelOne, modifyCommand)
+			assertPageCountIs(3, dcLabelOne)
 
-			pods, err := oc.KubeClient().Core().Pods(oc.Namespace()).List(kapi.ListOptions{LabelSelector: dcLabel})
+			pods, err := oc.KubeClient().Core().Pods(oc.Namespace()).List(kapi.ListOptions{LabelSelector: dcLabelOne})
 			o.Expect(err).NotTo(o.HaveOccurred())
 			o.Expect(len(pods.Items)).To(o.Equal(1))
 
 			g.By("turning on hot-deploy")
-			err = oc.Run("env").Args("rc", dcName, "PERL_APACHE2_RELOAD=true").Execute()
+			err = oc.Run("env").Args("dc", dcName, "PERL_APACHE2_RELOAD=true").Execute()
 			o.Expect(err).NotTo(o.HaveOccurred())
-			err = oc.Run("scale").Args("rc", dcName, "--replicas=0").Execute()
+			err = oc.Run("scale").Args("dc", dcName, "--replicas=0").Execute()
 			o.Expect(err).NotTo(o.HaveOccurred())
 			err = exutil.WaitUntilPodIsGone(oc.KubeClient().Core().Pods(oc.Namespace()), pods.Items[0].Name, 1*time.Minute)
 			o.Expect(err).NotTo(o.HaveOccurred())
-			err = oc.Run("scale").Args("rc", dcName, "--replicas=1").Execute()
+			err = oc.Run("scale").Args("dc", dcName, "--replicas=1").Execute()
 			o.Expect(err).NotTo(o.HaveOccurred())
 
 			g.By("modifying the source code with enabled hot deploy")
-			RunInPodContainer(oc, dcLabel, modifyCommand)
-			assertPageCountIs(1337)
+			assertPageCountIs(4, dcLabelTwo)
+			RunInPodContainer(oc, dcLabelTwo, modifyCommand)
+			assertPageCountIs(1337, dcLabelTwo)
 		})
 	})
 })
