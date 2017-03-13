5c388924ac92e37cf14f663553cb9c754d5a4a05
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-13 15:01:03 +0100

    apigroups: remove double registration of meta types for internal versions

--- pkg/template/api/register.go
+++ pkg/template/api/register.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	kapi "k8s.io/kubernetes/pkg/api"
@@ -66,7 +65,6 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&BrokerTemplateInstanceList{},
 		&kapi.List{},
 	)
-	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
 }
 
