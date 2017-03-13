125b652cc3697f5fb8a357daba75facccd0d3b67
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-13 12:15:16 +0100

    apigroups: adapt to new 1.6 style

--- pkg/route/api/install/apigroup.go
+++ pkg/route/api/install/apigroup.go
@@ -2,12 +2,20 @@ package install
 
 import (
 	"k8s.io/apimachinery/pkg/apimachinery/announced"
+	"k8s.io/apimachinery/pkg/apimachinery/registered"
+	"k8s.io/apimachinery/pkg/runtime"
+	kapi "k8s.io/kubernetes/pkg/api"
 
 	"github.com/openshift/origin/pkg/route/api"
 	"github.com/openshift/origin/pkg/route/api/v1"
 )
 
 func installApiGroup() {
+	Install(kapi.GroupFactoryRegistry, kapi.Registry, kapi.Scheme)
+}
+
+// Install registers the API group and adds types to a scheme
+func Install(groupFactoryRegistry announced.APIGroupFactoryRegistry, registry *registered.APIRegistrationManager, scheme *runtime.Scheme) {
 	if err := announced.NewGroupMetaFactory(
 		&announced.GroupMetaFactoryArgs{
 			GroupName:                  api.GroupName,
@@ -18,7 +26,7 @@ func installApiGroup() {
 		announced.VersionToSchemeFunc{
 			v1.SchemeGroupVersion.Version: v1.AddToScheme,
 		},
-	).Announce().RegisterAndEnable(); err != nil {
+	).Announce(groupFactoryRegistry).RegisterAndEnable(registry, scheme); err != nil {
 		panic(err)
 	}
 }
