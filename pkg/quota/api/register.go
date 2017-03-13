100bf3515386e8f97b13afa3f2f6141d00715f70
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-13 15:01:03 +0100

    apigroups: remove double registration of meta types for internal versions

--- pkg/quota/api/register.go
+++ pkg/quota/api/register.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -63,7 +62,6 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&AppliedClusterResourceQuota{},
 		&AppliedClusterResourceQuotaList{},
 	)
-	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
 }
 
