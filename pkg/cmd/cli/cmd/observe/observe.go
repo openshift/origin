eb00b5243d6cdcd6f609f217ea2a318184229d32
Andy Goldstein <agoldste@redhat.com>
2017-03-20 12:38:41 -0400

    k8s.io/kubernetes/pkg/healthz -> k8s.io/apiserver/pkg/server/healthz

--- pkg/cmd/cli/cmd/observe/observe.go
+++ pkg/cmd/cli/cmd/observe/observe.go
@@ -27,10 +27,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/apiserver/pkg/server/healthz"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/jsonpath"
 	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/healthz"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 	"k8s.io/kubernetes/pkg/kubectl/resource"
 
