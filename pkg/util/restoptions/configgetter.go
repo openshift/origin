c81ca3420ddb5442053842a30a848db99a17e305
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-22 10:47:29 +0100

    REVIEW: refactoring: merge storage factory code paths

--- pkg/util/restoptions/configgetter.go
+++ pkg/util/restoptions/configgetter.go
@@ -15,13 +15,10 @@ import (
 	"k8s.io/apiserver/pkg/storage"
 	"k8s.io/apiserver/pkg/storage/storagebackend"
 	"k8s.io/apiserver/pkg/storage/storagebackend/factory"
-	apiserveroptions "k8s.io/kubernetes/cmd/kube-apiserver/app/options"
-	kapi "k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/kubeapiserver"
 
 	"github.com/golang/glog"
 	configapi "github.com/openshift/origin/pkg/cmd/server/api"
-	cmdflags "github.com/openshift/origin/pkg/cmd/util/flags"
+	kubernetes "github.com/openshift/origin/pkg/cmd/server/kubernetes/master"
 )
 
 // UseConfiguredCacheSize indicates that the configured cache size should be used
@@ -37,111 +34,31 @@ type configRESTOptionsGetter struct {
 	storageFactory        serverstorage.StorageFactory
 	defaultResourceConfig *serverstorage.ResourceConfig
 
-	cacheEnabled            bool
-	defaultCacheSize        int
-	cacheSizes              map[schema.GroupResource]int
-	quorumResources         map[schema.GroupResource]struct{}
-	defaultResourcePrefixes map[schema.GroupResource]string
+	cacheEnabled     bool
+	defaultCacheSize int
+	cacheSizes       map[schema.GroupResource]int
+	quorumResources  map[schema.GroupResource]struct{}
 }
 
 // NewConfigGetter returns a restoptions.Getter implemented using information from the provided master config.
 // By default, the etcd watch cache is enabled with a size of 1000 per resource type.
 // TODO: this class should either not need to know about configapi.MasterConfig, or not be in pkg/util
-func NewConfigGetter(masterOptions configapi.MasterConfig, defaultResourceConfig *serverstorage.ResourceConfig, defaultResourcePrefixes map[schema.GroupResource]string, quorumResources map[schema.GroupResource]struct{}) Getter {
-	getter := &configRESTOptionsGetter{
-		masterOptions:           masterOptions,
-		cacheEnabled:            true,
-		defaultCacheSize:        1000,
-		cacheSizes:              map[schema.GroupResource]int{},
-		restOptionsMap:          map[schema.GroupResource]generic.RESTOptions{},
-		defaultResourceConfig:   defaultResourceConfig,
-		quorumResources:         quorumResources,
-		defaultResourcePrefixes: defaultResourcePrefixes,
-	}
-
-	if err := getter.loadSettings(); err != nil {
-		glog.Error(err)
-	}
-
-	return getter
-}
-
-func (g *configRESTOptionsGetter) loadSettings() error {
-	options := apiserveroptions.NewServerRunOptions()
-	if g.masterOptions.KubernetesMasterConfig != nil {
-		if errs := cmdflags.Resolve(g.masterOptions.KubernetesMasterConfig.APIServerArguments, options.AddFlags); len(errs) > 0 {
-			return kerrors.NewAggregate(errs)
-		}
-	}
-
-	storageGroupsToEncodingVersion, err := options.StorageSerialization.StorageGroupsToEncodingVersion()
+func NewConfigGetter(masterOptions configapi.MasterConfig, defaultResourceConfig *serverstorage.ResourceConfig, resourcePrefixOverrides map[schema.GroupResource]string, enforcedStorageVersions []schema.GroupVersionResource, quorumResources map[schema.GroupResource]struct{}) (Getter, error) {
+	apiserverOptions, err := kubernetes.BuildKubeAPIserverOptions(masterOptions)
 	if err != nil {
-		return err
+		return nil, err
 	}
-
-	storageConfig := options.Etcd.StorageConfig
-	storageConfig.Prefix = g.masterOptions.EtcdStorageConfig.OpenShiftStoragePrefix
-	storageConfig.ServerList = g.masterOptions.EtcdClientInfo.URLs
-	storageConfig.KeyFile = g.masterOptions.EtcdClientInfo.ClientCert.KeyFile
-	storageConfig.CertFile = g.masterOptions.EtcdClientInfo.ClientCert.CertFile
-	storageConfig.CAFile = g.masterOptions.EtcdClientInfo.CA
-
-	resourceEncodingConfig := serverstorage.NewDefaultResourceEncodingConfig(kapi.Registry)
-
-	storageFactory, err := kubeapiserver.NewStorageFactory(
-		storageConfig,
-		options.Etcd.DefaultStorageMediaType,
-		kapi.Codecs,
-		resourceEncodingConfig,
-		storageGroupsToEncodingVersion,
-		nil,
-		g.defaultResourceConfig,
-		options.APIEnablement.RuntimeConfig)
+	storageFactory, err := kubernetes.BuildStorageFactory(masterOptions, apiserverOptions, enforcedStorageVersions)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	storageFactory.DefaultResourcePrefixes = resourcePrefixOverrides
+	storageFactory.StorageConfig.Prefix = masterOptions.EtcdStorageConfig.OpenShiftStoragePrefix
 
-	// TODO: the following works, but better make single resource overrides possible in BuildDefaultStorageFactory
-	// instead of being late to the party and patching here:
-
-	// use legacy group name "" for all resources that existed when apigroups were introduced
-	for _, gvr := range []schema.GroupVersionResource{
-		{Group: "authorization.openshift.io", Version: "v1", Resource: "clusterpolicybindings"},
-		{Group: "authorization.openshift.io", Version: "v1", Resource: "clusterpolicies"},
-		{Group: "authorization.openshift.io", Version: "v1", Resource: "policybindings"},
-		{Group: "authorization.openshift.io", Version: "v1", Resource: "rolebindingrestrictions"},
-		{Group: "authorization.openshift.io", Version: "v1", Resource: "policies"},
-		{Group: "build.openshift.io", Version: "v1", Resource: "builds"},
-		{Group: "build.openshift.io", Version: "v1", Resource: "buildconfigs"},
-		{Group: "apps.openshift.io", Version: "v1", Resource: "deploymentconfigs"},
-		{Group: "image.openshift.io", Version: "v1", Resource: "imagestreams"},
-		{Group: "image.openshift.io", Version: "v1", Resource: "images"},
-		{Group: "oauth.openshift.io", Version: "v1", Resource: "oauthclientauthorizations"},
-		{Group: "oauth.openshift.io", Version: "v1", Resource: "oauthaccesstokens"},
-		{Group: "oauth.openshift.io", Version: "v1", Resource: "oauthauthorizetokens"},
-		{Group: "oauth.openshift.io", Version: "v1", Resource: "oauthclients"},
-		{Group: "project.openshift.io", Version: "v1", Resource: "projects"},
-		{Group: "quota.openshift.io", Version: "v1", Resource: "clusterresourcequotas"},
-		{Group: "route.openshift.io", Version: "v1", Resource: "routes"},
-		{Group: "network.openshift.io", Version: "v1", Resource: "netnamespaces"},
-		{Group: "network.openshift.io", Version: "v1", Resource: "hostsubnets"},
-		{Group: "network.openshift.io", Version: "v1", Resource: "clusternetworks"},
-		{Group: "network.openshift.io", Version: "v1", Resource: "egressnetworkpolicies"},
-		{Group: "template.openshift.io", Version: "v1", Resource: "templates"},
-		{Group: "user.openshift.io", Version: "v1", Resource: "groups"},
-		{Group: "user.openshift.io", Version: "v1", Resource: "users"},
-		{Group: "user.openshift.io", Version: "v1", Resource: "identities"},
-	} {
-		resourceEncodingConfig.SetResourceEncoding(gvr.GroupResource(), schema.GroupVersion{Version: gvr.Version}, schema.GroupVersion{Version: runtime.APIVersionInternal})
-	}
-
-	storageFactory.DefaultResourcePrefixes = g.defaultResourcePrefixes
-	g.storageFactory = storageFactory
-
-	g.cacheEnabled = options.Etcd.EnableWatchCache
-
+	// TODO: refactor vendor/k8s.io/kubernetes/pkg/registry/cachesize to remove our custom cache size code
 	errs := []error{}
-	for _, c := range options.GenericServerRunOptions.WatchCacheSizes {
+	cacheSizes := map[schema.GroupResource]int{}
+	for _, c := range apiserverOptions.GenericServerRunOptions.WatchCacheSizes {
 		tokens := strings.Split(c, "#")
 		if len(tokens) != 2 {
 			errs = append(errs, fmt.Errorf("invalid watch cache size value '%s', expecting <resource>#<size> format (e.g. builds#100)", c))
@@ -155,10 +72,22 @@ func (g *configRESTOptionsGetter) loadSettings() error {
 			errs = append(errs, fmt.Errorf("invalid watch cache size value '%s': %v", c, err))
 			continue
 		}
-
-		g.cacheSizes[resource] = size
+		cacheSizes[resource] = size
 	}
-	return kerrors.NewAggregate(errs)
+	if len(errs) > 0 {
+		return nil, kerrors.NewAggregate(errs)
+	}
+
+	return &configRESTOptionsGetter{
+		masterOptions:         masterOptions,
+		cacheEnabled:          apiserverOptions.Etcd.EnableWatchCache,
+		defaultCacheSize:      1000,
+		cacheSizes:            cacheSizes,
+		restOptionsMap:        map[schema.GroupResource]generic.RESTOptions{},
+		defaultResourceConfig: defaultResourceConfig,
+		quorumResources:       quorumResources,
+		storageFactory:        storageFactory,
+	}, nil
 }
 
 func (g *configRESTOptionsGetter) GetRESTOptions(resource schema.GroupResource) (generic.RESTOptions, error) {
@@ -210,7 +139,8 @@ func (g *configRESTOptionsGetter) GetRESTOptions(resource schema.GroupResource) 
 	resourceOptions := generic.RESTOptions{
 		StorageConfig:           config,
 		Decorator:               decorator,
-		DeleteCollectionWorkers: 1,
+		DeleteCollectionWorkers: 1,     // TODO(rebase): use upstream value here from Etcd options?
+		EnableGarbageCollection: false, // TODO(rebase): use upstream value here from Etcd options?
 		ResourcePrefix:          g.storageFactory.ResourcePrefix(resource),
 	}
 	g.restOptionsMap[resource] = resourceOptions
