cafc092394367afae671c9e1b15a3c45fe45c9ba
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-23 09:27:11 +0100

    add-test: legacy apigroup case for hpa test

--- pkg/api/kubegraph/edges.go
+++ pkg/api/kubegraph/edges.go
@@ -234,10 +234,11 @@ func AddHPAScaleRefEdges(g osgraph.Graph) {
 		}
 
 		var syntheticNode graph.Node
-		switch groupVersionResource.GroupResource() {
-		case kapi.Resource("replicationcontrollers"):
+		r := groupVersionResource.GroupResource()
+		switch {
+		case r == kapi.Resource("replicationcontrollers"):
 			syntheticNode = kubegraph.FindOrCreateSyntheticReplicationControllerNode(g, &kapi.ReplicationController{ObjectMeta: syntheticMeta})
-		case deployapi.Resource("deploymentconfigs"):
+		case deployapi.IsResourceOrLegacy("deploymentconfigs", r):
 			syntheticNode = deploygraph.FindOrCreateSyntheticDeploymentConfigNode(g, &deployapi.DeploymentConfig{ObjectMeta: syntheticMeta})
 		default:
 			continue
