202f2eaec0ba0e058b10ff27f0c65cc065ae4692
Andy Goldstein <agoldste@redhat.com>
2017-03-24 14:12:42 -0400

    pkg/cmd/server/kubernetes: pass in new options to persistentvolumecontroller.ControllerParameters

--- pkg/cmd/server/kubernetes/master/master.go
+++ pkg/cmd/server/kubernetes/master/master.go
@@ -99,6 +99,10 @@ func (c *MasterConfig) RunPersistentVolumeController(client kclientset.Interface
 		glog.Fatalf("A backward-compatible provisioner could not be created: %v, but one was expected. Provisioning will not work. This functionality is considered an early Alpha version.", err)
 	}
 
+	eventcast := record.NewBroadcaster()
+	recorder := eventcast.NewRecorder(kapi.Scheme, kclientv1.EventSource{Component: "persistent-volume-controller"})
+	eventcast.StartRecordingToSink(&kv1core.EventSinkImpl{Interface: kv1core.New(c.KubeClient.Core().RESTClient()).Events("")})
+
 	volumeController := persistentvolumecontroller.NewController(
 		persistentvolumecontroller.ControllerParameters{
 			KubeClient:                client,
@@ -107,6 +111,10 @@ func (c *MasterConfig) RunPersistentVolumeController(client kclientset.Interface
 			VolumePlugins:             probeRecyclableVolumePlugins(s.VolumeConfiguration, namespace, recyclerImageName, recyclerServiceAccountName),
 			Cloud:                     c.CloudProvider,
 			ClusterName:               s.ClusterName,
+			VolumeInformer:            c.Informers.KubernetesInformers().Core().V1().PersistentVolumes(),
+			ClaimInformer:             c.Informers.KubernetesInformers().Core().V1().PersistentVolumeClaims(),
+			ClassInformer:             c.Informers.KubernetesInformers().Storage().V1beta1().StorageClasses(),
+			EventRecorder:             recorder,
 			EnableDynamicProvisioning: s.VolumeConfiguration.EnableDynamicProvisioning,
 		})
 	volumeController.Run(utilwait.NeverStop)
