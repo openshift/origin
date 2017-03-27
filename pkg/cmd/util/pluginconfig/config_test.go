00b0022055204e121a4a5428a1127e66a755c2d5
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-27 19:09:51 +0200

    REVIEW: fix: write versioned AdmissionConfiguration for admission plugins
    Upstream switched to a new one-file-for-all admission plugin configuration. We
    have to wrap our legacy file format in those as embedded JSON.
    
    History of pain:
    
    - admission MasterConfig admission plugin snippets are wrapped into RawExtensions
      within internal AdmissionConfigurations
    - internal AdmissionConfigurations are converted to v1alpha1 AdmissionConfigurations
    - v1alpha1 AdmissionConfigurations are written to temporary files as JSON
    - temporary file name are passed to upstream admission
    - upstream admission loads and decodes them
    - finds the AdmissionPluginConfiguration for the given plugin
    - writes YAML into a temporary buffer
    - passes the YAML to the admission plugin.

--- pkg/cmd/util/pluginconfig/config_test.go
+++ pkg/cmd/util/pluginconfig/config_test.go
@@ -1,16 +1,21 @@
 package pluginconfig
 
 import (
+	"io/ioutil"
 	"reflect"
 	"testing"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	kapiserverinternal "k8s.io/apiserver/pkg/apis/apiserver"
+	kapiserverv1alpha1 "k8s.io/apiserver/pkg/apis/apiserver/v1alpha1"
 
 	oapi "github.com/openshift/origin/pkg/api"
 	configapi "github.com/openshift/origin/pkg/cmd/server/api"
 	"github.com/openshift/origin/pkg/cmd/server/api/latest"
 
+	// install server api
 	_ "github.com/openshift/origin/pkg/cmd/server/api/install"
 )
 
@@ -53,5 +58,59 @@ func TestGetPluginConfig(t *testing.T) {
 	}
 	if !reflect.DeepEqual(testConfig, resultConfig) {
 		t.Errorf("Unexpected config. Expected: %#v. Got: %#v", testConfig, resultConfig)
+	}
+}
+
+func TestGetAdmissionConfigurationConfig(t *testing.T) {
+	configapi.Scheme.AddKnownTypes(oapi.SchemeGroupVersion, &TestConfig{})
+	configapi.Scheme.AddKnownTypeWithName(latest.Version.WithKind("TestConfig"), &TestConfigV1{})
+	kapiserverv1alpha1.AddToScheme(configapi.Scheme)
+	kapiserverinternal.AddToScheme(configapi.Scheme)
+
+	testConfig := &TestConfig{
+		Item1: "item1value",
+		Item2: []string{"element1", "element2"},
+	}
+
+	cfg := configapi.AdmissionPluginConfig{
+		Location:      "/path/to/my/config",
+		Configuration: testConfig,
+	}
+	fileName, err := GetAdmissionConfigurationConfig("test", cfg)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	codec := configapi.Codecs.UniversalDecoder()
+	obj, err := runtime.Decode(codec, data)
+	if err != nil {
+		t.Fatalf("unexpected conversion error: %v", err)
+	}
+	admissionConfig, ok := obj.(*kapiserverinternal.AdmissionConfiguration)
+	if !ok {
+		t.Fatalf("expected kapiserverinternal.AdmissionConfiguration, got: %#v", obj)
+	}
+
+	if len(admissionConfig.Plugins) != 1 {
+		t.Fatalf("expected exactly one plugin config, got: %#v", admissionConfig.Plugins)
+	}
+
+	embeddedUnknown, ok := admissionConfig.Plugins[0].Configuration.(*runtime.Unknown)
+	if !ok {
+		t.Fatalf("expected embedded Unknown, got: %#v", admissionConfig.Plugins[0].Configuration)
+	}
+
+	resultConfig := &TestConfig{}
+	err = runtime.DecodeInto(codec, embeddedUnknown.Raw, resultConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(testConfig, resultConfig) {
+		t.Errorf("Unexpected config. Expected: %#v. Got: %#v", testConfig, admissionConfig.Plugins[0].Configuration)
 	}
 }
