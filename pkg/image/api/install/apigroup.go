0ea01bf18477ffdbcc6096107c9b71a8e0e23679
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-27 12:16:54 +0200

    fix: do not install Docker image types, only register

--- pkg/image/api/install/apigroup.go
+++ pkg/image/api/install/apigroup.go
@@ -21,13 +21,9 @@ func installApiGroup() {
 func Install(groupFactoryRegistry announced.APIGroupFactoryRegistry, registry *registered.APIRegistrationManager, scheme *runtime.Scheme) {
 	if err := announced.NewGroupMetaFactory(
 		&announced.GroupMetaFactoryArgs{
-			GroupName: api.GroupName,
-			VersionPreferenceOrder: []string{
-				v1.SchemeGroupVersion.Version,
-				dockerpre012.SchemeGroupVersion.Version,
-				docker10.SchemeGroupVersion.Version,
-			},
-			ImportPrefix: importPrefix,
+			GroupName:              api.GroupName,
+			VersionPreferenceOrder: []string{v1.SchemeGroupVersion.Version},
+			ImportPrefix:           importPrefix,
 			AddInternalObjectsToScheme: func(scheme *runtime.Scheme) error {
 				if err := docker10.AddToScheme(scheme); err != nil {
 					return err
@@ -39,11 +35,7 @@ func Install(groupFactoryRegistry announced.APIGroupFactoryRegistry, registry *r
 			},
 			RootScopedKinds: sets.NewString("Image", "ImageSignature"),
 		},
-		announced.VersionToSchemeFunc{
-			docker10.SchemeGroupVersion.Version:     docker10.AddToScheme,
-			dockerpre012.SchemeGroupVersion.Version: dockerpre012.AddToScheme,
-			v1.SchemeGroupVersion.Version:           v1.AddToScheme,
-		},
+		announced.VersionToSchemeFunc{v1.SchemeGroupVersion.Version: v1.AddToScheme},
 	).Announce(groupFactoryRegistry).RegisterAndEnable(registry, scheme); err != nil {
 		panic(err)
 	}
