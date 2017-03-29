15e7687a9427b91619f074b1ba603be197f4b2e0
Andy Goldstein <agoldste@redhat.com>
2017-03-29 16:14:29 -0400

    Fix imagesecrets rest get options

--- pkg/image/registry/imagesecret/rest.go
+++ pkg/image/registry/imagesecret/rest.go
@@ -3,7 +3,6 @@ package imagesecret
 import (
 	"fmt"
 
-	metainternal "k8s.io/apimachinery/pkg/apis/meta/internalversion"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	apirequest "k8s.io/apiserver/pkg/endpoints/request"
@@ -34,19 +33,19 @@ func (r *REST) NewGetOptions() (runtime.Object, bool, string) {
 // Get retrieves all pull type secrets in the current namespace. Name is currently ignored and
 // reserved for future use.
 func (r *REST) Get(ctx apirequest.Context, _ string, options runtime.Object) (runtime.Object, error) {
-	listOptions, ok := options.(*metainternal.ListOptions)
+	listOptions, ok := options.(*metav1.ListOptions)
 	if !ok {
-		return nil, fmt.Errorf("unexpected options: %v", listOptions)
+		return nil, fmt.Errorf("unexpected options: %T", options)
 	}
-	listOptionsv1 := metav1.ListOptions{}
-	if err := metainternal.Convert_internalversion_ListOptions_To_v1_ListOptions(listOptions, &listOptionsv1, nil); err != nil {
-		return nil, err
+	var opts metav1.ListOptions
+	if listOptions != nil {
+		opts = *listOptions
 	}
 	ns, ok := apirequest.NamespaceFrom(ctx)
 	if !ok {
 		ns = metav1.NamespaceAll
 	}
-	secrets, err := r.secrets.Secrets(ns).List(listOptionsv1)
+	secrets, err := r.secrets.Secrets(ns).List(opts)
 	if err != nil {
 		return nil, err
 	}
