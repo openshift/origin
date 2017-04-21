9ff5664b3b3f0307023a6627882e4595d789a22f
Solly Ross <sross@redhat.com>
2017-04-20 20:01:11 -0400

    Remove "vendored" userspace proxy
    We had a copy of the userspace proxy sitting in pkg/proxy until the
    equivalent changes got merged upstream.  Now that they are, remove
    pkg/proxy/userspace in favor of the upstream version.

--- pkg/proxy/userspace/rlimit.go
+++
@@ -1,9 +0,0 @@
-// +build !windows
-
-package userspace
-
-import "syscall"
-
-func setRLimit(limit uint64) error {
-	return syscall.Setrlimit(syscall.RLIMIT_NOFILE, &syscall.Rlimit{Max: limit, Cur: limit})
-}
--- pkg/proxy/userspace/rlimit_windows.go
+++
@@ -1,7 +0,0 @@
-// +build windows
-
-package userspace
-
-func setRLimit(limit uint64) error {
-	return nil
-}
