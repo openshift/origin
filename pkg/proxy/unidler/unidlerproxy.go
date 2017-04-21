9bce621d556e6125c68c6fbcab24d9194c67bd90
Solly Ross <sross@redhat.com>
2017-04-20 21:00:08 -0400

    Fix ObjectRefernce type in unidler proxy signaler
    The EventRecorder now only knows about
    "k8s.io/client-go/pkg/api/v1".ObjectReference, so we have to make
    sure to pass it that instead of
    "k8s.io/kubernetes/pkg/api".ObjectReference when signaling a "NeedPods"
    event.

--- pkg/proxy/unidler/unidlerproxy.go
+++ pkg/proxy/unidler/unidlerproxy.go
@@ -5,9 +5,9 @@ import (
 	"time"
 
 	utilnet "k8s.io/apimachinery/pkg/util/net"
+	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/api/v1"
 	"k8s.io/kubernetes/pkg/proxy/userspace"
 	utilexec "k8s.io/kubernetes/pkg/util/exec"
 	"k8s.io/kubernetes/pkg/util/iptables"
@@ -25,7 +25,18 @@ type eventSignaler struct {
 	recorder record.EventRecorder
 }
 
-func (sig *eventSignaler) NeedPods(serviceRef api.ObjectReference, port string) error {
+func (sig *eventSignaler) NeedPods(serviceRefInt api.ObjectReference, port string) error {
+	// TODO(directxman12): fix upstream so that it passes around v1.ObjectReference instead of api.ObjectReference
+	// NB: api.ObjectReference doesn't appear to be registered as a type, so we have to do manual conversion...
+	serviceRef := v1.ObjectReference{
+		Kind:            serviceRefInt.Kind,
+		Namespace:       serviceRefInt.Namespace,
+		Name:            serviceRefInt.Name,
+		UID:             serviceRefInt.UID,
+		APIVersion:      serviceRefInt.APIVersion,
+		ResourceVersion: serviceRefInt.ResourceVersion,
+	}
+
 	// HACK: make the message different to prevent event aggregation
 	sig.recorder.Eventf(&serviceRef, v1.EventTypeNormal, unidlingapi.NeedPodsReason, "The service-port %s:%s needs pods.", serviceRef.Name, port)
 
