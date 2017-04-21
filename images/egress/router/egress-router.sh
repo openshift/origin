877b5fb977f35acb89956040e26a1823726eed8c
Dan Winship <danw@redhat.com>
2017-04-21 12:08:50 -0400

    Reorg egress-router code, add "initContainer mode"
    If EGRESS_ROUTER_MODE=init is set in the environment, then exit after
    setting up the iptables rules. This can be used as an init container
    along with an unprivileged dummy "main" container so that there's no
    privileged container running most of the time.

--- images/egress/router/egress-router.sh
+++ images/egress/router/egress-router.sh
@@ -25,44 +25,75 @@ if [[ -n "${EGRESS_ROUTER_DEBUG:-}" ]]; then
     set -x
 fi
 
-# The pod may die and get restarted; only try to add the
-# address/route/rules if they are not already there.
-if ! ip route get "${EGRESS_GATEWAY}" | grep -q macvlan0; then
-    ip addr add "${EGRESS_SOURCE}"/32 dev macvlan0
-    ip link set up dev macvlan0
+function setup_network() {
+    # The pod may die and get restarted; only try to add the
+    # address/route/rules if they are not already there.
+    if ! ip route get "${EGRESS_GATEWAY}" | grep -q macvlan0; then
+	ip addr add "${EGRESS_SOURCE}"/32 dev macvlan0
+	ip link set up dev macvlan0
 
-    ip route add "${EGRESS_GATEWAY}"/32 dev macvlan0
-    ip route del default
-    ip route add default via "${EGRESS_GATEWAY}" dev macvlan0
+	ip route add "${EGRESS_GATEWAY}"/32 dev macvlan0
+	ip route del default
+	ip route add default via "${EGRESS_GATEWAY}" dev macvlan0
+    fi
 
-    iptables -t nat -A PREROUTING -i eth0 -j DNAT --to-destination "${EGRESS_DESTINATION}"
-    iptables -t nat -A POSTROUTING -j SNAT --to-source "${EGRESS_SOURCE}"
-fi
+    # Update neighbor ARP caches in case another node previously had the IP. (This is
+    # the same code ifup uses.)
+    arping -q -A -c 1 -I macvlan0 "${EGRESS_SOURCE}"
+    ( sleep 2;
+      arping -q -U -c 1 -I macvlan0 "${EGRESS_SOURCE}" || true ) > /dev/null 2>&1 < /dev/null &
+}
 
-# Update neighbor ARP caches in case another node previously had the IP. (This is
-# the same code ifup uses.)
-arping -q -A -c 1 -I macvlan0 "${EGRESS_SOURCE}"
-( sleep 2;
-  arping -q -U -c 1 -I macvlan0 "${EGRESS_SOURCE}" || true ) > /dev/null 2>&1 < /dev/null &
+function gen_iptables_rules() {
+    echo -A PREROUTING -i eth0 -j DNAT --to-destination "${EGRESS_DESTINATION}"
+    echo -A POSTROUTING -j SNAT --to-source "${EGRESS_SOURCE}"
+}
 
-# Now we just wait until we are killed...
-#
-# Kubernetes will use SIGTERM to kill us, but bash ignores SIGTERM by
-# default in interactive shells, and it thinks this shell is
-# interactive due to the way in which docker invokes it. We can get
-# bash to react to SIGTERM if we explicitly set a trap for it, except
-# that bash doesn't process signal traps while it is waiting for a
-# process to finish, and we have to be waiting for a process to finish
-# because there's no way to sleep forever within bash.
-#
-# Fortunately, signal traps do interrupt the "wait" builtin. So...
-# set up a SIGTERM trap, run a command that sleeps forever *in the
-# background*, and then wait for either the command to finish or the
-# signal to arrive.
+function setup_iptables() {
+    iptables -t nat -F
+    ( echo "*nat";
+      echo ":PREROUTING ACCEPT [0:0]";
+      echo ":POSTROUTING ACCEPT [0:0]";
+      gen_iptables_rules;
+      echo "COMMIT" ) | iptables-restore --noflush --table nat
+}
 
-trap "exit" TERM
-tail -f /dev/null &
-wait
+function wait_until_killed() {
+    # Kubernetes will use SIGTERM to kill us, but bash ignores SIGTERM by
+    # default in interactive shells, and it thinks this shell is
+    # interactive due to the way in which docker invokes it. We can get
+    # bash to react to SIGTERM if we explicitly set a trap for it, except
+    # that bash doesn't process signal traps while it is waiting for a
+    # process to finish, and we have to be waiting for a process to finish
+    # because there's no way to sleep forever within bash.
+    #
+    # Fortunately, signal traps do interrupt the "wait" builtin. So...
+    # set up a SIGTERM trap, run a command that sleeps forever *in the
+    # background*, and then wait for either the command to finish or the
+    # signal to arrive.
+
+    trap "exit" TERM
+    tail -f /dev/null &
+    wait
+}
+
+case "${EGRESS_ROUTER_MODE:=legacy}" in
+    init)
+	setup_network
+	setup_iptables
+	;;
+
+    legacy)
+	setup_network
+	setup_iptables
+	wait_until_killed
+	;;
+
+    *)
+        echo "Unrecognized EGRESS_ROUTER_MODE '${EGRESS_ROUTER_MODE}'"
+        exit 1
+        ;;
+esac
 
 # We don't have to do any cleanup because deleting the network
 # namespace will clean everything up for us.
