168a4ef1d39683d20045805b164ebddf4d934d10
deads2k <deads@redhat.com>
2017-04-25 14:53:52 -0400

    UPSTREAM: <carry>: openapi test, patch in updated package name

--- vendor/k8s.io/kubernetes/staging/src/k8s.io/apiserver/pkg/endpoints/openapi/openapi_test.go
+++ vendor/k8s.io/kubernetes/staging/src/k8s.io/apiserver/pkg/endpoints/openapi/openapi_test.go
@@ -60,8 +60,8 @@ func assertEqual(t *testing.T, expected, actual interface{}) {
 
 func TestGetDefinitionName(t *testing.T) {
 	testType := TestType{}
-	typePkgName := "k8s.io/kubernetes/vendor/k8s.io/apiserver/pkg/endpoints/openapi.TestType"
-	typeFriendlyName := "io.k8s.kubernetes.vendor.k8s.io.apiserver.pkg.endpoints.openapi.TestType"
+	typePkgName := "github.com/openshift/origin/vendor/k8s.io/apiserver/pkg/endpoints/openapi.TestType"
+	typeFriendlyName := "com.github.openshift.origin.vendor.k8s.io.apiserver.pkg.endpoints.openapi.TestType"
 	if strings.HasSuffix(reflect.TypeOf(testType).PkgPath(), "go_default_test") {
 		// the test is running inside bazel where the package name is changed and
 		// "go_default_test" will add to package path.
