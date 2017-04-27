4905d603e5ff2e353f91b98032003f68071801d6
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-04-27 11:32:56 +0200

    fix-test: hpa.{extensions -> autoscaling} in policy cmd test

--- test/cmd/policy.sh
+++ test/cmd/policy.sh
@@ -163,7 +163,7 @@ os::util::sed "s/RESOURCE_VERSION/${resourceversion}/g" ${workingdir}/cluster_ad
 os::cmd::expect_success "oc replace --config=${new_kubeconfig} clusterrole/alternate-cluster-admin -f ${workingdir}/cluster_admin_without_apigroups.yaml"
 
 # alternate-cluster-admin should NOT have the power add back star now
-os::cmd::try_until_failure "oc policy who-can update hpa.extensions | grep -q alternate-cluster-admin-user"
+os::cmd::try_until_failure "oc policy who-can update hpa.autoscaling | grep -q alternate-cluster-admin-user"
 resourceversion=$(oc get clusterrole/alternate-cluster-admin -o=jsonpath="{.metadata.resourceVersion}")
 cp ${OS_ROOT}/test/testdata/bootstrappolicy/alternate_cluster_admin.yaml ${workingdir}
 os::util::sed "s/RESOURCE_VERSION/${resourceversion}/g" ${workingdir}/alternate_cluster_admin.yaml
