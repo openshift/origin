685ea1b64e73c6099be014ae14fea3551cb66223
deads2k <deads@redhat.com>
2017-05-01 08:41:46 -0400

    UPSTREAM: 42911: combine kube-apiserver and kube-aggregator

--- vendor/k8s.io/kubernetes/staging/src/k8s.io/apiserver/pkg/server/config.go
+++ vendor/k8s.io/kubernetes/staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -358,10 +358,6 @@ func (c *Config) Complete() completedConfig {
 		c.FallThroughHandler = mux.NewPathRecorderMux()
 	}
 
-	if c.FallThroughHandler == nil {
-		c.FallThroughHandler = mux.NewPathRecorderMux()
-	}
-
 	return completedConfig{c}
 }
 
