edebf8c5ef69a464dfdb71bc1956ef81454e6f28
deads2k <deads@redhat.com>
2017-05-01 11:44:40 -0400

    allow SA tokens via websockets

--- pkg/cmd/server/origin/master_config.go
+++ pkg/cmd/server/origin/master_config.go
@@ -683,7 +683,7 @@ func newAuthenticator(config configapi.MasterConfig, restOptionsGetter restoptio
 			publicKeys = append(publicKeys, readPublicKeys...)
 		}
 		serviceAccountTokenAuthenticator := serviceaccount.JWTTokenAuthenticator(publicKeys, true, tokenGetter)
-		tokenAuthenticators = append(tokenAuthenticators, bearertoken.New(serviceAccountTokenAuthenticator, true))
+		tokenAuthenticators = append(tokenAuthenticators, bearertoken.New(serviceAccountTokenAuthenticator, true), paramtoken.New("access_token", serviceAccountTokenAuthenticator, true))
 	}
 
 	// OAuth token
