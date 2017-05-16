0a2cf7cba25a82ca2672cb9bf575c9c9641377c7
Ben Parees <bparees@redhat.com>
2017-05-16 17:19:29 -0400

    install service catalog w/ oc cluster up

--- pkg/bootstrap/docker/openshift/logging.go
+++ pkg/bootstrap/docker/openshift/logging.go
@@ -5,14 +5,10 @@ import (
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kerrors "k8s.io/apimachinery/pkg/util/errors"
 	kapi "k8s.io/kubernetes/pkg/api"
 
 	"github.com/openshift/origin/pkg/bootstrap/docker/errors"
-	"github.com/openshift/origin/pkg/client"
 	"github.com/openshift/origin/pkg/cmd/util/clientcmd"
-	configcmd "github.com/openshift/origin/pkg/config/cmd"
-	genappcmd "github.com/openshift/origin/pkg/generate/app/cmd"
 )
 
 const (
@@ -22,34 +18,6 @@ const (
 	loggingDeployerTemplate        = "logging-deployer-template"
 	loggingPlaybook                = "playbooks/byo/openshift-cluster/openshift-logging.yml"
 )
-
-func instantiateTemplate(client client.Interface, mapper configcmd.Mapper, templateNamespace, templateName, targetNamespace string, params map[string]string) error {
-	template, err := client.Templates(templateNamespace).Get(templateName, metav1.GetOptions{})
-	if err != nil {
-		return errors.NewError("cannot retrieve template %q from namespace %q", templateName, templateNamespace).WithCause(err)
-	}
-
-	// process the template
-	result, err := genappcmd.TransformTemplate(template, client, targetNamespace, params)
-	if err != nil {
-		return errors.NewError("cannot process template %s/%s", templateNamespace, templateName).WithCause(err)
-	}
-
-	// Create objects
-	bulk := &configcmd.Bulk{
-		Mapper: mapper,
-		Op:     configcmd.Create,
-	}
-	itemsToCreate := &kapi.List{
-		Items: result.Objects,
-	}
-	if errs := bulk.Run(itemsToCreate, targetNamespace); len(errs) > 0 {
-		err = kerrors.NewAggregate(errs)
-		return errors.NewError("cannot create objects from template %s/%s", templateNamespace, templateName).WithCause(err)
-	}
-
-	return nil
-}
 
 // InstallLoggingViaAnsible checks whether logging is installed and installs it if not already installed
 func (h *Helper) InstallLoggingViaAnsible(f *clientcmd.Factory, serverIP, publicHostname, loggerHost, imagePrefix, imageVersion, hostConfigDir, imageStreams string) error {
