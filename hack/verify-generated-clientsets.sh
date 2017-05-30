8754c3894066c6e70616d1eea042e73d338555a2
Andy Goldstein <andy.goldstein@gmail.com>
2017-05-30 15:31:25 -0400

    Fix verifying generated clientsets

--- hack/verify-generated-clientsets.sh
+++ hack/verify-generated-clientsets.sh
@@ -2,12 +2,11 @@
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
 echo "===== Verifying Generated Client sets ====="
-output=$(VERIFY=true ${OS_ROOT}/hack/update-generated-clientsets.sh)
-
-if [[ "$?" == "0" ]]; then
+if output=$(VERIFY=--verify-only ${OS_ROOT}/hack/update-generated-clientsets.sh 2>&1)
+then
   echo "SUCCESS: Generated client sets up to date."
 else
-  echo  $output
   echo "FAILURE: Generated client sets out of date. Please run hack/update-generated-clientsets.sh"
+  echo "${output}"
   exit 1
 fi
