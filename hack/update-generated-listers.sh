e7ce73674956ccb6967dc173138bdbe73d64d3a9
deads2k <deads@redhat.com>
2017-06-13 11:19:13 -0400

    script to manage the bulk of the moves

--- hack/update-generated-listers.sh
+++ hack/update-generated-listers.sh
@@ -7,36 +7,39 @@ os::util::ensure::built_binary_exists 'genlisters'
 
 # list of package to generate listers for
 packages=(
-  github.com/openshift/origin/pkg/authorization
-  github.com/openshift/origin/pkg/build
-  github.com/openshift/origin/pkg/deploy
-  github.com/openshift/origin/pkg/image
-  github.com/openshift/origin/pkg/oauth
-  github.com/openshift/origin/pkg/project
-  github.com/openshift/origin/pkg/quota
-  github.com/openshift/origin/pkg/route
-  github.com/openshift/origin/pkg/sdn
-  github.com/openshift/origin/pkg/security
-  github.com/openshift/origin/pkg/template
-  github.com/openshift/origin/pkg/user
+  github.com/openshift/origin/pkg/authorization/apis/authorization
+  github.com/openshift/origin/pkg/build/apis/build
+  github.com/openshift/origin/pkg/deploy/apis/apps
+  github.com/openshift/origin/pkg/image/apis/image
+  github.com/openshift/origin/pkg/oauth/apis/oauth
+  github.com/openshift/origin/pkg/project/apis/project
+  github.com/openshift/origin/pkg/quota/apis/quota
+  github.com/openshift/origin/pkg/route/apis/route
+  github.com/openshift/origin/pkg/sdn/apis/network
+  github.com/openshift/origin/pkg/security/apis/security
+  github.com/openshift/origin/pkg/template/apis/template
+  github.com/openshift/origin/pkg/user/apis/user
 )
 
 function generate_listers_for() {
   local package="$1";shift
   echo "-- Generating listers for ${package} ..."
+  grouppkg=$(realpath --canonicalize-missing --relative-to=$(pwd) ${package}/../..)
   genlisters --logtostderr \
              --go-header-file=hack/boilerplate.txt \
-             --input-dirs="${package}/api,${package}/api/v1" \
-             --output-package="${package}/generated/listers" \
+             --input-dirs="${package},${package}/v1" \
+             --output-package="${grouppkg}/generated/listers" \
              "$@"
 }
 
 verify="${VERIFY:-}"
 
 # remove the old listers
+# doing this deletes the custom indexes which breaks templates.  I don't know how templates worked before
 for pkg in "${packages[@]}"; do
   if [[ -z "${verify}" ]]; then
-    go list -f '{{.Dir}}' "${pkg}/generated/listers/..." | xargs rm -rf
+    grouppkg=$(realpath --canonicalize-missing --relative-to=$(pwd) ${pkg}/../..)
+    go list -f '{{.Dir}}' "${grouppkg}/generated/listers/..." | xargs rm -rf
   fi
 done
 
