ca704a11219397f84235133f216322f42543a157
deads2k <deads@redhat.com>
2017-06-15 15:41:19 -0400

    move SCC types to openshift

--- pkg/security/api/v1/register.go
+++ pkg/security/api/v1/register.go
@@ -16,16 +16,18 @@ var (
 	SchemeGroupVersion       = schema.GroupVersion{Group: GroupName, Version: "v1"}
 	LegacySchemeGroupVersion = schema.GroupVersion{Group: LegacyGroupName, Version: "v1"}
 
-	LegacySchemeBuilder    = runtime.NewSchemeBuilder(addLegacyKnownTypes)
+	LegacySchemeBuilder    = runtime.NewSchemeBuilder(addLegacyKnownTypes, addConversionFuncs, addDefaultingFuncs)
 	AddToSchemeInCoreGroup = LegacySchemeBuilder.AddToScheme
 
-	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes, addConversionFuncs, addDefaultingFuncs)
 	AddToScheme   = SchemeBuilder.AddToScheme
 )
 
 // Adds the list of known types to api.Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
+		&SecurityContextConstraints{},
+		&SecurityContextConstraintsList{},
 		&PodSecurityPolicySubjectReview{},
 		&PodSecurityPolicySelfSubjectReview{},
 		&PodSecurityPolicyReview{},
@@ -36,6 +38,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 
 func addLegacyKnownTypes(scheme *runtime.Scheme) error {
 	types := []runtime.Object{
+		&SecurityContextConstraints{},
+		&SecurityContextConstraintsList{},
 		&PodSecurityPolicySubjectReview{},
 		&PodSecurityPolicySelfSubjectReview{},
 		&PodSecurityPolicyReview{},
