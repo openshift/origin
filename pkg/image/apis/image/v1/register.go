1200ef28e6bfa8c13adda72dd1e4f0279d7f3df6
deads2k <deads@redhat.com>
2017-06-26 12:12:22 -0400

    move API groups to canonical locations

---
+++ pkg/image/apis/image/v1/register.go
@@ -0,0 +1,64 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	kapiv1 "k8s.io/kubernetes/pkg/api/v1"
+
+	"github.com/openshift/origin/pkg/image/apis/image/docker10"
+	"github.com/openshift/origin/pkg/image/apis/image/dockerpre012"
+)
+
+const (
+	GroupName       = "image.openshift.io"
+	LegacyGroupName = ""
+)
+
+var (
+	SchemeGroupVersion       = schema.GroupVersion{Group: GroupName, Version: "v1"}
+	LegacySchemeGroupVersion = schema.GroupVersion{Group: LegacyGroupName, Version: "v1"}
+
+	LegacySchemeBuilder    = runtime.NewSchemeBuilder(addLegacyKnownTypes, addConversionFuncs, addDefaultingFuncs, docker10.AddToSchemeInCoreGroup, dockerpre012.AddToSchemeInCoreGroup)
+	AddToSchemeInCoreGroup = LegacySchemeBuilder.AddToScheme
+
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes, addConversionFuncs, addDefaultingFuncs, docker10.AddToScheme, dockerpre012.AddToScheme)
+	AddToScheme   = SchemeBuilder.AddToScheme
+)
+
+// Adds the list of known types to api.Scheme.
+func addLegacyKnownTypes(scheme *runtime.Scheme) error {
+	types := []runtime.Object{
+		&Image{},
+		&ImageList{},
+		&ImageSignature{},
+		&ImageStream{},
+		&ImageStreamList{},
+		&ImageStreamMapping{},
+		&ImageStreamTag{},
+		&ImageStreamTagList{},
+		&ImageStreamImage{},
+		&ImageStreamImport{},
+	}
+	scheme.AddKnownTypes(LegacySchemeGroupVersion, types...)
+	return nil
+}
+
+// Adds the list of known types to api.Scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Image{},
+		&ImageList{},
+		&ImageSignature{},
+		&ImageStream{},
+		&ImageStreamList{},
+		&ImageStreamMapping{},
+		&ImageStreamTag{},
+		&ImageStreamTagList{},
+		&ImageStreamImage{},
+		&ImageStreamImport{},
+		&kapiv1.SecretList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
