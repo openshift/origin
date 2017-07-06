dbf879e7e0820481cab37ea329eb795e08eeeb22
deads2k <deads@redhat.com>
2017-07-06 15:39:12 -0400

    rewire controllers

--- pkg/cmd/server/kubernetes/master/controller/core.go
+++ pkg/cmd/server/kubernetes/master/controller/core.go
@@ -2,99 +2,18 @@ package controller
 
 import (
 	"fmt"
-	"net"
 
-	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	kv1core "k8s.io/client-go/kubernetes/typed/core/v1"
 	kclientv1 "k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/record"
 	kubecontroller "k8s.io/kubernetes/cmd/kube-controller-manager/app"
 	kapi "k8s.io/kubernetes/pkg/api"
 	kclientset "k8s.io/kubernetes/pkg/client/clientset_generated/clientset"
-	"k8s.io/kubernetes/pkg/cloudprovider"
-	nodecontroller "k8s.io/kubernetes/pkg/controller/node"
-	servicecontroller "k8s.io/kubernetes/pkg/controller/service"
-	"k8s.io/kubernetes/pkg/features"
 	"k8s.io/kubernetes/plugin/pkg/scheduler"
 	_ "k8s.io/kubernetes/plugin/pkg/scheduler/algorithmprovider"
 	schedulerapi "k8s.io/kubernetes/plugin/pkg/scheduler/api"
 	"k8s.io/kubernetes/plugin/pkg/scheduler/factory"
-
-	"github.com/golang/glog"
 )
-
-type NodeControllerConfig struct {
-	CloudProvider cloudprovider.Interface
-}
-
-func (c *NodeControllerConfig) RunController(ctx kubecontroller.ControllerContext) (bool, error) {
-	_, clusterCIDR, err := net.ParseCIDR(ctx.Options.ClusterCIDR)
-	if err != nil {
-		glog.Warningf("NodeController failed parsing cluster CIDR %v: %v", ctx.Options.ClusterCIDR, err)
-	}
-
-	_, serviceCIDR, err := net.ParseCIDR(ctx.Options.ServiceCIDR)
-	if err != nil {
-		glog.Warningf("NodeController failed parsing service CIDR %v: %v", ctx.Options.ServiceCIDR, err)
-	}
-
-	controller, err := nodecontroller.NewNodeController(
-		ctx.InformerFactory.Core().V1().Pods(),
-		ctx.InformerFactory.Core().V1().Nodes(),
-		ctx.InformerFactory.Extensions().V1beta1().DaemonSets(),
-		c.CloudProvider,
-		ctx.ClientBuilder.ClientOrDie("node-controller"),
-
-		ctx.Options.PodEvictionTimeout.Duration,
-		ctx.Options.NodeEvictionRate,
-		ctx.Options.SecondaryNodeEvictionRate,
-		ctx.Options.LargeClusterSizeThreshold,
-		ctx.Options.UnhealthyZoneThreshold,
-		ctx.Options.NodeMonitorGracePeriod.Duration,
-		ctx.Options.NodeStartupGracePeriod.Duration,
-		ctx.Options.NodeMonitorPeriod.Duration,
-
-		clusterCIDR,
-		serviceCIDR,
-
-		int(ctx.Options.NodeCIDRMaskSize),
-		ctx.Options.AllocateNodeCIDRs,
-		ctx.Options.EnableTaintManager,
-		utilfeature.DefaultFeatureGate.Enabled(features.TaintBasedEvictions),
-	)
-	if err != nil {
-		return false, fmt.Errorf("unable to start node controller: %v", err)
-	}
-
-	go controller.Run()
-
-	return true, nil
-}
-
-type ServiceLoadBalancerControllerConfig struct {
-	CloudProvider cloudprovider.Interface
-}
-
-func (c *ServiceLoadBalancerControllerConfig) RunController(ctx kubecontroller.ControllerContext) (bool, error) {
-	if c.CloudProvider == nil {
-		glog.Warningf("ServiceLoadBalancer controller will not start - no cloud provider configured")
-		return false, nil
-	}
-	serviceController, err := servicecontroller.New(
-		c.CloudProvider,
-		ctx.ClientBuilder.ClientOrDie("service-controller"),
-		ctx.InformerFactory.Core().V1().Services(),
-		ctx.InformerFactory.Core().V1().Nodes(),
-		ctx.Options.ClusterName,
-	)
-	if err != nil {
-		glog.Warningf("unable to start service load balancer controller: %v", err)
-		return false, nil
-	}
-
-	go serviceController.Run(ctx.Stop, int(ctx.Options.ConcurrentServiceSyncs))
-	return true, nil
-}
 
 type SchedulerControllerConfig struct {
 	// TODO: Move this closer to upstream, we want unprivileged client here.
@@ -141,7 +60,7 @@ func (c *SchedulerControllerConfig) RunController(ctx kubecontroller.ControllerC
 	config.Recorder = eventcast.NewRecorder(kapi.Scheme, kclientv1.EventSource{Component: kapi.DefaultSchedulerName})
 	eventcast.StartRecordingToSink(&kv1core.EventSinkImpl{Interface: kv1core.New(c.PrivilegedClient.CoreV1().RESTClient()).Events("")})
 
-	s := scheduler.New(config)
+	s := scheduler.NewFromConfig(config)
 	go s.Run()
 
 	return true, nil
