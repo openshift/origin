f723bd1a8372240ee72c05d691c3dbb86d7ad38f
deads2k <deads@redhat.com>
2017-07-06 10:09:52 -0400

    update generators to work again

--- tools/gendeepcopy/deep_copy.go
+++ tools/gendeepcopy/deep_copy.go
@@ -36,7 +36,7 @@ func main() {
 		"k8s.io/apimachinery/pkg/runtime",
 		"k8s.io/apimachinery/pkg/util/intstr",
 		"k8s.io/apimachinery/pkg/util/sets",
-		"k8s.io/kube-aggregator/pkg/apis/apiregistration/v1alpha1",
+		"k8s.io/kube-aggregator/pkg/apis/apiregistration/v1beta1",
 		"k8s.io/kube-aggregator/pkg/apis/apiregistration",
 		"github.com/openshift/origin/pkg/authorization/apis/authorization/v1",
 		"github.com/openshift/origin/pkg/authorization/apis/authorization",
