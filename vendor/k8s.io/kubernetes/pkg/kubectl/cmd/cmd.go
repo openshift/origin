3ca3f8902beee3609cd7502df1ad99479ee01b34
juanvallejo <jvallejo@redhat.com>
2017-07-13 14:45:54 -0400

    UPSTREAM: 48894: remove hardcoded use of "kubectl" in apply warning msg

--- vendor/k8s.io/kubernetes/pkg/kubectl/cmd/cmd.go
+++ vendor/k8s.io/kubernetes/pkg/kubectl/cmd/cmd.go
@@ -314,7 +314,7 @@ func NewKubectlCommand(f cmdutil.Factory, in io.Reader, out, err io.Writer) *cob
 		{
 			Message: "Advanced Commands:",
 			Commands: []*cobra.Command{
-				NewCmdApply(f, out, err),
+				NewCmdApply("kubectl", f, out, err),
 				NewCmdPatch(f, out),
 				NewCmdReplace(f, out),
 				NewCmdConvert(f, out),
