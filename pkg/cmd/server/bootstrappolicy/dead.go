f2263f6a9ee7c6ee56812298969ae0444fdcd66b
benjaminapetersen <admin@benjaminapetersen.me>
2017-07-17 16:08:10 -0400

    update bootstrappolicy/dead addDeadClusterRole to include systemOnly annotation
    Roles that have been replaced by kube controller roles should receive
    the systemOnly annotation to ensure they are not visible to typical
    end users (such as those who would use the web console).
    
    - updates bootstrappolicy/dead addDeadClusterRole
    - updates bootstrappolicy/web_console_role_test TestSystemOnlyRoles
    	- still skips controller roles (maintenance simplicity), but will throw an error if the annotation is missing

--- pkg/cmd/server/bootstrappolicy/dead.go
+++ pkg/cmd/server/bootstrappolicy/dead.go
@@ -21,7 +21,12 @@ func addDeadClusterRole(name string) {
 
 	deadClusterRoles = append(deadClusterRoles,
 		authorizationapi.ClusterRole{
-			ObjectMeta: metav1.ObjectMeta{Name: name},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+				Annotations: map[string]string{
+					roleSystemOnly: roleIsSystemOnly,
+				},
+			},
 		},
 	)
 }
