56af5a183c62039184eff22e23beb021bbb68ec0
Avesh Agarwal <avagarwa@redhat.com>
2017-07-20 14:50:32 -0400

    Create scheduler from SchedulerServer and Configurator.

--- pkg/cmd/server/kubernetes/master/controller/core.go
+++ pkg/cmd/server/kubernetes/master/controller/core.go
@@ -9,24 +9,23 @@ import (
 	kubecontroller "k8s.io/kubernetes/cmd/kube-controller-manager/app"
 	kapi "k8s.io/kubernetes/pkg/api"
 	kclientset "k8s.io/kubernetes/pkg/client/clientset_generated/clientset"
-	"k8s.io/kubernetes/plugin/pkg/scheduler"
+	schedulerapp "k8s.io/kubernetes/plugin/cmd/kube-scheduler/app"
+	scheduleroptions "k8s.io/kubernetes/plugin/cmd/kube-scheduler/app/options"
 	_ "k8s.io/kubernetes/plugin/pkg/scheduler/algorithmprovider"
-	schedulerapi "k8s.io/kubernetes/plugin/pkg/scheduler/api"
-	"k8s.io/kubernetes/plugin/pkg/scheduler/factory"
 )
 
 type SchedulerControllerConfig struct {
 	// TODO: Move this closer to upstream, we want unprivileged client here.
-	PrivilegedClient               kclientset.Interface
-	SchedulerName                  string
-	HardPodAffinitySymmetricWeight int
-	SchedulerPolicy                *schedulerapi.Policy
+	PrivilegedClient kclientset.Interface
+	SchedulerServer  *scheduleroptions.SchedulerServer
 }
 
 func (c *SchedulerControllerConfig) RunController(ctx kubecontroller.ControllerContext) (bool, error) {
-	// TODO make the rate limiter configurable
-	configFactory := factory.NewConfigFactory(
-		c.SchedulerName,
+	eventcast := record.NewBroadcaster()
+	recorder := eventcast.NewRecorder(kapi.Scheme, kclientv1.EventSource{Component: kapi.DefaultSchedulerName})
+	eventcast.StartRecordingToSink(&kv1core.EventSinkImpl{Interface: kv1core.New(c.PrivilegedClient.CoreV1().RESTClient()).Events("")})
+
+	s, err := schedulerapp.CreateScheduler(c.SchedulerServer,
 		c.PrivilegedClient,
 		ctx.InformerFactory.Core().V1().Nodes(),
 		ctx.InformerFactory.Core().V1().Pods(),
@@ -36,31 +35,12 @@ func (c *SchedulerControllerConfig) RunController(ctx kubecontroller.ControllerC
 		ctx.InformerFactory.Extensions().V1beta1().ReplicaSets(),
 		ctx.InformerFactory.Apps().V1beta1().StatefulSets(),
 		ctx.InformerFactory.Core().V1().Services(),
-		c.HardPodAffinitySymmetricWeight,
+		recorder,
 	)
-
-	var (
-		config *scheduler.Config
-		err    error
-	)
-
-	if c.SchedulerPolicy != nil {
-		config, err = configFactory.CreateFromConfig(*c.SchedulerPolicy)
-		if err != nil {
-			return true, fmt.Errorf("failed to create scheduler config from policy: %v", err)
-		}
-	} else {
-		config, err = configFactory.CreateFromProvider(factory.DefaultProvider)
-		if err != nil {
-			return true, fmt.Errorf("failed to create scheduler config: %v", err)
-		}
+	if err != nil {
+		return false, fmt.Errorf("error creating scheduler: %v", err)
 	}
 
-	eventcast := record.NewBroadcaster()
-	config.Recorder = eventcast.NewRecorder(kapi.Scheme, kclientv1.EventSource{Component: kapi.DefaultSchedulerName})
-	eventcast.StartRecordingToSink(&kv1core.EventSinkImpl{Interface: kv1core.New(c.PrivilegedClient.CoreV1().RESTClient()).Events("")})
-
-	s := scheduler.NewFromConfig(config)
 	go s.Run()
 
 	return true, nil
