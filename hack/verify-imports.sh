19388018fc7fba5a17f7f33b9b5a0613b106ee00
David Eads <deads@redhat.com>
2017-08-09 16:20:33 -0400

    start the rachet on our API types

--- hack/verify-imports.sh
+++ hack/verify-imports.sh
@@ -6,4 +6,4 @@ source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
 os::util::ensure::built_binary_exists 'import-verifier'
 
-import-verifier "${OS_ROOT}/hack/import-restrictions.json" || true
+import-verifier "${OS_ROOT}/hack/import-restrictions.json"
