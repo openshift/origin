a7b39000417a3927c0d4ebedc28c84d59e28a15b
Bruno Oliveira <bruno@abstractj.org>
2017-08-28 17:30:25 -0300

    cmd test flake: 0-length response instead of expected timeout
    Signed-off-by: Bruno Oliveira <bruno@abstractj.org>

--- test/cmd/timeout.sh
+++ test/cmd/timeout.sh
@@ -16,7 +16,7 @@ os::cmd::expect_success_and_text 'oc new-app node' 'Success'
 os::cmd::expect_success_and_text 'oc get dc node -w --request-timeout=1s 2>&1' 'Timeout exceeded while reading body'
 os::cmd::expect_success_and_text 'oc get dc node --request-timeout=1s' 'node'
 os::cmd::expect_success_and_text 'oc get dc node --request-timeout=1' 'node'
-os::cmd::expect_failure_and_text 'oc login -u testuser -p test --request-timeout=1ns' 'Timeout exceeded while awaiting headers'
+os::cmd::expect_success_and_text 'oc get pods --watch --request-timeout=1s 2>&1' 'Timeout exceeded while reading body'
 
 echo "request-timeout: ok"
 os::test::junit::declare_suite_end
