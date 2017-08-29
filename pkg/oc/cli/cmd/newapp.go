ef3ff33d10d9a9b84f63aa497cb332a07ae25e84
juanvallejo <jvallejo@redhat.com>
2017-08-29 19:46:02 -0400

    default to legacy decoder; fallback to universal / unstructured add dynamic mapper for unstructured objects

--- pkg/oc/cli/cmd/newapp.go
+++ pkg/oc/cli/cmd/newapp.go
@@ -157,11 +157,21 @@ func (o *ObjectGeneratorOptions) Complete(baseName, commandName string, f *clien
 	if err != nil {
 		return err
 	}
+
 	mapper, typer := f.Object()
+	dynamicMapper, dynamicTyper, err := f.UnstructuredObject()
+	if err != nil {
+		return err
+	}
 	// ignore errors.   We use this to make a best guess at preferred seralizations, but the command might run without a server
 	discoveryClient, _ := f.DiscoveryClient()
 
 	o.Action.Out, o.Action.ErrOut = out, o.ErrOut
+	o.Action.Bulk.DynamicMapper = &resource.Mapper{
+		RESTMapper:   dynamicMapper,
+		ObjectTyper:  dynamicTyper,
+		ClientMapper: resource.ClientMapperFunc(f.UnstructuredClientForMapping),
+	}
 	o.Action.Bulk.Mapper = &resource.Mapper{
 		RESTMapper:   mapper,
 		ObjectTyper:  typer,
