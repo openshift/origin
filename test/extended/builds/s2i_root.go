72b642b34044268b9845b04458a4a1973a164d6a
Ben Parees <bparees@redhat.com>
2017-08-31 08:47:16 -0400

    Revert "skip build tests on GCE because new images are needed there"
    This reverts commit 676bff335c3d0a9d86a904f81886d5c79d35bbbb.

--- test/extended/builds/s2i_root.go
+++ test/extended/builds/s2i_root.go
@@ -6,8 +6,6 @@ import (
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
 
-	"k8s.io/kubernetes/test/e2e/framework"
-
 	exutil "github.com/openshift/origin/test/extended/util"
 	s2istatus "github.com/openshift/source-to-image/pkg/util/status"
 )
@@ -28,7 +26,6 @@ var _ = g.Describe("[builds][Conformance] s2i build with a root user image", fun
 
 	g.Describe("Building using an image with a root default user", func() {
 		g.It("should fail the build immediately", func() {
-			framework.SkipIfProviderIs("gce")
 			oc.SetOutputDir(exutil.TestContext.OutputDir)
 
 			g.By(fmt.Sprintf("calling oc create -f %q", buildFixture))
