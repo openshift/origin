ed65560028593e417d0b6f7a6851c193d3ef8b50
Maciej Szulik <maszulik@redhat.com>
2017-09-07 16:13:06 +0200

    Rename pkg/deploy -> pkg/apps

--- pkg/apps/apis/apps/v1/doc.go
+++ pkg/apps/apis/apps/v1/doc.go
@@ -1,5 +1,5 @@
 // +k8s:deepcopy-gen=package,register
-// +k8s:conversion-gen=github.com/openshift/origin/pkg/deploy/apis/apps
+// +k8s:conversion-gen=github.com/openshift/origin/pkg/apps/apis/apps
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:openapi-gen=true
 
