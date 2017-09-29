40829117b062d4b80cac855f5bafba803be05980
Ben Parees <bparees@redhat.com>
2017-09-29 17:47:15 -0400

    resolve image secrets in the buildcontroller

--- test/extended/builds/image_source.go
+++ test/extended/builds/image_source.go
@@ -7,21 +7,29 @@ import (
 	o "github.com/onsi/gomega"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kapi "k8s.io/kubernetes/pkg/api"
+	kapiv1 "k8s.io/kubernetes/pkg/api/v1"
 
+	buildapi "github.com/openshift/origin/pkg/build/apis/build"
 	exutil "github.com/openshift/origin/test/extended/util"
 )
 
 var _ = g.Describe("[Feature:Builds][Slow] build can have Docker image source", func() {
 	defer g.GinkgoRecover()
 	var (
-		buildFixture     = exutil.FixturePath("testdata", "test-imagesource-build.yaml")
-		oc               = exutil.NewCLI("build-image-source", exutil.KubeConfigPath())
-		imageSourceLabel = exutil.ParseLabelsOrDie("app=imagesourceapp")
-		imageDockerLabel = exutil.ParseLabelsOrDie("app=imagedockerapp")
+		buildConfigFixture = exutil.FixturePath("testdata", "test-imagesource-buildconfig.yaml")
+		s2iBuildFixture    = exutil.FixturePath("testdata", "test-imageresolution-s2i-build.yaml")
+		dockerBuildFixture = exutil.FixturePath("testdata", "test-imageresolution-docker-build.yaml")
+		customBuildFixture = exutil.FixturePath("testdata", "test-imageresolution-custom-build.yaml")
+		oc                 = exutil.NewCLI("build-image-source", exutil.KubeConfigPath())
+		imageSourceLabel   = exutil.ParseLabelsOrDie("app=imagesourceapp")
+		imageDockerLabel   = exutil.ParseLabelsOrDie("app=imagedockerapp")
+		sourceBuildLabel   = exutil.ParseLabelsOrDie("openshift.io/build.name=imagesourcebuild")
+		dockerBuildLabel   = exutil.ParseLabelsOrDie("openshift.io/build.name=imagedockerbuild")
+		customBuildLabel   = exutil.ParseLabelsOrDie("openshift.io/build.name=imagecustombuild")
 	)
 
 	g.Context("", func() {
-
 		g.JustBeforeEach(func() {
 			g.By("waiting for builder service account")
 			err := exutil.WaitForBuilderAccount(oc.KubeClient().Core().ServiceAccounts(oc.Namespace()))
@@ -39,10 +47,10 @@ var _ = g.Describe("[Feature:Builds][Slow] build can have Docker image source", 
 			}
 		})
 
-		g.Describe("build with image source", func() {
+		g.Describe("buildconfig with input source image and s2i strategy", func() {
 			g.It("should complete successfully and contain the expected file", func() {
 				g.By("Creating build configs for source build")
-				err := oc.Run("create").Args("-f", buildFixture).Execute()
+				err := oc.Run("create").Args("-f", buildConfigFixture).Execute()
 				o.Expect(err).NotTo(o.HaveOccurred())
 
 				g.By("starting building the private input image")
@@ -50,7 +58,7 @@ var _ = g.Describe("[Feature:Builds][Slow] build can have Docker image source", 
 				br.AssertSuccess()
 
 				g.By("starting the source strategy build")
-				br, err = exutil.StartBuildAndWait(oc, "imagesourcebuild")
+				br, err = exutil.StartBuildAndWait(oc, "imagesourcebuildconfig")
 				br.AssertSuccess()
 
 				g.By("expecting the pod to deploy successfully")
@@ -66,10 +74,10 @@ var _ = g.Describe("[Feature:Builds][Slow] build can have Docker image source", 
 				o.Expect(out).To(o.ContainSubstring("ruby"))
 			})
 		})
-		g.Describe("build with image docker", func() {
+		g.Describe("buildconfig with input source image and docker strategy", func() {
 			g.It("should complete successfully and contain the expected file", func() {
 				g.By("Creating build configs for docker build")
-				err := oc.Run("create").Args("-f", buildFixture).Execute()
+				err := oc.Run("create").Args("-f", buildConfigFixture).Execute()
 				o.Expect(err).NotTo(o.HaveOccurred())
 
 				g.By("starting building the private input image")
@@ -77,7 +85,7 @@ var _ = g.Describe("[Feature:Builds][Slow] build can have Docker image source", 
 				br.AssertSuccess()
 
 				g.By("starting the docker strategy build")
-				br, err = exutil.StartBuildAndWait(oc, "imagedockerbuild")
+				br, err = exutil.StartBuildAndWait(oc, "imagedockerbuildconfig")
 				br.AssertSuccess()
 
 				g.By("expect the pod to deploy successfully")
@@ -92,7 +100,150 @@ var _ = g.Describe("[Feature:Builds][Slow] build can have Docker image source", 
 				o.Expect(err).NotTo(o.HaveOccurred())
 				o.Expect(out).To(o.ContainSubstring("ruby"))
 			})
+		})
+		g.Describe("creating a build with an input source image and s2i strategy", func() {
+			g.It("should resolve the imagestream references and secrets", func() {
+				g.By("Creating build configs for input image")
+				err := oc.Run("create").Args("-f", buildConfigFixture).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
 
+				g.By("starting building the private input image")
+				br, err := exutil.StartBuildAndWait(oc, "inputimage")
+				br.AssertSuccess()
+
+				g.By("Creating a build for source build")
+				err = oc.Run("create").Args("-f", s2iBuildFixture).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				g.By("expecting the build pod to start running")
+				pods, err := exutil.WaitForPods(oc.KubeClient().Core().Pods(oc.Namespace()), sourceBuildLabel, exutil.CheckPodIsRunningFn, 1, 2*time.Minute)
+				o.Expect(err).NotTo(o.HaveOccurred())
+				o.Expect(len(pods)).To(o.Equal(1))
+				pod, err := oc.KubeClient().Core().Pods(oc.Namespace()).Get(pods[0], metav1.GetOptions{})
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				foundEnv := false
+				for _, env := range pod.Spec.Containers[0].Env {
+					if env.Name == "BUILD" {
+						foundEnv = true
+
+						obj, _, err := kapi.Codecs.UniversalDecoder().Decode([]byte(env.Value), nil, nil)
+						o.Expect(err).NotTo(o.HaveOccurred())
+						ok := false
+						build, ok := obj.(*buildapi.Build)
+						o.Expect(ok).To(o.BeTrue(), "could not convert build env\n %s\n to a build object", env.Value)
+						o.Expect(build.Spec.Strategy.SourceStrategy.From.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Strategy.SourceStrategy.From.Name).To(o.ContainSubstring("@sha256:"))
+						o.Expect(build.Spec.Strategy.SourceStrategy.PullSecret).NotTo(o.BeNil())
+
+						o.Expect(build.Spec.Source.Images[0].From.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Source.Images[0].From.Name).To(o.ContainSubstring("@sha256:"))
+						o.Expect(build.Spec.Source.Images[0].PullSecret).NotTo(o.BeNil())
+
+						o.Expect(build.Spec.Output.To.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Output.PushSecret).NotTo(o.BeNil())
+					}
+				}
+				o.Expect(foundEnv).To(o.BeTrue(), "did not find BUILD env in build pod %#v", pod)
+			})
+		})
+		g.Describe("creating a build with an input source image and docker strategy", func() {
+			g.It("should resolve the imagestream references and secrets", func() {
+				g.By("Creating build configs for input image")
+				err := oc.Run("create").Args("-f", buildConfigFixture).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				g.By("starting building the private input image")
+				br, err := exutil.StartBuildAndWait(oc, "inputimage")
+				br.AssertSuccess()
+
+				g.By("Creating a build for docker build")
+				err = oc.Run("create").Args("-f", dockerBuildFixture).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				g.By("expecting the build pod to start running")
+				pods, err := exutil.WaitForPods(oc.KubeClient().Core().Pods(oc.Namespace()), dockerBuildLabel, exutil.CheckPodIsRunningFn, 1, 2*time.Minute)
+				o.Expect(err).NotTo(o.HaveOccurred())
+				o.Expect(len(pods)).To(o.Equal(1))
+				pod, err := oc.KubeClient().Core().Pods(oc.Namespace()).Get(pods[0], metav1.GetOptions{})
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				foundEnv := false
+				for _, env := range pod.Spec.Containers[0].Env {
+					if env.Name == "BUILD" {
+						foundEnv = true
+
+						obj, _, err := kapi.Codecs.UniversalDecoder().Decode([]byte(env.Value), nil, nil)
+						o.Expect(err).NotTo(o.HaveOccurred())
+						ok := false
+						build, ok := obj.(*buildapi.Build)
+						o.Expect(ok).To(o.BeTrue(), "could not convert build env\n %s\n to a build object", env.Value)
+						o.Expect(build.Spec.Strategy.DockerStrategy.From.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Strategy.DockerStrategy.From.Name).To(o.ContainSubstring("@sha256:"))
+						o.Expect(build.Spec.Strategy.DockerStrategy.PullSecret).NotTo(o.BeNil())
+
+						o.Expect(build.Spec.Source.Images[0].From.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Source.Images[0].From.Name).To(o.ContainSubstring("@sha256:"))
+						o.Expect(build.Spec.Source.Images[0].PullSecret).NotTo(o.BeNil())
+
+						o.Expect(build.Spec.Output.To.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Output.PushSecret).NotTo(o.BeNil())
+					}
+				}
+				o.Expect(foundEnv).To(o.BeTrue(), "did not find BUILD env in build pod %#v", pod)
+			})
+		})
+		g.Describe("creating a build with an input source image and custom strategy", func() {
+			g.It("should resolve the imagestream references and secrets", func() {
+				g.By("Creating build configs for input image")
+				err := oc.Run("create").Args("-f", buildConfigFixture).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				g.By("starting building the private input image")
+				br, err := exutil.StartBuildAndWait(oc, "inputimage")
+				br.AssertSuccess()
+
+				g.By("Creating a build for custom build")
+				err = oc.AsAdmin().Run("create").Args("-f", customBuildFixture).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				g.By("expecting the build pod to exist")
+				pods, err := exutil.WaitForPods(oc.KubeClient().Core().Pods(oc.Namespace()), customBuildLabel, func(kapiv1.Pod) bool { return true }, 1, 2*time.Minute)
+				o.Expect(err).NotTo(o.HaveOccurred())
+				o.Expect(len(pods)).To(o.Equal(1))
+				pod, err := oc.KubeClient().Core().Pods(oc.Namespace()).Get(pods[0], metav1.GetOptions{})
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				foundBuildEnv := false
+				foundCustomEnv := false
+				for _, env := range pod.Spec.Containers[0].Env {
+					if env.Name == "BUILD" {
+						foundBuildEnv = true
+
+						obj, _, err := kapi.Codecs.UniversalDecoder().Decode([]byte(env.Value), nil, nil)
+						o.Expect(err).NotTo(o.HaveOccurred())
+						ok := false
+						build, ok := obj.(*buildapi.Build)
+						o.Expect(ok).To(o.BeTrue(), "could not convert build env\n %s\n to a build object", env.Value)
+						o.Expect(build.Spec.Strategy.CustomStrategy.From.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Strategy.CustomStrategy.From.Name).To(o.ContainSubstring("@sha256:"))
+						o.Expect(build.Spec.Strategy.CustomStrategy.PullSecret).NotTo(o.BeNil())
+
+						o.Expect(build.Spec.Source.Images[0].From.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Source.Images[0].From.Name).To(o.ContainSubstring("@sha256:"))
+						o.Expect(build.Spec.Source.Images[0].PullSecret).NotTo(o.BeNil())
+
+						o.Expect(build.Spec.Output.To.Kind).To(o.Equal("DockerImage"))
+						o.Expect(build.Spec.Output.PushSecret).NotTo(o.BeNil())
+					}
+					if env.Name == buildapi.CustomBuildStrategyBaseImageKey {
+						foundCustomEnv = true
+						o.Expect(env.Value).To(o.ContainSubstring("@sha256:"))
+					}
+				}
+				o.Expect(foundBuildEnv).To(o.BeTrue(), "did not find BUILD env in build pod %#v", pod)
+				o.Expect(foundCustomEnv).To(o.BeTrue(), "did not find Custom base image env in build pod %#v", pod)
+			})
 		})
 	})
 })
