f3eeb736f2c08c52a2b2865065981b91d5462f46
Joel Smith <joesmith@redhat.com>
2017-10-12 11:25:11 -0600

    UPSTREAM: 53753: Reduce log spam in qos container manager

--- vendor/k8s.io/kubernetes/pkg/kubelet/cm/qos_container_manager_linux.go
+++ vendor/k8s.io/kubernetes/pkg/kubelet/cm/qos_container_manager_linux.go
@@ -282,7 +282,7 @@ func (m *qosContainerManagerImpl) UpdateCgroups() error {
 		}
 	}
 	if updateSuccess {
-		glog.V(2).Infof("[ContainerManager]: Updated QoS cgroup configuration")
+		glog.V(4).Infof("[ContainerManager]: Updated QoS cgroup configuration")
 		return nil
 	}
 
@@ -299,12 +299,12 @@ func (m *qosContainerManagerImpl) UpdateCgroups() error {
 	for _, config := range qosConfigs {
 		err := m.cgroupManager.Update(config)
 		if err != nil {
-			glog.V(2).Infof("[ContainerManager]: Failed to update QoS cgroup configuration")
+			glog.Errorf("[ContainerManager]: Failed to update QoS cgroup configuration")
 			return err
 		}
 	}
 
-	glog.V(2).Infof("[ContainerManager]: Updated QoS cgroup configuration on retry")
+	glog.V(4).Infof("[ContainerManager]: Updated QoS cgroup configuration on retry")
 	return nil
 }
 
