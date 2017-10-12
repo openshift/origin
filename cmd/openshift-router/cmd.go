47315e7c0153a8221e6c5df04a96e72c2fba3c77
David Eads <deads@redhat.com>
2017-10-12 10:51:53 -0400

    make openshift-router a real binary

---
+++ cmd/openshift-router/cmd.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	"k8s.io/kubernetes/pkg/util/logs"
+
+	irouter "github.com/openshift/origin/pkg/cmd/infra/router"
+	"github.com/openshift/origin/pkg/cmd/util/serviceability"
+
+	// install all APIs
+	_ "github.com/openshift/origin/pkg/api/install"
+	_ "k8s.io/kubernetes/pkg/api/install"
+	_ "k8s.io/kubernetes/pkg/apis/autoscaling/install"
+	_ "k8s.io/kubernetes/pkg/apis/batch/install"
+	_ "k8s.io/kubernetes/pkg/apis/extensions/install"
+)
+
+func main() {
+	logs.InitLogs()
+	defer logs.FlushLogs()
+	defer serviceability.BehaviorOnPanic(os.Getenv("OPENSHIFT_ON_PANIC"))()
+	defer serviceability.Profile(os.Getenv("OPENSHIFT_PROFILE")).Stop()
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	if len(os.Getenv("GOMAXPROCS")) == 0 {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
+	basename := filepath.Base(os.Args[0])
+	command := irouter.NewCommandTemplateRouter(basename)
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
