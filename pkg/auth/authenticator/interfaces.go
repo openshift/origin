b638188e44217e8190e22e664e2534a5f0962699
Monis Khan <mkhan@redhat.com>
2017-11-14 12:01:05 -0500

    Remove duplicate Origin authenticator interfaces
    This change removes the duplicate authenticator interfaces that are
    no longer needed.  The exact same interfaces exist upstream, so this
    change is purely mechanical.
    
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/auth/authenticator/interfaces.go
+++ pkg/auth/authenticator/interfaces.go
@@ -1,23 +1,10 @@
 package authenticator
 
 import (
-	"net/http"
+	"k8s.io/apiserver/pkg/authentication/user"
 
 	"github.com/openshift/origin/pkg/auth/api"
-	"k8s.io/apiserver/pkg/authentication/user"
 )
-
-type Token interface {
-	AuthenticateToken(token string) (user.Info, bool, error)
-}
-
-type Request interface {
-	AuthenticateRequest(req *http.Request) (user.Info, bool, error)
-}
-
-type Password interface {
-	AuthenticatePassword(user, password string) (user.Info, bool, error)
-}
 
 type Assertion interface {
 	AuthenticateAssertion(assertionType, data string) (user.Info, bool, error)
@@ -25,10 +12,4 @@ type Assertion interface {
 
 type Client interface {
 	AuthenticateClient(client api.Client) (user.Info, bool, error)
-}
-
-type RequestFunc func(req *http.Request) (user.Info, bool, error)
-
-func (f RequestFunc) AuthenticateRequest(req *http.Request) (user.Info, bool, error) {
-	return f(req)
 }
