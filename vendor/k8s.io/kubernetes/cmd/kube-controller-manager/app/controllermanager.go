917624503c69aa44778ad478dd85e82e7e41d1e6
Jordan Liggitt <jliggitt@redhat.com>
2017-11-15 23:46:01 -0500

    UPSTREAM: <carry>: allow controller context injection to share informers

--- vendor/k8s.io/kubernetes/cmd/kube-controller-manager/app/controllermanager.go
+++ vendor/k8s.io/kubernetes/cmd/kube-controller-manager/app/controllermanager.go
@@ -160,10 +160,10 @@ func Run(s *options.CMServer) error {
 
 		if StartInformers == nil {
 			ctx.InformerFactory.Start(ctx.Stop)
-			close(ctx.InformersStarted)
 		} else {
 			StartInformers(ctx.Stop)
 		}
+		close(ctx.InformersStarted)
 
 		select {}
 	}
