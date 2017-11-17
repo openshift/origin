df42d5a446fe7d433c3d79e2653fcb7b2dd7b6fa
Jordan Liggitt <jliggitt@redhat.com>
2017-11-17 13:25:07 -0500

    interesting: fixup template data types

--- pkg/oc/bootstrap/bindata.go
+++ pkg/oc/bootstrap/bindata.go
@@ -5492,11 +5492,11 @@ objects:
   spec:
     alternateBackends:
     - name: ${NAME}-green
-      weight: "0"
+      weight: 0
     to:
       kind: Service
       name: ${NAME}-blue
-      weight: "100"
+      weight: 100
 - apiVersion: v1
   kind: ImageStream
   metadata:
@@ -6151,7 +6151,7 @@ objects:
           kind: ImageStreamTag
           name: wildfly:latest
       type: Docker
-    triggers: {}
+    triggers: []
 - apiVersion: v1
   kind: DeploymentConfig
   metadata:
