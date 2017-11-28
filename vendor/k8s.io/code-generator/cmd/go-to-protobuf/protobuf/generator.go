4ef508bf991aefc25bdf70ebea5ef5860078ba68
David Eads <deads@redhat.com>
2017-11-28 08:07:08 -0500

    bump(*)

--- vendor/k8s.io/code-generator/cmd/go-to-protobuf/protobuf/generator.go
+++ vendor/k8s.io/code-generator/cmd/go-to-protobuf/protobuf/generator.go
@@ -513,13 +513,10 @@ func memberTypeToProtobufField(locator ProtobufLocator, field *protoField, t *ty
 				log.Printf("failed to alias: %s %s: err %v", t.Name, t.Underlying.Name, err)
 				return err
 			}
-			// If this is not an alias to a slice, cast to the alias
-			if !field.Repeated {
-				if field.Extras == nil {
-					field.Extras = make(map[string]string)
-				}
-				field.Extras["(gogoproto.casttype)"] = strconv.Quote(locator.CastTypeName(t.Name))
+			if field.Extras == nil {
+				field.Extras = make(map[string]string)
 			}
+			field.Extras["(gogoproto.casttype)"] = strconv.Quote(locator.CastTypeName(t.Name))
 		}
 	case types.Slice:
 		if t.Elem.Name.Name == "byte" && len(t.Elem.Name.Package) == 0 {
