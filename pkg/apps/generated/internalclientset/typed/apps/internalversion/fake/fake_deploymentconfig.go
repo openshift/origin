b948af9dd3bdd96a982525e0d60dab60d80c51da
Jordan Liggitt <jliggitt@redhat.com>
2017-11-30 15:49:03 -0500

    Fix deployment config UpdateScale

--- pkg/apps/generated/internalclientset/typed/apps/internalversion/fake/fake_deploymentconfig.go
+++ pkg/apps/generated/internalclientset/typed/apps/internalversion/fake/fake_deploymentconfig.go
@@ -154,14 +154,3 @@ func (c *FakeDeploymentConfigs) GetScale(deploymentConfigName string, options v1
 	}
 	return obj.(*v1beta1.Scale), err
 }
-
-// UpdateScale takes the representation of a scale and updates it. Returns the server's representation of the scale, and an error, if there is any.
-func (c *FakeDeploymentConfigs) UpdateScale(deploymentConfigName string, scale *v1beta1.Scale) (result *v1beta1.Scale, err error) {
-	obj, err := c.Fake.
-		Invokes(testing.NewUpdateSubresourceAction(deploymentconfigsResource, "scale", c.ns, scale), &v1beta1.Scale{})
-
-	if obj == nil {
-		return nil, err
-	}
-	return obj.(*v1beta1.Scale), err
-}
