ad4370b2da7428222a114a4e9d3a77894e8dafb7
David Eads <deads@redhat.com>
2017-12-07 12:03:45 -0500

    UPSTREAM: <carry>: exclude some origin resources from quota

--- vendor/k8s.io/kubernetes/pkg/quota/install/registry.go
+++ vendor/k8s.io/kubernetes/pkg/quota/install/registry.go
@@ -47,6 +47,10 @@ var ignoredResources = map[schema.GroupResource]struct{}{
 	{Group: "authorization.k8s.io", Resource: "selfsubjectaccessreviews"}:  {},
 	{Group: "authorization.k8s.io", Resource: "localsubjectaccessreviews"}: {},
 	{Group: "authorization.k8s.io", Resource: "selfsubjectrulesreviews"}:   {},
+	{Group: "authorization.openshift.io", Resource: "roles"}:      {},
+	{Group: "authorization.openshift.io", Resource: "rolebindings"}:  {},
+	{Group: "authorization.openshift.io", Resource: "clusterroles"}: {},
+	{Group: "authorization.openshift.io", Resource: "clusterrolebindings"}:   {},
 	{Group: "apiregistration.k8s.io", Resource: "apiservices"}:             {},
 	{Group: "apiextensions.k8s.io", Resource: "customresourcedefinitions"}: {},
 }
