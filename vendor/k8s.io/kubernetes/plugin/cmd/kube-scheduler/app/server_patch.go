7092eaa459c8ffc58690d898162e35eecd6fe428
David Eads <deads@redhat.com>
2017-12-08 12:27:36 -0500

    UPSTREAM: <carry>: patch scheduler to apply defaults.  drop once we run separate scheduler

--- vendor/k8s.io/kubernetes/plugin/cmd/kube-scheduler/app/server_patch.go
+++ vendor/k8s.io/kubernetes/plugin/cmd/kube-scheduler/app/server_patch.go
@@ -1,6 +1,10 @@
 package app
 
 
+func (o *Options) GetConfig() *componentconfig.KubeSchedulerConfiguration {
+	return o.config
+}
+
 func (o *Options) ReallyApplyDefaults() (err error) {
 	o.config, err = o.ApplyDefaults(o.config)
 	return err
