ce08d2fdb2990ff8b1b7fcad79236f87c0a621a6
David Eads <deads@redhat.com>
2017-12-18 13:40:43 -0500

    UPSTREAM: revert: 9176245: <carry>: allow controller context injection to share informers
    This reverts commit 917624503c69aa44778ad478dd85e82e7e41d1e6.

--- vendor/k8s.io/kubernetes/cmd/kube-controller-manager/app/patch.go
+++ vendor/k8s.io/kubernetes/cmd/kube-controller-manager/app/patch.go
@@ -7,6 +7,3 @@ import (
 
 // This allows overriding from inside the same process.  It's not pretty, but its fairly easy to maintain because conflicts are small.
 var CreateControllerContext func(s *options.CMServer, rootClientBuilder, clientBuilder controller.ControllerClientBuilder, stop <-chan struct{}) (ControllerContext, error) = createControllerContext
-
-// StartInformers allows overriding inside of the same process.
-var StartInformers func(stop <-chan struct{}) = nil
