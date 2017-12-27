3069212616404a976f01a233d756f04f29310b94
Eldad Marciano <mrsiano@gmail.com>
2017-12-27 14:53:19 +0200

    cluster_loader - BuildClient is no longer valid.
    replace the BuildClient with Clinet().Builds()

--- test/extended/cluster/cl.go
+++ test/extended/cluster/cl.go
@@ -129,14 +129,14 @@ var _ = g.Describe("[Feature:Performance][Serial][Slow] Load cluster", func() {
 
 		// Wait for builds and deployments to complete
 		for _, ns := range namespaces {
-			buildList, err := oc.BuildClient().Build().Builds(ns).List(metav1.ListOptions{})
+			buildList, err := oc.Client().Builds(ns).List(metav1.ListOptions{})
 			if err != nil {
 				e2e.Logf("Error listing builds: %v", err)
 			}
 			if len(buildList.Items) > 0 {
 				buildName := buildList.Items[0].Name
 				e2e.Logf("Waiting for build: %q", buildName)
-				err = exutil.WaitForABuild(oc.BuildClient().Build().Builds(ns), buildName, nil, nil, nil)
+				err = exutil.WaitForABuild(oc.Client().Builds(ns), buildName, nil, nil, nil)
 				if err != nil {
 					exutil.DumpBuildLogs(buildName, oc)
 				}
@@ -146,8 +146,11 @@ var _ = g.Describe("[Feature:Performance][Serial][Slow] Load cluster", func() {
 				// deploymentName is buildName without the -1 suffix
 				deploymentName := buildName[:len(buildName)-2]
 				e2e.Logf("Waiting for deployment: %q", deploymentName)
-				err = exutil.WaitForDeploymentConfig(oc.KubeClient(), oc.AppsClient().Apps(), ns, deploymentName, 1, oc)
-				o.Expect(err).NotTo(o.HaveOccurred())
+				out, err := oc.Run("rollout status deployment/" + deploymentName).Output()
+				// check if rollout completed successfully.
+				if !strings.Contains(out, "successfully rolled out") {
+					o.Expect(err).NotTo(o.HaveOccurred())
+				}
 				e2e.Logf("Deployment %q completed", deploymentName)
 			}
 		}
