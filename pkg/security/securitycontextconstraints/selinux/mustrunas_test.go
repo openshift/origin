cymtrick/kubernetes-error-fuzzer

selinux/mustrunas_test.go(TestMustRunAsValidate): add more test cases to improve code coverage.

pkg/security/podsecuritypolicy/selinux/mustrunas_test.go
@@ -93,9 +93,6 @@ func TestMustRunAsValidate(t *testing.T) {
 	user := newValidOpts()
 	user.User = "invalid"
 
-	level := newValidOpts()
-	level.Level = "invalid"
-
 	seType := newValidOpts()
 	seType.Type = "invalid"
 
@@ -116,15 +113,20 @@ func TestMustRunAsValidate(t *testing.T) {
 			pspSeLinux:  validOpts,
 			expectedMsg: "user: Invalid value",
 		},
-		"invalid level": {
-			podSeLinux:  level,
-			pspSeLinux:  validOpts,
+		"levels are not equal": {
+			podSeLinux:  newValidOptsWithLevel("s0"),
+			pspSeLinux:  newValidOptsWithLevel("s0:c1,c2"),
 			expectedMsg: "level: Invalid value",
 		},
-		"invalid type": {
-			podSeLinux:  seType,
-			pspSeLinux:  validOpts,
-			expectedMsg: "type: Invalid value",
+		"levels differ by sensitivity": {
+			podSeLinux:  newValidOptsWithLevel("s0:c6"),
+			pspSeLinux:  newValidOptsWithLevel("s1:c6"),
+			expectedMsg: "level: Invalid value",
+		},
+		"levels differ by categories": {
+			podSeLinux:  newValidOptsWithLevel("s0:c0,c8"),
+			pspSeLinux:  newValidOptsWithLevel("s0:c1,c7"),
+			expectedMsg: "level: Invalid value",
 		},
 		"valid": {
 			podSeLinux:  validOpts,

openshift/origin

pkg/security/securitycontextconstraints/selinux/mustrunas_test.go
@@ -79,9 +79,6 @@ func TestMustRunAsValidate(t *testing.T) {
 	user := newValidOpts()
 	user.User = "invalid"
 
-	level := newValidOpts()
-	level.Level = "invalid"
-
 	seType := newValidOpts()
 	seType.Type = "invalid"
 
@@ -102,15 +99,20 @@ func TestMustRunAsValidate(t *testing.T) {
 			sccSeLinux:  validOpts,
 			expectedMsg: "user: Invalid value",
 		},
-		"invalid level": {
-			podSeLinux:  level,
-			sccSeLinux:  validOpts,
+		"levels are not equal": {
+			podSeLinux:  newValidOptsWithLevel("s0"),
+			sccSeLinux:  newValidOptsWithLevel("s0:c1,c2"),
 			expectedMsg: "level: Invalid value",
 		},
-		"invalid type": {
-			podSeLinux:  seType,
-			sccSeLinux:  validOpts,
-			expectedMsg: "type: Invalid value",
+		"levels differ by sensitivity": {
+			podSeLinux:  newValidOptsWithLevel("s0:c6"),
+			sccSeLinux:  newValidOptsWithLevel("s1:c6"),
+			expectedMsg: "level: Invalid value",
+		},
+		"levels differ by categories": {
+			podSeLinux:  newValidOptsWithLevel("s0:c0,c8"),
+			sccSeLinux:  newValidOptsWithLevel("s0:c1,c7"),
+			expectedMsg: "level: Invalid value",
 		},
 		"valid": {
 			podSeLinux:  validOpts,