b1579e24a45b3a9d51e7ab73a6c7e0ef197acfa0
David Eads <deads@redhat.com>
2018-01-30 09:42:09 -0500

    clean up for code we know we're deleting

--- pkg/cmd/server/kubernetes/master/master_test.go
+++ pkg/cmd/server/kubernetes/master/master_test.go
@@ -4,24 +4,17 @@ import (
 	"testing"
 	"time"
 
-	"github.com/coreos/etcd/client"
 	"golang.org/x/net/context"
+
 	"k8s.io/apiserver/pkg/registry/generic"
 	"k8s.io/apiserver/pkg/storage/etcd/etcdtest"
 	etcdtesting "k8s.io/apiserver/pkg/storage/etcd/testing"
-	"k8s.io/apiserver/pkg/storage/storagebackend"
 	"k8s.io/kubernetes/pkg/api/testapi"
 )
 
 func TestNewMasterLeasesHasCorrectTTL(t *testing.T) {
-	server := etcdtesting.NewUnsecuredEtcdTestClientServer(t)
-	etcdStorage := &storagebackend.Config{
-		Type:                     "etcd2",
-		Prefix:                   etcdtest.PathPrefix(),
-		ServerList:               server.Client.Endpoints(),
-		DeserializationCacheSize: etcdtest.DeserializationCacheSize,
-		Codec: testapi.Groups[""].StorageCodec(),
-	}
+	server, etcdStorage := etcdtesting.NewUnsecuredEtcd3TestClientServer(t)
+	etcdStorage.Codec = testapi.Groups[""].StorageCodec()
 
 	restOptions := generic.RESTOptions{StorageConfig: etcdStorage, Decorator: generic.UndecoratedStorage, DeleteCollectionWorkers: 1}
 	storageInterface, _ := restOptions.Decorator(restOptions.StorageConfig, nil, "masterleases", nil, nil, nil, nil)
@@ -32,9 +25,8 @@ func TestNewMasterLeasesHasCorrectTTL(t *testing.T) {
 		t.Fatalf("error updating lease: %v", err)
 	}
 
-	etcdClient := server.Client
-	keys := client.NewKeysAPI(etcdClient)
-	resp, err := keys.Get(context.Background(), etcdtest.PathPrefix()+"/masterleases/1.2.3.4", nil)
+	etcdClient := server.V3Client
+	resp, err := etcdClient.Get(context.Background(), etcdtest.PathPrefix()+"/masterleases/1.2.3.4", nil)
 	if err != nil {
 		t.Fatalf("error getting key: %v", err)
 	}
