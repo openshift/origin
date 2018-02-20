80dad6af7196f3b47f92efa703f2a5b9e60a800d
Michal Minář <miminar@redhat.com>
2018-02-20 11:05:03 +0000

    debug
    Signed-off-by: Michal Minář <miminar@redhat.com>

--- test/extended/conformance.sh
+++ test/extended/conformance.sh
@@ -9,6 +9,14 @@ os::test::extended::focus "$@"
 
 exitstatus=0
 
+echo "DEBUG BEGIN ***************************************************"
+printf '%s:\t"%s"\n' \
+    "SERVER_CONFIG_DIR" "${SERVER_CONFIG_DIR:-}" \
+    "MASTER_CONFIG_DIR" "${MASTER_CONFIG_DIR:-}" \
+    "imagePolicyConfig" "$(sed -n  '/^imagePolicyConfig/,/^[^ ]/p' "${MASTER_CONFIG_DIR:-}/master-config.yaml")"
+    "OPENSHIFT_DEFAULT_REGISTRY" "${OPENSHIFT_DEFAULT_REGISTRY}"
+echo "DEBUG END *****************************************************"
+
 # run parallel tests
 os::log::info "Running parallel tests N=${PARALLEL_NODES:-<default>}"
 TEST_PARALLEL="${PARALLEL_NODES:-5}" TEST_REPORT_FILE_NAME=conformance_parallel os::test::extended::run -- -suite "parallel.conformance.openshift.io" -test.timeout 6h ${TEST_EXTENDED_ARGS-} || exitstatus=$?
