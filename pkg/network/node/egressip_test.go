6758ab7805da9c4c41e30c994e8c1727f2cf51ce
Dan Winship <danw@redhat.com>
2018-02-27 14:26:55 -0500

    Add assertOVSChanges helper to egressip_test
    This makes the tests a little bit more self-documenting, and makes it
    simpler to add new tests. Also, change it to only look at OVS flows in
    the egress table, so that when there is a mismatch, the error message
    is of a manageable length.

--- pkg/network/node/egressip_test.go
+++ pkg/network/node/egressip_test.go
@@ -2,6 +2,7 @@ package node
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -26,14 +27,100 @@ func assertNetlinkChange(eip *egressIPWatcher, expected string) error {
 	}
 }
 
+type egressTrafficType string
+
+const (
+	Normal  egressTrafficType = "normal"
+	Dropped egressTrafficType = "dropped"
+	Local   egressTrafficType = "local"
+	Remote  egressTrafficType = "remote"
+)
+
+type egressOVSChange struct {
+	vnid   uint32
+	egress egressTrafficType
+	remote string
+}
+
+// Takes the previous set of egress OVS flows, then fetches the current set and checks
+// that the expected changes have occurred. Each namespace whose egress has changed should
+// have an egressOVSChange struct describing the expected new state. On success, returns
+// the new/current set of flows in flows.
+func assertOVSChanges(eip *egressIPWatcher, flows *[]string, changes ...egressOVSChange) error {
+	oldFlows := *flows
+	newFlows, err := eip.oc.ovs.DumpFlows("table=100")
+	if err != nil {
+		return fmt.Errorf("unexpected error dumping OVS flows: %v", err)
+	}
+
+	flowChanges := []flowChange{}
+	for _, change := range changes {
+		vnidStr := fmt.Sprintf("reg0=%d", change.vnid)
+		for _, flow := range *flows {
+			if strings.Contains(flow, vnidStr) {
+				flowChanges = append(flowChanges,
+					flowChange{
+						kind:  flowRemoved,
+						match: []string{flow},
+					},
+				)
+			}
+		}
+
+		switch change.egress {
+		case Normal:
+			break
+		case Dropped:
+			flowChanges = append(flowChanges,
+				flowChange{
+					kind:  flowAdded,
+					match: []string{vnidStr, "drop"},
+				},
+			)
+		case Local:
+			flowChanges = append(flowChanges,
+				flowChange{
+					kind:  flowAdded,
+					match: []string{vnidStr, fmt.Sprintf("%s->pkt_mark", getMarkForVNID(change.vnid, eip.masqueradeBit)), "goto_table:101"},
+				},
+			)
+		case Remote:
+			flowChanges = append(flowChanges,
+				flowChange{
+					kind:  flowAdded,
+					match: []string{vnidStr, fmt.Sprintf("%s->tun_dst", change.remote)},
+				},
+			)
+		}
+	}
+	err = assertFlowChanges(oldFlows, newFlows, flowChanges...)
+	if err != nil {
+		return fmt.Errorf("unexpected flow changes: %v\nOrig:\n%s\nNew:\n%s", err,
+			strings.Join(oldFlows, "\n"), strings.Join(newFlows, "\n"))
+	}
+
+	*flows = newFlows
+	return nil
+}
+
+// Checks that no OVS changes have occurred (relative to the provided old flows)
+func assertNoOVSChanges(eip *egressIPWatcher, flows *[]string) error {
+	return assertOVSChanges(eip, flows)
+}
+
 func TestEgressIP(t *testing.T) {
-	ovsif, oc, origFlows := setupOVSController(t)
+	_, oc, _ := setupOVSController(t)
 	if oc.localIP != "172.17.0.4" {
 		panic("details of fake ovsController changed")
 	}
 	masqBit := int32(0)
 	eip := newEgressIPWatcher(oc, "172.17.0.4", &masqBit)
 	eip.testModeChan = make(chan string, 10)
+
+	flows, err := eip.oc.ovs.DumpFlows("table=100")
+	if err != nil {
+		t.Fatalf("unexpected error dumping OVS flows: %v", err)
+	}
 
 	eip.updateNodeEgress("172.17.0.3", []string{})
 	eip.updateNodeEgress("172.17.0.4", []string{})
@@ -45,17 +132,13 @@ func TestEgressIP(t *testing.T) {
 	}
 
 	// No namespaces use egress yet, so should be no changes
-	err := assertNoNetlinkChanges(eip)
+	err = assertNoNetlinkChanges(eip)
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err := ovsif.DumpFlows("")
+	err = assertNoOVSChanges(eip, &flows)
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
-	}
-	err = assertFlowChanges(origFlows, flows) // no changes
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
+		t.Fatalf("%v", err)
 	}
 
 	// Assign NetNamespace.EgressIP first, then HostSubnet.EgressIP, with a remote EgressIP
@@ -64,18 +147,9 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 42, egress: Dropped})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
-	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=42", "drop"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
+		t.Fatalf("%v", err)
 	}
 
 	ns42 := eip.namespacesByVNID[42]
@@ -88,20 +162,10 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 42, egress: Remote, remote: "172.17.0.3"})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
+		t.Fatalf("%v", err)
 	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=42", "172.17.0.3->tun_dst"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
-	}
-	origFlows = flows
 
 	node3 := eip.nodesByNodeIP["172.17.0.3"]
 	if node3 == nil || eip.namespacesByEgressIP["172.17.0.100"] != ns42 || eip.nodesByEgressIP["172.17.0.100"] != node3 {
@@ -114,13 +178,9 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertNoOVSChanges(eip, &flows)
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
-	}
-	err = assertFlowChanges(origFlows, flows)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
+		t.Fatalf("%v", err)
 	}
 
 	if eip.nodesByEgressIP["172.17.0.100"] != node3 || eip.nodesByEgressIP["172.17.0.101"] != node3 {
@@ -132,20 +192,10 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 43, egress: Remote, remote: "172.17.0.3"})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
+		t.Fatalf("%v", err)
 	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=43", "172.17.0.3->tun_dst"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
-	}
-	origFlows = flows
 
 	ns43 := eip.namespacesByVNID[43]
 	if ns43 == nil || eip.namespacesByEgressIP["172.17.0.101"] != ns43 || eip.nodesByEgressIP["172.17.0.101"] != node3 {
@@ -158,18 +208,9 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 44, egress: Dropped})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
-	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=44", "drop"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
+		t.Fatalf("%v", err)
 	}
 
 	ns44 := eip.namespacesByVNID[44]
@@ -182,20 +223,10 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 44, egress: Local})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
+		t.Fatalf("%v", err)
 	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=44", "0x0000002c->pkt_mark", "goto_table:101"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
-	}
-	origFlows = flows
 
 	node4 := eip.nodesByNodeIP["172.17.0.4"]
 	if node4 == nil || eip.nodesByEgressIP["172.17.0.102"] != node4 {
@@ -208,13 +239,9 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertNoOVSChanges(eip, &flows)
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
-	}
-	err = assertFlowChanges(origFlows, flows)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
+		t.Fatalf("%v", err)
 	}
 
 	if eip.nodesByEgressIP["172.17.0.102"] != node4 || eip.nodesByEgressIP["172.17.0.103"] != node4 {
@@ -226,20 +253,10 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 45, egress: Local})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
+		t.Fatalf("%v", err)
 	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=45", "0x0100002c->pkt_mark", "goto_table:101"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
-	}
-	origFlows = flows
 
 	ns45 := eip.namespacesByVNID[45]
 	if ns45 == nil || eip.namespacesByEgressIP["172.17.0.103"] != ns45 || eip.nodesByEgressIP["172.17.0.103"] != node4 {
@@ -252,20 +269,10 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 44, egress: Normal})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
+		t.Fatalf("%v", err)
 	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowRemoved,
-			match: []string{"table=100", "reg0=44", "0x0000002c->pkt_mark", "goto_table:101"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
-	}
-	origFlows = flows
 
 	if eip.namespacesByVNID[44] != nil || eip.namespacesByEgressIP["172.17.0.102"] != nil || eip.nodesByEgressIP["172.17.0.102"] != node4 {
 		t.Fatalf("Unexpected eip state: %#v", eip)
@@ -277,20 +284,10 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 44, egress: Local})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
+		t.Fatalf("%v", err)
 	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=44", "0x0000002c->pkt_mark", "goto_table:101"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
-	}
-	origFlows = flows
 
 	ns44 = eip.namespacesByVNID[44]
 	if ns44 == nil || eip.namespacesByEgressIP["172.17.0.102"] != ns44 || eip.nodesByEgressIP["172.17.0.102"] != node4 {
@@ -303,24 +300,10 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 43, egress: Dropped})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
+		t.Fatalf("%v", err)
 	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowRemoved,
-			match: []string{"table=100", "reg0=43", "172.17.0.3->tun_dst"},
-		},
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=43", "drop"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
-	}
-	origFlows = flows
 
 	if eip.namespacesByVNID[43] != ns43 || eip.namespacesByEgressIP["172.17.0.101"] != ns43 || eip.nodesByEgressIP["172.17.0.101"] != nil {
 		t.Fatalf("Unexpected eip state: %#v", eip)
@@ -332,27 +315,9 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertOVSChanges(eip, &flows, egressOVSChange{vnid: 45, egress: Dropped})
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
-	}
-	err = assertFlowChanges(origFlows, flows,
-		flowChange{
-			kind:  flowRemoved,
-			match: []string{"table=100", "reg0=45", "0x0100002c->pkt_mark", "goto_table:101"},
-		},
-		flowChange{
-			kind:  flowAdded,
-			match: []string{"table=100", "reg0=45", "drop"},
-		},
-	)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
-	}
-	origFlows = flows
-
-	if eip.namespacesByVNID[45] != ns45 || eip.namespacesByEgressIP["172.17.0.103"] != ns45 || eip.nodesByEgressIP["172.17.0.103"] != nil {
-		t.Fatalf("Unexpected eip state: %#v", eip)
+		t.Fatalf("%v", err)
 	}
 
 	// Trying to assign node IP as egress IP should fail. (It will log an error but this test doesn't notice that.)
@@ -361,13 +326,9 @@ func TestEgressIP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	flows, err = ovsif.DumpFlows("")
+	err = assertNoOVSChanges(eip, &flows)
 	if err != nil {
-		t.Fatalf("Unexpected error dumping flows: %v", err)
-	}
-	err = assertFlowChanges(origFlows, flows)
-	if err != nil {
-		t.Fatalf("Unexpected flow changes: %v\nOrig: %#v\nNew: %#v", err, origFlows, flows)
+		t.Fatalf("%v", err)
 	}
 }
 
