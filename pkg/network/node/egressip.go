c8542165c678feae22a49f4bfb0f5e7313d86071
Dan Winship <danw@redhat.com>
2018-03-01 12:00:27 -0500

    Have multiple egress IP ovscontroller methods rather than one confusing one

--- pkg/network/node/egressip.go
+++ pkg/network/node/egressip.go
@@ -194,7 +194,7 @@ func (eip *egressIPWatcher) maybeAddEgressIP(egressIP string) {
 		ns.assignedIP = egressIP
 		ns.nodeIP = nodeIP
 
-		err := eip.oc.UpdateNamespaceEgressRules(ns.vnid, ns.nodeIP, mark)
+		err := eip.oc.SetNamespaceEgressViaEgressIP(ns.vnid, ns.nodeIP, mark)
 		if err != nil {
 			utilruntime.HandleError(fmt.Errorf("Error updating Namespace egress rules: %v", err))
 		}
@@ -224,10 +224,10 @@ func (eip *egressIPWatcher) deleteEgressIP(egressIP string) {
 	var err error
 	if ns.requestedIP == "" {
 		// Namespace no longer wants EgressIP
-		err = eip.oc.UpdateNamespaceEgressRules(ns.vnid, "", "")
+		err = eip.oc.SetNamespaceEgressNormal(ns.vnid)
 	} else {
 		// Namespace still wants EgressIP but no node provides it
-		err = eip.oc.UpdateNamespaceEgressRules(ns.vnid, "", mark)
+		err = eip.oc.SetNamespaceEgressDropped(ns.vnid)
 	}
 	if err != nil {
 		utilruntime.HandleError(fmt.Errorf("Error updating Namespace egress rules: %v", err))
