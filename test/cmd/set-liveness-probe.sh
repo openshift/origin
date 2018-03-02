6f680065a523555e5903cc609615427bc6aa3557
Maciej Szulik <maszulik@redhat.com>
2018-03-02 13:35:33 +0100

    Expose --v in oc

--- test/cmd/set-liveness-probe.sh
+++ test/cmd/set-liveness-probe.sh
@@ -13,7 +13,7 @@ trap os::test::junit::reconcile_output EXIT
 os::test::junit::declare_suite_start "cmd/set-probe-liveness"
 # This test setting a liveness probe, without warning about replication controllers whose deployment depends on deployment configs
 os::cmd::expect_success_and_text 'oc create -f pkg/oc/graph/genericgraph/test/simple-deployment.yaml' 'deploymentconfig "simple-deployment" created'
-os::cmd::expect_success_and_text 'oc status -v' 'dc/simple-deployment has no liveness probe'
+os::cmd::expect_success_and_text 'oc status --suggest' 'dc/simple-deployment has no liveness probe'
 
 # test --local flag
 os::cmd::expect_failure_and_text 'oc set probe dc/simple-deployment --liveness --get-url=http://google.com:80 --local' 'You must provide one or more resources by argument or filename'
@@ -23,8 +23,8 @@ os::cmd::expect_success_and_text 'oc set probe dc/simple-deployment --liveness -
 # ensure backwards compatibility with -o formats acting as --dry-run (e.g. all commands after this one succeed if specifying -o without --dry-run does not mutate resources in server)
 os::cmd::expect_success_and_text 'oc set probe dc/simple-deployment --liveness --get-url=http://google.com:80 -o yaml' 'name: simple-deployment'
 
-os::cmd::expect_success_and_not_text 'oc status -v' 'rc/simple-deployment-1 has no liveness probe'
+os::cmd::expect_success_and_not_text 'oc status --suggest' 'rc/simple-deployment-1 has no liveness probe'
 os::cmd::expect_success_and_text 'oc set probe dc/simple-deployment --liveness --get-url=http://google.com:80' 'deploymentconfig "simple-deployment" updated'
-os::cmd::expect_success_and_not_text 'oc status -v' 'dc/simple-deployment has no liveness probe'
+os::cmd::expect_success_and_not_text 'oc status --suggest' 'dc/simple-deployment has no liveness probe'
 echo "set-probe-liveness: ok"
 os::test::junit::declare_suite_end
