c1a0e894e273c9c5d3d3de986873fc260b8307c4
Maciej Szulik <maszulik@redhat.com>
2018-03-02 13:35:33 +0100

    Expose --v in oc

--- test/cmd/services.sh
+++ test/cmd/services.sh
@@ -17,6 +17,7 @@ os::cmd::expect_failure_and_text 'oc create service nodeport mynodeport --tcp=80
 os::cmd::expect_failure_and_text 'oc create service nodeport mynodeport --tcp=8080:7777 --node-port=300' 'provided port is not in the valid range. The range of valid ports is 30000-32767'
 os::cmd::expect_success_and_text 'oc describe service mynodeport' 'NodePort\:.*30000'
 os::cmd::expect_success_and_text 'oc describe service mynodeport' 'NodePort\:.*8080-7777'
+os::cmd::expect_success_and_text 'oc describe --v=8 service mynodeport' 'Response Body'
 
 echo "create-services-nodeport: ok"
 os::test::junit::declare_suite_end
