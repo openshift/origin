95c4a1ad50c45bb1e65590ad5a68792448f63e6f
Vaidas Kascėnas <vaidas@kascenas.lt>
2018-03-30 20:33:39 +0300

    Var mistake

--- hack/lib/init.sh
+++ hack/lib/init.sh
@@ -36,7 +36,7 @@ export OS_ROOT
 cd "${OS_ROOT}"
 
 while IFS= read -r -d '' library_file; do
-	source $file
+	source "$library_file"
 done < <(find "${OS_ROOT}/hack/lib" -type f -name '*.sh' -not -path '*/hack/lib/init.sh')
 
 unset library_files library_file init_source
