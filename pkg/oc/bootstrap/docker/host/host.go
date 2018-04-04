5cd2b78b79153ff85b4a623493d52e0cee68282e
David Eads <deads@redhat.com>
2018-04-04 08:08:42 -0400

    remove oc cluster up experimental flags broken since 3.6.1

--- pkg/oc/bootstrap/docker/host/host.go
+++ pkg/oc/bootstrap/docker/host/host.go
@@ -2,10 +2,6 @@ package host
 
 import (
 	"fmt"
-	"strings"
-
-	"github.com/docker/docker/api/types"
-	"github.com/golang/glog"
 
 	"github.com/openshift/origin/pkg/oc/bootstrap/docker/dockerhelper"
 	"github.com/openshift/origin/pkg/oc/bootstrap/docker/errors"
@@ -26,25 +22,19 @@ grep -F %[1]s /rootfs/proc/1/mountinfo | grep shared || nsenter --mount=/rootfs/
 // HostHelper contains methods to help check settings on a Docker host machine
 // using a privileged container
 type HostHelper struct {
-	client               dockerhelper.Interface
-	runHelper            *run.RunHelper
-	image                string
-	volumesDir           string
-	configDir            string
-	dataDir              string
-	persistentVolumesDir string
+	client     dockerhelper.Interface
+	runHelper  *run.RunHelper
+	image      string
+	volumesDir string
 }
 
 // NewHostHelper creates a new HostHelper
-func NewHostHelper(dockerHelper *dockerhelper.Helper, image, volumesDir, configDir, dataDir, pvDir string) *HostHelper {
+func NewHostHelper(dockerHelper *dockerhelper.Helper, image, volumesDir string) *HostHelper {
 	return &HostHelper{
-		runHelper:            run.NewRunHelper(dockerHelper),
-		client:               dockerHelper.Client(),
-		image:                image,
-		volumesDir:           volumesDir,
-		configDir:            configDir,
-		dataDir:              dataDir,
-		persistentVolumesDir: pvDir,
+		runHelper:  run.NewRunHelper(dockerHelper),
+		client:     dockerHelper.Client(),
+		image:      image,
+		volumesDir: volumesDir,
 	}
 }
 
@@ -76,48 +66,6 @@ func (h *HostHelper) EnsureVolumeUseShareMount() error {
 		return errors.NewError("cannot create volume share").WithCause(err)
 	}
 	return nil
-}
-
-func (h *HostHelper) defaultBinds() []string {
-	return []string{fmt.Sprintf("%s:/var/lib/origin/openshift.local.config:z", h.configDir)}
-}
-
-// UploadFileToContainer copies a local file to the Docker host
-func (h *HostHelper) UploadFileToContainer(src, dst string) error {
-	container, err := h.runner().
-		Image(h.image).
-		Bind(h.defaultBinds()...).
-		Entrypoint("/bin/true").
-		Create()
-	if err != nil {
-		return err
-	}
-	defer func() {
-		errors.LogError(h.client.ContainerRemove(container, types.ContainerRemoveOptions{}))
-	}()
-	err = dockerhelper.UploadFileToContainer(h.client, container, src, dst)
-	if err != nil {
-		glog.V(4).Infof("An error occurred uploading the file: %v", err)
-	} else {
-		glog.V(4).Infof("Successfully uploaded file.")
-	}
-	return err
-}
-
-// Hostname retrieves the FQDN of the Docker host machine
-func (h *HostHelper) Hostname() (string, error) {
-	_, hostname, _, _, err := h.runner().
-		Image(h.image).
-		HostNetwork().
-		HostPid().
-		DiscardContainer().
-		Privileged().
-		Entrypoint("/bin/bash").
-		Command("-c", "uname -n").Output()
-	if err != nil {
-		return "", err
-	}
-	return strings.ToLower(strings.TrimSpace(hostname)), nil
 }
 
 func (h *HostHelper) runner() *run.Runner {
