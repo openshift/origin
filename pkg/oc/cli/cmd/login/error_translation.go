02e20aaf39c37d6aac97cf1217983d77b41a6679
David Eads <deads@redhat.com>
2018-04-12 14:44:02 -0400

    remove dead code from login pretty message making

--- pkg/oc/cli/cmd/login/error_translation.go
+++ pkg/oc/cli/cmd/login/error_translation.go
@@ -1,13 +1,10 @@
-package clientcmd
+package login
 
 import (
 	"crypto/x509"
 	"errors"
 	"fmt"
 	"strings"
-
-	kerrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 const (
@@ -16,7 +13,6 @@ const (
 	certificateAuthorityUnknownReason
 	certificateHostnameErrorReason
 	certificateInvalidReason
-	configurationInvalidReason
 	tlsOversizedRecordReason
 
 	certificateAuthorityUnknownMsg = "The server uses a certificate signed by unknown authority. You may need to use the --certificate-authority flag to provide the path to a certificate file for the certificate authority, or --insecure-skip-tls-verify to bypass the certificate check and use insecure connections."
@@ -28,13 +24,8 @@ You can also run this command again providing the path to a config file directly
 Ensure the specified server supports HTTPS.`
 )
 
-// GetPrettyMessageFor prettifys the message of the provided error
-func GetPrettyMessageFor(err error) string {
-	return GetPrettyMessageForServer(err, "")
-}
-
 // GetPrettyMessageForServer prettifys the message of the provided error
-func GetPrettyMessageForServer(err error, serverName string) string {
+func getPrettyMessageForServer(err error, serverName string) string {
 	if err == nil {
 		return ""
 	}
@@ -64,51 +55,9 @@ func GetPrettyMessageForServer(err error, serverName string) string {
 	return err.Error()
 }
 
-// GetPrettyErrorFor prettifys the message of the provided error
-func GetPrettyErrorFor(err error) error {
-	return GetPrettyErrorForServer(err, "")
-}
-
 // GetPrettyErrorForServer prettifys the message of the provided error
-func GetPrettyErrorForServer(err error, serverName string) error {
-	return errors.New(GetPrettyMessageForServer(err, serverName))
-}
-
-// IsNoServerFound checks whether the provided error is a 'no server found' error or not
-func IsNoServerFound(err error) bool {
-	return detectReason(err) == noServerFoundReason
-}
-
-// IsConfigurationInvalid checks whether the provided error is a 'invalid configuration' error or not
-func IsConfigurationInvalid(err error) bool {
-	return detectReason(err) == configurationInvalidReason
-}
-
-// IsCertificateAuthorityUnknown checks whether the provided error is a 'certificate authority unknown' error or not
-func IsCertificateAuthorityUnknown(err error) bool {
-	return detectReason(err) == certificateAuthorityUnknownReason
-}
-
-// IsForbidden checks whether the provided error is a 'forbidden' error or not
-func IsForbidden(err error) bool {
-	return kerrors.IsForbidden(err)
-}
-
-// IsTLSOversizedRecord checks whether the provided error is a url.Error
-// with "tls: oversized record received", which usually means TLS not supported.
-func IsTLSOversizedRecord(err error) bool {
-	return detectReason(err) == tlsOversizedRecordReason
-}
-
-// IsCertificateHostnameError checks whether the set of authorized names doesn't match the requested name
-func IsCertificateHostnameError(err error) bool {
-	return detectReason(err) == certificateHostnameErrorReason
-}
-
-// IsCertificateInvalid checks whether the certificate is invalid for reasons like expired,	CA not authorized
-// to sign, there are too many cert intermediates, or the cert usage is not valid for the wanted purpose.
-func IsCertificateInvalid(err error) bool {
-	return detectReason(err) == certificateInvalidReason
+func getPrettyErrorForServer(err error, serverName string) error {
+	return errors.New(getPrettyMessageForServer(err, serverName))
 }
 
 func detectReason(err error) int {
@@ -118,8 +67,6 @@ func detectReason(err error) int {
 			return certificateAuthorityUnknownReason
 		case strings.Contains(err.Error(), "no server defined"):
 			return noServerFoundReason
-		case clientcmd.IsConfigurationInvalid(err):
-			return configurationInvalidReason
 		case strings.Contains(err.Error(), "tls: oversized record received"):
 			return tlsOversizedRecordReason
 		}
