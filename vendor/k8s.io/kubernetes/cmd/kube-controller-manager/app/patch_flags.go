vrutkovs/kubernetes

UPSTREAM: <carry>: filter daemonset nodes by namespace node selectors
Origin-commit: f74ad818d0959db680dd660f1b4050ea748da9f4

cmd/kube-controller-manager/app/patch_flags.go
@@ -32,6 +32,42 @@ func getOpenShiftConfig(configFile string) (map[string]interface{}, error) {
 }
 
 func applyOpenShiftConfigFlags(controllerManagerOptions *options.KubeControllerManagerOptions, controllerManager *config.Config, openshiftConfig map[string]interface{}) error {
+	if err := applyOpenShiftConfigControllerArgs(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	if err := applyOpenShiftConfigDefaultProjectSelector(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	if err := applyOpenShiftConfigKubeDefaultProjectSelector(controllerManagerOptions, openshiftConfig); err != nil {
+		return err
+	}
+	return controllerManagerOptions.ApplyTo(controllerManager)
+}
+
+func applyOpenShiftConfigDefaultProjectSelector(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
+	projectConfig, ok := openshiftConfig["projectConfig"]
+	if !ok {
+		return nil
+	}
+
+	castProjectConfig := projectConfig.(map[string]interface{})
+	defaultNodeSelector, ok := castProjectConfig["defaultNodeSelector"]
+	if !ok {
+		return nil
+	}
+	controllerManagerOptions.OpenShiftContext.OpenShiftDefaultProjectNodeSelector = defaultNodeSelector.(string)
+
+	return nil
+}
+
+// this is an optimization.  It can be filled in later.  Looks like there are several special cases for this plugin upstream
+// TODO find this
+func applyOpenShiftConfigKubeDefaultProjectSelector(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
+	controllerManagerOptions.OpenShiftContext.KubeDefaultProjectNodeSelector = ""
+	return nil
+}
+
+func applyOpenShiftConfigControllerArgs(controllerManagerOptions *options.KubeControllerManagerOptions, openshiftConfig map[string]interface{}) error {
 	kubeMasterConfig, ok := openshiftConfig["kubernetesMasterConfig"]
 	if !ok {
 		return nil
@@ -52,7 +88,7 @@ func applyOpenShiftConfigFlags(controllerManagerOptions *options.KubeControllerM
 	if err := resolveFlags(args, kubeControllerManagerAddFlags(controllerManagerOptions)); len(err) > 0 {
 		return kerrors.NewAggregate(err)
 	}
-	return controllerManagerOptions.ApplyTo(controllerManager)
+	return nil
 }
 
 // applyFlags stores the provided arguments onto a flag set, reporting any errors

pkg/controller/daemon/patch_nodeselector.go
@@ -1,0 +1,90 @@
+package daemon
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	appsinformers "k8s.io/client-go/informers/apps/v1"
+	coreinformers "k8s.io/client-go/informers/core/v1"
+	clientset "k8s.io/client-go/kubernetes"
+)
+
+func NewNodeSelectorAwareDaemonSetsController(openshiftDefaultNodeSelectorString, kubeDefaultNodeSelectorString string, namepaceInformer coreinformers.NamespaceInformer, daemonSetInformer appsinformers.DaemonSetInformer, historyInformer appsinformers.ControllerRevisionInformer, podInformer coreinformers.PodInformer, nodeInformer coreinformers.NodeInformer, kubeClient clientset.Interface) (*DaemonSetsController, error) {
+	controller, err := NewDaemonSetsController(daemonSetInformer, historyInformer, podInformer, nodeInformer, kubeClient)
+	if err != nil {
+		return controller, err
+	}
+	controller.namespaceLister = namepaceInformer.Lister()
+	controller.namespaceStoreSynced = namepaceInformer.Informer().HasSynced
+	controller.openshiftDefaultNodeSelectorString = openshiftDefaultNodeSelectorString
+	if len(controller.openshiftDefaultNodeSelectorString) > 0 {
+		controller.openshiftDefaultNodeSelector, err = labels.Parse(controller.openshiftDefaultNodeSelectorString)
+		if err != nil {
+			return nil, err
+		}
+	}
+	controller.kubeDefaultNodeSelectorString = kubeDefaultNodeSelectorString
+	if len(controller.kubeDefaultNodeSelectorString) > 0 {
+		controller.kubeDefaultNodeSelector, err = labels.Parse(controller.kubeDefaultNodeSelectorString)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return controller, nil
+}
+
+func (dsc *DaemonSetsController) namespaceNodeSelectorMatches(node *v1.Node, ds *appsv1.DaemonSet) (bool, error) {
+	if dsc.namespaceLister == nil {
+		return true, nil
+	}
+
+	// this is racy (different listers) and we get to choose which way to fail.  This should requeue.
+	ns, err := dsc.namespaceLister.Get(ds.Namespace)
+	if apierrors.IsNotFound(err) {
+		return false, err
+	}
+	// if we had any error, default to the safe option of creating a pod for the node.
+	if err != nil {
+		utilruntime.HandleError(err)
+		return true, nil
+	}
+
+	return dsc.nodeSelectorMatches(node, ns), nil
+}
+
+func (dsc *DaemonSetsController) nodeSelectorMatches(node *v1.Node, ns *v1.Namespace) bool {
+	originNodeSelector, ok := ns.Annotations["openshift.io/node-selector"]
+	switch {
+	case ok:
+		selector, err := labels.Parse(originNodeSelector)
+		if err == nil {
+			if !selector.Matches(labels.Set(node.Labels)) {
+				return false
+			}
+		}
+	case !ok && len(dsc.openshiftDefaultNodeSelectorString) > 0:
+		if !dsc.openshiftDefaultNodeSelector.Matches(labels.Set(node.Labels)) {
+			return false
+		}
+	}
+
+	kubeNodeSelector, ok := ns.Annotations["scheduler.alpha.kubernetes.io/node-selector"]
+	switch {
+	case ok:
+		selector, err := labels.Parse(kubeNodeSelector)
+		if err == nil {
+			if !selector.Matches(labels.Set(node.Labels)) {
+				return false
+			}
+		}
+	case !ok && len(dsc.kubeDefaultNodeSelectorString) > 0:
+		if !dsc.kubeDefaultNodeSelector.Matches(labels.Set(node.Labels)) {
+			return false
+		}
+	}
+
+	return true
+}

openshift/origin

vendor/k8s.io/kubernetes/cmd/kube-controller-manager/app/patch_flags.go
@@ -31,6 +31,42 @@ func getOpenShiftConfig(configFile string) (map[string]interface{}, error) {
 }
 
 func applyOpenShiftConfigFlags(controllerManager *options.CMServer, openshiftConfig map[string]interface{}) error {
+	if err := applyOpenShiftConfigControllerArgs(controllerManager, openshiftConfig); err != nil {
+		return err
+	}
+	if err := applyOpenShiftConfigDefaultProjectSelector(controllerManager, openshiftConfig); err != nil {
+		return err
+	}
+	if err := applyOpenShiftConfigKubeDefaultProjectSelector(controllerManager, openshiftConfig); err != nil {
+		return err
+	}
+	return nil
+}
+
+func applyOpenShiftConfigDefaultProjectSelector(controllerManager *options.CMServer, openshiftConfig map[string]interface{}) error {
+	projectConfig, ok := openshiftConfig["projectConfig"]
+	if !ok {
+		return nil
+	}
+
+	castProjectConfig := projectConfig.(map[string]interface{})
+	defaultNodeSelector, ok := castProjectConfig["defaultNodeSelector"]
+	if !ok {
+		return nil
+	}
+	controllerManager.OpenShiftDefaultProjectNodeSelector = defaultNodeSelector.(string)
+
+	return nil
+}
+
+// this is an optimization.  It can be filled in later.  Looks like there are several special cases for this plugin upstream
+// TODO find this
+func applyOpenShiftConfigKubeDefaultProjectSelector(controllerManager *options.CMServer, openshiftConfig map[string]interface{}) error {
+	controllerManager.KubeDefaultProjectNodeSelector = ""
+	return nil
+}
+
+func applyOpenShiftConfigControllerArgs(controllerManager *options.CMServer, openshiftConfig map[string]interface{}) error {
 	kubeMasterConfig, ok := openshiftConfig["kubernetesMasterConfig"]
 	if !ok {
 		return nil

vendor/k8s.io/kubernetes/pkg/controller/daemon/patch_nodeselector.go
@@ -1,0 +1,91 @@
+package daemon
+
+import (
+	"k8s.io/api/core/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	appsinformers "k8s.io/client-go/informers/apps/v1beta1"
+	coreinformers "k8s.io/client-go/informers/core/v1"
+	extensionsinformers "k8s.io/client-go/informers/extensions/v1beta1"
+	clientset "k8s.io/client-go/kubernetes"
+)
+
+func NewNodeSelectorAwareDaemonSetsController(openshiftDefaultNodeSelectorString, kubeDefaultNodeSelectorString string, namepaceInformer coreinformers.NamespaceInformer, daemonSetInformer extensionsinformers.DaemonSetInformer, historyInformer appsinformers.ControllerRevisionInformer, podInformer coreinformers.PodInformer, nodeInformer coreinformers.NodeInformer, kubeClient clientset.Interface) (*DaemonSetsController, error) {
+	controller, err := NewDaemonSetsController(daemonSetInformer, historyInformer, podInformer, nodeInformer, kubeClient)
+	if err != nil {
+		return controller, err
+	}
+	controller.namespaceLister = namepaceInformer.Lister()
+	controller.namespaceStoreSynced = namepaceInformer.Informer().HasSynced
+	controller.openshiftDefaultNodeSelectorString = openshiftDefaultNodeSelectorString
+	if len(controller.openshiftDefaultNodeSelectorString) > 0 {
+		controller.openshiftDefaultNodeSelector, err = labels.Parse(controller.openshiftDefaultNodeSelectorString)
+		if err != nil {
+			return nil, err
+		}
+	}
+	controller.kubeDefaultNodeSelectorString = kubeDefaultNodeSelectorString
+	if len(controller.kubeDefaultNodeSelectorString) > 0 {
+		controller.kubeDefaultNodeSelector, err = labels.Parse(controller.kubeDefaultNodeSelectorString)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return controller, nil
+}
+
+func (dsc *DaemonSetsController) namespaceNodeSelectorMatches(node *v1.Node, ds *extensions.DaemonSet) (bool, error) {
+	if dsc.namespaceLister == nil {
+		return true, nil
+	}
+
+	// this is racy (different listers) and we get to choose which way to fail.  This should requeue.
+	ns, err := dsc.namespaceLister.Get(ds.Namespace)
+	if apierrors.IsNotFound(err) {
+		return false, err
+	}
+	// if we had any error, default to the safe option of creating a pod for the node.
+	if err != nil {
+		utilruntime.HandleError(err)
+		return true, nil
+	}
+
+	return dsc.nodeSelectorMatches(node, ns), nil
+}
+
+func (dsc *DaemonSetsController) nodeSelectorMatches(node *v1.Node, ns *v1.Namespace) bool {
+	originNodeSelector, ok := ns.Annotations["openshift.io/node-selector"]
+	switch {
+	case ok:
+		selector, err := labels.Parse(originNodeSelector)
+		if err == nil {
+			if !selector.Matches(labels.Set(node.Labels)) {
+				return false
+			}
+		}
+	case !ok && len(dsc.openshiftDefaultNodeSelectorString) > 0:
+		if !dsc.openshiftDefaultNodeSelector.Matches(labels.Set(node.Labels)) {
+			return false
+		}
+	}
+
+	kubeNodeSelector, ok := ns.Annotations["scheduler.alpha.kubernetes.io/node-selector"]
+	switch {
+	case ok:
+		selector, err := labels.Parse(kubeNodeSelector)
+		if err == nil {
+			if !selector.Matches(labels.Set(node.Labels)) {
+				return false
+			}
+		}
+	case !ok && len(dsc.kubeDefaultNodeSelectorString) > 0:
+		if !dsc.kubeDefaultNodeSelector.Matches(labels.Set(node.Labels)) {
+			return false
+		}
+	}
+
+	return true
+}