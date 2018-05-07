b4bf264a81b9504b1cd1271bb4a6a5fce0d9ecab
Jordan Liggitt <jliggitt@redhat.com>
2018-05-07 12:16:46 -0400

    UPSTREAM: 63832: Close all kubelet->API connections on heartbeat failure

--- vendor/k8s.io/kubernetes/cmd/kubelet/app/server.go
+++ vendor/k8s.io/kubernetes/cmd/kubelet/app/server.go
@@ -385,6 +385,7 @@ func run(s *options.KubeletServer, kubeDeps *kubelet.Dependencies) (err error) {
 		var kubeClient clientset.Interface
 		var eventClient v1core.EventsGetter
 		var heartbeatClient v1core.CoreV1Interface
+		var closeAllConns func()
 		var externalKubeClient clientset.Interface
 
 		clientConfig, err := CreateAPIServerClientConfig(s)
@@ -399,7 +400,7 @@ func run(s *options.KubeletServer, kubeDeps *kubelet.Dependencies) (err error) {
 			}
 			// we set exitIfExpired to true because we use this client configuration to request new certs - if we are unable
 			// to request new certs, we will be unable to continue normal operation
-			_, err := kubeletcertificate.UpdateTransport(wait.NeverStop, clientConfig, clientCertificateManager, true)
+			closeAllConns, err = kubeletcertificate.UpdateTransport(wait.NeverStop, clientConfig, clientCertificateManager, true)
 			if err != nil {
 				return err
 			}
@@ -447,6 +448,7 @@ func run(s *options.KubeletServer, kubeDeps *kubelet.Dependencies) (err error) {
 		kubeDeps.ExternalKubeClient = externalKubeClient
 		if heartbeatClient != nil {
 			kubeDeps.HeartbeatClient = heartbeatClient
+			kubeDeps.OnHeartbeatFailure = closeAllConns
 		}
 		if eventClient != nil {
 			kubeDeps.EventClient = eventClient
