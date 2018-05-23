bb0262b7100593f137de1a071cadb9e314292793
Ben Parees <bparees@redhat.com>
2018-05-23 13:59:20 -0400

    add conformance tag to fast parallel registry tests

--- test/extended/images/hardprune.go
+++ test/extended/images/hardprune.go
@@ -22,7 +22,7 @@ import (
 	testutil "github.com/openshift/origin/test/util"
 )
 
-var _ = g.Describe("[Feature:ImagePrune][registry][Serial] Image hard prune", func() {
+var _ = g.Describe("[Feature:ImagePrune][registry][Serial][Suite:openshift/registry/serial] Image hard prune", func() {
 	defer g.GinkgoRecover()
 	var oc = exutil.NewCLI("prune-images", exutil.KubeConfigPath())
 	var originalAcceptSchema2 *bool
