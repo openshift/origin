0c5937556a6bda3eff7d82ea8c753aa20bbae081
Maciej Szulik <maszulik@redhat.com>
2018-06-08 11:43:50 +0200

    Update example

--- pkg/oc/cli/cmd/example.go
+++ pkg/oc/cli/cmd/example.go
@@ -2,13 +2,15 @@ package cmd
 
 import (
 	"fmt"
-	"io"
-	"os"
 
 	"github.com/spf13/cobra"
 
 	"k8s.io/kubernetes/pkg/kubectl/cmd/templates"
 	kcmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	"k8s.io/kubernetes/pkg/kubectl/genericclioptions"
+	"k8s.io/kubernetes/pkg/kubectl/genericclioptions/printers"
+
+	"github.com/openshift/origin/pkg/oc/util/ocscheme"
 )
 
 var (
@@ -21,39 +23,56 @@ var (
 )
 
 type TYPEOptions struct {
-	In          io.Reader
-	Out, ErrOut io.Writer
+	PrintFlags *genericclioptions.PrintFlags
+
+	Printer printers.ResourcePrinter
+	DryRun  bool
+
+	genericclioptions.IOStreams
+}
+
+// NewTYPEOptions returns a TYPEOptions with proper defaults.
+// This is an example type for templating.
+func NewTYPEOptions(streams genericclioptions.IOStreams) *TYPEOptions {
+	return &TYPEOptions{
+		PrintFlags: genericclioptions.NewPrintFlags("action performed").WithTypeSetter(ocscheme.PrintingInternalScheme),
+		IOStreams:  streams,
+	}
 }
 
 // NewCmdTYPE implements a TYPE command
 // This is an example type for templating.
-func NewCmdTYPE(fullName string, f kcmdutil.Factory, in io.Reader, out, errout io.Writer) *cobra.Command {
-	options := &TYPEOptions{
-		In:     in,
-		Out:    out,
-		ErrOut: errout,
-	}
+func NewCmdTYPE(fullName string, f kcmdutil.Factory, streams genericclioptions.IOStreams) *cobra.Command {
+	o := NewTYPEOptions(streams)
 	cmd := &cobra.Command{
 		Use:     "NAME [...]",
 		Short:   "A short description",
 		Long:    internalTYPELong,
 		Example: fmt.Sprintf(internalTYPEExample, fullName),
 		Run: func(cmd *cobra.Command, args []string) {
-			kcmdutil.CheckErr(options.Complete(f, cmd, args))
-			kcmdutil.CheckErr(options.Validate())
-			if err := options.Run(); err != nil {
-				// TODO: move me to kcmdutil
-				if err == kcmdutil.ErrExit {
-					os.Exit(1)
-				}
-				kcmdutil.CheckErr(err)
-			}
+			kcmdutil.CheckErr(o.Complete(f, cmd, args))
+			kcmdutil.CheckErr(o.Validate())
+			kcmdutil.CheckErr(o.Run())
 		},
 	}
+
+	o.PrintFlags.AddFlags(cmd)
+	kcmdutil.AddDryRunFlag(cmd)
+
 	return cmd
 }
 
-func (o *TYPEOptions) Complete(f kcmdutil.Factory, c *cobra.Command, args []string) error {
+func (o *TYPEOptions) Complete(f kcmdutil.Factory, cmd *cobra.Command, args []string) error {
+	var err error
+	o.DryRun = kcmdutil.GetDryRunFlag(cmd)
+	if o.DryRun {
+		o.PrintFlags.Complete("%s (dry run)")
+	}
+	o.Printer, err = o.PrintFlags.ToPrinter()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
