8da3e772737532edda269b04596a9ff6e8187af6
Maciej Szulik <maszulik@redhat.com>
2018-06-11 17:31:35 +0200

    Update set env

--- pkg/oc/cli/cmd/set/set.go
+++ pkg/oc/cli/cmd/set/set.go
@@ -35,6 +35,7 @@ func NewCmdSet(fullName string, f *clientcmd.Factory, streams genericclioptions.
 		{
 			Message: "Replication controllers, deployments, and daemon sets:",
 			Commands: []*cobra.Command{
+				// FIXME-REBASE: replace with upstream version since it's cleaner and it has full parity
 				NewCmdEnv(name, f, streams),
 				NewCmdResources(name, f, streams),
 				NewCmdVolume(name, f, streams),
