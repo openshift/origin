6359a955909ce54b1d00a54757f68bffc6a81e71
juanvallejo <jvallejo@redhat.com>
2018-06-18 12:58:10 -0400

    UPSTREAM: 65189: fix paths w shortcuts when copying from pods

--- vendor/k8s.io/kubernetes/pkg/kubectl/cmd/cp.go
+++ vendor/k8s.io/kubernetes/pkg/kubectl/cmd/cp.go
@@ -244,6 +244,7 @@ func copyFromPod(f cmdutil.Factory, cmd *cobra.Command, cmderr io.Writer, src, d
 // stripPathShortcuts removes any leading or trailing "../" from a given path
 func stripPathShortcuts(p string) string {
 	newPath := path.Clean(p)
+	newPath = strings.TrimLeft(p, "../")
 	if len(newPath) > 0 && string(newPath[0]) == "/" {
 		return newPath[1:]
 	}
@@ -398,8 +399,24 @@ func untarAll(reader io.Reader, destFile, prefix string) error {
 }
 
 func getPrefix(file string) string {
+	segs := strings.Split(file, "/")
+	prefix := file
+
+	lastIdx := -1
+	for i := len(segs) - 1; i > 0; i-- {
+		if len(segs[i]) == 0 {
+			continue
+		}
+		lastIdx = i
+		break
+	}
+
+	if lastIdx >= 0 {
+		prefix = strings.Join(segs[:lastIdx], "/")
+	}
+
 	// tar strips the leading '/' if it's there, so we will too
-	return strings.TrimLeft(file, "/")
+	return strings.TrimLeft(prefix, "/")
 }
 
 func execute(f cmdutil.Factory, cmd *cobra.Command, options *ExecOptions) error {
