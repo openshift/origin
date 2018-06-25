0a7c9b3ef8d28d8d2bd14bbeeb8f1058de505967
Ben Parees <bparees@redhat.com>
2018-06-25 15:53:49 -0400

    do not merge

--- vendor/github.com/openshift/imagebuilder/dockerclient/conformance_test.go
+++ vendor/github.com/openshift/imagebuilder/dockerclient/conformance_test.go
@@ -60,7 +60,7 @@ func TestMount(t *testing.T) {
 
 	out := &bytes.Buffer{}
 	e.Out, e.ErrOut = out, out
-	e.Directory = tmpDir
+	e.Directory = "."
 	e.Tag = filepath.Base(tmpDir)
 	e.TransientMounts = []Mount{
 		{SourcePath: "testdata/volume/", DestinationPath: "/tmp/test"},
@@ -84,6 +84,69 @@ func TestMount(t *testing.T) {
 
 	if out.String() != expected {
 		t.Errorf("Unexpected build output:\n%s", out.String())
+	}
+}
+
+func TestCopyFrom(t *testing.T) {
+	c, err := docker.NewClientFromEnv()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testCases := []struct {
+		name   string
+		create string
+		copy   string
+		extra  string
+		expect string
+	}{
+		{name: "copy file to root", create: "touch /a /b", copy: "/a /", expect: "[[ -f /a ]]"},
+		{name: "copy file to same file", create: "touch /a", copy: "/a /a", expect: "[[ -f /a ]]"},
+		{name: "copy file to workdir", create: "touch /a", extra: "WORKDIR /b", copy: "/a .", expect: "[[ -f /b/a ]]"},
+		{name: "copy file to workdir rename", create: "touch /a", extra: "WORKDIR /b", copy: "/a ./b", expect: "[[ -f /b/b ]]"},
+		{name: "copy folder contents to higher level", create: "mkdir -p /a/b && touch /a/b/1 /a/b/2", copy: "/a/b/ /b/", expect: "[[ -f /b/1 && -f /b/2 && ! -e /a ]]"},
+		{name: "copy wildcard folder contents to higher level", create: "mkdir -p /a/b && touch /a/b/1 /a/b/2", copy: "/a/b/* /b/", expect: "ls -al /b/1 /b/2 /b && ! ls -al /a /b/a /b/b"},
+		{name: "copy folder with dot contents to higher level", create: "mkdir -p /a/b && touch /a/b/1 /a/b/2", copy: "/a/b/. /b/", expect: "ls -al /b/1 /b/2 /b && ! ls -al /a /b/a /b/b"},
+		{name: "copy root file to different root name", create: "touch /b", copy: "/b /a", expect: "ls -al /a && ! ls -al /b"},
+		{name: "copy nested file to different root name", create: "mkdir -p /a && touch /a/b", copy: "/a/b /a", expect: "ls -al /a && ! ls -al /b"},
+		{name: "copy file to deeper directory with explicit slash", create: "mkdir -p /a && touch /a/1", copy: "/a/1 /a/b/c/", expect: "ls -al /a/b/c/1 && ! ls -al /a/b/1"},
+		{name: "copy file to deeper directory without explicit slash", create: "mkdir -p /a && touch /a/1", copy: "/a/1 /a/b/c", expect: "ls -al /a/b/c && ! ls -al /a/b/1"},
+		{name: "copy directory to deeper directory without explicit slash", create: "mkdir -p /a && touch /a/1", copy: "/a /a/b/c", expect: "ls -al /a/b/c/1 && ! ls -al /a/b/1"},
+	}
+	for i, testCase := range testCases {
+		name := fmt.Sprintf("%d", i)
+		if len(testCase.name) > 0 {
+			name = testCase.name
+		}
+		test := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			e := NewClientExecutor(c)
+			defer e.Release()
+
+			out := &bytes.Buffer{}
+			e.Out, e.ErrOut = out, out
+			b := imagebuilder.NewBuilder(nil)
+			dockerfile := fmt.Sprintf(`
+				FROM busybox AS base
+				RUN %s
+				FROM busybox
+				%s
+				COPY --from=base %s
+				RUN %s
+			`, test.create, test.extra, test.copy, test.expect,
+			)
+			t.Log(dockerfile)
+			node, err := imagebuilder.ParseDockerfile(strings.NewReader(dockerfile))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			stages := imagebuilder.NewStages(node, b)
+			if _, err := e.Stages(b, stages, ""); err != nil {
+				t.Fatal(err)
+			}
+		})
 	}
 }
 
@@ -187,6 +250,16 @@ func TestConformanceInternal(t *testing.T) {
 		{
 			Name:       "directory",
 			ContextDir: "testdata/dir",
+		},
+		{
+			Name:       "directory with slash",
+			ContextDir: "testdata/overlapdir",
+			Dockerfile: "Dockerfile.with_slash",
+		},
+		{
+			Name:       "directory without slash",
+			ContextDir: "testdata/overlapdir",
+			Dockerfile: "Dockerfile.without_slash",
 		},
 		// TODO: Fix this test
 		// {
@@ -423,19 +496,23 @@ func conformanceTester(t *testing.T, c *docker.Client, test conformanceTest, i i
 			}
 		}
 
-	case len(test.Dockerfile) > 0:
+	case len(test.ContextDir) > 0:
+		input = filepath.Join(test.ContextDir, dockerfile)
+		dockerfilePath = filepath.Join(test.ContextDir, "Dockerfile")
+		contextDir = test.ContextDir
+		dir = test.ContextDir
+
+		if len(test.Dockerfile) > 0 {
+			dockerfilePath = filepath.Join(dir, test.Dockerfile)
+		}
+
+	default:
 		input = dockerfile
 		dockerfilePath = filepath.Join(dir, "Dockerfile")
 		if _, err := fileutils.CopyFile(filepath.Join("", dockerfile), dockerfilePath); err != nil {
 			t.Fatal(err)
 		}
 		dockerfile = "Dockerfile"
-
-	default:
-		input = filepath.Join(test.ContextDir, dockerfile)
-		dockerfilePath = input
-		contextDir = test.ContextDir
-		dir = test.ContextDir
 	}
 
 	// read the dockerfile
@@ -718,20 +795,20 @@ func equivalentImages(t *testing.T, c *docker.Client, a, b string, testFilesyste
 			return false
 		}
 		for k, v := range differs {
-			if ignoreFuncs(ignoreFns).Ignore(v[0], v[1]) {
+			if ignoreFuncs(ignoreFns).Ignore(v[0].Header, v[1].Header) {
 				delete(differs, k)
 				continue
 			}
-			t.Errorf("%s %s differs:\n%#v\n%#v", a, k, v[0], v[1])
+			t.Errorf("%s %s differs:\n%#v\n%#v", a, k, v[0].Header, v[1].Header)
 		}
 		for k, v := range onlyA {
-			if ignoreFuncs(ignoreFns).Ignore(v, nil) {
+			if ignoreFuncs(ignoreFns).Ignore(v.Header, nil) {
 				delete(onlyA, k)
 				continue
 			}
 		}
 		for k, v := range onlyB {
-			if ignoreFuncs(ignoreFns).Ignore(nil, v) {
+			if ignoreFuncs(ignoreFns).Ignore(nil, v.Header) {
 				delete(onlyB, k)
 				continue
 			}
@@ -812,7 +889,7 @@ func ignoreDockerfileSize(dockerfile string) ignoreFunc {
 // compareImageFS exports the file systems of two images and returns a map
 // of files that differ in any way (modification time excluded), only exist in
 // image A, or only existing in image B.
-func compareImageFS(c *docker.Client, a, b string) (differ map[string][]*tar.Header, onlyA, onlyB map[string]*tar.Header, err error) {
+func compareImageFS(c *docker.Client, a, b string) (differ map[string][]tarHeader, onlyA, onlyB map[string]tarHeader, err error) {
 	fsA, err := imageFSMetadata(c, a)
 	if err != nil {
 		return nil, nil, nil, err
@@ -821,8 +898,8 @@ func compareImageFS(c *docker.Client, a, b string) (differ map[string][]*tar.Hea
 	if err != nil {
 		return nil, nil, nil, err
 	}
-	differ = make(map[string][]*tar.Header)
-	onlyA = make(map[string]*tar.Header)
+	differ = make(map[string][]tarHeader)
+	onlyA = make(map[string]tarHeader)
 	onlyB = fsB
 	for k, v1 := range fsA {
 		v2, ok := fsB[k]
@@ -835,14 +912,26 @@ func compareImageFS(c *docker.Client, a, b string) (differ map[string][]*tar.Hea
 		v1.ModTime = time.Time{}
 		v2.ModTime = time.Time{}
 		if !reflect.DeepEqual(v1, v2) {
-			differ[k] = []*tar.Header{v1, v2}
+			differ[k] = []tarHeader{v1, v2}
 		}
 	}
 	return differ, onlyA, onlyB, nil
 }
 
+type tarHeader struct {
+	*tar.Header
+}
+
+func (h tarHeader) String() string {
+	th := h.Header
+	if th == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("<%d %s>", th.Size, th.FileInfo().Mode())
+}
+
 // imageFSMetadata creates a container and reads the filesystem metadata out of the archive.
-func imageFSMetadata(c *docker.Client, name string) (map[string]*tar.Header, error) {
+func imageFSMetadata(c *docker.Client, name string) (map[string]tarHeader, error) {
 	container, err := c.CreateContainer(docker.CreateContainerOptions{Name: name + "-export", Config: &docker.Config{Image: name}})
 	if err != nil {
 		return nil, err
@@ -850,7 +939,7 @@ func imageFSMetadata(c *docker.Client, name string) (map[string]*tar.Header, err
 	defer c.RemoveContainer(docker.RemoveContainerOptions{ID: container.ID, RemoveVolumes: true, Force: true})
 
 	ch := make(chan struct{})
-	result := make(map[string]*tar.Header)
+	result := make(map[string]tarHeader)
 	r, w := io.Pipe()
 	go func() {
 		defer close(ch)
@@ -865,7 +954,7 @@ func imageFSMetadata(c *docker.Client, name string) (map[string]*tar.Header, err
 				}
 				break
 			}
-			result[h.Name] = h
+			result[h.Name] = tarHeader{h}
 		}
 	}()
 	if err := c.ExportContainer(docker.ExportContainerOptions{ID: container.ID, OutputStream: w}); err != nil {
