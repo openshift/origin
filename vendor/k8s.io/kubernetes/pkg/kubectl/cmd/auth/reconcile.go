b10515ad58bda69b033bfdc1fd462e03448e4a7c
David Eads <deads@redhat.com>
2018-07-05 13:14:59 -0400

    UPSTREAM: 65715: fail on rbac resources of non-v1 versions in reconcile

--- vendor/k8s.io/kubernetes/pkg/kubectl/cmd/auth/reconcile.go
+++ vendor/k8s.io/kubernetes/pkg/kubectl/cmd/auth/reconcile.go
@@ -23,6 +23,8 @@ import (
 	"github.com/spf13/cobra"
 
 	rbacv1 "k8s.io/api/rbac/v1"
+	rbacv1beta1 "k8s.io/api/rbac/v1beta1"
+	rbacv1alpha1 "k8s.io/api/rbac/v1alpha1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	rbacv1client "k8s.io/client-go/kubernetes/typed/rbac/v1"
 	"k8s.io/kubernetes/pkg/kubectl/cmd/templates"
@@ -32,6 +34,7 @@ import (
 	"k8s.io/kubernetes/pkg/kubectl/genericclioptions/resource"
 	"k8s.io/kubernetes/pkg/kubectl/scheme"
 	"k8s.io/kubernetes/pkg/registry/rbac/reconciliation"
+	"fmt"
 )
 
 // ReconcileOptions is the start of the data required to perform the operation.  As new fields are added, add them here instead of
@@ -232,6 +235,16 @@ func (o *ReconcileOptions) RunReconcile() error {
 				return err
 			}
 			o.PrintObject(result.RoleBinding.GetObject(), o.Out)
+
+		case *rbacv1beta1.Role,
+			*rbacv1beta1.RoleBinding,
+			*rbacv1beta1.ClusterRole,
+			*rbacv1beta1.ClusterRoleBinding,
+			*rbacv1alpha1.Role,
+			*rbacv1alpha1.RoleBinding,
+			*rbacv1alpha1.ClusterRole,
+			*rbacv1alpha1.ClusterRoleBinding:
+			return fmt.Errorf("only rbac.authorization.k8s.io/v1 is supported: not %T", t)
 
 		default:
 			glog.V(1).Infof("skipping %#v", info.Object.GetObjectKind())
