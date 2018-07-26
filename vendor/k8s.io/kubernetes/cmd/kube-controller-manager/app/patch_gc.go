880890d7c13b25e9bb04a04417955240996528de
Monis Khan <mkhan@redhat.com>
2018-07-26 14:58:15 -0400

    UPSTREAM: <carry>: Access restriction changes for GC and quota
    This change makes GC and quota ignore access restrictions (there is
    likely no value in tracking them via these mechanisms).
    
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- vendor/k8s.io/kubernetes/cmd/kube-controller-manager/app/patch_gc.go
+++ vendor/k8s.io/kubernetes/cmd/kube-controller-manager/app/patch_gc.go
@@ -11,6 +11,7 @@ func applyOpenShiftGCConfig(controllerManager *config.Config) error {
 	controllerManager.ComponentConfig.GarbageCollectorController.GCIgnoredResources = append(controllerManager.ComponentConfig.GarbageCollectorController.GCIgnoredResources,
 		// explicitly disabled from GC for now - not enough value to track them
 		componentconfig.GroupResource{Group: "authorization.openshift.io", Resource: "rolebindingrestrictions"},
+		componentconfig.GroupResource{Group: "authorization.openshift.io", Resource: "accessrestrictions"},
 		componentconfig.GroupResource{Group: "network.openshift.io", Resource: "clusternetworks"},
 		componentconfig.GroupResource{Group: "network.openshift.io", Resource: "egressnetworkpolicies"},
 		componentconfig.GroupResource{Group: "network.openshift.io", Resource: "hostsubnets"},
