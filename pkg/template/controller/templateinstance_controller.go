458083225df30b5a11ce8bbe12a74e6206a43e8e
Ben Parees <bparees@redhat.com>
2018-08-01 16:14:41 -0400

    set full user info for SAR check on template readiness checks

--- pkg/template/controller/templateinstance_controller.go
+++ pkg/template/controller/templateinstance_controller.go
@@ -206,7 +206,17 @@ func (c *TemplateInstanceController) checkReadiness(templateInstance *templateap
 		return false, fmt.Errorf("Timeout")
 	}
 
-	u := &user.DefaultInfo{Name: templateInstance.Spec.Requester.Username}
+	extra := map[string][]string{}
+	for k, v := range templateInstance.Spec.Requester.Extra {
+		extra[k] = []string(v)
+	}
+
+	u := &user.DefaultInfo{
+		Name:   templateInstance.Spec.Requester.Username,
+		UID:    templateInstance.Spec.Requester.UID,
+		Groups: templateInstance.Spec.Requester.Groups,
+		Extra:  extra,
+	}
 
 	for _, object := range templateInstance.Status.Objects {
 		if !CanCheckReadiness(object.Ref) {
