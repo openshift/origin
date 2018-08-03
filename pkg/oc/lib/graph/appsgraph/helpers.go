21de3b73478eeff33fca74f3d260b94386c805fd
Maciej Szulik <maszulik@redhat.com>
2018-08-03 11:22:18 +0200

    Move apps helpers to graph helpers

--- pkg/oc/lib/graph/appsgraph/helpers.go
+++ pkg/oc/lib/graph/appsgraph/helpers.go
@@ -1,11 +1,12 @@
 package appsgraph
 
 import (
+	"fmt"
 	"sort"
 
-	kapi "k8s.io/kubernetes/pkg/apis/core"
+	corev1 "k8s.io/api/core/v1"
 
-	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
+	appsv1 "github.com/openshift/api/apps/v1"
 	appsutil "github.com/openshift/origin/pkg/apps/util"
 	appsgraph "github.com/openshift/origin/pkg/oc/lib/graph/appsgraph/nodes"
 	osgraph "github.com/openshift/origin/pkg/oc/lib/graph/genericgraph"
@@ -33,7 +34,7 @@ func RelevantDeployments(g osgraph.Graph, dcNode *appsgraph.DeploymentConfigNode
 	return nil, allDeployments
 }
 
-func BelongsToDeploymentConfig(config *appsapi.DeploymentConfig, b *kapi.ReplicationController) bool {
+func BelongsToDeploymentConfig(config *appsv1.DeploymentConfig, b *corev1.ReplicationController) bool {
 	if b.Annotations != nil {
 		return config.Name == appsutil.DeploymentConfigNameFor(b)
 	}
@@ -46,4 +47,103 @@ func (m RecentDeploymentReferences) Len() int      { return len(m) }
 func (m RecentDeploymentReferences) Swap(i, j int) { m[i], m[j] = m[j], m[i] }
 func (m RecentDeploymentReferences) Less(i, j int) bool {
 	return appsutil.DeploymentVersionFor(m[i].ReplicationController) > appsutil.DeploymentVersionFor(m[j].ReplicationController)
+}
+
+// TemplateImage is a structure for helping a caller iterate over a PodSpec
+type TemplateImage struct {
+	Image string
+
+	Ref *imageapi.DockerImageReference
+
+	From *corev1.ObjectReference
+
+	Container *corev1.Container
+}
+
+// templateImageForContainer takes a container and returns a TemplateImage.
+func templateImageForContainer(container *corev1.Container, triggerFn TriggeredByFunc) (TemplateImage, error) {
+	var ref imageapi.DockerImageReference
+	if trigger, ok := triggerFn(container); ok {
+		trigger.Image = container.Image
+		trigger.Container = container
+		return trigger, nil
+	}
+	ref, err := imageapi.ParseDockerImageReference(container.Image)
+	if err != nil {
+		return TemplateImage{Image: container.Image, Container: container}, err
+	}
+	return TemplateImage{Image: container.Image, Ref: &ref, Container: container}, nil
+}
+
+// TemplateImageForContainer locates the requested container in a pod spec, returning information about the
+// trigger (if it exists), or an error.
+func TemplateImageForContainer(pod *corev1.PodSpec, triggerFn TriggeredByFunc, containerName string) (TemplateImage, error) {
+	for i := range pod.Containers {
+		container := &pod.Containers[i]
+		if container.Name != containerName {
+			continue
+		}
+		return templateImageForContainer(container, triggerFn)
+	}
+	for i := range pod.InitContainers {
+		container := &pod.InitContainers[i]
+		if container.Name != containerName {
+			continue
+		}
+		return templateImageForContainer(container, triggerFn)
+	}
+	return TemplateImage{}, fmt.Errorf("no container %q found", containerName)
+}
+
+// eachTemplateImage invokes triggerFn and fn on the provided container.
+func eachTemplateImage(container *corev1.Container, triggerFn TriggeredByFunc, fn func(TemplateImage, error)) {
+	image, err := templateImageForContainer(container, triggerFn)
+	fn(image, err)
+}
+
+// EachTemplateImage iterates a pod spec, looking for triggers that match each container and invoking
+// fn with each located image.
+func EachTemplateImage(pod *corev1.PodSpec, triggerFn TriggeredByFunc, fn func(TemplateImage, error)) {
+	for i := range pod.Containers {
+		eachTemplateImage(&pod.Containers[i], triggerFn, fn)
+	}
+	for i := range pod.InitContainers {
+		eachTemplateImage(&pod.InitContainers[i], triggerFn, fn)
+	}
+}
+
+// TriggeredByFunc returns a TemplateImage or error from the provided container
+type TriggeredByFunc func(container *corev1.Container) (TemplateImage, bool)
+
+// IgnoreTriggers ignores the triggers
+func IgnoreTriggers(container *corev1.Container) (TemplateImage, bool) {
+	return TemplateImage{}, false
+}
+
+// DeploymentConfigHasTrigger returns a function that can identify the image for each container.
+func DeploymentConfigHasTrigger(config *appsv1.DeploymentConfig) TriggeredByFunc {
+	return func(container *corev1.Container) (TemplateImage, bool) {
+		for _, trigger := range config.Spec.Triggers {
+			params := trigger.ImageChangeParams
+			if params == nil {
+				continue
+			}
+			for _, name := range params.ContainerNames {
+				if container.Name == name {
+					if len(params.From.Name) == 0 {
+						continue
+					}
+					from := params.From
+					if len(from.Namespace) == 0 {
+						from.Namespace = config.Namespace
+					}
+					return TemplateImage{
+						Image: container.Image,
+						From:  &from,
+					}, true
+				}
+			}
+		}
+		return TemplateImage{}, false
+	}
 }
