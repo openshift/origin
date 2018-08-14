75a195d188514dcf9db7df307aacc85b1cbaabfb
David Eads <deads@redhat.com>
2018-08-14 16:19:17 -0400

    expose external type installation for configapi

--- pkg/cmd/server/apis/config/v1/register.go
+++ pkg/cmd/server/apis/config/v1/register.go
@@ -5,6 +5,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	coreinternalconversions "k8s.io/kubernetes/pkg/apis/core"
 
+	buildv1 "github.com/openshift/api/build/v1"
 	buildinternalconversions "github.com/openshift/origin/pkg/build/apis/build/v1"
 	"github.com/openshift/origin/pkg/cmd/server/apis/config"
 )
@@ -23,6 +24,12 @@ var (
 		addDefaultingFuncs,
 	)
 	InstallLegacy = legacySchemeBuilder.AddToScheme
+
+	externalLegacySchemeBuilder = runtime.NewSchemeBuilder(
+		addKnownTypesToLegacy,
+		buildv1.Install,
+	)
+	InstallLegacyExternal = externalLegacySchemeBuilder.AddToScheme
 )
 
 // Adds the list of known types to api.Scheme.
