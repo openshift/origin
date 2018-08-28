e7cd9f48b170554b7119fd67fda3922ca6dd08a3
Clayton Coleman <ccoleman@redhat.com>
2018-08-27 20:40:38 -0400

    Create a minimal suite for use from single node jobs in limited spots
    Remove use of -suite parameter in favor of a temporary SUITE env var
    for old jobs. Newer jobs will call gingko directly. Remove the setup
    code from the extended tests.

--- test/extended/conformance.sh
+++ test/extended/conformance.sh
@@ -11,10 +11,10 @@ exitstatus=0
 
 # run parallel tests
 os::log::info "Running parallel tests N=${PARALLEL_NODES:-<default>}"
-TEST_PARALLEL="${PARALLEL_NODES:-5}" TEST_REPORT_FILE_NAME=conformance_parallel os::test::extended::run -- -suite "parallel.conformance.openshift.io" -test.timeout 6h ${TEST_EXTENDED_ARGS-} || exitstatus=$?
+SUITE=openshift/conformance/parallel/minimal TEST_PARALLEL="${PARALLEL_NODES:-5}" TEST_REPORT_FILE_NAME=conformance_parallel os::test::extended::run -- -test.timeout 6h ${TEST_EXTENDED_ARGS-} || exitstatus=$?
 
 # run tests in serial
 os::log::info "Running serial tests"
-TEST_REPORT_FILE_NAME=conformance_serial os::test::extended::run -- -suite "serial.conformance.openshift.io" -test.timeout 2h ${TEST_EXTENDED_ARGS-} || exitstatus=$?
+SUITE=openshift/conformance/serial/minimal TEST_REPORT_FILE_NAME=conformance_serial os::test::extended::run -- -test.timeout 2h ${TEST_EXTENDED_ARGS-} || exitstatus=$?
 
 exit $exitstatus
