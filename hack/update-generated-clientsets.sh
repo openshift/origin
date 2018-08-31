29aa94e1b6f899aaf562e228f6b3d46998852acc
Michal Fojtik <mfojtik@redhat.com>
2018-08-31 14:23:53 +0200

    remove internal build client

--- hack/update-generated-clientsets.sh
+++ hack/update-generated-clientsets.sh
@@ -8,7 +8,6 @@ os::util::ensure::built_binary_exists 'client-gen' 'vendor/k8s.io/kubernetes/sta
 # list of package to generate client set for
 packages=(
   github.com/openshift/origin/pkg/authorization/apis/authorization
-  github.com/openshift/origin/pkg/build/apis/build
   github.com/openshift/origin/pkg/image/apis/image
   github.com/openshift/origin/pkg/oauth/apis/oauth
   github.com/openshift/origin/pkg/project/apis/project
