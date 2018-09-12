3ab82a476c2823a6d66b0964f9a33cfe5b5d086e
Miloslav Trmač <mitr@redhat.com>
2018-09-12 03:35:32 +0200

    Fix a GitHub issue URL
    This is not strictly related, but I was looking it up to make sure the
    code still needs to exist.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- pkg/oc/clusterup/docker/dockerhelper/helper.go
+++ pkg/oc/clusterup/docker/dockerhelper/helper.go
@@ -204,7 +204,7 @@ func (h *Helper) CheckAndPull(image string, out io.Writer) error {
 		return starterrors.NewError("error pulling Docker image %s", image).WithCause(err)
 	}
 
-	// This is to work around issue https://github.com/docker/docker/api/issues/138
+	// This is to work around issue https://github.com/docker/engine-api/issues/138
 	// where engine-api/client/ImagePull does not return an error when it should.
 	// which also still seems to exist in https://github.com/moby/moby/blob/master/client/image_pull.go
 	_, _, err = h.client.ImageInspectWithRaw(image, false)
