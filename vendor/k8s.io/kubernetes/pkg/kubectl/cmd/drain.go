49c06486ae57374b82aedfa7f5556385416661b2
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2018-09-20 17:17:53 -0400

    UPSTREAM: 68767: Fix drain for evicting terminal DS pods and pods with local storage

--- vendor/k8s.io/kubernetes/pkg/kubectl/cmd/drain.go
+++ vendor/k8s.io/kubernetes/pkg/kubectl/cmd/drain.go
@@ -403,14 +403,17 @@ func (o *DrainOptions) unreplicatedFilter(pod corev1.Pod) (bool, *warning, *fata
 
 func (o *DrainOptions) daemonsetFilter(pod corev1.Pod) (bool, *warning, *fatal) {
 	// Note that we return false in cases where the pod is DaemonSet managed,
-	// regardless of flags.  We never delete them, the only question is whether
-	// their presence constitutes an error.
+	// regardless of flags.
 	//
 	// The exception is for pods that are orphaned (the referencing
 	// management resource - including DaemonSet - is not found).
 	// Such pods will be deleted if --force is used.
 	controllerRef := o.getPodController(pod)
 	if controllerRef == nil || controllerRef.Kind != "DaemonSet" {
+		return true, nil, nil
+	}
+	// Any finished pod can be removed.
+	if pod.Status.Phase == corev1.PodSucceeded || pod.Status.Phase == corev1.PodFailed {
 		return true, nil, nil
 	}
 
@@ -448,6 +451,10 @@ func hasLocalStorage(pod corev1.Pod) bool {
 
 func (o *DrainOptions) localStorageFilter(pod corev1.Pod) (bool, *warning, *fatal) {
 	if !hasLocalStorage(pod) {
+		return true, nil, nil
+	}
+	// Any finished pod can be removed.
+	if pod.Status.Phase == corev1.PodSucceeded || pod.Status.Phase == corev1.PodFailed {
 		return true, nil, nil
 	}
 	if !o.DeleteLocalData {
