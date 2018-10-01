8850c84fd1d2a767b416e8dca55de0458e6380f8
Dan Winship <danw@redhat.com>
2018-10-01 14:39:12 -0400

    Drop unversioned network types

--- pkg/network/apis/network/plugin.go
+++
@@ -1,7 +0,0 @@
-package network
-
-const (
-
-	// HostSubnet annotations. (Note: should be "hostsubnet.network.openshift.io/", but the incorrect name is now part of the API.)
-	AssignHostSubnetAnnotation = "pod.network.openshift.io/assign-subnet"
-)
--- pkg/network/apis/network/v1/register.go
+++ pkg/network/apis/network/v1/register.go
@@ -4,12 +4,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/openshift/api/network/v1"
-	"github.com/openshift/origin/pkg/network/apis/network"
 )
 
 var (
 	localSchemeBuilder = runtime.NewSchemeBuilder(
-		network.Install,
 		v1.Install,
 		RegisterDefaults,
 	)
