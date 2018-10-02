41c1aa9e5c11868bed5b037b27160e02e0c7bc09
Ben Parees <bparees@redhat.com>
2018-10-02 12:57:52 -0400

    use common storage for image extraction in docker builds

--- pkg/build/controller/strategy/docker.go
+++ pkg/build/controller/strategy/docker.go
@@ -147,6 +147,7 @@ func (bs *DockerBuildStrategy) CreateBuildPod(build *buildv1.Build) (*v1.Pod, er
 		}
 		setupDockerSecrets(pod, &extractImageContentContainer, build.Spec.Output.PushSecret, strategy.PullSecret, build.Spec.Source.Images)
 		setupContainersConfigs(pod, &extractImageContentContainer)
+		setupContainersStorage(pod, &extractImageContentContainer)
 		pod.Spec.InitContainers = append(pod.Spec.InitContainers, extractImageContentContainer)
 	}
 	pod.Spec.InitContainers = append(pod.Spec.InitContainers,
