6b3e8b518fd0e3768ac8fb3d2835eea09a25bf36
Clayton Coleman <ccoleman@redhat.com>
2018-10-03 16:43:37 -0400

    Disable a local storage serial test

--- test/extended/util/test.go
+++ test/extended/util/test.go
@@ -356,6 +356,7 @@ var (
 		"[Flaky]": {
 			`Job should run a job to completion when tasks sometimes fail and are not locally restarted`, // seems flaky, also may require too many resources
 			`openshift mongodb replication creating from a template`,                                     // flaking on deployment
+			`should use be able to process many pods and reuse local volumes`,                            // https://bugzilla.redhat.com/show_bug.cgi?id=1635893
 		},
 		// tests that must be run without competition
 		"[Serial]": {
