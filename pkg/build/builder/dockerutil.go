f9ce587bd8034996661790fc2ee2396642ce2697
Miloslav Trmač <mitr@redhat.com>
2018-10-09 00:01:21 +0200

    Use a callback instead of type-unsafe opts in RetryImageAction
    Motivation for this commit sequence: we will need to create a new
    imageprogress.NewPullWriter/NewPushWriter objects for each pull/push,
    so that we can detect errors reported by the pull/push writer for each
    pull/push.  This would be very cumbersome with the current RetryImageAction,
    which hard-codes knowledge about how to do a pull/push, based on a type
    of its untyped opts{} paramter.
    
    First, move the actual pull/push into a func() callback, so that
    the code is located is in the pullImage/pushImage implementations directly;
    that also allows us to drop the client and authConfig parameters to
    RetryImageAction.
    
    The untyped opts{} still remains, only for actionName; that will be cleaned
    up immediately.
    
    Also renames RetryImageAction to make it private; AFAICS there are no
    other users, and we are breaking its API anyway.
    
    Should not change behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- pkg/build/builder/dockerutil.go
+++ pkg/build/builder/dockerutil.go
@@ -68,7 +68,7 @@ type DockerClient interface {
 	TagImage(name string, opts docker.TagImageOptions) error
 }
 
-func RetryImageAction(client DockerClient, opts interface{}, authConfig docker.AuthConfiguration) error {
+func retryImageAction(opts interface{}, action func() error) error {
 	var err error
 	var retriableError = false
 	var actionName string
@@ -79,10 +79,10 @@ func RetryImageAction(client DockerClient, opts interface{}, authConfig docker.A
 	for retries := 0; retries <= DefaultPushOrPullRetryCount; retries++ {
 		if reflect.TypeOf(opts) == reflect.TypeOf(pullOpt) {
 			actionName = "Pull"
-			err = client.PullImage(opts.(docker.PullImageOptions), authConfig)
+			err = action()
 		} else if reflect.TypeOf(opts) == reflect.TypeOf(pushOpt) {
 			actionName = "Push"
-			err = client.PushImage(opts.(docker.PushImageOptions), authConfig)
+			err = action()
 		} else {
 			return errors.New("not match Pull or Push action")
 		}
@@ -141,7 +141,9 @@ func pullImage(client DockerClient, name string, authConfig docker.AuthConfigura
 		opts.OutputStream = os.Stderr
 		opts.RawJSONStream = false
 	}
-	return RetryImageAction(client, opts, authConfig)
+	return retryImageAction(opts, func() error {
+		return client.PullImage(opts, authConfig)
+	})
 }
 
 // pushImage pushes a docker image to the registry specified in its tag.
@@ -173,7 +175,9 @@ func pushImage(client DockerClient, name string, authConfig docker.AuthConfigura
 		RawJSONStream: true,
 	}
 
-	if err := RetryImageAction(client, opts, authConfig); err != nil {
+	if err := retryImageAction(opts, func() error {
+		return client.PushImage(opts, authConfig)
+	}); err != nil {
 		return "", err
 	}
 	return digestWriter.Digest, nil
