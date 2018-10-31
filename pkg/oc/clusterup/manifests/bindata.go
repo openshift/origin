c539a79ad8587d4db297ba6ea2093b1b3e242cc1
Ben Parees <bparees@redhat.com>
2018-10-31 15:02:50 -0400

    patch up e2e tests to use version-aligned templates

--- pkg/oc/clusterup/manifests/bindata.go
+++ pkg/oc/clusterup/manifests/bindata.go
@@ -7290,6 +7290,7 @@ spec:
     type: Git
     git:
       uri: https://github.com/openshift/jenkins-client-plugin.git
+      ref: release-3.10
   strategy:
     type: JenkinsPipeline
     jenkinsPipelineStrategy:
