213e625837ad83739a43f3e696091703b7760157
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2018-11-05 15:20:15 +0900

    Update completion to use api-resources

--- pkg/oc/cli/cli_bashcomp_func.go
+++ pkg/oc/cli/cli_bashcomp_func.go
@@ -55,9 +55,14 @@ __oc_get_namespaces()
 __oc_get_resource()
 {
     if [[ ${#nouns[@]} -eq 0 ]]; then
-        return 1
+      local oc_out
+      if oc_out=$(oc api-resources $(__oc_override_flags) -o name --cached --request-timeout=5s --verbs=get 2>/dev/null); then
+          COMPREPLY=( $( compgen -W "${oc_out[*]}" -- "$cur" ) )
+          return 0
+      fi
+      return 1
     fi
-    __oc_parse_get ${nouns[${#nouns[@]} -1]}
+    __oc_parse_get "${nouns[${#nouns[@]} -1]}"
 }
 
 # $1 is the name of the pod we want to get the list of containers inside
