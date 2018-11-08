32a06b6d4cad20a2e4a9fb27ded58d1de4491a41
Clayton Coleman <ccoleman@redhat.com>
2018-11-08 17:11:58 -0500

    cli: Teach `oc image mirror` to accept a file from stdin
    `oc image mirror -f -` should accept file definitions from STDIN like
    the rest of our commands.

--- pkg/oc/cli/image/mirror/mirror.go
+++ pkg/oc/cli/image/mirror/mirror.go
@@ -151,7 +151,7 @@ func (o *MirrorImageOptions) Complete(cmd *cobra.Command, args []string) error {
 		return err
 	}
 	for _, filename := range o.Filenames {
-		mappings, err := parseFile(filename, overlap)
+		mappings, err := parseFile(filename, overlap, o.In)
 		if err != nil {
 			return err
 		}
