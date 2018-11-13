3cb3efa898d49c34c95fb4c84a3857ce77d69fe3
Maciej Szulik <maszulik@redhat.com>
2018-11-13 14:00:01 +0100

    Add missing versioned polymorphic helpers

--- pkg/oc/originpolymorphichelpers/mapbasedselectorforobject.go
+++ pkg/oc/originpolymorphichelpers/mapbasedselectorforobject.go
@@ -1,17 +1,20 @@
 package originpolymorphichelpers
 
 import (
-	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
-
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/kubernetes/pkg/kubectl"
 	"k8s.io/kubernetes/pkg/kubectl/polymorphichelpers"
+
+	appsv1 "github.com/openshift/api/apps/v1"
+	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
 )
 
 func NewMapBasedSelectorForObjectFn(delegate polymorphichelpers.MapBasedSelectorForObjectFunc) polymorphichelpers.MapBasedSelectorForObjectFunc {
 	return func(object runtime.Object) (string, error) {
 		switch t := object.(type) {
 		case *appsapi.DeploymentConfig:
+			return kubectl.MakeLabels(t.Spec.Selector), nil
+		case *appsv1.DeploymentConfig:
 			return kubectl.MakeLabels(t.Spec.Selector), nil
 
 		default:
--- pkg/oc/originpolymorphichelpers/portsforobject.go
+++ pkg/oc/originpolymorphichelpers/portsforobject.go
@@ -3,10 +3,12 @@ package originpolymorphichelpers
 import (
 	"strconv"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	api "k8s.io/kubernetes/pkg/apis/core"
+	"k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/kubernetes/pkg/kubectl/polymorphichelpers"
 
+	appsv1 "github.com/openshift/api/apps/v1"
 	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
 )
 
@@ -14,6 +16,8 @@ func NewPortsForObjectFn(delegate polymorphichelpers.PortsForObjectFunc) polymor
 	return func(object runtime.Object) ([]string, error) {
 		switch t := object.(type) {
 		case *appsapi.DeploymentConfig:
+			return getPortsInternal(t.Spec.Template.Spec), nil
+		case *appsv1.DeploymentConfig:
 			return getPorts(t.Spec.Template.Spec), nil
 
 		default:
@@ -22,7 +26,17 @@ func NewPortsForObjectFn(delegate polymorphichelpers.PortsForObjectFunc) polymor
 	}
 }
 
-func getPorts(spec api.PodSpec) []string {
+func getPorts(spec corev1.PodSpec) []string {
+	result := []string{}
+	for _, container := range spec.Containers {
+		for _, port := range container.Ports {
+			result = append(result, strconv.Itoa(int(port.ContainerPort)))
+		}
+	}
+	return result
+}
+
+func getPortsInternal(spec core.PodSpec) []string {
 	result := []string{}
 	for _, container := range spec.Containers {
 		for _, port := range container.Ports {
