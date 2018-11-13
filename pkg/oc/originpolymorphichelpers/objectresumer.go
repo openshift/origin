c63ebc4d97aaf78b10d91758b38d5b60b4e4ba33
Maciej Szulik <maszulik@redhat.com>
2018-11-13 14:00:01 +0100

    Add missing versioned polymorphic helpers

--- pkg/oc/originpolymorphichelpers/objectresumer.go
+++ pkg/oc/originpolymorphichelpers/objectresumer.go
@@ -5,7 +5,9 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/kubernetes/pkg/kubectl/polymorphichelpers"
+	"k8s.io/kubernetes/pkg/kubectl/scheme"
 
+	appsv1 "github.com/openshift/api/apps/v1"
 	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
 )
 
@@ -19,6 +21,13 @@ func NewObjectResumerFn(delegate polymorphichelpers.ObjectResumerFunc) polymorph
 			t.Spec.Paused = false
 			// TODO: Resume the deployer containers.
 			return runtime.Encode(internalVersionJSONEncoder(), obj)
+
+		case *appsv1.DeploymentConfig:
+			if !t.Spec.Paused {
+				return nil, errors.New("is not paused")
+			}
+			t.Spec.Paused = false
+			return runtime.Encode(scheme.Codecs.LegacyCodec(appsv1.SchemeGroupVersion), obj)
 
 		default:
 			return delegate(obj)
--- pkg/oc/originpolymorphichelpers/protocolsforobject.go
+++ pkg/oc/originpolymorphichelpers/protocolsforobject.go
@@ -3,14 +3,30 @@ package originpolymorphichelpers
 import (
 	"strconv"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	api "k8s.io/kubernetes/pkg/apis/core"
+	"k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/kubernetes/pkg/kubectl/polymorphichelpers"
 
+	appsv1 "github.com/openshift/api/apps/v1"
 	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
 )
 
-func getProtocols(spec api.PodSpec) map[string]string {
+func NewProtocolsForObjectFn(delegate polymorphichelpers.ProtocolsForObjectFunc) polymorphichelpers.ProtocolsForObjectFunc {
+	return func(object runtime.Object) (map[string]string, error) {
+		switch t := object.(type) {
+		case *appsapi.DeploymentConfig:
+			return getProtocolsInternal(t.Spec.Template.Spec), nil
+		case *appsv1.DeploymentConfig:
+			return getProtocols(t.Spec.Template.Spec), nil
+
+		default:
+			return delegate(object)
+		}
+	}
+}
+
+func getProtocols(spec corev1.PodSpec) map[string]string {
 	result := make(map[string]string)
 	for _, container := range spec.Containers {
 		for _, port := range container.Ports {
@@ -20,14 +36,12 @@ func getProtocols(spec api.PodSpec) map[string]string {
 	return result
 }
 
-func NewProtocolsForObjectFn(delegate polymorphichelpers.ProtocolsForObjectFunc) polymorphichelpers.ProtocolsForObjectFunc {
-	return func(object runtime.Object) (map[string]string, error) {
-		switch t := object.(type) {
-		case *appsapi.DeploymentConfig:
-			return getProtocols(t.Spec.Template.Spec), nil
-
-		default:
-			return delegate(object)
+func getProtocolsInternal(spec core.PodSpec) map[string]string {
+	result := make(map[string]string)
+	for _, container := range spec.Containers {
+		for _, port := range container.Ports {
+			result[strconv.Itoa(int(port.ContainerPort))] = string(port.Protocol)
 		}
 	}
+	return result
 }
