deb48ca617a5b55680e5357c229710b0a1aaf43a
Daein Park <bysnupy@hotmail.com>
2019-01-07 22:08:45 +0900

    Add hostname lookup condition for selecting default local IP on the host having multiple NICs.
    It's required the following configuring node hostname to install OpenShift, so hostname lookup IP is more reasonable to configuring as a default local IP. Specially it's helpful on the node hosts having multiple interfaces.
      "Each node’s host name must be resolvable, and each node must be able to reach each other node."
    
    - Related Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1662253

--- pkg/cmd/util/ip.go
+++ pkg/cmd/util/ip.go
@@ -3,6 +3,7 @@ package util
 import (
 	"errors"
 	"net"
+	"os"
 )
 
 // ErrorNoDefaultIP is returned when no suitable non-loopback address can be found.
@@ -11,9 +12,17 @@ var ErrorNoDefaultIP = errors.New("no suitable IP address")
 // DefaultLocalIP4 returns an IPv4 address that this host can be reached
 // on. Will return NoDefaultIP if no suitable address can be found.
 func DefaultLocalIP4() (net.IP, error) {
+	var firstIP net.IP = nil
+	var hostIP = ""
 	devices, err := net.Interfaces()
 	if err != nil {
 		return nil, err
+	}
+	// Get an IP as hostname lookup.
+	if hostname, err := os.Hostname(); err == nil && len(hostname) > 0 {
+		if ip, err := net.LookupHost(hostname); err == nil && len(ip) == 1 {
+			hostIP = ip[0]
+		}
 	}
 	for _, dev := range devices {
 		if (dev.Flags&net.FlagUp != 0) && (dev.Flags&net.FlagLoopback == 0) {
@@ -24,11 +33,22 @@ func DefaultLocalIP4() (net.IP, error) {
 			for i := range addrs {
 				if ip, ok := addrs[i].(*net.IPNet); ok {
 					if ip.IP.To4() != nil {
-						return ip.IP, nil
+						// Save first IP that this host can be reached on.
+						if firstIP == nil {
+							firstIP = ip.IP
+						}
+						// When host IP is existing on this host, return the IP.
+						if ip.IP.String() == hostIP {
+							return ip.IP, nil
+						}
 					}
 				}
 			}
 		}
+	}
+	// When the host IP is not matched, first interface's IP would return.
+	if firstIP != nil {
+		return firstIP, nil
 	}
 	return nil, ErrorNoDefaultIP
 }
