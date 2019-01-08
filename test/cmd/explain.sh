a0fbe81355f778db242657758de1f14f10bcf77a
Maciej Szulik <maszulik@redhat.com>
2019-01-08 18:44:14 +0100

    Re-enable oc explain tests

--- test/cmd/explain.sh
+++ test/cmd/explain.sh
@@ -13,17 +13,17 @@ trap os::test::junit::reconcile_output EXIT
 os::test::junit::declare_suite_start "cmd/explain"
 # This test validates that the explain command works with openshift resources
 
-#os::cmd::expect_success 'oc explain dc'
-#os::cmd::expect_success_and_text 'oc explain dc.status.replicas' 'FIELD\: +replicas'
-#
-#os::cmd::expect_success 'oc explain routes'
-#os::cmd::expect_success_and_text 'oc explain route.metadata.name' 'string'
-#
-#os::cmd::expect_success 'oc explain bc'
-#os::cmd::expect_success 'oc explain image'
-#os::cmd::expect_success 'oc explain is'
-#
-#os::cmd::expect_success 'oc explain cronjob'
+os::cmd::expect_success 'oc explain dc'
+os::cmd::expect_success_and_text 'oc explain dc.status.replicas' 'FIELD\: +replicas'
+
+os::cmd::expect_success 'oc explain routes'
+os::cmd::expect_success_and_text 'oc explain route.metadata.name' 'string'
+
+os::cmd::expect_success 'oc explain bc'
+os::cmd::expect_success 'oc explain image'
+os::cmd::expect_success 'oc explain is'
+
+os::cmd::expect_success 'oc explain cronjob'
 
 echo "explain: ok"
 os::test::junit::declare_suite_end
