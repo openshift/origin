b55a5cc1780cda5419e88f5ed0e0cd8903c4985d
David Eads <deads@redhat.com>
2019-01-18 09:01:21 -0500

    add most basic proxy.config.openshift.io validation

--- pkg/admission/customresourcevalidation/customresourcevalidationregistration/cr_validation_registration.go
+++ pkg/admission/customresourcevalidation/customresourcevalidationregistration/cr_validation_registration.go
@@ -2,14 +2,17 @@ package customresourcevalidationregistration
 
 import (
 	"github.com/openshift/origin/pkg/admission/customresourcevalidation/image"
+	"github.com/openshift/origin/pkg/admission/customresourcevalidation/proxy"
 	"k8s.io/apiserver/pkg/admission"
 )
 
 // AllCustomResourceValidators are the names of all custom resource validators that should be registered
 var AllCustomResourceValidators = []string{
 	"config.openshift.io/ValidateImage",
+	"config.openshift.io/ValidateProxy",
 }
 
 func RegisterCustomResourceValidation(plugins *admission.Plugins) {
 	image.Register(plugins)
+	proxy.Register(plugins)
 }
