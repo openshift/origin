3c011c08a6b1f3e16d397681ce947973b82d7ab8
Monis Khan <mkhan@redhat.com>
2019-01-21 14:53:21 -0500

    Add boilerplate for OAuth validation
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/admission/customresourcevalidation/customresourcevalidationregistration/cr_validation_registration.go
+++ pkg/admission/customresourcevalidation/customresourcevalidationregistration/cr_validation_registration.go
@@ -5,18 +5,21 @@ import (
 
 	"github.com/openshift/origin/pkg/admission/customresourcevalidation/authentication"
 	"github.com/openshift/origin/pkg/admission/customresourcevalidation/image"
+	"github.com/openshift/origin/pkg/admission/customresourcevalidation/oauth"
 	"github.com/openshift/origin/pkg/admission/customresourcevalidation/project"
 )
 
 // AllCustomResourceValidators are the names of all custom resource validators that should be registered
 var AllCustomResourceValidators = []string{
-	image.PluginName,
-	project.PluginName,
 	authentication.PluginName,
+	image.PluginName,
+	oauth.PluginName,
+	project.PluginName,
 }
 
 func RegisterCustomResourceValidation(plugins *admission.Plugins) {
-	image.Register(plugins)
-	project.Register(plugins)
 	authentication.Register(plugins)
+	image.Register(plugins)
+	oauth.Register(plugins)
+	project.Register(plugins)
 }
