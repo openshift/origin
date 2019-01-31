cd71bfb3a7d5d0a098d4a75fde6e9d1c8eb5a087
Ben Parees <bparees@redhat.com>
2019-01-31 17:25:23 -0500

    clean up some of our test skipping logic

--- test/extended/builds/pullsecret.go
+++ test/extended/builds/pullsecret.go
@@ -37,9 +37,6 @@ var _ = g.Describe("[Feature:Builds][Slow] using pull secrets in a build", func(
 
 			g.Describe("binary builds", func() {
 				g.It("should be able to run a build that is implicitly pulling from the internal registry", func() {
-					if !exutil.IsClusterOperated(oc) {
-						g.Skip("Skipping on non-installer cluster because this test expects image-registry, not docker-registry")
-					}
 					g.By("creating a build")
 					err := oc.Run("new-build").Args("--binary", "--strategy=docker", "--name=docker").Execute()
 					o.Expect(err).NotTo(o.HaveOccurred())
