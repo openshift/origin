af68cb5d67844ea26782e5e0848f767d3a3f41a1
Ravi Sankar Penta <rpenta@redhat.com>
2019-02-05 23:30:34 -0800

    Added router test to check if openshift-monitoring is able to pull haproxy metrics
    - Also fixed router test cases related to metrics

--- test/extended/router/metrics.go
+++ test/extended/router/metrics.go
@@ -2,10 +2,13 @@ package router
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
-	"net"
 	"net/http"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	g "github.com/onsi/ginkgo"
@@ -18,6 +21,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
+	clientset "k8s.io/client-go/kubernetes"
+	watchtools "k8s.io/client-go/tools/watch"
+	"k8s.io/kubernetes/pkg/client/conditions"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 
 	exutil "github.com/openshift/origin/test/extended/util"
@@ -30,28 +36,30 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 
 		username, password, execPodName, ns, host string
 		statsPort                                 int
-		hasHealth, hasMetrics                     bool
+		routerNamespace, serviceIP, bearerToken   string
 	)
 
 	g.BeforeEach(func() {
-		template, _, err := exutil.GetRouterPodTemplate(oc)
+		var err error
+		var template *corev1.PodTemplateSpec
+		template, routerNamespace, err = exutil.GetRouterPodTemplate(oc)
 		if kapierrs.IsNotFound(err) {
 			g.Skip("no router installed on the cluster")
 			return
 		}
 		o.Expect(err).NotTo(o.HaveOccurred())
 		env := template.Spec.Containers[0].Env
-		username, password = findEnvVar(env, "STATS_USERNAME"), findEnvVar(env, "STATS_PASSWORD")
-		statsPortString := findEnvVar(env, "STATS_PORT")
-		hasMetrics = len(findEnvVar(env, "ROUTER_METRICS_TYPE")) > 0
-		listenAddr := findEnvVar(env, "ROUTER_LISTEN_ADDR")
+
+		if len(findEnvVar(env, "ROUTER_METRICS_TYPE")) == 0 {
+			g.Skip("router does not have ROUTER_METRICS_TYPE set")
+			return
+		}
+
+		username, password, err = findStatsUsernameAndPassword(oc, routerNamespace, env)
+		o.Expect(err).NotTo(o.HaveOccurred())
 
 		statsPort = 1936
-		if len(listenAddr) > 0 {
-			hasHealth = true
-			_, port, _ := net.SplitHostPort(listenAddr)
-			statsPortString = port
-		}
+		statsPortString := findEnvVar(env, "STATS_PORT")
 		if len(statsPortString) > 0 {
 			if port, err := strconv.Atoi(statsPortString); err == nil {
 				statsPort = port
@@ -59,6 +67,12 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 		}
 
 		host, err = waitForRouterMetricsIP(oc)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		serviceIP, err = waitForRouterServiceIP(oc)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		bearerToken, err = findMetricsBearerToken(oc)
 		o.Expect(err).NotTo(o.HaveOccurred())
 
 		ns = oc.KubeFramework().Namespace.Name
@@ -72,9 +86,6 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 
 	g.Describe("The HAProxy router", func() {
 		g.It("should expose a health check on the metrics port", func() {
-			if !hasHealth {
-				g.Skip("router does not have ROUTER_LISTEN_ADDR set")
-			}
 			execPodName = exutil.CreateExecPodOrFail(oc.AdminKubeClient().Core(), ns, "execpod")
 			defer func() { oc.AdminKubeClient().Core().Pods(ns).Delete(execPodName, metav1.NewDeleteOptions(1)) }()
 
@@ -84,10 +95,6 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 		})
 
 		g.It("should expose prometheus metrics for a route", func() {
-			if !hasMetrics {
-				g.Skip("router does not have ROUTER_METRICS_TYPE set")
-			}
-
 			g.By("when a route exists")
 			configPath := exutil.FixturePath("testdata", "router-metrics.yaml")
 			err := oc.Run("create").Args("-f", configPath).Execute()
@@ -100,28 +107,34 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 			err = expectURLStatusCodeExec(ns, execPodName, fmt.Sprintf("http://%s:%d/metrics", host, statsPort), 401, 403)
 			o.Expect(err).NotTo(o.HaveOccurred())
 
+			g.By("validate access using username and password")
+			_, err = getAuthenticatedURLViaPod(ns, execPodName, fmt.Sprintf("http://%s:%d/metrics", host, statsPort), username, password)
+			o.Expect(err).NotTo(o.HaveOccurred())
+
 			g.By("checking for the expected metrics")
 			routeLabels := promLabels{"backend": "http", "namespace": ns, "route": "weightedroute"}
 			serverLabels := promLabels{"namespace": ns, "route": "weightedroute"}
 			var metrics map[string]*dto.MetricFamily
-			times := 10
-			p := expfmt.TextParser{}
 			var results string
 			defer func() { e2e.Logf("initial metrics:\n%s", results) }()
+			times := 10
+			p := expfmt.TextParser{}
+
 			err = wait.PollImmediate(2*time.Second, 240*time.Second, func() (bool, error) {
-				results, err = getAuthenticatedURLViaPod(ns, execPodName, fmt.Sprintf("http://%s:%d/metrics", host, statsPort), username, password)
+				results, err = getBearerTokenURLViaPod(ns, execPodName, fmt.Sprintf("http://%s:%d/metrics", host, statsPort), bearerToken)
 				o.Expect(err).NotTo(o.HaveOccurred())
 
 				metrics, err = p.TextToMetricFamilies(bytes.NewBufferString(results))
 				o.Expect(err).NotTo(o.HaveOccurred())
 				//e2e.Logf("Metrics:\n%s", results)
+
 				if len(findGaugesWithLabels(metrics["haproxy_server_up"], serverLabels)) == 2 {
 					if findGaugesWithLabels(metrics["haproxy_backend_connections_total"], routeLabels)[0] >= float64(times) {
 						return true, nil
 					}
 					// send a burst of traffic to the router
 					g.By("sending traffic to a weighted route")
-					err = expectRouteStatusCodeRepeatedExec(ns, execPodName, fmt.Sprintf("http://%s", host), "weighted.metrics.example.com", http.StatusOK, times)
+					err = expectRouteStatusCodeRepeatedExec(ns, execPodName, fmt.Sprintf("http://%s", serviceIP), "weighted.metrics.example.com", http.StatusOK, times)
 					o.Expect(err).NotTo(o.HaveOccurred())
 				}
 				g.By("retrying metrics until all backend servers appear")
@@ -183,7 +196,7 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 			time.Sleep(15 * time.Second)
 
 			g.By("checking that some metrics are not reset to 0 after router restart")
-			updatedResults, err := getAuthenticatedURLViaPod(ns, execPodName, fmt.Sprintf("http://%s:%d/metrics", host, statsPort), username, password)
+			updatedResults, err := getBearerTokenURLViaPod(ns, execPodName, fmt.Sprintf("http://%s:%d/metrics", host, statsPort), bearerToken)
 			o.Expect(err).NotTo(o.HaveOccurred())
 			defer func() { e2e.Logf("final metrics:\n%s", updatedResults) }()
 
@@ -194,9 +207,6 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 		})
 
 		g.It("should expose the profiling endpoints", func() {
-			if !hasHealth {
-				g.Skip("router does not have ROUTER_LISTEN_ADDR set")
-			}
 			execPodName = exutil.CreateExecPodOrFail(oc.AdminKubeClient().Core(), ns, "execpod")
 			defer func() { oc.AdminKubeClient().Core().Pods(ns).Delete(execPodName, metav1.NewDeleteOptions(1)) }()
 
@@ -208,6 +218,26 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 			results, err := getAuthenticatedURLViaPod(ns, execPodName, fmt.Sprintf("http://%s:%d/debug/pprof/heap?debug=1", host, statsPort), username, password)
 			o.Expect(err).NotTo(o.HaveOccurred())
 			o.Expect(results).To(o.ContainSubstring("# runtime.MemStats"))
+		})
+
+		g.It("should enable openshift-monitoring to pull metrics", func() {
+			prometheusURL, token, exists := locatePrometheus(oc)
+			if !exists {
+				g.Skip("prometheus not found on this cluster")
+			}
+
+			execPodName := e2e.CreateExecPodOrFail(oc.AdminKubeClient(), ns, "execpod", func(pod *corev1.Pod) { pod.Spec.Containers[0].Image = "centos:7" })
+			defer func() { oc.AdminKubeClient().Core().Pods(ns).Delete(execPodName, metav1.NewDeleteOptions(1)) }()
+
+			contents, err := getBearerTokenURLViaPod(ns, execPodName, fmt.Sprintf("%s/api/v1/targets", prometheusURL), token)
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			targets := &promTargets{}
+			err = json.Unmarshal([]byte(contents), targets)
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			err = targets.Expect(promLabels{"job": "router-internal-default"}, "up", "^https://.*/metrics$")
+			o.Expect(err).NotTo(o.HaveOccurred())
 		})
 	})
 })
@@ -221,6 +251,82 @@ func (l promLabels) With(name, value string) promLabels {
 	}
 	n[name] = value
 	return n
+}
+
+type promTargets struct {
+	Data struct {
+		ActiveTargets []struct {
+			Labels    map[string]string
+			Health    string
+			ScrapeUrl string
+		}
+	}
+	Status string
+}
+
+func (t *promTargets) Expect(l promLabels, health, scrapeURLPattern string) error {
+	for _, target := range t.Data.ActiveTargets {
+		match := true
+		for k, v := range l {
+			if target.Labels[k] != v {
+				match = false
+				break
+			}
+		}
+		if !match {
+			continue
+		}
+		if health != target.Health {
+			continue
+		}
+		if !regexp.MustCompile(scrapeURLPattern).MatchString(target.ScrapeUrl) {
+			continue
+		}
+		return nil
+	}
+	return fmt.Errorf("no match for %v with health %s and scrape URL %s", l, health, scrapeURLPattern)
+}
+
+func waitForServiceAccountInNamespace(c clientset.Interface, ns, serviceAccountName string, timeout time.Duration) error {
+	w, err := c.Core().ServiceAccounts(ns).Watch(metav1.SingleObject(metav1.ObjectMeta{Name: serviceAccountName}))
+	if err != nil {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	_, err = watchtools.UntilWithoutRetry(ctx, w, conditions.ServiceAccountHasSecrets)
+	return err
+}
+
+func locatePrometheus(oc *exutil.CLI) (url, bearerToken string, ok bool) {
+	_, err := oc.AdminKubeClient().Core().Services("openshift-monitoring").Get("prometheus-k8s", metav1.GetOptions{})
+	if kapierrs.IsNotFound(err) {
+		return "", "", false
+	}
+
+	waitForServiceAccountInNamespace(oc.AdminKubeClient(), "openshift-monitoring", "prometheus-k8s", 2*time.Minute)
+	for i := 0; i < 30; i++ {
+		secrets, err := oc.AdminKubeClient().Core().Secrets("openshift-monitoring").List(metav1.ListOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		for _, secret := range secrets.Items {
+			if secret.Type != corev1.SecretTypeServiceAccountToken {
+				continue
+			}
+			if !strings.HasPrefix(secret.Name, "prometheus-") {
+				continue
+			}
+			bearerToken = string(secret.Data[corev1.ServiceAccountTokenKey])
+			break
+		}
+		if len(bearerToken) == 0 {
+			e2e.Logf("Waiting for prometheus service account secret to show up")
+			time.Sleep(time.Second)
+			continue
+		}
+	}
+	o.Expect(bearerToken).ToNot(o.BeEmpty())
+
+	return "https://prometheus-k8s.openshift-monitoring.svc:9091", bearerToken, true
 }
 
 func findEnvVar(vars []corev1.EnvVar, key string) string {
@@ -305,4 +411,74 @@ func getAuthenticatedURLViaPod(ns, execPodName, url, user, pass string) (string,
 		return "", fmt.Errorf("host command failed: %v\n%s", err, output)
 	}
 	return output, nil
+}
+
+func getBearerTokenURLViaPod(ns, execPodName, url, bearer string) (string, error) {
+	cmd := fmt.Sprintf("curl -s -k -H 'Authorization: Bearer %s' %q", bearer, url)
+	output, err := e2e.RunHostCmd(ns, execPodName, cmd)
+	if err != nil {
+		return "", fmt.Errorf("host command failed: %v\n%s", err, output)
+	}
+	return output, nil
+}
+
+func findEnvVarSecret(vars []corev1.EnvVar, key string) (string, string) {
+	for _, v := range vars {
+		if v.Name == key {
+			if v.ValueFrom != nil && v.ValueFrom.SecretKeyRef != nil {
+				ref := v.ValueFrom.SecretKeyRef
+				return ref.Name, ref.Key
+			}
+		}
+	}
+	return "", ""
+}
+
+func findStatsUsernameAndPassword(oc *exutil.CLI, ns string, env []corev1.EnvVar) (string, string, error) {
+	secretName, userKey := findEnvVarSecret(env, "STATS_USERNAME")
+	_, passKey := findEnvVarSecret(env, "STATS_PASSWORD")
+
+	if len(secretName) == 0 || len(userKey) == 0 || len(passKey) == 0 {
+		return "", "", fmt.Errorf("stats username and password not found, env: %v", env)
+	}
+
+	secret, err := oc.AdminKubeClient().Core().Secrets(ns).Get(secretName, metav1.GetOptions{})
+	if err != nil {
+		return "", "", err
+	}
+
+	username, ok1 := secret.Data[userKey]
+	password, ok2 := secret.Data[passKey]
+	if !ok1 || !ok2 {
+		return "", "", fmt.Errorf("secret '%s/%s' does not contain key %q or %q", ns, secretName, userKey, passKey)
+	}
+	return string(username), string(password), nil
+}
+
+func findMetricsBearerToken(oc *exutil.CLI) (string, error) {
+	sa, err := oc.AdminKubeClient().Core().ServiceAccounts("openshift-monitoring").Get("prometheus-k8s", metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	var secretName string
+	for _, secret := range sa.Secrets {
+		if strings.Contains(secret.Name, "prometheus-k8s-token") {
+			secretName = secret.Name
+			break
+		}
+	}
+	if len(secretName) == 0 {
+		return "", fmt.Errorf("serviceaccount 'openshift-monitoring/prometheus-k8s' does not contain 'prometheus-k8s-token' secret")
+	}
+
+	secret, err := oc.AdminKubeClient().Core().Secrets("openshift-monitoring").Get(secretName, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+	token, ok := secret.Data["token"]
+	if !ok {
+		return "", fmt.Errorf("secret 'openshift-monitoring/%s' does not contain bearer token", secretName)
+	}
+	return string(token), nil
 }
