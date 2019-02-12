7cbc34cde95ce676923fcb0ffc535de7e6685c19
Derek Carr <decarr@redhat.com>
2019-02-12 02:53:29 -0500

    Add smoke test for machine-api

--- test/extended/include.go
+++ test/extended/include.go
@@ -37,6 +37,7 @@ import (
 	_ "github.com/openshift/origin/test/extended/images/trigger"
 	_ "github.com/openshift/origin/test/extended/jobs"
 	_ "github.com/openshift/origin/test/extended/localquota"
+	_ "github.com/openshift/origin/test/extended/machines"
 	_ "github.com/openshift/origin/test/extended/networking"
 	_ "github.com/openshift/origin/test/extended/operators"
 	_ "github.com/openshift/origin/test/extended/prometheus"
