ba0484ab3ad51f6f2b24abec8de33d5e263cca66
Maciej Szulik <maszulik@redhat.com>
2019-02-14 16:21:18 +0100

    UPSTREAM: 74067: apiextensions-apiserver: make API helpers reusable

--- vendor/k8s.io/kubernetes/staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/helpers.go
+++ vendor/k8s.io/kubernetes/staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/helpers.go
@@ -23,6 +23,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+var swaggerMetadataDescriptions = metav1.ObjectMeta{}.SwaggerDoc()
+
 // SetCRDCondition sets the status condition.  It either overwrites the existing one or
 // creates a new one
 func SetCRDCondition(crd *CustomResourceDefinition, newCondition CustomResourceDefinitionCondition) {
@@ -144,6 +146,111 @@ func IsStoredVersion(crd *CustomResourceDefinition, version string) bool {
 		if version == v {
 			return true
 		}
+	}
+	return false
+}
+
+// GetSchemaForVersion returns the validation schema for given version in given CRD.
+func GetSchemaForVersion(crd *CustomResourceDefinition, version string) (*CustomResourceValidation, error) {
+	if !HasPerVersionSchema(crd.Spec.Versions) {
+		return crd.Spec.Validation, nil
+	}
+	if crd.Spec.Validation != nil {
+		return nil, fmt.Errorf("malformed CustomResourceDefinition %s version %s: top-level and per-version schemas must be mutual exclusive", crd.Name, version)
+	}
+	// for _, v := range crd.Spec.Versions {
+	// 	if version == v.Name {
+	// 		return v.Schema, nil
+	// 	}
+	// }
+	return nil, fmt.Errorf("version %s not found in CustomResourceDefinition: %v", version, crd.Name)
+}
+
+// GetSubresourcesForVersion returns the subresources for given version in given CRD.
+func GetSubresourcesForVersion(crd *CustomResourceDefinition, version string) (*CustomResourceSubresources, error) {
+	if !HasPerVersionSubresources(crd.Spec.Versions) {
+		return crd.Spec.Subresources, nil
+	}
+	if crd.Spec.Subresources != nil {
+		return nil, fmt.Errorf("malformed CustomResourceDefinition %s version %s: top-level and per-version subresources must be mutual exclusive", crd.Name, version)
+	}
+	// for _, v := range crd.Spec.Versions {
+	// 	if version == v.Name {
+	// 		return v.Subresources, nil
+	// 	}
+	// }
+	return nil, fmt.Errorf("version %s not found in CustomResourceDefinition: %v", version, crd.Name)
+}
+
+// GetColumnsForVersion returns the columns for given version in given CRD.
+// NOTE: the newly logically-defaulted columns is not pointing to the original CRD object.
+// One cannot mutate the original CRD columns using the logically-defaulted columns. Please iterate through
+// the original CRD object instead.
+func GetColumnsForVersion(crd *CustomResourceDefinition, version string) ([]CustomResourceColumnDefinition, error) {
+	if !HasPerVersionColumns(crd.Spec.Versions) {
+		return serveDefaultColumnsIfEmpty(crd.Spec.AdditionalPrinterColumns), nil
+	}
+	if len(crd.Spec.AdditionalPrinterColumns) > 0 {
+		return nil, fmt.Errorf("malformed CustomResourceDefinition %s version %s: top-level and per-version additionalPrinterColumns must be mutual exclusive", crd.Name, version)
+	}
+	// for _, v := range crd.Spec.Versions {
+	// 	if version == v.Name {
+	// 		return serveDefaultColumnsIfEmpty(v.AdditionalPrinterColumns), nil
+	// 	}
+	// }
+	return nil, fmt.Errorf("version %s not found in CustomResourceDefinition: %v", version, crd.Name)
+}
+
+// HasPerVersionSchema returns true if a CRD uses per-version schema.
+func HasPerVersionSchema(versions []CustomResourceDefinitionVersion) bool {
+	// for _, v := range versions {
+	// 	if v.Schema != nil {
+	// 		return true
+	// 	}
+	// }
+	return false
+}
+
+// HasPerVersionSubresources returns true if a CRD uses per-version subresources.
+func HasPerVersionSubresources(versions []CustomResourceDefinitionVersion) bool {
+	// for _, v := range versions {
+	// 	if v.Subresources != nil {
+	// 		return true
+	// 	}
+	// }
+	return false
+}
+
+// HasPerVersionColumns returns true if a CRD uses per-version columns.
+func HasPerVersionColumns(versions []CustomResourceDefinitionVersion) bool {
+	// for _, v := range versions {
+	// 	if len(v.AdditionalPrinterColumns) > 0 {
+	// 		return true
+	// 	}
+	// }
+	return false
+}
+
+// serveDefaultColumnsIfEmpty applies logically defaulting to columns, if the input columns is empty.
+// NOTE: in this way, the newly logically-defaulted columns is not pointing to the original CRD object.
+// One cannot mutate the original CRD columns using the logically-defaulted columns. Please iterate through
+// the original CRD object instead.
+func serveDefaultColumnsIfEmpty(columns []CustomResourceColumnDefinition) []CustomResourceColumnDefinition {
+	if len(columns) > 0 {
+		return columns
+	}
+	return []CustomResourceColumnDefinition{
+		{Name: "Age", Type: "date", Description: swaggerMetadataDescriptions["creationTimestamp"], JSONPath: ".metadata.creationTimestamp"},
+	}
+}
+
+// HasVersionServed returns true if given CRD has given version served
+func HasVersionServed(crd *CustomResourceDefinition, version string) bool {
+	for _, v := range crd.Spec.Versions {
+		if !v.Served || v.Name != version {
+			continue
+		}
+		return true
 	}
 	return false
 }
