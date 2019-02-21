bc877b833d5736a87d0f04e216bf63f81b94a906
Tomas Nozicka <tnozicka@redhat.com>
2019-02-21 21:18:59 +0100

    Add generic manifest reader for extended tests

---
+++ test/extended/util/manifests.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	o "github.com/onsi/gomega"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/kubernetes/pkg/kubectl/scheme"
+)
+
+func ReadFixture(path string) (runtime.Object, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %q: %v", path, err)
+	}
+
+	obj, _, err := scheme.Codecs.UniversalDeserializer().Decode(data, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
+
+func ReadFixtureOrFail(path string) runtime.Object {
+	obj, err := ReadFixture(path)
+
+	o.Expect(err).NotTo(o.HaveOccurred())
+
+	return obj
+}
