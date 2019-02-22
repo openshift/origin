8ff0a1efd06645c9ee8a236a532d62768e240290
Dan Mace <ironcladlou@gmail.com>
2019-02-22 17:42:33 -0500

    Temporarily mark the router prometheus metrics test as flaky
    A fix will be forthcoming.

--- test/extended/router/metrics.go
+++ test/extended/router/metrics.go
@@ -94,7 +94,7 @@ var _ = g.Describe("[Conformance][Area:Networking][Feature:Router]", func() {
 			o.Expect(err).NotTo(o.HaveOccurred())
 		})
 
-		g.It("should expose prometheus metrics for a route", func() {
+		g.It("[Flaky] should expose prometheus metrics for a route", func() {
 			g.By("when a route exists")
 			configPath := exutil.FixturePath("testdata", "router-metrics.yaml")
 			err := oc.Run("create").Args("-f", configPath).Execute()
