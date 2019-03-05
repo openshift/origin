1a2497070c4237973ac944348584e2b9817e258a
Adam Kaplan <adam.kaplan@redhat.com>
2019-03-05 11:43:34 -0500

    handle api server bounce/rollout

--- test/extended/util/cvo.go
+++ test/extended/util/cvo.go
@@ -1,12 +1,12 @@
 package util
 
 import (
-	"fmt"
 	"time"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
 
 	configv1 "github.com/openshift/api/config/v1"
 	configclientv1 "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
@@ -28,10 +28,12 @@ func WaitForClusterProgression(cv configclientv1.ClusterOperatorsGetter) error {
 			success = 0
 			// Transient client-side timeout
 			if errors.IsTimeout(err) {
+				e2e.Logf("Timeout listing cluster operators")
 				return false, nil
 			}
 			// Wait if underlying service is unavailable or times out on server side - indicates apiserver churn
 			if errors.IsServiceUnavailable(err) || errors.IsServerTimeout(err) {
+				e2e.Logf("API servers are unavailable or timing out.")
 				return false, nil
 			}
 			return false, err
@@ -41,20 +43,23 @@ func WaitForClusterProgression(cv configclientv1.ClusterOperatorsGetter) error {
 				switch status.Type {
 				case configv1.OperatorFailing:
 					if status.Status == configv1.ConditionTrue {
-						// Operator is failing - abort with error
+						// Operator is failing - can happen if API servers crash on MCO rollout
 						success = 0
-						return false, fmt.Errorf("operator %s is failing due to %s: %s", op.Name, status.Reason, status.Message)
+						e2e.Logf("Operator %s is failing due to %s: %s", op.Name, status.Reason, status.Message)
+						return false, nil
 					}
 				case configv1.OperatorAvailable:
 					if status.Status == configv1.ConditionFalse {
 						// Operator is not available - not ready
 						success = 0
+						e2e.Logf("Waiting for operator %s to become available", op.Name)
 						return false, nil
 					}
 				case configv1.OperatorProgressing:
 					if status.Status == configv1.ConditionTrue {
 						// Operator is progressing - not ready
 						success = 0
+						e2e.Logf("Waiting for operator %s to finish progress on %s: %s", op.Name, status.Reason, status.Message)
 						return false, nil
 					}
 				}
