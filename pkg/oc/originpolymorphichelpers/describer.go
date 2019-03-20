0636135420a0c9d8a65001d1ec9bcf67c137d0e1
David Eads <deads@redhat.com>
2019-03-20 10:04:39 -0400

    update kubectl describers for external

--- pkg/oc/originpolymorphichelpers/describer.go
+++ pkg/oc/originpolymorphichelpers/describer.go
@@ -5,15 +5,14 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	kinternalclient "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	kcmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
-	"k8s.io/kubernetes/pkg/printers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/pkg/kubectl/describe"
 
-	"github.com/openshift/origin/pkg/oc/lib/describe"
+	odescribe "github.com/openshift/origin/pkg/oc/lib/describe"
 )
 
-func NewDescriberFn(delegate kcmdutil.DescriberFunc) kcmdutil.DescriberFunc {
-	return func(restClientGetter genericclioptions.RESTClientGetter, mapping *meta.RESTMapping) (printers.Describer, error) {
+func NewDescriberFn(delegate describe.DescriberFunc) describe.DescriberFunc {
+	return func(restClientGetter genericclioptions.RESTClientGetter, mapping *meta.RESTMapping) (describe.Describer, error) {
 
 		// TODO we need to refactor the describer logic to handle misses or run serverside.
 		// for now we can special case our "sometimes origin, sometimes kube" resource
@@ -23,11 +22,11 @@ func NewDescriberFn(delegate kcmdutil.DescriberFunc) kcmdutil.DescriberFunc {
 		if err != nil {
 			return nil, fmt.Errorf("unable to create client config %s: %v", mapping.GroupVersionKind.Kind, err)
 		}
-		kClient, err := kinternalclient.NewForConfig(clientConfig)
+		kubeClient, err := kubernetes.NewForConfig(clientConfig)
 		if err != nil {
 			return nil, fmt.Errorf("unable to create client %s: %v", mapping.GroupVersionKind.Kind, err)
 		}
-		describer, ok := describe.DescriberFor(mapping.GroupVersionKind.GroupKind(), clientConfig, kClient, clientConfig.Host)
+		describer, ok := odescribe.DescriberFor(mapping.GroupVersionKind.GroupKind(), clientConfig, kubeClient, clientConfig.Host)
 		if ok {
 			return describer, nil
 		}
