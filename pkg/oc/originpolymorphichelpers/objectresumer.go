75fbf58d9b00e072f8b190e707011a038de922be
David Eads <deads@redhat.com>
2019-03-22 11:36:38 -0400

    kubectl: switch dc to external

--- pkg/oc/originpolymorphichelpers/objectresumer.go
+++ pkg/oc/originpolymorphichelpers/objectresumer.go
@@ -8,26 +8,17 @@ import (
 	"k8s.io/kubernetes/pkg/kubectl/scheme"
 
 	appsv1 "github.com/openshift/api/apps/v1"
-	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
 )
 
 func NewObjectResumerFn(delegate polymorphichelpers.ObjectResumerFunc) polymorphichelpers.ObjectResumerFunc {
 	return func(obj runtime.Object) ([]byte, error) {
 		switch t := obj.(type) {
-		case *appsapi.DeploymentConfig:
-			if !t.Spec.Paused {
-				return nil, errors.New("is not paused")
-			}
-			t.Spec.Paused = false
-			// TODO: Resume the deployer containers.
-			return runtime.Encode(internalVersionJSONEncoder(), obj)
-
 		case *appsv1.DeploymentConfig:
 			if !t.Spec.Paused {
 				return nil, errors.New("is not paused")
 			}
 			t.Spec.Paused = false
-			return runtime.Encode(scheme.Codecs.LegacyCodec(appsv1.SchemeGroupVersion), obj)
+			return runtime.Encode(scheme.DefaultJSONEncoder(), obj)
 
 		default:
 			return delegate(obj)
