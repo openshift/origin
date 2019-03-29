f2957f8b5a3e51ce133c4197ea9918a9ee0e7d65
Mangirdas <mangirdas@judeikis.lt>
2019-03-29 13:57:15 +0000

    UPSTREAM: 70002: Cherry-picked

--- vendor/k8s.io/kubernetes/pkg/volume/azure_dd/azure_common_test.go
+++ vendor/k8s.io/kubernetes/pkg/volume/azure_dd/azure_common_test.go
@@ -24,6 +24,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"k8s.io/kubernetes/pkg/util/mount"
 )
 
@@ -132,5 +133,60 @@ func TestIoHandler(t *testing.T) {
 		if disk != "/dev/"+devName || err != nil {
 			t.Errorf("no data disk found: disk %v err %v", disk, err)
 		}
+	}
+}
+
+func TestGetDiskLUN(t *testing.T) {
+	tests := []struct {
+		deviceInfo  string
+		expectedLUN int32
+		expectError bool
+	}{
+		{
+			deviceInfo:  "0",
+			expectedLUN: 0,
+			expectError: false,
+		},
+		{
+			deviceInfo:  "10",
+			expectedLUN: 10,
+			expectError: false,
+		},
+		{
+			deviceInfo:  "11d",
+			expectedLUN: -1,
+			expectError: true,
+		},
+		{
+			deviceInfo:  "999",
+			expectedLUN: -1,
+			expectError: true,
+		},
+		{
+			deviceInfo:  "",
+			expectedLUN: -1,
+			expectError: true,
+		},
+		{
+			deviceInfo:  "/dev/disk/azure/scsi1/lun2",
+			expectedLUN: 2,
+			expectError: false,
+		},
+		{
+			deviceInfo:  "/dev/disk/azure/scsi0/lun12",
+			expectedLUN: 12,
+			expectError: false,
+		},
+		{
+			deviceInfo:  "/dev/disk/by-id/scsi1/lun2",
+			expectedLUN: -1,
+			expectError: true,
+		},
+	}
+
+	for _, test := range tests {
+		result, err := getDiskLUN(test.deviceInfo)
+		assert.Equal(t, result, test.expectedLUN)
+		assert.Equal(t, err != nil, test.expectError, fmt.Sprintf("error msg: %v", err))
 	}
 }
