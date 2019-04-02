394a3c3cfff3b04e872f57642b3fdf49641648d6
paulfantom <pawel@krupa.net.pl>
2019-04-02 14:26:00 +0200

    e2e: test more prometheus scrape targets

--- test/extended/prometheus/prometheus.go
+++ test/extended/prometheus/prometheus.go
@@ -138,6 +138,10 @@ var _ = g.Describe("[Feature:Prometheus][Conformance] Prometheus", func() {
 					targets.Expect(labels{"job": "kubelet"}, "up", "^https://.*/metrics$"),
 					targets.Expect(labels{"job": "kubelet"}, "up", "^https://.*/metrics/cadvisor$"),
 					targets.Expect(labels{"job": "node-exporter"}, "up", "^https://.*/metrics$"),
+					targets.Expect(labels{"job": "prometheus-operator"}, "up", "^http://.*/metrics$"),
+					targets.Expect(labels{"job": "alertmanager-main"}, "up", "^https://.*/metrics$"),
+					targets.Expect(labels{"job": "crio"}, "up", "^http://.*/metrics$"),
+					targets.Expect(labels{"job": "telemeter-client"}, "up", "^https://.*/metrics$"),
 				)
 				if len(lastErrs) > 0 {
 					e2e.Logf("missing some targets: %v", lastErrs)
