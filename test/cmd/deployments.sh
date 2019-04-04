c61c9eb73547027cf2e0a20895b3038c801748e5
David Eads <deads@redhat.com>
2019-04-04 12:24:54 -0400

    kubectl: wait for deletion of dependants

--- test/cmd/deployments.sh
+++ test/cmd/deployments.sh
@@ -135,7 +135,7 @@ os::cmd::expect_success 'oc delete svc/fromdc'
 os::cmd::expect_failure_and_text 'oc stop dc/database' 'delete'
 os::cmd::expect_success 'oc delete dc/database'
 os::cmd::expect_failure 'oc get dc/database'
-os::cmd::expect_failure 'oc get rc/database-1'
+os::cmd::try_until_failure 'oc get rc/database-1'
 echo "stop: ok"
 os::test::junit::declare_suite_end
 
