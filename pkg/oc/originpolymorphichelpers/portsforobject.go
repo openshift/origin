e49116eaf430a96d399f8e7d7a8620c10f274bdc
Maciej Szulik <maszulik@redhat.com>
2019-04-05 21:39:37 +0200

    Cleanup origin polymorphichelpers from internal types

--- pkg/oc/originpolymorphichelpers/portsforobject.go
+++ pkg/oc/originpolymorphichelpers/portsforobject.go
@@ -5,18 +5,14 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/kubernetes/pkg/kubectl/polymorphichelpers"
 
 	appsv1 "github.com/openshift/api/apps/v1"
-	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
 )
 
 func NewPortsForObjectFn(delegate polymorphichelpers.PortsForObjectFunc) polymorphichelpers.PortsForObjectFunc {
 	return func(object runtime.Object) ([]string, error) {
 		switch t := object.(type) {
-		case *appsapi.DeploymentConfig:
-			return getPortsInternal(t.Spec.Template.Spec), nil
 		case *appsv1.DeploymentConfig:
 			return getPorts(t.Spec.Template.Spec), nil
 
@@ -27,16 +23,6 @@ func NewPortsForObjectFn(delegate polymorphichelpers.PortsForObjectFunc) polymor
 }
 
 func getPorts(spec corev1.PodSpec) []string {
-	result := []string{}
-	for _, container := range spec.Containers {
-		for _, port := range container.Ports {
-			result = append(result, strconv.Itoa(int(port.ContainerPort)))
-		}
-	}
-	return result
-}
-
-func getPortsInternal(spec core.PodSpec) []string {
 	result := []string{}
 	for _, container := range spec.Containers {
 		for _, port := range container.Ports {
--- pkg/oc/originpolymorphichelpers/protocolsforobject.go
+++ pkg/oc/originpolymorphichelpers/protocolsforobject.go
@@ -5,18 +5,14 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/kubernetes/pkg/kubectl/polymorphichelpers"
 
 	appsv1 "github.com/openshift/api/apps/v1"
-	appsapi "github.com/openshift/origin/pkg/apps/apis/apps"
 )
 
 func NewProtocolsForObjectFn(delegate polymorphichelpers.ProtocolsForObjectFunc) polymorphichelpers.ProtocolsForObjectFunc {
 	return func(object runtime.Object) (map[string]string, error) {
 		switch t := object.(type) {
-		case *appsapi.DeploymentConfig:
-			return getProtocolsInternal(t.Spec.Template.Spec), nil
 		case *appsv1.DeploymentConfig:
 			return getProtocols(t.Spec.Template.Spec), nil
 
@@ -27,16 +23,6 @@ func NewProtocolsForObjectFn(delegate polymorphichelpers.ProtocolsForObjectFunc)
 }
 
 func getProtocols(spec corev1.PodSpec) map[string]string {
-	result := make(map[string]string)
-	for _, container := range spec.Containers {
-		for _, port := range container.Ports {
-			result[strconv.Itoa(int(port.ContainerPort))] = string(port.Protocol)
-		}
-	}
-	return result
-}
-
-func getProtocolsInternal(spec core.PodSpec) map[string]string {
 	result := make(map[string]string)
 	for _, container := range spec.Containers {
 		for _, port := range container.Ports {
