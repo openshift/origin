2696fe80a3b7b947e17a816871c11ead61122860
Michal Fojtik <mfojtik@redhat.com>
2019-05-02 19:05:46 +0200

    whoami: add --show-console option to reveal the web console URL to CLI users

--- pkg/oc/cli/whoami/whoami.go
+++ pkg/oc/cli/whoami/whoami.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd/api"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd/api"
 	kcmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 	"k8s.io/kubernetes/pkg/kubectl/util/templates"
 
@@ -16,7 +18,11 @@ import (
 	userv1typedclient "github.com/openshift/client-go/user/clientset/versioned/typed/user/v1"
 )
 
-const WhoAmIRecommendedCommandName = "whoami"
+const (
+	WhoAmIRecommendedCommandName        = "whoami"
+	openShiftConfigManagedNamespaceName = "openshift-config-managed"
+	consolePublicConfigMap              = "console-public"
+)
 
 var whoamiLong = templates.LongDesc(`
 	Show information about the current session
@@ -29,11 +35,13 @@ type WhoAmIOptions struct {
 	UserInterface userv1typedclient.UserV1Interface
 
 	ClientConfig *rest.Config
+	KubeClient   kubernetes.Interface
 	RawConfig    api.Config
 
-	ShowToken   bool
-	ShowContext bool
-	ShowServer  bool
+	ShowToken      bool
+	ShowContext    bool
+	ShowServer     bool
+	ShowConsoleUrl bool
 
 	genericclioptions.IOStreams
 }
@@ -61,6 +69,7 @@ func NewCmdWhoAmI(name, fullName string, f kcmdutil.Factory, streams genericclio
 	cmd.Flags().BoolVarP(&o.ShowToken, "show-token", "t", o.ShowToken, "Print the token the current session is using. This will return an error if you are using a different form of authentication.")
 	cmd.Flags().BoolVarP(&o.ShowContext, "show-context", "c", o.ShowContext, "Print the current user context name")
 	cmd.Flags().BoolVar(&o.ShowServer, "show-server", o.ShowServer, "If true, print the current server's REST API URL")
+	cmd.Flags().BoolVar(&o.ShowConsoleUrl, "show-console", o.ShowConsoleUrl, "If true, print the current server's web console URL")
 
 	return cmd
 }
@@ -76,10 +85,17 @@ func (o WhoAmIOptions) WhoAmI() (*userv1.User, error) {
 
 func (o *WhoAmIOptions) Complete(f kcmdutil.Factory) error {
 	var err error
+
 	o.ClientConfig, err = f.ToRESTConfig()
 	if err != nil {
 		return err
 	}
+
+	kubeClient, err := kubernetes.NewForConfig(o.ClientConfig)
+	if err != nil {
+		return err
+	}
+	o.KubeClient = kubeClient
 
 	o.RawConfig, err = f.ToRawKubeConfigLoader().RawConfig()
 	return err
@@ -96,17 +112,40 @@ func (o *WhoAmIOptions) Validate() error {
 	return nil
 }
 
+func (o *WhoAmIOptions) getWebConsoleUrl() (string, error) {
+	consolePublicConfig, err := o.KubeClient.CoreV1().ConfigMaps(openShiftConfigManagedNamespaceName).Get(consolePublicConfigMap, metav1.GetOptions{})
+	// This means the command was run against 3.x server
+	if errors.IsNotFound(err) {
+		return o.ClientConfig.Host, nil
+	}
+	if err != nil {
+		return "", fmt.Errorf("unable to determine console location: %v", err)
+	}
+
+	consoleUrl, exists := consolePublicConfig.Data["consoleURL"]
+	if !exists {
+		return "", fmt.Errorf("unable to determine console location from the cluster")
+	}
+	return consoleUrl, nil
+}
+
 func (o *WhoAmIOptions) Run() error {
-	if o.ShowToken {
+	switch {
+	case o.ShowToken:
 		fmt.Fprintf(o.Out, "%s\n", o.ClientConfig.BearerToken)
 		return nil
-	}
-	if o.ShowContext {
+	case o.ShowContext:
 		fmt.Fprintf(o.Out, "%s\n", o.RawConfig.CurrentContext)
 		return nil
-	}
-	if o.ShowServer {
+	case o.ShowServer:
 		fmt.Fprintf(o.Out, "%s\n", o.ClientConfig.Host)
+		return nil
+	case o.ShowConsoleUrl:
+		consoleUrl, err := o.getWebConsoleUrl()
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(o.Out, "%s\n", consoleUrl)
 		return nil
 	}
 
