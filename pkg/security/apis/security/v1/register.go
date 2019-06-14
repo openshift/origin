60ad8b2ff18aadd221bc223fcd931a8e6153aeba
Mike Dame <mdame@redhat.com>
2019-06-14 14:23:21 -0400

    Change SCC admission to directly call defaulting method

--- pkg/security/apis/security/v1/register.go
+++ pkg/security/apis/security/v1/register.go
@@ -4,14 +4,14 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	corev1conversions "k8s.io/kubernetes/pkg/apis/core/v1"
 
-	"github.com/openshift/api/security/v1"
+	securityv1 "github.com/openshift/api/security/v1"
 	"github.com/openshift/origin/pkg/security/apis/security"
 )
 
 var (
 	localSchemeBuilder = runtime.NewSchemeBuilder(
 		security.Install,
-		v1.Install,
+		securityv1.Install,
 		corev1conversions.AddToScheme,
 
 		AddConversionFuncs,
