059dddb3dccf308399db27f66fcb0867c031ccd5
David Eads <deads@redhat.com>
2019-06-18 11:34:44 -0400

    seems to work ok....

--- pkg/security/apis/security/v1/defaults.go
+++ pkg/security/apis/security/v1/defaults.go
@@ -1,18 +1,16 @@
 package v1
 
 import (
-	//v1 "github.com/openshift/api/security/v1"
-	//"github.com/openshift/origin/pkg/cmd/openshift-kube-apiserver/admission/customresourcevalidation/securitycontextconstraints"
+	v1 "github.com/openshift/api/security/v1"
+	"github.com/openshift/origin/pkg/cmd/openshift-kube-apiserver/admission/customresourcevalidation/securitycontextconstraints"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func AddDefaultingFuncs(scheme *runtime.Scheme) error {
 	RegisterDefaults(scheme)
-	/*
-		scheme.AddTypeDefaultingFunc(&v1.SecurityContextConstraints{}, func(obj interface{}) {
-			securitycontextconstraints.SetDefaults_SCC(obj.(*v1.SecurityContextConstraints))
-		})
-	*/
+	scheme.AddTypeDefaultingFunc(&v1.SecurityContextConstraints{}, func(obj interface{}) {
+		securitycontextconstraints.SetDefaults_SCC(obj.(*v1.SecurityContextConstraints))
+	})
 
 	return nil
 }
