8e45f61c37eb45dabe790348591a07d776fefc96
David Eads <deads@redhat.com>
2019-06-21 16:05:18 -0400

    use the side-effecty global flags from upstream for tests

--- test/extended/util/test.go
+++ test/extended/util/test.go
@@ -27,9 +27,13 @@ import (
 	rbacv1client "k8s.io/client-go/kubernetes/typed/rbac/v1"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/retry"
+	reale2e "k8s.io/kubernetes/test/e2e"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 	"k8s.io/kubernetes/test/e2e/framework/testfiles"
 	"k8s.io/kubernetes/test/e2e/generated"
+
+	// this appears to inexplicably auto-register global flags.
+	_ "k8s.io/kubernetes/test/e2e/storage/drivers"
 
 	securityv1client "github.com/openshift/client-go/security/clientset/versioned"
 	"github.com/openshift/oc/pkg/cli/admin/policy"
@@ -52,14 +56,17 @@ var TestContext *e2e.TestContextType = &e2e.TestContext
 // TEST_REPORT_FILE_NAME - If set, will determine the name of the file that JUnit output is written to
 func Init() {
 	flag.StringVar(&syntheticSuite, "suite", "", "DEPRECATED: Optional suite selector to filter which tests are run. Use focus.")
-	e2e.ViperizeFlags()
+	reale2e.ViperizeFlags(reale2e.GetViperConfig())
+	e2e.HandleFlags()
 	InitTest()
 }
 
 func InitStandardFlags() {
 	e2e.RegisterCommonFlags()
 	e2e.RegisterClusterFlags()
-	e2e.RegisterStorageFlags()
+
+	// replaced by a bare import above.
+	//e2e.RegisterStorageFlags()
 }
 
 func InitTest() {
