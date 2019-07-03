69568bca54f47647ed28b223ffd0a6dbb78fd846
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-07-03 11:58:30 +0200

    test/extended/templates: fix imports

--- test/extended/templates/templateinstance_security.go
+++ test/extended/templates/templateinstance_security.go
@@ -6,10 +6,10 @@ import (
 
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
-	"k8s.io/kubernetes/staging/src/k8s.io/apimachinery/pkg/api/apitesting"
 
 	corev1 "k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
+	"k8s.io/apimachinery/pkg/api/apitesting"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
