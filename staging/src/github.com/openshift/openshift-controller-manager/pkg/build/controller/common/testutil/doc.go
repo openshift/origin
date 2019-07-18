b1e6e44af3374f389287a3db5728d550f1111bd8
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-07-18 12:46:23 +0200

    staging: remove openshift-controller-manager

--- staging/src/github.com/openshift/openshift-controller-manager/pkg/build/controller/common/testutil/doc.go
+++
@@ -1,8 +0,0 @@
-/*
-Package testutil contains utilities to test admission control plugins.
-
-It contains a TestPod and TestBuild class that allow quickly building mock objects
-for tests
-*/
-
-package test
