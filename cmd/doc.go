82e405deded5aa7326d1eecb1e6e5bf141e2dd16
David Eads <deads@redhat.com>
2019-07-25 19:08:36 -0400

    add back oc until the RPM is dead

--- cmd/doc.go
+++ cmd/doc.go
@@ -1,2 +1,17 @@
 // Package cmd contains the executables for OpenShift 3.
 package cmd
+
+import (
+	// for hyperkube
+	_ "k8s.io/kubernetes/cmd/cloud-controller-manager/app"
+	_ "k8s.io/kubernetes/cmd/kube-apiserver/app"
+	_ "k8s.io/kubernetes/cmd/kube-controller-manager/app"
+	_ "k8s.io/kubernetes/cmd/kube-proxy/app"
+	_ "k8s.io/kubernetes/cmd/kube-scheduler/app"
+	_ "k8s.io/kubernetes/cmd/kubelet/app"
+	_ "k8s.io/kubernetes/pkg/client/metrics/prometheus" // for client metric registration
+	_ "k8s.io/kubernetes/pkg/kubectl/cmd"
+	_ "k8s.io/kubernetes/pkg/version/prometheus" // for version metric registration
+
+	_ "github.com/openshift/oc/pkg/cli"
+)
