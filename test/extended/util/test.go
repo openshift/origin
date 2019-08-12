52b5b5fcf8cdf01501cc34c9e7327d57fcc7b58b
Jan Safranek <jsafrane@redhat.com>
2019-08-12 15:29:52 +0200

    Remove Ceph RBD rule
    It does not match any test. RBD tests are matched by [Driver: rbd]
    below.

--- test/extended/util/test.go
+++ test/extended/util/test.go
@@ -365,7 +365,6 @@ var (
 			`DNS configMap`, // this tests dns federation configuration via configmap, which we don't support yet
 
 			// See the CanSupport implementation in upstream to determine wether these work.
-			`Ceph RBD`,                              // Works if ceph-common Binary installed (but we can't guarantee this on all clusters).
 			`GlusterFS`,                             // May work if /sbin/mount.glusterfs to be installed for plugin to work (also possibly blocked by serial pulling)
 			`authentication: OpenLDAP`,              // needs separate setup and bucketing for openldap bootstrapping
 			`NodeProblemDetector`,                   // requires a non-master node to run on
