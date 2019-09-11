d2ff7bac4ab8a629fd0a1338fc59d1efa8d7c4a3
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2019-09-11 12:35:04 +0200

    inrease timeout for hpa e2e tests

--- vendor/k8s.io/kubernetes/test/e2e/autoscaling/horizontal_pod_autoscaling.go
+++ vendor/k8s.io/kubernetes/test/e2e/autoscaling/horizontal_pod_autoscaling.go
@@ -115,7 +115,7 @@ type HPAScaleTest struct {
 // The second state change (optional) is due to the CPU burst parameter, which HPA again responds to.
 // TODO The use of 3 states is arbitrary, we could eventually make this test handle "n" states once this test stabilizes.
 func (scaleTest *HPAScaleTest) run(name string, kind schema.GroupVersionKind, rc *common.ResourceConsumer, f *framework.Framework) {
-	const timeToWait = 15 * time.Minute
+	const timeToWait = 45 * time.Minute
 	rc = common.NewDynamicResourceConsumer(name, f.Namespace.Name, kind, scaleTest.initPods, scaleTest.totalInitialCPUUsage, 0, 0, scaleTest.perPodCPURequest, 200, f.ClientSet, f.ScalesGetter)
 	defer rc.CleanUp()
 	hpa := common.CreateCPUHorizontalPodAutoscaler(rc, scaleTest.targetCPUUtilizationPercent, scaleTest.minPods, scaleTest.maxPods)
