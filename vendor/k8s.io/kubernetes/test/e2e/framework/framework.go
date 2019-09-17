2e88026f632a4515ab9e5fa128e0c8c0992a9dcc
Monis Khan <mkhan@redhat.com>
2019-09-16 20:58:43 -0400

    UPSTREAM: <carry>: tolerate slow pod delete on GCP
    Namespace clean up on GCP takes a long time because pods take a long
    time to reap:
    
    namespace_controller.go:148] unexpected items still remain in namespace: e2e-svcaccounts-X for gvr: /v1, Resource=pods
    
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- vendor/k8s.io/kubernetes/test/e2e/framework/framework.go
+++ vendor/k8s.io/kubernetes/test/e2e/framework/framework.go
@@ -59,7 +59,7 @@ const (
 	maxKubectlExecRetries = 5
 	// TODO(mikedanese): reset this to 5 minutes once #47135 is resolved.
 	// ref https://github.com/kubernetes/kubernetes/issues/47135
-	DefaultNamespaceDeletionTimeout = 10 * time.Minute
+	DefaultNamespaceDeletionTimeout = 20 * time.Minute
 )
 
 // Framework supports common operations used by e2e tests; it will keep a client & a namespace for you.
