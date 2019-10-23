a1cc4eac6ce29fea597ea9982a5e4365a6b946a3
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2019-10-23 21:26:20 +0200

    bump(*)

--- vendor/github.com/gonum/matrix/cmat128/doc.go
+++ vendor/github.com/gonum/matrix/cmat128/doc.go
@@ -6,6 +6,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// This repository is no longer maintained.
+// Development has moved to https://github.com/gonum/gonum.
+//
 // Package cmat128 provides implementations of complex128 matrix structures and
 // linear algebra operations on them.
 //
