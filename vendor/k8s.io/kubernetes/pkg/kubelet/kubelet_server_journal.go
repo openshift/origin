d2d8b616899a13fb12addd3879eb9d3d70385eb1
Maciej Szulik <maszulik@redhat.com>
2019-11-07 16:24:39 +0100

    UPSTREAM: <carry>: Expose a simple journald shim on the kubelet logs endpoint

--- vendor/k8s.io/kubernetes/pkg/kubelet/kubelet_server_journal.go
+++ vendor/k8s.io/kubernetes/pkg/kubelet/kubelet_server_journal.go
@@ -241,7 +241,7 @@ var (
 )
 
 const (
-	dateFormat         = `"2006-01-02T15:04:05.999999Z`
+	dateFormat         = `2006-01-02T15:04:05.999999Z`
 	maxParameterLength = 100
 	maxTotalLength     = 1000
 )
