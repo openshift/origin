ff885f256566a93dc2e42cc40b34bb9a9ca0ffa8
Fabio Bertinatto <fbertina@redhat.com>
2019-11-07 10:22:18 +0100

    UPSTREAM: 83747: Improve efficiency of csiMountMgr.GetAttributes

--- vendor/k8s.io/kubernetes/pkg/volume/csi/csi_mounter.go
+++ vendor/k8s.io/kubernetes/pkg/volume/csi/csi_mounter.go
@@ -56,18 +56,19 @@ var (
 
 type csiMountMgr struct {
 	csiClientGetter
-	k8s            kubernetes.Interface
-	plugin         *csiPlugin
-	driverName     csiDriverName
-	driverMode     driverMode
-	volumeID       string
-	specVolumeID   string
-	readOnly       bool
-	spec           *volume.Spec
-	pod            *api.Pod
-	podUID         types.UID
-	options        volume.VolumeOptions
-	publishContext map[string]string
+	k8s             kubernetes.Interface
+	plugin          *csiPlugin
+	driverName      csiDriverName
+	driverMode      driverMode
+	volumeID        string
+	specVolumeID    string
+	readOnly        bool
+	supportsSELinux bool
+	spec            *volume.Spec
+	pod             *api.Pod
+	podUID          types.UID
+	options         volume.VolumeOptions
+	publishContext  map[string]string
 	volume.MetricsProvider
 }
 
@@ -262,6 +263,12 @@ func (c *csiMountMgr) SetUpAt(dir string, fsGroup *int64) error {
 		return err
 	}
 
+	mounter := c.plugin.host.GetMounter(c.plugin.GetPluginName())
+	c.supportsSELinux, err = mounter.GetSELinuxSupport(dir)
+	if err != nil {
+		klog.V(2).Info(log("error checking for SELinux support: %s", err))
+	}
+
 	// apply volume ownership
 	// The following logic is derived from https://github.com/kubernetes/kubernetes/issues/66323
 	// if fstype is "", then skip fsgroup (could be indication of non-block filesystem)
@@ -327,18 +334,10 @@ func (c *csiMountMgr) podAttributes() (map[string]string, error) {
 }
 
 func (c *csiMountMgr) GetAttributes() volume.Attributes {
-	mounter := c.plugin.host.GetMounter(c.plugin.GetPluginName())
-	path := c.GetPath()
-	supportSelinux, err := mounter.GetSELinuxSupport(path)
-	if err != nil {
-		klog.V(2).Info(log("error checking for SELinux support: %s", err))
-		// Best guess
-		supportSelinux = false
-	}
 	return volume.Attributes{
 		ReadOnly:        c.readOnly,
 		Managed:         !c.readOnly,
-		SupportsSELinux: supportSelinux,
+		SupportsSELinux: c.supportsSELinux,
 	}
 }
 
