d8e4bef010ae921f3d60d28d5084f2e19c4208eb
Andrew McDermott <amcdermo@redhat.com>
2019-11-08 14:18:41 +0000

    Bug 1752646: e2e: Re-enable HAProxy router header test
    This change loosens the test expectations for AWS only.
    
    On AWS the packet (GET request) leaves the cluster and will be NAT'd;
    source addresses will now reflect the elastic IP address of the node.
    Matching on the PODs IP address is therefore not possible on AWS; we
    now assert that we have an 'X-Forwarded-For' header only.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1752646

--- test/extended/util/test.go
+++ test/extended/util/test.go
@@ -418,9 +418,6 @@ var (
 
 			`\[Driver: nfs\] \[Testpattern: Dynamic PV \(default fs\)\] provisioning should access volume from different nodes`, // https://bugzilla.redhat.com/show_bug.cgi?id=1711688
 
-			// Test fails on platforms that use LoadBalancerService and HostNetwork endpoint publishing strategy
-			`\[Conformance\]\[Area:Networking\]\[Feature:Router\] The HAProxy router should set Forwarded headers appropriately`, // https://bugzilla.redhat.com/show_bug.cgi?id=1752646
-
 			// requires a 1.14 kubelet, enable when rhcos is built for 4.2
 			"when the NodeLease feature is enabled",
 			"RuntimeClass should reject",
