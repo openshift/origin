openshift/sdn

Label egress IPs on the interface so we can clean up stale ones on restart

pkg/network/node/egressip.go
@@ -5,7 +5,6 @@ package node
 import (
 	"fmt"
 	"os/exec"
-	"sync"
 	"syscall"
 	"time"
 
@@ -19,7 +18,7 @@ import (
 )
 
 type egressIPWatcher struct {
-	sync.Mutex
+	// We don't need a mutex because tracker serializes all of its callbacks to us
 
 	tracker *common.EgressIPTracker
 
@@ -59,6 +58,45 @@ func (eip *egressIPWatcher) Start(networkInformers networkinformers.SharedInform
 
 	eip.tracker.Start(networkInformers.Network().V1().HostSubnets(), networkInformers.Network().V1().NetNamespaces())
 	return nil
+}
+
+func (eip *egressIPWatcher) Synced() {
+	link, _, err := GetLinkDetails(eip.localIP)
+	if err != nil {
+		// shouldn't happen, but obviously there's nothing to clean up...
+		return
+	}
+	label, err := egressIPLabel(link)
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("Could not check for stale egress IPs: %v", err))
+		return
+	}
+	addrs, err := netlink.AddrList(link, netlink.FAMILY_V4)
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("Could not check for stale egress IPs: %v", err))
+		return
+	}
+
+	for _, addr := range addrs {
+		ip := addr.IP.String()
+		if addr.Label == label && eip.iptablesMark[ip] == "" {
+			klog.Infof("Cleaning up stale egress IP %s", addr.IP.String())
+			err = netlink.AddrDel(link, &addr)
+			if err != nil {
+				utilruntime.HandleError(fmt.Errorf("Could not clean up stale egress IP: %v", err))
+			}
+		}
+	}
+}
+
+func egressIPLabel(link netlink.Link) (string, error) {
+	// An address label must start with the link name plus ":", and must be at most 15
+	// characters long. If the link name is too long then we can't label egress IPs.
+	label := link.Attrs().Name + ":eip"
+	if len(label) > 15 {
+		return "", fmt.Errorf("link name %q is too long", link.Attrs().Name)
+	}
+	return label, nil
 }
 
 // Convert vnid to a hex value that is not 0, does not have masqueradeBit set, and isn't
@@ -143,6 +181,7 @@ func (eip *egressIPWatcher) assignEgressIP(egressIP, mark string) error {
 	if !localEgressNet.Contains(addr.IP) {
 		return fmt.Errorf("egress IP %q is not in local network %s of interface %s", egressIP, localEgressNet.String(), localEgressLink.Attrs().Name)
 	}
+	addr.Label, _ = egressIPLabel(localEgressLink)
 	err = netlink.AddrAdd(localEgressLink, addr)
 	if err != nil {
 		if err == syscall.EEXIST {

openshift/origin

pkg/network/node/egressip.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net"
 	"os/exec"
-	"sync"
 	"syscall"
 	"time"
 
@@ -20,7 +19,7 @@ import (
 )
 
 type egressIPWatcher struct {
-	sync.Mutex
+	// We don't need a mutex because tracker serializes all of its callbacks to us
 
 	tracker *common.EgressIPTracker
 
@@ -69,6 +68,45 @@ func (eip *egressIPWatcher) Start(networkInformers networkinformers.SharedInform
 
 	eip.tracker.Start(networkInformers.Network().V1().HostSubnets(), networkInformers.Network().V1().NetNamespaces())
 	return nil
+}
+
+func (eip *egressIPWatcher) Synced() {
+	link, _, err := GetLinkDetails(eip.localIP)
+	if err != nil {
+		// shouldn't happen, but obviously there's nothing to clean up...
+		return
+	}
+	label, err := egressIPLabel(link)
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("Could not check for stale egress IPs: %v", err))
+		return
+	}
+	addrs, err := netlink.AddrList(link, netlink.FAMILY_V4)
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("Could not check for stale egress IPs: %v", err))
+		return
+	}
+
+	for _, addr := range addrs {
+		ip := addr.IP.String()
+		if addr.Label == label && eip.iptablesMark[ip] == "" {
+			glog.Infof("Cleaning up stale egress IP %s", addr.IP.String())
+			err = netlink.AddrDel(link, &addr)
+			if err != nil {
+				utilruntime.HandleError(fmt.Errorf("Could not clean up stale egress IP: %v", err))
+			}
+		}
+	}
+}
+
+func egressIPLabel(link netlink.Link) (string, error) {
+	// An address label must start with the link name plus ":", and must be at most 15
+	// characters long. If the link name is too long then we can't label egress IPs.
+	label := link.Attrs().Name + ":eip"
+	if len(label) > 15 {
+		return "", fmt.Errorf("link name %q is too long", link.Attrs().Name)
+	}
+	return label, nil
 }
 
 // Convert vnid to a hex value that is not 0, does not have masqueradeBit set, and isn't
@@ -148,6 +186,7 @@ func (eip *egressIPWatcher) assignEgressIP(egressIP, mark string) error {
 	if !eip.localEgressNet.Contains(addr.IP) {
 		return fmt.Errorf("egress IP %q is not in local network %s of interface %s", egressIP, eip.localEgressNet.String(), eip.localEgressLink.Attrs().Name)
 	}
+	addr.Label, _ = egressIPLabel(eip.localEgressLink)
 	err = netlink.AddrAdd(eip.localEgressLink, addr)
 	if err != nil {
 		if err == syscall.EEXIST {