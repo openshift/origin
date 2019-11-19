183044a27ba37e780b893d853dc4d153ad7ed3ec
David Eads <deads@redhat.com>
2019-11-19 15:02:37 -0500

    UPSTREAM: <carry>: remove version skew filter

--- vendor/k8s.io/kubernetes/openshift-kube-apiserver/openshiftkubeapiserver/patch_handlerchain.go
+++ vendor/k8s.io/kubernetes/openshift-kube-apiserver/openshiftkubeapiserver/patch_handlerchain.go
@@ -4,20 +4,18 @@ import (
 	"net/http"
 	"strings"
 
+	authenticationv1 "k8s.io/api/authentication/v1"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 
-	kubecontrolplanev1 "github.com/openshift/api/kubecontrolplane/v1"
+	authorizationv1 "github.com/openshift/api/authorization/v1"
 	"github.com/openshift/library-go/pkg/apiserver/httprequest"
 )
 
 // TODO switch back to taking a kubeapiserver config.  For now make it obviously safe for 3.11
-func BuildHandlerChain(userAgentMatchingConfig kubecontrolplanev1.UserAgentMatchingConfig, consolePublicURL string) (func(apiHandler http.Handler, kc *genericapiserver.Config) http.Handler, error) {
+func BuildHandlerChain(consolePublicURL string) (func(apiHandler http.Handler, kc *genericapiserver.Config) http.Handler, error) {
 	return func(apiHandler http.Handler, genericConfig *genericapiserver.Config) http.Handler {
-			// these are after the kube handler
-			handler := versionSkewFilter(apiHandler, userAgentMatchingConfig)
-
 			// this is the normal kube handler chain
-			handler = genericapiserver.DefaultBuildHandlerChain(handler, genericConfig)
+			handler := genericapiserver.DefaultBuildHandlerChain(apiHandler, genericConfig)
 
 			// these handlers are all before the normal kube chain
 			handler = translateLegacyScopeImpersonation(handler)
@@ -44,6 +42,23 @@ func withConsoleRedirect(handler http.Handler, consolePublicURL string) http.Han
 			return
 		}
 		// Dispatch to the next handler
+		handler.ServeHTTP(w, req)
+	})
+}
+
+// legacyImpersonateUserScopeHeader is the header name older servers were using
+// just for scopes, so we need to translate it from clients that may still be
+// using it.
+const legacyImpersonateUserScopeHeader = "Impersonate-User-Scope"
+
+// translateLegacyScopeImpersonation is a filter that will translates user scope impersonation for openshift into the equivalent kube headers.
+func translateLegacyScopeImpersonation(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		for _, scope := range req.Header[legacyImpersonateUserScopeHeader] {
+			req.Header[authenticationv1.ImpersonateUserExtraHeaderPrefix+authorizationv1.ScopesKey] =
+				append(req.Header[authenticationv1.ImpersonateUserExtraHeaderPrefix+authorizationv1.ScopesKey], scope)
+		}
+
 		handler.ServeHTTP(w, req)
 	})
 }
