3745e39c830b195cc3630b1d912bb658953fa954
Tomas Nozicka <tnozicka@redhat.com>
2019-12-02 15:52:45 +0100

    Rewrite commitchecker so it validates bumps as well

--- tools/rebasehelpers/util/git.go
+++ tools/rebasehelpers/util/git.go
@@ -9,25 +9,87 @@ import (
 	"strings"
 )
 
-var UpstreamSummaryPattern = regexp.MustCompile(`UPSTREAM: (revert: )?(([\w\.-]+\/[\w-\.-]+)?: )?(\d+:|<carry>:|<drop>:)`)
+var (
+	UpstreamSummaryPattern = regexp.MustCompile(`^UPSTREAM: (revert: )?(([\w\.-]+\/[\w-\.-]+)?: )?(\d+:|<carry>:|<drop>:)`)
+	BumpSummaryPattern     = regexp.MustCompile(`^bump[\(\w].*`)
 
-// supportedHosts maps source hosts to the number of path segments that
-// represent the account/repo for that host. This is necessary because we
-// can't tell just by looking at an import path whether the repo is identified
-// by the first 2 or 3 path segments.
-//
-// If dependencies are introduced from new hosts, they'll need to be added
-// here.
-var SupportedHosts = map[string]int{
-	"bitbucket.org":     3,
-	"cloud.google.com":  2,
-	"code.google.com":   3,
-	"github.com":        3,
-	"golang.org":        3,
-	"google.golang.org": 2,
-	"gopkg.in":          2,
-	"k8s.io":            2,
-	"speter.net":        2,
+	// patchRegexps holds regexps for paths inside vendor dir that are allowed to be patched directly.
+	// It must corresponds to published repositories.
+	PatchRegexps = []*regexp.Regexp{
+		regexp.MustCompile("^k8s.io/kubernetes/.*"),
+	}
+
+	// supportedHosts maps source hosts to the number of path segments that
+	// represent the account/repo for that host. This is necessary because we
+	// can't tell just by looking at an import path whether the repo is identified
+	// by the first 2 or 3 path segments.
+	//
+	// If dependencies are introduced from new hosts, they'll need to be added
+	// here.
+	SupportedHosts = map[string]int{
+		"bitbucket.org":     3,
+		"cloud.google.com":  2,
+		"code.google.com":   3,
+		"github.com":        3,
+		"golang.org":        3,
+		"google.golang.org": 2,
+		"gopkg.in":          2,
+		"k8s.io":            2,
+		"speter.net":        2,
+	}
+)
+
+func RegexpsToStrings(a []*regexp.Regexp) []string {
+	var res []string
+	for _, r := range a {
+		res = append(res, r.String())
+	}
+	return res
+}
+
+type File string
+
+func (f File) HasVendoredCodeChanges() bool {
+	return strings.HasPrefix(string(f), "vendor")
+}
+
+func (f File) IsPatch() bool {
+	if !strings.HasPrefix(string(f), "vendor/") {
+		return false
+	}
+
+	for _, r := range PatchRegexps {
+		if r.Match([]byte(strings.TrimPrefix(string(f), "vendor/"))) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (f File) VendorRepo() (string, error) {
+	if !strings.HasPrefix(string(f), "vendor/") {
+		return "", fmt.Errorf("file %q doesn't appear to be a vendor change", string(f))
+	}
+
+	p := strings.TrimPrefix(string(f), "vendor/")
+
+	parts := strings.Split(p, string(os.PathSeparator))
+
+	if len(parts) < 1 {
+		return "", fmt.Errorf("invalid file %q", string(f))
+	}
+
+	numSegments, ok := SupportedHosts[parts[0]]
+	if !ok {
+		return "", fmt.Errorf("unsupported host for file %q", string(f))
+	}
+
+	if numSegments < 1 {
+		return "", fmt.Errorf("invalid number of segments %d when processing file path %q", numSegments, string(f))
+	}
+
+	return strings.Join(parts[0:numSegments], string(os.PathSeparator)), nil
 }
 
 type Commit struct {
@@ -38,18 +100,15 @@ type Commit struct {
 	Email       string
 }
 
-func (c Commit) DeclaresUpstreamChange() bool {
-	return strings.HasPrefix(strings.ToLower(c.Summary), "upstream")
-}
-
 func (c Commit) MatchesUpstreamSummaryPattern() bool {
 	return UpstreamSummaryPattern.MatchString(c.Summary)
 }
 
+func (c Commit) MatchesBumpSummaryPattern() bool {
+	return BumpSummaryPattern.MatchString(c.Summary)
+}
+
 func (c Commit) DeclaredUpstreamRepo() (string, error) {
-	if !c.DeclaresUpstreamChange() {
-		return "", fmt.Errorf("commit declares no upstream changes")
-	}
 	if !c.MatchesUpstreamSummaryPattern() {
 		return "", fmt.Errorf("commit doesn't match the upstream commit summary pattern")
 	}
@@ -61,8 +120,6 @@ func (c Commit) DeclaredUpstreamRepo() (string, error) {
 	return repo, nil
 }
 
-// HasVendoredCodeChanges verifies if the commit has any changes to Godeps/_workspace/
-// or vendor/ directories.
 func (c Commit) HasVendoredCodeChanges() bool {
 	for _, file := range c.Files {
 		if file.HasVendoredCodeChanges() {
@@ -72,18 +129,6 @@ func (c Commit) HasVendoredCodeChanges() bool {
 	return false
 }
 
-// HasGodepsChanges verifies if the commit has any changes to Godeps/Godeps.json file.
-func (c Commit) HasGodepsChanges() bool {
-	for _, file := range c.Files {
-		if file.HasGodepsChanges() {
-			return true
-		}
-	}
-	return false
-}
-
-// HasNonVendoredCodeChanges verifies if the commit didn't modify Godeps/_workspace/
-// or vendor directories.
 func (c Commit) HasNonVendoredCodeChanges() bool {
 	for _, file := range c.Files {
 		if !file.HasVendoredCodeChanges() {
@@ -93,94 +138,41 @@ func (c Commit) HasNonVendoredCodeChanges() bool {
 	return false
 }
 
-func (c Commit) GodepsReposChanged() ([]string, error) {
-	repos := map[string]struct{}{}
-	for _, file := range c.Files {
-		if !file.HasVendoredCodeChanges() {
-			continue
+func (c Commit) HasPatches() bool {
+	for _, f := range c.Files {
+		if f.IsPatch() {
+			return true
 		}
-		repo, err := file.GodepsRepoChanged()
-		if err != nil {
-			return nil, fmt.Errorf("problem with file %q in commit %s: %s", file, c.Sha, err)
-		}
-		repos[repo] = struct{}{}
 	}
-	changed := []string{}
-	for repo := range repos {
-		changed = append(changed, repo)
-	}
-	return changed, nil
+	return false
 }
 
-type File string
-
-// HasVendoredCodeChanges verifies if the modified file is from Godeps/_workspace/
-// or vendor/ directories.
-func (f File) HasVendoredCodeChanges() bool {
-	return strings.HasPrefix(string(f), "Godeps/_workspace") ||
-		strings.HasPrefix(string(f), "vendor") ||
-		strings.HasPrefix(string(f), "pkg/build/vendor")
+func (c Commit) HasBumpedFiles() bool {
+	for _, f := range c.Files {
+		if f.HasVendoredCodeChanges() && !f.IsPatch() {
+			return true
+		}
+	}
+	return false
 }
 
-// HasGodepsChanges verifies if the modified file is Godeps/Godeps.json.
-func (f File) HasGodepsChanges() bool {
-	return f == "Godeps/Godeps.json"
-}
-
-func (f File) GodepsRepoChanged() (string, error) {
-	if !f.HasVendoredCodeChanges() {
-		return "", fmt.Errorf("file doesn't appear to be a Godeps or vendor change")
-	}
-	// Find the _workspace or vendor path segment index.
-	workspaceIdx, vendorIdx := -1, -1
-	parts := strings.Split(string(f), string(os.PathSeparator))
-	for i, part := range parts {
-		if part == "_workspace" {
-			workspaceIdx = i
-			break
-		}
-		if part == "vendor" {
-			vendorIdx = i
-			break
+func (c Commit) PatchedRepos() ([]string, error) {
+	var repos []string
+	seenKeys := map[string]struct{}{}
+	for _, f := range c.Files {
+		if f.IsPatch() {
+			repo, err := f.VendorRepo()
+			if err != nil {
+				return nil, err
+			}
+			_, ok := seenKeys[repo]
+			if !ok {
+				repos = append(repos, repo)
+				seenKeys[repo] = struct{}{}
+			}
 		}
 	}
-	var nextIdx int
-	switch {
-	case workspaceIdx != -1:
-		// Godeps path struture assumption: Godeps/_workspace/src/...
-		if len(parts) < (workspaceIdx + 3) {
-			return "", fmt.Errorf("file doesn't appear to be a Godeps workspace path")
-		}
-		nextIdx = workspaceIdx + 2
-	case vendorIdx != -1:
-		// Godeps path struture assumption: vendor/...
-		if len(parts) < (vendorIdx + 1) {
-			return "", fmt.Errorf("file doesn't appear to be a vendor path")
-		}
-		nextIdx = vendorIdx + 1
-	default:
-		return "", fmt.Errorf("file doesn't appear to be a Godeps workspace path or vendor path")
-	}
-
-	// Deal with repos which could be identified by either 2 or 3 path segments.
-	host := parts[nextIdx]
-	segments := -1
-	for supportedHost, count := range SupportedHosts {
-		if host == supportedHost {
-			segments = count
-			break
-		}
-	}
-	if segments == -1 {
-		return "", fmt.Errorf("file modifies an unsupported repo host %q", host)
-	}
-	switch segments {
-	case 2:
-		return fmt.Sprintf("%s/%s", host, parts[nextIdx+1]), nil
-	case 3:
-		return fmt.Sprintf("%s/%s/%s", host, parts[nextIdx+1], parts[nextIdx+2]), nil
-	}
-	return "", fmt.Errorf("file modifies an unsupported repo host %q", host)
+	return repos, nil
 }
 
 func IsCommit(a string) bool {
