25a2cc31894980a4c552802946e7e560ee61d8bf
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2019-12-24 13:21:44 +0100

    UPSTREAM: <drop>: brings back disabled PodSecurityPolicy e2e test

--- vendor/k8s.io/kubernetes/test/e2e/auth/pod_security_policy.go
+++ vendor/k8s.io/kubernetes/test/e2e/auth/pod_security_policy.go
@@ -52,7 +52,6 @@ var _ = SIGDescribe("PodSecurityPolicy", func() {
 	var c clientset.Interface
 	var ns string // Test namespace, for convenience
 	ginkgo.BeforeEach(func() {
-		framework.Skipf("This test should always be skipped for us, but occasionally PSP is on!")
 		if !framework.IsPodSecurityPolicyEnabled(f.ClientSet) {
 			framework.Skipf("PodSecurityPolicy not enabled")
 		}
