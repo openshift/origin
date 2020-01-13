db9e7cf5b9f1a91d95e7f27925ca5ee5433a6f7d
gabemontero <gmontero@redhat.com>
2020-01-13 18:15:17 -0500

    move more image-eco db template tests from upstream repos to sample operator installed

--- test/extended/image_ecosystem/mariadb_ephemeral.go
+++ test/extended/image_ecosystem/mariadb_ephemeral.go
@@ -12,7 +12,7 @@ import (
 var _ = g.Describe("[image_ecosystem][mariadb][Slow] openshift mariadb image", func() {
 	defer g.GinkgoRecover()
 	var (
-		templatePath = exutil.FixturePath("..", "..", "examples", "db-templates", "mariadb-ephemeral-template.json")
+		templatePath = "mariadb-ephemeral"
 		oc           = exutil.NewCLI("mariadb-create", exutil.KubeConfigPath())
 	)
 
@@ -33,8 +33,8 @@ var _ = g.Describe("[image_ecosystem][mariadb][Slow] openshift mariadb image", f
 			g.It(fmt.Sprintf("should instantiate the template"), func() {
 				exutil.WaitForOpenShiftNamespaceImageStreams(oc)
 
-				g.By(fmt.Sprintf("calling oc process -f %q", templatePath))
-				configFile, err := oc.Run("process").Args("-f", templatePath).OutputToFile("config.json")
+				g.By(fmt.Sprintf("calling oc process %q", templatePath))
+				configFile, err := oc.Run("process").Args("openshift//" + templatePath).OutputToFile("config.json")
 				o.Expect(err).NotTo(o.HaveOccurred())
 
 				g.By(fmt.Sprintf("calling oc create -f %q", configFile))
