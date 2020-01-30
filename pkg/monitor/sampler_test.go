8a1baa1fff3bbdd9505b947ae79e7d45fb30ec6d
Clayton Coleman <ccoleman@redhat.com>
2020-01-30 16:29:03 -0500

    monitor: Condition sampling incorrectly extended intervals
    When transitioning from any condition to no condition, we have to
    record a tombstone that indicates when all conditions stopped.

---
+++ pkg/monitor/sampler_test.go
@@ -0,0 +1,77 @@
+package monitor
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/diff"
+)
+
+func TestStartSampling(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	m := NewMonitorWithInterval(5 * time.Millisecond)
+
+	doneCh := make(chan struct{})
+	var count int
+	m.AddSampler(StartSampling(ctx, m, 21*time.Millisecond, func(previous bool) (condition *Condition, next bool) {
+		defer func() { count++ }()
+		switch {
+		case count <= 5:
+			return nil, true
+		case count == 6:
+			return &Condition{Level: Error, Locator: "tester", Message: "dying"}, false
+		case count == 7:
+			return &Condition{Level: Info, Locator: "tester", Message: "recovering"}, true
+		case count <= 12:
+			return nil, true
+		case count == 13:
+			return &Condition{Level: Error, Locator: "tester", Message: "dying 2"}, false
+		case count <= 16:
+			return nil, false
+		case count == 17:
+			return &Condition{Level: Info, Locator: "tester", Message: "recovering 2"}, true
+		case count <= 20:
+			return nil, true
+		default:
+			doneCh <- struct{}{}
+			return nil, true
+		}
+	}).ConditionWhenFailing(&Condition{
+		Level:   Error,
+		Locator: "tester",
+		Message: "down",
+	}))
+
+	m.StartSampling(ctx)
+	<-doneCh
+	cancel()
+
+	var describe []string
+	var log []string
+	events := m.Events(time.Time{}, time.Time{})
+	for _, interval := range events {
+		i := interval.To.Sub(interval.From)
+		describe = append(describe, fmt.Sprintf("%v %s", *interval.Condition, i))
+		log = append(log, fmt.Sprintf("%v", *interval.Condition))
+	}
+	expected := []string{
+		"{2 tester dying}",
+		"{2 tester down}",
+		"{0 tester recovering}",
+		"{2 tester dying 2}",
+		"{2 tester down}",
+		"{0 tester recovering 2}",
+	}
+	if !reflect.DeepEqual(log, expected) {
+		t.Fatalf("%s", diff.ObjectReflectDiff(log, expected))
+	}
+	if events[4].To.Sub(events[4].From) < 2*events[1].To.Sub(events[1].From) {
+		t.Fatalf("last condition should be at least 2x first condition length:\n%s", strings.Join(describe, "\n"))
+	} else {
+		t.Logf("%s", strings.Join(describe, "\n"))
+	}
+}
