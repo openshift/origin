d7ef215cb3c53a60faa01ca37eed22b24175a905
Manjunath A Kumatagi <mkumatag@in.ibm.com>
2020-02-06 15:21:41 +0530

    Copy the kubeconfig into container with proper name

--- test/extended/oauth/groupsync.go
+++ test/extended/oauth/groupsync.go
@@ -74,7 +74,7 @@ var _ = g.Describe("[Suite:openshift/oauth][Serial] ldap group sync", func() {
 		err = pod.CopyFromHost(ldapCAPath, remoteTmp)
 		o.Expect(err).NotTo(o.HaveOccurred())
 
-		err = pod.CopyFromHost(testutil.KubeConfigPath(), remoteTmp)
+		err = pod.CopyFromHost(testutil.KubeConfigPath(), path.Join(remoteTmp, kubeConfigFileName))
 		o.Expect(err).NotTo(o.HaveOccurred())
 
 		groupSyncScriptPath := path.Join(tmpDir, "groupsync.sh")
