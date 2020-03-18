52cc5dd8bea02a960784e93b4d0518b4205e55f3
Maru Newby <marun@redhat.com>
2020-03-18 16:30:37 -0700

    Update godep, conformance test and docker image versions

--- test/extended/conformance-k8s.sh
+++ test/extended/conformance-k8s.sh
@@ -43,7 +43,7 @@ To recreate these results
 Nightly conformance tests are run against release branches and reported https://openshift-gce-devel.appspot.com/builds/origin-ci-test/logs/periodic-ci-origin-conformance-k8s/
 END
 
-version="${KUBERNETES_VERSION:-release-1.17}"
+version="${KUBERNETES_VERSION:-release-1.18}"
 kubernetes="${KUBERNETES_ROOT:-${OS_ROOT}/../../../k8s.io/kubernetes}"
 if [[ -d "${kubernetes}" ]]; then
   git fetch origin --tags
