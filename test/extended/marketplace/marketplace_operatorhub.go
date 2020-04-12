607d474958ed7925e8d2c1d81bc5b32d7f2bbd8e
jfan <jfan@redhat.com>
2020-04-12 20:58:27 +0800

    Auto check the update of operatorhub

---
+++ test/extended/marketplace/marketplace_operatorhub.go
@@ -0,0 +1,55 @@
+package marketplace
+
+import (
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
+
+	exutil "github.com/openshift/origin/test/extended/util"
+)
+
+var _ = g.Describe("[sig-operator][Feature:Marketplace] Marketplace operatorhub test", func() {
+
+	defer g.GinkgoRecover()
+
+	var (
+		oc            = exutil.NewCLI("marketplace")
+		marketplaceNs = "openshift-marketplace"
+	)
+	g.AfterEach(func() {
+		
+	})
+	// Test the operatorhub config impact the default opsrc"
+	g.It("operatorhub config test [Serial]", func() {
+
+		// Get the default opsrc of cluster
+		output, err := oc.AsAdmin().Run("get").Args("operatorsource", "-o", "name", "-n", marketplaceNs).Output()
+		// Output get redhat-operators, community-operators, certified-operators
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(output).Should(o.ContainSubstring("redhat-operators"))
+		o.Expect(output).Should(o.ContainSubstring("community-operators"))
+		o.Expect(output).Should(o.ContainSubstring("certified-operators"))
+
+		// Set the disableAll to be true, and the redhat-operators' disable to be true, community-operators' disable to be false
+		_, err = oc.AsAdmin().Run("patch").Args("operatorhub/cluster", "-p", `{"spec":{"disableAllDefaultSources": true}}`, "--type=merge").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		_, err = oc.AsAdmin().Run("patch").Args("operatorhub/cluster", "-p", `{"spec":{"sources":[{"disabled": true,"name": "redhat-operators"},{"disabled": false,"name": "community-operators"}]}}`, "--type=merge").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		output, err = oc.AsAdmin().Run("get").Args("operatorsource", "-o", "name", "-n", marketplaceNs).Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		// Output doesn't have redhat-operators, certified-operators
+		o.Expect(output).ShouldNot(o.ContainSubstring("redhat-operators"))
+		o.Expect(output).ShouldNot(o.ContainSubstring("certified-operators"))
+
+		// Set the disableAll to be false
+		_, err = oc.AsAdmin().Run("patch").Args("operatorhub/cluster", "-p", `{"spec":{"disableAllDefaultSources": false}}`, "--type=merge").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		output, err = oc.AsAdmin().Run("get").Args("operatorsource", "-o", "name", "-n", marketplaceNs).Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		// Output doesn't have redhat-operators
+		o.Expect(output).ShouldNot(o.ContainSubstring("redhat-operators"))
+
+		// Set the redhat-operators enable
+		_, err = oc.AsAdmin().Run("patch").Args("operatorhub/cluster", "-p", `{"spec":{"sources":[{"disabled": false,"name": "redhat-operators"}]}}`, "--type=merge").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+	})
+})
