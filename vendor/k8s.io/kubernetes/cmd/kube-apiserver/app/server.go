3b0c72dd7b9f9367dda8f8645909d9277a6c29e9
David Eads <deads@redhat.com>
2020-05-11 15:21:09 -0400

    UPSTREAM: <carry>: eliminate unnecessary closure in openshift configuration wiring

--- vendor/k8s.io/kubernetes/cmd/kube-apiserver/app/server.go
+++ vendor/k8s.io/kubernetes/cmd/kube-apiserver/app/server.go
@@ -117,16 +117,14 @@ cluster's shared state through which all other components interact.`,
 			verflag.PrintAndExitIfRequested()
 
 			if len(s.OpenShiftConfig) > 0 {
+				// if we are running openshift, we modify the admission chain defaults accordingly
+				admissionenablement.InstallOpenShiftAdmissionPlugins(s)
+
 				openshiftConfig, err := enablement.GetOpenshiftConfig(s.OpenShiftConfig)
 				if err != nil {
 					klog.Fatal(err)
 				}
 				enablement.ForceOpenShift(openshiftConfig)
-
-				// this forces a patch to be called
-				// TODO we're going to try to remove bits of the patching.
-				configPatchFn := openshiftkubeapiserver.NewOpenShiftKubeAPIServerConfigPatch(openshiftConfig)
-				OpenShiftKubeAPIServerConfigPatch = configPatchFn
 
 				args, err := openshiftkubeapiserver.ConfigToFlags(openshiftConfig)
 				if err != nil {
@@ -142,8 +140,6 @@ cluster's shared state through which all other components interact.`,
 				utilflag.PrintFlags(cmd.Flags())
 
 				enablement.ForceGlobalInitializationForOpenShift()
-				admissionenablement.InstallOpenShiftAdmissionPlugins(s)
-
 			} else {
 				// print default flags
 				utilflag.PrintFlags(cmd.Flags())
@@ -593,7 +589,7 @@ func buildGenericConfig(
 		return
 	}
 
-	if err := PatchKubeAPIServerConfig(genericConfig, versionedInformers, &pluginInitializers); err != nil {
+	if err := openshiftkubeapiserver.OpenShiftKubeAPIServerConfigPatch(genericConfig, versionedInformers, &pluginInitializers); err != nil {
 		lastErr = fmt.Errorf("failed to patch: %v", err)
 		return
 	}
