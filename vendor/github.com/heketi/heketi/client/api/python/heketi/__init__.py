6febae93ec573ad53802ef33368681ce0b35b049
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2020-05-20 13:12:25 +0200

    bump(*)

---
+++ vendor/github.com/heketi/heketi/client/api/python/heketi/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+__title__ = 'heketi'
+__version__ = '1.0.2'
+__author__ = 'Heketi authors'
+__license__ = 'Apache License (2.0) or LGPLv3+'
+__copyright__ = 'Copyright 2016 Heketi authors'
+
+
+from .heketi import HeketiClient
