094fc2f4349204a27271e392f1f7d9a52bb6c326
Siva <vareti@redhat.com>
2020-09-29 18:43:46 -0500

    move gem install directory to writable location

--- test/extended/testdata/bindata.go
+++ test/extended/testdata/bindata.go
@@ -2249,7 +2249,7 @@ EXPOSE 8080
 ENV RACK_ENV production
 ENV RAILS_ENV production
 COPY . /opt/app-root/src/
-RUN scl enable rh-ruby25 "bundle install"
+RUN scl enable rh-ruby25 "GEM_HOME=~/.gem bundle install"
 CMD ["scl", "enable", "rh-ruby25", "./run.sh"]
 
 USER default
