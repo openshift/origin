5d3da340aaad309a9e99378f0ff566e72c2d6c4c
Stephen Greene <sgreene@redhat.com>
2020-10-01 10:48:01 -0400

    exutil: Add GetClusterProxyConfig() helper
    Add a simple proxy util for fetching the
    cluster's egress proxy config.

--- test/extended/util/proxy.go
+++ test/extended/util/proxy.go
@@ -3,8 +3,13 @@ package util
 import (
 	"context"
 
+	v1 "github.com/openshift/api/config/v1"
+
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	rest "k8s.io/client-go/rest"
+
+	configclientset "github.com/openshift/client-go/config/clientset/versioned"
 )
 
 // IsClusterProxyEnabled returns true if the cluster has a global proxy enabled
@@ -17,4 +22,11 @@ func IsClusterProxyEnabled(oc *CLI) (bool, error) {
 		return false, err
 	}
 	return len(proxy.Status.HTTPProxy) > 0 || len(proxy.Status.HTTPSProxy) > 0, nil
+}
+
+func GetClusterProxyConfig(oc *rest.Config) *v1.Proxy {
+	configClient := configclientset.NewForConfigOrDie(oc)
+	proxy, _ := configClient.ConfigV1().Proxies().Get(context.Background(), "cluster", metav1.GetOptions{})
+
+	return proxy
 }
