35a6aa8e94251ba3a625070e77b8af3f35408d5a
Jan Safranek <jsafrane@redhat.com>
2020-11-02 09:25:59 +0100

    Remove CSI driver installation from openshift-tests
    AWS EBS CSI driver is now installed by default in an OCP cluster on AWS,
    therefore we don't need to install it via openshift-install.

--- cmd/openshift-tests/csi.go
+++ cmd/openshift-tests/csi.go
@@ -8,39 +8,14 @@ import (
 
 	"k8s.io/kubernetes/test/e2e/framework/testfiles"
 	"k8s.io/kubernetes/test/e2e/storage/external"
-
-	"github.com/openshift/origin/test/extended/csi"
 )
 
 const (
-	manifestEnvVar       = "TEST_CSI_DRIVER_FILES"
-	installDriversEnvVar = "TEST_INSTALL_CSI_DRIVERS"
+	manifestEnvVar = "TEST_CSI_DRIVER_FILES"
 )
 
 // Initialize openshift/csi suite, i.e. define CSI tests from TEST_CSI_DRIVER_FILES.
 func initCSITests(dryRun bool) error {
-	driverList := os.Getenv(installDriversEnvVar)
-	if driverList != "" {
-		drivers := strings.Split(driverList, ",")
-		for _, driver := range drivers {
-			manifestFile, err := csi.InstallCSIDriver(driver, dryRun)
-			if err != nil {
-				return fmt.Errorf("failed to install CSI driver from %q: %s", driver, err)
-			}
-			// Children processes need to see the newly introduced manifest,
-			// store it in TEST_CSI_DRIVER_FILES env. var for them.
-			manifestList := os.Getenv(manifestEnvVar)
-			if len(manifestList) > 0 {
-				manifestList += ","
-			}
-			manifestList += manifestFile
-			os.Setenv(manifestEnvVar, manifestList)
-		}
-	}
-
-	// Clear TEST_INSTALL_CSI_DRIVERS, we don't want the driver installed by children too.
-	os.Setenv(installDriversEnvVar, "")
-
 	manifestList := os.Getenv(manifestEnvVar)
 	if manifestList != "" {
 		manifests := strings.Split(manifestList, ",")
--- cmd/openshift-tests/e2e.go
+++ cmd/openshift-tests/e2e.go
@@ -157,15 +157,9 @@ var staticSuites = []*ginkgo.TestSuite{
 	{
 		Name: "openshift/csi",
 		Description: templates.LongDesc(`
-		Run tests for an CSI driver. Set the TEST_INSTALL_CSI_DRIVERS environment variable to the name of the driver to load.
-		For example, TEST_INSTALL_CSI_DRIVERS=aws-ebs will test the AWS EBS CSI driver. To change the location of the images,
-		specify IMAGE_FORMAT=myregistry.com/myrepository/${component}:4.5 where ${component} will be substituted with the
-		names of required CSI component images:
-
-			csi-external-attacher, csi-external-provisioner, csi-external-resizer,
-			csi-external-snapshotter, csi-node-driver-registrar, csi-livenessprobe
-
-		See https://github.com/kubernetes/kubernetes/blob/master/test/e2e/storage/external/README.md for required format of the files.
+		Run tests for an CSI driver. Set the TEST_CSI_DRIVER_FILES environment variable to the name of file with
+		CSI driver test manifest. The manifest specifies Kubernetes + CSI features to test with the driver.
+		See https://github.com/kubernetes/kubernetes/blob/master/test/e2e/storage/external/README.md for required format of the file.
 		`),
 		Matches: func(name string) bool {
 			return strings.Contains(name, "External Storage [Driver:") && !strings.Contains(name, "[Disruptive]")
