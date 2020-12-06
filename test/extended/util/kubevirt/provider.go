1ec7c23aa778e743f2a39c5e551a24f21b139f24
Danielle Barda <dbarda@redhat.com>
2020-12-06 18:27:47 +0200

    add kubevirt as a provide to openshift tests

---
+++ test/extended/util/kubevirt/provider.go
@@ -0,0 +1,18 @@
+package kubevirt
+
+import (
+	"k8s.io/kubernetes/test/e2e/framework"
+)
+
+func init() {
+	framework.RegisterProvider("kubevirt", newProvider)
+}
+
+func newProvider() (framework.ProviderInterface, error) {
+	return &Provider{}, nil
+}
+
+// Provider is a structure to handle kubevirt for e2e testing
+type Provider struct {
+	framework.NullProvider
+}
