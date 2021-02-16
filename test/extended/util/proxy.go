daff0d5de21838907519ac5f18de64ed2429b2ac
Stephen Greene <sgreene@redhat.com>
2021-02-15 23:37:44 -0500

    Add GetClusterProxy to util/proxy.go

--- test/extended/util/proxy.go
+++ test/extended/util/proxy.go
@@ -3,18 +3,29 @@ package util
 import (
 	"context"
 
+	v1 "github.com/openshift/api/config/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // IsClusterProxyEnabled returns true if the cluster has a global proxy enabled
 func IsClusterProxyEnabled(oc *CLI) (bool, error) {
-	proxy, err := oc.AdminConfigClient().ConfigV1().Proxies().Get(context.Background(), "cluster", metav1.GetOptions{})
-	if kerrors.IsNotFound(err) {
-		return false, nil
-	}
+	proxy, err := GetClusterProxy(oc)
 	if err != nil {
 		return false, err
 	}
 	return len(proxy.Status.HTTPProxy) > 0 || len(proxy.Status.HTTPSProxy) > 0, nil
+}
+
+func GetClusterProxy(oc *CLI) (*v1.Proxy, error) {
+	proxy := &v1.Proxy{}
+	proxy, err := oc.AdminConfigClient().ConfigV1().Proxies().Get(context.Background(), "cluster", metav1.GetOptions{})
+	if kerrors.IsNotFound(err) {
+		return proxy, nil
+	}
+	if err != nil {
+		return proxy, err
+	}
+
+	return proxy, nil
 }
