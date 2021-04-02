cb54944c96ffef4843505e071638bcaeeafbcd38
Andrew McDermott <amcdermo@redhat.com>
2021-04-02 06:16:05 +0100

    test/extended/router/grpc-interop: rework and enable test

--- test/extended/router/grpc-interop.go
+++ test/extended/router/grpc-interop.go
@@ -3,177 +3,201 @@ package router
 import (
 	"context"
 	"fmt"
-	"sort"
-	"strings"
 	"time"
 
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
+	configv1 "github.com/openshift/api/config/v1"
 	routev1 "github.com/openshift/api/route/v1"
-	routeclientset "github.com/openshift/client-go/route/clientset/versioned"
 
+	"github.com/openshift/origin/test/extended/router/certgen"
 	grpcinterop "github.com/openshift/origin/test/extended/router/grpc-interop"
+	"github.com/openshift/origin/test/extended/router/shard"
 	exutil "github.com/openshift/origin/test/extended/util"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
-)
-
-const (
-	// gRPCInteropTestTimeout is the timeout value for the
-	// internal tests.
-	gRPCInteropTestTimeout = 5 * time.Minute
-
-	// gRPCInteropTestCaseIterations is the number of times each gRPC
-	// interop test case should be invoked.
-	gRPCInteropTestCaseIterations = 2
 )
 
 var _ = g.Describe("[sig-network-edge][Conformance][Area:Networking][Feature:Router]", func() {
 	defer g.GinkgoRecover()
 
 	var (
-		configPath = exutil.FixturePath("testdata", "router", "router-grpc-interop.yaml")
-		oc         = exutil.NewCLI("router-grpc")
+		grpcServiceConfigPath     = exutil.FixturePath("testdata", "router", "router-grpc-interop.yaml")
+		grpcRoutesConfigPath      = exutil.FixturePath("testdata", "router", "router-grpc-interop-routes.yaml")
+		grpcRouterShardConfigPath = exutil.FixturePath("testdata", "router", "router-shard.yaml")
+		oc                        = exutil.NewCLI("grpc-interop")
+		shardConfigPath           string // computed
 	)
 
 	// this hook must be registered before the framework namespace teardown
 	// hook
 	g.AfterEach(func() {
 		if g.CurrentGinkgoTestDescription().Failed {
-			client := routeclientset.NewForConfigOrDie(oc.AdminConfig()).RouteV1().Routes(oc.KubeFramework().Namespace.Name)
-			if routes, _ := client.List(context.Background(), metav1.ListOptions{}); routes != nil {
-				outputIngress(routes.Items...)
-			}
 			exutil.DumpPodLogsStartingWith("grpc", oc)
+			exutil.DumpPodLogsStartingWithInNamespace("router", "openshift-ingress", oc.AsAdmin())
+		}
+		if len(shardConfigPath) > 0 {
+			oc.AsAdmin().Run("delete").Args("-n", "openshift-ingress-operator", "-f", shardConfigPath).Execute()
 		}
 	})
 
 	g.Describe("The HAProxy router", func() {
 		g.It("should pass the gRPC interoperability tests", func() {
-			g.Skip("disabled for https://bugzilla.redhat.com/show_bug.cgi?id=1853711")
-			g.By(fmt.Sprintf("creating test fixture from a config file %q", configPath))
-			err := oc.Run("new-app").Args("-f", configPath).Execute()
-			o.Expect(err).NotTo(o.HaveOccurred())
-			e2e.ExpectNoError(e2epod.WaitForPodRunningInNamespaceSlow(oc.KubeClient(), "grpc-interop", oc.KubeFramework().Namespace.Name))
+			infra, err := oc.AdminConfigClient().ConfigV1().Infrastructures().Get(context.Background(), "cluster", metav1.GetOptions{})
+			o.Expect(err).NotTo(o.HaveOccurred(), "failed to get cluster-wide infrastructure")
+			switch infra.Status.PlatformStatus.Type {
+			case configv1.OvirtPlatformType, configv1.KubevirtPlatformType, configv1.LibvirtPlatformType, configv1.VSpherePlatformType, configv1.BareMetalPlatformType, configv1.NonePlatformType:
+				g.Skip("Skip on platforms where the default router is not exposed by a load balancer service.")
+			}
 
-			g.By("Discovering the set of supported test cases")
-			ns := oc.KubeFramework().Namespace.Name
-			output, err := grpcInteropExecClientShellCmd(ns, gRPCInteropTestTimeout, "/workdir/grpc-client -list-tests")
-			o.Expect(err).NotTo(o.HaveOccurred())
-			testCases := strings.Split(strings.TrimSpace(output), "\n")
-			o.Expect(testCases).ShouldNot(o.BeEmpty())
-			sort.Strings(testCases)
+			defaultDomain, err := getDefaultIngressClusterDomainName(oc, time.Minute)
+			o.Expect(err).NotTo(o.HaveOccurred(), "failed to find default domain name")
 
-			grpcInteropExecInClusterRouteTests(oc, testCases, gRPCInteropTestCaseIterations)
-			grpcInteropExecInClusterServiceTests(oc, testCases, gRPCInteropTestCaseIterations)
-			grpcInteropExecOutOfClusterRouteTests(oc, testCases, gRPCInteropTestCaseIterations)
+			g.By("Locating the canary image reference")
+			image, err := getCanaryImage(oc)
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			g.By("Creating grpc-interop test service")
+			err = oc.Run("new-app").Args("-f", grpcServiceConfigPath, "-p", "IMAGE="+image).Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			g.By("Waiting for grpc-interop pod to be running")
+			e2e.ExpectNoError(e2epod.WaitForPodRunningInNamespaceSlow(oc.KubeClient(), "grpc-interop", oc.Namespace()), "grpc-interop backend server pod not running")
+
+			// certificate start and end time are very
+			// lenient to avoid any clock drift between
+			// between the test machine and the cluster
+			// under test.
+			notBefore := time.Now().Add(-24 * time.Hour)
+			notAfter := time.Now().Add(24 * time.Hour)
+
+			// Generate crt/key for routes that need them.
+			_, tlsCrtData, tlsPrivateKey, err := certgen.GenerateKeyPair(notBefore, notAfter)
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			derKey, err := certgen.MarshalPrivateKeyToDERFormat(tlsPrivateKey)
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			pemCrt, err := certgen.MarshalCertToPEMString(tlsCrtData)
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			shardFQDN := oc.Namespace() + "." + defaultDomain
+
+			g.By("Creating routes to test for gRPC interoperability")
+			err = oc.Run("new-app").Args("-f", grpcRoutesConfigPath,
+				"-p", "DOMAIN="+shardFQDN,
+				"-p", "TLS_CRT="+pemCrt,
+				"-p", "TLS_KEY="+derKey,
+				"-p", "TYPE="+oc.Namespace()).Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			g.By("Creating a test-specific router shard")
+			shardConfigPath, err = shard.DeployNewRouterShard(oc, 10*time.Minute, shard.Config{
+				FixturePath: grpcRouterShardConfigPath,
+				Domain:      shardFQDN,
+				Type:        oc.Namespace(),
+			})
+			o.Expect(err).NotTo(o.HaveOccurred(), "new router shard did not rollout")
+
+			// Shard is using a namespace selector so
+			// label the test namespace to match.
+			g.By("By labelling the namespace")
+			err = oc.AsAdmin().Run("label").Args("namespace", oc.Namespace(), "type="+oc.Namespace()).Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			g.By("Getting LB service")
+			shardService, err := getRouterService(oc, 5*time.Minute, "router-"+oc.Namespace())
+			o.Expect(err).NotTo(o.HaveOccurred())
+			o.Expect(shardService).NotTo(o.BeNil())
+			o.Expect(shardService.Status.LoadBalancer.Ingress).To(o.Not(o.BeEmpty()))
+
+			testCases := []string{
+				"cancel_after_begin",
+				"cancel_after_first_response",
+				"client_streaming",
+				"custom_metadata",
+				"empty_unary",
+				"large_unary",
+				"ping_pong",
+				"server_streaming",
+				"special_status_message",
+				"status_code_and_message",
+				"timeout_on_sleeping_server",
+				"unimplemented_method",
+				"unimplemented_service",
+			}
+
+			for _, routeType := range []routev1.TLSTerminationType{
+				routev1.TLSTerminationEdge,
+				routev1.TLSTerminationReencrypt,
+				routev1.TLSTerminationPassthrough,
+			} {
+				if routeType == routev1.TLSTerminationEdge {
+					e2e.Logf("skipping %v tests - needs https://github.com/openshift/router/pull/104", routeType)
+					continue
+				}
+
+				var addrs []string
+
+				if len(shardService.Status.LoadBalancer.Ingress[0].Hostname) > 0 {
+					g.By("Waiting for LB hostname to register in DNS")
+					addrs, err = resolveHost(oc, time.Minute, 15*time.Minute, shardService.Status.LoadBalancer.Ingress[0].Hostname)
+					o.Expect(err).NotTo(o.HaveOccurred())
+					o.Expect(addrs).NotTo(o.BeEmpty())
+				} else {
+					addrs = append(addrs, shardService.Status.LoadBalancer.Ingress[0].IP)
+				}
+
+				g.By("Waiting for route hostname to register in DNS")
+				host := fmt.Sprintf("grpc-interop-%s.%s", routeType, shardFQDN)
+				addrs, err = resolveHostAsAddress(oc, time.Minute, 15*time.Minute, host, addrs[0])
+				o.Expect(err).NotTo(o.HaveOccurred())
+				o.Expect(addrs).NotTo(o.BeEmpty())
+
+				err := grpcExecTestCases(oc, routeType, 5*time.Minute, testCases...)
+				o.Expect(err).NotTo(o.HaveOccurred())
+			}
 		})
 	})
 })
 
-// grpcInteropClientShellCmd construct the grpc-client command to run
-// within the cluster.
-func grpcInteropClientShellCmd(host string, port int, useTLS bool, caCert string, insecure bool, count int) string {
-	cmd := fmt.Sprintf("/workdir/grpc-client -host %q -port %v", host, port)
-	if count > 0 {
-		cmd = fmt.Sprintf("%s -count %v", cmd, count)
+// grpcExecTestCases run gRPC interop test cases.
+func grpcExecTestCases(oc *exutil.CLI, routeType routev1.TLSTerminationType, timeout time.Duration, testCases ...string) error {
+	dialParams := grpcinterop.DialParams{
+		Host:     getHostnameForRoute(oc, fmt.Sprintf("grpc-interop-%s", routeType)),
+		Port:     443,
+		UseTLS:   true,
+		Insecure: true,
 	}
-	if useTLS {
-		cmd = fmt.Sprintf("%s -tls", cmd)
-	}
-	if caCert != "" {
-		cmd = fmt.Sprintf("%s -ca-cert %q", cmd, caCert)
-	}
-	if insecure {
-		cmd = fmt.Sprintf("%s -insecure", cmd)
-	}
-	return cmd
-}
 
-// grpcInteropExecInClusterRouteTests run gRPC interop tests using routes
-// from a POD within the test cluster.
-func grpcInteropExecInClusterRouteTests(oc *exutil.CLI, testCases []string, iterations int) {
-	for _, route := range []routev1.TLSTerminationType{
-		routev1.TLSTerminationEdge,
-		routev1.TLSTerminationPassthrough,
-		routev1.TLSTerminationReencrypt,
-	} {
-		if route == routev1.TLSTerminationEdge {
-			e2e.Logf("skipping %v tests - needs https://github.com/openshift/router/pull/104", route)
-			continue
-		}
+	for _, name := range testCases {
+		e2e.Logf("Running gRPC interop test case %q via host %q", name, dialParams.Host)
 
-		host := getHostnameForRoute(oc, fmt.Sprintf("grpc-interop-%s", route))
-		cmd := grpcInteropClientShellCmd(host, 443, true, "", true, iterations) + " " + strings.Join(testCases, " ")
-		e2e.Logf("Running gRPC interop tests %v in the cluster using route %q", testCases, host)
-		_, err := grpcInteropExecClientShellCmd(oc.KubeFramework().Namespace.Name, gRPCInteropTestTimeout, cmd)
-		o.Expect(err).NotTo(o.HaveOccurred())
-	}
-}
-
-// grpcInteropExecInClusterServiceTests run gRPC interop tests against the
-// internal service from a POD within the test cluster.
-func grpcInteropExecInClusterServiceTests(oc *exutil.CLI, testCases []string, iterations int) {
-	for _, tc := range []struct {
-		port     int
-		useTLS   bool
-		caCert   string
-		insecure bool
-	}{{
-		port:   1110, // h2c
-		useTLS: false,
-	}, {
-		port:   8443, // h2
-		useTLS: true,
-		caCert: "/etc/service-ca/service-ca.crt",
-	}} {
-		svc := fmt.Sprintf("grpc-interop.%s.svc", oc.KubeFramework().Namespace.Name)
-		cmd := grpcInteropClientShellCmd(svc, tc.port, tc.useTLS, tc.caCert, tc.insecure, iterations) + " " + strings.Join(testCases, " ")
-		e2e.Logf("Running gRPC interop tests %v in the cluster using service %q", testCases, svc)
-		_, err := grpcInteropExecClientShellCmd(oc.KubeFramework().Namespace.Name, gRPCInteropTestTimeout, cmd)
-		o.Expect(err).NotTo(o.HaveOccurred())
-	}
-}
-
-// grpcInteropExecOutOfClusterRouteTests run gRPC interop tests using
-// routes and from outside of the test cluster.
-func grpcInteropExecOutOfClusterRouteTests(oc *exutil.CLI, testCases []string, iterations int) {
-	for _, route := range []routev1.TLSTerminationType{
-		routev1.TLSTerminationEdge,
-		routev1.TLSTerminationReencrypt,
-		routev1.TLSTerminationPassthrough,
-	} {
-		if route == routev1.TLSTerminationEdge {
-			e2e.Logf("skipping %v tests - needs https://github.com/openshift/router/pull/104", route)
-			continue
-		}
-
-		dialParams := grpcinterop.DialParams{
-			Host:     getHostnameForRoute(oc, fmt.Sprintf("grpc-interop-%s", route)),
-			Port:     443,
-			UseTLS:   true,
-			Insecure: true,
-		}
-
-		conn, err := grpcinterop.Dial(dialParams)
-		o.Expect(err).NotTo(o.HaveOccurred())
-
-		for i := 0; i < iterations; i++ {
-			e2e.Logf("[%v/%v] Running gRPC interop test cases %v using route %q", i+1, iterations, testCases, dialParams.Host)
-			for _, name := range testCases {
-				err := grpcinterop.ExecTestCase(conn, name)
-				o.Expect(err).NotTo(o.HaveOccurred())
+		if err := wait.PollImmediate(time.Second, timeout, func() (bool, error) {
+			e2e.Logf("Dialling: %+v", dialParams)
+			conn, err := grpcinterop.Dial(dialParams)
+			if err != nil {
+				e2e.Logf("error: connection failed: %v, retrying...", err)
+				return false, nil
 			}
+
+			defer func() {
+				conn.Close()
+			}()
+
+			if err := grpcinterop.ExecTestCase(conn, name); err != nil {
+				e2e.Logf("error: running gRPC interop test case %q through %q: %v, retrying...", name, dialParams.Host, err)
+				return false, nil
+			}
+
+			return true, nil
+		}); err != nil {
+			return err
 		}
-
-		o.Expect(conn.Close()).NotTo(o.HaveOccurred())
 	}
-}
 
-// grpcInteropExecClientShellCmd runs the given cmd in the context of
-// the "client-shell" container in the "grpc-interop" POD.
-func grpcInteropExecClientShellCmd(ns string, timeout time.Duration, cmd string) (string, error) {
-	return e2e.RunKubectl(ns, "exec", fmt.Sprintf("--namespace=%v", ns), "grpc-interop", "-c", "client-shell", "--", "/bin/sh", "-x", "-c", fmt.Sprintf("timeout %v %s", timeout.Seconds(), cmd))
+	return nil
 }
