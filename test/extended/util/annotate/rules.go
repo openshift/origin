873ad796a53347f658661e0b6e51603df1316554
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2021-05-20 17:01:43 -0400

    Remove network policy tests

--- test/extended/util/annotate/rules.go
+++ test/extended/util/annotate/rules.go
@@ -61,6 +61,10 @@ var (
 
 			// https://bugzilla.redhat.com/show_bug.cgi?id=1957894
 			`\[sig-node\] Container Runtime blackbox test when running a container with a new image should be able to pull from private registry with secret`,
+
+			// Broken for metal-ipi-ovn-ipv6
+			// https://bugzilla.redhat.com/show_bug.cgi?id=1962950
+			`\[Feature:NetworkPolicy\]`,
 		},
 		// tests that may work, but we don't support them
 		"[Disabled:Unsupported]": {},
