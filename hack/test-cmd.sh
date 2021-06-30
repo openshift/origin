0e37d916e2c63cba4720e4701a882daee40ddca5
Brenton Leanhardt <bleanhar@redhat.com>
2021-06-30 15:02:38 -0400

    Loading the imagepullsecret for test-cmd.sh
    In CI this relies on the docker creds are in the homedir.  It then
    mounts them into the test container.

--- hack/test-cmd.sh
+++ hack/test-cmd.sh
@@ -34,6 +34,30 @@ function find_tests() {
 }
 tests=( $(find_tests ${1:-.*}) )
 
+# install_image_pull_secret will look for ~/docker/config.json and create an image pull secret.  If no suitable credentials are found a warning will be displayed.  It assumes a running server.
+#
+# Globals:
+#  - ADMIN_KUBECONFIG
+#  - REAL_HOME
+# Arguments:
+#  None
+# Returns:
+#  None
+function install_image_pull_secret() {
+	local ns="${1:-}"
+	local dockerconfig="${REAL_HOME}/.docker/config.json"
+
+	if [[ -f $dockerconfig ]]; then
+		os::log::debug "Installing imagestreamsecret..."
+		oc create secret generic imagestreamsecret --dry-run -o yaml --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=${dockerconfig} -n ${ns} --config=$ADMIN_KUBECONFIG >& /tmp/secret.yaml
+		oc apply -f /tmp/secret.yaml -n ${ns} --config=$ADMIN_KUBECONFIG
+	else
+		os::log::debug "No docker credentials found.  Image pull secret will not be installed."
+	fi
+}
+readonly -f install_image_pull_secret
+
+
 # deconflict ports so we can run in parallel with other test suites
 export API_PORT=${API_PORT:-28443}
 export ETCD_PORT=${ETCD_PORT:-24001}
@@ -66,6 +90,7 @@ os::start::master
 
 os::start::registry
 
+export REAL_HOME=$HOME
 export HOME="${FAKE_HOME_DIR}"
 mkdir -p "${HOME}/.kube"
 cp "${MASTER_CONFIG_DIR}/admin.kubeconfig" "${HOME}/.kube/non-default-config"
@@ -87,6 +112,7 @@ for test in "${tests[@]}"; do
   os::cmd::expect_success "oc login --server='${KUBERNETES_MASTER}' --certificate-authority='${MASTER_CONFIG_DIR}/ca.crt' -u test-user -p anything"
   os::cmd::expect_success "oc project ${CLUSTER_ADMIN_CONTEXT}"
   os::cmd::expect_success "oc new-project '${namespace}'"
+  install_image_pull_secret "${namespace}"
   # wait for the project cache to catch up and correctly list us in the new project
   os::cmd::try_until_text "oc get projects -o name" "project.project.openshift.io/${namespace}"
   os::test::junit::declare_suite_end
