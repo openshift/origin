6a53f638f89f90f33013e866f7e37b2d7717069f
David Eads <deads@redhat.com>
2021-08-10 11:04:58 -0400

    track the final state of pods and events from e2e tests for later debugging

--- pkg/monitor/types.go
+++ pkg/monitor/types.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/openshift/origin/pkg/monitor/monitorapi"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 type IntervalCreationFunc func(intervals monitorapi.Intervals, beginning, end time.Time) monitorapi.Intervals
@@ -13,9 +14,15 @@ type SamplerFunc func(time.Time) []*monitorapi.Condition
 type Interface interface {
 	Intervals(from, to time.Time) monitorapi.Intervals
 	Conditions(from, to time.Time) monitorapi.Intervals
+	CurrentResourceState() monitorapi.ResourcesMap
 }
 
 type Recorder interface {
+	// RecordResource stores a resource for later serialization.  Deletion is not tracked, so this can be used
+	// to determine the final state of resource that are deleted in a namespace.
+	// Annotations are added to indicate number of updates and the number of recreates.
+	RecordResource(resourceType string, obj runtime.Object)
+
 	Record(conditions ...monitorapi.Condition)
 	RecordAt(t time.Time, conditions ...monitorapi.Condition)
 
