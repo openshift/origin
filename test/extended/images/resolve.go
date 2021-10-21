dfcd83c629bf907419941b490b7a56dc9bf869d3
Oleg Bulatov <obulatov@redhat.com>
2021-10-21 15:46:13 +0200

    Add more tests for image policy

--- test/extended/images/resolve.go
+++ test/extended/images/resolve.go
@@ -8,7 +8,8 @@ import (
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
 
-	appsv1 "k8s.io/api/apps/v1"
+	appsv1 "github.com/openshift/api/apps/v1"
+	kappsv1 "k8s.io/api/apps/v1"
 	kbatchv1 "k8s.io/api/batch/v1"
 	kbatchv1beta1 "k8s.io/api/batch/v1beta1"
 	kapiv1 "k8s.io/api/core/v1"
@@ -72,9 +73,9 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		defer func() { oc.KubeClient().CoreV1().Pods(oc.Namespace()).Delete(ctx, pod.Name, metav1.DeleteOptions{}) }()
 
 		// replica sets should auto replace local references
-		rs, err := oc.KubeClient().AppsV1().ReplicaSets(oc.Namespace()).Create(ctx, &appsv1.ReplicaSet{
+		rs, err := oc.KubeClient().AppsV1().ReplicaSets(oc.Namespace()).Create(ctx, &kappsv1.ReplicaSet{
 			ObjectMeta: metav1.ObjectMeta{Name: "resolve"},
-			Spec: appsv1.ReplicaSetSpec{
+			Spec: kappsv1.ReplicaSetSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve": "true"},
 				},
@@ -96,6 +97,125 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		defer func() {
 			oc.KubeClient().AppsV1().ReplicaSets(oc.Namespace()).Delete(ctx, rs.Name, metav1.DeleteOptions{})
 		}()
+
+		// replica sets should auto replace local references on update
+		rs, err = oc.KubeClient().AppsV1().ReplicaSets(oc.Namespace()).Patch(context.Background(), rs.Name, types.StrategicMergePatchType, []byte(`{"spec": {"template": {"spec": {"containers": [{"name": "resolve", "image": "busybox:latest"}]}}}}`), metav1.PatchOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(rs.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
+
+		// daemon sets should auto replace local references
+		ds, err := oc.KubeClient().AppsV1().DaemonSets(oc.Namespace()).Create(ctx, &kappsv1.DaemonSet{
+			ObjectMeta: metav1.ObjectMeta{Name: "resolve"},
+			Spec: kappsv1.DaemonSetSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{"resolve": "true"},
+				},
+				Template: kapiv1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{"resolve": "true"},
+					},
+					Spec: kapiv1.PodSpec{
+						TerminationGracePeriodSeconds: &one,
+						Containers: []kapiv1.Container{
+							{Name: "resolve", Image: "busybox:latest", Command: []string{"/bin/true"}},
+						},
+					},
+				},
+			},
+		}, metav1.CreateOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(ds.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
+		defer func() {
+			oc.KubeClient().AppsV1().DaemonSets(oc.Namespace()).Delete(ctx, ds.Name, metav1.DeleteOptions{})
+		}()
+
+		// daemon sets should auto replace local references on update
+		ds, err = oc.KubeClient().AppsV1().DaemonSets(oc.Namespace()).Patch(context.Background(), ds.Name, types.StrategicMergePatchType, []byte(`{"spec": {"template": {"spec": {"containers": [{"name": "resolve", "image": "busybox:latest"}]}}}}`), metav1.PatchOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(ds.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
+
+		// stateful sets should auto replace local references
+		sts, err := oc.KubeClient().AppsV1().StatefulSets(oc.Namespace()).Create(ctx, &kappsv1.StatefulSet{
+			ObjectMeta: metav1.ObjectMeta{Name: "resolve"},
+			Spec: kappsv1.StatefulSetSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{"resolve": "true"},
+				},
+				Template: kapiv1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{"resolve": "true"},
+					},
+					Spec: kapiv1.PodSpec{
+						TerminationGracePeriodSeconds: &one,
+						Containers: []kapiv1.Container{
+							{Name: "resolve", Image: "busybox:latest", Command: []string{"/bin/true"}},
+						},
+					},
+				},
+			},
+		}, metav1.CreateOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(sts.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
+		defer func() {
+			oc.KubeClient().AppsV1().StatefulSets(oc.Namespace()).Delete(ctx, sts.Name, metav1.DeleteOptions{})
+		}()
+
+		// stateful sets should auto replace local references on update
+		sts, err = oc.KubeClient().AppsV1().StatefulSets(oc.Namespace()).Patch(context.Background(), sts.Name, types.StrategicMergePatchType, []byte(`{"spec": {"template": {"spec": {"containers": [{"name": "resolve", "image": "busybox:latest"}]}}}}`), metav1.PatchOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(sts.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
+	})
+
+	g.It("should update OpenShift object image fields when local names are on", func() {
+		err := oc.Run("tag").Args(k8simage.GetE2EImage(k8simage.BusyBox), "busybox:latest").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		err = oc.Run("set", "image-lookup").Args("busybox").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		is, err := oc.ImageClient().ImageV1().ImageStreams(oc.Namespace()).Get(ctx, "busybox", metav1.GetOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(is.Spec.LookupPolicy.Local).To(o.BeTrue())
+
+		var internalImageReference string
+		var lastErr error
+		err = wait.PollImmediate(5*time.Second, 2*time.Minute, func() (bool, error) {
+			tag, err := oc.ImageClient().ImageV1().ImageTags(oc.Namespace()).Get(ctx, "busybox:latest", metav1.GetOptions{})
+			if err != nil || tag.Image == nil {
+				lastErr = err
+				return false, nil
+			}
+			internalImageReference = tag.Image.DockerImageReference
+			return true, nil
+		})
+		o.Expect(err).NotTo(o.HaveOccurred(), fmt.Sprintf("unable to wait for image to be imported: %v", lastErr))
+
+		// deployment configs should auto replace local references
+		dc, err := oc.AppsClient().AppsV1().DeploymentConfigs(oc.Namespace()).Create(ctx, &appsv1.DeploymentConfig{
+			ObjectMeta: metav1.ObjectMeta{Name: "resolve"},
+			Spec: appsv1.DeploymentConfigSpec{
+				Selector: map[string]string{"resolve": "true"},
+				Template: &kapiv1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{"resolve": "true"},
+					},
+					Spec: kapiv1.PodSpec{
+						TerminationGracePeriodSeconds: &one,
+						Containers: []kapiv1.Container{
+							{Name: "resolve", Image: "busybox:latest", Command: []string{"/bin/true"}},
+						},
+					},
+				},
+			},
+		}, metav1.CreateOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(dc.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
+		defer func() {
+			oc.AppsClient().AppsV1().DeploymentConfigs(oc.Namespace()).Delete(ctx, dc.Name, metav1.DeleteOptions{})
+		}()
+
+		dc, err = oc.AppsClient().AppsV1().DeploymentConfigs(oc.Namespace()).Patch(context.Background(), dc.Name, types.StrategicMergePatchType, []byte(`{"spec": {"template": {"spec": {"containers": [{"name": "resolve", "image": "busybox:latest"}]}}}}`), metav1.PatchOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(dc.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
 	})
 
 	g.It("should perform lookup when the object has the resolve-names annotation", func() {
@@ -142,14 +262,14 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		o.Expect(pod.Spec.Containers[1].Image).To(o.HaveSuffix("/" + oc.Namespace() + "/busybox:unknown"))
 
 		g.By("auto replacing local references on ReplicaSets")
-		rs, err := oc.KubeClient().AppsV1().ReplicaSets(oc.Namespace()).Create(ctx, &appsv1.ReplicaSet{
+		rs, err := oc.KubeClient().AppsV1().ReplicaSets(oc.Namespace()).Create(ctx, &kappsv1.ReplicaSet{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "resolve",
 				Annotations: map[string]string{
 					"alpha.image.policy.openshift.io/resolve-names": "*",
 				},
 			},
-			Spec: appsv1.ReplicaSetSpec{
+			Spec: kappsv1.ReplicaSetSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve": "true"},
 				},
@@ -168,14 +288,14 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		o.Expect(rs.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
 
 		g.By("auto replacing local references on Deployments")
-		deployment, err := oc.KubeClient().AppsV1().Deployments(oc.Namespace()).Create(ctx, &appsv1.Deployment{
+		deployment, err := oc.KubeClient().AppsV1().Deployments(oc.Namespace()).Create(ctx, &kappsv1.Deployment{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "resolve",
 				Annotations: map[string]string{
 					"alpha.image.policy.openshift.io/resolve-names": "*",
 				},
 			},
-			Spec: appsv1.DeploymentSpec{
+			Spec: kappsv1.DeploymentSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve": "true"},
 				},
@@ -194,11 +314,11 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		o.Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
 
 		g.By("auto replacing local references on Deployments (in pod template)")
-		deployment, err = oc.KubeClient().AppsV1().Deployments(oc.Namespace()).Create(ctx, &appsv1.Deployment{
+		deployment, err = oc.KubeClient().AppsV1().Deployments(oc.Namespace()).Create(ctx, &kappsv1.Deployment{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "resolve-template",
 			},
-			Spec: appsv1.DeploymentSpec{
+			Spec: kappsv1.DeploymentSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve-template": "true"},
 				},
@@ -222,14 +342,14 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		o.Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
 
 		g.By("auto replacing local references on DaemonSets")
-		daemonset, err := oc.KubeClient().AppsV1().DaemonSets(oc.Namespace()).Create(ctx, &appsv1.DaemonSet{
+		daemonset, err := oc.KubeClient().AppsV1().DaemonSets(oc.Namespace()).Create(ctx, &kappsv1.DaemonSet{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "resolve",
 				Annotations: map[string]string{
 					"alpha.image.policy.openshift.io/resolve-names": "*",
 				},
 			},
-			Spec: appsv1.DaemonSetSpec{
+			Spec: kappsv1.DaemonSetSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve": "true"},
 				},
@@ -250,11 +370,11 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		o.Expect(daemonset.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
 
 		g.By("auto replacing local references on DaemonSets (in pod template)")
-		daemonset, err = oc.KubeClient().AppsV1().DaemonSets(oc.Namespace()).Create(ctx, &appsv1.DaemonSet{
+		daemonset, err = oc.KubeClient().AppsV1().DaemonSets(oc.Namespace()).Create(ctx, &kappsv1.DaemonSet{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "resolve-template",
 			},
-			Spec: appsv1.DaemonSetSpec{
+			Spec: kappsv1.DaemonSetSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve-template": "true"},
 				},
@@ -278,14 +398,14 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		o.Expect(daemonset.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
 
 		g.By("auto replacing local references on StatefulSets")
-		statefulset, err := oc.KubeClient().AppsV1().StatefulSets(oc.Namespace()).Create(ctx, &appsv1.StatefulSet{
+		statefulset, err := oc.KubeClient().AppsV1().StatefulSets(oc.Namespace()).Create(ctx, &kappsv1.StatefulSet{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "resolve",
 				Annotations: map[string]string{
 					"alpha.image.policy.openshift.io/resolve-names": "*",
 				},
 			},
-			Spec: appsv1.StatefulSetSpec{
+			Spec: kappsv1.StatefulSetSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve": "true"},
 				},
@@ -306,11 +426,11 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		o.Expect(statefulset.Spec.Template.Spec.Containers[0].Image).To(o.Equal(internalImageReference))
 
 		g.By("auto replacing local references on StatefulSets (in pod template)")
-		statefulset, err = oc.KubeClient().AppsV1().StatefulSets(oc.Namespace()).Create(ctx, &appsv1.StatefulSet{
+		statefulset, err = oc.KubeClient().AppsV1().StatefulSets(oc.Namespace()).Create(ctx, &kappsv1.StatefulSet{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "resolve-template",
 			},
-			Spec: appsv1.StatefulSetSpec{
+			Spec: kappsv1.StatefulSetSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve-template": "true"},
 				},
@@ -461,11 +581,11 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageLookup] Image policy", func
 		})
 		o.Expect(err).NotTo(o.HaveOccurred(), fmt.Sprintf("unable to wait for image to be imported: %v", lastErr))
 
-		deployment, err := oc.KubeClient().AppsV1().Deployments(oc.Namespace()).Create(ctx, &appsv1.Deployment{
+		deployment, err := oc.KubeClient().AppsV1().Deployments(oc.Namespace()).Create(ctx, &kappsv1.Deployment{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "resolve",
 			},
-			Spec: appsv1.DeploymentSpec{
+			Spec: kappsv1.DeploymentSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{"resolve": "true"},
 				},
