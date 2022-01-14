a882ff31b9d54dd7b14c59f3affc325c44793b7e
Damien Grisonnet <dgrisonn@redhat.com>
2022-01-14 11:50:33 +0100

    test/extended: refine apiserver latency thresholds
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

--- test/extended/apiserver/latency.go
+++ test/extended/apiserver/latency.go
@@ -14,38 +14,41 @@ import (
 	promhelper "github.com/openshift/origin/test/extended/util/prometheus"
 )
 
+const (
+	apiserverReadVerbs  = "GET|LIST"
+	apiserverWriteVerbs = "POST|PUT|PATCH|DELETE"
+)
+
 var _ = g.Describe("[sig-api-machinery][Late] API Server latency", func() {
 	defer g.GinkgoRecover()
 
 	oc := exutil.NewCLIWithoutNamespace("apiserver-latency")
 
-	// TODO: refine latency thresholds based on actual CI run results
-	//
+	// Latency thresholds defined below are based on the following CI jobs:
+	// - https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/26739/pull-ci-openshift-origin-master-e2e-gcp/1481905976666755072
+	// - https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/26739/pull-ci-openshift-origin-master-e2e-gcp/1481879797410828288
+	// - https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/26739/pull-ci-openshift-origin-master-e2e-aws-fips/1481879797377273856
+	// - https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/26739/pull-ci-openshift-origin-master-e2e-aws-fips/1481905976616423424
 	// Latency thresholds should be calculed based on the following queries:
-	// - histogram_quantile(0.99,sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",scope=~"resource|"}[$testDuration])) by (le,verb))
-	// - histogram_quantile(0.99,sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",scope="namespace"}[$testDuration])) by (le,verb))
-	// - histogram_quantile(0.99,sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",scope="cluster"}[$testDuration])) by (le,verb))
+	// - histogram_quantile(0.99,sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",verb=~"GET|LIST"}[$testDuration])) by (le,scope))
+	// - histogram_quantile(0.99,sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",verb=~"POST|PUT|PATCH|DELETE"}[$testDuration])) by (le,scope))
 	g.It("should verify that the apiserver requests latency are within expected thresholds", func() {
 		promClient := oc.NewPrometheusClient(context.TODO())
 
 		// 99th percentile of resource-scoped requests latency
 		scope := "resource|"
-		expectRequestLatency(promClient, "GET", scope, parseDuration("100ms"))
-		expectRequestLatency(promClient, "POST", scope, parseDuration("100ms"))
-		expectRequestLatency(promClient, "PUT", scope, parseDuration("100ms"))
-		expectRequestLatency(promClient, "PATCH", scope, parseDuration("100ms"))
-		expectRequestLatency(promClient, "DELETE", scope, parseDuration("100ms"))
+		expectRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("200ms"))    // got 130ms
+		expectRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("1s250ms")) // got 850ms
 
 		// 99th percentile of namespace-scoped requests latency
 		scope = "namespace"
-		expectRequestLatency(promClient, "LIST", scope, parseDuration("500ms"))
-		expectRequestLatency(promClient, "POST", scope, parseDuration("500ms"))
-		expectRequestLatency(promClient, "DELETE", scope, parseDuration("500ms"))
+		expectRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("150ms"))    // got 100ms
+		expectRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("1s350ms")) // got 900ms
 
 		// 99th percentile of cluster-scoped requests latency
 		scope = "cluster"
-		expectRequestLatency(promClient, "LIST", scope, parseDuration("5s"))
-		expectRequestLatency(promClient, "POST", scope, parseDuration("5s"))
+		expectRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("1s350ms")) // got 900ms
+		expectRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("300ms"))  // got 200ms
 	})
 })
 
@@ -60,7 +63,7 @@ func expectRequestLatency(prometheusClient prometheusv1.API, verb string, scope 
 		context.TODO(),
 		prometheusClient,
 		fmt.Sprintf(
-			`histogram_quantile(0.99, sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",verb="%s",scope=~"%s"}[%s])) by (le))`,
+			`histogram_quantile(0.99, sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",verb=~"%s",scope=~"%s"}[%s])) by (le))`,
 			verb, scope, exutil.DurationSinceStartInSeconds().String(),
 		),
 	)
