d67f738b82fac7f7a533dee7eb4351f296f46275
Damien Grisonnet <dgrisonn@redhat.com>
2022-01-27 19:21:02 +0100

    test/extended: check apiserver average latency
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

--- test/extended/apiserver/latency.go
+++ test/extended/apiserver/latency.go
@@ -10,6 +10,7 @@ import (
 
 	prometheusv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 
+	testresult "github.com/openshift/origin/pkg/test/ginkgo/result"
 	exutil "github.com/openshift/origin/test/extended/util"
 	promhelper "github.com/openshift/origin/test/extended/util/prometheus"
 )
@@ -24,31 +25,37 @@ var _ = g.Describe("[sig-api-machinery][Late] API Server latency", func() {
 
 	oc := exutil.NewCLIWithoutNamespace("apiserver-latency")
 
-	// Latency thresholds defined below are based on the following CI jobs:
-	// - https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/26739/pull-ci-openshift-origin-master-e2e-gcp/1481905976666755072
-	// - https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/26739/pull-ci-openshift-origin-master-e2e-gcp/1481879797410828288
-	// - https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/26739/pull-ci-openshift-origin-master-e2e-aws-fips/1481879797377273856
-	// - https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/26739/pull-ci-openshift-origin-master-e2e-aws-fips/1481905976616423424
+	// Latency thresholds defined below are based on the following search.ci graphs:
+	// - https://search.ci.openshift.org/graph/metrics?metric=cluster%3Aapi%3Aread%3Arequests%3Alatency%3Atotal%3Aavg&job=periodic-ci-openshift-release-master-ci-4.10-e2e-aws&job=periodic-ci-openshift-release-master-ci-4.10-e2e-azure&job=periodic-ci-openshift-release-master-ci-4.10-e2e-gcp&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-aws-upgrade&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-azure-upgrade&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-gcp-upgrade
+	// - https://search.ci.openshift.org/graph/metrics?metric=cluster%3Aapi%3Aread%3Arequests%3Alatency%3Atotal%3Aquantile&job=periodic-ci-openshift-release-master-ci-4.10-e2e-aws&job=periodic-ci-openshift-release-master-ci-4.10-e2e-azure&job=periodic-ci-openshift-release-master-ci-4.10-e2e-gcp&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-aws-upgrade&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-azure-upgrade&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-gcp-upgrade
+	// - https://search.ci.openshift.org/graph/metrics?metric=cluster%3Aapi%3Awrite%3Arequests%3Alatency%3Atotal%3Aavg&job=periodic-ci-openshift-release-master-ci-4.10-e2e-aws&job=periodic-ci-openshift-release-master-ci-4.10-e2e-azure&job=periodic-ci-openshift-release-master-ci-4.10-e2e-gcp&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-aws-upgrade&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-azure-upgrade&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-gcp-upgrade
+	// - https://search.ci.openshift.org/graph/metrics?metric=cluster%3Aapi%3Awrite%3Arequests%3Alatency%3Atotal%3Aquantile&job=periodic-ci-openshift-release-master-ci-4.10-e2e-aws&job=periodic-ci-openshift-release-master-ci-4.10-e2e-azure&job=periodic-ci-openshift-release-master-ci-4.10-e2e-gcp&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-aws-upgrade&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-azure-upgrade&job=periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-gcp-upgrade
 	// Latency thresholds should be calculed based on the following queries:
 	// - histogram_quantile(0.99,sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",verb=~"GET|LIST"}[$testDuration])) by (le,scope))
 	// - histogram_quantile(0.99,sum(rate(apiserver_request_duration_seconds_bucket{job="apiserver",verb=~"POST|PUT|PATCH|DELETE"}[$testDuration])) by (le,scope))
 	g.It("should verify that the apiserver requests latency are within expected thresholds", func() {
 		promClient := oc.NewPrometheusClient(context.TODO())
 
-		// 99th percentile of resource-scoped requests latency
+		// resource-scoped requests latency
 		scope := "resource|"
-		expectRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("200ms"))    // got 130ms
-		expectRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("1s250ms")) // got 850ms
+		expectAverageRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("275ms"))         // got 200ms in gcp
+		expectAverageRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("120ms"))        // got 90ms in gcp
+		expect99PercentileRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("1s"))       // got 750ms in azure
+		expect99PercentileRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("1s600ms")) // got 1s200ms in azure
 
-		// 99th percentile of namespace-scoped requests latency
+		// namespace-scoped requests latency
 		scope = "namespace"
-		expectRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("150ms"))    // got 100ms
-		expectRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("1s350ms")) // got 900ms
+		expectAverageRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("50ms"))          // got 35ms in azure
+		expectAverageRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("100ms"))        // got 75ms in azure
+		expect99PercentileRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("1s200ms"))  // got 900ms in azure
+		expect99PercentileRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("1s300ms")) // got 1s in azure
 
-		// 99th percentile of cluster-scoped requests latency
+		// cluster-scoped requests latency
 		scope = "cluster"
-		expectRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("1s350ms")) // got 900ms
-		expectRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("300ms"))  // got 200ms
+		expectAverageRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("250ms"))         // got 180ms in azure
+		expectAverageRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("120ms"))        // got 90ms in azure
+		expect99PercentileRequestLatency(promClient, apiserverReadVerbs, scope, parseDuration("6s"))       // got 4s in gcp
+		expect99PercentileRequestLatency(promClient, apiserverWriteVerbs, scope, parseDuration("1s300ms")) // got 1s in gcp
 	})
 })
 
@@ -58,7 +65,26 @@ func parseDuration(s string) time.Duration {
 	return d
 }
 
-func expectRequestLatency(prometheusClient prometheusv1.API, verb string, scope string, threshold time.Duration) {
+func expectAverageRequestLatency(prometheusClient prometheusv1.API, verb string, scope string, threshold time.Duration) {
+	resp, err := promhelper.RunQuery(
+		context.TODO(),
+		prometheusClient,
+		fmt.Sprintf(
+			`sum(rate(apiserver_request_duration_seconds_sum{job="apiserver",verb=~"%s",scope=~"%s"}[%s])) / sum(rate(apiserver_request_duration_seconds_count{job="apiserver",verb=~"%s",scope=~"%s"}[%s]))`,
+			verb, scope, exutil.DurationSinceStartInSeconds().String(), verb, scope, exutil.DurationSinceStartInSeconds(),
+		),
+	)
+	o.Expect(err).NotTo(o.HaveOccurred())
+	o.Expect(resp.Data.Result).NotTo(o.BeEmpty())
+
+	latency := time.Duration(float64(resp.Data.Result[0].Value) * float64(time.Second))
+	if latency.Seconds() < threshold.Seconds() {
+		// Flake to gather more insights on the impact of the test.
+		testresult.Flakef("expected average apiserver request latency with verb=%q and scope=%q to be less than %s, got %s", verb, scope, threshold, latency)
+	}
+}
+
+func expect99PercentileRequestLatency(prometheusClient prometheusv1.API, verb string, scope string, threshold time.Duration) {
 	resp, err := promhelper.RunQuery(
 		context.TODO(),
 		prometheusClient,
@@ -67,12 +93,12 @@ func expectRequestLatency(prometheusClient prometheusv1.API, verb string, scope 
 			verb, scope, exutil.DurationSinceStartInSeconds().String(),
 		),
 	)
-
 	o.Expect(err).NotTo(o.HaveOccurred())
 	o.Expect(resp.Data.Result).NotTo(o.BeEmpty())
 
 	latency := time.Duration(float64(resp.Data.Result[0].Value) * float64(time.Second))
-	o.Expect(latency.Seconds()).Should(o.BeNumerically("<", threshold.Seconds()),
-		fmt.Sprintf("expected 99th percentile of apiserver request latency with verb=%q and scope=%q to be less than %s, got %s", verb, scope, threshold, latency),
-	)
+	if latency.Seconds() > threshold.Seconds() {
+		// Flake to gather more insights on the impact of the test.
+		testresult.Flakef("expected 99th percentile of apiserver request latency with verb=%q and scope=%q to be less than %s, got %s", verb, scope, threshold, latency)
+	}
 }
