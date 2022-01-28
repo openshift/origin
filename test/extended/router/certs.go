646a575de6190b02d0a7c1d23ba21c42d94832f9
Miciah Masters <miciah.masters@gmail.com>
2022-01-28 18:53:27 -0500

    Fix HAProxy tests on FIPS properly
    Change existing HAProxy tests to use a FIPS-compatible, 2048-bit RSA key.
    Add a new test to verify that using a FIPS-incompatible, 1024-bit RSA key
    for the router's default certificate fails on FIPS clusters and succeeds on
    non-FIPS clusters.
    
    The certificate used in this new test comes from the default certificate
    that is currently built into the router image (which is why the tests were
    failing on FIPS).  The certificate used for the already existing tests was
    generated using the following Shell commands on a RHEL 8.4 system with
    OpenSSL 1.1.1g:
    
        openssl req -x509 -newkey rsa:2048 -days 3650 -keyout ca.key -out ca.crt -nodes -subj '/C=US/ST=SC/L=Default City/O=Default Company Ltd/OU=Test CA/CN=www.exampleca.com/emailAddress=example@example.com'
        openssl req -newkey rsa:2048 -nodes -keyout tls.key -out router.csr -subj '/CN=www.example.com/ST=SC/C=US/emailAddress=example@example.com/O=Example/OU=Example'
        printf 'basicConstraints=CA:FALSE\n' > router.cnf
        openssl x509 -req -days 3650 -in router.csr -signkey tls.key -CA ca.crt -CAcreateserial -CAkey ca.key -out tls.crt -clrext -extfile router.cnf
        cat tls.{crt,key} > default_pub_keys.pem
    
    These commands generate a certificate that has the same parameters as the
    old one except for the serial number, signature algorithm, validity period,
    and key size.
    
    This commit fixes bug 2047913.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=2047913
    
    * test/extended/router/certs.go: New file with the new tests.
    * test/extended/testdata/router/router-common.yaml: Change route-1 and
    route-2 from non-TLS routes to edge-terminated TLS routes with
    insecureEdgeTerminationPolicy: Allow.
    * test/extended/testdata/router/router-override-domains.yaml:
    * test/extended/testdata/router/router-override.yaml:
    * test/extended/testdata/router/router-scoped.yaml:
    * test/extended/testdata/router/weighted-router.yaml: Add a parameter for
    the default certificate with the default value being a certificate with a
    FIPS-compatible, 2048-bit RSA key.
    * test/extended/testdata/bindata.go:
    * test/extended/util/annotate/generated/zz_generated.annotations.go:
    Regenerate.

---
+++ test/extended/router/certs.go
@@ -0,0 +1,186 @@
+package router
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	routeclientset "github.com/openshift/client-go/route/clientset/versioned"
+	exutil "github.com/openshift/origin/test/extended/util"
+	exurl "github.com/openshift/origin/test/extended/util/url"
+)
+
+var _ = g.Describe("[sig-network][Feature:Router]", func() {
+	defer g.GinkgoRecover()
+	var (
+		oc          *exutil.CLI
+		ns          string
+		routerImage string
+		isFIPS      bool
+	)
+	const (
+		pemData = `-----BEGIN CERTIFICATE-----
+MIIDIjCCAgqgAwIBAgIBBjANBgkqhkiG9w0BAQUFADCBoTELMAkGA1UEBhMCVVMx
+CzAJBgNVBAgMAlNDMRUwEwYDVQQHDAxEZWZhdWx0IENpdHkxHDAaBgNVBAoME0Rl
+ZmF1bHQgQ29tcGFueSBMdGQxEDAOBgNVBAsMB1Rlc3QgQ0ExGjAYBgNVBAMMEXd3
+dy5leGFtcGxlY2EuY29tMSIwIAYJKoZIhvcNAQkBFhNleGFtcGxlQGV4YW1wbGUu
+Y29tMB4XDTE2MDExMzE5NDA1N1oXDTI2MDExMDE5NDA1N1owfDEYMBYGA1UEAxMP
+d3d3LmV4YW1wbGUuY29tMQswCQYDVQQIEwJTQzELMAkGA1UEBhMCVVMxIjAgBgkq
+hkiG9w0BCQEWE2V4YW1wbGVAZXhhbXBsZS5jb20xEDAOBgNVBAoTB0V4YW1wbGUx
+EDAOBgNVBAsTB0V4YW1wbGUwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAM0B
+u++oHV1wcphWRbMLUft8fD7nPG95xs7UeLPphFZuShIhhdAQMpvcsFeg+Bg9PWCu
+v3jZljmk06MLvuWLfwjYfo9q/V+qOZVfTVHHbaIO5RTXJMC2Nn+ACF0kHBmNcbth
+OOgF8L854a/P8tjm1iPR++vHnkex0NH7lyosVc/vAgMBAAGjDTALMAkGA1UdEwQC
+MAAwDQYJKoZIhvcNAQEFBQADggEBADjFm5AlNH3DNT1Uzx3m66fFjqqrHEs25geT
+yA3rvBuynflEHQO95M/8wCxYVyuAx4Z1i4YDC7tx0vmOn/2GXZHY9MAj1I8KCnwt
+Jik7E2r1/yY0MrkawljOAxisXs821kJ+Z/51Ud2t5uhGxS6hJypbGspMS7OtBbw7
+8oThK7cWtCXOldNF6ruqY1agWnhRdAq5qSMnuBXuicOP0Kbtx51a1ugE3SnvQenJ
+nZxdtYUXvEsHZC/6bAtTfNh+/SwgxQJuL2ZM+VG3X2JIKY8xTDui+il7uTh422lq
+wED8uwKl+bOj6xFDyw4gWoBxRobsbFaME8pkykP1+GnKDberyAM=
+-----END CERTIFICATE-----
+-----BEGIN RSA PRIVATE KEY-----
+MIICWwIBAAKBgQDNAbvvqB1dcHKYVkWzC1H7fHw+5zxvecbO1Hiz6YRWbkoSIYXQ
+EDKb3LBXoPgYPT1grr942ZY5pNOjC77li38I2H6Pav1fqjmVX01Rx22iDuUU1yTA
+tjZ/gAhdJBwZjXG7YTjoBfC/OeGvz/LY5tYj0fvrx55HsdDR+5cqLFXP7wIDAQAB
+AoGAfE7P4Zsj6zOzGPI/Izj7Bi5OvGnEeKfzyBiH9Dflue74VRQkqqwXs/DWsNv3
+c+M2Y3iyu5ncgKmUduo5X8D9To2ymPRLGuCdfZTxnBMpIDKSJ0FTwVPkr6cYyyBk
+5VCbc470pQPxTAAtl2eaO1sIrzR4PcgwqrSOjwBQQocsGAECQQD8QOra/mZmxPbt
+bRh8U5lhgZmirImk5RY3QMPI/1/f4k+fyjkU5FRq/yqSyin75aSAXg8IupAFRgyZ
+W7BT6zwBAkEA0A0ugAGorpCbuTa25SsIOMxkEzCiKYvh0O+GfGkzWG4lkSeJqGME
+keuJGlXrZNKNoCYLluAKLPmnd72X2yTL7wJARM0kAXUP0wn324w8+HQIyqqBj/gF
+Vt9Q7uMQQ3s72CGu3ANZDFS2nbRZFU5koxrggk6lRRk1fOq9NvrmHg10AQJABOea
+pgfj+yGLmkUw8JwgGH6xCUbHO+WBUFSlPf+Y50fJeO+OrjqPXAVKeSV3ZCwWjKT4
+9viXJNJJ4WfF0bO/XwJAOMB1wQnEOSZ4v+laMwNtMq6hre5K8woqteXICoGcIWe8
+u3YLAbyW/lHhOCiZu2iAI8AbmXem9lW6Tr7p/97s0w==
+-----END RSA PRIVATE KEY-----
+`
+	)
+
+	g.AfterEach(func() {
+		if g.CurrentGinkgoTestDescription().Failed {
+			client := routeclientset.NewForConfigOrDie(oc.AdminConfig()).RouteV1().Routes(ns)
+			if routes, _ := client.List(context.Background(), metav1.ListOptions{}); routes != nil {
+				outputIngress(routes.Items...)
+			}
+			exutil.DumpPodLogsStartingWith("router-", oc)
+		}
+	})
+
+	oc = exutil.NewCLI("router-certs")
+
+	g.BeforeEach(func() {
+		ns = oc.Namespace()
+
+		var err error
+		routerImage, err = exutil.FindRouterImage(oc)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		isFIPS, err = exutil.IsFIPS(oc.AdminKubeClient().CoreV1())
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		configPath := exutil.FixturePath("testdata", "router", "router-common.yaml")
+		err = oc.AsAdmin().Run("new-app").Args("-f", configPath).Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+	})
+
+	g.When("FIPS is enabled", func() {
+		g.Describe("the HAProxy router", func() {
+			g.It("should not work when configured with a 1024-bit RSA key", func() {
+				if !isFIPS {
+					g.Skip("skipping on non-FIPS cluster")
+				}
+
+				configPath := exutil.FixturePath("testdata", "router", "router-scoped.yaml")
+				g.By(fmt.Sprintf("creating a router from a config file %q", configPath))
+				err := oc.AsAdmin().Run("new-app").Args("-f", configPath,
+					"-p=IMAGE="+routerImage,
+					`-p=ROUTER_NAME=test-1024bit`,
+					`-p=DEFAULT_CERTIFICATE=`+pemData,
+				).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				ns := oc.KubeFramework().Namespace.Name
+				execPod := exutil.CreateExecPodOrFail(oc.AdminKubeClient(), ns, "execpod")
+				defer func() {
+					oc.AdminKubeClient().CoreV1().Pods(ns).Delete(context.Background(), execPod.Name, *metav1.NewDeleteOptions(1))
+				}()
+
+				var routerIP string
+				err = wait.Poll(time.Second, changeTimeoutSeconds*time.Second, func() (bool, error) {
+					pod, err := oc.KubeFramework().ClientSet.CoreV1().Pods(oc.KubeFramework().Namespace.Name).Get(context.Background(), "router-scoped", metav1.GetOptions{})
+					if err != nil {
+						return false, err
+					}
+					routerIP = pod.Status.PodIP
+					podIsReady := podConditionStatus(pod, corev1.PodReady)
+
+					return len(routerIP) != 0 && podIsReady == corev1.ConditionTrue, nil
+				})
+				o.Expect(err).To(o.HaveOccurred())
+			})
+		})
+	})
+
+	g.When("FIPS is disabled", func() {
+		g.Describe("the HAProxy router", func() {
+			g.It("should serve routes when configured with a 1024-bit RSA key", func() {
+				if isFIPS {
+					g.Skip("skipping on FIPS cluster")
+				}
+
+				configPath := exutil.FixturePath("testdata", "router", "router-scoped.yaml")
+				g.By(fmt.Sprintf("creating a router from a config file %q", configPath))
+				err := oc.AsAdmin().Run("new-app").Args("-f", configPath,
+					"-p=IMAGE="+routerImage,
+					`-p=ROUTER_NAME=test-1024bit`,
+					`-p=DEFAULT_CERTIFICATE=`+pemData,
+				).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				ns := oc.KubeFramework().Namespace.Name
+				execPod := exutil.CreateExecPodOrFail(oc.AdminKubeClient(), ns, "execpod")
+				defer func() {
+					oc.AdminKubeClient().CoreV1().Pods(ns).Delete(context.Background(), execPod.Name, *metav1.NewDeleteOptions(1))
+				}()
+
+				var routerIP string
+				err = wait.Poll(time.Second, changeTimeoutSeconds*time.Second, func() (bool, error) {
+					pod, err := oc.KubeFramework().ClientSet.CoreV1().Pods(oc.KubeFramework().Namespace.Name).Get(context.Background(), "router-scoped", metav1.GetOptions{})
+					if err != nil {
+						return false, err
+					}
+					routerIP = pod.Status.PodIP
+					podIsReady := podConditionStatus(pod, corev1.PodReady)
+
+					return len(routerIP) != 0 && podIsReady == corev1.ConditionTrue, nil
+				})
+				o.Expect(err).NotTo(o.HaveOccurred())
+
+				g.By("waiting for the router's healthz endpoint to respond")
+				healthzURI := fmt.Sprintf("http://%s:1936/healthz", routerIP)
+				healthzt := exurl.NewTester(oc.AdminKubeClient(), ns).WithErrorPassthrough(true)
+				defer healthzt.Close()
+				healthzt.Within(
+					time.Minute,
+					exurl.Expect("GET", healthzURI).SkipTLSVerification().HasStatusCode(200),
+				)
+
+				g.By("waiting for the route to respond")
+				url := "https://first.example.com/Letter"
+				t := exurl.NewTester(oc.AdminKubeClient(), ns).WithErrorPassthrough(true)
+				defer t.Close()
+				t.Within(
+					time.Minute,
+					exurl.Expect("GET", url).Through(routerIP).SkipTLSVerification().HasStatusCode(200),
+				)
+			})
+		})
+	})
+})
