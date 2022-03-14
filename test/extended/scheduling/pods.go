30112326bbcd94532dc7811af0eabad3447acb64
Omer Tuchfeld <omer@tuchfeld.dev>
2022-03-14 16:03:42 +0100

    Pod scheduling tests improve pod deployment identification
    The pod scheduling tests should use owner references to find pods
    associated with a particular deployment rather than just relying on the
    pod name to contain the deployment name
    
    This is to avoid false-positives like `image-registry` and
    `cluster-image-registry-operator` being considered in the same
    deployment

--- test/extended/scheduling/pods.go
+++ test/extended/scheduling/pods.go
@@ -5,28 +5,98 @@ import (
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
-	"strings"
 
 	exutil "github.com/openshift/origin/test/extended/util"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// requirePodsOnDifferentNodesTest fails if any pods with the prefix in the namespace are scheduled to the same node
+// requirePodsOnDifferentNodesTest fails if any pods with the prefix in the
+// namespace are scheduled to the same node
 type requirePodsOnDifferentNodesTest struct {
-	namespace string
-	podPrefix string
+	namespace  string
+	deployment string
 }
 
-func (p requirePodsOnDifferentNodesTest) run(oc *exutil.CLI) {
+// getPodDeploymentName returns the name of the deployment associated with a
+// given pod. To do that, it needs a list of all replicasets in the pod's
+// namespace. If the Pod is not part of a Deployment, an empty string is
+// returned
+func getPodDeploymentName(pod *corev1.Pod, namespaceReplicaSets *appsv1.ReplicaSetList) string {
+	podOwnerReplicaSetName := ""
+	for _, owner := range pod.OwnerReferences {
+		if owner.APIVersion == "apps/v1" && owner.Kind == "ReplicaSet" {
+			podOwnerReplicaSetName = owner.Name
+			break
+		}
+	}
+
+	if podOwnerReplicaSetName == "" {
+		// Pod is not owned by a replicaset, so it doesn't have a deployment
+		// associated with it
+		return ""
+	}
+
+	podOwnerReplicaSet := (*appsv1.ReplicaSet)(nil)
+	for _, namespaceReplicaSet := range namespaceReplicaSets.Items {
+		namespaceReplicaSet := namespaceReplicaSet
+		if namespaceReplicaSet.Name == podOwnerReplicaSetName {
+			podOwnerReplicaSet = &namespaceReplicaSet
+			break
+		}
+	}
+
+	if podOwnerReplicaSet == nil {
+		e2e.Failf("Could not find ReplicaSet resource associated with the Pod ReplicaSet owner reference")
+	}
+
+	for _, replicaSetOwner := range podOwnerReplicaSet.OwnerReferences {
+		if replicaSetOwner.APIVersion == "apps/v1" && replicaSetOwner.Kind == "Deployment" {
+			return replicaSetOwner.Name
+		}
+	}
+
+	// Pod is part of a ReplicaSet which is not owned by any deployment
+	return ""
+}
+
+func (p requirePodsOnDifferentNodesTest) getDeploymentPods(oc *exutil.CLI) []*corev1.Pod {
 	pods, err := oc.KubeFramework().ClientSet.CoreV1().Pods(p.namespace).List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		e2e.Failf("unable to list pods: %v", err)
 	}
-	nodeNameMap := map[string]string{}
+
+	replicaSets, err := oc.KubeFramework().ClientSet.AppsV1().ReplicaSets(p.namespace).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		e2e.Failf("unable to list replicaSets: %v", err)
+	}
+
+	deploymentPods := make([]*corev1.Pod, 0, 2)
 	for _, pod := range pods.Items {
-		if !strings.Contains(pod.Name, p.podPrefix) {
-			continue
+		pod := pod
+		podDeploymentName := getPodDeploymentName(&pod, replicaSets)
+
+		if p.deployment == podDeploymentName {
+			deploymentPods = append(deploymentPods, &pod)
 		}
+	}
+
+	return deploymentPods
+}
+
+func (p requirePodsOnDifferentNodesTest) run(oc *exutil.CLI) {
+	deploymentPods := p.getDeploymentPods(oc)
+
+	if len(deploymentPods) == 0 {
+		// This is not a bug. Not all deployments are available all the time.
+		// For example, the openshift-etcd/etcd-quorum-guard deployment is not
+		// created for SingleReplica ControlPlaneTopology.
+		return
+	}
+
+	nodeNameMap := map[string]string{}
+	for _, pod := range deploymentPods {
 		if podName, ok := nodeNameMap[pod.Spec.NodeName]; ok {
 			e2e.Failf("ns/%s pod %s and pod %s are running on the same node: %s", p.namespace, pod.Name, podName, pod.Spec.NodeName)
 		} else {
@@ -48,67 +118,67 @@ var _ = g.Describe("[sig-scheduling][Early]", func() {
 
 	g.Describe("The HAProxy router pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-ingress", podPrefix: "router-default"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-ingress", deployment: "router-default"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-apiserver pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-apiserver", podPrefix: "apiserver"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-apiserver", deployment: "apiserver"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-authentication pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-authentication", podPrefix: "oauth-openshift"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-authentication", deployment: "oauth-openshift"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-console pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-console", podPrefix: "console"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-console", deployment: "console"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-console pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-console", podPrefix: "downloads"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-console", deployment: "downloads"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-etcd pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-etcd", podPrefix: "etcd-quorum-guard"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-etcd", deployment: "etcd-quorum-guard"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-image-registry pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-image-registry", podPrefix: "image-registry"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-image-registry", deployment: "image-registry"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-monitoring pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-monitoring", podPrefix: "prometheus-adapter"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-monitoring", deployment: "prometheus-adapter"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-monitoring pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-monitoring", podPrefix: "thanos-querier"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-monitoring", deployment: "thanos-querier"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-oauth-apiserver pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-oauth-apiserver", podPrefix: "apiserver"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-oauth-apiserver", deployment: "apiserver"}.run(oc)
 		})
 	})
 
 	g.Describe("The openshift-operator-lifecycle-manager pods", func() {
 		g.It("should be scheduled on different nodes", func() {
-			requirePodsOnDifferentNodesTest{namespace: "openshift-operator-lifecycle-manager", podPrefix: "packageserver"}.run(oc)
+			requirePodsOnDifferentNodesTest{namespace: "openshift-operator-lifecycle-manager", deployment: "packageserver"}.run(oc)
 		})
 	})
 })
