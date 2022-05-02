8d52f2bbb5196295a130fc9311449612db31298d
David Eads <deads@redhat.com>
2022-05-02 17:50:44 -0400

    fill out list

--- pkg/synthetictests/allowedalerts/all.go
+++ pkg/synthetictests/allowedalerts/all.go
@@ -30,6 +30,9 @@ func AllAlertTests(ctx context.Context, clientConfig *rest.Config, duration time
 	return []AlertTest{
 		newWatchdogAlert(),
 
+		newAlert("Cloud Credential Operator", "CloudCredentialOperatorProvisioningFailed").pending().neverFail(),
+		newAlert("Cloud Credential Operator", "CloudCredentialOperatorProvisioningFailed").firing(),
+
 		newAlert("Cluster Version Operator", "ClusterVersionOperatorDown").pending().neverFail(),
 		newAlert("Cluster Version Operator", "ClusterVersionOperatorDown").firing(),
 
@@ -55,16 +58,29 @@ func AllAlertTests(ctx context.Context, clientConfig *rest.Config, duration time
 		newAlert("etcd", "etcdHighNumberOfLeaderChanges").pending().neverFail(),
 		newAlert("etcd", "etcdHighNumberOfLeaderChanges").withAllowance(etcdAllowance).firing(),
 
-		// controle plane owns most aggregated APIsserver.  Not perfect, but closer than random.
+		// controle plane owns most aggregated apiservers.  Not perfect, but closer than random.
 		newAlert("kube-apiserver", "AggregatedAPIDown").pending().neverFail(),
 		newAlert("kube-apiserver", "AggregatedAPIDown").firing(),
+		// controle plane owns most aggregated apiservers.  Not perfect, but closer than random.
+		newAlert("kube-apiserver", "KubeAggregatedAPIDown").pending().neverFail(),
+		newAlert("kube-apiserver", "KubeAggregatedAPIDown").firing(),
+		// controle plane owns most aggregated apiservers.  Not perfect, but closer than random.
+		newAlert("kube-apiserver", "KubeAggregatedAPIErrors").pending().neverFail(),
+		newAlert("kube-apiserver", "KubeAggregatedAPIErrors").firing(),
 		newAlert("kube-apiserver", "KubeAPIErrorBudgetBurn").pending().neverFail(),
 		newAlert("kube-apiserver", "KubeAPIErrorBudgetBurn").firing(),
 		newAlert("kube-apiserver", "KubeClientErrors").pending().neverFail(),
 		newAlert("kube-apiserver", "KubeClientErrors").firing(),
+		newAlert("kube-apiserver", "KubeAPITerminatedRequests").pending().neverFail(),
+		newAlert("kube-apiserver", "KubeAPITerminatedRequests").firing(),
 		// control plane owns most of the kube pods.  This isn't perfect, but it's slightly closer than random.
 		newAlert("kube-apiserver", "KubePodCrashLooping").pending().neverFail(),
 		newAlert("kube-apiserver", "KubePodCrashLooping").firing(),
+		newAlert("kube-apiserver", "KubeAPIDown").pending().neverFail(),
+		newAlert("kube-apiserver", "KubeAPIDown").firing(),
+
+		newAlert("kube-scheduler", "KubeSchedulerDown").pending().neverFail(),
+		newAlert("kube-scheduler", "KubeSchedulerDown").firing(),
 
 		newAlert("machine config operator", "MCDDrainError").pending().neverFail(),
 		newAlert("machine config operator", "MCDDrainError").firing(),
@@ -81,8 +97,28 @@ func AllAlertTests(ctx context.Context, clientConfig *rest.Config, duration time
 		newAlert("monitoring", "TargetDown").pending().neverFail(),
 		newAlert("monitoring", "TargetDown").firing(),
 
+		newAlert("Networking", "SDNPodNotReady").pending().neverFail(),
+		newAlert("Networking", "SDNPodNotReady").firing(),
+		newAlert("Networking", "NoRunningOvnMaster").pending().neverFail(),
+		newAlert("Networking", "NoRunningOvnMaster").firing(),
+
 		newAlert("Node", "KubeletDown").pending().neverFail(),
 		newAlert("Node", "KubeletDown").firing(),
+		newAlert("Node", "KubeNodeNotReady").pending().neverFail(),
+		newAlert("Node", "KubeNodeNotReady").firing(),
+		newAlert("Node", "NodeProxyApplySlow").pending().neverFail(),
+		newAlert("Node", "NodeProxyApplySlow").firing(),
+		newAlert("Node", "NTOPodsNotReady").pending().neverFail(),
+		newAlert("Node", "NTOPodsNotReady").firing(),
+
+		newAlert("OLM", "CommunityOperatorsCatalogError").pending().neverFail(),
+		newAlert("OLM", "CommunityOperatorsCatalogError").firing(),
+		newAlert("OLM", "RedhatMarketplaceCatalogError").pending().neverFail(),
+		newAlert("OLM", "RedhatMarketplaceCatalogError").firing(),
+		newAlert("OLM", "CertifiedOperatorsCatalogError").pending().neverFail(),
+		newAlert("OLM", "CertifiedOperatorsCatalogError").firing(),
+		newAlert("OLM", "RedhatOperatorsCatalogError").pending().neverFail(),
+		newAlert("OLM", "RedhatOperatorsCatalogError").firing(),
 
 		newAlert("Routing", "IngressControllerDegraded").pending().neverFail(),
 		newAlert("Routing", "IngressControllerDegraded").firing(),
@@ -91,5 +127,24 @@ func AllAlertTests(ctx context.Context, clientConfig *rest.Config, duration time
 		newAlert("storage", "KubePersistentVolumeErrors").firing(),
 		newAlert("storage", "VSphereOpenshiftNodeHealthFail").pending().neverFail(),
 		newAlert("storage", "VSphereOpenshiftNodeHealthFail").firing().neverFail(), // https://bugzilla.redhat.com/show_bug.cgi?id=2055729
+
+		// need to split by name
+		newAlert("Unknown", "ClusterOperatorDown").pending().neverFail(),
+		newAlert("Unknown", "ClusterOperatorDown").firing(),
+		newAlert("Unknown", "KubeContainerWaiting").pending().neverFail(),
+		newAlert("Unknown", "KubeContainerWaiting").firing(),
+		// This isn't perfect, but it's slightly closer than random.  To change ownership, provide evidence of the top
+		// three ways this test fails.
+		newAlert("Unknown", "KubeDeploymentReplicasMismatch").pending().neverFail(),
+		newAlert("Unknown", "KubeDeploymentReplicasMismatch").firing(),
+		// this is usually down to a scheduler problem on node restart.  This isn't perfect, but it's slightly closer than random.
+		newAlert("Unknown", "KubePodNotReady").pending().neverFail(),
+		newAlert("Unknown", "KubePodNotReady").firing(),
+		newAlert("Unknown", "KubeStatefulSetReplicasMismatch").pending().neverFail(),
+		newAlert("Unknown", "KubeStatefulSetReplicasMismatch").firing(),
+		newAlert("Unknown", "ClusterOperatorDegraded").pending().neverFail(),
+		newAlert("Unknown", "ClusterOperatorDegraded").firing(),
+		newAlert("Unknown", "KubeJobFailed").pending().neverFail(),
+		newAlert("Unknown", "KubeJobFailed").firing(),
 	}
 }
