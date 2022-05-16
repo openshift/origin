1a35c5c2660cef09628860e423228b69a1bc0cc2
Dennis Periquet <dperique@redhat.com>
2022-05-16 08:49:08 -0400

    revert to original

--- test/extended/testdata/bindata.go
+++ test/extended/testdata/bindata.go
@@ -52870,7 +52870,7 @@ var _e2echartE2eChartTemplateHtml = []byte(`<html lang="en">
     zQualitative(true).
     enableAnimations(false).
     leftMargin(240).
-    rightMargin(1550).
+    rightMargin(550).
     maxLineHeight(20).
     maxHeight(10000).
     zColorScale(ordinalScale).
@@ -52879,7 +52879,7 @@ var _e2echartE2eChartTemplateHtml = []byte(`<html lang="en">
 
 
     // force a minimum width for smaller devices (which otherwise get an unusable display)
-    setTimeout(() => { if (myChart.width() < 3000) { myChart.width(3000) }}, 1)
+    setTimeout(() => { if (myChart.width() < 1300) { myChart.width(1300) }}, 1)
 </script>
 </body>
 </html>
