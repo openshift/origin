d1e5ab6de407ce00bf57df23b1d88f37cbe81b4b
Maciej Szulik <maszulik@redhat.com>
2022-05-30 17:14:07 +0200

    Add missing log argument

--- pkg/monitor/monitoring_reflector_store.go
+++ pkg/monitor/monitoring_reflector_store.go
@@ -136,7 +136,7 @@ func newMonitoringStore(
 		for _, obj := range items {
 			currentUID := uidOf(obj)
 			metadata, _ := meta.Accessor(obj)
-			framework.Logf("#### replacing uid/%v pod/%v ns/%v resourceVersion/%v\n", currentUID, metadata.GetName(), metadata.GetNamespace())
+			framework.Logf("#### replacing uid/%v pod/%v ns/%v resourceVersion/%v\n", currentUID, metadata.GetName(), metadata.GetNamespace(), metadata.GetResourceVersion())
 
 			_, oldObjExists := s.cacheOfNow[currentUID]
 			switch {
