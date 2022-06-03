57b715d83b9f570557e646e5e3ef3e7360d138c7
Forrest Babcock <forrestb@redhat.com>
2022-06-03 11:37:37 -0400

    Add test cases to evaluate BestMatching

--- pkg/synthetictests/allowedbackenddisruption/matches_test.go
+++ pkg/synthetictests/allowedbackenddisruption/matches_test.go
@@ -104,7 +104,7 @@ func TestGetClosestP95Value(t *testing.T) {
 					Topology:     "single",
 				},
 			},
-			expectedDuration: mustDuration("2102.3s"),
+			expectedDuration: mustDuration("560.0s"),
 			expectedDetails:  `(no exact match for historicaldata.DataKey{Name:"ingress-to-console-new-connections", JobType:platformidentification.JobType{Release:"4.11", FromRelease:"4.10", Platform:"aws", Architecture:"amd64", Network:"ovn", Topology:"single"}}, fell back to historicaldata.DataKey{Name:"ingress-to-console-new-connections", JobType:platformidentification.JobType{Release:"4.10", FromRelease:"4.10", Platform:"aws", Architecture:"amd64", Network:"sdn", Topology:"single"}})`,
 		},
 		{
@@ -121,6 +121,147 @@ func TestGetClosestP95Value(t *testing.T) {
 			},
 			expectedDuration: mustDuration("2.718s"),
 			expectedDetails:  `(no exact or fuzzy match for jobType=platformidentification.JobType{Release:"4.10", FromRelease:"4.10", Platform:"azure", Architecture:"amd64", Network:"", Topology:"missing"})`,
+		},
+		// request is azure 4.11 from 4.10 without network, falls back to 4.10 from 4.10 sdn
+		{
+			name: "fuzzy-match-single-image-registry-new-connections",
+			args: args{
+				backendName: "image-registry-new-connections",
+				jobType: platformidentification.JobType{
+					Release:      "4.11",
+					FromRelease:  "4.10",
+					Platform:     "azure",
+					Architecture: "amd64",
+					Topology:     "single",
+				},
+			},
+			expectedDuration: mustDuration("250.76s"),
+			expectedDetails:  `(no exact match for historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.11", FromRelease:"4.10", Platform:"azure", Architecture:"amd64", Network:"", Topology:"single"}}, fell back to historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.10", FromRelease:"4.10", Platform:"azure", Architecture:"amd64", Network:"sdn", Topology:"single"}})`,
+		},
+		// request is azure 4.11 from 4.10 sdn, falls back to 4.11 from 4.11 sdn
+		{
+			name: "fuzzy-match-single-image-registry-new-connections",
+			args: args{
+				backendName: "image-registry-new-connections",
+				jobType: platformidentification.JobType{
+					Release:      "4.11",
+					FromRelease:  "4.10",
+					Platform:     "azure",
+					Network:      "sdn",
+					Architecture: "amd64",
+					Topology:     "single",
+				},
+			},
+			expectedDuration: mustDuration("246.9s"),
+			expectedDetails:  `(no exact match for historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.11", FromRelease:"4.10", Platform:"azure", Architecture:"amd64", Network:"sdn", Topology:"single"}}, fell back to historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.11", FromRelease:"4.11", Platform:"azure", Architecture:"amd64", Network:"sdn", Topology:"single"}})`,
+		},
+		// request is azure 4.11 from 4.10 ovn, falls back to 4.10 from 4.10 sdn
+		{
+			name: "fuzzy-match-single-image-registry-new-connections",
+			args: args{
+				backendName: "image-registry-new-connections",
+				jobType: platformidentification.JobType{
+					Release:      "4.11",
+					FromRelease:  "4.10",
+					Platform:     "azure",
+					Network:      "ovn",
+					Architecture: "amd64",
+					Topology:     "single",
+				},
+			},
+			expectedDuration: mustDuration("250.76s"),
+			expectedDetails:  `(no exact match for historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.11", FromRelease:"4.10", Platform:"azure", Architecture:"amd64", Network:"ovn", Topology:"single"}}, fell back to historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.10", FromRelease:"4.10", Platform:"azure", Architecture:"amd64", Network:"sdn", Topology:"single"}})`,
+		},
+		// request is aws 4.11 from 4.10 ovn, falls back to 4.10 from 4.10 sdn
+		{
+			name: "fuzzy-match-single-image-registry-new-connections",
+			args: args{
+				backendName: "image-registry-new-connections",
+				jobType: platformidentification.JobType{
+					Release:      "4.11",
+					FromRelease:  "4.10",
+					Platform:     "aws",
+					Network:      "ovn",
+					Architecture: "amd64",
+					Topology:     "single",
+				},
+			},
+			expectedDuration: mustDuration("234.28s"),
+			expectedDetails:  `(no exact match for historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.11", FromRelease:"4.10", Platform:"aws", Architecture:"amd64", Network:"ovn", Topology:"single"}}, fell back to historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.10", FromRelease:"4.10", Platform:"aws", Architecture:"amd64", Network:"sdn", Topology:"single"}})`,
+		},
+		// request is for aws 4.11 from 4.10 sdn, falls back to 4.10 from 4.10 sdn
+		{
+			name: "fuzzy-match-single-image-registry-new-connections",
+			args: args{
+				backendName: "image-registry-new-connections",
+				jobType: platformidentification.JobType{
+					Release:      "4.11",
+					FromRelease:  "4.10",
+					Platform:     "aws",
+					Network:      "sdn",
+					Architecture: "amd64",
+					Topology:     "single",
+				},
+			},
+			expectedDuration: mustDuration("234.28s"),
+			expectedDetails:  `(no exact match for historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.11", FromRelease:"4.10", Platform:"aws", Architecture:"amd64", Network:"sdn", Topology:"single"}}, fell back to historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.10", FromRelease:"4.10", Platform:"aws", Architecture:"amd64", Network:"sdn", Topology:"single"}})`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actualDuration, actualDetails, actualErr := GetAllowedDisruption(tt.args.backendName, tt.args.jobType)
+			if got, want := actualDuration, tt.expectedDuration; !reflect.DeepEqual(got, want) {
+				t.Errorf("GetClosestP99Value() = %v, want %v", got, want)
+			}
+			if got, want := actualDetails, tt.expectedDetails; !reflect.DeepEqual(got, want) {
+				t.Errorf("GetClosestP99Value() = %v, want %v", got, want)
+			}
+			if got, want := actualErr, tt.expectedErr; !reflect.DeepEqual(got, want) {
+				t.Errorf("GetClosestP99Value() = %v, want %v", got, want)
+			}
+		})
+	}
+}
+
+func TestSingleGetClosestP95Value(t *testing.T) {
+
+	// isolate a single case for debug / validation
+	// change as needed
+
+	mustDuration := func(durationString string) *time.Duration {
+		ret, err := time.ParseDuration(durationString)
+		if err != nil {
+			panic(err)
+		}
+		return &ret
+	}
+	type args struct {
+		backendName string
+		jobType     platformidentification.JobType
+	}
+	tests := []struct {
+		name             string
+		args             args
+		expectedDuration *time.Duration
+		expectedDetails  string
+		expectedErr      error
+	}{
+
+		{
+			name: "fuzzy-match-single-image-registry-new-connections",
+			args: args{
+				backendName: "image-registry-new-connections",
+				jobType: platformidentification.JobType{
+					Release:      "4.11",
+					FromRelease:  "4.10",
+					Platform:     "aws",
+					Network:      "sdn",
+					Architecture: "amd64",
+					Topology:     "single",
+				},
+			},
+			expectedDuration: mustDuration("234.28s"),
+			expectedDetails:  `(no exact match for historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.11", FromRelease:"4.10", Platform:"aws", Architecture:"amd64", Network:"sdn", Topology:"single"}}, fell back to historicaldata.DataKey{Name:"image-registry-new-connections", JobType:platformidentification.JobType{Release:"4.10", FromRelease:"4.10", Platform:"aws", Architecture:"amd64", Network:"sdn", Topology:"single"}})`,
 		},
 	}
 	for _, tt := range tests {
