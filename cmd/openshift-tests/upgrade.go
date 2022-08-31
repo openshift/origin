9ce2243320f9bca4d9beeeedaffca031c003bff2
Devan Goodwin <dgoodwin@redhat.com>
2022-08-31 15:33:42 -0300

    Fix upgrade dry-run.
    Fixes #27383
    TRT-445

--- cmd/openshift-tests/upgrade.go
+++ cmd/openshift-tests/upgrade.go
@@ -77,18 +77,20 @@ var upgradeSuites = testSuites{
 // upgradeTestPreSuite validates the test options and gathers data useful prior to launching the upgrade and it's
 // related tests.
 func upgradeTestPreSuite(opt *runOptions) error {
-	testOpt := ginkgo.NewTestOptions(os.Stdout, os.Stderr)
-	config, err := decodeProvider(os.Getenv("TEST_PROVIDER"), testOpt.DryRun, false, nil)
-	if err != nil {
-		return err
-	}
-	if err := initializeTestFramework(exutil.TestContext, config, testOpt.DryRun); err != nil {
-		return err
-	}
-	klog.V(4).Infof("Loaded test configuration: %#v", exutil.TestContext)
+	if !opt.DryRun {
+		testOpt := ginkgo.NewTestOptions(os.Stdout, os.Stderr)
+		config, err := decodeProvider(os.Getenv("TEST_PROVIDER"), testOpt.DryRun, false, nil)
+		if err != nil {
+			return err
+		}
+		if err := initializeTestFramework(exutil.TestContext, config, testOpt.DryRun); err != nil {
+			return err
+		}
+		klog.V(4).Infof("Loaded test configuration: %#v", exutil.TestContext)
 
-	if err := upgrade.GatherPreUpgradeResourceCounts(); err != nil {
-		return errors.Wrap(err, "error gathering preupgrade resource counts")
+		if err := upgrade.GatherPreUpgradeResourceCounts(); err != nil {
+			return errors.Wrap(err, "error gathering preupgrade resource counts")
+		}
 	}
 
 	// Upgrade test output is important for debugging because it shows linear progress
