3eeaa35a147f8efc5423391d959b6b497d709f57
Stephen Benjamin <stephen@redhat.com>
2022-09-13 11:04:03 -0400

    Fix s2i ruby test that relys on rack
    This test is failing in CI:
    
    ```
    [sig-builds][Feature:Builds][Slow] s2i build with environment file in
    sources Building from a template should create a image from
    "test-env-build.json" template and run it in a pod
    [apigroup:build.openshift.io][apigroup:image.openshift.io]
    ```
    
    In https://github.com/rack/rack/pull/1937, rackup was moved to a
    different gem, which is what the s2i ruby config relies on:
    
    https://github.com/sclorg/s2i-ruby-container/blob/master/2.2/s2i/bin/assemble#L52
    
    This adds "rackup" gem.

--- test/extended/testdata/bindata.go
+++ test/extended/testdata/bindata.go
@@ -18793,6 +18793,7 @@ func testExtendedTestdataBuildsS2iEnvironmentBuildAppS2iEnvironment() (*asset, e
 var _testExtendedTestdataBuildsS2iEnvironmentBuildAppGemfile = []byte(`source "https://rubygems.org"
 
 gem "rack"
+gem "rackup"
 `)
 
 func testExtendedTestdataBuildsS2iEnvironmentBuildAppGemfileBytes() ([]byte, error) {
