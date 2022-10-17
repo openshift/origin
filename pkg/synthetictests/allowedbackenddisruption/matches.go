fea8634da6571256f7c524167171020ff107bf29
David Eads <deads@redhat.com>
2022-10-17 17:40:41 -0400

    disruption checking is disabled for prior releases because we lack a good baseline

--- pkg/synthetictests/allowedbackenddisruption/matches.go
+++ pkg/synthetictests/allowedbackenddisruption/matches.go
@@ -9,5 +9,6 @@ import (
 // GetAllowedDisruption uses the backend and information about the cluster to choose the best historical p95 to operate against.
 // We enforce "don't get worse" for disruption by watching the aggregate data in CI over many runs.
 func GetAllowedDisruption(backendName string, jobType platformidentification.JobType) (*time.Duration, string, error) {
-	return getCurrentResults().BestMatchP99(backendName, jobType)
+	t := 6 * time.Hour
+	return &t, "disruption checking is disabled for prior releases because we lack a good baseline", nil
 }
