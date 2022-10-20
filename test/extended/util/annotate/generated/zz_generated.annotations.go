75e02ada711d12eb773cac3e29b2253d27f0ed86
Forrest Babcock <forrestb@redhat.com>
2022-10-20 13:45:56 -0400

    remove templatebroker e2e and security tests as well

--- test/extended/util/annotate/generated/zz_generated.annotations.go
+++ test/extended/util/annotate/generated/zz_generated.annotations.go
@@ -2009,10 +2009,6 @@ var annotations = map[string]string{
 
 	"[Top Level] [sig-devex][Feature:Templates] templateinstance security tests [apigroup:authorization.openshift.io][apigroup:template.openshift.io] should pass security tests [apigroup:route.openshift.io]": "should pass security tests [apigroup:route.openshift.io] [Suite:openshift/conformance/parallel]",
 
-	"[Top Level] [sig-devex][Feature:Templates] templateservicebroker end-to-end test [apigroup:template.openshift.io][apigroup:authorization.openshift.io]  should pass an end-to-end test": "should pass an end-to-end test [Suite:openshift/conformance/parallel]",
-
-	"[Top Level] [sig-devex][Feature:Templates] templateservicebroker security test [apigroup:template.openshift.io][apigroup:authorization.openshift.io]  should pass security tests": "should pass security tests [Suite:openshift/conformance/parallel]",
-
 	"[Top Level] [sig-etcd] etcd cluster has the same number of master nodes and voting members from the endpoints configmap [Early][apigroup:config.openshift.io]": "cluster has the same number of master nodes and voting members from the endpoints configmap [Early][apigroup:config.openshift.io] [Suite:openshift/conformance/parallel]",
 
 	"[Top Level] [sig-etcd] etcd leader changes are not excessive [Late][apigroup:config.openshift.io]": "leader changes are not excessive [Late][apigroup:config.openshift.io] [Suite:openshift/conformance/parallel]",
