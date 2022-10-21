4fb6e4b7ef1393076e6c7ed727ade8090ae504cf
Devan Goodwin <dgoodwin@redhat.com>
2022-10-21 15:20:00 -0300

    damn edge cases, still busted

--- pkg/monitor/monitorapi/identification_test.go
+++ pkg/monitor/monitorapi/identification_test.go
@@ -7,16 +7,41 @@ import (
 )
 
 func TestLocatorParts(t *testing.T) {
-	assert.Equal(t,
-		map[string]string{
-			"e2e-test": "\"test a\"",
+	tests := []struct {
+		name          string
+		locator       string
+		expectedParts map[string]string
+	}{
+		{
+			name:    "multi-word test name",
+			locator: `e2e-test/"test a"`,
+			expectedParts: map[string]string{
+				"e2e-test": "\"test a\"",
+			},
 		},
-		LocatorParts(`e2e-test/"test a"`))
-	assert.Equal(t,
-		map[string]string{
-			"e2e-test":   "\"test a\"",
-			"status":     "Passed",
-			"jUnitSuite": "openshift-tests-upgrade",
+		{
+			name:    "multi-word test name with other tags",
+			locator: `e2e-test/"test a" jUnitSuite/openshift-tests-upgrade status/Passed`,
+			expectedParts: map[string]string{
+				"e2e-test":   "\"test a\"",
+				"jUnitSuite": "openshift-tests-upgrade",
+				"status":     "Passed",
+			},
 		},
-		LocatorParts(`e2e-test/"test a" jUnitSuite/openshift-tests-upgrade status/Passed`))
+		{
+			name:    "multi-word test name with quotes",
+			locator: `e2e-test/"[sig-builds][Feature:Builds] result image \\"test-docker-build.json\\" something something [apigroup:build.openshift.io][apigroup:image.openshift.io]\"" jUnitSuite/openshift-tests-upgrade status/Passed`,
+			expectedParts: map[string]string{
+				"e2e-test":   "\"[sig-builds][Feature:Builds] result image \\\"test-docker-build.json\\\" something something [apigroup:build.openshift.io][apigroup:image.openshift.io]\"",
+				"status":     "Passed",
+				"jUnitSuite": "openshift-tests-upgrade",
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := LocatorParts(tc.locator)
+			assert.Equal(t, tc.expectedParts["e2e-test"], result["e2e-test"])
+		})
+	}
 }
