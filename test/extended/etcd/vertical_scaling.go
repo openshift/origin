868184832a410f65c7728f6f430ecd56d72b8175
Mustafa Elbehery <melbeher@redhat.com>
2022-10-27 14:44:54 +0200

    wait for failed phase

--- test/extended/etcd/vertical_scaling.go
+++ test/extended/etcd/vertical_scaling.go
@@ -8,6 +8,7 @@ import (
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
 
+	machinev1beta1 "github.com/openshift/api/machine/v1beta1"
 	machineclient "github.com/openshift/client-go/machine/clientset/versioned"
 	testlibraryapi "github.com/openshift/library-go/test/library/apiserver"
 	scalingtestinglibrary "github.com/openshift/origin/test/extended/etcd/helpers"
@@ -162,6 +163,17 @@ var _ = g.Describe("[sig-etcd][Serial] etcd [apigroup:config.openshift.io]", fun
 		memberName, err := scalingtestinglibrary.MachineNameToEtcdMemberName(ctx, kubeClient, machineClient, victimMachineName)
 		o.Expect(err).ToNot(o.HaveOccurred())
 
+		// wait till the victim's machine phase becomes `Failed`
+		o.Eventually(func() (*machinev1beta1.Machine, error) {
+			machine, err := machineClient.Get(ctx, victimMachineName, metav1.GetOptions{})
+			if err != nil {
+				return nil, err
+			}
+
+			return machine, nil
+		}).Should(o.HaveField("Status.Phase", o.BeEquivalentTo("Failed")))
+
+		// once victim machine is `Failed`, delete from api-server
 		err = machineClient.Delete(ctx, victimMachineName, metav1.DeleteOptions{})
 		o.Expect(err).ToNot(o.HaveOccurred())
 		framework.Logf("successfully deleted master machine %q from the API", victimMachineName)
