1baa63712d6821c0324375187f3800ff973bb209
David Eads <deads@redhat.com>
2022-10-31 16:54:18 -0400

    more logging

--- vendor/k8s.io/client-go/rest/request.go
+++ vendor/k8s.io/client-go/rest/request.go
@@ -994,7 +994,7 @@ func (r *Request) transformResponseWithContext(ctx context.Context, resp *http.R
 			}
 		}
 	}
-	GinkgoLogf("read %v bytes", len(body))
+	GinkgoLogf("read %v bytes from %T %#v", len(body), resp.Body, resp.Body)
 	StepEnd(bodyReadCtx, bodyReadStart)
 
 
