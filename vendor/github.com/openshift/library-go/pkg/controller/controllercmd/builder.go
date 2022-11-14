fa105be21587bfdc87bedeb9ee1811401f509feb
Hemant Kumar <hekumar@redhat.com>
2022-11-14 10:30:04 -0500

    Bump openshift/kubernetes to latest master

--- vendor/github.com/openshift/library-go/pkg/controller/controllercmd/builder.go
+++ vendor/github.com/openshift/library-go/pkg/controller/controllercmd/builder.go
@@ -269,7 +269,7 @@ func (b *ControllerBuilder) Run(ctx context.Context, config *unstructured.Unstru
 
 	var server *genericapiserver.GenericAPIServer
 	if b.servingInfo != nil {
-		serverConfig, err := serving.ToServerConfig(ctx, *b.servingInfo, *b.authenticationConfig, *b.authorizationConfig, kubeConfig)
+		serverConfig, err := serving.ToServerConfig(ctx, *b.servingInfo, *b.authenticationConfig, *b.authorizationConfig, kubeConfig, kubeClient, b.leaderElection)
 		if err != nil {
 			return err
 		}
