08c19d5858223508c1694010b59156ac744637e0
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-01-10 16:24:13 +0100

    ushift: skip alert invariants if there is no monitoring

--- pkg/synthetictests/alerts.go
+++ pkg/synthetictests/alerts.go
@@ -8,12 +8,34 @@ import (
 
 	"k8s.io/client-go/rest"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
 	"github.com/openshift/origin/pkg/monitor/monitorapi"
 	"github.com/openshift/origin/pkg/synthetictests/allowedalerts"
 )
 
 func testAlerts(events monitorapi.Intervals, restConfig *rest.Config, duration time.Duration, recordedResource *monitorapi.ResourcesMap) []*junitapi.JUnitTestCase {
+	
+
+	kubeClient, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return &junitapi.JUnitTestCase{
+			Name: "Alert setup, kube client",
+			FailureOutput: &junitapi.FailureOutput{
+				Output: err.Error(),
+			},
+			SystemOut: err.Error(),
+		}
+	}
+
 	ret := []*junitapi.JUnitTestCase{}
+
+	_, err = kubeClient.CoreV1().Namespaces().Get(context.Background(), "openshift-monitoring", metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		return ret
+	}
 
 	alertTests := allowedalerts.AllAlertTests(context.TODO(), restConfig, duration)
 	for i := range alertTests {
