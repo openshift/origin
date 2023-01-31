bb965a4d2ff10a6e5757538a71b623f3de91afd4
Feny Mehta <fbm3307@gmail.com>
2023-01-31 20:26:18 +0530

    Update S2i image tests for dotnet3.1 EOL and add dotnet70
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- test/extended/image_ecosystem/s2i_images.go
+++ test/extended/image_ecosystem/s2i_images.go
@@ -201,18 +201,11 @@ var s2iImages = map[string][]tc{
 			Arches:   []string{"amd64", "arm64", "s390x"},
 		},
 		{
-			Version:  "31",
+			Version:  "70",
 			Cmd:      "dotnet --version",
-			Expected: "3.1",
-			Tag:      "3.1-ubi8",
-			Arches:   []string{"amd64"},
-		},
-		{
-			Version:  "31",
-			Cmd:      "dotnet --version",
-			Expected: "3.1",
-			Tag:      "3.1-el7",
-			Arches:   []string{"amd64"},
+			Expected: "7.0",
+			Tag:      "7.0-ubi8",
+			Arches:   []string{"amd64", "arm64", "s390x"},
 		},
 	},
 }
