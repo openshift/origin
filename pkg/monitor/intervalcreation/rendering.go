0a50f229b322d5c3f7d62aaf73a8be2e00d83267
David Eads <deads@redhat.com>
2023-03-29 13:19:37 -0400

    only use new filtering on spyglass view
    Just a dirty copy/paste because TRT's end goal is to entirely remove
    this generation.

--- pkg/monitor/intervalcreation/rendering.go
+++ pkg/monitor/intervalcreation/rendering.go
@@ -63,6 +63,10 @@ func (r eventIntervalRenderer) writeEventData(artifactDir, filenameBase string, 
 
 	}
 	e2eChartTemplate := testdata.MustAsset("e2echart/e2e-chart-template.html")
+	// choosing to intercept here because it should be temporary until TRT transitions to a new mechanism to display these intervals.
+	if !strings.Contains(r.name, "spyglass") {
+		e2eChartTemplate = testdata.MustAsset("e2echart/non-spyglass-e2e-chart-template.html")
+	}
 	e2eChartTitle := fmt.Sprintf("Intervals - %s%s", r.name, timeSuffix)
 	e2eChartHTML := bytes.ReplaceAll(e2eChartTemplate, []byte("EVENT_INTERVAL_TITLE_GOES_HERE"), []byte(e2eChartTitle))
 	e2eChartHTML = bytes.ReplaceAll(e2eChartHTML, []byte("EVENT_INTERVAL_JSON_GOES_HERE"), eventIntervalsJSON)
