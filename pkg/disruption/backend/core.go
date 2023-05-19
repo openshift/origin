e709efb2b844f7325cfd869228c54717c4861633
Abu Kashem <akashem@redhat.com>
2023-05-19 16:45:20 -0400

    disruption: add a backend sampler

--- pkg/disruption/backend/core.go
+++ pkg/disruption/backend/core.go
@@ -2,6 +2,8 @@ package backend
 
 import (
 	"net/http"
+
+	"github.com/openshift/origin/pkg/disruption/sampler"
 )
 
 // Client sends a given request to the server, and
@@ -14,4 +16,10 @@ type ClientFunc func(*http.Request) (*http.Response, error)
 
 func (f ClientFunc) Do(r *http.Request) (*http.Response, error) {
 	return f(r)
+}
+
+// SampleResult holds the result of a request (sample) sent to the server
+type SampleResult struct {
+	RequestResponse
+	Sample *sampler.Sample
 }
