854183f0c53eeda68b0474ce9dfb77f3ad6a0e17
Devan Goodwin <dgoodwin@redhat.com>
2023-07-13 11:50:53 -0300

    Refactor to fix a significant bug in what namespace intervals were getting labelled with

--- pkg/monitor/loki_upload.go
+++ pkg/monitor/loki_upload.go
@@ -29,9 +29,30 @@ var batchCtr int
 // pushBatch sends logs to Loki in batches of 500 using raw HTTP requets. Attempts were made to vendor and use the
 // promtail library but this gets extremely complicated when you already vendor kube.
 // Includes a rudimentary retry mechanism.
-func pushBatch(client *http.Client, bearerToken string, lokiData LokiData) error {
+func pushBatch(client *http.Client, bearerToken, invoker, namespace string, values [][]interface{}) error {
 	batchCtr++
-	url := "https://logging-loki-openshift-operators-redhat.apps.cr.j7t7.p1.openshiftapps.com/api/logs/v1/openshift-trt/loki/api/v1/push"
+	logger := logrus.WithField("namespace", namespace)
+	logger.Info("uploading an intervals batch")
+
+	labels := map[string]string{
+		"invoker": invoker,
+		"type":    "origin-interval",
+		// TODO: in future, may group intervals in origin rather than in the js itself, if we do,
+		// ensure this group gets carried over to this series.
+	}
+	if namespace != "" {
+		labels["namespace"] = namespace
+	}
+	lokiData := LokiData{
+		Streams: []Streams{
+			{
+				Stream: labels,
+				Values: values,
+			},
+		},
+	}
+
+	lokiURL := "https://logging-loki-openshift-operators-redhat.apps.cr.j7t7.p1.openshiftapps.com/api/logs/v1/openshift-trt/loki/api/v1/push"
 	headers := map[string]string{
 		"Content-Type":  "application/json",
 		"Authorization": "Bearer " + bearerToken,
@@ -39,13 +60,13 @@ func pushBatch(client *http.Client, bearerToken string, lokiData LokiData) error
 	jsonData, err := json.Marshal(lokiData)
 	if err != nil {
 
-		logrus.WithError(err).Error("error marshalling loki data")
+		logger.WithError(err).Error("error marshalling loki data")
 		return err
 	}
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	req, err := http.NewRequest("POST", lokiURL, bytes.NewBuffer(jsonData))
 	if err != nil {
-		logrus.WithError(err).Error("error creating HTTP request")
+		logger.WithError(err).Error("error creating HTTP request")
 		return err
 	}
 
@@ -62,34 +83,34 @@ func pushBatch(client *http.Client, bearerToken string, lokiData LokiData) error
 		},
 		func(err error) bool {
 			// re-try on any error
-			logrus.WithError(err).Warningf("error occurred on batch %d, re-trying", batchCtr)
+			logger.WithError(err).Warningf("error occurred on batch %d, re-trying", batchCtr)
 			return true
 		},
 		func() error {
-			logrus.Infof("pushing batch %d of %d intervals", batchCtr,
+			logger.Infof("pushing batch %d of %d intervals", batchCtr,
 				len(lokiData.Streams[0].Values))
 			response, err := client.Do(req)
 			if err != nil {
-				logrus.WithError(err).Error("error making HTTP request")
+				logger.WithError(err).Error("error making HTTP request")
 				return err
 			}
 			defer response.Body.Close()
 
 			body, err := ioutil.ReadAll(response.Body)
 			if err != nil {
-				logrus.WithError(err).Error("error reading response body")
+				logger.WithError(err).Error("error reading response body")
 				return err
 			}
 
 			status := response.StatusCode
 			if len(body) > 0 {
-				logrus.Infof("request body: %s", body)
+				logger.Infof("request body: %s", body)
 			}
 			if status >= 200 && status < 300 {
-				logrus.WithField("status", status).
+				logger.WithField("status", status).
 					Infof("%d log entries pushed to loki", len(lokiData.Streams[0].Values))
 			} else {
-				logrus.WithField("status", status).
+				logger.WithField("status", status).
 					Errorf("%d log entries failed to push to loki", len(lokiData.Streams[0].Values))
 				return fmt.Errorf("logs push to loki failed with http status %d", status)
 			}
@@ -128,87 +149,19 @@ func UploadIntervalsToLoki(intervals monitorapi.Intervals) error {
 	logrus.Info("bearer token obtained")
 	client := &http.Client{}
 
-	// NOTE: there is a promtail go client available at https://github.com/grafana/loki/blob/main/clients/pkg/promtail/client/client.go,
-	// however it appears near impossible to vendor. For now, we roll our own with http requests, the worst
-	// part of which is we lose out on resilient retries in the event the ingesters are overloaded.
-
-	values := make([][]interface{}, 0)
 	// This format matches the labels used for the cluster logs in loki, and we want this
 	// to be labelled the same.
 	invoker := fmt.Sprintf("openshift-internal-ci/%s/%s", jobName, buildID)
 	if invoker == "" {
 		return fmt.Errorf("OPENSHIFT_INSTALL_INVOKER is needed for the invoker loki label")
 	}
-	labels := map[string]string{
-		"invoker": invoker,
-		"type":    "origin-interval",
-		// TODO: in future, may group intervals in origin rather than in the js itself, if we do,
-		// ensure this group gets carried over to this series.
-	}
+	// maps namespace name to all the log entries for that namespace. used as we need to batch up all the log entries
+	// per stream (set of labels), which includes namespace. Any intervals without a namespace, or in a non-openshift
+	// namespace, will be under the key ""
+	nsIntervals := map[string][][]interface{}{}
 
 	for _, i := range intervals {
-		logLine := map[string]string{
-			"_entry": i.Message,
-		}
-
-		if strings.HasPrefix(i.Locator, "e2e-test/\"") {
-			startIndex := strings.Index(i.Locator, "\"") + 1
-			endIndex := strings.LastIndex(i.Locator, "\"")
-			logLine["e2e-test"] = i.Locator[startIndex:endIndex]
-			continue
-		}
-
-		locatorParts := strings.Split(i.Locator, " ")
-
-		for _, lp := range locatorParts {
-			parts := strings.Split(lp, "/")
-			if len(parts) < 2 {
-				logrus.Warnf("unable to split locator parts: %+v", lp)
-				continue
-			}
-			tag, val := parts[0], parts[1]
-			if strings.TrimSpace(tag) == "" {
-				// Have seen this fail on: WARN[0002] unable to process locator tag: May 12 11:10:00.000 I /machine-config reason/OperatorVersionChanged clusteroperator/machine-config-operator version changed from [{operator 4.14.0-0.nightly-2023-05-03-163151}] to [{operator 4.14.0-0.nightly-2023-05-12-121801}]
-				// Would be interesting to track down where this is coming from. Should be identification.go OperatorLocator, but this is impossible.
-				logrus.Warnf("unable to process locator tag: %+v", i)
-				continue
-			}
-
-			// some locator fields can slip in with '-', which is not a valid json key and then breaks unpack in loki.
-			// replace them with '_'.
-			tag = strings.ReplaceAll(tag, "-", "_")
-
-			// WARNING: opting for a potentially risky change here, I want namespace filtering to be available as a
-			// label in loki soon,	and thus I am translating some labels we used historically in origin intervals to
-			// match those we pull from pod logs in the cluster itself. This will require our intervals charts in sippy
-			// be able to parse this new name instead of the other. Ideally, we would go rename these fully in origin wherever used.
-			if tag == "ns" {
-				tag = "namespace"
-			}
-			if tag == "node" {
-				tag = "host"
-			}
-
-			logLine[tag] = val
-		}
-
-		// Loki logs are timestamped, for this we use the "from" of the interval. We'll include a durationSec
-		// so we can calculate the "to" time in our tooling. This will also have the benefit of allowing loki queries
-		// on durationSec > 5 or similar.
-		if i.To.IsZero() {
-			// Not 100% sure To is always set
-			logLine["durationSec"] = "1"
-		} else {
-			d := i.To.Sub(i.From)
-			durInSeconds := math.Ceil(d.Seconds())
-			if durInSeconds < 1 {
-				durInSeconds = 1
-			}
-			logLine["durationSec"] = strconv.FormatFloat(durInSeconds, 'f', -1, 64)
-		}
-
-		// TODO: add a concept of filename for upgrade vs e2e phases
-		// TODO: add level
+		namespace, logLine, err := intervalToLogLine(i)
 
 		// Convert the log data to a packed json log line, where the message is in '_entry'.
 		// This allows us to search additional fields we didn't index as labels using the unpack/json
@@ -219,50 +172,109 @@ func UploadIntervalsToLoki(intervals monitorapi.Intervals) error {
 			continue
 		}
 
-		if strings.HasPrefix(logLine["namespace"], "openshift-") {
-			// If we have a namespace in the locator, bump it up to a proper loki label:
-			labels["namespace"] = logLine["namespace"]
+		if _, ok := nsIntervals[namespace]; !ok {
+			nsIntervals[namespace] = make([][]interface{}, 0)
 		}
-
-		values = append(values, []interface{}{
+		nsIntervals[namespace] = append(nsIntervals[namespace], []interface{}{
 			strconv.FormatInt(i.From.UnixNano(), 10),
 			string(logLineJson),
 		})
 
-		if len(values) == batchSize {
-			ld := LokiData{
-				Streams: []Streams{
-					{
-						Stream: labels,
-						Values: values,
-					},
-				},
-			}
-			err := pushBatch(client, bearerToken, ld)
+		if len(nsIntervals[namespace]) == batchSize {
+			err := pushBatch(client, bearerToken, invoker, namespace, nsIntervals[namespace])
 			if err != nil {
 				return err
 			}
-			values = make([][]interface{}, 0)
+			nsIntervals[namespace] = make([][]interface{}, 0)
 		}
 	}
 
-	// Push the final batch:
-	if len(values) > 0 {
-		ld := LokiData{
-			Streams: []Streams{
-				{
-					Stream: labels,
-					Values: values,
-				},
-			},
+	// Push the final batches for each namespace:
+	logrus.Info("pushing final batches for each remaining namespace")
+	for namespace, v := range nsIntervals {
+		if len(v) > 0 {
+			err := pushBatch(client, bearerToken, invoker, namespace, nsIntervals[namespace])
+			if err != nil {
+				return err
+			}
+			nsIntervals[namespace] = make([][]interface{}, 0)
 		}
-		err := pushBatch(client, bearerToken, ld)
-		if err != nil {
-			return err
-		}
-		values = make([][]interface{}, 0)
 	}
 	return nil
+}
+
+func intervalToLogLine(i monitorapi.EventInterval) (string, map[string]string, error) {
+
+	logLine := map[string]string{
+		"_entry": i.Message,
+	}
+
+	if strings.HasPrefix(i.Locator, "e2e-test/\"") {
+		startIndex := strings.Index(i.Locator, "\"") + 1
+		endIndex := strings.LastIndex(i.Locator, "\"")
+		logLine["e2e-test"] = i.Locator[startIndex:endIndex]
+		return "", logLine, nil
+	}
+
+	locatorParts := strings.Split(i.Locator, " ")
+
+	for _, lp := range locatorParts {
+		parts := strings.Split(lp, "/")
+		if len(parts) < 2 {
+			logrus.Warnf("unable to split locator parts: %+v", lp)
+			continue
+		}
+		tag, val := parts[0], parts[1]
+		if strings.TrimSpace(tag) == "" {
+			// Have seen this fail on: WARN[0002] unable to process locator tag: May 12 11:10:00.000 I /machine-config reason/OperatorVersionChanged clusteroperator/machine-config-operator version changed from [{operator 4.14.0-0.nightly-2023-05-03-163151}] to [{operator 4.14.0-0.nightly-2023-05-12-121801}]
+			// Would be interesting to track down where this is coming from. Should be identification.go OperatorLocator, but this is impossible.
+			logrus.Warnf("unable to process locator tag: %+v", i)
+			continue
+		}
+
+		// some locator fields can slip in with '-', which is not a valid json key and then breaks unpack in loki.
+		// replace them with '_'.
+		tag = strings.ReplaceAll(tag, "-", "_")
+
+		// WARNING: opting for a potentially risky change here, I want namespace filtering to be available as a
+		// label in loki soon,	and thus I am translating some labels we used historically in origin intervals to
+		// match those we pull from pod logs in the cluster itself. This will require our intervals charts in sippy
+		// be able to parse this new name instead of the other. Ideally, we would go rename these fully in origin wherever used.
+		if tag == "ns" {
+			tag = "namespace"
+		}
+		if tag == "node" {
+			tag = "host"
+		}
+
+		logLine[tag] = val
+	}
+
+	// Loki logs are timestamped, for this we use the "from" of the interval. We'll include a durationSec
+	// so we can calculate the "to" time in our tooling. This will also have the benefit of allowing loki queries
+	// on durationSec > 5 or similar.
+	if i.To.IsZero() {
+		// Not 100% sure To is always set
+		logLine["durationSec"] = "1"
+	} else {
+		d := i.To.Sub(i.From)
+		durInSeconds := math.Ceil(d.Seconds())
+		if durInSeconds < 1 {
+			durInSeconds = 1
+		}
+		logLine["durationSec"] = strconv.FormatFloat(durInSeconds, 'f', -1, 64)
+	}
+
+	// TODO: add a concept of filename for upgrade vs e2e phases
+	// TODO: add level
+
+	// If we have a namespace in the locator, return and it will be used as a proper label:
+	var namespace string
+	if strings.HasPrefix(logLine["namespace"], "openshift-") {
+		namespace = logLine["namespace"]
+	}
+
+	return namespace, logLine, nil
 }
 
 type Streams struct {
