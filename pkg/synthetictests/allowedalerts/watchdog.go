d2a3c5b8ccb6c89ee31c469c7be5797f749c480d
Devan Goodwin <dgoodwin@redhat.com>
2023-07-20 13:04:45 -0300

    Better search for watchdog alert

--- pkg/synthetictests/allowedalerts/watchdog.go
+++ pkg/synthetictests/allowedalerts/watchdog.go
@@ -71,7 +71,8 @@ func (a *watchdogAlertTest) TestAlert(ctx context.Context, prometheusClient prom
 }
 
 func IsWatchdogAlert(eventInterval monitorapi.Interval) bool {
-	return eventInterval.Locator == "alert/Watchdog namespace/openshift-monitoring"
+	return eventInterval.StructuredLocator.Keys[monitorapi.LocatorAlertKey] == "Watchdog" &&
+		eventInterval.StructuredLocator.Keys[monitorapi.LocatorNamespaceKey] == "openshift-monitoring"
 }
 
 func (a *watchdogAlertTest) InvariantCheck(alertIntervals monitorapi.Intervals, _ monitorapi.ResourcesMap) ([]*junitapi.JUnitTestCase, error) {
