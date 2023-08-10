dc61140199db865922310aea1e9f341c1e064c14
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-08-10 14:15:52 +0200

    Skip external service monitoring invariant for microshift

--- pkg/invariants/testframework/disruptionexternalservicemonitoring/invariant.go
+++ pkg/invariants/testframework/disruptionexternalservicemonitoring/invariant.go
@@ -14,6 +14,8 @@ import (
 	"github.com/openshift/origin/pkg/monitor/monitorapi"
 	"github.com/openshift/origin/pkg/test/ginkgo/junitapi"
 	"github.com/openshift/origin/test/extended/util/imageregistryutil"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -31,8 +33,9 @@ type availability struct {
 	routeClient        routeclient.Interface
 	imageRegistryRoute *routev1.Route
 
-	disruptionChecker *disruptionlibrary.Availability
-	suppressJunit     bool
+	disruptionChecker  *disruptionlibrary.Availability
+	notSupportedReason string
+	suppressJunit      bool
 }
 
 func NewAvailabilityInvariant() invariants.InvariantTest {
@@ -57,6 +60,14 @@ func (w *availability) StartCollection(ctx context.Context, adminRESTConfig *res
 		return err
 	}
 
+	_, err = w.kubeClient.CoreV1().Namespaces().Get(context.Background(), "openshift-image-registry", metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		w.notSupportedReason = "namespace openshift-image-registry not present"
+		return nil
+	}
+	if err != nil {
+		return err
+	}
 	w.imageRegistryRoute, err = imageregistryutil.ExposeImageRegistryGenerateName(ctx, w.routeClient, "test-disruption-")
 	if err != nil {
 		return err
@@ -86,6 +97,9 @@ func (w *availability) StartCollection(ctx context.Context, adminRESTConfig *res
 }
 
 func (w *availability) CollectData(ctx context.Context, storageDir string, beginning, end time.Time) (monitorapi.Intervals, []*junitapi.JUnitTestCase, error) {
+	if len(w.notSupportedReason) > 0 {
+		return nil, nil, nil
+	}
 	return w.disruptionChecker.CollectData(ctx)
 }
 
