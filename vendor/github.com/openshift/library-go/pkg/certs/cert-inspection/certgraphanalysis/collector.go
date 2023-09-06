e522f60640145994d6b09595c5d36957615adaf7
David Eads <deads@redhat.com>
2023-09-06 16:05:23 -0400

    to add to vendor

--- vendor/github.com/openshift/library-go/pkg/certs/cert-inspection/certgraphanalysis/collector.go
+++ vendor/github.com/openshift/library-go/pkg/certs/cert-inspection/certgraphanalysis/collector.go
@@ -2,14 +2,61 @@ package certgraphanalysis
 
 import (
 	"context"
+	"strings"
 
 	"github.com/openshift/library-go/pkg/certs/cert-inspection/certgraphapi"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/client-go/kubernetes"
 )
 
 func GatherCertsFromAllNamespaces(ctx context.Context, kubeClient kubernetes.Interface) (*certgraphapi.PKIList, error) {
+	return gatherFilteredCerts(ctx, kubeClient, allConfigMaps, allSecrets)
+}
+
+func GatherCertsFromPlatformNamespaces(ctx context.Context, kubeClient kubernetes.Interface) (*certgraphapi.PKIList, error) {
+	return gatherFilteredCerts(ctx, kubeClient, platformConfigMaps, platformSecrets)
+}
+
+var wellKnownPlatformNamespaces = sets.NewString(
+	"openshift",
+	"default",
+	"kube-system",
+	"kube-public",
+	"kubernetes",
+	)
+
+func isPlatformNamespace(nsName string) bool{
+	if strings.HasPrefix(nsName, "openshift-"){
+		return true
+	}
+	if strings.HasPrefix(nsName, "kubernetes-"){
+		return true
+	}
+	return wellKnownPlatformNamespaces.Has(nsName)
+}
+
+type configMapFilterFunc func(configMap *corev1.ConfigMap) bool
+
+func allConfigMaps(_ *corev1.ConfigMap) bool {
+	return true
+}
+func platformConfigMaps(obj *corev1.ConfigMap) bool {
+	return isPlatformNamespace(obj.Namespace)
+}
+
+type secretFilterFunc func(configMap *corev1.Secret) bool
+
+func allSecrets(_ *corev1.Secret) bool {
+	return true
+}
+func platformSecrets(obj *corev1.Secret) bool {
+	return isPlatformNamespace(obj.Namespace)
+}
+
+func gatherFilteredCerts(ctx context.Context, kubeClient kubernetes.Interface, acceptConfigMap configMapFilterFunc, acceptSecret secretFilterFunc) (*certgraphapi.PKIList, error) {
 	certs := []*certgraphapi.CertKeyPair{}
 	caBundles := []*certgraphapi.CertificateAuthorityBundle{}
 	errs := []error{}
@@ -20,6 +67,9 @@ func GatherCertsFromAllNamespaces(ctx context.Context, kubeClient kubernetes.Int
 		errs = append(errs, err)
 	default:
 		for _, configMap := range configMapList.Items {
+			if !acceptConfigMap(&configMap){
+				continue
+			}
 			details, err := InspectConfigMap(&configMap)
 			if details != nil {
 				caBundles = append(caBundles, details)
@@ -36,6 +86,9 @@ func GatherCertsFromAllNamespaces(ctx context.Context, kubeClient kubernetes.Int
 		errs = append(errs, err)
 	default:
 		for _, secret := range secretList.Items {
+			if !acceptSecret(&secret){
+				continue
+			}
 			details, err := InspectSecret(&secret)
 			if details != nil {
 				certs = append(certs, details)
