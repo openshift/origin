cdc76cfc80a8e9dd7498880eb53492044bb4020c
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2023-10-10 15:50:49 +0200

    Revert "SDN-4042: Increase total upgrade time on OVN platforms"

--- test/e2e/upgrade/upgrade.go
+++ test/e2e/upgrade/upgrade.go
@@ -373,20 +373,15 @@ func clusterUpgrade(f *framework.Framework, c configv1client.Interface, dc dynam
 		Network  string
 	}
 	var upgradeDurationLimits = map[limitLocator]float64{
-		// NOTE: The OVNAWSPlatformType and OVNGCPPlatformType upgrade times are originally 85 and 90 minutes each.
-		// The OVNBareMetalPlatformType and OVNVSpherePlatformType upgrade times are originally 80 and 95 each.
-		// Due to well-known reasons (https://issues.redhat.com/browse/SDN-4042), these numbers are increased to
-		// 100 minutes only for the 4.13->4.14 upgrades - same as OVNAzurePlatformType. These numbers will be brought
-		// down to their original values in OCP 4.15 (https://issues.redhat.com/browse/OTA-999)
-		{configv1.AWSPlatformType, OVN}:       100,
+		{configv1.AWSPlatformType, OVN}:       85,
 		{configv1.AWSPlatformType, SDN}:       95,
 		{configv1.AzurePlatformType, OVN}:     100,
 		{configv1.AzurePlatformType, SDN}:     100,
-		{configv1.GCPPlatformType, OVN}:       100,
+		{configv1.GCPPlatformType, OVN}:       90,
 		{configv1.GCPPlatformType, SDN}:       75,
-		{configv1.BareMetalPlatformType, OVN}: 100,
+		{configv1.BareMetalPlatformType, OVN}: 80,
 		{configv1.BareMetalPlatformType, SDN}: 70,
-		{configv1.VSpherePlatformType, OVN}:   100,
+		{configv1.VSpherePlatformType, OVN}:   95,
 		{configv1.VSpherePlatformType, SDN}:   70,
 	}
 
