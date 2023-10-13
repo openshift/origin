50dbd82967cc2ebc3292072c64a0287242810fb1
ehila <ehila@redhat.com>
2023-10-12 23:20:45 -0400

    feat: updated tests to be aware of capabilities
    updated the tests to be able to query if ImageRegistry is present to determine course of action
    
    Signed-off-by: ehila <ehila@redhat.com>

--- test/extended/images/extract.go
+++ test/extended/images/extract.go
@@ -15,9 +15,11 @@ import (
 	k8simage "k8s.io/kubernetes/test/utils/image"
 	admissionapi "k8s.io/pod-security-admission/api"
 
+	configv1 "github.com/openshift/api/config/v1"
 	imageapi "github.com/openshift/api/image/v1"
 	imageclientset "github.com/openshift/client-go/image/clientset/versioned"
 	exutil "github.com/openshift/origin/test/extended/util"
+	"github.com/openshift/origin/test/extended/util/image"
 )
 
 var _ = g.Describe("[sig-imageregistry][Feature:ImageExtract] Image extract", func() {
@@ -35,12 +37,23 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageExtract] Image extract", fu
 	oc = exutil.NewCLIWithPodSecurityLevel("image-extract", admissionapi.LevelBaseline)
 
 	g.It("should extract content from an image [apigroup:image.openshift.io]", func() {
+		hasImageRegistry, err := exutil.IsCapabilityEnabled(oc, configv1.ClusterVersionCapabilityImageRegistry)
+		o.Expect(err).NotTo(o.HaveOccurred())
 		is, err := oc.ImageClient().ImageV1().ImageStreams("openshift").Get(context.Background(), "tools", metav1.GetOptions{})
 		o.Expect(err).NotTo(o.HaveOccurred())
-		o.Expect(is.Status.DockerImageRepository).NotTo(o.BeEmpty(), "registry not yet configured?")
-		registry := strings.Split(is.Status.DockerImageRepository, "/")[0]
 
 		ns = oc.Namespace()
+		extractImage := image.ShellImage()
+
+		// If ImageRegistry is present, we expect DockerImageRepository to have a value
+		// and we create the registry url for ImageStream extracting, otherwise we use
+		// the ShellImage()
+		if hasImageRegistry {
+			o.Expect(is.Status.DockerImageRepository).NotTo(o.BeEmpty(), "registry not yet configured?")
+			registry := strings.Split(is.Status.DockerImageRepository, "/")[0]
+			extractImage = fmt.Sprintf("%s/%s/1:tools", registry, ns)
+		}
+
 		cli := e2epod.PodClientNS(oc.KubeFramework(), ns)
 		client := imageclientset.NewForConfigOrDie(oc.UserConfig()).ImageV1()
 
@@ -74,35 +87,30 @@ var _ = g.Describe("[sig-imageregistry][Feature:ImageExtract] Image extract", fu
 			o.Expect(img.Status.Status).To(o.Equal("Success"), fmt.Sprintf("imagestreamimport status for spec.image[%d] (message: %s)", i, img.Status.Message))
 		}
 
-		// toolsLayers := isi.Status.Images[0].Image.DockerImageLayers
-		// toolsLen := len(toolsLayers)
-		// mysqlLayers := isi.Status.Images[1].Image.DockerImageLayers
-		// mysqlLen := len(mysqlLayers)
-
 		pod := cli.Create(context.TODO(), cliPodWithPullSecret(oc, heredoc.Docf(`
 			set -x
 
 			# command exits if directory doesn't exist
-			! oc image extract --insecure %[2]s/%[1]s/1:tools --path=/:/tmp/doesnotexist
+			! oc image extract --insecure %[1]s --path=/:/tmp/doesnotexist
 			# command exits if directory isn't empty
-			! oc image extract --insecure %[2]s/%[1]s/1:tools --path=/:/
+			! oc image extract --insecure %[1]s --path=/:/
 
 			# extract a directory to a directory, verify the contents
 			mkdir -p /tmp/test
-			oc image extract --insecure %[2]s/%[1]s/1:tools --path=/etc/cron.d/:/tmp/test/
+			oc image extract --insecure %[1]s --path=/etc/cron.d/:/tmp/test/
 			[ -f /tmp/test/0hourly ] && grep root /tmp/test/0hourly
 
 			# extract multiple individual files
 			mkdir -p /tmp/test2
-			oc image extract --insecure %[2]s/%[1]s/1:tools --path=/etc/shadow:/tmp/test2 --path=/etc/system-release:/tmp/test2
+			oc image extract --insecure %[1]s --path=/etc/shadow:/tmp/test2 --path=/etc/system-release:/tmp/test2
 			[ -f /tmp/test2/shadow ] && [ -L /tmp/test2/system-release ]
 
 			# extract a single file to the current directory
 			mkdir -p /tmp/test3
 			cd /tmp/test3
-			oc image extract --insecure %[2]s/%[1]s/1:tools --file=/etc/shadow
+			oc image extract --insecure %[1]s --file=/etc/shadow
 			[ -f /tmp/test3/shadow ]
-		`, ns, registry)))
+		`, extractImage)))
 		cli.WaitForSuccess(context.TODO(), pod.Name, podStartupTimeout)
 	})
 })
