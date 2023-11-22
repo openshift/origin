3f33fbe7519112f79974d84ee945522aea828e7c
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-11-22 13:20:15 +0100

    USHIFT-1858: Adapt router unprivileged tests to use public images

--- test/extended/util/annotate/generated/zz_generated.annotations.go
+++ test/extended/util/annotate/generated/zz_generated.annotations.go
@@ -1367,7 +1367,7 @@ var Annotations = map[string]string{
 
 	"[sig-network][Feature:Router][apigroup:route.openshift.io] The HAProxy router should override the route host with a custom value": " [Skipped:Disconnected] [Suite:openshift/conformance/parallel]",
 
-	"[sig-network][Feature:Router][apigroup:route.openshift.io] The HAProxy router should run even if it has no access to update status [apigroup:image.openshift.io]": " [Skipped:Disconnected] [Suite:openshift/conformance/parallel]",
+	"[sig-network][Feature:Router][apigroup:route.openshift.io] The HAProxy router should run even if it has no access to update status": " [Skipped:Disconnected] [Suite:openshift/conformance/parallel]",
 
 	"[sig-network][Feature:Router][apigroup:route.openshift.io] The HAProxy router should serve the correct routes when running with the haproxy config manager": " [Suite:openshift/conformance/parallel]",
 
