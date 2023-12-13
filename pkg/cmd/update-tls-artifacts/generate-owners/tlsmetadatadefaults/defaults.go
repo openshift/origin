002662c8041f9d426b10622480efe7b4bf9ac5f4
David Eads <deads@redhat.com>
2023-12-13 17:23:38 -0500

    add description requirement

--- pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadatadefaults/defaults.go
+++ pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadatadefaults/defaults.go
@@ -2,6 +2,7 @@ package tlsmetadatadefaults
 
 import (
 	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadata/autoregenerate_after_expiry"
+	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadata/descriptions"
 	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadata/ownership"
 	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadatainterfaces"
 )
@@ -10,5 +11,6 @@ func GetDefaultTLSRequirements() []tlsmetadatainterfaces.Requirement {
 	return []tlsmetadatainterfaces.Requirement{
 		ownership.NewOwnerRequirement(),
 		autoregenerate_after_expiry.NewAutoRegenerateAfterOfflineExpiryRequirement(),
+		descriptions.NewDescriptionRequirement(),
 	}
 }
