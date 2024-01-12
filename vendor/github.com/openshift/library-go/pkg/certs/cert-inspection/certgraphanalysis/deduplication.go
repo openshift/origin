2401c465946927fc029dca62f894caaaaeee7228
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-01-12 12:58:10 +0100

    Bump library-go to latest master

--- vendor/github.com/openshift/library-go/pkg/certs/cert-inspection/certgraphanalysis/deduplication.go
+++ vendor/github.com/openshift/library-go/pkg/certs/cert-inspection/certgraphanalysis/deduplication.go
@@ -1,31 +1,55 @@
 package certgraphanalysis
 
-import "github.com/openshift/library-go/pkg/certs/cert-inspection/certgraphapi"
+import (
+	"github.com/openshift/library-go/pkg/certs/cert-inspection/certgraphapi"
+)
 
 func deduplicateCertKeyPairs(in []*certgraphapi.CertKeyPair) []*certgraphapi.CertKeyPair {
 	ret := []*certgraphapi.CertKeyPair{}
 	for _, currIn := range in {
+		if currIn == nil {
+			panic("currIn is nil")
+		}
+
 		found := false
 		for j, currOut := range ret {
-			if currIn == nil {
-				panic("one")
-			}
 			if currOut == nil {
-				panic("two")
+				panic("currOut is nil")
 			}
-			if currOut.Name == currIn.Name {
+			// currOut has no name set - skip and allow it to be found later
+			if len(currOut.Name) == 0 {
+				continue
+			}
+			// Merge if cert identifiers match
+			if currOut.Spec.CertMetadata.CertIdentifier.PubkeyModulus == currIn.Spec.CertMetadata.CertIdentifier.PubkeyModulus {
+				found = true
 				ret[j] = combineSecretLocations(ret[j], currIn.Spec.SecretLocations)
 				ret[j] = combineCertOnDiskLocations(ret[j], currIn.Spec.OnDiskLocations)
-				found = true
 				break
 			}
 		}
 
+		// No match found - add currIn as is
 		if !found {
 			ret = append(ret, currIn.DeepCopy())
 		}
 	}
 
+	return ret
+}
+
+func deduplicateCertKeyPairList(in *certgraphapi.CertKeyPairList) *certgraphapi.CertKeyPairList {
+	ret := &certgraphapi.CertKeyPairList{
+		Items: []certgraphapi.CertKeyPair{},
+	}
+	certs := []*certgraphapi.CertKeyPair{}
+	for idx := range in.Items {
+		certs = append(certs, &in.Items[idx])
+	}
+	dedup := deduplicateCertKeyPairs(certs)
+	for idx := range dedup {
+		ret.Items = append(ret.Items, *dedup[idx])
+	}
 	return ret
 }
 
@@ -47,6 +71,7 @@ func combineSecretLocations(in *certgraphapi.CertKeyPair, rhs []certgraphapi.InC
 }
 
 func combineCertOnDiskLocations(in *certgraphapi.CertKeyPair, rhs []certgraphapi.OnDiskCertKeyPairLocation) *certgraphapi.CertKeyPair {
+	keyLocation := certgraphapi.OnDiskLocation{}
 	out := in.DeepCopy()
 	for _, curr := range rhs {
 		found := false
@@ -56,7 +81,22 @@ func combineCertOnDiskLocations(in *certgraphapi.CertKeyPair, rhs []certgraphapi
 			}
 		}
 		if !found {
+			// Store key to be merged into Cert
+			if len(curr.Cert.Path) == 0 {
+				keyLocation = curr.Key
+				continue
+			}
 			out.Spec.OnDiskLocations = append(out.Spec.OnDiskLocations, curr)
+		}
+	}
+
+	// Fill in Key property if it was found earlier and unset
+	if len(keyLocation.Path) == 0 {
+		return out
+	}
+	for idx, loc := range out.Spec.OnDiskLocations {
+		if len(loc.Key.Path) == 0 {
+			out.Spec.OnDiskLocations[idx].Key = keyLocation
 		}
 	}
 
@@ -66,18 +106,19 @@ func combineCertOnDiskLocations(in *certgraphapi.CertKeyPair, rhs []certgraphapi
 func deduplicateCABundles(in []*certgraphapi.CertificateAuthorityBundle) []*certgraphapi.CertificateAuthorityBundle {
 	ret := []*certgraphapi.CertificateAuthorityBundle{}
 	for _, currIn := range in {
+		if currIn == nil {
+			panic("one")
+		}
+
 		found := false
 		for j, currOut := range ret {
-			if currIn == nil {
-				panic("one")
-			}
 			if currOut == nil {
 				panic("two")
 			}
 			if currOut.Name == currIn.Name {
+				found = true
 				ret[j] = combineConfigMapLocations(ret[j], currIn.Spec.ConfigMapLocations)
 				ret[j] = combineCABundleOnDiskLocations(ret[j], currIn.Spec.OnDiskLocations)
-				found = true
 				break
 			}
 		}
@@ -87,6 +128,21 @@ func deduplicateCABundles(in []*certgraphapi.CertificateAuthorityBundle) []*cert
 		}
 	}
 
+	return ret
+}
+
+func deduplicateCABundlesList(in *certgraphapi.CertificateAuthorityBundleList) *certgraphapi.CertificateAuthorityBundleList {
+	ret := &certgraphapi.CertificateAuthorityBundleList{
+		Items: []certgraphapi.CertificateAuthorityBundle{},
+	}
+	bundles := []*certgraphapi.CertificateAuthorityBundle{}
+	for idx := range in.Items {
+		bundles = append(bundles, &in.Items[idx])
+	}
+	dedup := deduplicateCABundles(bundles)
+	for idx := range dedup {
+		ret.Items = append(ret.Items, *dedup[idx])
+	}
 	return ret
 }
 
--- vendor/github.com/openshift/library-go/pkg/operator/resource/resourceapply/admissionregistration.go
+++ vendor/github.com/openshift/library-go/pkg/operator/resource/resourceapply/admissionregistration.go
@@ -63,7 +63,7 @@ func ApplyMutatingWebhookConfigurationImproved(ctx context.Context, client admis
 	toWrite := existingCopy // shallow copy so the code reads easier
 	toWrite.Webhooks = required.Webhooks
 
-	klog.V(4).Infof("MutatingWebhookConfiguration %q changes: %v", required.GetNamespace()+"/"+required.GetName(), JSONPatchNoError(existing, toWrite))
+	klog.V(2).Infof("MutatingWebhookConfiguration %q changes: %v", required.GetNamespace()+"/"+required.GetName(), JSONPatchNoError(existing, toWrite))
 
 	actual, err := client.MutatingWebhookConfigurations().Update(ctx, toWrite, metav1.UpdateOptions{})
 	reportUpdateEvent(recorder, required, err)
@@ -138,7 +138,7 @@ func ApplyValidatingWebhookConfigurationImproved(ctx context.Context, client adm
 	toWrite := existingCopy // shallow copy so the code reads easier
 	toWrite.Webhooks = required.Webhooks
 
-	klog.V(4).Infof("ValidatingWebhookConfiguration %q changes: %v", required.GetNamespace()+"/"+required.GetName(), JSONPatchNoError(existing, toWrite))
+	klog.V(2).Infof("ValidatingWebhookConfiguration %q changes: %v", required.GetNamespace()+"/"+required.GetName(), JSONPatchNoError(existing, toWrite))
 
 	actual, err := client.ValidatingWebhookConfigurations().Update(ctx, toWrite, metav1.UpdateOptions{})
 	reportUpdateEvent(recorder, required, err)
