eda79f290a34bde08b248ae028268a20597a37dd
DennisPeriquet <DennisPeriquet@users.noreply.github.com>
2024-01-16 14:27:30 -0500

    Automated - test of prcreator by dperique
    Testing of prcreator by dperique

---
+++ test.sh
@@ -0,0 +1,8 @@
+~/prcreator \
+        -github-token-path=./oauth \
+        -organization=openshift \
+        -repo=origin \
+        -git-message="Testing of prcreator by dperique" \
+        -pr-message "This is a prcreator test by dperique" \
+        -pr-title="Automated - test of prcreator by dperique" \
+		-pr-assignee="DennisPeriquet,neisw,dgoodwin"
