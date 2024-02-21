99b817bae01ddf8be7bbc2409dd8968bd58242cd
Shilpa-Gokul <shilpagokul95@gmail.com>
2024-02-21 15:27:26 +0530

    Skip tests for image-registry operator with single replica

--- pkg/monitortests/clusterversionoperator/legacycvomonitortests/monitortest.go
+++ pkg/monitortests/clusterversionoperator/legacycvomonitortests/monitortest.go
@@ -40,7 +40,7 @@ func (w *legacyMonitorTests) EvaluateTestsFromConstructedIntervals(ctx context.C
 
 	isUpgrade := platformidentification.DidUpgradeHappenDuringCollection(finalIntervals, time.Time{}, time.Time{})
 	if isUpgrade {
-		junits = append(junits, testUpgradeOperatorStateTransitions(finalIntervals)...)
+		junits = append(junits, testUpgradeOperatorStateTransitions(finalIntervals, w.adminRESTConfig)...)
 	} else {
 		junits = append(junits, testStableSystemOperatorStateTransitions(finalIntervals)...)
 	}
