a031732dff16a4baecd3b06d9e2fd30c6bf6c887
Kevin Hannon <kehannon@redhat.com>
2024-04-04 16:47:49 -0400

    push image test
    Signed-off-by: Kevin Hannon <kehannon@redhat.com>

--- pkg/monitortests/node/kubeletselinuxlabels/util.go
+++ pkg/monitortests/node/kubeletselinuxlabels/util.go
@@ -1,7 +1,7 @@
 package kubeletselinuxlabels
 
 import (
-	images "github.com/openshift/origin/test/extended/util/image"
+	"github.com/openshift/origin/test/extended/util/image"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -41,7 +41,7 @@ func selinuxPodSpec(name, namespace, nodeName string) *corev1.Pod {
 			Containers: []corev1.Container{
 				{
 
-					Image:           images.LimitedShellImage(),
+					Image:           image.LocationFor("quay.io/redhat-developer/nfs-server:1.1"),
 					ImagePullPolicy: corev1.PullIfNotPresent,
 					Name:            name,
 					Command: []string{
