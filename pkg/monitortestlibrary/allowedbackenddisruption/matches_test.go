5988528d2e60071d9546c0b1dcae983245f78166
Dennis Periquet <dperique@redhat.com>
2024-04-05 08:12:30 -0400

    relax minJobs requirement to lower than available

--- pkg/monitortestlibrary/allowedbackenddisruption/matches_test.go
+++ pkg/monitortestlibrary/allowedbackenddisruption/matches_test.go
@@ -247,7 +247,7 @@ func TestDisruptionDataFileParsing(t *testing.T) {
 		Topology:     "ha",
 	}
 
-	percentiles, _, err := disruptionMatcher.BestMatchDuration("kube-api-new-connections", jobType, 100)
+	percentiles, _, err := disruptionMatcher.BestMatchDuration("kube-api-new-connections", jobType, 50)
 	// We can't really check a value here as it could very likely be 0,
 	// so instead we'll make sure we didn't get a msg complaining about no match with no fallback.
 	assert.NotEqual(t, percentiles, historicaldata.StatisticalDuration{}, "BestMatchDuration found no match and could not fall back for kube-api-new-connections aws amd64 ovn ha")
