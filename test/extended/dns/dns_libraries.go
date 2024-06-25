2b04039bc9fd965dd2e1c5461bd2f7b7fa8e64f1
Grant Spence <gspence@redhat.com>
2024-06-25 17:27:19 -0400

    NE-1526: DNS Libraries E2E clean up
    Clean up comments, logging, and error handling in the DNS Libraries
    E2E tests.

--- test/extended/dns/dns_libraries.go
+++ test/extended/dns/dns_libraries.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 	"strings"
 
-	. "github.com/onsi/ginkgo/v2"
+	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
 
 	buildv1 "github.com/openshift/api/build/v1"
@@ -26,28 +26,28 @@ import (
 // checkForPodLogFailures goes through the pod logs and determines if there was a failure
 // by looking for "fail" keyword in the logs.
 func checkForPodLogFailures(f *e2e.Framework, pod *kapiv1.Pod) {
-	By("submitting the pod to kubernetes")
+	g.By("creating the pod and waiting for success status")
 	podClient := f.ClientSet.CoreV1().Pods(f.Namespace.Name)
 	updated, err := podClient.Create(context.Background(), pod, metav1.CreateOptions{})
-	if err != nil {
-		e2e.Failf("Failed to create %s pod: %v", pod.Name, err)
-	}
+	o.Expect(err).NotTo(o.HaveOccurred())
 
-	w, err := f.ClientSet.CoreV1().Pods(f.Namespace.Name).Watch(context.Background(), metav1.SingleObject(metav1.ObjectMeta{Name: pod.Name, ResourceVersion: updated.ResourceVersion}))
-	if err != nil {
-		e2e.Failf("Failed to watch pods: %v", err)
-	}
+	w, err := f.ClientSet.CoreV1().Pods(f.Namespace.Name).Watch(context.Background(), metav1.SingleObject(
+		metav1.ObjectMeta{
+			Name:            pod.Name,
+			ResourceVersion: updated.ResourceVersion,
+		},
+	))
+	o.Expect(err).NotTo(o.HaveOccurred())
+
 	ctx, cancel := context.WithTimeout(context.Background(), e2e.PodStartTimeout)
 	defer cancel()
 	if _, err = watchtools.UntilWithoutRetry(ctx, w, PodSucceeded); err != nil {
 		e2e.Failf("Failed: %v", err)
 	}
 
-	By("retrieving the pod logs")
+	g.By("retrieving the pod logs")
 	r, err := podClient.GetLogs(pod.Name, &kapiv1.PodLogOptions{Container: "querier"}).Stream(context.Background())
-	if err != nil {
-		e2e.Failf("Failed to get pod logs %s: %v", pod.Name, err)
-	}
+	o.Expect(err).NotTo(o.HaveOccurred())
 
 	scan := bufio.NewScanner(r)
 	for scan.Scan() {
@@ -58,12 +58,12 @@ func checkForPodLogFailures(f *e2e.Framework, pod *kapiv1.Pod) {
 	}
 }
 
-var _ = Describe("[sig-network-edge] DNS lookup", func() {
+var _ = g.Describe("[sig-network-edge] DNS lookup", func() {
 	f := e2e.NewDefaultFramework("dns-libraries")
 	oc := exutil.NewCLI("dns-libraries")
 	nodeSelector := make(map[string]string)
 
-	BeforeEach(func() {
+	g.BeforeEach(func() {
 		controlPlaneTopology, err := exutil.GetControlPlaneTopology(oc)
 		o.Expect(err).NotTo(o.HaveOccurred())
 		if *controlPlaneTopology != configv1.ExternalTopologyMode {
@@ -74,21 +74,18 @@ var _ = Describe("[sig-network-edge] DNS lookup", func() {
 	// Create pods using the image built from the Dockerfile located in testdata/dns/go-dns-resolver.
 	// These pods perform a DNS query to verify different versions of Go's DNS resolver function correctly
 	// with OpenShift DNS.
-	It("using Go's DNS resolver", func() {
+	g.It("using Go's DNS resolver", func() {
 		configClient, err := configclient.NewForConfig(f.ClientConfig())
-		if err != nil {
-			e2e.Failf("Failed to get config client: %v", err)
-		}
+		o.Expect(err).NotTo(o.HaveOccurred())
 		kubeClient := oc.AdminKubeClient()
 		if isMicroShift, _ := exutil.IsMicroShiftCluster(kubeClient); isMicroShift {
-			Skip("Skip DNS lookup using Go's DNS resolver on MicroShift as it lacks support for the APIs we use")
+			g.Skip("Skip DNS lookup using Go's DNS resolver on MicroShift as it lacks support for the APIs we use")
 		}
 		infra, err := configClient.ConfigV1().Infrastructures().Get(context.Background(), "cluster", metav1.GetOptions{})
-		if err != nil {
-			e2e.Failf("Failed to get cluster config: %v", err)
-		}
+		o.Expect(err).NotTo(o.HaveOccurred())
+
 		if infra.Status.PlatformStatus.Type == configv1.BareMetalPlatformType {
-			Skip("Skip DNS lookup using Go's DNS resolver on BareMetal as there is no external connection to pull the base image.")
+			g.Skip("Skip DNS lookup using Go's DNS resolver on BareMetal as there is no external connection to pull the base image.")
 		}
 
 		dnsService, err := f.ClientSet.CoreV1().Services("openshift-dns").Get(context.Background(), "dns-default", metav1.GetOptions{})
@@ -102,7 +99,7 @@ var _ = Describe("[sig-network-edge] DNS lookup", func() {
 
 		// Create a pod with the latest Go toolset image to identify the Go version it is running.
 		// This allows us to test older Go versions up to and including this latest version.
-		By(fmt.Sprintf("identifying the Go version running in %s:latest", goToolsetImage))
+		g.By(fmt.Sprintf("identifying the Go version running in %s:latest", goToolsetImage))
 		podExec, err := exutil.NewPodExecutor(oc, "go-latest-version-finder", goToolsetImage+":latest")
 		o.Expect(err).NotTo(o.HaveOccurred())
 		goLatestVersion, err := podExec.Exec("echo -n ${GO_MAJOR_VERSION}.${GO_MINOR_VERSION}")
@@ -122,7 +119,7 @@ var _ = Describe("[sig-network-edge] DNS lookup", func() {
 		for curGoMinorVersion <= latestGoMinorVersion {
 			goVersion := fmt.Sprintf("1.%d", curGoMinorVersion)
 			configName := "dns-libraries-go-" + strings.Replace(goVersion, ".", "-", -1)
-			By(fmt.Sprintf("creating build and deployment for %s testing Go Version %s", configName, goVersion))
+			g.By(fmt.Sprintf("creating build and deployment for %s testing Go Version %s", configName, goVersion))
 
 			// Build an image from testdata/dns/go-dns-resolver/Dockerfile while specifying
 			// a base image with a specific Go version to test.
@@ -188,14 +185,14 @@ var _ = Describe("[sig-network-edge] DNS lookup", func() {
 			podExec, err := exutil.NewPodExecutor(oc, configName, br.Build.Status.OutputDockerImageReference)
 			o.Expect(err).NotTo(o.HaveOccurred())
 
-			By("expecting the pod to have the right go version")
+			g.By("expecting the pod to have the right go version")
 			out, err := podExec.Exec("echo -n ${GO_MAJOR_VERSION}.${GO_MINOR_VERSION}")
 			o.Expect(err).NotTo(o.HaveOccurred())
 			if goVersion != out {
 				e2e.Failf("expected GO version to be %s got %s", goVersion, out)
 			}
 
-			By("expect the go application to successfully resolve DNS")
+			g.By("expect the go application to successfully resolve DNS")
 			out, err = podExec.Exec(fmt.Sprintf("go run /go/dns_libraries.go -cluster-ip=%q", dnsService.Spec.ClusterIP))
 			o.Expect(err).NotTo(o.HaveOccurred())
 			if !strings.Contains(out, "Successfully") {
@@ -205,12 +202,13 @@ var _ = Describe("[sig-network-edge] DNS lookup", func() {
 		}
 	})
 
-	// creates a simple Pod that is using getent to perform DNS queries, to make sure glibc's DNS resolver works fine with OpenShift DNS.
-	It("using glibc's DNS resolver", func() {
-		// using www.redhat.com just as a sample host for dns queries
+	// Create a simple Pod that is using getent to perform DNS queries
+	// to make sure glibc's DNS resolver works fine with OpenShift DNS.
+	g.It("using glibc's DNS resolver", func() {
+		// Using www.redhat.com just as a sample host for dns queries
 		const host = "www.redhat.com"
 
-		// running getent command 10 times to see if it can resolve the dns steadily
+		// Running getent command 10 times to see if it can resolve the dns steadily
 		cmd := repeatCommand(
 			10,
 			fmt.Sprintf("getent -s dns ahosts %s || echo $(date) fail", host),
