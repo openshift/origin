ef130ad66b64c6166fdfeed7c4a4f833d8ee54e9
Jan Safranek <jsafrane@redhat.com>
2024-08-01 11:20:47 +0200

    Add OCP specific CSI certification tests
    We use testsuite "openshift/csi" to run test against 3rd party CSI drivers
    as a way to certify them in OpenShift. So far we used upstream tests there.
    
    Add OCP specific tests to the suite, based on a new OCP specific YAML
    manifest provided by the CSI driver vendor.
    
    Usage:
    TEST_OCP_CSI_DRIVER_FILES=azure-disk-ocp.yaml TEST_CSI_DRIVER_FILES=azure-disk-manifest.yaml ./openshift-tests run openshift/csi

--- pkg/clioptions/clusterdiscovery/csi.go
+++ pkg/clioptions/clusterdiscovery/csi.go
@@ -6,23 +6,51 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/openshift/origin/test/extended/storage/csi"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/kubernetes/test/e2e/framework/testfiles"
 	"k8s.io/kubernetes/test/e2e/storage/external"
+	"sigs.k8s.io/yaml"
 )
 
 const (
 	CSIManifestEnvVar = "TEST_CSI_DRIVER_FILES"
+	OCPManifestEnvVar = "TEST_OCP_DRIVER_FILES"
 )
 
 // Initialize openshift/csi suite, i.e. define CSI tests from TEST_CSI_DRIVER_FILES.
 func initCSITests(dryRun bool) error {
-	manifestList := os.Getenv(CSIManifestEnvVar)
-	if manifestList != "" {
-		manifests := strings.Split(manifestList, ",")
+	ocpDrivers := sets.New[string]()
+	upstreamDrivers := sets.New[string]()
+
+	// Load OCP specific tests first, because AddOpenShiftCSITests() modifies global list of
+	// testsuites.CSISuites used by AddDriverDefinition() below.
+	ocpManifestList := os.Getenv(OCPManifestEnvVar)
+	if ocpManifestList != "" {
+		manifests := strings.Split(ocpManifestList, ",")
+		for _, manifest := range manifests {
+			fmt.Printf("Loading OCP test manifest from %q\n", manifest)
+			csiDriver, err := csi.AddOpenShiftCSITests(manifest)
+			if err != nil {
+				return fmt.Errorf("failed to load OCP manifest from %q: %s", manifest, err)
+			}
+			ocpDrivers.Insert(csiDriver)
+		}
+	}
+
+	upstreamManifestList := os.Getenv(CSIManifestEnvVar)
+	if upstreamManifestList != "" {
+		manifests := strings.Split(upstreamManifestList, ",")
 		for _, manifest := range manifests {
 			if err := external.AddDriverDefinition(manifest); err != nil {
 				return fmt.Errorf("failed to load manifest from %q: %s", manifest, err)
 			}
+			csiDriver, err := parseDriverName(manifest)
+			if err != nil {
+				return fmt.Errorf("failed to parse CSI driver name from manifest %q: %s", manifest, err)
+			}
+			upstreamDrivers.Insert(csiDriver)
+
 			// Register the base dir of the manifest file as a file source.
 			// With this we can reference the CSI driver's storageClass
 			// in the manifest file (FromFile field).
@@ -32,5 +60,37 @@ func initCSITests(dryRun bool) error {
 		}
 	}
 
+	// We allow missing OCP specific manifest for CI jobs that do not have it defined yet,
+	// but all OCP specific manifest must have a corresponding upstream manifest.
+	if ocpDrivers.Difference(upstreamDrivers).Len() > 0 {
+		return fmt.Errorf("env. var %s must describe the same CSI drivers as %s: %v vs. %v", OCPManifestEnvVar, CSIManifestEnvVar, ocpDrivers.UnsortedList(), upstreamDrivers.UnsortedList())
+	}
+
 	return nil
+}
+
+func parseDriverName(filename string) (string, error) {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	// Minimal chunk of the upstream CSI driver manifest to extract the driver name.
+	// See vendor/k8s.io/kubernetes/test/e2e/storage/external/external.go for the full definition.
+	// It's private in that file, so we can't import it here.
+	type upstreamManifest struct {
+		DriverInfo struct {
+			Name string
+		}
+	}
+	manifest := &upstreamManifest{}
+	err = yaml.Unmarshal(bytes, manifest)
+	if err != nil {
+		return "", err
+	}
+	if manifest.DriverInfo.Name == "" {
+		return "", fmt.Errorf("missing driver name in manifest %q", filename)
+	}
+	return manifest.DriverInfo.Name, nil
+
 }
