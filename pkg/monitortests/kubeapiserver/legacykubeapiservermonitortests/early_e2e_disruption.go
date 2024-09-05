7b2a18460566cbc73c4979bc7e40f2910c73978c
Luke Meyer <lmeyer@redhat.com>
2024-09-05 13:55:12 -0400

    separate disruption junits by oauth/kube rather than before/after

--- pkg/monitortests/kubeapiserver/legacykubeapiservermonitortests/early_e2e_disruption.go
+++ pkg/monitortests/kubeapiserver/legacykubeapiservermonitortests/early_e2e_disruption.go
@@ -10,69 +10,37 @@ import (
 	"github.com/openshift/origin/pkg/test/ginkgo/junitapi"
 )
 
-// amount of time after E2E tests start that we consider "early"
+// amount of time near E2E test start that we consider "early"
 const preE2ECheckDuration = 3 * time.Minute
 const earlyE2ECheckDuration = 3 * time.Minute
 
 func testEarlyE2EAPIServerDisruption(events monitorapi.Intervals) []*junitapi.JUnitTestCase {
-	var e2eStart time.Time // anchor the time period we're interested in disruption
+	var periodStart, periodEnd time.Time // the time period we're interested in disruption
 	for _, event := range events {
 		if event.Source == monitorapi.SourceE2ETest {
-			e2eStart = event.From
+			periodStart = event.From.Add(-preE2ECheckDuration)
+			periodEnd = event.From.Add(earlyE2ECheckDuration)
 			break
 		}
 	}
 
-	const preTestName = "[Jira:\"oauth-apiserver\"] oauth servers should not experience disruption shortly before the start of E2E testing"
-	const earlyTestName = "[Jira:\"kube-apiserver\"] API servers should not experience disruption during the start of E2E testing"
-	if e2eStart.IsZero() {
+	const kubeTestName = "[Jira:\"kube-apiserver\"] kube API servers should not experience disruption near the start of E2E testing"
+	const oauthTestName = "[Jira:\"oauth-apiserver\"] oauth API servers should not experience disruption near the start of E2E testing"
+	if periodStart.IsZero() {
+		msg := &junitapi.SkipMessage{
+			Message: "no E2E tests ran", // no point in this test
+		}
 		return []*junitapi.JUnitTestCase{
-			{
-				Name: preTestName,
-				SkipMessage: &junitapi.SkipMessage{
-					Message: "no E2E tests ran", // no point in this test
-				},
-			},
-			{
-				Name: earlyTestName,
-				SkipMessage: &junitapi.SkipMessage{
-					Message: "no E2E tests ran", // no point in this test
-				},
-			},
+			{Name: kubeTestName, SkipMessage: msg},
+			{Name: oauthTestName, SkipMessage: msg},
 		}
 	}
 
-	junits := []*junitapi.JUnitTestCase{}
+	kubeMatcher := regexp.MustCompile(`^(cache-)?(openshift|kube)-api-`)
+	kubeEvents := []string{} // the api disruption described in TRT-1794
+	oauthMatcher := regexp.MustCompile(`^(cache-)?oauth-api-`)
+	oauthEvents := []string{} // the oauth disruption described in OCPBUGS-39021
 
-	// find the oauth disruption described in OCPBUGS-39021
-	eventsFound := findDisruptionEvents(events, e2eStart.Add(-preE2ECheckDuration), e2eStart, regexp.MustCompile(`^(cache-)?oauth-api-`))
-	if count := len(eventsFound); count > 0 {
-		junits = append(junits, &junitapi.JUnitTestCase{
-			Name: preTestName,
-			FailureOutput: &junitapi.FailureOutput{
-				Output: fmt.Sprintf("found %d oauth disruption events shortly before E2E test start (%s) with messages:\n%s",
-					count, e2eStart.Format(monitorapi.TimeFormat), strings.Join(eventsFound, "\n")),
-			},
-		})
-	}
-	junits = append(junits, &junitapi.JUnitTestCase{Name: preTestName}) // success after fail makes a flake, to record when this is happening
-
-	// find the api disruption described in TRT-1794
-	eventsFound = findDisruptionEvents(events, e2eStart, e2eStart.Add(earlyE2ECheckDuration), regexp.MustCompile(`^(cache-)?(openshift|kube|oauth)-api-`))
-	if count := len(eventsFound); count > 0 {
-		junits = append(junits, &junitapi.JUnitTestCase{
-			Name: earlyTestName,
-			FailureOutput: &junitapi.FailureOutput{
-				Output: fmt.Sprintf("found %d disruption events shortly after E2E test start (%s) with messages:\n%s",
-					count, e2eStart.Format(monitorapi.TimeFormat), strings.Join(eventsFound, "\n")),
-			},
-		})
-	}
-	return append(junits, &junitapi.JUnitTestCase{Name: earlyTestName}) // success after fail makes a flake, to record when this is happening
-}
-
-func findDisruptionEvents(events monitorapi.Intervals, periodStart time.Time, periodEnd time.Time, backendMatcher *regexp.Regexp) []string {
-	eventsFound := []string{}
 	for _, event := range events {
 		if event.Source != monitorapi.SourceDisruption || event.Message.Reason != monitorapi.DisruptionBeganEventReason {
 			continue // only interested in disruption
@@ -84,9 +52,33 @@ func findDisruptionEvents(events monitorapi.Intervals, periodStart time.Time, pe
 			break // no need to examine events entirely later than the period
 		}
 		// we are left with disruption events where some or all is in the period of interest
-		if backendMatcher.MatchString(event.Locator.Keys["backend-disruption-name"]) {
-			eventsFound = append(eventsFound, event.String())
+		if backend := event.Locator.Keys["backend-disruption-name"]; kubeMatcher.MatchString(backend) {
+			kubeEvents = append(kubeEvents, event.String())
+		} else if oauthMatcher.MatchString(backend) {
+			oauthEvents = append(oauthEvents, event.String())
 		}
 	}
-	return eventsFound
+
+	junits := []*junitapi.JUnitTestCase{}
+	if count := len(kubeEvents); count > 0 {
+		junits = append(junits, &junitapi.JUnitTestCase{
+			Name: kubeTestName,
+			FailureOutput: &junitapi.FailureOutput{
+				Output: fmt.Sprintf("found %d apiserver disruption events near E2E test start (%s) with messages:\n%s",
+					count, periodStart.Format(monitorapi.TimeFormat), strings.Join(kubeEvents, "\n")),
+			},
+		})
+	}
+	junits = append(junits, &junitapi.JUnitTestCase{Name: kubeTestName}) // success after fail makes a flake, to record when this is happening
+
+	if count := len(oauthEvents); count > 0 {
+		junits = append(junits, &junitapi.JUnitTestCase{
+			Name: oauthTestName,
+			FailureOutput: &junitapi.FailureOutput{
+				Output: fmt.Sprintf("found %d oauthserver disruption events near E2E test start (%s) with messages:\n%s",
+					count, periodStart.Format(monitorapi.TimeFormat), strings.Join(oauthEvents, "\n")),
+			},
+		})
+	}
+	return append(junits, &junitapi.JUnitTestCase{Name: oauthTestName}) // success after fail makes a flake, to record when this is happening
 }
