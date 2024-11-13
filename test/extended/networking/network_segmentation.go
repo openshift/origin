0f359d7996f9d8945e75d8a744edc2929c2bd04b
Or Mergi <ormergi@redhat.com>
2024-11-13 14:34:33 +0200

    ovn-k, udn crd e2e: Fix status report consumer assertion
    Following the incoming changes of the CUDN controller, the assertion on the UDN
    status is no longer valid.
    
    Fix the assertion with the expected condition message.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- test/extended/networking/network_segmentation.go
+++ test/extended/networking/network_segmentation.go
@@ -853,7 +853,7 @@ func assertUDNStatusReportsConsumers(udnNamesapce, udnName, expectedPodName stri
 	var conditions []metav1.Condition
 	Expect(json.Unmarshal([]byte(conditionsRaw), &conditions)).To(Succeed())
 	conditions = normalizeConditions(conditions)
-	expectedMsg := fmt.Sprintf("failed to verify NAD not in use [%[1]s/%[2]s]: network in use by the following pods: [%[1]s/%[3]s]",
+	expectedMsg := fmt.Sprintf("failed to delete NetworkAttachmentDefinition [%[1]s/%[2]s]: network in use by the following pods: [%[1]s/%[3]s]",
 		udnNamesapce, udnName, expectedPodName)
 	found := false
 	for _, condition := range conditions {
