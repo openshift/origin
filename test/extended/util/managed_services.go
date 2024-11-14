6fb63478aaaab808194ac3a49dae4b4cf967b05e
Stephen Benjamin <stephen@redhat.com>
2024-11-14 08:00:34 -0500

    managed services: centralize definition of namespaces
    Some ephemeral pods in rosa namespaces like openshift-sre-pruning are
    triggering tests, just skip examining these namespaces alltogether.
    
    Example:
    https://prow.ci.openshift.org/view/gs/test-platform-results/logs/periodic-ci-openshift-release-master-nightly-4.18-e2e-rosa-sts-ovn/1856870032659714048

---
+++ test/extended/util/managed_services.go
@@ -0,0 +1,28 @@
+package util
+
+import "k8s.io/apimachinery/pkg/util/sets"
+
+// ManagedServiceNamespaces is the set of namespaces used by managed service platforms
+// like ROSA, ARO, etc. These are typically exempt from the requirements we impose on
+// core platform namespaces.
+var ManagedServiceNamespaces = sets.New[string](
+	"openshift-addon-operator",
+	"openshift-backplane",
+	"openshift-backplane-srep",
+	"openshift-custom-domains-operator",
+	"openshift-deployment-validation-operator",
+	"openshift-managed-node-metadata-operator",
+	"openshift-managed-upgrade-operator",
+	"openshift-marketplace",
+	"openshift-must-gather-operator",
+	"openshift-observability-operator",
+	"openshift-ocm-agent-operator",
+	"openshift-osd-metrics",
+	"openshift-package-operator",
+	"openshift-rbac-permissions",
+	"openshift-route-monitor-operator",
+	"openshift-security",
+	"openshift-splunk-forwarder-operator",
+	"openshift-sre-pruning",
+	"openshift-validation-webhook",
+)
