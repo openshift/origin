1715bef82194b310915debdaabf07e82a5ea53c7
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-11-26 14:03:50 +0100

    TLS registry: add refresh period requirement
    New cert requirement: refresh period should be specified so that users would be aware how often certificate content is being updated

--- pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadatadefaults/defaults.go
+++ pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadatadefaults/defaults.go
@@ -4,6 +4,7 @@ import (
 	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadata/autoregenerate_after_expiry"
 	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadata/descriptions"
 	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadata/ownership"
+	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadata/refresh_period"
 	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadata/testcase"
 	"github.com/openshift/origin/pkg/cmd/update-tls-artifacts/generate-owners/tlsmetadatainterfaces"
 )
@@ -13,6 +14,7 @@ func GetDefaultTLSRequirements() []tlsmetadatainterfaces.Requirement {
 		ownership.NewOwnerRequirement(),
 		testcase.NewTestNameRequirement(),
 		autoregenerate_after_expiry.NewAutoRegenerateAfterOfflineExpiryRequirement(),
+		refresh_period.NewRefreshPeriodRequirement(),
 		descriptions.NewDescriptionRequirement(),
 	}
 }
