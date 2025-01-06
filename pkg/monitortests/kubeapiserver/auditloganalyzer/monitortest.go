e51f9d4ab51b9f6705ea234d2a9fc2535322fc02
Vadim Rutkovsky <vrutkovs@redhat.com>
2025-01-06 17:54:30 +0100

    kubeapiserver auditloganalyzer: debug usernames to applies

--- pkg/monitortests/kubeapiserver/auditloganalyzer/monitortest.go
+++ pkg/monitortests/kubeapiserver/auditloganalyzer/monitortest.go
@@ -201,6 +201,7 @@ func (w *auditLogAnalyzer) EvaluateTestsFromConstructedIntervals(ctx context.Con
 		flakes := []string{}
 		for username, numberOfApplies := range usersToApplies {
 			if numberOfApplies > 200 {
+				errorMessage := fmt.Sprintf("user %v had %d applies, check the audit log and operator log to figure out why", username, numberOfApplies)
 				switch username {
 				case "system:serviceaccount:openshift-infra:serviceaccount-pull-secrets-controller",
 					"system:serviceaccount:openshift-network-operator:cluster-network-operator",
@@ -210,9 +211,9 @@ func (w *auditLogAnalyzer) EvaluateTestsFromConstructedIntervals(ctx context.Con
 
 					// These usernames are already creating more than 200 applies, so flake instead of fail.
 					// We really want to find a way to track namespaces created by the payload versus everything else.
-					flakes = append(flakes, fmt.Sprintf("user %v had %d applies, check the audit log and operator log to figure out why", username, numberOfApplies))
+					flakes = append(flakes, errorMessage)
 				default:
-					failures = append(failures, fmt.Sprintf("user %v had %d applies, check the audit log and operator log to figure out why", username, numberOfApplies))
+					failures = append(failures, errorMessage)
 				}
 			}
 		}
@@ -234,7 +235,7 @@ func (w *auditLogAnalyzer) EvaluateTestsFromConstructedIntervals(ctx context.Con
 				&junitapi.JUnitTestCase{
 					Name: testName,
 					FailureOutput: &junitapi.FailureOutput{
-						Message: strings.Join(failures, "\n"),
+						Message: strings.Join(flakes, "\n"),
 						Output:  "details in audit log",
 					},
 				},
