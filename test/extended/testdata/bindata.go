c9ee7a3bcbd736e179322fae466118ac91062b24
Vadim Rutkovsky <vrutkovs@redhat.com>
2025-01-28 11:53:01 +0100

    add bootstrap removed event from CEO

--- test/extended/testdata/bindata.go
+++ test/extended/testdata/bindata.go
@@ -53556,6 +53556,11 @@ var _e2echartE2eChartTemplateHtml = []byte(`<html lang="en">
 
     }
 
+    function isEtcdBootstrap(eventInterval) {
+        return eventInterval.source === 'PodLog' && eventInterval.message.reason === "EtcdBootstrap";
+
+    }
+
     function isPodLog(eventInterval) {
         if (eventInterval.source === 'PodLog') {
             return true
@@ -53955,6 +53960,7 @@ var _e2echartE2eChartTemplateHtml = []byte(`<html lang="en">
 
         timelineGroups.push({ group: "etcd-leaders", data: [] })
         createTimelineData(etcdLeadershipLogsValue, timelineGroups[timelineGroups.length - 1].data, eventIntervals, isEtcdLeadershipAndNotEmpty, regex)
+        createTimelineData("Bootstrap", timelineGroups[timelineGroups.length - 1].data, eventIntervals, isEtcdBootstrap, regex)
 
         timelineGroups.push({group: "cloud-metrics", data: []})
         createTimelineData(cloudMetricsValue, timelineGroups[timelineGroups.length - 1].data, eventIntervals, isCloudMetrics, regex)
