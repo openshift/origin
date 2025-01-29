a8bcd79150a36a6cad70f0bb2114095c80d78b6a
chiragkyal <ckyal@redhat.com>
2025-01-29 20:17:06 +0530

    e2e: external certificate : working
    Signed-off-by: chiragkyal <ckyal@redhat.com>

---
+++ test/extended/router/README.md
@@ -0,0 +1,8 @@
+`make update`
+
+`make`
+
+
+```sh
+./openshift-tests run-test "[sig-network][OCPFeatureGate:RouteExternalCertificate][Feature:Router][apigroup:route.openshift.io] The router should support external certificate [Suite:openshift/conformance/parallel]"
+```
--- test/extended/util/annotate/generated/zz_generated.annotations.go
+++ test/extended/util/annotate/generated/zz_generated.annotations.go
@@ -1701,6 +1701,8 @@ var Annotations = map[string]string{
 
 	"[sig-network][OCPFeatureGate:PersistentIPsForVirtualization][Feature:Layer2LiveMigration] Kubevirt Virtual Machines when using openshift ovn-kubernetes with user defined networks and persistent ips configured created using [OCPFeatureGate:NetworkSegmentation] UserDefinedNetwork [Suite:openshift/network/virtualization] should keep ip when the VMI attached to a secondary UDN is migrated between nodes": "",
 
+	"[sig-network][OCPFeatureGate:RouteExternalCertificate][Feature:Router][apigroup:route.openshift.io] The router should support external certificate": " [Suite:openshift/conformance/parallel]",
+
 	"[sig-network][endpoints] admission [apigroup:config.openshift.io] blocks manual creation of EndpointSlices pointing to the cluster or service network": " [Suite:openshift/conformance/parallel]",
 
 	"[sig-network][endpoints] admission [apigroup:config.openshift.io] blocks manual creation of Endpoints pointing to the cluster or service network": " [Suite:openshift/conformance/parallel]",
