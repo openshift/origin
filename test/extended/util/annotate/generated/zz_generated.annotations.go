cafd87c72888c89e50f3c2304c339b13a84175ba
Periyasamy Palanisamy <pepalani@redhat.com>
2025-02-18 17:50:25 +0100

    Avoid changing IPsec modes in the e2e test
    When IPsec mode are changed across tests within IPsec test suite,
    it causes reboot of ovnkube-node daemonset pods, It's expected
    workload traffic would fail temporarily until pods are settle down
    after IPsec is properly configured in every node's OVN and OvS across
    the cluster. So we should not test ipsec mode change in the ipsec
    test suite and instead for every ipsec mode, there should be one CI lane,
    then in the test corresponding configuration and traffic must be tested.
    
    So this commit removes everything related to IPsec mode changes and having
    a single test which can be run from Full and External IPsec mode CI lanes.
    
    Signed-off-by: Periyasamy Palanisamy <pepalani@redhat.com>
    (cherry picked from commit c53288dcd04245e78a189e5a0ae872534cce739d)

--- test/extended/util/annotate/generated/zz_generated.annotations.go
+++ test/extended/util/annotate/generated/zz_generated.annotations.go
@@ -1477,9 +1477,7 @@ var Annotations = map[string]string{
 
 	"[sig-network][Feature:EgressRouterCNI] when using openshift ovn-kubernetes should ensure ipv6 egressrouter cni resources are created [apigroup:operator.openshift.io]": " [Suite:openshift/conformance/parallel]",
 
-	"[sig-network][Feature:IPsec] when using openshift ovn-kubernetes check traffic [apigroup:config.openshift.io] [Suite:openshift/network/ipsec] with IPsec in external mode": "",
-
-	"[sig-network][Feature:IPsec] when using openshift ovn-kubernetes check traffic [apigroup:config.openshift.io] [Suite:openshift/network/ipsec] with IPsec in full mode": "",
+	"[sig-network][Feature:IPsec] when using openshift ovn-kubernetes check traffic with IPsec [apigroup:config.openshift.io] [Suite:openshift/network/ipsec]": "",
 
 	"[sig-network][Feature:Layer2LiveMigration][Suite:openshift/network/virtualization] Kubevirt Virtual Machines Placeholder test for GA": "",
 
