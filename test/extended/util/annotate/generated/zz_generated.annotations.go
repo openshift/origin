3ef9216167a3c5cf0d8de6a6a078ee152188af61
Hongan Li <hongli@redhat.com>
2025-03-11 15:59:55 +0800

    NE-1968: add e2e tests for FeatureGate GatewayAPI
    Implements e2e tests for GatewayAPI featuregate. These tests cover the following scenarios:
    
        The Gateway API CRDs should already be installed
        The Gateway API CRDs should not be created
        The Gateway API CRDs should not be deleted
        The Gateway API CRDs should not be updated
        The Gateway API CRDs can be deleted once VAP is removed [Serial][Disruptive]

--- test/extended/util/annotate/generated/zz_generated.annotations.go
+++ test/extended/util/annotate/generated/zz_generated.annotations.go
@@ -1589,6 +1589,16 @@ var Annotations = map[string]string{
 
 	"[sig-network][OCPFeatureGate:DNSNameResolver][Feature:EgressFirewall] when using openshift ovn-kubernetes should ensure egressfirewall with wildcard dns rules is created": " [Suite:openshift/conformance/parallel]",
 
+	"[sig-network][OCPFeatureGate:GatewayAPI][Feature:Router][apigroup:gateway.networking.k8s.io] The Gateway API CRDs should already be installed": " [Suite:openshift/conformance/parallel]",
+
+	"[sig-network][OCPFeatureGate:GatewayAPI][Feature:Router][apigroup:gateway.networking.k8s.io] The Gateway API CRDs should not be created": " [Suite:openshift/conformance/parallel]",
+
+	"[sig-network][OCPFeatureGate:GatewayAPI][Feature:Router][apigroup:gateway.networking.k8s.io] The Gateway API CRDs should not be deleted": " [Suite:openshift/conformance/parallel]",
+
+	"[sig-network][OCPFeatureGate:GatewayAPI][Feature:Router][apigroup:gateway.networking.k8s.io] The Gateway API CRDs should not be updated": " [Suite:openshift/conformance/parallel]",
+
+	"[sig-network][OCPFeatureGate:GatewayAPI][Serial][Feature:Router][apigroup:gateway.networking.k8s.io] The Gateway API CRDs can be deleted once VAP is removed": " [Suite:openshift/conformance/serial]",
+
 	"[sig-network][OCPFeatureGate:NetworkDiagnosticsConfig][Serial] Should be enabled by default": " [Suite:openshift/conformance/serial]",
 
 	"[sig-network][OCPFeatureGate:NetworkDiagnosticsConfig][Serial] Should function without any target pods": " [Suite:openshift/conformance/serial]",
