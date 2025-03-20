a3d72ad2417f291b8f154ff40ae764cbd689675f
Ayato Tokubi <atokubi@redhat.com>
2025-03-20 09:47:40 +0000

    OCPNODE-2315: Add nested container test

--- test/extended/testdata/bindata.go
+++ test/extended/testdata/bindata.go
@@ -451,6 +451,10 @@
 // test/extended/testdata/net-attach-defs/whereabouts-nad.yml
 // test/extended/testdata/net-attach-defs/whereabouts-race-awake.yml
 // test/extended/testdata/net-attach-defs/whereabouts-race-sleepy.yml
+// test/extended/testdata/node/nested_container/Dockerfile
+// test/extended/testdata/node/nested_container/containers.conf
+// test/extended/testdata/node/nested_container/run_tests.sh
+// test/extended/testdata/node/nested_container/skip_tests.sh
 // test/extended/testdata/node_tuning/nto-stalld.yaml
 // test/extended/testdata/oauthserver/cabundle-cm.yaml
 // test/extended/testdata/oauthserver/oauth-network.yaml
@@ -50327,6 +50331,271 @@ func testExtendedTestdataNetAttachDefsWhereaboutsRaceSleepyYml() (*asset, error)
 	return a, nil
 }
 
+var _testExtendedTestdataNodeNested_containerDockerfile = []byte(`FROM registry.fedoraproject.org/fedora:41
+ARG VERSION=v5.4.0
+
+# TODO: rpm --setcaps... needed due to Fedora (base) image builds
+#       being (maybe still?) affected by
+#       https://bugzilla.redhat.com/show_bug.cgi?id=1995337#c3
+RUN dnf -y makecache && \
+    dnf -y update && \
+    rpm --setcaps shadow-utils 2>/dev/null && \
+    dnf -y install \
+    awk \
+    podman \
+    fuse-overlayfs \
+    openssh-clients \
+    ucpp \
+	bats \
+	buildah \
+	file \
+	git \
+	go \
+	gpgme-devel \
+	htpasswd \
+	jq \
+	libseccomp-devel \
+	make \
+	nc \
+	openssl \
+	procps \
+	python-yaml \
+	skopeo \
+	slirp4netns \
+	socat \
+	ss \
+	timeout \
+    --exclude container-selinux && \
+    ln -s /usr/bin/ucpp /usr/local/bin/cpp && \
+    dnf clean all && \
+    rm -rf /var/cache /var/log/dnf* /var/log/yum.*
+
+RUN useradd podman && \
+    echo -e "podman:1:999\npodman:1001:64535" > /etc/subuid && \
+    echo -e "podman:1:999\npodman:1001:64535" > /etc/subgid
+
+ADD /containers.conf /etc/containers/containers.conf
+
+RUN mkdir -p /home/podman/.local/share/containers && \
+    chown podman:podman -R /home/podman && \
+    chmod 644 /etc/containers/containers.conf
+
+# Copy & modify the defaults to provide reference if runtime changes needed.
+# Changes here are required for running with fuse-overlay storage inside container.
+RUN sed -e 's|^#mount_program|mount_program|g' \
+           -e '/additionalimage.*/a "/var/lib/shared",' \
+           -e 's|^mountopt[[:space:]]*=.*$|mountopt = "nodev,fsync=0"|g' \
+           /usr/share/containers/storage.conf \
+           > /etc/containers/storage.conf
+
+RUN mkdir -p /run/user/1000 && chown podman:podman $_
+
+USER podman
+RUN mkdir -p /home/podman/go/src/github.com/containers && \
+	cd $_ && \
+	git clone --depth=1 --branch ${VERSION} https://github.com/containers/podman && \
+	cd podman && \
+	make bin/podman-testing bin/podman
+
+WORKDIR /home/podman/go/src/github.com/containers/podman
+COPY --chown=podman:podman --chmod=755 skip_tests.sh skip_tests.sh
+RUN ./skip_tests.sh
+
+COPY --chown=podman:podman --chmod=755 run_tests.sh run_tests.sh
+
+ENTRYPOINT ["/usr/libexec/podman/catatonit", "--"]
+CMD [ "./run_tests.sh" ]`)
+
+func testExtendedTestdataNodeNested_containerDockerfileBytes() ([]byte, error) {
+	return _testExtendedTestdataNodeNested_containerDockerfile, nil
+}
+
+func testExtendedTestdataNodeNested_containerDockerfile() (*asset, error) {
+	bytes, err := testExtendedTestdataNodeNested_containerDockerfileBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindataFileInfo{name: "test/extended/testdata/node/nested_container/Dockerfile", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
+	a := &asset{bytes: bytes, info: info}
+	return a, nil
+}
+
+var _testExtendedTestdataNodeNested_containerContainersConf = []byte(`[containers]
+netns="private"
+userns="host"
+ipcns="shareable"
+utsns="private"
+cgroupns="private"
+cgroups="enabled"
+log_driver = "k8s-file"
+volumes = [
+    "/proc:/proc",
+	"/sys:/sys",
+]
+default_sysctls = []
+[engine]
+cgroup_manager = "cgroupfs"
+events_logger="file"
+runtime="crun"`)
+
+func testExtendedTestdataNodeNested_containerContainersConfBytes() ([]byte, error) {
+	return _testExtendedTestdataNodeNested_containerContainersConf, nil
+}
+
+func testExtendedTestdataNodeNested_containerContainersConf() (*asset, error) {
+	bytes, err := testExtendedTestdataNodeNested_containerContainersConfBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindataFileInfo{name: "test/extended/testdata/node/nested_container/containers.conf", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
+	a := &asset{bytes: bytes, info: info}
+	return a, nil
+}
+
+var _testExtendedTestdataNodeNested_containerRun_testsSh = []byte(`#!/usr/bin/env bash
+
+mkdir -p serial-junit
+PODMAN=$(pwd)/bin/podman bats -T --report-formatter junit -o serial-junit --filter-tags '!ci:parallel' test/system/ || touch fail
+mkdir -p parallel-junit
+PODMAN=$(pwd)/bin/podman bats -T --report-formatter junit -o parallel-junit --filter-tags ci:parallel -j $(nproc) test/system/ || touch fail
+touch done
+echo "Finished running tests."
+# wait for the test results to be retrieved
+tail -f /dev/null
+`)
+
+func testExtendedTestdataNodeNested_containerRun_testsShBytes() ([]byte, error) {
+	return _testExtendedTestdataNodeNested_containerRun_testsSh, nil
+}
+
+func testExtendedTestdataNodeNested_containerRun_testsSh() (*asset, error) {
+	bytes, err := testExtendedTestdataNodeNested_containerRun_testsShBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindataFileInfo{name: "test/extended/testdata/node/nested_container/run_tests.sh", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
+	a := &asset{bytes: bytes, info: info}
+	return a, nil
+}
+
+var _testExtendedTestdataNodeNested_containerSkip_testsSh = []byte(`#!/usr/bin/env bash
+set -e
+
+TEST_DIR=test/system
+function insert_skip {
+	file=$1
+	testcase=$2
+	# escape slash
+	testcase=$(echo $testcase | sed 's/\//\\\//g')
+	echo $testcase
+	sed -i "/$testcase/a\
+	skip 'not working in cri-o'" "$TEST_DIR/$file"
+}
+
+# cannot pause without cgroup, and no cgroup in rootless container
+rm $TEST_DIR/080-pause.bats
+# systemd
+rm $TEST_DIR/250-systemd.bats
+rm $TEST_DIR/251-system-service.bats
+rm $TEST_DIR/252-quadlet.bats
+rm $TEST_DIR/255-auto-update.bats
+rm $TEST_DIR/270-socket-activation.bats
+
+# broken tests https://github.com/containers/podman/commit/c65bb903b63c60a1ef2ccd3c21e118c4784d2f6b#diff-78c7444aee8aa212e7101395b618015dec2d5d1eab06fb7de2a8ecdf7418cde4
+insert_skip 030-run.bats 'podman run - basic tests'
+insert_skip 030-run.bats 'podman run - no entrypoint'
+
+# Error: retrieving label for image "a6b6f7ac212ddfdb879e9b9c4df1655c9b07ead8b028110e657364485c577bff": you may need to remove the image to resolve the error: fallback error checking whether image is a manifest list: choosing image instance: no image found in manifest list for architecture "amd64", variant "", OS "linux": choosing image instance: no image found in manifest list for architecture "amd64", variant "", OS "linux"
+insert_skip 012-manifest.bats "podman images - bare manifest"
+# There is only one tty - /dev/tty
+insert_skip 030-run.bats 'podman run --privileged as rootless will not mount /dev/tty\\d+'
+# systemd can't run in nested container
+insert_skip 030-run.bats "podman run - /run must not be world-writable in systemd containers"
+# Error: OCI runtime error: crun: write to ` + "`" + `/proc/self/oom_score_adj` + "`" + `: Invalid argument
+insert_skip 030-run.bats "podman run doesn't override oom-score-adj"
+# flaky
+insert_skip 090-events.bats "image events"
+# mount not shown
+insert_skip 160-volumes.bats "podman run --volumes : basic"
+# ping: permission denied (are you root?) 
+insert_skip 200-pod.bats "podman pod create - hashtag AllTheOptions"
+# /home/podman/go/src/github.com/containers/podman/test/system/helpers.bash: line 1117: /usr/bin/expr: Argument list too long
+insert_skip 200-pod.bats "podman pod top - containers in different PID namespaces"
+# flaky
+insert_skip 320-system-df.bats "podman system df --format json functionality"
+# FAIL: Pause process 42825 is still running even after podman system migrate
+insert_skip 550-pause-process.bats "rootless podman only ever uses single pause process"
+ 
+# Error: cannot remove container 31261d3890fe98a254b9fc2eb219753bc28df05efdf22c0f34222c33a2185a27 as it could not be stopped: given PID did not die within timeout
+insert_skip 065-cp.bats "podman cp file from/to host while --pid=host"
+insert_skip 195-run-namespaces.bats "podman test all namespaces"
+ 
+# userns
+insert_skip 170-run-userns.bats "podman userns=auto in config file"
+insert_skip 170-run-userns.bats "podman userns=auto and secrets"
+insert_skip 170-run-userns.bats "podman userns=auto with id mapping"
+insert_skip 700-play.bats "podman kube restore user namespace" # usernamespace
+ 
+# # won't fix
+
+# cat: /proc/sys/net/core/wmem_default: No such file or directory
+# https://github.com/moby/moby/issues/30778
+insert_skip 505-networking-pasta.bats "UDP/IPv4 large transfer, tap"
+insert_skip 505-networking-pasta.bats "UDP/IPv4 large transfer, loopback"
+insert_skip 505-networking-pasta.bats "UDP/IPv6 large transfer, tap"
+insert_skip 505-networking-pasta.bats "UDP/IPv6 large transfer, loopback"
+
+# healthcheck
+insert_skip 055-rm.bats "podman container rm doesn't affect stopping containers"
+insert_skip 055-rm.bats "podman container rm --force doesn't leave running processes"
+insert_skip 220-healthcheck.bats "podman healthcheck"
+insert_skip 700-play.bats "podman kube play healthcheck should wait initialDelaySeconds before updating status"
+
+# cgroup
+# cannot pause the container without a cgroup
+# https://github.com/containers/podman/issues/12782
+insert_skip 200-pod.bats "podman pod cleans cgroup and keeps limits"
+# opening file ` + "`" + `memory.max` + "`" + ` for writing: Permission denied
+insert_skip 280-update.bats "podman update - test all options"
+insert_skip 280-update.bats "podman update - resources on update are not changed unless requested"
+insert_skip 600-completion.bats "podman shell completion test"
+
+# systemd
+insert_skip 030-run.bats "podman run --log-driver" # FAIL: podman logs, with driver 'journald'
+insert_skip 035-logs.bats "podman logs - tail test, journald"
+insert_skip 035-logs.bats "podman logs - multi journald"
+insert_skip 035-logs.bats "podman logs restarted journald"
+insert_skip 035-logs.bats "podman logs - since journald"
+insert_skip 035-logs.bats "podman logs - until journald"
+insert_skip 035-logs.bats "podman logs - --follow journald"
+insert_skip 035-logs.bats "podman logs - --since --follow journald"
+insert_skip 035-logs.bats "podman logs - --until --follow journald"
+insert_skip 035-logs.bats "podman logs - journald log driver requires journald events backend"
+insert_skip 090-events.bats "events with disjunctive filters - journald"
+insert_skip 090-events.bats "events with file backend and journald logdriver with --follow failure"
+insert_skip 090-events.bats "events - container inspect data - journald"
+insert_skip 220-healthcheck.bats "podman healthcheck --health-log-destination journal"
+insert_skip 420-cgroups.bats "podman run, preserves initial --cgroup-manager"
+`)
+
+func testExtendedTestdataNodeNested_containerSkip_testsShBytes() ([]byte, error) {
+	return _testExtendedTestdataNodeNested_containerSkip_testsSh, nil
+}
+
+func testExtendedTestdataNodeNested_containerSkip_testsSh() (*asset, error) {
+	bytes, err := testExtendedTestdataNodeNested_containerSkip_testsShBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindataFileInfo{name: "test/extended/testdata/node/nested_container/skip_tests.sh", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
+	a := &asset{bytes: bytes, info: info}
+	return a, nil
+}
+
 var _testExtendedTestdataNode_tuningNtoStalldYaml = []byte(`apiVersion: tuned.openshift.io/v1
 kind: Tuned
 metadata:
@@ -58815,6 +59084,10 @@ var _bindata = map[string]func() (*asset, error){
 	"test/extended/testdata/net-attach-defs/whereabouts-nad.yml":                                             testExtendedTestdataNetAttachDefsWhereaboutsNadYml,
 	"test/extended/testdata/net-attach-defs/whereabouts-race-awake.yml":                                      testExtendedTestdataNetAttachDefsWhereaboutsRaceAwakeYml,
 	"test/extended/testdata/net-attach-defs/whereabouts-race-sleepy.yml":                                     testExtendedTestdataNetAttachDefsWhereaboutsRaceSleepyYml,
+	"test/extended/testdata/node/nested_container/Dockerfile":                                                testExtendedTestdataNodeNested_containerDockerfile,
+	"test/extended/testdata/node/nested_container/containers.conf":                                           testExtendedTestdataNodeNested_containerContainersConf,
+	"test/extended/testdata/node/nested_container/run_tests.sh":                                              testExtendedTestdataNodeNested_containerRun_testsSh,
+	"test/extended/testdata/node/nested_container/skip_tests.sh":                                             testExtendedTestdataNodeNested_containerSkip_testsSh,
 	"test/extended/testdata/node_tuning/nto-stalld.yaml":                                                     testExtendedTestdataNode_tuningNtoStalldYaml,
 	"test/extended/testdata/oauthserver/cabundle-cm.yaml":                                                    testExtendedTestdataOauthserverCabundleCmYaml,
 	"test/extended/testdata/oauthserver/oauth-network.yaml":                                                  testExtendedTestdataOauthserverOauthNetworkYaml,
@@ -59608,6 +59881,14 @@ var _bintree = &bintree{nil, map[string]*bintree{
 					"whereabouts-nad.yml":         {testExtendedTestdataNetAttachDefsWhereaboutsNadYml, map[string]*bintree{}},
 					"whereabouts-race-awake.yml":  {testExtendedTestdataNetAttachDefsWhereaboutsRaceAwakeYml, map[string]*bintree{}},
 					"whereabouts-race-sleepy.yml": {testExtendedTestdataNetAttachDefsWhereaboutsRaceSleepyYml, map[string]*bintree{}},
+				}},
+				"node": {nil, map[string]*bintree{
+					"nested_container": {nil, map[string]*bintree{
+						"Dockerfile":      {testExtendedTestdataNodeNested_containerDockerfile, map[string]*bintree{}},
+						"containers.conf": {testExtendedTestdataNodeNested_containerContainersConf, map[string]*bintree{}},
+						"run_tests.sh":    {testExtendedTestdataNodeNested_containerRun_testsSh, map[string]*bintree{}},
+						"skip_tests.sh":   {testExtendedTestdataNodeNested_containerSkip_testsSh, map[string]*bintree{}},
+					}},
 				}},
 				"node_tuning": {nil, map[string]*bintree{
 					"nto-stalld.yaml": {testExtendedTestdataNode_tuningNtoStalldYaml, map[string]*bintree{}},
