4a936de9190cc52a5cf35f46d0c248870de16f64
Devan Goodwin <dgoodwin@redhat.com>
2025-04-03 13:45:17 -0300

    Revert "Add monitortest for elevated etcd disk fsync intervals"
    This reverts commit 59633605693bde323209c5b3aa432e11248d3940.
    This was mistakenly merged with another pr.

--- pkg/monitor/monitorapi/types.go
+++ pkg/monitor/monitorapi/types.go
@@ -255,8 +255,6 @@ const (
 	// client metrics show error connecting to the kube-apiserver
 	APIUnreachableFromClientMetrics IntervalReason = "APIUnreachableFromClientMetrics"
 
-	EtcdHighDiskFsyncReason IntervalReason = "EtcdHighDiskFsync"
-
 	LeaseAcquiring        IntervalReason = "Acquiring"
 	LeaseAcquiringStarted IntervalReason = "StartedAcquiring"
 	LeaseAcquired         IntervalReason = "Acquired"
@@ -367,7 +365,6 @@ const (
 	SourceCloudMetrics                           = "CloudMetrics"
 
 	SourceAPIUnreachableFromClient IntervalSource = "APIUnreachableFromClient"
-	SourceEtcdDiskFsyncMonitor     IntervalSource = "EtcdDiskFsyncMonitor"
 	SourceMachine                  IntervalSource = "MachineMonitor"
 
 	SourceGenerationMonitor IntervalSource = "GenerationMonitor"
