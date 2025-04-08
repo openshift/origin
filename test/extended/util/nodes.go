e21b820908a896a3e6a296001663f161b32de072
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2025-04-08 19:12:54 +0000

    networking: add BGP VRF-Lite tests
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- test/extended/util/nodes.go
+++ test/extended/util/nodes.go
@@ -1,9 +1,12 @@
 package util
 
 import (
+	"context"
 	"strings"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
@@ -48,4 +51,39 @@ func DebugNodeRetryWithOptionsAndChroot(oc *CLI, nodeName string, debugNodeNames
 		return true, nil
 	})
 	return stdOut, err
+}
+
+func GetClusterNodesBySelector(oc *CLI, selector string) ([]corev1.Node, error) {
+	nodes, err := oc.AsAdmin().KubeClient().CoreV1().Nodes().List(
+		context.TODO(),
+		metav1.ListOptions{
+			LabelSelector: selector,
+		})
+	if err != nil {
+		return nil, err
+	}
+	return nodes.Items, nil
+}
+
+func GetAllClusterNodes(oc *CLI) ([]corev1.Node, error) {
+	return GetClusterNodesBySelector(oc, "")
+}
+
+func DebugSelectedNodesRetryWithOptionsAndChroot(oc *CLI, selector string, debugNodeNamespace string, cmd ...string) (map[string]string, error) {
+	nodes, err := GetClusterNodesBySelector(oc, selector)
+	if err != nil {
+		return nil, err
+	}
+	stdOut := make(map[string]string, len(nodes))
+	for _, node := range nodes {
+		stdOut[node.Name], err = DebugNodeRetryWithOptionsAndChroot(oc, node.Name, debugNodeNamespace, cmd...)
+		if err != nil {
+			return stdOut, err
+		}
+	}
+	return stdOut, nil
+}
+
+func DebugAllNodesRetryWithOptionsAndChroot(oc *CLI, debugNodeNamespace string, cmd ...string) (map[string]string, error) {
+	return DebugSelectedNodesRetryWithOptionsAndChroot(oc, "", debugNodeNamespace, cmd...)
 }
