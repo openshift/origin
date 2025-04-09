71f97d319161f5cba40f13912c52ad614fbeaa96
German Parente <german.parente@gmail.com>
2025-04-09 15:35:58 +0200

    OCPBUGS-54771: Fix egress firewall tests by updating the URL from docs.redhat.com to redhat.com

--- test/extended/testdata/bindata.go
+++ test/extended/testdata/bindata.go
@@ -39600,7 +39600,7 @@ spec:
   egress:
   - type: Allow
     to:
-      dnsName: docs.openshift.com
+      dnsName: docs.redhat.com
   - type: Deny
     to:
       dnsName: www.google.com
@@ -39643,7 +39643,7 @@ spec:
   egress:
   - type: Allow
     to:
-      dnsName: docs.openshift.com
+      dnsName: docs.redhat.com
   - type: Allow
     to:
       cidrSelector: 8.8.8.8/32
