540c5bdda7f75317f49ee3b7189bcaba38b0d6fe
Stephen Benjamin <stephen@redhat.com>
2025-04-21 19:39:02 -0400

    Support sharding tests across multiple invocations of openshift-tests

---
+++ pkg/test/ginkgo/sharder.go
@@ -0,0 +1,45 @@
+package ginkgo
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Sharder interface {
+	// Shard selects tests for the given executor and total shards.
+	// The returned slice must be deterministic and non-overlapping across shards.
+	Shard(allTests []*testCase, shardCount, shardID int) ([]*testCase, error)
+
+	// Name returns the name of the strategy (e.g. "hash", "time-balanced").
+	Name() string
+}
+
+type HashSharder struct{}
+
+func (h *HashSharder) Shard(allTests []*testCase, shardCount, shardID int) ([]*testCase, error) {
+	var selected []*testCase
+
+	if shardID > shardCount {
+		return nil, fmt.Errorf("shard %d is greater than %d", shardID, shardCount)
+	}
+
+	if shardID == 0 || shardCount == 0 {
+		logrus.Warningf("Sharding disabled, returning all tests")
+		return allTests, nil
+	}
+
+	for _, test := range allTests {
+		sum := sha256.Sum256([]byte(test.name))
+		val := binary.BigEndian.Uint32(sum[:4])
+		shard := (int(val) % shardCount) + 1
+		if shard == shardID {
+			selected = append(selected, test)
+		}
+	}
+	return selected, nil
+}
+
+func (h *HashSharder) Name() string { return "hash" }
