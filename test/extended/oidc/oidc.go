ce772dcb9e76ba162d784fbe8e60505f1851cb3d
Bryce Palmer <bpalmer@redhat.com>
2025-06-16 14:32:06 -0400

    wip: initial set of tests
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- test/extended/oidc/oidc.go
+++ test/extended/oidc/oidc.go
@@ -1,9 +1,14 @@
 package oidc
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"path"
 	"time"
 
@@ -13,6 +18,8 @@ import (
 	routev1 "github.com/openshift/api/route/v1"
 	exutil "github.com/openshift/origin/test/extended/util"
 	appsv1 "k8s.io/api/apps/v1"
+	authzv1 "k8s.io/api/authorization/v1"
+	authnv1 "k8s.io/api/authentication/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -28,57 +35,131 @@ type kubeObject interface {
 	metav1.Object
 }
 
-var _ = g.Describe("[sig-auth][Serial][Slow][OCPFeatureGate:ExternalOIDC] Configuring an external OIDC provider", func() {
+var _ = g.Describe("[sig-auth][Serial][Slow][OCPFeatureGate:ExternalOIDC]", func() {
 	defer g.GinkgoRecover()
 	oc := exutil.NewCLI("external-oidc")
-	oc.KubeFramework().NamespacePodSecurityLevel = api.LevelPrivileged
-	var resources []kubeObject
-	var originalAuth *configv1.Authentication
 
-	g.BeforeAll(func() {
-		var err error
-		ctx := context.TODO()
-		resources, err = deployKeycloak(ctx, oc)
-		o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error deploying keycloak")
+	g.Context("Configuring an external OIDC provider", func() {
+		oc.KubeFramework().NamespacePodSecurityLevel = api.LevelPrivileged
+		var resources []kubeObject
+		var originalAuth *configv1.Authentication
+		var keycloakURL string
 
-		// TODO: Configure Keycloak
+		g.BeforeAll(func() {
+			var err error
+			ctx := context.TODO()
+			resources, err = deployKeycloak(ctx, oc)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error deploying keycloak")
 
-		original, _, err := configureOIDCAuthentication(ctx, oc)
-		o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error configuring OIDC authentication")
-		originalAuth = original
-	})
+			kcURL, err := admittedURLForRoute(ctx, oc, keycloakResourceName)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error getting keycloak route URL")
+			keycloakURL = kcURL
 
-	g.AfterAll(func() {
-		ctx := context.TODO()
-		err := removeResources(ctx, oc, resources...)
-		o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error cleaning up keycloak resources")
+			original, _, err := configureOIDCAuthentication(ctx, oc)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error configuring OIDC authentication")
+			originalAuth = original
+		})
 
-		_, err = oc.AdminConfigClient().ConfigV1().Authentications().Update(ctx, originalAuth, metav1.UpdateOptions{})
-		o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error reverting authentication to original state")
-	})
+		g.AfterAll(func() {
+			ctx := context.TODO()
+			err := removeResources(ctx, oc, resources...)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error cleaning up keycloak resources")
 
-	g.It("should rollout configuration on the kube-apiserver successfully", func() {
-		g.Fail("not implemented")
-	})
+			_, err = oc.AdminConfigClient().ConfigV1().Authentications().Update(ctx, originalAuth, metav1.UpdateOptions{})
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error reverting authentication to original state")
+		})
 
-	g.It("should remove the OpenShift OAuth stack", func() {
-		g.Fail("not implemented")
-	})
+		g.It("should configure kube-apiserver", func() {
+			kas, err := oc.AdminOperatorClient().OperatorV1().KubeAPIServers().Get(context.TODO(), "cluster", metav1.GetOptions{})
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error getting the kubeapiservers.operator.openshift.io/cluster")
 
-	g.It("should not accept tokens provided by the OAuth server", func() {
-		g.Fail("not implemented")
-	})
+			observedConfig := map[string]interface{}{}
+			err = json.Unmarshal(kas.Spec.ObservedConfig.Raw, observedConfig)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error unmarshalling the KAS observed configuration")
 
-	g.It("should accept tokens issued by the external IdP", func() {
-		g.Fail("not implemented")
-	})
+			apiServerArgs := observedConfig["apiServerArguments"].(map[string]interface{})
 
-	g.It("should accept authentication via a kubeconfig (break-glass)", func() {
-		g.Fail("not implemented")
-	})
+			o.Expect(apiServerArgs["authentication-token-webhook-config-file"]).To(o.BeNil(), "authentication-token-webhook-config-file argument should not be specified when OIDC authentication is configured")
+			o.Expect(apiServerArgs["authentication-token-webhook-version"]).To(o.BeNil(), "authentication-token-webhook-version argument should not be specified when OIDC authentication is configured")
+			o.Expect(apiServerArgs["authConfig"]).To(o.BeNil(), "authConfig argument should not be specified when OIDC authentication is configured")
 
-	g.It("should map cluster identities correctly", func() {
-		g.Fail("not implemented")
+			o.Expect(apiServerArgs["authentication-config"]).NotTo(o.BeNil(), "authentication-config argument should be specified when OIDC authentication is configured")
+			o.Expect(apiServerArgs["authentication-config"].([]interface{})[0].(string)).To(o.Equal("/etc/kubernetes/static-pod-resources/configmaps/auth-config/auth-config.json"))
+		})
+
+		g.It("should remove the OpenShift OAuth stack", func() {
+			g.Skip("functionality not yet implemented")
+		})
+
+		g.It("should not accept tokens provided by the OAuth server", func() {
+			g.Fail("not implemented")
+		})
+
+		g.It("should accept tokens issued by the external IdP", func() {
+			// TODO: variables for test user data
+			kc, err := keycloakClientFor(keycloakURL)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error creating a keycloak client")
+
+			// First authenticate as the admin keyloak user so we can add new groups and users
+			err = kc.Authenticate("admin-cli", keycloakAdminUsername, keycloakAdminPassword)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error authenticating as keycloak admin")
+
+			o.Expect(kc.CreateGroup("ocp-test-accepted-token-group")).To(o.Succeed(), "should be able to create a new keycloak group")
+			o.Expect(kc.CreateUser("homersimpson", "donuts", "ocp-test-accepted-token-group")).To(o.Succeed(), "should be able to create a new keycloak user")
+
+			err = kc.Authenticate("admin-cli", "homersimpson", "donuts")
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error authenticating as keycloak user")
+
+			tokenOC := oc.WithToken(kc.IdToken())
+
+			// should always be able to create an SSAR for yourself
+			_, err = tokenOC.KubeClient().AuthorizationV1().SelfSubjectAccessReviews().Create(context.TODO(), &authzv1.SelfSubjectAccessReview{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "can-homer-get-pods",
+				},
+				Spec: authzv1.SelfSubjectAccessReviewSpec{
+					ResourceAttributes: &authzv1.ResourceAttributes{
+						Resource: "pods",
+						Verb:     "get",
+					},
+				},
+			}, metav1.CreateOptions{})
+			o.Expect(err).NotTo(o.HaveOccurred(), "should be able to create a SelfSubjectAccessReview")
+		})
+
+		g.It("should accept authentication via a kubeconfig (break-glass)", func() {
+			_, err := oc.KubeClient().CoreV1().Pods(oc.Namespace()).List(context.TODO(), metav1.ListOptions{})
+			o.Expect(err).NotTo(o.HaveOccurred(), "should be able to list pods using certificate-based authentication")
+		})
+
+		g.It("should map cluster identities correctly", func() {
+			// TODO: variables for test user data
+			kc, err := keycloakClientFor(keycloakURL)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error creating a keycloak client")
+
+			// First authenticate as the admin keyloak user so we can add new groups and users
+			err = kc.Authenticate("admin-cli", keycloakAdminUsername, keycloakAdminPassword)
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error authenticating as keycloak admin")
+
+			o.Expect(kc.CreateGroup("ocp-test-cluster-identity-group")).To(o.Succeed(), "should be able to create a new keycloak group")
+			o.Expect(kc.CreateUser("bartsimpson", "donuts", "ocp-test-cluster-identity-group")).To(o.Succeed(), "should be able to create a new keycloak user")
+
+			err = kc.Authenticate("admin-cli", "bartsimpson", "donuts")
+			o.Expect(err).NotTo(o.HaveOccurred(), "should not encounter an error authenticating as keycloak user")
+
+			tokenOC := oc.WithToken(kc.IdToken())
+
+			// should always be able to create an SSAR for yourself
+			ssr, err := tokenOC.KubeClient().AuthenticationV1().SelfSubjectReviews().Create(context.TODO(), &authnv1.SelfSubjectReview{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "bart-info",
+				},
+			}, metav1.CreateOptions{})
+			o.Expect(err).NotTo(o.HaveOccurred(), "should be able to create a SelfSubjectReview")
+
+			o.Expect(ssr.Status.UserInfo.Username).To(o.Equal("bartsimpson"))
+			o.Expect(ssr.Status.UserInfo.Groups).To(o.Equal([]string{"ocp-test-cluster-identity-group"}))
+		})
 	})
 })
 
@@ -167,8 +248,11 @@ func deployKeycloak(ctx context.Context, client *exutil.CLI) ([]kubeObject, erro
 	}
 	resources = append(resources, route)
 
-	// TODO: CA bundle ConfigMap in the openshift-config namespace
-	// Done by copying the default ingress CA in openshift-config-managed/default-ingress-cert
+	caConfigMap, err := createKeycloakCAConfigMap(ctx, client)
+	if err != nil {
+		return resources, fmt.Errorf("creating CA configmap for keycloak: %w", err)
+	}
+	resources = append(resources, caConfigMap)
 
 	return resources, nil
 }
@@ -515,4 +599,161 @@ func admittedURLForRoute(ctx context.Context, client *exutil.CLI, routeName stri
 		return false, fmt.Errorf("no admitted ingress for route %q", route.Name)
 	})
 	return admittedURL, err
+}
+
+type keycloakClient struct {
+	realm    string
+	client   *http.Client
+	adminURL *url.URL
+
+	accessToken string
+	idToken     string
+}
+
+func keycloakClientFor(keycloakURL string) (*keycloakClient, error) {
+	baseURL, err := url.Parse(keycloakURL)
+	if err != nil {
+		return nil, fmt.Errorf("parsing url: %w", err)
+	}
+	return &keycloakClient{
+		realm:    "master",
+		client:   http.DefaultClient,
+		adminURL: baseURL.JoinPath("admin", "realms", "master"),
+	}, nil
+}
+
+func (kc *keycloakClient) CreateGroup(name string) error {
+	groupURL := kc.adminURL.JoinPath("groups")
+
+	group := map[string]interface{}{
+		"name": name,
+	}
+
+	groupBytes, err := json.Marshal(group)
+	if err != nil {
+		return fmt.Errorf("marshalling group configuration %v", group)
+	}
+
+	resp, err := kc.DoRequest(http.MethodPost, groupURL.String(), runtime.ContentTypeJSON, bytes.NewBuffer(groupBytes))
+	if err != nil {
+		return fmt.Errorf("sending POST request to %q to create group %s", groupURL.String(), name)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		respBytes, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed creating group %q: %s - %s", name, resp.Status, respBytes)
+	}
+
+	return nil
+}
+
+func (kc *keycloakClient) CreateUser(username, password string, groups ...string) error {
+	userURL := kc.adminURL.JoinPath("users")
+
+	user := map[string]interface{}{
+		"username":      username,
+		"email":         fmt.Sprintf("%s@payload.openshift.io", username),
+		"enabled":       true,
+		"emailVerified": true,
+		"groups":        groups,
+		"credentials": map[string]interface{}{
+			"temporary": false,
+			"type":      "password",
+			"value":     password,
+		},
+	}
+
+	userBytes, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("marshalling user configuration %v", user)
+	}
+
+	resp, err := kc.DoRequest(http.MethodPost, userURL.String(), runtime.ContentTypeJSON, bytes.NewBuffer(userBytes))
+	if err != nil {
+		return fmt.Errorf("sending POST request to %q to create user %v", userURL.String(), user)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		respBytes, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed creating user %v: %s - %s", user, resp.Status, respBytes)
+	}
+
+	return nil
+}
+
+func (kc *keycloakClient) Authenticate(clientID, username, password string) error {
+	data := url.Values{
+		"username":   []string{username},
+		"password":   []string{password},
+		"grant_type": []string{"password"},
+		"client_id":  []string{clientID},
+		"scope":      []string{"openid"},
+	}
+
+	tokenURL := *kc.adminURL
+	tokenURL.Path = fmt.Sprintf("/realms/%s/protocol/openid-connect/token", kc.realm)
+
+	resp, err := kc.DoRequest(http.MethodPost, tokenURL.String(), "application/x-www-form-urlencoded", bytes.NewBuffer([]byte(data.Encode())))
+	if err != nil {
+		return fmt.Errorf("authenticating as user %q: %w", username, err)
+	}
+	defer resp.Body.Close()
+
+	respBody := map[string]interface{}{}
+	respBodyData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("reading response data: %w", err)
+	}
+
+	err = json.Unmarshal(respBodyData, respBody)
+	if err != nil {
+		return fmt.Errorf("unmarshalling response body %s: %w", respBodyData, err)
+	}
+
+	accessTokenData, ok := respBody["access_token"]
+	if !ok {
+		return errors.New("unable to extract access token from the response body: access_token field is missing")
+	}
+
+	accessToken, ok := accessTokenData.(string)
+	if !ok {
+		return fmt.Errorf("expected accessToken to be of type string but was %T", accessTokenData)
+	}
+	kc.accessToken = accessToken
+
+	idTokenData, ok := respBody["id_token"]
+	if !ok {
+		return errors.New("unable to extract id token from the response body: id_token field is missing")
+	}
+
+	idToken, ok := idTokenData.(string)
+	if !ok {
+		return fmt.Errorf("expected idToken to be of type string but was %T", idTokenData)
+	}
+	kc.idToken = idToken
+
+	return nil
+}
+
+func (kc *keycloakClient) DoRequest(method, url, contentType string, body io.Reader) (*http.Response, error) {
+	if len(kc.accessToken) == 0 {
+		panic("must authenticate before calling keycloakClient.DoRequest")
+	}
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, fmt.Errorf("building request: %w", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", kc.accessToken))
+	req.Header.Set("Content-Type", contentType)
+	req.Header.Set("Accept", runtime.ContentTypeJSON)
+
+	return kc.client.Do(req)
+}
+
+func (kc *keycloakClient) IdToken() string {
+	return kc.idToken
 }
