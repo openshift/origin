33eae7302e50e18d6431b5a144dff4d8ad68d9a0
Deirdre Malone <dmalone@redhat.com>
2025-06-17 13:29:13 +0100

    OCPBUGS-57517 - Remove Python 3.8-ubi8 from Origin

--- test/extended/image_ecosystem/s2i_images.go
+++ test/extended/image_ecosystem/s2i_images.go
@@ -67,13 +67,6 @@ var s2iImages = map[string][]tc{
 			Arches:   []string{"amd64", "arm64", "ppc64le", "s390x"},
 		},
 		{
-			Version:  "38",
-			Cmd:      "python --version",
-			Expected: "Python 3.8",
-			Tag:      "3.8-ubi8",
-			Arches:   []string{"amd64", "arm64", "ppc64le", "s390x"},
-		},
-		{
 			Version:  "39",
 			Cmd:      "python --version",
 			Expected: "Python 3.9",
