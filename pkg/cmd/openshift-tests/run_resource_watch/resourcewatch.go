ba6df65381ec6e1f6e34157a8aae538fddbeafdb
Matthew Booth <mbooth@redhat.com>
2025-07-04 15:52:42 +0100

    run-resourcewatch: Add --to-json and --from-json
    These allow:
    * writing to a json file instead of a git repository
    * reading from a json file instead of an api-server

--- pkg/cmd/openshift-tests/run_resource_watch/resourcewatch.go
+++ pkg/cmd/openshift-tests/run_resource_watch/resourcewatch.go
@@ -1,12 +1,16 @@
 package run_resource_watch
 
 import (
+	"fmt"
+
 	"github.com/openshift/origin/pkg/resourcewatch/operator"
 	"github.com/spf13/cobra"
 	"k8s.io/kubectl/pkg/util/templates"
 )
 
 func NewRunResourceWatchCommand() *cobra.Command {
+	var toJsonPath, fromJsonPath string
+
 	cmd := &cobra.Command{
 		Use:   "run-resourcewatch",
 		Short: "Run watch for resource changes and commit each to a git repository",
@@ -22,12 +26,22 @@ func NewRunResourceWatchCommand() *cobra.Command {
 
 		SilenceUsage:  true,
 		SilenceErrors: true,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			// Check for mutual exclusivity of --to-json and --from-json
+			if toJsonPath != "" && fromJsonPath != "" {
+				return fmt.Errorf("--to-json and --from-json are mutually exclusive")
+			}
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return operator.RunResourceWatch()
+			return operator.RunResourceWatch(toJsonPath, fromJsonPath)
 		},
 	}
 	var dummy string
 	cmd.Flags().StringVar(&dummy, "kubeconfig", "", "This option is not used any more. It will be removed in later releases")
 	cmd.Flags().StringVar(&dummy, "namespace", "", "This option is not used any more. It will be removed in later releases")
+	cmd.Flags().StringVar(&toJsonPath, "to-json", "", "Path to JSON file for output (mutually exclusive with --from-json)")
+	cmd.Flags().StringVar(&fromJsonPath, "from-json", "", "Path to JSON file for input (mutually exclusive with --to-json)")
+
 	return cmd
 }
