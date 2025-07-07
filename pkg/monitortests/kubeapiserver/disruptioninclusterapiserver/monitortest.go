f0d87c4b8c305d886923d700820fbdb916ddaf3c
Forrest Babcock <36795216+neisw@users.noreply.github.com>
2025-07-07 07:44:21 -0400

    Revert "trt-2163: return error"

--- pkg/monitortests/kubeapiserver/disruptioninclusterapiserver/monitortest.go
+++ pkg/monitortests/kubeapiserver/disruptioninclusterapiserver/monitortest.go
@@ -15,7 +15,6 @@ import (
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 
 	"github.com/openshift/origin/pkg/monitortestlibrary/disruptionlibrary"
-	"github.com/openshift/origin/pkg/monitortestlibrary/platformidentification"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
 
@@ -329,14 +328,8 @@ func (i *InvariantInClusterDisruption) StartCollection(ctx context.Context, admi
 	cmd.Stdout = out
 	cmd.Stderr = errOut
 	if err := cmd.Run(); err != nil {
-		// specifically ipv6 disconnected isn't expected to work
-		jobType, err := platformidentification.GetJobType(context.TODO(), adminRESTConfig)
-		if err == nil && jobType.Platform == "metal" {
-			i.notSupportedReason = fmt.Sprintf("unable to determine openshift-tests image for metal platform: %v: %v", err, errOut.String())
-			return nil
-		}
-
-		return fmt.Errorf("unable to determine openshift-tests image: %v: %v", err, errOut.String())
+		i.notSupportedReason = fmt.Sprintf("unable to determine openshift-tests image: %v: %v", err, errOut.String())
+		return nil
 	}
 	i.openshiftTestsImagePullSpec = strings.TrimSpace(out.String())
 	log.Infof("openshift-tests image pull spec is %v", i.openshiftTestsImagePullSpec)
