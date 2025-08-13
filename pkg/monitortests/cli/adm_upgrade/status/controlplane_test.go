cc663f6630bdb804f83af1d986c969fdbf016912
Petr Muller <muller@redhat.com>
2025-08-13 19:13:07 +0200

    upgrade status CLI monitortest: fix naming conflicts

--- pkg/monitortests/cli/adm_upgrade/status/controlplane_test.go
+++ pkg/monitortests/cli/adm_upgrade/status/controlplane_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/openshift/origin/pkg/test/ginkgo/junitapi"
 )
 
-var exampleOutput = `Unable to fetch alerts, ignoring alerts in 'Update Health':  failed to get alerts from Thanos: no token is currently in use for this session
+var cpExampleOutput = `Unable to fetch alerts, ignoring alerts in 'Update Health':  failed to get alerts from Thanos: no token is currently in use for this session
 = Control Plane =
 Assessment:      Progressing
 Target Version:  4.20.0-0.ci-2025-08-13-121604-test-ci-op-njttt0ww-latest (from 4.20.0-0.ci-2025-08-13-114210-test-ci-op-njttt0ww-initial)
@@ -44,7 +44,7 @@ ip-10-0-106-212.us-west-1.compute.internal   Outdated     Pending   4.20.0-0.ci-
 SINCE   LEVEL   IMPACT   MESSAGE
 8m57s   Info    None     Update is proceeding well`
 
-var badOutput = `= Control Plane =
+var cpBadOutput = `= Control Plane =
 Assessment:      Progressing
 Target Version:  4.20.0-0.ci-2025-08-13-121604-test-ci-op-njttt0ww-latest (from 4.20.0-0.ci-2025-08-13-114210-test-ci-op-njttt0ww-initial)
 Completion:      6% (2 operators updated, 1 updating, 31 waiting)
@@ -112,9 +112,9 @@ func TestMonitor_ControlPlane(t *testing.T) {
 		{
 			name: "good snapshots",
 			snapshots: []snapshot{
-				{when: time.Now(), out: exampleOutput},
-				{when: time.Now(), out: exampleOutput},
-				{when: time.Now(), out: exampleOutput},
+				{when: time.Now(), out: cpExampleOutput},
+				{when: time.Now(), out: cpExampleOutput},
+				{when: time.Now(), out: cpExampleOutput},
 			},
 			expected: &junitapi.JUnitTestCase{
 				Name: "[sig-cli][OCPFeatureGate:UpgradeStatus] oc adm upgrade status control plane section is consistent",
@@ -123,9 +123,9 @@ func TestMonitor_ControlPlane(t *testing.T) {
 		{
 			name: "errored snapshots are skipped",
 			snapshots: []snapshot{
-				{when: time.Now(), out: exampleOutput},
-				{when: time.Now(), out: badOutput, err: errors.New("some error")},
-				{when: time.Now(), out: exampleOutput},
+				{when: time.Now(), out: cpExampleOutput},
+				{when: time.Now(), out: cpBadOutput, err: errors.New("some error")},
+				{when: time.Now(), out: cpExampleOutput},
 			},
 			expected: &junitapi.JUnitTestCase{
 				Name: "[sig-cli][OCPFeatureGate:UpgradeStatus] oc adm upgrade status control plane section is consistent",
@@ -134,9 +134,9 @@ func TestMonitor_ControlPlane(t *testing.T) {
 		{
 			name: "unparseable snapshots are skipped",
 			snapshots: []snapshot{
-				{when: time.Now(), out: exampleOutput},
+				{when: time.Now(), out: cpExampleOutput},
 				{when: time.Now(), out: "unparseable output"},
-				{when: time.Now(), out: exampleOutput},
+				{when: time.Now(), out: cpExampleOutput},
 			},
 			expected: &junitapi.JUnitTestCase{
 				Name: "[sig-cli][OCPFeatureGate:UpgradeStatus] oc adm upgrade status control plane section is consistent",
