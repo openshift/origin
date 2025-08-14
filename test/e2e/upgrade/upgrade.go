95eb6ed9864b92dfb8c4b001919d9488101e4fe7
Vu Dinh <vudinh@outlook.com>
2025-08-14 12:00:43 -0400

    Add wait.Poll retry logic to checkUpgradeability with 30s timeout
    Signed-off-by: Vu Dinh <vudinh@outlook.com>

--- test/e2e/upgrade/upgrade.go
+++ test/e2e/upgrade/upgrade.go
@@ -188,9 +188,13 @@ var _ = g.Describe("[sig-arch][Feature:ClusterUpgrade]", func() {
 						clusterUpgrade(f, client, dynamicClient, config, upgCtx.Versions[i]),
 						fmt.Sprintf("during upgrade to %s", upgCtx.Versions[i].NodeImage))
 				}
-				// Sleep to give some time to the workloads on the last upgraded
-				// node to restart.
-				time.Sleep(30 * time.Second)
+				err = wait.PollImmediate(1*time.Second, 30*time.Second, func() (bool, error) {
+					if err := checkUpgradeability(client); err != nil {
+						framework.Logf("Upgradeability check failed, retrying: %v", err)
+						return false, nil // retry on error
+					}
+					return true, nil
+				})
 			},
 		)
 	})
@@ -199,18 +203,7 @@ var _ = g.Describe("[sig-arch][Feature:ClusterUpgrade]", func() {
 		config, err := framework.LoadConfig()
 		framework.ExpectNoError(err)
 		client := configv1client.NewForConfigOrDie(config)
-		var lastErr error
-		err = wait.PollImmediate(1*time.Second, 30*time.Second, func() (bool, error) {
-			if err := checkUpgradeability(client); err != nil {
-				lastErr = err
-				framework.Logf("Upgradeability check failed, retrying: %v", err)
-				return false, nil // retry on error
-			}
-			return true, nil
-		})
-		if err != nil && lastErr != nil {
-			err = lastErr
-		}
+		err = checkUpgradeability(client)
 		framework.ExpectNoError(err)
 	})
 })
