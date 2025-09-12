9810f24c1a9461e8113a963f788ea6f7a3552e8b
ehila <ehila@redhat.com>
2025-09-12 12:07:50 -0400

    upkeep: removed two node suite from regular checks
    updated tests to always target two node topology instead of being a suite tests
    small refactor and testing name change
    
    Signed-off-by: ehila <ehila@redhat.com>

--- test/extended/two_node/tnf_topology.go
+++ test/extended/two_node/tnf_topology.go
@@ -3,18 +3,18 @@ package two_node
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
 	v1 "github.com/openshift/api/config/v1"
 	exutil "github.com/openshift/origin/test/extended/util"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const ensurePodmanEtcdContainerIsRunning = "podman inspect --format '{{.State.Running}}' etcd"
 
-var _ = g.Describe("[sig-node][apigroup:config.openshift.io][OCPFeatureGate:DualReplica][Suite:openshift/two-node] Two Node with Fencing topology", func() {
+var _ = g.Describe("[sig-node][apigroup:config.openshift.io][OCPFeatureGate:DualReplica] Two Node with Fencing topology", func() {
 	defer g.GinkgoRecover()
 	var (
 		oc = exutil.NewCLIWithoutNamespace("")
@@ -24,13 +24,13 @@ var _ = g.Describe("[sig-node][apigroup:config.openshift.io][OCPFeatureGate:Dual
 		skipIfNotTopology(oc, v1.DualReplicaTopologyMode)
 	})
 
-	g.It("Should validate the number of control-planes, arbiters as configured", func() {
+	g.It("should only have two control plane nodes and no arbiter nodes", func() {
 		const (
 			expectedControlPlanes = 2
 			expectedArbiters      = 0
 		)
 
-		g.By(fmt.Sprintf("Ensuring only %d Control-plane nodes in the cluster, and %d Arbiter nodes", expectedControlPlanes, expectedArbiters))
+		g.By(fmt.Sprintf("Ensuring only %d control-plane nodes in the cluster and no arbiter nodes", expectedControlPlanes))
 		controlPlaneNodes, err := oc.AdminKubeClient().CoreV1().Nodes().List(context.Background(), metav1.ListOptions{
 			LabelSelector: labelNodeRoleControlPlane,
 		})
@@ -45,59 +45,56 @@ var _ = g.Describe("[sig-node][apigroup:config.openshift.io][OCPFeatureGate:Dual
 	})
 })
 
-var _ = g.Describe("[sig-etcd][apigroup:config.openshift.io][OCPFeatureGate:DualReplica][Suite:openshift/two-node] Two Node with Fencing pods and podman containers", func() {
+var _ = g.Describe("[sig-etcd][apigroup:config.openshift.io][OCPFeatureGate:DualReplica] Two Node with Fencing", func() {
 	defer g.GinkgoRecover()
 	var (
-		oc    = exutil.NewCLIWithoutNamespace("")
-		nodes *corev1.NodeList
+		oc = exutil.NewCLIWithoutNamespace("")
 	)
 
 	g.BeforeEach(func() {
 		skipIfNotTopology(oc, v1.DualReplicaTopologyMode)
-
-		var err error
-		nodes, err = oc.AdminKubeClient().CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
-		o.Expect(err).To(o.BeNil(), "Expected to retrieve all nodes without error")
 	})
-	g.It("Should validate the number of etcd pods and containers as configured", func() {
+	g.It("should have etcd pods and containers configured correctly", func() {
 		const (
 			expectedEtcdPod           = 2
 			expectedEtcdCtlContainers = 2
 			expectedEtcdContainers    = 0
 		)
 
-		nodeNameA := nodes.Items[0].Name
-		nodeNameB := nodes.Items[1].Name
-
 		g.By("Ensuring 0 etcd pod containers and 2 etcdctl pod containers are running in the cluster ")
-		pods, err := oc.AdminKubeClient().CoreV1().Pods("openshift-etcd").List(context.Background(), metav1.ListOptions{})
+		pods, err := oc.AdminKubeClient().CoreV1().Pods("openshift-etcd").List(context.Background(), metav1.ListOptions{
+			LabelSelector: "app=etcd",
+		})
 		o.Expect(err).To(o.BeNil(), "Expected to retrieve etcd pods in openshift-etcd namespace without error")
+		o.Expect(pods.Items).To(o.HaveLen(expectedEtcdPod), "Expected to retrieve %d etcd pods in openshift-etcd namespace", expectedEtcdPod)
 
-		etcdPodCount := 0
 		etcdContainerCount := 0
 		etcdctlContainerCount := 0
+
 		for _, pod := range pods.Items {
-			if pod.Name == "etcd-"+nodeNameA || pod.Name == "etcd-"+nodeNameB {
-				etcdPodCount += 1
-				for _, container := range pod.Spec.Containers {
-					if container.Name == "etcd" {
-						etcdContainerCount += 1
-					}
-					if container.Name == "etcdctl" {
-						etcdctlContainerCount += 1
-					}
+			for _, container := range pod.Spec.Containers {
+				if container.Name == "etcd" {
+					etcdContainerCount += 1
+				}
+				if container.Name == "etcdctl" {
+					etcdctlContainerCount += 1
 				}
 			}
 		}
-		o.Expect(etcdPodCount).To(o.Equal(expectedEtcdPod))
 		o.Expect(etcdctlContainerCount).To(o.Equal(expectedEtcdCtlContainers))
 		o.Expect(etcdContainerCount).To(o.Equal(expectedEtcdContainers))
 	})
 
-	g.It("Should verify the number of podman-etcd containers as configured", func() {
+	g.It("should have podman etcd containers running on each node", func() {
+		nodes, err := oc.AdminKubeClient().CoreV1().Nodes().List(context.Background(), metav1.ListOptions{
+			LabelSelector: labelNodeRoleControlPlane,
+		})
+		o.Expect(err).To(o.BeNil(), "Expected to retrieve control plane nodes without error")
+		o.Expect(nodes.Items).To(o.HaveLen(2), "Expected to retrieve two control plane nodes for DualReplica topology")
+
 		g.By("Ensuring one podman etcd container is running on each node")
 		for _, node := range nodes.Items {
-			got, err := exutil.DebugNodeRetryWithOptionsAndChroot(oc, node.Name, "openshift-etcd", "podman", "inspect", "--format", "'{{.State.Running}}'", "etcd")
+			got, err := exutil.DebugNodeRetryWithOptionsAndChroot(oc, node.Name, "openshift-etcd", strings.Split(ensurePodmanEtcdContainerIsRunning, " ")...)
 			o.Expect(err).To(o.BeNil(), fmt.Sprintf("expected to call podman without errors on Node %s: error %v", node.Name, err))
 			o.Expect(got).To(o.Equal("'true'"), fmt.Sprintf("expected a podman etcd container running on Node %s: got running %s", node.Name, got))
 		}
