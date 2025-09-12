5afc5f93eb202debfb1795cec39e6a11a86d20e0
ehila <ehila@redhat.com>
2025-09-12 13:02:32 -0400

    feat: add tests to validate DualReplica topology effects
    added test to check platform type is set to baremetal or none
    added test to check baremetal hosts are set to detached
    fixed suite filter to capture OCPFeatureGate
    
    Signed-off-by: ehila <ehila@redhat.com>

--- test/extended/two_node/tnf_topology.go
+++ test/extended/two_node/tnf_topology.go
@@ -10,6 +10,8 @@ import (
 	v1 "github.com/openshift/api/config/v1"
 	exutil "github.com/openshift/origin/test/extended/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 const ensurePodmanEtcdContainerIsRunning = "podman inspect --format '{{.State.Running}}' etcd"
@@ -43,6 +45,44 @@ var _ = g.Describe("[sig-node][apigroup:config.openshift.io][OCPFeatureGate:Dual
 		o.Expect(err).ShouldNot(o.HaveOccurred(), "Expected to retrieve arbiter nodes without error")
 		o.Expect(len(arbiterNodes.Items)).To(o.Equal(expectedArbiters), fmt.Sprintf("Expected %d Arbiter Nodes, found %d", expectedArbiters, len(arbiterNodes.Items)))
 	})
+
+	g.It("should have infrastructure platform type set correctly", func() {
+		g.By("Checking that the infrastructure platform is set to baremetal or none or external")
+		infrastructure, err := oc.AdminConfigClient().ConfigV1().Infrastructures().Get(context.Background(), "cluster", metav1.GetOptions{})
+		o.Expect(err).ShouldNot(o.HaveOccurred(), "Expected to retrieve infrastructure configuration without error")
+
+		platformType := infrastructure.Status.PlatformStatus.Type
+		o.Expect(platformType).To(o.Or(o.Equal(v1.BareMetalPlatformType), o.Equal(v1.NonePlatformType), o.Equal(v1.ExternalPlatformType)),
+			fmt.Sprintf("Expected infrastructure platform to be baremetal or none or external, but found %s", platformType))
+	})
+
+	g.It("should have BareMetalHost operational status set to detached if they exist", func() {
+		g.By("Checking that BareMetalHost objects have operational status set to detached")
+		dc := oc.AdminDynamicClient()
+
+		// Use Dynamic Client to get BareMetalHost objects
+		// Note: move this to common.go if this is used in other tests
+		baremetalGVR := schema.GroupVersionResource{Group: "metal3.io", Resource: "baremetalhosts", Version: "v1alpha1"}
+		baremetalClient := dc.Resource(baremetalGVR).Namespace("openshift-machine-api")
+
+		hosts, err := baremetalClient.List(context.Background(), metav1.ListOptions{})
+		o.Expect(err).ShouldNot(o.HaveOccurred(), "Expected to retrieve BareMetalHost objects without error")
+
+		// If no BareMetalHost objects exist, skip the test, this is valid for TNF deployments
+		if len(hosts.Items) == 0 {
+			g.Skip("No BareMetalHost objects found in openshift-machine-api namespace")
+		}
+
+		// Check each BareMetalHost has operational status set to detached
+		for _, host := range hosts.Items {
+			operationalStatus, found, err := unstructured.NestedString(host.Object, "status", "operationalStatus")
+			o.Expect(err).ShouldNot(o.HaveOccurred(), fmt.Sprintf("Expected to parse operational status for BareMetalHost %s without error", host.GetName()))
+			o.Expect(found).To(o.BeTrue(), fmt.Sprintf("Expected operational status field to exist for BareMetalHost %s", host.GetName()))
+			o.Expect(operationalStatus).To(o.Equal("detached"),
+				fmt.Sprintf("Expected BareMetalHost %s operational status to be 'detached', but found '%s'", host.GetName(), operationalStatus))
+		}
+	})
+
 })
 
 var _ = g.Describe("[sig-etcd][apigroup:config.openshift.io][OCPFeatureGate:DualReplica] Two Node with Fencing", func() {
