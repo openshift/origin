f23c2a84eeb779b066477df2f07d22df16f0a81b
Evan Hearne <ehearne@redhat.com>
2025-09-24 11:42:37 +0100

    move noisy code to helper funcs

--- pkg/monitortests/authentication/nodefaultserviceaccountoperatorstests/monitortest.go
+++ pkg/monitortests/authentication/nodefaultserviceaccountoperatorstests/monitortest.go
@@ -31,6 +31,78 @@ func (n *noDefaultServiceAccountChecker) Cleanup(ctx context.Context) error {
 	return nil
 }
 
+// fetchRelatedObjects returns a list of RelatedObjects found within a specified
+// namespace for a given list of cluster operators .
+func fetchRelatedObjects(clusterOperators *v1.ClusterOperatorList, ns corev1.Namespace) [][]v1.ObjectReference {
+	clusterOperatorsItems := []v1.ClusterOperator{} // used to filter on namespace
+
+	for _, item := range clusterOperators.Items {
+		if item.Namespace == ns.Name {
+			clusterOperatorsItems = append(clusterOperatorsItems, item)
+		}
+	}
+
+	// Grab the related objects within each cluster operator
+	clusterOperatorsItemsRelatedObjects := [][]v1.ObjectReference{}
+
+	for _, item := range clusterOperatorsItems {
+		// append related objects for each operator in the list
+		clusterOperatorsItemsRelatedObjects = append(clusterOperatorsItemsRelatedObjects, item.Status.RelatedObjects)
+	}
+
+	return clusterOperatorsItemsRelatedObjects
+}
+
+// fetchPodObjects returns a list of pod related objects from a list of related objects.
+func fetchPodObjects(clusterOperatorsItemsRelatedObjects [][]v1.ObjectReference) []v1.ObjectReference {
+	// create new list for pod items (object references to pods)
+	clusterOperatorPodObjects := []v1.ObjectReference{}
+	// filter any objects that are pods using the Group ref. use later to perform lookup on pods service accounts.
+	for _, item := range clusterOperatorsItemsRelatedObjects {
+		// check the kind of the object (i.e. is it Pod ?)
+		for _, item1 := range item {
+			if item1.Group == "pods" {
+				// add the item to the list
+				clusterOperatorPodObjects = append(clusterOperatorPodObjects, item1)
+			}
+		}
+	}
+	return clusterOperatorPodObjects
+}
+
+// fetchPodList returns a list of pods from a list of pod related objects based on a list of pods
+// retrieved elsewhere - in our case, from a namespace.
+func fetchPodList(clusterOperatorPodObjects []v1.ObjectReference, pods corev1.PodList) []corev1.Pod {
+	// create a pod list
+	podList := []corev1.Pod{}
+
+	// perform a lookup on the kubeclient to obtain all ACTUAL pod objects, which contain the `.Spec.ServiceAccountName` attribute.
+	for _, object := range clusterOperatorPodObjects {
+		for _, pod := range pods.Items {
+			if pod.Name == object.Name && pod.Namespace == object.Namespace { // this check ensures uniqueness because a pod name MUST be unique in a given namespace
+				podList = append(podList, pod)
+			}
+		}
+	}
+	return podList
+}
+
+// generateDefaultSAFailures generates a list of failures where the pod in a list of pods
+// violated the default service account check.
+func generateDefaultSAFailures(podList []corev1.Pod) []string {
+	failures := make([]string, 0)
+	for _, pod := range podList {
+		podSA := pod.Spec.ServiceAccountName
+		// if the service account name is not default, we can exit for that iteration
+		if podSA != "default" {
+			continue
+		}
+		// otherwise, we need to flag the failure
+		failures = append(failures, fmt.Sprintf("service account name %s is being used in pod %s", podSA, pod.Name))
+	}
+	return failures
+}
+
 // CollectData implements monitortestframework.MonitorTest.
 func (n *noDefaultServiceAccountChecker) CollectData(ctx context.Context, storageDir string, beginning time.Time, end time.Time) (monitorapi.Intervals, []*junitapi.JUnitTestCase, error) {
 
@@ -53,64 +125,26 @@ func (n *noDefaultServiceAccountChecker) CollectData(ctx context.Context, storag
 			continue
 		}
 
-		clusterOperatorsItems := []v1.ClusterOperator{} // used to filter on namespace
+		// use helper method to fetch related objects for cluster operators in the current ns iteration.
+		clusterOperatorsItemsRelatedObjects := fetchRelatedObjects(clusterOperators, ns)
 
-		for _, item := range clusterOperators.Items {
-			if item.Namespace == ns.Name {
-				clusterOperatorsItems = append(clusterOperatorsItems, item)
-			}
-		}
+		// use helper method to fetch pod related objects
+		clusterOperatorPodObjects := fetchPodObjects(clusterOperatorsItemsRelatedObjects)
 
-		// Grab the related objects within each cluster operator
-
-		clusterOperatorsItemsRelatedObjects := [][]v1.ObjectReference{}
-
-		for _, item := range clusterOperatorsItems {
-			// append related objects for each operator in the list
-			clusterOperatorsItemsRelatedObjects = append(clusterOperatorsItemsRelatedObjects, item.Status.RelatedObjects)
-		}
-
-		// create new list for pod items
-		clusterOperatorObjects := []v1.ObjectReference{}
-
-		// filter any objects that are pods using the Group ref. use later to perform lookup on pods service accounts.
-		for _, item := range clusterOperatorsItemsRelatedObjects {
-			// check the kind of the object (i.e. is it Pod ?)
-			for _, item1 := range item {
-				if item1.Group == "pods" {
-					// add the item to the list
-					clusterOperatorObjects = append(clusterOperatorObjects, item1)
-				}
-			}
-		}
-
-		// create a pod list
-		podList := []corev1.Pod{}
-
+		// get list of all pods in the namespace
 		pods, err := n.kubeClient.CoreV1().Pods(ns.Name).List(ctx, metav1.ListOptions{})
+
 		if err != nil {
 			return nil, nil, err
 		}
 
-		// perform a lookup on the kubeclient to obtain all ACTUAL pod objects, which contain the `.Spec.ServiceAccountName` attribute.
-		for _, object := range clusterOperatorObjects {
-			for _, pod := range pods.Items {
-				if pod.Name == object.Name && pod.Namespace == object.Namespace { // this check ensures uniqueness because a pod name MUST be unique in a given namespace
-					podList = append(podList, pod)
-				}
-			}
-		}
+		// use helper method to fetch pod list for pod related objects
+		podList := fetchPodList(clusterOperatorPodObjects, *pods)
 
-		failures := make([]string, 0)
-		for _, pod := range podList {
-			podSA := pod.Spec.ServiceAccountName
-			// if the service account name is not default, we can exit for that iteration
-			if podSA != "default" {
-				continue
-			}
-			// otherwise, we need to flag the failure
-			failures = append(failures, fmt.Sprintf("service account name %s is being used in pod %s", podSA, pod.Name))
-		}
+		// use helper method to generate default service account failures
+		failures := generateDefaultSAFailures(podList)
+
+		// generate tests for given namespace
 		testName := fmt.Sprintf("[sig-auth] all operators in ns/%s must not use the 'default' service account", ns.Name)
 		if len(failures) == 0 {
 			junits = append(junits, &junitapi.JUnitTestCase{Name: testName})
