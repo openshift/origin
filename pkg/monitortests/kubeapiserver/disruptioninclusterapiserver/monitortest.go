1cc65c6411fbfc88eddcefc8b6a2f26604325eac
wangke19 <kewang@redhat.com>
2025-10-16 12:35:56 +0800

    make update-gofmt

--- pkg/monitortests/kubeapiserver/disruptioninclusterapiserver/monitortest.go
+++ pkg/monitortests/kubeapiserver/disruptioninclusterapiserver/monitortest.go
@@ -78,7 +78,7 @@ type InvariantInClusterDisruption struct {
 	isAROHCPCluster             bool
 	isBareMetalHypershift       bool
 	adminRESTConfig             *rest.Config
-	kubeClient      kubernetes.Interface
+	kubeClient                  kubernetes.Interface
 }
 
 func NewInvariantInClusterDisruption(info monitortestframework.MonitorTestInitializationInfo) monitortestframework.MonitorTest {
