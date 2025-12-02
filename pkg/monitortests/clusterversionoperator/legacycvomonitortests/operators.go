d4c21febb9293603e478c8643b7b48f2d419bd8f
Hongkai Liu <hongkailiu@users.noreply.github.com>
2025-12-01 20:07:03 -0500

    Replacing OCPBUGS-23745 with OCPBUGS-66230
    OCPBUGS-23745 has been fixed and shipped with 4.15.
    However, the symptom is still there in 4.21 and we
    create OCPBUGS-66230 to track the issue.

--- pkg/monitortests/clusterversionoperator/legacycvomonitortests/operators.go
+++ pkg/monitortests/clusterversionoperator/legacycvomonitortests/operators.go
@@ -402,9 +402,8 @@ func testUpgradeOperatorStateTransitions(events monitorapi.Intervals, clientConf
 						condition.Reason == "UpdatingAlertmanagerFailed" ||
 						condition.Reason == "UpdatingConsolePluginComponentsFailed" ||
 						condition.Reason == "UpdatingPrometheusK8SFailed" ||
-						condition.Reason == "UpdatingPrometheusOperatorFailed")) ||
-				(condition.Status == configv1.ConditionUnknown && condition.Reason == "UpdatingPrometheusFailed") {
-				return "https://issues.redhat.com/browse/OCPBUGS-23745"
+						condition.Reason == "UpdatingPrometheusOperatorFailed")) {
+				return "https://issues.redhat.com/browse/OCPBUGS-66230"
 			}
 			if condition.Type == configv1.OperatorDegraded && condition.Status == configv1.ConditionTrue {
 				return "https://issues.redhat.com/browse/OCPBUGS-39026"
