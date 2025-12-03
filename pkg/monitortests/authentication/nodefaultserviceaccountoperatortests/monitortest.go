3f22edfc422bd036088989475149faf628945a86
Evan Hearne <ehearne@redhat.com>
2025-12-03 14:09:04 +0000

    bring back ns level testing + exempt dynamic ns names

--- pkg/monitortests/authentication/nodefaultserviceaccountoperatortests/monitortest.go
+++ pkg/monitortests/authentication/nodefaultserviceaccountoperatortests/monitortest.go
@@ -80,11 +80,12 @@ var exceptions = []func(pod corev1.Pod) (string, bool){
 
 // generateTestCases evaluates that no pods in the provided namespace are using the default service account.
 // It returns the evaluated test cases or an error if any errors are encountered during the evaluation of the namespace.
-func (n *noDefaultServiceAccountChecker) generateTestCases(ctx context.Context, testName string, namespace string) ([]*junitapi.JUnitTestCase, error) {
+func (n *noDefaultServiceAccountChecker) generateTestCases(ctx context.Context, namespace string) ([]*junitapi.JUnitTestCase, error) {
 	podList, err := n.kubeClient.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
+	testName := fmt.Sprintf("[sig-auth] all pods in %s namespace must not use the default service account.", namespace)
 	junits := []*junitapi.JUnitTestCase{}
 	exceptionList := []string{}
 	failureList := []string{}
@@ -97,7 +98,7 @@ func (n *noDefaultServiceAccountChecker) generateTestCases(ctx context.Context, 
 		}
 		isException := false
 
-		failure := fmt.Sprintf("pod %q/%q is using the default service account", namespace, pod.Name)
+		failure := fmt.Sprintf("pod %q is using the default service account", pod.Name)
 		for _, exception := range exceptions {
 			if msg, ok := exception(pod); ok {
 				failure += fmt.Sprintf(" (exception: %s)", msg)
@@ -161,19 +162,21 @@ func (n *noDefaultServiceAccountChecker) CollectData(ctx context.Context, storag
 	if n.cfgClient == nil || n.kubeClient == nil {
 		return nil, nil, nil
 	}
-	testName := "[sig-auth] all pods must not use the default service account." // static name to ensure no ci failure
 	namespaces, err := n.kubeClient.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, nil, err
 	}
 	junits := []*junitapi.JUnitTestCase{}
 	for _, ns := range namespaces.Items {
-		if !strings.HasPrefix(ns.Name, "openshift") && !strings.HasPrefix(ns.Name, "kube-") && ns.Name != "default" {
+		if ns.GenerateName != "" { // dynamically generated namespace names are exempt from checks
+			continue
+		}
+		if !strings.HasPrefix(ns.Name, "openshift-") && !strings.HasPrefix(ns.Name, "kube-") && ns.Name != "default" {
 			continue
 		}
 
 		// use helper method to generate default service account failures
-		testCases, err := n.generateTestCases(ctx, testName, ns.Name)
+		testCases, err := n.generateTestCases(ctx, ns.Name)
 		if err != nil {
 			return nil, nil, err
 		}
