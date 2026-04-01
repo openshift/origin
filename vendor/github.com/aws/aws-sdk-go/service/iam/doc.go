c503cfbdefdead61715a29077bc7c0a3c2eaf7d3
Thuan Vo <thvo@redhat.com>
2026-04-01 11:46:24 -0700

    go mod vendor

--- vendor/github.com/aws/aws-sdk-go/service/iam/doc.go
+++ vendor/github.com/aws/aws-sdk-go/service/iam/doc.go
@@ -30,4 +30,7 @@
 // See the AWS Identity and Access Management client IAM for more
 // information on creating client for this service.
 // https://docs.aws.amazon.com/sdk-for-go/api/service/iam/#New
+//
+// Deprecated: aws-sdk-go is deprecated. Use aws-sdk-go-v2.
+// See https://aws.amazon.com/blogs/developer/announcing-end-of-support-for-aws-sdk-for-go-v1-on-july-31-2025/.
 package iam
