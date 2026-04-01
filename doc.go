797fc9751c7c11dfa9c0c956d55e2dacbeb71f5c
Deep Mistry <dmistry@redhat.com>
2026-04-01 14:37:41 -0400

    Test PR: minor doc.go change
    This is a test commit to verify CI pipeline behavior.
    Can be safely reverted.

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 // This is the source repository for OpenShift Origin - the best way to build, manage, and deploy
 // applications in the cloud.  The OpenShift 3.0 codebase is based around container images and containers
 // and the Kubernetes container management system.
+//
+// Test change - can be reverted.
 package origin
