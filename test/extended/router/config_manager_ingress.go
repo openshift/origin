e17d2f0c357f66f1f14ff49fbf097f7356fa8172
Joao Morais <jomorais@redhat.com>
2026-04-10 17:40:01 -0300

    DCM tests need to be backward compatible
    DCM tests need to be backward compatible with unsupported annotation.
    This is a temporary override during the API promotion.
    
    https://redhat.atlassian.net/browse/OCPBUGS-82557

--- test/extended/router/config_manager_ingress.go
+++ test/extended/router/config_manager_ingress.go
@@ -93,7 +93,7 @@ var _ = g.Describe("[sig-network-edge][Feature:Router][apigroup:route.openshift.
 					ConfigurationManagement: operatorv1.IngressControllerConfigurationManagementDynamic,
 				},
 				UnsupportedConfigOverrides: runtime.RawExtension{
-					Raw: fmt.Appendf(nil, `{"maxDynamicServers":"%d"}`, maxDynamicServers),
+					Raw: fmt.Appendf(nil, `{"dynamicConfigManager":"true","maxDynamicServers":"%d"}`, maxDynamicServers),
 				},
 			},
 		}
