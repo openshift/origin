fc9427243c4a402d74471247bf7c99243ebe75b5
Rohit Patil <ropatil@ropatil-thinkpadp16vgen1.bengluru.csb>
2026-04-23 09:23:24 +0530

    Remove unused apierrors import
    Remove k8s.io/apimachinery/pkg/api/errors import that is no longer
    needed after refactoring to use discovery API for baremetal detection.
    
    Co-Authored-By: Rohit Patil <ropatil@redhat.com>

--- test/extended/machines/scale.go
+++ test/extended/machines/scale.go
@@ -14,7 +14,6 @@ import (
 	bmhelper "github.com/openshift/origin/test/extended/baremetal"
 	"github.com/stretchr/objx"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
