272b38627f3b62c1ab1586c4e23f7baa006c3f24
rrasouli <rrasouli@redhat.com>
2026-07-16 14:48:58 +0300

    WINC-1966: Register WMCO OTE binary in extension registry

--- pkg/test/extensions/binary.go
+++ pkg/test/extensions/binary.go
@@ -345,6 +345,10 @@ var extensionBinaries = []TestBinary{
 		imageTag:   "vsphere-csi-driver-operator",
 		binaryPath: "/usr/bin/vmware-vsphere-csi-driver-operator-tests-ext.gz",
 	},
+	{
+		imageTag:   "windows-machine-config-operator-test",
+		binaryPath: "/usr/bin/wmco-tests-ext.gz",
+	},
 }
 
 // extractJSON finds the first JSON object or array in output, skipping any non-JSON log lines
