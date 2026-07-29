0d77e2989c6eb5236433d426cfc993e413b8ecb4
Stephen Benjamin <stephen@redhat.com>
2026-07-29 16:01:12 -0400

    test(upgrade): always clean up after setup failures
    Signed-off-by: Stephen Benjamin <stephen@redhat.com>

--- test/extended/util/disruption/disruption.go
+++ test/extended/util/disruption/disruption.go
@@ -177,6 +177,14 @@ type chaosMonkeyAdapter struct {
 	framework       *framework.Framework
 }
 
+func runUpgradeTest(ctx context.Context, test upgrades.Test, f *framework.Framework, ready func(), stopCh <-chan struct{}, upgradeType upgrades.UpgradeType) {
+	// upgrades.Test requires teardown to run even when setup fails.
+	defer test.Teardown(ctx, f)
+	test.Setup(ctx, f)
+	ready()
+	test.Test(ctx, f, stopCh, upgradeType)
+}
+
 func (cma *chaosMonkeyAdapter) Test(ctx context.Context, sem *chaosmonkey.Semaphore) {
 	start := time.Now()
 	var once sync.Once
@@ -198,10 +206,7 @@ func (cma *chaosMonkeyAdapter) Test(ctx context.Context, sem *chaosmonkey.Semaph
 		return
 	}
 	cma.framework.BeforeEach(ctx)
-	cma.test.Setup(ctx, cma.framework)
-	defer cma.test.Teardown(ctx, cma.framework)
-	ready()
-	cma.test.Test(ctx, cma.framework, sem.StopCh, cma.UpgradeType)
+	runUpgradeTest(ctx, cma.test, cma.framework, ready, sem.StopCh, cma.UpgradeType)
 }
 
 func finalizeTest(start time.Time, testName, className string, ts *junitapi.JUnitTestSuite, f *framework.Framework) {
