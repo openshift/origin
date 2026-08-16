f93ab3b8485934afda5fbafc9016ab36ff2a1fef
Neeraj Krishna Gopalakrishna <ngopalak@redhat.com>
2026-08-16 11:29:17 +0530

    Run long running tests in parallel

--- test/extended/node/node_e2e/node.go
+++ test/extended/node/node_e2e/node.go
@@ -34,41 +34,25 @@ var _ = g.Describe("[sig-node] [Jira:Node/Kubelet] [NodeResource:numNodes=1,labe
 		var kubelet string
 		var err error
 
-		g.By("Polling to check kubelet log level on ready nodes")
+		node, err := nodeutils.GetNodeResource(ctx, oc, "node_e2e")
+		o.Expect(err).NotTo(o.HaveOccurred(), "Error getting NodeResource node")
+
+		g.By("Polling to check kubelet log level on node " + node)
 		waitErr := wait.Poll(10*time.Second, 1*time.Minute, func() (bool, error) {
-			g.By("Getting all node names in the cluster")
-			nodeName, nodeErr := oc.AsAdmin().Run("get").Args("nodes", "-o=jsonpath={.items[*].metadata.name}").Output()
-			o.Expect(nodeErr).NotTo(o.HaveOccurred())
-			e2e.Logf("\nNode Names are %v", nodeName)
-			nodes := strings.Fields(nodeName)
+			g.By("Checking KUBELET_LOG_LEVEL in kubelet.service on node " + node)
+			kubeservice, err = nodeutils.ExecOnNodeWithChroot(ctx, oc, node, "/bin/bash", "-c", "systemctl show kubelet.service | grep KUBELET_LOG_LEVEL")
+			o.Expect(err).NotTo(o.HaveOccurred())
 
-			for _, node := range nodes {
-				g.By("Checking if node " + node + " is Ready")
-				nodeStatus, statusErr := oc.AsAdmin().Run("get").Args("nodes", node, "-o=jsonpath={.status.conditions[?(@.type=='Ready')].status}").Output()
-				o.Expect(statusErr).NotTo(o.HaveOccurred())
-				e2e.Logf("\nNode %s Status is %s\n", node, nodeStatus)
+			g.By("Checking kubelet process for --v=2 flag on node " + node)
+			kubelet, err = nodeutils.ExecOnNodeWithChroot(ctx, oc, node, "/bin/bash", "-c", "ps aux | grep [k]ubelet")
+			o.Expect(err).NotTo(o.HaveOccurred())
 
-				if nodeStatus == "True" {
-					g.By("Checking KUBELET_LOG_LEVEL in kubelet.service on node " + node)
-					kubeservice, err = nodeutils.ExecOnNodeWithChroot(ctx, oc, node, "/bin/bash", "-c", "systemctl show kubelet.service | grep KUBELET_LOG_LEVEL")
-					o.Expect(err).NotTo(o.HaveOccurred())
-
-					g.By("Checking kubelet process for --v=2 flag on node " + node)
-					kubelet, err = nodeutils.ExecOnNodeWithChroot(ctx, oc, node, "/bin/bash", "-c", "ps aux | grep [k]ubelet")
-					o.Expect(err).NotTo(o.HaveOccurred())
-
-					g.By("Verifying KUBELET_LOG_LEVEL is set and kubelet is running with --v=2")
-					if strings.Contains(kubeservice, "KUBELET_LOG_LEVEL") && strings.Contains(kubelet, "--v=2") {
-						e2e.Logf("KUBELET_LOG_LEVEL is 2.\n")
-						return true, nil
-					} else {
-						e2e.Logf("KUBELET_LOG_LEVEL is not 2.\n")
-						return false, nil
-					}
-				} else {
-					e2e.Logf("\nNode %s is not Ready, Skipping\n", node)
-				}
+			g.By("Verifying KUBELET_LOG_LEVEL is set and kubelet is running with --v=2")
+			if strings.Contains(kubeservice, "KUBELET_LOG_LEVEL") && strings.Contains(kubelet, "--v=2") {
+				e2e.Logf("KUBELET_LOG_LEVEL is 2.\n")
+				return true, nil
 			}
+			e2e.Logf("KUBELET_LOG_LEVEL is not 2.\n")
 			return false, nil
 		})
 
